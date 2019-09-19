185d30cabf4571b65d9015f1069ad66f40cd5c3d
Ben Vanik <benvanik@google.com>
2019-09-19 14:24:11 -0700

    Initial CMake files for base/ to test how things work. Not yet complete.
    PiperOrigin-RevId: 270124613

---
+++ build_tools/cmake/iree_cc_library.cmake
@@ -0,0 +1,153 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include(CMakeParseArguments)
+
+# iree_cc_library()
+#
+# CMake function to imitate Bazel's cc_library rule.
+#
+# Parameters:
+# NAME: name of target (see Note)
+# HDRS: List of public header files for the library
+# SRCS: List of source files for the library
+# DEPS: List of other libraries to be linked in to the binary targets
+# COPTS: List of private compile options
+# DEFINES: List of public defines
+# LINKOPTS: List of link options
+# PUBLIC: Add this so that this library will be exported under iree::
+# Also in IDE, target will appear in IREE folder while non PUBLIC will be in IREE/internal.
+# TESTONLY: When added, this target will only be built if user passes -DIREE_RUN_TESTS=ON to CMake.
+#
+# Note:
+# By default, iree_cc_library will always create a library named iree_${NAME},
+# and alias target iree::${NAME}. The iree:: form should always be used.
+# This is to reduce namespace pollution.
+#
+# iree_cc_library(
+#   NAME
+#     awesome
+#   HDRS
+#     "a.h"
+#   SRCS
+#     "a.cc"
+# )
+# iree_cc_library(
+#   NAME
+#     fantastic_lib
+#   SRCS
+#     "b.cc"
+#   DEPS
+#     iree::awesome # not "awesome" !
+#   PUBLIC
+# )
+#
+# iree_cc_library(
+#   NAME
+#     main_lib
+#   ...
+#   DEPS
+#     iree::fantastic_lib
+# )
+#
+# TODO: Implement "ALWAYSLINK"
+function(iree_cc_library)
+  cmake_parse_arguments(IREE_CC_LIB
+    "PUBLIC;TESTONLY"
+    "NAME"
+    "HDRS;SRCS;COPTS;DEFINES;LINKOPTS;DEPS"
+    ${ARGN}
+  )
+
+  if(NOT IREE_CC_LIB_TESTONLY OR IREE_RUN_TESTS)
+    set(_NAME "iree_${IREE_CC_LIB_NAME}")
+
+    # Check if this is a header-only library
+    # Note that as of February 2019, many popular OS's (for example, Ubuntu
+    # 16.04 LTS) only come with cmake 3.5 by default.  For this reason, we can't
+    # use list(FILTER...)
+    set(IREE_CC_SRCS "${IREE_CC_LIB_SRCS}")
+    foreach(src_file IN LISTS IREE_CC_SRCS)
+      if(${src_file} MATCHES ".*\\.(h|inc)")
+        list(REMOVE_ITEM IREE_CC_SRCS "${src_file}")
+      endif()
+    endforeach()
+    if("${IREE_CC_SRCS}" STREQUAL "")
+      set(IREE_CC_LIB_IS_INTERFACE 1)
+    else()
+      set(IREE_CC_LIB_IS_INTERFACE 0)
+    endif()
+
+    if(NOT IREE_CC_LIB_IS_INTERFACE)
+      add_library(${_NAME} STATIC "")
+      target_sources(${_NAME} PRIVATE ${IREE_CC_LIB_SRCS} ${IREE_CC_LIB_HDRS})
+      target_include_directories(${_NAME}
+        PUBLIC
+          "$<BUILD_INTERFACE:${IREE_COMMON_INCLUDE_DIRS}>"
+      )
+      target_compile_options(${_NAME}
+        PRIVATE
+          ${IREE_CC_LIB_COPTS}
+          ${IREE_DEFAULT_COPTS}
+      )
+      target_link_libraries(${_NAME}
+        PUBLIC
+          ${IREE_CC_LIB_DEPS}
+        PRIVATE
+          ${IREE_CC_LIB_LINKOPTS}
+          ${IREE_DEFAULT_LINKOPTS}
+      )
+      target_compile_definitions(${_NAME} PUBLIC ${IREE_CC_LIB_DEFINES})
+
+      # Add all IREE targets to a a folder in the IDE for organization.
+      if(IREE_CC_LIB_PUBLIC)
+        set_property(TARGET ${_NAME} PROPERTY FOLDER ${IREE_IDE_FOLDER})
+      elseif(IREE_CC_LIB_TESTONLY)
+        set_property(TARGET ${_NAME} PROPERTY FOLDER ${IREE_IDE_FOLDER}/test)
+      else()
+        set_property(TARGET ${_NAME} PROPERTY FOLDER ${IREE_IDE_FOLDER}/internal)
+      endif()
+
+      # INTERFACE libraries can't have the CXX_STANDARD property set
+      set_property(TARGET ${_NAME} PROPERTY CXX_STANDARD ${IREE_CXX_STANDARD})
+      set_property(TARGET ${_NAME} PROPERTY CXX_STANDARD_REQUIRED ON)
+
+      # When being installed, we lose the iree_ prefix.  We want to put it back
+      # to have properly named lib files.  This is a no-op when we are not being
+      # installed.
+      set_target_properties(${_NAME} PROPERTIES
+        OUTPUT_NAME "iree_${_NAME}"
+      )
+    else()
+      # Generating header-only library
+      add_library(${_NAME} INTERFACE)
+      target_include_directories(${_NAME}
+        INTERFACE
+          "$<BUILD_INTERFACE:${IREE_COMMON_INCLUDE_DIRS}>"
+        )
+      target_link_libraries(${_NAME}
+        INTERFACE
+          ${IREE_CC_LIB_DEPS}
+          ${IREE_CC_LIB_LINKOPTS}
+          ${IREE_DEFAULT_LINKOPTS}
+      )
+      target_compile_definitions(${_NAME}
+        INTERFACE
+          ${IREE_CC_LIB_DEFINES}
+      )
+    endif()
+
+    add_library(iree::${IREE_CC_LIB_NAME} ALIAS ${_NAME})
+  endif()
+endfunction()
---
+++ build_tools/cmake/iree_cc_test.cmake
@@ -0,0 +1,100 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include(CMakeParseArguments)
+
+# iree_cc_test()
+#
+# CMake function to imitate Bazel's cc_test rule.
+#
+# Parameters:
+# NAME: name of target (see Usage below)
+# SRCS: List of source files for the binary
+# DEPS: List of other libraries to be linked in to the binary targets
+# COPTS: List of private compile options
+# DEFINES: List of public defines
+# LINKOPTS: List of link options
+#
+# Note:
+# By default, iree_cc_test will always create a binary named iree_${NAME}.
+# This will also add it to ctest list as iree_${NAME}.
+#
+# Usage:
+# iree_cc_library(
+#   NAME
+#     awesome
+#   HDRS
+#     "a.h"
+#   SRCS
+#     "a.cc"
+#   PUBLIC
+# )
+#
+# iree_cc_test(
+#   NAME
+#     awesome_test
+#   SRCS
+#     "awesome_test.cc"
+#   DEPS
+#     iree::awesome
+#     gmock
+#     gtest_main
+# )
+function(iree_cc_test)
+  if(NOT IREE_RUN_TESTS)
+    return()
+  endif()
+
+  cmake_parse_arguments(IREE_CC_TEST
+    ""
+    "NAME"
+    "SRCS;COPTS;DEFINES;LINKOPTS;DEPS"
+    ${ARGN}
+  )
+
+  set(_NAME "iree_${IREE_CC_TEST_NAME}")
+  add_executable(${_NAME} "")
+  target_sources(${_NAME}
+    PRIVATE
+      ${IREE_CC_TEST_SRCS}
+  )
+  target_include_directories(${_NAME}
+    PUBLIC
+      ${IREE_COMMON_INCLUDE_DIRS}
+    PRIVATE
+      ${GMOCK_INCLUDE_DIRS}
+      ${GTEST_INCLUDE_DIRS}
+  )
+  target_compile_definitions(${_NAME}
+    PUBLIC
+      ${IREE_CC_TEST_DEFINES}
+  )
+  target_compile_options(${_NAME}
+    PRIVATE
+      ${IREE_CC_TEST_COPTS}
+  )
+  target_link_libraries(${_NAME}
+    PUBLIC
+      ${IREE_CC_TEST_DEPS} gmock gunit
+    PRIVATE
+      ${IREE_CC_TEST_LINKOPTS}
+  )
+  # Add all Abseil targets to a a folder in the IDE for organization.
+  set_property(TARGET ${_NAME} PROPERTY FOLDER ${IREE_IDE_FOLDER}/test)
+
+  set_property(TARGET ${_NAME} PROPERTY CXX_STANDARD ${IREE_CXX_STANDARD})
+  set_property(TARGET ${_NAME} PROPERTY CXX_STANDARD_REQUIRED ON)
+
+  add_test(NAME ${_NAME} COMMAND ${_NAME})
+endfunction()
