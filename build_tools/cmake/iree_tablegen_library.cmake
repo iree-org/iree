b5b1cf6209f08482bbdd633e4498e00b1be1a838
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2019-12-04 08:35:33 +0100

    Allow setting tablegen implementation; defaults to MLIR

--- build_tools/cmake/iree_tablegen_library.cmake
+++ build_tools/cmake/iree_tablegen_library.cmake
@@ -21,7 +21,7 @@ function(iree_tablegen_library)
   cmake_parse_arguments(
     _RULE
     "TESTONLY"
-    "NAME"
+    "NAME;TBLGEN"
     "SRCS;OUTS"
     ${ARGN}
   )
@@ -30,6 +30,12 @@ function(iree_tablegen_library)
     # Prefix the library with the package name, so we get: iree_package_name
     iree_package_name(_PACKAGE_NAME)
     set(_NAME "${_PACKAGE_NAME}_${_RULE_NAME}")
+
+    if(${_RULE_TBLGEN} MATCHES "IREE")
+      set(_TBLGEN "IREE")
+    else()
+      set(_TBLGEN "MLIR")
+    endif()
 
     set(LLVM_TARGET_DEFINITIONS ${_RULE_SRCS})
     set(_INCLUDE_DIRS ${IREE_COMMON_INCLUDE_DIRS})
@@ -41,7 +47,7 @@ function(iree_tablegen_library)
       list(REMOVE_AT _RULE_OUTS 0)
       list(GET _RULE_OUTS 0 _FILE)
       list(REMOVE_AT _RULE_OUTS 0)
-      tablegen(MLIR ${_FILE} ${_COMMAND} ${_INCLUDE_DIRS})
+      tablegen(${_TBLGEN} ${_FILE} ${_COMMAND} ${_INCLUDE_DIRS})
       list(APPEND _OUTPUTS ${CMAKE_CURRENT_BINARY_DIR}/${_FILE})
     endwhile()
     add_custom_target(${_NAME}_target DEPENDS ${_OUTPUTS})
