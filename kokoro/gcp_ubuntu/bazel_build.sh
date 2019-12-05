ebe4b0383a269acf255fec3388fdfa530faadc19
Geoffrey Martin-Noble <gcmn@google.com>
2019-12-05 11:29:09 -0800

    Install clang 6.0 and remove SSH debug hook

--- kokoro/gcp_ubuntu/bazel_build.sh
+++ kokoro/gcp_ubuntu/bazel_build.sh
@@ -24,11 +24,13 @@ set -e
 set -x
 
 # Hackery to allow SSHing into the VM for debugging 
-echo "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEHXMM+6DzYMf3u/EEGGYKANayUoPLNFR7uQO/qYaw/jI+Nj+sYmN/fB8CAQTG/Dvjpzl8kMXLMqbDFuSALA4OA= you@gnubby.key" >> ~/.ssh/authorized_keys
-external_ip=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
-echo "INSTANCE_EXTERNAL_IP=${external_ip}"
-sleep 3600 # Give me an hour to fiddle with this
+# echo "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEHXMM+6DzYMf3u/EEGGYKANayUoPLNFR7uQO/qYaw/jI+Nj+sYmN/fB8CAQTG/Dvjpzl8kMXLMqbDFuSALA4OA= you@gnubby.key" >> ~/.ssh/authorized_keys
+# external_ip=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+# echo "INSTANCE_EXTERNAL_IP=${external_ip}"
+# sleep 3600 # Give me an hour to fiddle with this
 
+echo "Deleting corrupted ppa sources"
+sudo rm -rf /etc/apt/sources.list.d/nvidia-docker.list*
 
 echo "Installing bazel $BAZEL_VERSION"
 export BAZEL_VERSION=1.1.0
@@ -39,11 +41,18 @@ chmod +x bazel-$BAZEL_VERSION-installer-linux-x86_64.sh
 export PATH=$HOME/bin:$PATH
 bazel --version
 
+echo "Installing clang 6.0"
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-6.0 main"
+sudo apt-get update
+sudo apt-get install -y clang-6.0
+clang++-6.0 --version
+
 python3 -V
 
 echo "Preparing environment variables"
-export CXX=clang++-5.0
-export CC=clang-5.0
+export CXX=clang++-6.0
+export CC=clang-6.0
 export PYTHON_BIN="$(which python3)"
 
 echo "$CXX"
