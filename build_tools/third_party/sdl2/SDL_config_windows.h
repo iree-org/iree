607f30b6bef7d328dc2f8838d4f0922acfc7d4f4
Scott Todd <scotttodd@google.com>
2019-12-10 18:32:47 -0800

    Remove OpenGL support from SDL2 config and build, just use Vulkan.

--- build_tools/third_party/sdl2/SDL_config_windows.h
+++ build_tools/third_party/sdl2/SDL_config_windows.h
@@ -187,35 +187,35 @@ typedef unsigned int uintptr_t;
 /* Enable various timer systems */
 #define SDL_TIMER_WINDOWS 1
 
-/* Enable various video drivers */
+/* Enable/disable various video drivers */
 #define SDL_VIDEO_DRIVER_DUMMY 1
 #define SDL_VIDEO_DRIVER_WINDOWS 1
 
 #ifndef SDL_VIDEO_RENDER_D3D
-#define SDL_VIDEO_RENDER_D3D 1
+#define SDL_VIDEO_RENDER_D3D 0
 #endif
 #ifndef SDL_VIDEO_RENDER_D3D11
 #define SDL_VIDEO_RENDER_D3D11 0
 #endif
 
-/* Enable OpenGL support */
+/* Disable OpenGL support, just use Vulkan */
 #ifndef SDL_VIDEO_OPENGL
-#define SDL_VIDEO_OPENGL 1
+#define SDL_VIDEO_OPENGL 0
 #endif
 #ifndef SDL_VIDEO_OPENGL_WGL
-#define SDL_VIDEO_OPENGL_WGL 1
+#define SDL_VIDEO_OPENGL_WGL 0
 #endif
 #ifndef SDL_VIDEO_RENDER_OGL
-#define SDL_VIDEO_RENDER_OGL 1
+#define SDL_VIDEO_RENDER_OGL 0
 #endif
 #ifndef SDL_VIDEO_RENDER_OGL_ES2
-#define SDL_VIDEO_RENDER_OGL_ES2 1
+#define SDL_VIDEO_RENDER_OGL_ES2 0
 #endif
 #ifndef SDL_VIDEO_OPENGL_ES2
-#define SDL_VIDEO_OPENGL_ES2 1
+#define SDL_VIDEO_OPENGL_ES2 0
 #endif
 #ifndef SDL_VIDEO_OPENGL_EGL
-#define SDL_VIDEO_OPENGL_EGL 1
+#define SDL_VIDEO_OPENGL_EGL 0
 #endif
 
 /* Enable Vulkan support */
