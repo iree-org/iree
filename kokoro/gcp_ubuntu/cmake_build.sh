ab3b490becc0e8e37d37d420bb1be0c7b5685cd1
Geoffrey Martin-Noble <gcmn@google.com>
2020-01-15 12:02:56 -0800

    Add build script for CMake on kokoro

---
+++ kokoro/gcp_ubuntu/cmake_build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Build the project with bazel using Kokoro.
+
+set -e
+set -x
+
+# Print the UTC time when set -x is on
+export PS4='[$(date -u "+%T %Z")] '
+
+# Check these exist and print the versions for later debugging
+cmake --version
+clang --version
+clang++ --version
+
+# Kokoro checks out the repository here.
+cd ${KOKORO_ARTIFACTS_DIR?}/github/iree
+echo "Initializing submodules"
+./git_scripts/submodule_versions.py init
+
+echo "Building and testing with bazel"
+./build_tools/scripts/cmake_build.sh
