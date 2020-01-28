616b7e54bd9c630379ca0dbcd2de09b76b43b825
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2020-01-28 14:48:30 -0800

    Refactor iree_alwayslink.cmake
    * Splits setting ALWAYSLINK properties into multiple function calls
    * Sets ALWAYSLINK directly after the config for targets is generated
    * Makes checking if a target exists optional (generate an error by
      default if a target does not exist)
    * Reintroduces checking for alias targets (e.g. tensorflow normaly is)
    
    Closes https://github.com/google/iree/issues/496
    
    Closes https://github.com/google/iree/pull/585
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/google/iree/pull/585 from iml130:fix-496 16e6fa23a7b0702217121291108ed2aa62ec3775
    PiperOrigin-RevId: 292018336

--- build_tools/cmake/iree_alwayslink.cmake
+++ build_tools/cmake/iree_alwayslink.cmake
@@ -12,30 +12,81 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+include(CMakeParseArguments)
+
 # Additional libraries containing statically registered functions/flags, which
 # should always be linked in to binaries.
 
-set(_ALWAYSLINK_LIBS
-  MLIRAffineOps
-  MLIRAnalysis
-  MLIREDSC
-  MLIRIR
-  MLIRParser
-  MLIRPass
-  MLIRSPIRV
-  MLIRSPIRVSerialization
-  MLIRSPIRVTransforms
-  MLIRStandardOps
-  MLIRTransforms
-  MLIRTranslation
-  MLIRSupport
-  MLIRVectorOps
-  MLIRLinalgOps
-  tensorflow::mlir_xla
-)
 
-foreach(LIB ${_ALWAYSLINK_LIBS})
-  if(TARGET LIB)
-    set_property(TARGET ${LIB} PROPERTY ALWAYSLINK 1)
-  endif()
-endforeach(LIB)
+# set_alwayslink_property()
+#
+# CMake function to set the ALWAYSLINK on external libraries
+#
+# Parameters:
+# ALWAYSLINK_LIBS: List of libraries
+# SKIP_NONEXISTING: When added, ALWAYSLINK is only set on existing libraries.
+
+function(set_alwayslink_property)
+  cmake_parse_arguments(
+    _RULE
+    "SKIP_NONEXISTING"
+    ""
+    "ALWAYSLINK_LIBS"
+    ${ARGN}
+  )
+
+  foreach(_LIB ${_RULE_ALWAYSLINK_LIBS})
+    # If SKIP_NONEXISTING is false: Always try to set the property.
+    # If SKIP_NONEXISTING is true : Only set the property if the target exists.
+    if(NOT TARGET ${_LIB} AND _RULE_SKIP_NONEXISTING)
+      continue()
+    endif()
+
+    # Check if the target is an aliased target.
+    # If so get the non aliased target.
+    get_target_property(_ALIASED_TARGET ${_LIB} ALIASED_TARGET)
+    if(_ALIASED_TARGET)
+      set(_LIB ${_ALIASED_TARGET})
+    endif()
+
+    set_property(TARGET ${_LIB} PROPERTY ALWAYSLINK 1)
+  endforeach()
+endfunction()
+
+
+function(set_alwayslink_mlir_libs)
+  set(_ALWAYSLINK_LIBS_MLIR
+    MLIRAffineOps
+    MLIRAnalysis
+    MLIREDSC
+    MLIRIR
+    MLIRParser
+    MLIRPass
+    MLIRSPIRV
+    MLIRSPIRVSerialization
+    MLIRSPIRVTransforms
+    MLIRStandardOps
+    MLIRTransforms
+    MLIRTranslation
+    MLIRSupport
+    MLIRVectorOps
+    MLIRLinalgOps
+  )
+
+  set_alwayslink_property(
+    ALWAYSLINK_LIBS
+      ${_ALWAYSLINK_LIBS_MLIR}
+  )
+endfunction()
+
+
+function(set_alwayslink_tensorflow_libs)
+  set(_ALWAYSLINK_LIBS_TENSORFLOW
+    tensorflow::mlir_xla
+  )
+
+  set_alwayslink_property(
+    ALWAYSLINK_LIBS
+      ${_ALWAYSLINK_LIBS_TENSORFLOW}
+  )
+endfunction()
