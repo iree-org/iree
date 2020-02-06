83c0865659c1d9e8967b88aee55d79915662d01c
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2020-02-06 11:57:37 +0100

    iree_pybind_cc_library: Drop unused pybind headers variable

--- bindings/python/build_tools/cmake/iree_pybind_cc_library.cmake
+++ bindings/python/build_tools/cmake/iree_pybind_cc_library.cmake
@@ -65,9 +65,7 @@ function(iree_pybind_cc_library)
         "$<BUILD_INTERFACE:${IREE_COMMON_INCLUDE_DIRS}>"
         "$<BUILD_INTERFACE:${_RULE_INCLUDES}>"
       PRIVATE
-        # TODO(marbre): Check if both pybind includes are necessary
         ${PYBIND11_INCLUDE_DIR}
-        ${pybind11_INCLUDE_DIR} 
         ${Python3_INCLUDE_DIRS}
     )
     target_compile_options(${_NAME}
