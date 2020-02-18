42cc5e6aad6a3cc8e69b2fd3d078c08c787cd3da
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2020-02-18 11:17:12 -0800

    Enable ALWAYSLINK for python extentions build with CMake; Add py_libraries and py_tests
    The python bindings with CMake were so far not fully functional.
    
    * Adds ALWAYSLINK support to iree_py_extension(), similar to iree_cc_binary()
    * Fixes the library name of targets generated by iree_py_extension()
    * Changes the iree_py_extension() targets to STATIC objects
    * Adds iree_py_library CMake module
    * Adds iree_py_test CMake module
    * Adds iree_py_library and iree_py_test targets
    
    ---
    
    **Test status before**
    98% tests passed, 3 tests failed out of 173
    
    Total Test time (real) =  35.13 sec
    
    The following tests FAILED:
            168 - iree_tools_vm_util_test (Failed)
            172 - iree_samples_custom_modules_custom_modules_test (Failed)
            173 - iree_samples_simple_embedding_simple_embedding_test (Failed)
    
    **Test status afterwards**
    98% tests passed, 3 tests failed out of 178
    
    Total Test time (real) =  38.23 sec
    
    The following tests FAILED:
            173 - iree_tools_vm_util_test (Failed)
            177 - iree_samples_custom_modules_custom_modules_test (Failed)
            178 - iree_samples_simple_embedding_simple_embedding_test (Failed)
    
    Closes https://github.com/google/iree/pull/747
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/google/iree/pull/747 from iml130:cmake-python-bindings-alwayslink 12c3a0205dd43db22d05ded82abbbfb91819e4bc
    PiperOrigin-RevId: 295778540

--- bindings/python/build_tools/cmake/iree_py_extension.cmake
+++ bindings/python/build_tools/cmake/iree_py_extension.cmake
@@ -14,6 +14,10 @@
 
 include(CMakeParseArguments)
 
+if (NOT DEFINED _IREE_PY_EXTENSION_NAMES)
+  set(_IREE_PY_EXTENSION_NAMES "")
+endif()
+
 # iree_py_extension()
 #
 # CMake function to imitate Bazel's iree_py_extension rule.
@@ -22,7 +26,7 @@ include(CMakeParseArguments)
 # NAME: name of target
 # HDRS: List of public header files for the library
 # SRCS: List of source files for the library
-# DEPS: List of other libraries to be linked in to the binary targets
+# DEPS: List of other libraries to be linked in to the py extension targets
 # COPTS: List of private compile options
 # DEFINES: List of public defines
 # INCLUDES: Include directories to add to dependencies
@@ -82,14 +86,6 @@ function(iree_py_extension)
         ${IREE_DEFAULT_COPTS}
     )
 
-    target_link_libraries(${_NAME}
-      PUBLIC
-        ${_RULE_DEPS}
-      PRIVATE
-        ${_RULE_LINKOPTS}
-        ${IREE_DEFAULT_LINKOPTS}
-        ${PYTHON_LIBRARY}
-    )
     target_compile_definitions(${_NAME}
       PUBLIC
         ${_RULE_DEFINES}
@@ -112,5 +108,145 @@ function(iree_py_extension)
       # For example, foo/bar/ library 'bar' would end up as 'foo::bar'.
       add_library(${_PACKAGE_NS} ALIAS ${_NAME})
     endif()
+    # Defer computing transitive dependencies and calling target_link_libraries()
+    # until all libraries have been declared.
+    # Track target and deps, use in iree_complete_py_extension_link_options() later.
+    set_property(GLOBAL APPEND PROPERTY _IREE_PY_EXTENSION_NAMES "${_NAME}")
+    set_property(TARGET ${_NAME} PROPERTY DIRECT_DEPS ${_RULE_DEPS})
   endif()
+endfunction()
+
+# Lists all transitive dependencies of DIRECT_DEPS in TRANSITIVE_DEPS.
+function(_iree_transitive_dependencies DIRECT_DEPS TRANSITIVE_DEPS)
+  set(_TRANSITIVE "")
+
+  foreach(_DEP ${DIRECT_DEPS})
+    _iree_transitive_dependencies_helper(${_DEP} _TRANSITIVE)
+  endforeach(_DEP)
+
+  set(${TRANSITIVE_DEPS} "${_TRANSITIVE}" PARENT_SCOPE)
+endfunction()
+
+# Recursive helper function for _iree_transitive_dependencies.
+# Performs a depth-first search through the dependency graph, appending all
+# dependencies of TARGET to the TRANSITIVE_DEPS list.
+function(_iree_transitive_dependencies_helper TARGET TRANSITIVE_DEPS)
+  if (NOT TARGET "${TARGET}")
+    # Excluded from the project, or invalid name? Just ignore.
+    return()
+  endif()
+
+  # Resolve aliases, canonicalize name formatting.
+  get_target_property(_ALIASED_TARGET ${TARGET} ALIASED_TARGET)
+  if(_ALIASED_TARGET)
+    set(_TARGET_NAME ${_ALIASED_TARGET})
+  else()
+    string(REPLACE "::" "_" _TARGET_NAME ${TARGET})
+  endif()
+
+  set(_RESULT "${${TRANSITIVE_DEPS}}")
+  if (${_TARGET_NAME} IN_LIST _RESULT)
+    # Already visited, ignore.
+    return()
+  endif()
+
+  # Append this target to the list. Dependencies of this target will be added
+  # (if valid and not already visited) in recursive function calls.
+  list(APPEND _RESULT ${_TARGET_NAME})
+
+  # Check for non-target identifiers again after resolving the alias.
+  if (NOT TARGET ${_TARGET_NAME})
+    return()
+  endif()
+
+  # Get the list of direct dependencies for this target.
+  get_target_property(_TARGET_TYPE ${_TARGET_NAME} TYPE)
+  if(NOT ${_TARGET_TYPE} STREQUAL "INTERFACE_LIBRARY")
+    get_target_property(_TARGET_DEPS ${_TARGET_NAME} LINK_LIBRARIES)
+  else()
+    get_target_property(_TARGET_DEPS ${_TARGET_NAME} INTERFACE_LINK_LIBRARIES)
+  endif()
+
+  if(_TARGET_DEPS)
+    # Recurse on each dependency.
+    foreach(_TARGET_DEP ${_TARGET_DEPS})
+      _iree_transitive_dependencies_helper(${_TARGET_DEP} _RESULT)
+    endforeach(_TARGET_DEP)
+  endif()
+
+  # Propagate the augmented list up to the parent scope.
+  set(${TRANSITIVE_DEPS} "${_RESULT}" PARENT_SCOPE)
+endfunction()
+
+# Sets target_link_libraries() on all registered py extensions.
+# This must be called after all libraries have been declared.
+function(iree_complete_py_extension_link_options)
+  get_property(_NAMES GLOBAL PROPERTY _IREE_PY_EXTENSION_NAMES)
+
+  foreach(_NAME ${_NAMES})
+    get_target_property(_DIRECT_DEPS ${_NAME} DIRECT_DEPS)
+
+    # List all dependencies, including transitive dependencies, then split the
+    # dependency list into one for whole archive (ALWAYSLINK) and one for
+    # standard linking (which only links in symbols that are directly used).
+    _iree_transitive_dependencies("${_DIRECT_DEPS}" _TRANSITIVE_DEPS)
+    set(_ALWAYS_LINK_DEPS "")
+    set(_STANDARD_DEPS "")
+    foreach(_DEP ${_TRANSITIVE_DEPS})
+      # Check if _DEP is a library with the ALWAYSLINK property set.
+      set(_DEP_IS_ALWAYSLINK OFF)
+      if (TARGET ${_DEP})
+        get_target_property(_DEP_TYPE ${_DEP} TYPE)
+        if(${_DEP_TYPE} STREQUAL "INTERFACE_LIBRARY")
+          # Can't be ALWAYSLINK since it's an INTERFACE library.
+          # We also can't even query for the property, since it isn't whitelisted.
+        else()
+          get_target_property(_DEP_IS_ALWAYSLINK ${_DEP} ALWAYSLINK)
+        endif()
+      endif()
+
+      # Append to the corresponding list of deps.
+      if(_DEP_IS_ALWAYSLINK)
+        list(APPEND _ALWAYS_LINK_DEPS ${_DEP})
+
+        # For MSVC, also add a `-WHOLEARCHIVE:` version of the dep.
+        # CMake treats -WHOLEARCHIVE[:lib] as a link flag and will not actually
+        # try to link the library in, so we need the flag *and* the dependency.
+        if(MSVC)
+          get_target_property(_ALIASED_TARGET ${_DEP} ALIASED_TARGET)
+          if (_ALIASED_TARGET)
+            list(APPEND _ALWAYS_LINK_DEPS "-WHOLEARCHIVE:${_ALIASED_TARGET}")
+          else()
+            list(APPEND _ALWAYS_LINK_DEPS "-WHOLEARCHIVE:${_DEP}")
+          endif()
+        endif()
+      else()
+        list(APPEND _STANDARD_DEPS ${_DEP})
+      endif()
+    endforeach(_DEP)
+
+    # Call into target_link_libraries with the lists of deps.
+    # TODO(scotttodd): `-Wl,-force_load` version
+    if(MSVC)
+      target_link_libraries(${_NAME}
+        PUBLIC
+          ${_ALWAYS_LINK_DEPS}
+          ${_STANDARD_DEPS}
+        PRIVATE
+          ${_RULE_LINKOPTS}
+          ${PYTHON_LIBRARY}
+      )
+    else()
+      target_link_libraries(${_NAME}
+        PUBLIC
+          "-Wl,--whole-archive"
+          ${_ALWAYS_LINK_DEPS}
+          "-Wl,--no-whole-archive"
+          ${_STANDARD_DEPS}
+        PRIVATE
+          ${_RULE_LINKOPTS}
+          ${PYTHON_LIBRARY}
+      )
+    endif()
+  endforeach(_NAME)
 endfunction()
