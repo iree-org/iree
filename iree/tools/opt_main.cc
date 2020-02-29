7ee6f0815c78acdee4f181bc3cf509fa20bb1f2f
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2020-02-29 18:46:23 +0100

    Fix licence header and ParseCommandLineOptions

--- iree/tools/opt_main.cc
+++ iree/tools/opt_main.cc
@@ -1,14 +1,20 @@
-//===- mlir-opt.cpp - MLIR Optimizer Driver -------------------------------===//
+// Copyright 2020 Google LLC
 //
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//===----------------------------------------------------------------------===//
+//      https://www.apache.org/licenses/LICENSE-2.0
 //
-// Main entry function for mlir-opt for when built as standalone binary.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Main entry function for iree-opt and derived binaries.
 //
-//===----------------------------------------------------------------------===//
+// Based on mlir-opt but without registering passes and dialects we don't care about.
 
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/InitLLVM.h"
@@ -127,7 +133,7 @@ int main(int argc, char **argv) {
 
   // Parse pass names in main to ensure static initialization completed.
   llvm::cl::ParseCommandLineOptions(argc, argv,
-                                    "MLIR modular optimizer driver\n");
+                                    "IREE modular optimizer driver\n");
 
   // Set up the input file.
   std::string errorMessage;
