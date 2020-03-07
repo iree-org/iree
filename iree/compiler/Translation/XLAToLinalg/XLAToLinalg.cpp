8db5f460fc5f71b99a423e55f46c8f63dca1a33a
ataei <ataei@google.com>
2020-03-06 16:10:53 -0800

    Implement xla_hlo.reshape -> linalg.reshape convertion on tensors.
    This reshape support expanding a single dim into multiple consecutive dims or collapsing  into a single dim.
    
    Ex:
    (1x16x2) -> (1x4x2x2x2) will linearize the target domain affine_map<(d0, d1, d2, d3, d4) -> (d0, d1 * 4 + d2 * 2 + d3, d4)>
    (1x4x2x2x2) -> (1x16x2) will expand the target domain with affine_map<(d0, d1, d2) -> (d0, (d1 divfloor 4) mod 4, (d1 divfloor 2) mod 2, d1 mod 2, d2>
    
    or in general to expand dim d_i into d_j wave d_j = (d_i divfloor strides[d_i]) % shape[d_j]
    
    PiperOrigin-RevId: 299458015

--- iree/compiler/Translation/XLAToLinalg/XLAToLinalg.cpp
+++ iree/compiler/Translation/XLAToLinalg/XLAToLinalg.cpp
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#include <cstdint>
 #include <memory>
 
 #include "iree/compiler/Translation/XLAToLinalg/Passes.h"
@@ -27,6 +28,7 @@
 #include "mlir/Pass/Pass.h"
 #include "mlir/Transforms/DialectConversion.h"
 #include "mlir/Transforms/Passes.h"
+#include "tensorflow/compiler/mlir/xla/ir/hlo_ops.h"
 #include "tensorflow/compiler/mlir/xla/transforms/rewriters.h"
 
 namespace mlir {
@@ -91,6 +93,133 @@ class SplatConstConverter : public OpConversionPattern<ConstantOp> {
   }
 };
 
+// Reshape by collapsing or expanding consecutive dims e.g (3, 2, 4) -> (3, 8),
+// (12, 3) -> (3, 4, 3).
+class ReshapeExpandOrCollapse : public OpConversionPattern<xla_hlo::ReshapeOp> {
+ public:
+  using OpConversionPattern<xla_hlo::ReshapeOp>::OpConversionPattern;
+
+  PatternMatchResult matchAndRewrite(
+      xla_hlo::ReshapeOp op, ArrayRef<Value> args,
+      ConversionPatternRewriter& rewriter) const final {
+    auto operandType = op.operand().getType().cast<ShapedType>();
+    auto resultType = op.getResult().getType().cast<ShapedType>();
+    if (!operandType.getRank()) return matchFailure();
+
+    auto srcDims = operandType.getShape();
+    auto srcRank = operandType.getRank();
+
+    auto dstDims = resultType.getShape();
+    auto dstRank = resultType.getRank();
+
+    SmallVector<AffineExpr, 2> inputExprs;
+
+    // This is always an expand dims where srcRank < dstRank.
+    auto findRange =
+        [](const ArrayRef<int64_t> srcShape,
+           const ArrayRef<int64_t> dstShape) -> std::pair<int64_t, int64_t> {
+      const std::pair<int64_t, int64_t> invalidRange = {-1, -1};
+      int64_t start = 0, end = 0;
+      int64_t srcRank = srcShape.size();
+      int64_t dstRank = dstShape.size();
+      while (start < srcRank && srcShape[start] == dstShape[start]) start++;
+      if (start >= srcRank) return invalidRange;
+      int64_t size = srcShape[start];
+      int64_t dstSize = 1;
+      end = start;
+      while (end < dstRank && dstSize < size) dstSize *= dstShape[end++];
+      if (end > dstRank) return invalidRange;
+      return {start, end - 1};
+    };
+
+    // Return strides in the range[start, end]
+    auto getRangeStrides = [](const ArrayRef<int64_t> dims, int start,
+                              int end) -> llvm::SmallVector<int64_t, 4> {
+      int rangeSize = end - start + 1;
+      llvm::SmallVector<int64_t, 4> strides(rangeSize, 1);
+      strides[rangeSize - 1] = 1;
+      for (int i = rangeSize - 2; i >= 0; --i) {
+        strides[i] = strides[i + 1] * dims[start + i + 1];
+      }
+      return strides;
+    };
+    // ExpandDim
+    if (dstRank > srcRank) {
+      auto range = findRange(srcDims, dstDims);
+      if (range.second == -1 || range.first == -1) return matchFailure();
+      // Don't match expand by ones.
+      for (int i = range.first; i <= range.second; ++i) {
+        if (dstDims[i] == srcDims[range.first]) return matchFailure();
+      }
+      auto strides = getRangeStrides(dstDims, range.first, range.second);
+      int dimSize = range.second - range.first;
+      for (int i = 0; i < srcRank; ++i) {
+        if (i == range.first) {
+          auto dimExpr = rewriter.getAffineDimExpr(i) * strides[0];
+          for (int j = 1; j <= dimSize; ++j) {
+            dimExpr = dimExpr + rewriter.getAffineDimExpr(i + j) * strides[j];
+          }
+          inputExprs.push_back(dimExpr);
+        } else if (i > range.first) {
+          inputExprs.push_back(rewriter.getAffineDimExpr(i + dimSize));
+        } else {
+          inputExprs.push_back(rewriter.getAffineDimExpr(i));
+        }
+      }
+    } else if (srcRank > dstRank) {
+      // CollapseDim
+      auto range = findRange(dstDims, srcDims);
+      if (range.first == -1 || range.second == -1) return matchFailure();
+      // Don't match expand by ones.
+      for (int i = range.first; i <= range.second; ++i) {
+        if (srcDims[i] == dstDims[range.first]) return matchFailure();
+      }
+      auto strides = getRangeStrides(srcDims, range.first, range.second);
+      int dimSize = range.second - range.first;
+      for (int i = 0; i < srcRank; ++i) {
+        if (i < range.first) {
+          inputExprs.push_back(rewriter.getAffineDimExpr(i));
+        } else if (i > range.second) {
+          const auto index = i - dimSize;
+          index < dstRank
+              ? inputExprs.push_back(rewriter.getAffineDimExpr(index))
+              : inputExprs.push_back(rewriter.getAffineConstantExpr(0));
+
+        } else {
+          inputExprs.push_back(rewriter.getAffineDimExpr(range.first)
+                                   .floorDiv(strides[i - range.first]) %
+                               srcDims[i]);
+        }
+      }
+    } else {
+      return matchFailure();
+    }
+    auto srcMap = AffineMap::get(dstRank, /*symbolCount=*/0, inputExprs);
+    auto dstMap = rewriter.getMultiDimIdentityMap(dstRank);
+
+    auto indexingMapsAttr = rewriter.getAffineMapArrayAttr({srcMap, dstMap});
+
+    OpBuilder::InsertionGuard linalgOpGuard(rewriter);
+    auto nloops = resultType.getRank();
+    auto loc = op.getLoc();
+    auto linalgOp = rewriter.create<linalg::GenericOp>(
+        loc, resultType, args, rewriter.getI64IntegerAttr(1),
+        rewriter.getI64IntegerAttr(1), indexingMapsAttr,
+        getNParallelLoopsAttrs(nloops, rewriter),
+        /*doc=*/nullptr, /*fun=*/nullptr, /*library_call=*/nullptr);
+
+    auto* region = &linalgOp.region();
+    auto* block = rewriter.createBlock(region, region->end());
+    block->addArguments(operandType.getElementType());
+
+    rewriter.setInsertionPointToEnd(block);
+    rewriter.create<linalg::YieldOp>(loc, block->getArgument(0));
+
+    rewriter.replaceOp(op, linalgOp.getOperation()->getResults());
+    return matchSuccess();
+  }
+};
+
 struct XlaLegalizeToLinalg : public FunctionPass<XlaLegalizeToLinalg> {
   void runOnFunction() override {
     OwningRewritePatternList patterns;
@@ -113,7 +242,7 @@ struct XlaLegalizeToLinalg : public FunctionPass<XlaLegalizeToLinalg> {
 void populateXlaToLinalgConversionPattern(MLIRContext* context,
                                           OwningRewritePatternList* patterns) {
   xla_hlo::populateHLOToLinalgConversionPattern(context, patterns);
-  patterns->insert<SplatConstConverter>(context);
+  patterns->insert<SplatConstConverter, ReshapeExpandOrCollapse>(context);
 }
 
 std::unique_ptr<OpPassBase<FuncOp>> createXLAToLinalgPass() {
