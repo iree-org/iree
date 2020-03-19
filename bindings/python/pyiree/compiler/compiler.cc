8a4fa282ccbcddd950c31903d5bb0d4c36c8e3e3
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2020-03-19 15:06:40 -0700

    Add missing include to python bindings
    The CMake build fails with
    
    ```
    /home/mbrehler/repos/iree/bindings/python/pyiree/compiler/compiler.cc:64:14: error: no type named 'SetOneShotPipeSignalFunction' in namespace 'llvm::sys'
      llvm::sys::SetOneShotPipeSignalFunction(
      ~~~~~~~~~~~^
    /home/mbrehler/repos/iree/bindings/python/pyiree/compiler/compiler.cc:65:18: error: definition or redeclaration of 'DefaultOneShotPipeSignalHandler' not allowed inside a function
          llvm::sys::DefaultOneShotPipeSignalHandler);
          ~~~~~~~~~~~^
    /home/mbrehler/repos/iree/bindings/python/pyiree/compiler/compiler.cc:65:18: error: no member named 'DefaultOneShotPipeSignalHandler' in namespace 'llvm::sys'
          llvm::sys::DefaultOneShotPipeSignalHandler);
          ~~~~~~~~~~~^
    /home/mbrehler/repos/iree/bindings/python/pyiree/compiler/compiler.cc:66:14: error: no member named 'PrintStackTraceOnErrorSignal' in namespace 'llvm::sys'
      llvm::sys::PrintStackTraceOnErrorSignal("pyiree");
    ```
    caused by the missing include.
    
    Closes https://github.com/google/iree/pull/1164
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/google/iree/pull/1164 from iml130:python-includes 5a6746804fa6705b8c4270072c8ee7041ab9701e
    PiperOrigin-RevId: 301903358

--- bindings/python/pyiree/compiler/compiler.cc
+++ bindings/python/pyiree/compiler/compiler.cc
@@ -27,6 +27,7 @@
 #include "iree/tools/init_dialects.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/PrettyStackTrace.h"
+#include "llvm/Support/Signals.h"
 #include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/raw_ostream.h"
 #include "mlir/IR/Attributes.h"
