edc21ffba8a613ddbd44bec1a00f484d18c516a5
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2020-03-24 13:48:40 -0700

    Move test directory to iree directory
    Closes https://github.com/google/iree/pull/1239
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/google/iree/pull/1239 from iml130:test-directory c430b46c0772d2812b00812b0fbbfc48f3e11da0
    PiperOrigin-RevId: 302738986

--- build_tools/bazel/build.sh
+++ build_tools/bazel/build.sh
@@ -37,7 +37,7 @@ echo "Running with test env args: ${test_env_args[@]}"
 # `bazel test //...` because the latter excludes targets tagged "manual". The
 # "manual" tag allows targets to be excluded from human wildcard builds, but we
 # want them built by CI unless they are excluded with "nokokoro".
-bazel query '//iree/... + //test/... + //bindings/... except attr("tags", "nokokoro", //...)' | \
+bazel query '//iree/... + //bindings/... except attr("tags", "nokokoro", //...)' | \
   xargs bazel test ${test_env_args[@]} --keep_going --test_output=errors --config=rs
 
 # Disable RBE until compatibility issues with the experimental_repo_remote_exec
