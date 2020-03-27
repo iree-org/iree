fa7bed4c837fbac82c7dd116789a1a5a01bb8691
Geoffrey Martin-Noble <gcmn@google.com>
2020-03-26 21:58:32 -0700

    cleanup include directories

--- build_tools/cmake/iree_copts.cmake
+++ build_tools/cmake/iree_copts.cmake
@@ -147,7 +147,6 @@ set(IREE_TABLEGEN_EXE iree-tblgen)
 
 list(APPEND IREE_COMMON_INCLUDE_DIRS
   ${CMAKE_CURRENT_SOURCE_DIR}/third_party/tensorflow
-  ${CMAKE_CURRENT_SOURCE_DIR}/third_party/ruy
 )
 
 #-------------------------------------------------------------------------------
