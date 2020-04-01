75d2d0769cc6bcd54d97b34cf94902b7f5076271
Hanhan Wang <hanchung@google.com>
2020-04-01 15:34:01 -0700

    Drop unrolling multi-dimensional reductions for ReduceWindowOp.
    ReduceWindowOp is different from ReduceOp, since the reducetion is applied on
    all of the dimensions.
    
    PiperOrigin-RevId: 304275721

--- iree/compiler/Dialect/VMLA/Transforms/UnrollReductions.cpp
+++ iree/compiler/Dialect/VMLA/Transforms/UnrollReductions.cpp
@@ -59,83 +59,6 @@ void unrollReduceOp(xla_hlo::ReduceOp reduceOp) {
   reduceOp.erase();
 }
 
-// Unrolls a multi-dimensional xla_hlo.reduce_window op into one
-// xla_hlo.reduce_window op per dimension. The XLA operation semantics state
-// that this is a valid transformation.
-void unrollReduceWindowOp(xla_hlo::ReduceWindowOp reduceOp) {
-  // Create one dispatch per dimension being reduced.
-  // We'll do this by chaining the original input through with the temporary
-  // reduction results. The results we end up with will be the originally
-  // requested shape and we can just substitute them.
-  using WindowTuple = std::tuple<int64_t, int64_t, int64_t, int64_t>;
-  auto windowDimensions = reduceOp.window_dimensions();
-  auto windowStrides = reduceOp.window_stridesAttr();
-  auto baseDilations = reduceOp.base_dilationsAttr();
-  auto windowDilations = reduceOp.window_dilationsAttr();
-  SmallVector<WindowTuple, 4> sortedWindowAttrs;
-  for (uint32_t i = 0; i < windowDimensions.getNumElements(); ++i) {
-    int64_t windowDimension = windowDimensions.getValue<int64_t>({i});
-    int64_t windowStride = windowStrides.getValue<int64_t>({i});
-    int64_t baseDilation = baseDilations.getValue<int64_t>({i});
-    int64_t windowDilation = windowDilations.getValue<int64_t>({i});
-    sortedWindowAttrs.push_back(WindowTuple(windowDimension, windowStride,
-                                            baseDilation, windowDilation));
-  }
-  llvm::sort(sortedWindowAttrs, [](WindowTuple a, WindowTuple b) {
-    return (std::get<0>(a) - std::get<0>(b)) > 0;
-  });
-
-  // Insert at the same place as the original op.
-  OpBuilder builder(reduceOp);
-  Value temp = reduceOp.operand();
-  for (auto windowAttrs : llvm::enumerate(sortedWindowAttrs)) {
-    int64_t windowDimension = std::get<0>(windowAttrs.value());
-    int64_t windowStride = std::get<1>(windowAttrs.value());
-    int64_t baseDilation = std::get<2>(windowAttrs.value());
-    int64_t windowDilation = std::get<3>(windowAttrs.value());
-
-    // Compute the new result.
-    // NOTE: this should not be required and instead be on the HLO op.
-    auto tempType = temp.getType().cast<RankedTensorType>();
-    SmallVector<int64_t, 4> newShape;
-    for (int64_t i = 0; i < tempType.getRank(); ++i) {
-      if (i != windowDimension) {
-        newShape.push_back(tempType.getDimSize(i));
-      }
-    }
-    auto resultType =
-        RankedTensorType::get(newShape, tempType.getElementType());
-
-    // Create the new reduction using the results of the previous operation.
-    auto singleAttrType =
-        RankedTensorType::get({1}, builder.getIntegerType(64));
-    auto singleReduceOp = builder.create<xla_hlo::ReduceWindowOp>(
-        reduceOp.getLoc(), resultType, temp, reduceOp.init_value(),
-        DenseIntElementsAttr::get(
-            singleAttrType,
-            {builder.getI64IntegerAttr(windowDimension).cast<Attribute>()}),
-        DenseIntElementsAttr::get(
-            singleAttrType,
-            {builder.getI64IntegerAttr(windowStride).cast<Attribute>()}),
-        DenseIntElementsAttr::get(
-            singleAttrType,
-            {builder.getI64IntegerAttr(baseDilation).cast<Attribute>()}),
-        DenseIntElementsAttr::get(
-            singleAttrType,
-            {builder.getI64IntegerAttr(windowDilation).cast<Attribute>()}),
-        reduceOp.paddingAttr());
-    BlockAndValueMapping mapping;
-    reduceOp.body().cloneInto(&singleReduceOp.body(), mapping);
-    temp = singleReduceOp.getResult();
-  }
-
-  // Replace uses of the existing results with the new results.
-  reduceOp.getResult().replaceAllUsesWith(temp);
-
-  // Erase original op.
-  reduceOp.erase();
-}
-
 }  // namespace
 
 class UnrollReductionsPass : public FunctionPass<UnrollReductionsPass> {
@@ -146,14 +69,6 @@ class UnrollReductionsPass : public FunctionPass<UnrollReductionsPass> {
       for (auto reduceOp : reduceOps) {
         if (reduceOp.dimensions().getNumElements() > 1) {
           unrollReduceOp(reduceOp);
-        }
-      }
-
-      auto reduceWindowOps =
-          llvm::to_vector<4>(block.getOps<xla_hlo::ReduceWindowOp>());
-      for (auto reduceOp : reduceWindowOps) {
-        if (reduceOp.window_dimensions().getNumElements() > 1) {
-          unrollReduceWindowOp(reduceOp);
         }
       }
     }
