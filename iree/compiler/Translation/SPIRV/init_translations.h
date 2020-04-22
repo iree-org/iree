2639b00c05a518033a428f63a4ab7b99299d0fd2
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2020-04-22 11:27:51 -0700

    Register SPIR-V translation passes statically and drop alwayslink
    Closes https://github.com/google/iree/pull/1611
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/google/iree/pull/1611 from iml130:translation_spirv 6912e251ff3ac281b1176140cd447050ddd9b74d
    PiperOrigin-RevId: 307859082

---
+++ iree/compiler/Translation/SPIRV/init_translations.h
@@ -0,0 +1,48 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef IREE_COMPILER_TRANSLATION_SPIRV_INIT_TRANSLATIONS_H_
+#define IREE_COMPILER_TRANSLATION_SPIRV_INIT_TRANSLATIONS_H_
+
+#include "iree/compiler/Translation/SPIRV/IndexComputation/IndexComputationPass.h"
+#include "iree/compiler/Translation/SPIRV/LinalgToSPIRV/Passes.h"
+#include "iree/compiler/Translation/SPIRV/Passes/Passes.h"
+#include "iree/compiler/Translation/SPIRV/XLAToSPIRV/IREEToSPIRVPass.h"
+#include "mlir/Pass/Pass.h"
+
+namespace mlir {
+namespace iree_compiler {
+
+// This function should be called before creating any MLIRContext if one
+// expects all the possible translations to be made available to the context
+// automatically.
+inline void registerSPRIVTranslation() {
+  static bool init_once = []() {
+    // XLAToSPIRV
+    createIndexComputationPass();
+    createAdjustIntegerWidthPass();
+    createIREEToSPIRVPass();
+
+    // LinalgToSPIRV
+    createConvertToGPUPass();
+    createLinalgTileAndFusePass();
+    return true;
+  }();
+  (void)init_once;
+}
+
+}  // namespace iree_compiler
+}  // namespace mlir
+
+#endif  // IREE_COMPILER_TRANSLATION_SPIRV_INIT_TRANSLATIONS_H_
--- iree/tools/translate_main.cc
+++ iree/tools/translate_main.cc
@@ -17,6 +17,7 @@
 // We need this entry function because we want to register PassManager CLI
 // options, which is missing in MLIR's translation main entry function.
 
+#include "iree/compiler/Translation/SPIRV/init_translations.h"
 #include "iree/tools/init_compiler_modules.h"
 #include "iree/tools/init_dialects.h"
 #include "iree/tools/init_targets.h"
@@ -56,6 +57,7 @@ int main(int argc, char **argv) {
   mlir::iree_compiler::registerIreeCompilerModuleDialects();
   mlir::iree_compiler::registerHALTargetBackends();
   mlir::registerMlirTranslations();
+  mlir::iree_compiler::registerSPRIVTranslation();
 
   // Register MLIRContext command-line options like
   // -mlir-print-op-on-diagnostic.
