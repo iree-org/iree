9192119cef0f762f653b5a5109113b601be18bce
Phoenix Meadowlark <meadowlark@google.com>
2020-05-15 15:28:01 -0700

    Update kokoro/gcp_ubuntu/bazel/build.sh
    Co-authored-by: Geoffrey Martin-Noble <gcmn@google.com>

--- kokoro/gcp_ubuntu/bazel/build.sh
+++ kokoro/gcp_ubuntu/bazel/build.sh
@@ -26,6 +26,10 @@ export PS4='[$(date -u "+%T %Z")] '
 
 # Check these exist and print the versions for later debugging
 bazel --version
+"$CXX" --version
+"$CC" --version
+"$PYTHON_BIN" -V
+# TODO( #1875 ): Make PYTHON_BIN also control the runtime version
 python3 -V
 
 echo "Initializing submodules"
