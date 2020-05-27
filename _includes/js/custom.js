08c13bddde718c47c3512f3063e35bb5e9e75124
Phoenix Meadowlark <meadowlark@google.com>
2020-05-27 10:28:50 -0700

    Add parent classes and SASS styling

--- _includes/js/custom.js
+++ _includes/js/custom.js
@@ -12,18 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Colors taken and slightly modified from the Just the Docs theme to match
-// the rest of the site.
-let SUCCESS_COLOR = '#7253ed66';
-let FAILURE_COLOR = '#f5f6fa';
-
-window.onload = () => {
+document.addEventListener('DOMContentLoaded', () => {
   let successes = document.body.getElementsByClassName('success-table-element');
   Array.prototype.forEach.call(successes, element => {
-    element.parentElement.style.backgroundColor = SUCCESS_COLOR;
-  })
+    element.parentElement.classList.add('success-table-cell');
+  });
   let failures = document.body.getElementsByClassName('failure-table-element');
   Array.prototype.forEach.call(failures, element => {
-    element.parentElement.style.backgroundColor = FAILURE_COLOR;
-  })
-}
+    element.parentElement.classList.add('failure-table-cell');
+  });
+});
