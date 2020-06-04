217904d28d5628f3a67d402bb2d1c4b1e0b53387
Scott Todd <scotttodd@google.com>
2020-06-04 13:38:32 -0700

    Start on "dylib" HAL backend for running code from dynamic libraries.
    The plan here is to use this for LLVM ahead-of-time (AOT) execution: a new compiler target will generate modules targeting this HAL backend that will contain ahead-of-time compiled platform/architecture-specific dynamic libraries (.so/.dll/.dylib).
    
    This is modeled after iree/hal/llvmjit, but is intended to be generic and should not directly reference LLVM code or concepts. Existing restrictions on dynamic shapes, op coverage, etc. from llvmjit are mirrored here for the moment.
    
    I've partially tested manually using some test .dll files and a (next PR) LLVMAOTTarget on the compiler side. I'd like to expand on `iree/hal/cts/` coverage soon.
    
    Closes https://github.com/google/iree/pull/2099
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/google/iree/pull/2099 from ScottTodd:dylib d6356b683104d9e6d9bbd9f592a983787013e716
    PiperOrigin-RevId: 314794646

---
+++ iree/hal/dylib/dylib_driver.cc
@@ -0,0 +1,63 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "iree/hal/dylib/dylib_driver.h"
+
+#include <memory>
+
+#include "iree/hal/device_info.h"
+#include "iree/hal/dylib/dylib_device.h"
+
+namespace iree {
+namespace hal {
+namespace dylib {
+namespace {
+
+DeviceInfo GetDefaultDeviceInfo() {
+  DeviceFeatureBitfield supported_features = DeviceFeature::kNone;
+  // TODO(benvanik): implement debugging/profiling features.
+  // supported_features |= DeviceFeature::kDebugging;
+  // supported_features |= DeviceFeature::kCoverage;
+  // supported_features |= DeviceFeature::kProfiling;
+  DeviceInfo device_info("dylib", "Dynamic Library (dylib)",
+                         supported_features);
+  // TODO(benvanik): device info.
+  return device_info;
+}
+
+}  // namespace
+
+DyLibDriver::DyLibDriver() : Driver("dylib") {}
+
+DyLibDriver::~DyLibDriver() = default;
+
+StatusOr<std::vector<DeviceInfo>> DyLibDriver::EnumerateAvailableDevices() {
+  std::vector<DeviceInfo> device_infos;
+  device_infos.push_back(GetDefaultDeviceInfo());
+  return device_infos;
+}
+
+StatusOr<ref_ptr<Device>> DyLibDriver::CreateDefaultDevice() {
+  // Only one device, pass a dummy device_id.
+  return CreateDevice(0);
+}
+
+StatusOr<ref_ptr<Device>> DyLibDriver::CreateDevice(DriverDeviceID device_id) {
+  // Only one device, ignore device_id.
+  return DyLibDevice::CreateDyLibDevice(GetDefaultDeviceInfo());
+}
+
+}  // namespace dylib
+}  // namespace hal
+}  // namespace iree
---
+++ iree/hal/dylib/dylib_executable.h
@@ -0,0 +1,59 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef IREE_HAL_DYLIB_DYLIB_EXECUTABLE_H_
+#define IREE_HAL_DYLIB_DYLIB_EXECUTABLE_H_
+
+#include <memory>
+#include <string>
+
+#include "absl/container/inlined_vector.h"
+#include "absl/types/span.h"
+#include "iree/base/dynamic_library.h"
+#include "iree/base/status.h"
+#include "iree/hal/allocator.h"
+#include "iree/hal/executable.h"
+#include "iree/hal/executable_spec.h"
+
+namespace iree {
+namespace hal {
+namespace dylib {
+
+struct MemrefType;
+
+class DyLibExecutable final : public Executable {
+ public:
+  static StatusOr<ref_ptr<DyLibExecutable>> Load(hal::Allocator* allocator,
+                                                 ExecutableSpec spec);
+  DyLibExecutable(ExecutableSpec spec);
+  ~DyLibExecutable() override;
+
+  bool supports_debugging() const override { return false; }
+
+  Status Invoke(int func_id, absl::Span<void*> args) const;
+
+ private:
+  Status Initialize();
+
+  ExecutableSpec spec_;
+  std::string executable_library_temp_path_;
+  std::unique_ptr<DynamicLibrary> executable_library_;
+  absl::InlinedVector<void*, 4> entry_functions_;
+};
+
+}  // namespace dylib
+}  // namespace hal
+}  // namespace iree
+
+#endif  // IREE_HAL_DYLIB_DYLIB_EXECUTABLE_H_
