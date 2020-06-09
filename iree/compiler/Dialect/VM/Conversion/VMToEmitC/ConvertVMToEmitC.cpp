17f49b9cfd19e0218a37660bbc6d353c0faa35ef
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2020-06-09 19:31:41 +0200

    Fix format

--- iree/compiler/Dialect/VM/Conversion/VMToEmitC/ConvertVMToEmitC.cpp
+++ iree/compiler/Dialect/VM/Conversion/VMToEmitC/ConvertVMToEmitC.cpp
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 #include "iree/compiler/Dialect/VM/Conversion/VMToEmitC/ConvertVMToEmitC.h"
 
 #include "emitc/Dialect/EmitC/EmitCDialect.h"
