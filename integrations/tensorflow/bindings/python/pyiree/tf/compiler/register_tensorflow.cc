ddf73fa389f4b29e83052af3fc0ef9464b1b0710
meadowlark@google.com <phoenix@cs.washington.edu>
2020-06-23 13:03:02 -0500

    Small spacing fix in error messages

--- integrations/tensorflow/bindings/python/pyiree/tf/compiler/register_tensorflow.cc
+++ integrations/tensorflow/bindings/python/pyiree/tf/compiler/register_tensorflow.cc
@@ -63,7 +63,7 @@ CompilerModuleBundle LoadSavedModel(
                               absl::MakeSpan(mutable_exported_names));
   if (!module_or.status().ok()) {
     std::stringstream msg;
-    msg << "Failed to convert saved model to MLIR'" << saved_model_dir
+    msg << "Failed to convert saved model to MLIR '" << saved_model_dir
         << "': " << module_or.status();
     throw RaisePyError(PyExc_RuntimeError, msg.str().c_str());
   }
@@ -93,7 +93,7 @@ CompilerModuleBundle LoadSignatureDefSavedModel(
                                 context_bundle->mlir_context());
   if (!module_or.status().ok()) {
     std::stringstream msg;
-    msg << "Failed to convert saved model to MLIR'" << saved_model_dir
+    msg << "Failed to convert saved model to MLIR '" << saved_model_dir
         << "': " << module_or.status();
     throw RaisePyError(PyExc_RuntimeError, msg.str().c_str());
   }
