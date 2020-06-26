810366395bea92a3808e82115545b077863cc4e8
scotttodd <scotttodd@google.com>
2020-06-26 13:47:47 -0700

    Fix memory leak in timeline semaphore emulation.
    PiperOrigin-RevId: 318536662

--- iree/hal/vulkan/timepoint_util.h
+++ iree/hal/vulkan/timepoint_util.h
@@ -153,7 +153,8 @@ class TimePointFencePool final : public RefObject<TimePointFencePool> {
 
   absl::Mutex mutex_;
 
-  IntrusiveList<TimePointFence> free_fences_ ABSL_GUARDED_BY(mutex_);
+  IntrusiveList<std::unique_ptr<TimePointFence>> free_fences_
+      ABSL_GUARDED_BY(mutex_);
 };
 
 // A pool of `VkSemaphore`s that can be used by `EmulatedTimelineSemaphore` to
