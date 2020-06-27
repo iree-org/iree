3b6a42ade965bcde31faeac2c36078a833ef9806
Thomas Raoux <thomasraoux@google.com>
2020-06-26 18:18:48 -0700

    Add CMake build for ModelBuilder project
    Update bazel file to allow bazel_to_cmake to work. Add few workarounds
    for dynamic library not working with the script.

--- build_tools/bazel_to_cmake/bazel_to_cmake_targets.py
+++ build_tools/bazel_to_cmake/bazel_to_cmake_targets.py
@@ -49,6 +49,9 @@ EXPLICIT_TARGET_MAPPING = {
     "@llvm-project//mlir:StandardToSPIRVConversions": [
         "MLIRStandardToSPIRVTransforms"
     ],
+    "@llvm-project//mlir:GPUToVulkanTransforms": [
+        "MLIRGPUtoVulkanTransforms"
+    ],
     "@llvm-project//mlir:mlir_c_runner_utils": ["MLIRExecutionEngine"],
     "@llvm-project//mlir:mlir-translate": ["mlir-translate"],
     "@llvm-project//mlir:MlirTableGenMain": ["MLIRTableGen"],
