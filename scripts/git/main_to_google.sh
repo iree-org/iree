4cf02d7b08a6fd9bab53bf087b18360001985673
Geoffrey Martin-Noble <gcmn@google.com>
2020-07-15 13:43:26 -0700

    Fix up other scripts

--- scripts/git/main_to_google.sh
+++ scripts/git/main_to_google.sh
@@ -14,18 +14,57 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Checks out a branch at the specified green commit (default "main") and
+# creates a PR from that branch based on google.
+#
+# - Requries the gh CLI (https://github.com/cli/cli) to create a PR.
+# - Will force push to the configured PR_BRANCH (default "main-to-google") on
+#   the configured FORK_REMOTE (default "fork")
+# - Requires that local "main" branch is a pristine (potentially stale) copy
+#   of the "main" branch on the configured UPSTREAM_REMOTE
+#   (default "upstream").
+# - Requires that the working directory be clean. Will abort otherwise.
+
 set -x
 set -e
 set -o pipefail
 
 GREEN_COMMIT="${1:-main}"
-BRANCH="${BRANCH:-main-to-google}"
 
-git sync
-git checkout "${GREEN_COMMIT?}"
-git submodule update
-git checkout -B "${BRANCH?}"
-git push -f fork "${BRANCH?}"
+PR_BRANCH="${PR_BRANCH:-main-to-google}"
+UPSTREAM_REMOTE="${UPSTREAM_REMOTE:-upstream}"
+FORK_REMOTE="${FORK_REMOTE:-fork}"
+
+if [[ -n "$(git status --porcelain)" ]]; then
+  echo "Working directory not clean. Aborting"
+  git status
+  exit 1
+fi
+if ! git symbolic-ref -q HEAD; then
+  echo "In a detached HEAD state. Aborting"
+  git status
+  exit 1
+fi
+git checkout main
+git pull "${UPSTREAM_REMOTE?}" main --ff-only
+git submodule update --init
+if [[ -n "$(git status --porcelain)" ]]; then
+  echo "Working directory not clean after sync. Aborting"
+  git status
+  exit 1
+fi
+if [[ "${GREEN_COMMIT}" != "main" ]]; then
+  git checkout "${GREEN_COMMIT?}"
+  git submodule update
+fi
+git checkout -B "${PR_BRANCH?}"
+git push -f "${FORK_REMOTE?}" "${PR_BRANCH?}"
+if [[ -z "$(which gh)" ]]; then
+  echo "gh not found on path."
+  echo "Have you installed the GitHub CLI (https://github.com/cli/cli)?"
+  echo "Cannot create PR. Branch ${BRANCH?} pushed, but aborting."
+  exit 1
+fi
 gh pr create \
     --base google \
     --title="Merge main -> google" \
--- scripts/git/update_tf_submodule.sh
+++ scripts/git/update_tf_submodule.sh
@@ -14,16 +14,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Creates a PR based on the specified BASE_BRANCH (default "google") to update
+# the TF submodule to the configured TENSORFLOW_COMMIT (default REMOTE for
+# current HEAD).
+#
+# - Requries the gh CLI (https://github.com/cli/cli) to create a PR.
+# - Will force push to the configured PR_BRANCH (default "tf-submodule-update")
+#   on the configured FORK_REMOTE (default "fork")
+# - Requires that local BASE_BRANCH branch is a pristine (potentially stale)
+#   copy of the same branch on the configured UPSTREAM_REMOTE
+#   (default "upstream").
+# - Requires that the working directory be clean. Will abort otherwise.
+# - An optional TF_COMMIT_NICKNAME nickname can be given to the commit for the
+#   PR description. Otherwise, it will default to "current HEAD" if
+#   TENSORFLOW_COMMIT is REMOTE and the trimmed commit sha otherwise.
 
 set -e
 set -x
 set -o pipefail
 
 TENSORFLOW_COMMIT="${1:-REMOTE}"
-BRANCH="submodule-update"
+PR_BRANCH="tf-submodule-update"
+BASE_BRANCH="${1:-google}"
+UPSTREAM_REMOTE="${UPSTREAM_REMOTE:-upstream}"
+FORK_REMOTE="${FORK_REMOTE:-fork}"
+TF_COMMIT_NICKNAME=""
 
-git update google
-git checkout -B "${BRANCH?}"
+if [[ -n "$(git status --porcelain)" ]]; then
+  echo "Working directory not clean. Aborting"
+  git status
+  exit 1
+fi
+if ! git symbolic-ref -q HEAD; then
+  echo "In a detached HEAD state. Aborting"
+  git status
+  exit 1
+fi
+git checkout "${BASE_BRANCH?}"
+git pull "${UPSTREAM_REMOTE?}" "${BASE_BRANCH?}" --ff-only
+git submodule update --init
+if [[ -n "$(git status --porcelain)" ]]; then
+  echo "Working directory not clean after sync. Aborting"
+  git status
+  exit 1
+fi
+git checkout -B "${PR_BRANCH?}"
 
 CMD="./scripts/git/update_tf_llvm_submodules.py --llvm_commit=KEEP --update_build_files=true --tensorflow_commit=${TENSORFLOW_COMMIT?}"
 
@@ -31,15 +66,15 @@ bash -c "${CMD?}"
 
 TF_SHA="$(git submodule status third_party/tensorflow | awk '{print $1}' | cut -c -12)"
 
-TF_COMMIT_NICKNAME=""
-if [[ "${TENSORFLOW_COMMIT?}" == "REMOTE" ]]; then
-  TF_COMMIT_NICKNAME=" current HEAD"
+if [[ -z "${TF_COMMIT_NICKNAME?}" && "${TENSORFLOW_COMMIT?}" == "REMOTE" ]]; then
+  TF_COMMIT_NICKNAME="current HEAD"
 fi
+TF_COMMIT_NICKNAME="${TF_COMMIT_NICKNAME:-${TF_SHA?}}"
 
-TITLE="Integrate TF at ${TF_SHA?}"
+TITLE="Integrate TF at tensorflow/tensorflow@${TF_SHA?}"
 BODY="$(cat <<-EOF
-Updates TF to${TF_COMMIT_NICKNAME?}
-(https://github.com/tensorflow/tensorflow/commit/${TF_SHA?})
+Updates TF to
+[${TF_COMMIT_NICKNAME?}](https://github.com/tensorflow/tensorflow/commit/${TF_SHA?})
 and copies over the LLVM BUILD files.
 
 \`${CMD?}\`
@@ -47,5 +82,5 @@ EOF
 )"
 
 git commit -am "${TITLE?}"
-git push -f fork "${BRANCH?}"
-gh pr create --title="${TITLE?}" --body="${BODY?}" --base="google"
+git push -f fork "${PR_BRANCH?}"
+gh pr create --title="${TITLE?}" --body="${BODY?}" --base="${BASE_BRANCH?}"
