111561d78e14be32aed4c4ae0410a9463d752576
Scott Todd <scotttodd@google.com>
2020-07-16 16:50:13 -0700

    Link to GitHub issue for reenabling warning.

--- build_tools/cmake/iree_copts.cmake
+++ build_tools/cmake/iree_copts.cmake
@@ -80,7 +80,7 @@ iree_select_compiler_opts(IREE_DEFAULT_COPTS
     "-Wno-unused-local-typedef"
     "-Wno-unused-private-field"
     "-Wno-user-defined-warnings"
-    "-Wno-macro-redefined" # IREE and TensorFlow define some of the same macros
+    "-Wno-macro-redefined" # TODO(GH-2556): Re-enable (IREE and TF both define LOG)
     # Explicitly enable some additional warnings.
     # Some of these aren't on by default, or under -Wall, or are subsets of
     # warnings turned off above.
