5a482b6dfaeda55634cb30b17700882569df2196
meadowlark@google.com <meadowlark@google.com>
2020-07-16 20:08:03 -0500

    Simplify compile_module and CompiledModule by requiring classes.

--- integrations/tensorflow/e2e/keras/lstm_static_test.py
+++ integrations/tensorflow/e2e/keras/lstm_static_test.py
@@ -27,20 +27,22 @@ NUM_BATCH = 7
 INPUT_SHAPE = [NUM_BATCH, NUM_TIMESTEPS, NUM_UNITS]
 
 
-def lstm_module():
-  tf_utils.set_random_seed()
-  inputs = tf.keras.layers.Input(batch_size=NUM_BATCH, shape=INPUT_SHAPE[1:])
-  outputs = tf.keras.layers.LSTM(units=NUM_UNITS, return_sequences=True)(inputs)
-  model = tf.keras.Model(inputs, outputs)
-  module = tf.Module()
-  module.m = model
-  module.predict = tf.function(
-      input_signature=[tf.TensorSpec(INPUT_SHAPE, tf.float32)])(
-          model.call)
-  return module
+class LstmStatic(tf.Module):
+
+  def __init__(self):
+    super(LstmStatic, self).__init__()
+    tf_utils.set_random_seed()
+    inputs = tf.keras.layers.Input(batch_size=NUM_BATCH, shape=INPUT_SHAPE[1:])
+    outputs = tf.keras.layers.LSTM(
+        units=NUM_UNITS, return_sequences=True)(
+            inputs)
+    self.m = tf.keras.Model(inputs, outputs)
+    self.predict = tf.function(
+        input_signature=[tf.TensorSpec(INPUT_SHAPE, tf.float32)])(
+            self.m.call)
 
 
-@tf_test_utils.compile_module(lstm_module, exported_names=["predict"])
+@tf_test_utils.compile_module(LstmStatic, exported_names=["predict"])
 class LstmTest(tf_test_utils.SavedModelTestCase):
 
   def test_lstm(self):
