914a62f1d25598ade4b6ff67089255674642d486
Geoffrey Martin-Noble <gcmn@google.com>
2020-07-24 22:03:34 -0700

    Clean up comments

--- build_tools/kokoro/gcp_ubuntu/docker_common.sh
+++ build_tools/kokoro/gcp_ubuntu/docker_common.sh
@@ -22,15 +22,37 @@
 # Sets the environment variable DOCKER_RUN_ARGS to be used by subsequent
 # `docker run` invocations.
 function docker_setup() {
-    # Setup to run docker as the current user.
-    # Provide a place to mount files that would normally be under /etc/
+    # Make the source repository available and launch containers in that
+    # directory.
+    local workdir="${KOKORO_ARTIFACTS_DIR?}/github/iree"
+    DOCKER_RUN_ARGS=(
+      --volume="${workdir?}:${workdir?}"
+      --workdir="${workdir?}"
+    )
+
+    # Delete the container after the run is complete.
+    DOCKER_RUN_ARGS+=(--rm)
+
+
+    # Run as the current user and group. If only it were this simple.
+    DOCKER_RUN_ARGS+=(--user="$(id -u):$(id -g)")
+
+
+    # The Docker container doesn't know about the users and groups of the host
+    # system. We have to tell it. This is just a mapping of IDs to names though.
+    # The thing that really matters is the IDs, so the key thing is that Docker
+    # writes files as the same ID as the current user, which we set above, but
+    # without the group and passwd file, lots of things get upset because they
+    # don't recognize the current user ID (e.g. `whoami` fails).
+    # So we make the container share the host mapping, which guarantees that the
+    # current user is mapped. If there was any user or group in the container
+    # that we cared about, this wouldn't work, but luckily we don't.
     # We don't just mount the real /etc/passwd and /etc/group because Google
     # Linux workstations do some interesting stuff with user/group permissions
     # such that they don't contain the information about normal users and we
     # want these scripts to be runnable locally for debugging.
+    # Instead we dump the results of `getent` to some fake files.
     local fake_etc_dir="${KOKORO_ROOT?}/fake_etc"
-    # Bazel wants a home directory
-    # TODO: more explanation if this works
     mkdir -p "${fake_etc_dir?}"
 
     local fake_group="${fake_etc_dir?}/group"
@@ -39,26 +61,34 @@ function docker_setup() {
     getent group > "${fake_group?}"
     getent passwd > "${fake_passwd?}"
 
-    local fake_home="${KOKORO_ROOT?}/fake_home"
-    mkdir -p "${fake_home}"
-
-    local workdir="${KOKORO_ARTIFACTS_DIR?}/github/iree"
-
-    DOCKER_RUN_ARGS=(
-      # Run as the current user and group
-      --user="$(id -u):$(id -g)"
-      # Make the source repository available
-      --volume="${workdir?}:${workdir?}"
-      --workdir="${workdir?}"
-      # Tell docker about the host users and groups. Bazel needs this
-      # information, but it also makes some other things more pleasant.
+    DOCKER_RUN_ARGS+=(
       --volume="${fake_group?}:/etc/group:ro"
       --volume="${fake_passwd?}:/etc/passwd:ro"
-      --volume="${fake_home?}:${HOME?}"
-      # Make gcloud credentials available. This isn't necessary when running
-      # in GCE but enables using this script locally with RBE.
-      --volume="${HOME?}/.config/gcloud:${HOME?}/.config/gcloud:ro"
-      # Delete the container after
-      --rm
+    )
+
+
+    # Bazel stores its cache in the user home directory by default. It's
+    # possible to override this, but that would require changing our Bazel
+    # startup options, which means polluting all our scripts and making them not
+    # runnable locally. Instead, we give it a special home directory to write
+    # into. We don't just mount the user home directory (or some subset thereof)
+    # for two reasons:
+    #   1. We probably don't want to just implicitly share this between runs or
+    #      pollute a local directory when running locally.
+    #   2. When running with Kokoro, we mount a local scratch SSD to KOKORO_ROOT
+    #      whereas the home directory is on the persistent SSD boot disk. It
+    #      turns out that makes a huge difference for the Bazel cache directory.
+    local fake_home_dir="${KOKORO_ROOT?}/fake_home"
+    mkdir -p "${fake_home_dir}"
+
+    DOCKER_RUN_ARGS+=(
+      --env HOME="${fake_home_dir?}"
+      --volume="${fake_home_dir?}:${fake_home_dir?}"
+    )
+
+    # Make gcloud credentials available. This isn't necessary when running in
+    # GCE but enables using this script locally with RBE.
+    DOCKER_RUN_ARGS+=(
+      --volume="${HOME?}/.config/gcloud:${fake_home_dir?}/.config/gcloud:ro"
     )
 }
