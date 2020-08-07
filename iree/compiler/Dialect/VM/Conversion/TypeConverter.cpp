99ed353d40ec8c240ad23694d4de822be1fbcc2c
Ben Vanik <benvanik@google.com>
2020-08-06 19:13:49 -0700

    Reworking type conversion to support VM target options.

--- iree/compiler/Dialect/VM/Conversion/TypeConverter.cpp
+++ iree/compiler/Dialect/VM/Conversion/TypeConverter.cpp
@@ -34,53 +34,61 @@ TypeConverter::TypeConverter(TargetOptions targetOptions)
   addConversion([](IREE::VM::RefType type) { return type; });
   // Wrap ref types.
   addConversion([](Type type) -> Optional<Type> {
-    if (IREE::VM::RefType::isCompatible(type)) {
-      return IREE::VM::RefType::get(type);
-    }
-    return llvm::None;
-  });
-  // Convert integer types.
-  addConversion([](IntegerType integerType) -> Optional<Type> {
-    if (integerType.isSignlessInteger(32)) {
-      return integerType;
-    } else if (integerType.isInteger(1)) {
-      // Promote i1 -> i32.
-      return IntegerType::get(32, integerType.getContext());
-    } else if (integerType.isSignlessInteger()) {
-      // TODO(benvanik): ensure we want to do this and not something that forces
-      // materialization of trunc/ext.
-      return IntegerType::get(32, integerType.getContext());
+    if (RefType::isCompatible(type)) {
+      return RefType::get(type);
     }
     return llvm::None;
   });
 
-  // Convert index types to i32.
-  addConversion([](IndexType indexType) -> Optional<Type> {
-    return IntegerType::get(32, indexType.getContext());
-  });
-
-  // All ref_ptr types are passed through unmodified.
-  addConversion([this](IREE::PtrType type) -> Type {
+  // Pointer types remain as pointer types types are passed through unmodified.
+  addConversion([this](IREE::PtrType type) -> Optional<Type> {
     // Recursively handle pointer target types (we want to convert ptr<index> to
     // ptr<i32>, for example).
     auto targetType = convertType(type.getTargetType());
     if (!targetType) {
-      return Type();
+      return llvm::None;
     }
     return IREE::PtrType::get(targetType);
   });
 
+  // Convert integer types.
+  addConversion([this](IntegerType integerType) -> Optional<Type> {
+    if (integerType.isInteger(32)) {
+      // i32 is always supported by the runtime.
+      return integerType;
+    } else if (integerType.getIntOrFloatBitWidth() < 32) {
+      // Promote i1/i8/i16 -> i32.
+      return IntegerType::get(32, integerType.getContext());
+    } else if (integerType.isInteger(64)) {
+      if (targetOptions_.i64Extension) {
+        // i64 is supported by the VM, use directly.
+        return integerType;
+      } else if (targetOptions_.truncateUnsupportedIntegers) {
+        // i64 is not supported and we still want to compile, so truncate to i32
+        // (unsafe if all bits are actually required!).
+        return IntegerType::get(32, integerType.getContext());
+      }
+    }
+    return llvm::None;
+  });
+
+  // Convert index types to the target bit width.
+  addConversion([this](IndexType indexType) -> Optional<Type> {
+    return IntegerType::get(targetOptions_.indexBits, indexType.getContext());
+  });
+
   // Convert ranked shape types (expanding all dims).
-  addConversion(
-      [](Shape::RankedShapeType rankedShape, SmallVectorImpl<Type> &results) {
-        auto vmIntegerType = IntegerType::get(32, rankedShape.getContext());
-        for (int i = 0; i < rankedShape.getRank(); ++i) {
-          if (rankedShape.isDimDynamic(i)) {
-            results.push_back(vmIntegerType);
-          }
-        }
-        return success();
-      });
+  addConversion([this](Shape::RankedShapeType rankedShape,
+                       SmallVectorImpl<Type> &results) {
+    auto indexType =
+        IntegerType::get(targetOptions_.indexBits, rankedShape.getContext());
+    for (int i = 0; i < rankedShape.getRank(); ++i) {
+      if (rankedShape.isDimDynamic(i)) {
+        results.push_back(indexType);
+      }
+    }
+    return success();
+  });
 
   // TODO(b/145876978): materialize conversion for other types
   addArgumentMaterialization([](OpBuilder &builder,
