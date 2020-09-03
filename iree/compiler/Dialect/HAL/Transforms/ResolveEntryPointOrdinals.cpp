f930dadc855be182fd169a685d504d53080261ec
Scott Todd <scotttodd@google.com>
2020-09-03 11:20:32 -0700

    Refactor ResolveEntryPointOrdinals to use rewrite patterns.

--- iree/compiler/Dialect/HAL/Transforms/ResolveEntryPointOrdinals.cpp
+++ iree/compiler/Dialect/HAL/Transforms/ResolveEntryPointOrdinals.cpp
@@ -12,19 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <utility>
-
-// TODO(scotttodd): trim includes
 #include "iree/compiler/Dialect/HAL/IR/HALOps.h"
 #include "iree/compiler/Dialect/HAL/Transforms/Passes.h"
-#include "iree/compiler/Dialect/IREE/IR/IREEOps.h"
-#include "llvm/ADT/StringSet.h"
-#include "mlir/Dialect/StandardOps/IR/Ops.h"
-#include "mlir/IR/Attributes.h"
-#include "mlir/IR/BlockAndValueMapping.h"
-#include "mlir/IR/Builders.h"
-#include "mlir/IR/Diagnostics.h"
-#include "mlir/IR/StandardTypes.h"
+#include "mlir/IR/PatternMatch.h"
 #include "mlir/Pass/Pass.h"
 
 namespace mlir {
@@ -32,54 +22,49 @@ namespace iree_compiler {
 namespace IREE {
 namespace HAL {
 
-// TODO(scotttodd): rewrite as conversion from CommandBufferDispatchOp to
-//                  CommandBufferDispatchOrdinalOp?
+class ResolveCommandBufferDispatchOrdinals
+    : public OpRewritePattern<IREE::HAL::CommandBufferDispatchOp> {
+ public:
+  using OpRewritePattern<IREE::HAL::CommandBufferDispatchOp>::OpRewritePattern;
+  LogicalResult matchAndRewrite(IREE::HAL::CommandBufferDispatchOp op,
+                                PatternRewriter &rewriter) const override {
+    auto entryPointOp = dyn_cast<IREE::HAL::ExecutableEntryPointOp>(
+        SymbolTable::lookupNearestSymbolFrom(op, op.entry_point()));
+    rewriter.replaceOpWithNewOp<IREE::HAL::CommandBufferDispatchOrdinalOp>(
+        op, op.command_buffer(), op.executable(), entryPointOp.ordinalAttr(),
+        op.workgroup_x(), op.workgroup_y(), op.workgroup_z());
+    return success();
+  }
+};
+
+class ResolveCommandBufferDispatchIndirectOrdinals
+    : public OpRewritePattern<IREE::HAL::CommandBufferDispatchIndirectOp> {
+ public:
+  using OpRewritePattern<
+      IREE::HAL::CommandBufferDispatchIndirectOp>::OpRewritePattern;
+  LogicalResult matchAndRewrite(IREE::HAL::CommandBufferDispatchIndirectOp op,
+                                PatternRewriter &rewriter) const override {
+    auto entryPointOp = dyn_cast<IREE::HAL::ExecutableEntryPointOp>(
+        SymbolTable::lookupNearestSymbolFrom(op, op.entry_point()));
+    rewriter
+        .replaceOpWithNewOp<IREE::HAL::CommandBufferDispatchIndirectOrdinalOp>(
+            op, op.command_buffer(), op.executable(),
+            entryPointOp.ordinalAttr(), op.workgroups_buffer(),
+            op.workgroups_offset());
+    return success();
+  }
+};
+
 class ResolveEntryPointOrdinalsPass
     : public PassWrapper<ResolveEntryPointOrdinalsPass,
                          OperationPass<ModuleOp>> {
  public:
   void runOnOperation() override {
-    auto moduleOp = getOperation();
-
-    SmallVector<IREE::HAL::CommandBufferDispatchOp, 4> dispatchOps;
-    moduleOp.walk([&](IREE::HAL::CommandBufferDispatchOp dispatchOp) {
-      dispatchOps.push_back(dispatchOp);
-    });
-    for (auto dispatchOp : dispatchOps) {
-      // Extract entry point ordinal from the nested symbol reference.
-      auto entryPointOp = dyn_cast_or_null<IREE::HAL::ExecutableEntryPointOp>(
-          SymbolTable::lookupNearestSymbolFrom(dispatchOp,
-                                               dispatchOp.entry_point()));
-      if (!entryPointOp) continue;
-      OpBuilder builder(dispatchOp);
-      builder.create<IREE::HAL::CommandBufferDispatchOrdinalOp>(
-          dispatchOp.getLoc(), dispatchOp.command_buffer(),
-          dispatchOp.executable(), entryPointOp.ordinalAttr(),
-          dispatchOp.workgroup_x(), dispatchOp.workgroup_y(),
-          dispatchOp.workgroup_z());
-      dispatchOp.erase();
-    }
-
-    SmallVector<IREE::HAL::CommandBufferDispatchIndirectOp, 4>
-        dispatchIndirectOps;
-    moduleOp.walk(
-        [&](IREE::HAL::CommandBufferDispatchIndirectOp dispatchIndirectOp) {
-          dispatchIndirectOps.push_back(dispatchIndirectOp);
-        });
-    for (auto dispatchIndirectOp : dispatchIndirectOps) {
-      // Extract entry point ordinal from the nested symbol reference.
-      auto entryPointOp = dyn_cast_or_null<IREE::HAL::ExecutableEntryPointOp>(
-          SymbolTable::lookupNearestSymbolFrom(
-              dispatchIndirectOp, dispatchIndirectOp.entry_point()));
-      if (!entryPointOp) continue;
-      OpBuilder builder(dispatchIndirectOp);
-      builder.create<IREE::HAL::CommandBufferDispatchIndirectOrdinalOp>(
-          dispatchIndirectOp.getLoc(), dispatchIndirectOp.command_buffer(),
-          dispatchIndirectOp.executable(), entryPointOp.ordinalAttr(),
-          dispatchIndirectOp.workgroups_buffer(),
-          dispatchIndirectOp.workgroups_offset());
-      dispatchIndirectOp.erase();
-    }
+    MLIRContext *context = &getContext();
+    OwningRewritePatternList patterns;
+    patterns.insert<ResolveCommandBufferDispatchOrdinals>(context);
+    patterns.insert<ResolveCommandBufferDispatchIndirectOrdinals>(context);
+    applyPatternsAndFoldGreedily(getOperation(), patterns);
   }
 };
 
