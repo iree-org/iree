10c5aa0b52ef772655875fa3519a6527f52c8ead
Hanhan Wang <hanchung@google.com>
2020-09-21 07:53:43 -0700

    Lower mhlo.concatenate op to subview + fill/copy

--- iree/compiler/Conversion/HLOToLinalg/HLOToLinalgOnBuffers.cpp
+++ iree/compiler/Conversion/HLOToLinalg/HLOToLinalgOnBuffers.cpp
@@ -550,9 +550,7 @@ LogicalResult ConvOpConversion::apply(
 //===----------------------------------------------------------------------===//
 
 namespace {
-/// Converts a mhlo.concatenate op to an indexed_generic op. The
-/// implementation adds more dimensions to make the loops correct, because
-/// each dimension in indexing maps matches to exactly one range.
+/// Converts a mhlo.concatenate op to subview ops + linalg.copy/fill ops.
 class ConcatenateOpConversion
     : public ConvertToLinalgBufferOp<ConcatenateOpConversion,
                                      mhlo::ConcatenateOp> {
@@ -571,77 +569,34 @@ LogicalResult ConcatenateOpConversion::apply(
   Location loc = op.getLoc();
   int dim = op.dimension();
   int rank = inputBuffers[0].getType().cast<ShapedType>().getRank();
-
-  SmallVector<Attribute, 2> indexingMaps;
-  SmallVector<AffineExpr, 4> exprs;
-  exprs.resize(rank);
-  for (int i = 0, j = 0, e = rank; i < e; ++i) {
-    if (i == dim) continue;
-    exprs[i] = rewriter.getAffineDimExpr(j++);
+  SmallVector<Value, 3> offsets, sizes, strides;
+  for (int i = 0; i < rank; ++i) {
+    offsets.push_back(rewriter.create<ConstantIndexOp>(loc, 0));
+    Value size = rewriter.create<DimOp>(loc, resultBuffers[0], i);
+    sizes.push_back(size);
+    strides.push_back(rewriter.create<ConstantIndexOp>(loc, 1));
   }
-  int nloops = rank + inputBuffers.size();
-  for (int i = 0, e = inputBuffers.size(); i < e; ++i) {
-    exprs[dim] = rewriter.getAffineDimExpr(rank + i);
-    indexingMaps.emplace_back(AffineMapAttr::get(AffineMap::get(
-        nloops, /*symbolCount=*/0, exprs, rewriter.getContext())));
-  }
-  exprs[dim] = rewriter.getAffineDimExpr(rank - 1);
-  indexingMaps.emplace_back(AffineMapAttr::get(
-      AffineMap::get(nloops, /*symbolCount=*/0, exprs, rewriter.getContext())));
-
-  SmallVector<Type, 4> bodyArgTypes, opResultTypes;
-  // Also make the dimension to be concatenated not a parallel loop.
-  int nonParallelLoops = nloops - rank + 1;
-  SmallVector<Value, 2> linalgOpArgs(inputBuffers.begin(), inputBuffers.end());
-  linalgOpArgs.push_back(resultBuffers[0]);
-  auto linalgOp = rewriter.create<linalg::IndexedGenericOp>(
-      loc, opResultTypes, linalgOpArgs,
-      rewriter.getI64IntegerAttr(inputBuffers.size()),  // args_in
-      rewriter.getI64IntegerAttr(1),                    // args_out
-      rewriter.getArrayAttr(indexingMaps),
-      getParallelAndReductionIterAttrs(rewriter, nloops, nonParallelLoops),
-      /*doc=*/nullptr, /*library_call=*/nullptr, /*symbol_source=*/nullptr);
-
-  // Add a block to the region.
-  auto *region = &linalgOp.region();
-  auto *block = rewriter.createBlock(region, region->end());
-  bodyArgTypes.append(nloops, rewriter.getIndexType());
-  auto resultType = op.getResult().getType().dyn_cast<ShapedType>();
-  bodyArgTypes.append(linalgOpArgs.size(), resultType.getElementType());
-  block->addArguments(bodyArgTypes);
-  rewriter.setInsertionPointToEnd(block);
 
   Value accBound = rewriter.create<ConstantIndexOp>(loc, 0);
-  Value dimArg = block->getArgument(rank - 1);
-  Value res = block->getArgument(nloops);
-  // Update the output buffer only when it iterate on the correct index of the
-  // operand. For example, if we are updating the first element of the
-  // concatenating dimension, the index of the first operand must be 0. If it's
-  // 1 or other, we need to keep the element the same.
-  Value canUpdate =
-      rewriter.create<ConstantIntOp>(loc, /*value=*/0, /*width=*/1);
-  for (int i = 0, e = inputBuffers.size(); i < e; ++i) {
-    Value t1 = rewriter.create<SubIOp>(loc, dimArg, accBound);
-    // The loop indice of operands start from the `rank`-th argument of the
-    // block.
-    Value t2 = rewriter.create<CmpIOp>(loc, CmpIPredicate::eq, t1,
-                                       block->getArgument(rank + i));
-    canUpdate = rewriter.create<OrOp>(loc, canUpdate, t2);
-
-    Value dimSize = rewriter.create<DimOp>(loc, inputBuffers[i], dim);
-    Value lbCond =
-        rewriter.create<CmpIOp>(loc, CmpIPredicate::sge, dimArg, accBound);
-    accBound = rewriter.create<AddIOp>(loc, accBound, dimSize);
-    Value ubCond =
-        rewriter.create<CmpIOp>(loc, CmpIPredicate::slt, dimArg, accBound);
-    Value cond = rewriter.create<AndOp>(loc, lbCond, ubCond);
-    // The first `nloops` arguments are indices.
-    res = rewriter.create<SelectOp>(loc, cond, block->getArgument(nloops + i),
-                                    res);
+  for (auto inBuf : inputBuffers) {
+    offsets[dim] = accBound;
+    if (auto cstOp = inBuf.getDefiningOp<ConstantOp>()) {
+      sizes[dim] = rewriter.create<ConstantIndexOp>(
+          loc, cstOp.getType().cast<ShapedType>().getShape()[dim]);
+      auto subViewOp = rewriter.create<SubViewOp>(loc, resultBuffers[0],
+                                                  offsets, sizes, strides);
+      auto inputConstAttr =
+          cstOp.valueAttr().cast<DenseElementsAttr>().getSplatValue();
+      Value cstVal = rewriter.create<ConstantOp>(loc, inputConstAttr);
+      rewriter.create<linalg::FillOp>(loc, subViewOp, cstVal);
+    } else {
+      sizes[dim] = rewriter.create<DimOp>(loc, inBuf, dim);
+      auto subViewOp = rewriter.create<SubViewOp>(loc, resultBuffers[0],
+                                                  offsets, sizes, strides);
+      rewriter.create<linalg::CopyOp>(loc, inBuf, subViewOp);
+    }
+    accBound = rewriter.create<AddIOp>(loc, accBound, sizes[dim]);
   }
-  res = rewriter.create<SelectOp>(
-      loc, canUpdate, res, block->getArgument(nloops + inputBuffers.size()));
-  rewriter.create<linalg::YieldOp>(loc, res);
 
   return success();
 }
