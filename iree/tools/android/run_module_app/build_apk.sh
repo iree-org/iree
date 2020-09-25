32b5d5f405b14f0ea625b533f39b8e2be61a2846
Lei Zhang <antiagainst@google.com>
2020-09-25 19:03:11 -0400

    Quote as much as possible

--- iree/tools/android/run_module_app/build_apk.sh
+++ iree/tools/android/run_module_app/build_apk.sh
@@ -83,7 +83,7 @@ while (( "$#" )); do
       exit 1
       ;;
     *) # Positional arguments
-      if [[ -z ${IREE_ARTIFACT_ROOT+x} ]]; then
+      if [[ -z "${IREE_ARTIFACT_ROOT+x}" ]]; then
         IREE_ARTIFACT_ROOT=$(readlink -f $1)
       else
         echo "Error: <artifact-directory> already set to ${IREE_ARTIFACT_ROOT}" >&2
@@ -94,9 +94,9 @@ while (( "$#" )); do
   esac
 done
 
-if [[ -z ${IREE_ARTIFACT_ROOT+x} ]] || [[ -z ${IREE_INPUT_MODULE_FILE+x} ]] || \
-   [[ -z ${IREE_ENTRY_FUNCTION+x} ]] || [[ -z ${IREE_INPUT_BUFFER_FILE+x} ]] || \
-   [[ -z ${IREE_DRIVER+x} ]]; then
+if [[ -z "${IREE_ARTIFACT_ROOT+x}" ]] || [[ -z "${IREE_INPUT_MODULE_FILE+x}" ]] || \
+   [[ -z "${IREE_ENTRY_FUNCTION+x}" ]] || [[ -z "${IREE_INPUT_BUFFER_FILE+x}" ]] || \
+   [[ -z "${IREE_DRIVER+x}" ]]; then
   echo "Error: missing necessary parameters" >&2
   print_usage_and_exit
 fi
@@ -106,13 +106,13 @@ fi
 #################################
 
 # The final Android APK name; default to "iree-run-module.apk".
-IREE_APK_NAME=${IREE_APK_NAME:-iree-run-module}
+IREE_APK_NAME="${IREE_APK_NAME:-iree-run-module}"
 # The CMAKE build type for compiling IREE. default to Release.
-IREE_BUILD_TYPE=${IREE_BUILD_TYPE:-Release}
+IREE_BUILD_TYPE="${IREE_BUILD_TYPE:-Release}"
 # The target Android API level; default to Android 10 (API level 29).
-IREE_ANDROID_API_LEVEL=${IREE_ANDROID_API_LEVEL:-29}
+IREE_ANDROID_API_LEVEL="${IREE_ANDROID_API_LEVEL:-29}"
 # The target Android platform ABI; default to arm64-v8a.
-IREE_ANDROID_ABI=${IREE_ANDROID_ABI:-arm64-v8a}
+IREE_ANDROID_ABI="${IREE_ANDROID_ABI:-arm64-v8a}"
 
 ################################
 # Android SDK/NDK configurations
@@ -123,63 +123,57 @@ IREE_ANDROID_ABI=${IREE_ANDROID_ABI:-arm64-v8a}
 # * $HOME/Android/Sdk for Linux.
 # * $HOME/Library/Android/sdk for macOS.
 # * $%LOCALAPPDATA%\Android\sdk for Windows.
-ANDROID_SDK_ROOT=${ANDROID_SDK_ROOT:?environment variable not set}
+ANDROID_SDK_ROOT="${ANDROID_SDK_ROOT:?environment variable not set}"
 # Android NDK root; must be provided as environment variable.
-ANDROID_NDK_ROOT=${ANDROID_NDK_ROOT:?environment variable not set}
-# Android SDK build tools version; default to empty given it's hard to
-# provide a default.
-# This typically should be provided by the caller and corresponds to a
-# directory under ${ANDROID_SDK_ROOT}/build-tools/, but there are cases where
-# an empty string is actually valid. For example, on Ubuntu, one can install
-# the SDK build tools via `apt install android-sdk-build-tools`, which will
-# then install it under /usr/lib/android-sdk/build-tools/.
-ANDROID_SDK_BUILD_TOOLS_VERSION=${ANDROID_SDK_BUILD_TOOLS_VERSION:-}
+ANDROID_NDK_ROOT="${ANDROID_NDK_ROOT:?environment variable not set}"
+# Android SDK build tools version; must be provided as environment variable.
+ANDROID_SDK_BUILD_TOOLS_VERSION="${ANDROID_SDK_BUILD_TOOLS_VERSION?environment variable not set}"
 # Key store for signing apk files; default to Android debug keystore created
 # by Android Studio.
-ANDROID_KEYSTORE=${ANDROID_KEYSTORE:-${HOME}/.android/debug.keystore}
+ANDROID_KEYSTORE="${ANDROID_KEYSTORE:-${HOME}/.android/debug.keystore}"
 
 ######################
 # IREE build toolchain
 ######################
 
-CMAKE_BIN=${CMAKE_BIN:-$(which cmake)}
-NINJA_BIN=${NINJA_BIN:-$(which ninja)}
-CC_BIN=${CC_BIN:-$(which clang)}
-CXX_BIN=${CXX_BIN:-$(which clang++)}
+CMAKE_BIN="${CMAKE_BIN:-$(which cmake)}"
+NINJA_BIN="${NINJA_BIN:-$(which ninja)}"
+CC_BIN="${CC_BIN:-$(which clang)}"
+CXX_BIN="${CXX_BIN:-$(which clang++)}"
 
-JAVAC_BIN=${JAVAC_BIN:-$(which javac)}
+JAVAC_BIN="${JAVAC_BIN:-$(which javac)}"
 
 ##################################
 # IREE source/artifact directories
 ##################################
 
 # IREE project source root.
-IREE_SOURCE_ROOT=$(git rev-parse --show-toplevel)
+IREE_SOURCE_ROOT="$(git rev-parse --show-toplevel)"
 # iree-run-module Android app source root.
-IREE_NATIVE_APP_SOURCE_ROOT=${IREE_SOURCE_ROOT}/iree/tools/android/run_module_app
+IREE_NATIVE_APP_SOURCE_ROOT="${IREE_SOURCE_ROOT}/iree/tools/android/run_module_app"
 
 # Directory for IREE native code intermediate intermediate artifacts.
-IREE_NATIVE_LIB_BUILD_DIR=${IREE_ARTIFACT_ROOT}/iree/${IREE_BUILD_TYPE}
+IREE_NATIVE_LIB_BUILD_DIR="${IREE_ARTIFACT_ROOT}/iree/${IREE_BUILD_TYPE}"
 # Directory for IREE native libraries.
-IREE_NATIVE_LIB_DIR=${IREE_ARTIFACT_ROOT}/libs/lib/${IREE_ANDROID_ABI}
+IREE_NATIVE_LIB_DIR="${IREE_ARTIFACT_ROOT}/libs/lib/${IREE_ANDROID_ABI}"
 # Directory for Android app assets.
-IREE_ASSET_DIR=${IREE_ARTIFACT_ROOT}/assets
+IREE_ASSET_DIR="${IREE_ARTIFACT_ROOT}/assets"
 # Directory for Android app R.class.
-IREE_RESOURCE_GEN_DIR=${IREE_ARTIFACT_ROOT}/rclass
+IREE_RESOURCE_GEN_DIR="${IREE_ARTIFACT_ROOT}/rclass"
 
 #########################
 # Android build toolchain
 #########################
 
-ANDROID_SDK_BUILD_TOOLS_DIR=${ANDROID_SDK_ROOT}/build-tools/${ANDROID_SDK_BUILD_TOOLS_VERSION}
-ANDROID_SDK_PLATFORMS_DIR=${ANDROID_SDK_ROOT}/platforms/android-${IREE_ANDROID_API_LEVEL}
+ANDROID_SDK_BUILD_TOOLS_DIR="${ANDROID_SDK_ROOT}/build-tools/${ANDROID_SDK_BUILD_TOOLS_VERSION}"
+ANDROID_SDK_PLATFORMS_DIR="${ANDROID_SDK_ROOT}/platforms/android-${IREE_ANDROID_API_LEVEL}"
 
-AAPT_BIN=${ANDROID_SDK_BUILD_TOOLS_DIR}/aapt
-DX_BIN=${ANDROID_SDK_BUILD_TOOLS_DIR}/dx
-ZIPALIGN_BIN=${ANDROID_SDK_BUILD_TOOLS_DIR}/zipalign
-APKSIGNER_BIN=${ANDROID_SDK_BUILD_TOOLS_DIR}/apksigner
+AAPT_BIN="${ANDROID_SDK_BUILD_TOOLS_DIR}/aapt"
+DX_BIN="${ANDROID_SDK_BUILD_TOOLS_DIR}/dx"
+ZIPALIGN_BIN="${ANDROID_SDK_BUILD_TOOLS_DIR}/zipalign"
+APKSIGNER_BIN="${ANDROID_SDK_BUILD_TOOLS_DIR}/apksigner"
 
-if [[ ! -x ${AAPT_BIN} ]] || [[ ! -x ${DX_BIN} ]] || [[ ! -x ${ZIPALIGN_BIN} ]] || [[ ! -x ${APKSIGNER_BIN} ]]; then
+if [[ ! -x "${AAPT_BIN}" ]] || [[ ! -x "${DX_BIN}" ]] || [[ ! -x "${ZIPALIGN_BIN}" ]] || [[ ! -x "${APKSIGNER_BIN}" ]]; then
   if [[ -z "${ANDROID_SDK_BUILD_TOOLS_VERSION}" ]]; then
     echo "Error: 'ANDROID_SDK_BUILD_TOOLS_VERSION' environment variable not set" &>2
   else
@@ -202,95 +196,95 @@ JAVAC="${JAVAC_BIN} -classpath ${ANDROID_SDK_PLATFORMS_DIR}/android.jar -sourcep
 # Build IREE native libraries
 #############################
 
-if [[ ! -d ${IREE_NATIVE_LIB_BUILD_DIR} ]]; then
-  mkdir -p ${IREE_NATIVE_LIB_BUILD_DIR}
+if [[ ! -d "${IREE_NATIVE_LIB_BUILD_DIR}" ]]; then
+  mkdir -p "${IREE_NATIVE_LIB_BUILD_DIR}"
 fi
 
 echo ">>> Building IREE native libraries <<<"
 
 IREE_NATIVE_LIB_NAME=iree_tools_android_run_module_app_iree_run_module_app
 
-pushd ${IREE_NATIVE_LIB_BUILD_DIR}
+pushd "${IREE_NATIVE_LIB_BUILD_DIR}"
 "${CMAKE_BIN}" "${IREE_SOURCE_ROOT}" -G Ninja \
-  -DCMAKE_BUILD_TYPE=${IREE_BUILD_TYPE} \
+  -DCMAKE_BUILD_TYPE="${IREE_BUILD_TYPE}" \
   -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK_ROOT}/build/cmake/android.toolchain.cmake" \
-  -DANDROID_ABI=${IREE_ANDROID_ABI} \
-  -DANDROID_PLATFORM=android-${IREE_ANDROID_API_LEVEL} \
+  -DANDROID_ABI="${IREE_ANDROID_ABI}" \
+  -DANDROID_PLATFORM="android-${IREE_ANDROID_API_LEVEL}" \
   -DIREE_HOST_C_COMPILER="${CC_BIN}" \
   -DIREE_HOST_CXX_COMPILER="${CXX_BIN}" \
   -DIREE_BUILD_COMPILER=OFF \
   -DIREE_BUILD_TESTS=OFF \
   -DIREE_BUILD_SAMPLES=OFF
-${NINJA_BIN} ${IREE_NATIVE_LIB_NAME}
+"${NINJA_BIN}" "${IREE_NATIVE_LIB_NAME}"
 popd
 
 #####################
 # Package Android app
 #####################
 
-rm -rf ${IREE_ARTIFACT_ROOT}/*.apk
+rm -rf "${IREE_ARTIFACT_ROOT}"/*.apk
 
 echo ">>> Generating AndroidManifest.xml <<<"
 
 # Create an AndroidManifest.xml with proper target SDK version.
-rm -rf ${IREE_ARTIFACT_ROOT}/AndroidManifest.xml
-IREE_ANDROID_API_LEVEL=${IREE_ANDROID_API_LEVEL} envsubst \
-  < ${IREE_NATIVE_APP_SOURCE_ROOT}/AndroidManifest.xml.template \
-  > ${IREE_ARTIFACT_ROOT}/AndroidManifest.xml
+rm -rf "${IREE_ARTIFACT_ROOT}/AndroidManifest.xml"
+IREE_ANDROID_API_LEVEL="${IREE_ANDROID_API_LEVEL}" envsubst \
+  < "${IREE_NATIVE_APP_SOURCE_ROOT}/AndroidManifest.xml.template" \
+  > "${IREE_ARTIFACT_ROOT}/AndroidManifest.xml"
 
 echo ">>> Preparing shared libraries and vm module information <<<"
 
 # Find the compiled iree_run_module_app shared library and symlink it to a
 # known location for packaging.
-rm -rf ${IREE_NATIVE_LIB_DIR} && mkdir -p ${IREE_NATIVE_LIB_DIR}
+rm -rf "${IREE_NATIVE_LIB_DIR}" && mkdir -p "${IREE_NATIVE_LIB_DIR}"
 IREE_NATIVE_LIB=$(find ${IREE_NATIVE_LIB_BUILD_DIR} -name "lib${IREE_NATIVE_LIB_NAME}.so")
 # Note: the target link name must match with
 # run_module_app/AndroidManifest.xml.template.
-ln -sf ${IREE_NATIVE_LIB} ${IREE_NATIVE_LIB_DIR}/libiree_run_module_app.so
+ln -sf "${IREE_NATIVE_LIB}" "${IREE_NATIVE_LIB_DIR}/libiree_run_module_app.so"
 
 # Copy the VM FlatBuffer and iree-run-module invocation related information
 # over as assets.
-rm -rf ${IREE_ASSET_DIR} && mkdir -p ${IREE_ASSET_DIR}
+rm -rf "${IREE_ASSET_DIR}" && mkdir -p "${IREE_ASSET_DIR}"
 # Note: the following files must match with run_module_app/src/main.cc.
-cp ${IREE_INPUT_MODULE_FILE} ${IREE_ASSET_DIR}/module.vmfb
-cp ${IREE_INPUT_BUFFER_FILE} ${IREE_ASSET_DIR}/inputs.txt
-echo -n ${IREE_ENTRY_FUNCTION} > ${IREE_ASSET_DIR}/entry_function.txt
-echo -n ${IREE_DRIVER} > ${IREE_ASSET_DIR}/driver.txt
+cp "${IREE_INPUT_MODULE_FILE}" "${IREE_ASSET_DIR}/module.vmfb"
+cp "${IREE_INPUT_BUFFER_FILE}" "${IREE_ASSET_DIR}/inputs.txt"
+echo -n "${IREE_ENTRY_FUNCTION}" > "${IREE_ASSET_DIR}/entry_function.txt"
+echo -n "${IREE_DRIVER}" > "${IREE_ASSET_DIR}/driver.txt"
 
 echo ">>> Compiling app resources <<<"
 
 # Generate the R.java for resources.
-rm -rf ${IREE_RESOURCE_GEN_DIR} && mkdir -p ${IREE_RESOURCE_GEN_DIR}
+rm -rf "${IREE_RESOURCE_GEN_DIR}" && mkdir -p "${IREE_RESOURCE_GEN_DIR}"
 ${AAPT_PACK} --non-constant-id -m \
-  -M ${IREE_ARTIFACT_ROOT}/AndroidManifest.xml \
-  -S ${IREE_NATIVE_APP_SOURCE_ROOT}/res \
-  -J ${IREE_RESOURCE_GEN_DIR} \
+  -M "${IREE_ARTIFACT_ROOT}/AndroidManifest.xml" \
+  -S "${IREE_NATIVE_APP_SOURCE_ROOT}/res" \
+  -J "${IREE_RESOURCE_GEN_DIR}" \
   --generate-dependencies
 
 # Compile the R.java and create classes.dex out of it for Android.
 echo "Using javac: '${JAVAC_BIN}'"
-rm -rf ${IREE_ARTIFACT_ROOT}/classes.dex ${IREE_ARTIFACT_ROOT}/com/
-${JAVAC} ${IREE_RESOURCE_GEN_DIR}/com/google/iree/run_module/*.java
-${DX} --output="${IREE_ARTIFACT_ROOT}/classes.dex" ${IREE_ARTIFACT_ROOT}
+rm -rf "${IREE_ARTIFACT_ROOT}/classes.dex" "${IREE_ARTIFACT_ROOT}/com/"
+${JAVAC} "${IREE_RESOURCE_GEN_DIR}"/com/google/iree/run_module/*.java
+${DX} --output="${IREE_ARTIFACT_ROOT}/classes.dex" "${IREE_ARTIFACT_ROOT}"
 
 echo ">>> Packaging apk file <<<"
 
 # Package assets and shared libraries into an apk file.
 ${AAPT_PACK} -m \
-  -M ${IREE_ARTIFACT_ROOT}/AndroidManifest.xml \
-  -S ${IREE_NATIVE_APP_SOURCE_ROOT}/res \
-  -A ${IREE_ARTIFACT_ROOT}/assets \
+  -M "${IREE_ARTIFACT_ROOT}/AndroidManifest.xml" \
+  -S "${IREE_NATIVE_APP_SOURCE_ROOT}/res" \
+  -A "${IREE_ARTIFACT_ROOT}/assets" \
   -F "${IREE_ARTIFACT_ROOT}/${IREE_APK_NAME}.unaligned.apk" \
-  --shared-lib ${IREE_ARTIFACT_ROOT}/libs
+  --shared-lib "${IREE_ARTIFACT_ROOT}/libs"
 
-pushd ${IREE_ARTIFACT_ROOT}
+pushd "${IREE_ARTIFACT_ROOT}"
 # Also package the resources into the apk file.
-${AAPT_ADD} ${IREE_APK_NAME}.unaligned.apk classes.dex
+${AAPT_ADD} "${IREE_APK_NAME}.unaligned.apk" classes.dex
 echo ">>> Aligning apk file <<<"
-${ZIPALIGN} ${IREE_APK_NAME}.unaligned.apk ${IREE_APK_NAME}.apk
+${ZIPALIGN} "${IREE_APK_NAME}.unaligned.apk" "${IREE_APK_NAME}.apk"
 echo ">>> Signing apk file <<<"
 echo "NOTE: if you are using the Android Studio's debug keystore, the password is 'android'."
-${APKSIGN} --ks ${ANDROID_KEYSTORE} --min-sdk-version 28 ${IREE_APK_NAME}.apk
+${APKSIGN} --ks "${ANDROID_KEYSTORE}" --min-sdk-version 28 "${IREE_APK_NAME}.apk"
 popd
 
 echo ">>> Done: '${IREE_ARTIFACT_ROOT}/${IREE_APK_NAME}.apk' <<<"
