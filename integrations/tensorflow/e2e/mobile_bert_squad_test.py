892ce3ffdfc92f75cd3212ea0f24e7a056f99e58
Phoenix Meadowlark <meadowlark@google.com>
2020-09-29 12:34:02 -0700

    Open source `mobile_bert_squad_test` with TFLite support (#3281)
    Open sources a test for MobileBert on the SQuAD task. 
    
    Support for compiling `tf.Module`s with `tf.compat.v1.lite` was 
    added to get TFLite to support compiling the model. The comparison 
    `atol` is set to `1e0` because TFLite has a numerical difference of 
    `~0.13` with TensorFlow on this model/input pair.
    
    These tests don't run on the CI, so I tested them locally.

---
+++ integrations/tensorflow/e2e/mobile_bert_squad_test.py
@@ -0,0 +1,91 @@
+# Lint as: python3
+"""Test MobileBERT.
+
+Model topology and weights are from
+https://github.com/google-research/google-research/tree/master/mobilebert
+"""
+
+import os
+import posixpath
+
+from absl import app
+from absl import flags
+import numpy as np
+from pyiree.tf.support import tf_test_utils
+import tensorflow.compat.v2 as tf
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_boolean("use_quantized_weights", False,
+                     "Whether to use quantized or floating point weights.")
+
+MAX_SEQ_LENGTH = 384  # Max input sequence length used in mobilebert_squad.
+
+FILE_NAME = "mobilebert_squad_savedmodels.tar.gz"
+MODEL_URL = posixpath.join(
+    "https://storage.googleapis.com/cloud-tpu-checkpoints/mobilebert/",
+    FILE_NAME)
+
+
+class MobileBertSquad(tf.Module):
+  """Wrapper of MobileBertSquad saved model v1."""
+
+  def __init__(self):
+    self.model_path = self.get_model_path()
+    self.saved_model = tf.saved_model.load(self.model_path, tags=["serve"])
+    self.inference_func = self.saved_model.signatures["serving_default"]
+
+  @staticmethod
+  def get_model_path():
+    model_type = "quant_saved_model" if FLAGS.use_quantized_weights else "float"
+
+    # Get_file will download the model weights from a publicly available folder,
+    # save them to cache_dir=~/.keras/datasets/ and return a path to them.
+    model_path = tf.keras.utils.get_file(FILE_NAME, MODEL_URL, untar=True)
+    model_dir = os.path.dirname(model_path)
+    extracted_name = FILE_NAME.split(".")[0]
+    model_path = os.path.join(model_dir, extracted_name, model_type)
+    return model_path
+
+  @staticmethod
+  def get_legacy_tflite_saved_model_converter_kwargs():
+    return dict([("input_arrays", ["input_ids", "input_mask", "segment_ids"]),
+                 ("output_arrays", ["start_logits", "end_logits"]),
+                 ("exported_name", "predict"),
+                 ("model_path", MobileBertSquad.get_model_path())])
+
+  @tf.function(input_signature=[
+      tf.TensorSpec((1, MAX_SEQ_LENGTH), tf.int32),
+      tf.TensorSpec((1, MAX_SEQ_LENGTH), tf.int32),
+      tf.TensorSpec((1, MAX_SEQ_LENGTH), tf.int32),
+  ])
+  def predict(self, input_ids, input_mask, segment_ids):
+    inputs = {
+        "input_ids": input_ids,
+        "input_mask": input_mask,
+        "segment_ids": segment_ids,
+    }
+    return self.inference_func(**inputs)
+
+
+@tf_test_utils.compile_module(MobileBertSquad, exported_names=["predict"])
+class MobileBertSquadTest(tf_test_utils.TracedModuleTestCase):
+  """Tests of MobileBertSquad."""
+
+  def test_predict(self):
+
+    def predict(module):
+      input_ids = np.zeros((1, MAX_SEQ_LENGTH), dtype=np.int32)
+      input_mask = np.zeros((1, MAX_SEQ_LENGTH), dtype=np.int32)
+      segment_ids = np.zeros((1, MAX_SEQ_LENGTH), dtype=np.int32)
+
+      module.predict(input_ids, input_mask, segment_ids, atol=1e0)
+
+    self.compare_backends(predict)
+
+
+if __name__ == "__main__":
+  if hasattr(tf, "enable_v2_behavior"):
+    tf.enable_v2_behavior()
+
+  tf.test.main()
