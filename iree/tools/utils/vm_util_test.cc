92f0c671f8fcf475b0c184d24e5541607f7c87af
Lei Zhang <antiagainst@google.com>
2020-10-01 10:00:59 -0400

    [android] Wrap IREE core libraries as an Android Native App (#3108)
    This commit adds necessary scaffolding to create an Android
    application that executes a single IREE module as a native activity.
    
    Specifically, it adds a new shared libary, libiree_run_module_app.so,
    which implements android_main to connect with Android system with
    native app glue. A script is provided to package the shared libary,
    VM module and its invocation information, and all other necessary
    APK componets into an Android APK file.
    
    
    Along the way, create subdirectory for utility targets in tools/.
    This allows us to have a better source file organization. It also
    allows other directories to depend on those utility targets.

--- iree/tools/utils/vm_util_test.cc
+++ iree/tools/utils/vm_util_test.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "iree/tools/vm_util.h"
+#include "iree/tools/utils/vm_util.h"
 
 #include <sstream>
 
