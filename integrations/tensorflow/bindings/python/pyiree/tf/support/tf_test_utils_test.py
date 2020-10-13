5296b80254927646f7c3171f268a3cfe34a81b18
Phoenix Meadowlark <meadowlark@google.com>
2020-10-13 15:59:52 -0700

    Propagate comparison errors into e2e test failures (#3456)
    Tracks the specific comparison errors found during testing 
    and propagates them to the `self.fail` statement so that it is 
    easier to understand them without having access to the full logs.
    
    Example message:
    
    ```
    ======================================================================
    FAIL: test_predict (__main__.AppTest)
    AppTest.test_predict
    ----------------------------------------------------------------------
    Traceback (most recent call last):
      File ".../integrations/tensorflow/e2e/keras/vision_model_test.py", line 158, in test_predict
        self.compare_backends(predict, self._modules)
      File .../integrations/tensorflow/bindings/python/pyiree/tf/support/tf_test_utils.py", line 762, in compare_backends
        self.fail(
      File "/usr/local/google/home/meadowlark/.pyenv/versions/3.8.3/lib/python3.8/site-packages/absl/testing/absltest.py", line 1764, in fail
        return super(TestCase, self).fail(self._formatMessage(prefix, msg))
    AssertionError: Comparison between the reference backend and the following targets failed: ['iree_llvmjit']. Errors: 
      - Floating point difference between ref and tar was too large. Max abs diff: 5.28e-01, atol: 1.00e-05, max relative diff: 9.98e-01, rtol: 1.00e-05
    See the logs above for more details about the non-matching calls.
    
    ----------------------------------------------------------------------
    ```

--- integrations/tensorflow/bindings/python/pyiree/tf/support/tf_test_utils_test.py
+++ integrations/tensorflow/bindings/python/pyiree/tf/support/tf_test_utils_test.py
@@ -104,7 +104,7 @@ class UtilsTests(tf.test.TestCase, parameterized.TestCase):
         ],
     }
     # yapf: enable
-    same = tf_test_utils.Trace._check_same(ref, tar, rtol=1e-6, atol=1e-6)
+    same, _ = tf_test_utils.Trace._check_same(ref, tar, rtol=1e-6, atol=1e-6)
     self.assertEqual(tar_same, same)
 
   def test_trace_inputs_and_outputs(self):
@@ -171,7 +171,9 @@ class UtilsTests(tf.test.TestCase, parameterized.TestCase):
     vmla_trace = tf_test_utils.Trace(vmla_module, vmla_function)
     vmla_function(tf_test_utils.TracedModule(vmla_module, vmla_trace))
 
-    self.assertFalse(tf_test_utils.Trace.compare_traces(tf_trace, vmla_trace))
+    same, error_messages = tf_test_utils.Trace.compare_traces(
+        tf_trace, vmla_trace)
+    self.assertFalse(same)
 
   def test_trace_serialize_and_load(self):
 
