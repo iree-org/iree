d23c116089f72961cb9bcfecd06e0be35176a0a3
meadowlark@google.com <meadowlark@google.com>
2020-10-16 15:34:18 -0500

    Update license

--- third_party/format_diff/format_diff.py
+++ third_party/format_diff/format_diff.py
@@ -2,7 +2,7 @@
 #
 #===- format_diff.py - Diff Reformatter ----*- python -*--===#
 #
-# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #
