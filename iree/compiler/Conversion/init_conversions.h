a250132408c603d9da59ec62c3643930aa37018f
Ahmed S. Taei <ataei@google.com>
2020-10-23 15:57:27 +0000

    Register llvm conversion passes

--- iree/compiler/Conversion/init_conversions.h
+++ iree/compiler/Conversion/init_conversions.h
@@ -59,7 +59,9 @@ inline void registerLinalgToSPIRVPasses() {
 inline void registerLinalgToLLVMPasses() {
   static bool init_once = []() {
     // LinalgToLLVM
+    createConvImg2ColMatmulConversionPass();
     createLinalgTileAndDistributePass();
+    createMatMulTileAndVectorizePass();
     return true;
   }();
   (void)init_once;
