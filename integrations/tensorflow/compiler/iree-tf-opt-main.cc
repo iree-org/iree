e4b95e0a35b2a424c8c3642c42bc5a38304e8f54
Geoffrey Martin-Noble <gcmn@google.com>
2020-11-04 20:33:16 -0800

    Refactor shared functionality out of IREE tools (#3727)

--- integrations/tensorflow/compiler/iree-tf-opt-main.cc
+++ integrations/tensorflow/compiler/iree-tf-opt-main.cc
@@ -18,68 +18,16 @@
 
 #include "integrations/tensorflow/compiler/dialect/tf_strings/ir/dialect.h"
 #include "integrations/tensorflow/compiler/dialect/tf_tensorlist/ir/tf_tensorlist_dialect.h"
-#include "iree/compiler/Conversion/HLOToLinalg/Passes.h"
-#include "iree/compiler/Conversion/init_conversions.h"
-#include "iree/compiler/Dialect/HAL/Conversion/Passes.h"
-#include "iree/tools/init_compiler_modules.h"
-#include "iree/tools/init_iree_dialects.h"
-#include "iree/tools/init_iree_passes.h"
-#include "iree/tools/init_mlir_dialects.h"
-#include "iree/tools/init_mlir_passes.h"
+#include "iree/tools/init_dialects.h"
+#include "iree/tools/init_passes.h"
 #include "iree/tools/init_targets.h"
-#include "iree/tools/init_xla_dialects.h"
-#include "llvm/Support/CommandLine.h"
 #include "llvm/Support/InitLLVM.h"
-#include "llvm/Support/SourceMgr.h"
-#include "llvm/Support/ToolOutputFile.h"
-#include "mlir/IR/AsmState.h"
 #include "mlir/IR/Dialect.h"
-#include "mlir/Pass/Pass.h"
-#include "mlir/Pass/PassManager.h"
-#include "mlir/Support/FileUtilities.h"
 #include "mlir/Support/MlirOptMain.h"
 #include "tensorflow/compiler/mlir/tensorflow/ir/tf_device.h"
 #include "tensorflow/compiler/mlir/tensorflow/ir/tf_executor.h"
 #include "tensorflow/compiler/mlir/tensorflow/ir/tf_ops.h"
 #include "tensorflow/compiler/mlir/tensorflow/ir/tf_saved_model.h"
-
-#ifdef IREE_HAVE_EMITC_DIALECT
-#include "emitc/InitDialect.h"
-#endif  // IREE_HAVE_EMITC_DIALECT
-
-static llvm::cl::opt<std::string> inputFilename(llvm::cl::Positional,
-                                                llvm::cl::desc("<input file>"),
-                                                llvm::cl::init("-"));
-
-static llvm::cl::opt<std::string> outputFilename(
-    "o", llvm::cl::desc("Output filename"), llvm::cl::value_desc("filename"),
-    llvm::cl::init("-"));
-
-static llvm::cl::opt<bool> splitInputFile(
-    "split-input-file",
-    llvm::cl::desc("Split the input file into pieces and process each "
-                   "chunk independently"),
-    llvm::cl::init(false));
-
-static llvm::cl::opt<bool> verifyDiagnostics(
-    "verify-diagnostics",
-    llvm::cl::desc("Check that emitted diagnostics match "
-                   "expected-* lines on the corresponding line"),
-    llvm::cl::init(false));
-
-static llvm::cl::opt<bool> verifyPasses(
-    "verify-each",
-    llvm::cl::desc("Run the verifier after each transformation pass"),
-    llvm::cl::init(true));
-
-static llvm::cl::opt<bool> allowUnregisteredDialects(
-    "allow-unregistered-dialect",
-    llvm::cl::desc("Allow operation with no registered dialects"),
-    llvm::cl::init(true));
-
-static llvm::cl::opt<bool> showDialects(
-    "show-dialects", llvm::cl::desc("Print the list of registered dialects"),
-    llvm::cl::init(false));
 
 void registerTFDialects(mlir::DialectRegistry &registry) {
   registry.insert<mlir::TF::TensorFlowDialect,
@@ -94,70 +42,20 @@ void registerExtensionDialects(mlir::DialectRegistry &registry) {
 }
 
 int main(int argc, char **argv) {
-  // TODO(#2958): There's a lot of duplication with iree-opt here. Factor out
-  // the common functionality.
   llvm::InitLLVM y(argc, argv);
 
   mlir::DialectRegistry registry;
-  mlir::registerMlirDialects(registry);
-  mlir::registerMlirPasses();
-#ifdef IREE_HAVE_EMITC_DIALECT
-  mlir::registerEmitCDialect(registry);
-#endif  // IREE_HAVE_EMITC_DIALECT
-  mlir::registerXLADialects(registry);
-  mlir::iree_compiler::registerIreeDialects(registry);
-  mlir::iree_compiler::registerIreeCompilerModuleDialects(registry);
+  mlir::iree_compiler::registerAllDialects(registry);
   registerTFDialects(registry);
   registerExtensionDialects(registry);
 
-  mlir::iree_compiler::registerAllIreePasses();
-  mlir::iree_compiler::registerHALConversionPasses();
+  mlir::iree_compiler::registerAllPasses();
   mlir::iree_compiler::registerHALTargetBackends();
-  mlir::iree_compiler::registerLinalgToSPIRVPasses();
-  mlir::iree_compiler::registerHLOToLinalgPasses();
-  mlir::iree_compiler::registerLinalgToLLVMPasses();
 
-  // Register MLIRContext command-line options like
-  // -mlir-print-op-on-diagnostic.
-  mlir::registerMLIRContextCLOptions();
-  // Register assembly printer command-line options like
-  // -mlir-print-op-generic.
-  mlir::registerAsmPrinterCLOptions();
-  // Register pass manager command-line options like -print-ir-*.
-  mlir::registerPassManagerCLOptions();
-
-  mlir::PassPipelineCLParser passPipeline("", "Compiler passes to run");
-
-  // Parse pass names in main to ensure static initialization completed.
-  llvm::cl::ParseCommandLineOptions(argc, argv,
-                                    "IREE modular optimizer driver\n");
-
-  if (showDialects) {
-    llvm::outs() << "Available Dialects:\n";
-    interleave(
-        registry, llvm::outs(),
-        [](auto &registryEntry) { llvm::outs() << registryEntry.first; }, "\n");
-    return 0;
-  }
-
-  // Set up the input file.
-  std::string errorMessage;
-  auto file = mlir::openInputFile(inputFilename, &errorMessage);
-  if (!file) {
-    llvm::errs() << errorMessage << "\n";
+  if (failed(MlirOptMain(argc, argv, "IREE-TF modular optimizer driver\n",
+                         registry,
+                         /*preloadDialectsInContext=*/false))) {
     return 1;
   }
-
-  auto output = mlir::openOutputFile(outputFilename, &errorMessage);
-  if (!output) {
-    llvm::errs() << errorMessage << "\n";
-    exit(1);
-  }
-
-  if (failed(mlir::MlirOptMain(output->os(), std::move(file), passPipeline,
-                               registry, splitInputFile, verifyDiagnostics,
-                               verifyPasses, allowUnregisteredDialects,
-                               /*preloadDialectsInContext=*/false))) {
-    return 1;
-  }
+  return 0;
 }
