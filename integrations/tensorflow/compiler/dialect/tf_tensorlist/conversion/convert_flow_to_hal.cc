581145d6f6d7351b9575545539f2e85683952377
Rob Suderman <suderman@google.com>
2020-11-19 14:43:08 -0800

    Remove shape from TensorList module arguments
    Shape is a redundant parameter and can be tracked within the tensorlist
    structure. Storing on the structure avoids handing the data around needlessly.
    
    PiperOrigin-RevId: 343373511

--- integrations/tensorflow/compiler/dialect/tf_tensorlist/conversion/convert_flow_to_hal.cc
+++ integrations/tensorflow/compiler/dialect/tf_tensorlist/conversion/convert_flow_to_hal.cc
@@ -16,10 +16,120 @@
 
 #include "integrations/tensorflow/compiler/dialect/tf_tensorlist/ir/tf_tensorlist_ops.h"
 #include "iree/compiler/Dialect/HAL/Conversion/ConversionTarget.h"
+#include "iree/compiler/Dialect/HAL/IR/HALOps.h"
+#include "iree/compiler/Dialect/HAL/IR/HALTypes.h"
+#include "iree/compiler/Dialect/HAL/Utils/TypeUtils.h"
 #include "iree/compiler/Dialect/Modules/TensorList/IR/TensorListOps.h"
+#include "iree/compiler/Dialect/Modules/TensorList/IR/TensorListTypes.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/IR/StandardTypes.h"
 
 namespace mlir {
 namespace iree_compiler {
+
+namespace {
+
+LogicalResult getBufferView(Operation *srcOp, Value srcOperand,
+                            Value dstOperand,
+                            ConversionPatternRewriter &rewriter,
+                            Value &retValue) {
+  auto operand = IREE::HAL::TensorRewriteAdaptor::getChecked(
+      srcOp->getLoc(), srcOperand, dstOperand, rewriter);
+  if (!operand.hasValue()) {
+    return srcOp->emitOpError() << "unable to create adaptor for operand";
+  }
+  auto bufferView = operand->getBufferView();
+  if (!bufferView) {
+    return srcOp->emitOpError() << "unable to get buffer view for operand";
+  }
+
+  retValue = bufferView;
+  return success();
+}
+
+class ReserveOpConversion : public OpConversionPattern<tf_tensorlist::Reserve> {
+ public:
+  ReserveOpConversion(MLIRContext *ctx, TypeConverter &converter)
+      : OpConversionPattern(ctx) {}
+
+  LogicalResult matchAndRewrite(
+      tf_tensorlist::Reserve reserveOp, llvm::ArrayRef<Value> newOperands,
+      ConversionPatternRewriter &rewriter) const override {
+    auto elementTy = reserveOp.element_type();
+    auto element_value = IREE::HAL::getElementTypeValue(elementTy).getValue();
+
+    Value operand0, operand1;
+    getBufferView(reserveOp, reserveOp.getOperand(0), newOperands[0], rewriter,
+                  operand0);
+    getBufferView(reserveOp, reserveOp.getOperand(1), newOperands[1], rewriter,
+                  operand1);
+
+    rewriter.replaceOpWithNewOp<IREE::TensorList::Reserve>(
+        reserveOp,
+        IREE::TensorList::TensorListType::get(reserveOp.getContext()), operand0,
+        operand1, rewriter.getI32IntegerAttr(element_value));
+    return success();
+  }
+};
+
+class ConcatOpConversion : public OpConversionPattern<tf_tensorlist::Concat> {
+ public:
+  ConcatOpConversion(MLIRContext *ctx, TypeConverter &converter)
+      : OpConversionPattern(ctx) {}
+
+  LogicalResult matchAndRewrite(
+      tf_tensorlist::Concat concatOp, llvm::ArrayRef<Value> newOperands,
+      ConversionPatternRewriter &rewriter) const override {
+    auto device =
+        rewriter.createOrFold<IREE::HAL::ExSharedDeviceOp>(concatOp.getLoc());
+    auto allocator =
+        rewriter.create<IREE::HAL::DeviceAllocatorOp>(concatOp.getLoc(), device)
+            .getResult();
+
+    auto newConcatOp = rewriter.createOrFold<IREE::TensorList::Concat>(
+        concatOp.getLoc(),
+        IREE::HAL::BufferViewType::get(rewriter.getContext()), allocator,
+        newOperands[0]);
+
+    auto bufferOp = rewriter.createOrFold<IREE::HAL::BufferViewBufferOp>(
+        newConcatOp.getLoc(), newConcatOp);
+
+    rewriter.replaceOp(concatOp, bufferOp);
+    return success();
+  }
+};
+
+class StackOpConversion : public OpConversionPattern<tf_tensorlist::Stack> {
+ public:
+  StackOpConversion(MLIRContext *ctx, TypeConverter &converter)
+      : OpConversionPattern(ctx) {}
+
+  LogicalResult matchAndRewrite(
+      tf_tensorlist::Stack stackOp, llvm::ArrayRef<Value> newOperands,
+      ConversionPatternRewriter &rewriter) const override {
+    auto device =
+        rewriter.createOrFold<IREE::HAL::ExSharedDeviceOp>(stackOp.getLoc());
+    auto allocator =
+        rewriter.create<IREE::HAL::DeviceAllocatorOp>(stackOp.getLoc(), device)
+            .getResult();
+
+    Value operand1;
+    getBufferView(stackOp, stackOp.getOperand(1), newOperands[1], rewriter,
+                  operand1);
+
+    auto newStackOp = rewriter.createOrFold<IREE::TensorList::Stack>(
+        stackOp.getLoc(), IREE::HAL::BufferViewType::get(rewriter.getContext()),
+        allocator, newOperands[0], operand1);
+
+    auto bufferOp = rewriter.createOrFold<IREE::HAL::BufferViewBufferOp>(
+        stackOp.getLoc(), newStackOp);
+
+    rewriter.replaceOp(stackOp, bufferOp);
+    return success();
+  }
+};
+
+}  // namespace
 
 void populateTensorListToHALPatterns(MLIRContext *context,
                                      OwningRewritePatternList &patterns,
@@ -28,9 +138,9 @@ void populateTensorListToHALPatterns(MLIRContext *context,
   // as we just want the simple form. If we wanted to perform additional
   // verification or have a specific use case (such as a place where only the
   // buffer is required and the shape is not) we could add our own.
-  patterns.insert<
-      HALOpConversion<tf_tensorlist::Reserve, IREE::TensorList::Reserve>>(
-      context, typeConverter);
+  // patterns.insert<
+  //     HALOpConversion<tf_tensorlist::Reserve, IREE::TensorList::Reserve>>(
+  //     context, typeConverter);
   patterns.insert<
       HALOpConversion<tf_tensorlist::GetItem, IREE::TensorList::GetItem>>(
       context, typeConverter);
@@ -40,12 +150,10 @@ void populateTensorListToHALPatterns(MLIRContext *context,
   patterns.insert<
       HALOpConversion<tf_tensorlist::FromTensor, IREE::TensorList::FromTensor>>(
       context, typeConverter);
-  patterns
-      .insert<HALOpConversion<tf_tensorlist::Concat, IREE::TensorList::Concat>>(
-          context, typeConverter);
-  patterns
-      .insert<HALOpConversion<tf_tensorlist::Stack, IREE::TensorList::Stack>>(
-          context, typeConverter);
+
+  patterns.insert<ConcatOpConversion>(context, typeConverter);
+  patterns.insert<ReserveOpConversion>(context, typeConverter);
+  patterns.insert<StackOpConversion>(context, typeConverter);
 }
 
 }  // namespace iree_compiler
