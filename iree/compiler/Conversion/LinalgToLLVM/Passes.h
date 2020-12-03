77aaf620c53c3a3086a181a41c936f1dbbb12cca
Nicolas Vasilache <ntv@google.com>
2020-12-03 13:27:43 -0800

    Sketch an experimental Linalg on tensors LLVM compiler
    This is a minimal sketch of a prototype pass to connect Linalg on tensors to CPU execution. This does not require dispatch region reordering and can connect to end-to-end execution once the 2 missing passes are upstreamed.
    
    At the same time, this allows working on a common pass ordering sketch to allow reordering dispatch region creation as necessary.
    
    PiperOrigin-RevId: 345522312

--- iree/compiler/Conversion/LinalgToLLVM/Passes.h
+++ iree/compiler/Conversion/LinalgToLLVM/Passes.h
@@ -45,6 +45,10 @@ void populateConvImg2ColMatmulConversionPatterns(
 /// Performs the final conversion to LLVM dialect.
 std::unique_ptr<OperationPass<ModuleOp>> createConvertToLLVMPass();
 
+/// Pass to rewrite Linalg on tensors destructive updates into updates through
+/// memory.
+std::unique_ptr<OperationPass<FuncOp>> createLinalgRewriteDestructiveUpdates();
+
 /// Populates passes needed to lower a XLA HLO op to LLVM dialect via the
 /// structured ops path. The pass manager `pm` in here should operate on the
 /// module within the IREE::HAL::ExecutableOp.
