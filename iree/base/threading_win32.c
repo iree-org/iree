150a4b4143eb7816861d50709836764f57a0e714
Ben Vanik <benvanik@google.com>
2021-01-22 11:44:20 -0800

    Making threads join on shutdown (for sanity). Doesn't seem to change anything but there's not really a reason to detach and I'd rather never have to think about this code again.

--- iree/base/threading_win32.c
+++ iree/base/threading_win32.c
@@ -203,6 +203,7 @@ static void iree_thread_delete(iree_thread_t* thread) {
 
   iree_thread_resume(thread);
 
+  WaitForSingleObject(thread->handle, INFINITE);
   CloseHandle(thread->handle);
   iree_thread_override_list_deinitialize(&thread->qos_override_list);
   iree_allocator_free(thread->allocator, thread);
