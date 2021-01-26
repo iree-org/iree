7deb8e1c7595820fa4c2e18cf9f9d9390836346f
not-jenni <jennik@google.com>
2021-01-25 23:20:01 -0800

    Moves the java bindings to the experimental directory

--- experimental/bindings/java/com/google/iree/native/instance_wrapper.cc
+++ experimental/bindings/java/com/google/iree/native/instance_wrapper.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "bindings/java/com/google/iree/native/instance_wrapper.h"
+#include "experimental/bindings/java/com/google/iree/native/instance_wrapper.h"
 
 #include <mutex>
 
