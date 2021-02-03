be76f5a1569bbb8bd45217674a2cbc17ee434143
Ben Vanik <benvanik@google.com>
2021-02-02 19:33:34 -0800

    Removing ref_ptr.h include where unused.

--- iree/vm/stack_test.cc
+++ iree/vm/stack_test.cc
@@ -17,7 +17,6 @@
 #include <cstring>
 
 #include "iree/base/api.h"
-#include "iree/base/ref_ptr.h"
 #include "iree/testing/gtest.h"
 #include "iree/testing/status_matchers.h"
 #include "iree/vm/ref.h"
