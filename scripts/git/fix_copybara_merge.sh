6830fe5f2aec87273fa76c18997f03c6b7b1a776
Geoffrey Martin-Noble <gcmn@google.com>
2021-02-03 18:21:10 -0800

    More error checking and documentation

--- scripts/git/fix_copybara_merge.sh
+++ scripts/git/fix_copybara_merge.sh
@@ -15,6 +15,7 @@
 
 # Fixes a Copybara push that failed to create a merge commit, using the
 # COPYBARA_TAG label to add a second parent to the HEAD commit.
+# This is mostly intended to be used by automation.
 
 set -e
 
@@ -22,19 +23,38 @@ COPYBARA_TAG="COPYBARA_INTEGRATE_REVIEW"
 UPSTREAM_REMOTE="${UPSTREAM_REMOTE:-upstream}"
 
 if [[ -n "$(git status --porcelain)" ]]; then
-    echo "Working directory not clean. Aborting"
-    git status
-    exit 1
+  echo "Working directory not clean. Aborting"
+  git status
+  exit 1
 fi
 
-# Get the commit message for the head commit
+if [[ -n "$(git rev-list --merges HEAD^..HEAD)" ]]; then
+  echo "HEAD commit is already a merge commit. Aborting"
+  git show HEAD
+  exit 1
+fi
+
+# Get the commit message of the HEAD commit
 MESSAGE="$(git log --format=%B -n 1 HEAD)"
 
 # Extract the commit to merge from using the Copybara tag.
 MERGE_FROM="$(echo "${MESSAGE?}" | awk -v pat="${COPYBARA_TAG?}" '$0~pat{print $NF}')"
 
+if [[ -z "${MERGE_FROM?}" ]]; then
+  echo "HEAD commit was not tagged with ${COPYBARA_TAG?}. Aborting"
+  git show HEAD
+  exit 1
+fi
+
 # And create a new message with the tag removed
 NEW_MESSAGE="$(echo "${MESSAGE?}" | sed "/${COPYBARA_TAG?}/d")"
 
+# Make sure we actually have the commit we need to merge from.
 git fetch "${UPSTREAM_REMOTE?}" "${MERGE_FROM?}"
+
+# Create a new commit object `git commit-tree` based on the tree of the current
+# HEAD commit with the parent of the HEAD commit as first parent and the commit
+# to merge from as the second. Use the new message as the commit message. Reset
+# the current branch to this commit.
+# See https://stackoverflow.com/q/48560351
 git reset --soft "$(git commit-tree -m "${NEW_MESSAGE?}" -p HEAD^ -p ${MERGE_FROM?} HEAD^{tree})"
