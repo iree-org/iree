4340f5d81c4c2066b59d9d7a39a24896ce2f0a66
Lei Zhang <antiagainst@google.com>
2021-02-05 16:43:16 -0500

    [codegen] Enable vectorizing convolution with output height != 1
    The previous restriction is artificial, just to get started.
    Allowing conv with output height != 1 will allow us to support
    more cases.

--- iree/compiler/Conversion/LinalgToVector/VectorizeConv.cpp
+++ iree/compiler/Conversion/LinalgToVector/VectorizeConv.cpp
@@ -43,7 +43,6 @@ namespace {
 /// - Output: NHoWoCo format
 /// - For output:
 ///   - N must be 1.
-///   - Ho must be 1.
 ///   - Co must be a multiple of 4.
 /// - For filter:
 ///   - Hf must be 1.
@@ -84,12 +83,8 @@ struct VectorizeLinalgConv : OpRewritePattern<linalg::ConvOp> {
       return failure();
     }
 
-    // The output batch and height dimensions should be 1. If not, other
-    // patterns can generate parallel loops can distribute them.
-    if (outputViewOp.getStaticSize(0) != 1 ||
-        outputViewOp.getStaticSize(1) != 1) {
-      return failure();
-    }
+    // The output batch dimension should be 1.
+    if (outputViewOp.getStaticSize(0) != 1) return failure();
 
     // We addtionally expect the filter height/width dimensions are both 1 to
     // simplify vectorization. Other patterns can generate loops to create 1x1
@@ -103,13 +98,18 @@ struct VectorizeLinalgConv : OpRewritePattern<linalg::ConvOp> {
     int64_t numOutputChannels = filterViewOp.getStaticSize(3);
     if (numInputChannels != 4 || numOutputChannels % 4 != 0) return failure();
 
+    int64_t numOutputHeights = outputViewOp.getStaticSize(1);
     int64_t numOutputWidths = outputViewOp.getStaticSize(2);
+    int64_t heightStride = convOp.getStride(0);
     int64_t widthStride = convOp.getStride(1);
 
-    // This invocation handles a batch of (numOutputWidths * numOutputChannels).
+    // This invocation handles a batch of
+    // (numOutputHeights * numOutputWidths * numOutputChannels).
     LLVM_DEBUG({
+      llvm::dbgs() << "# output height: " << numOutputHeights << "\n";
       llvm::dbgs() << "# output width: " << numOutputWidths << "\n";
       llvm::dbgs() << "# output channels: " << numOutputChannels << "\n";
+      llvm::dbgs() << "height stride: " << heightStride << "\n";
       llvm::dbgs() << "width stride: " << widthStride << "\n";
     });
 
@@ -120,7 +120,7 @@ struct VectorizeLinalgConv : OpRewritePattern<linalg::ConvOp> {
     auto filterVectorType =
         VectorType::get({numInputChannels, numOutputChannels}, elementType);
     auto vector1x4Type = VectorType::get({1, 4}, elementType);
-    Value zero = rewriter.create<ConstantIndexOp>(loc, 0);
+    Value zero = rewriter.createOrFold<ConstantIndexOp>(loc, 0);
 
     // Load the entire filter subview.
     SmallVector<Value, 4> filterIndices(4, zero);
@@ -158,41 +158,48 @@ struct VectorizeLinalgConv : OpRewritePattern<linalg::ConvOp> {
         {getParallelIteratorTypeName(), getParallelIteratorTypeName(),
          getReductionIteratorTypeName()});
 
-    // Compute the (numOutputWidths * numOutputChannels) batch. We only
-    // contribute numInputChannels accumulation along the reduction dimension.
-    // So read in the result from the output, compose a chain of
-    // numInputChannels vector dot operations, and then write out.
-    for (int ow = 0; ow < numOutputWidths; ++ow) {
-      // Read in the input vector for these 4 input channels a a batch. The
-      // input vector are used for computing all output channels so data can
-      // be reused.
-      SmallVector<Value, 4> inputIndices(4, zero);
-      inputIndices[2] = rewriter.create<ConstantIndexOp>(loc, ow * widthStride);
-      Value inputVector = rewriter.create<vector::TransferReadOp>(
-          loc, vector1x4Type, inputViewOp, inputIndices);
+    // Compute the (numOutputHeights * numOutputWidths * numOutputChannels)
+    // batch. We only contribute numInputChannels accumulation along the
+    // reduction dimension. So read in the result from the output, compose a
+    // chain of numInputChannels vector dot operations, and then write out.
+    for (int oh = 0; oh < numOutputHeights; ++oh) {
+      for (int ow = 0; ow < numOutputWidths; ++ow) {
+        // Read in the input vector for these 4 input channels a a batch. The
+        // input vector are used for computing all output channels so data can
+        // be reused.
+        SmallVector<Value, 4> inputIndices(4, zero);
+        inputIndices[1] =
+            rewriter.createOrFold<ConstantIndexOp>(loc, oh * heightStride);
+        inputIndices[2] =
+            rewriter.createOrFold<ConstantIndexOp>(loc, ow * widthStride);
+        Value inputVector = rewriter.create<vector::TransferReadOp>(
+            loc, vector1x4Type, inputViewOp, inputIndices);
 
-      for (int oc = 0; oc < numOutputChannels / 4; ++oc) {
-        // Read in the initial value for this output vector.
-        SmallVector<Value, 4> outputIndices(4, zero);
-        outputIndices[2] = rewriter.create<ConstantIndexOp>(loc, ow);
-        outputIndices[3] = rewriter.create<ConstantIndexOp>(loc, oc * 4);
-        Value outputVector = rewriter.create<vector::TransferReadOp>(
-            loc, vector1x4Type, outputViewOp, outputIndices);
+        for (int oc = 0; oc < numOutputChannels / 4; ++oc) {
+          // Read in the initial value for this output vector.
+          SmallVector<Value, 4> outputIndices(4, zero);
+          outputIndices[1] = rewriter.createOrFold<ConstantIndexOp>(loc, oh);
+          outputIndices[2] = rewriter.createOrFold<ConstantIndexOp>(loc, ow);
+          outputIndices[3] =
+              rewriter.createOrFold<ConstantIndexOp>(loc, oc * 4);
+          Value outputVector = rewriter.create<vector::TransferReadOp>(
+              loc, vector1x4Type, outputViewOp, outputIndices);
 
-        // Peform a chain of dot product and accumulation.
-        for (int i = 0; i < numInputChannels; ++i) {
-          auto inputSlice = rewriter.create<vector::ExtractStridedSliceOp>(
-              loc, inputVector, /*offsets=*/ArrayRef<int64_t>({0, i}),
-              /*sizes=*/ArrayRef<int64_t>({1, 1}),
-              /*strides=*/ArrayRef<int64_t>({1, 1}));
-          outputVector = rewriter.create<vector::ContractionOp>(
-              loc, inputSlice, filterVectors[i][oc], outputVector, indexingMaps,
-              iterators);
+          // Peform a chain of dot product and accumulation.
+          for (int i = 0; i < numInputChannels; ++i) {
+            auto inputSlice = rewriter.create<vector::ExtractStridedSliceOp>(
+                loc, inputVector, /*offsets=*/ArrayRef<int64_t>({0, i}),
+                /*sizes=*/ArrayRef<int64_t>({1, 1}),
+                /*strides=*/ArrayRef<int64_t>({1, 1}));
+            outputVector = rewriter.create<vector::ContractionOp>(
+                loc, inputSlice, filterVectors[i][oc], outputVector,
+                indexingMaps, iterators);
+          }
+
+          // Write out the output vector.
+          rewriter.create<vector::TransferWriteOp>(loc, outputVector,
+                                                   outputViewOp, outputIndices);
         }
-
-        // Write out the output vector.
-        rewriter.create<vector::TransferWriteOp>(loc, outputVector,
-                                                 outputViewOp, outputIndices);
       }
     }
 
