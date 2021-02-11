iree-org/iree

commit e775b548d5434859889de04719b645645abb30ee

Added address sanitization feature for translating ML models to LLVM modules (#4676)
1. Added an option, -iree-llvm-address-sanitizer. Users can turn it on and it injects ASAN to binary of the module.
2. If ASAN is used on compilation, it changes an ubyte to a enum value in flat buffer to mark it is compiled with ASAN enabled. IREE runtime can read the ubyte before running the module. And, it will make an error message if runtime does not have ASAN feature when running address sanitized module.

iree/hal/local/loaders/legacy_library_loader.cc
@@ -17,6 +17,7 @@
 #include "iree/base/dynamic_library.h"
 #include "iree/base/internal/file_io.h"
 #include "iree/base/internal/file_path.h"
+#include "iree/base/target_platform.h"
 #include "iree/base/tracing.h"
 #include "iree/hal/local/local_executable.h"
 
@@ -79,6 +80,20 @@ static iree_status_t iree_hal_dylib_executable_flatbuffer_verify(
           iree_DyLibExecutableDef_library_embedded_get(executable_def))) {
     return iree_make_status(IREE_STATUS_INVALID_ARGUMENT,
                             "executable library_embedded is missing/empty");
+  }
+
+  switch (iree_DyLibExecutableDef_sanitized_kind_get(executable_def)) {
+    case iree_SanitizerKind_None:
+      break;
+    case iree_SanitizerKind_Address: {
+      if (!IREE_SANITIZER_ADDRESS) {
+        return iree_make_status(
+            IREE_STATUS_UNAVAILABLE,
+            "Dynamic library executable is compiled with ASAN support, but "
+            "this host application failed to enable ASAN to load this "
+            "executable");
+      }
+    } break;
   }
 
   return iree_ok_status();

iree/compiler/Dialect/HAL/Target/LLVM/LLVMAOTTarget.cpp
@@ -230,6 +230,15 @@ class LLVMAOTTargetBackend final : public TargetBackend {
                                      "dialect to the native llvm::Module";
     }
 
+    switch (options_.sanitizerKind) {
+      case SanitizerKind::kNone:
+        break;
+      case SanitizerKind::kAddress: {
+        for (auto &function : llvmModule->getFunctionList())
+          function.addFnAttr(llvm::Attribute::SanitizeAddress);
+      } break;
+    }
+
     // Try to grab a linker tool based on the options (and target environment).
     auto linkerTool = LinkerTool::getForTarget(targetTriple, options_);
     if (!linkerTool) {
@@ -340,6 +349,8 @@ class LLVMAOTTargetBackend final : public TargetBackend {
         builder, debugDatabaseFilenameRef);
     iree_DyLibExecutableDef_debug_database_embedded_add(builder,
                                                         debugDatabaseRef);
+    iree_DyLibExecutableDef_sanitized_kind_add(
+        builder, static_cast<unsigned char>(options_.sanitizerKind));
     iree_DyLibExecutableDef_end_as_root(builder);
 
     // Add the binary data to the target executable.

iree/compiler/Dialect/HAL/Target/LLVM/LLVMIRPasses.cpp
@@ -27,6 +27,7 @@
 #include "llvm/Support/Host.h"
 #include "llvm/Support/TargetRegistry.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/Instrumentation/AddressSanitizer.h"
 
 namespace mlir {
 namespace iree_compiler {
@@ -87,6 +88,31 @@ LogicalResult runLLVMIRPasses(const LLVMTargetOptions &options,
   passBuilder.registerLoopAnalyses(loopAnalysisManager);
   passBuilder.crossRegisterProxies(loopAnalysisManager, functionAnalysisManager,
                                    cGSCCAnalysisManager, moduleAnalysisManager);
+
+  switch (options.sanitizerKind) {
+    case SanitizerKind::kNone:
+      break;
+    case SanitizerKind::kAddress: {
+      passBuilder.registerOptimizerLastEPCallback(
+          [](llvm::ModulePassManager &modulePassManager,
+             llvm::PassBuilder::OptimizationLevel Level) {
+            bool compileKernel = false;
+            bool recover = false;
+            bool useAfterScope = true;
+            bool moduleUseAfterScope = false;
+            bool useOdrIndicator = false;
+            modulePassManager.addPass(
+                llvm::RequireAnalysisPass<llvm::ASanGlobalsMetadataAnalysis,
+                                          llvm::Module>());
+            modulePassManager.addPass(llvm::ModuleAddressSanitizerPass(
+                compileKernel, recover, moduleUseAfterScope, useOdrIndicator));
+            modulePassManager.addPass(
+                createModuleToFunctionPassAdaptor(llvm::AddressSanitizerPass(
+                    compileKernel, recover, useAfterScope)));
+          });
+    } break;
+  }
+
   if (options.optLevel != llvm::PassBuilder::OptimizationLevel::O0) {
     llvm::ModulePassManager modulePassManager;
     modulePassManager =

iree/compiler/Dialect/HAL/Target/LLVM/LLVMTargetOptions.cpp
@@ -82,6 +82,13 @@ LLVMTargetOptions getLLVMTargetOptionsFromFlags() {
     llvmTargetOptions.targetCPUFeatures = clTargetCPUFeatures;
   }
 
+  static llvm::cl::opt<SanitizerKind> clSanitizerKind(
+      "iree-llvm-sanitize", llvm::cl::desc("Apply LLVM sanitize feature"),
+      llvm::cl::init(SanitizerKind::kNone),
+      llvm::cl::values(clEnumValN(SanitizerKind::kAddress, "address",
+                                  "Address sanitizer support")));
+  llvmTargetOptions.sanitizerKind = clSanitizerKind;
+
   static llvm::cl::opt<std::string> clTargetABI(
       "iree-llvm-target-abi",
       llvm::cl::desc("LLVM target machine ABI; specify for -mabi"),

iree/compiler/Dialect/HAL/Target/LLVM/LLVMTargetOptions.h
@@ -23,6 +23,13 @@ namespace iree_compiler {
 namespace IREE {
 namespace HAL {
 
+// Defines kinds of Sanitizer
+// The order in enum class should be same as one in flat buffer schema
+enum class SanitizerKind {
+  kNone = 0,
+  kAddress,
+};
+
 struct LLVMTargetOptions {
   // Target machine configuration.
   std::string targetTriple;
@@ -38,6 +45,9 @@ struct LLVMTargetOptions {
   // information is valid) it may significantly change the output program
   // and benchmarking
   bool debugSymbols = true;
+
+  // Sanitizer Kind for CPU Kernels
+  SanitizerKind sanitizerKind = SanitizerKind::kNone;
 
   // Link any required runtime libraries into the produced binaries statically.
   // This increases resulting binary size but enables the binaries to be used on

iree/schemas/dylib_executable_def.fbs
@@ -14,6 +14,13 @@
 
 namespace iree;
 
+// Define kinds of sanitizers (the order in enum should be same as enum in LLVM
+// TargetOptions)
+enum SanitizerKind : ubyte {
+  None = 0,
+  Address
+}
+
 // 'Dynamic Library (dylib) Executable'.
 
 file_identifier "DLIB";
@@ -21,7 +28,8 @@ file_extension "dlib";
 
 // Dynamic library (.so/.dll/.dylib) executable module.
 table DyLibExecutableDef {
-  // A map of entry points to string names with the same order as in the executable op.
+  // A map of entry points to string names with the same order as in the
+  // executable op.
   entry_points:[string];
   // An embedded (as opposed to external) dynamic library file.
   // TODO(scotttodd): List of embedded files?
@@ -30,6 +38,7 @@ table DyLibExecutableDef {
 
   debug_database_filename:string;
   debug_database_embedded:[ubyte];
+  sanitized_kind:SanitizerKind = None;
 
   // TODO(scotttodd): Relative file path from this flatbuffer file
 }

commit 295424b3d2c8b8f38da91ba1039a33caa2bb8c52

Fixing build breakage from #4676.

iree/compiler/Dialect/HAL/Target/LLVM/BUILD
@@ -74,6 +74,7 @@ cc_library(
         ":LLVMTargetOptions",
         "@llvm-project//llvm:Analysis",
         "@llvm-project//llvm:Core",
+        "@llvm-project//llvm:Instrumentation",
         "@llvm-project//llvm:Passes",
         "@llvm-project//llvm:Support",
         "@llvm-project//llvm:Target",

iree/compiler/Dialect/HAL/Target/LLVM/CMakeLists.txt
@@ -62,6 +62,7 @@ iree_cc_library(
     ::LLVMTargetOptions
     LLVMAnalysis
     LLVMCore
+    LLVMInstrumentation
     LLVMPasses
     LLVMSupport
     LLVMTarget

iree/hal/local/loaders/BUILD
@@ -32,6 +32,7 @@ cc_library(
     ],
     deps = [
         "//iree/base:api",
+        "//iree/base:core_headers",
         "//iree/base:dynamic_library",
         "//iree/base:flatcc",
         "//iree/base:tracing",

iree/hal/local/loaders/CMakeLists.txt
@@ -23,6 +23,7 @@ iree_cc_library(
     "legacy_library_loader.cc"
   DEPS
     iree::base::api
+    iree::base::core_headers
     iree::base::dynamic_library
     iree::base::flatcc
     iree::base::internal::file_io

iree/hal/local/loaders/legacy_library_loader.cc
@@ -85,15 +85,20 @@ static iree_status_t iree_hal_dylib_executable_flatbuffer_verify(
   switch (iree_DyLibExecutableDef_sanitized_kind_get(executable_def)) {
     case iree_SanitizerKind_None:
       break;
-    case iree_SanitizerKind_Address: {
-      if (!IREE_SANITIZER_ADDRESS) {
-        return iree_make_status(
-            IREE_STATUS_UNAVAILABLE,
-            "Dynamic library executable is compiled with ASAN support, but "
-            "this host application failed to enable ASAN to load this "
-            "executable");
-      }
-    } break;
+#if !defined(IREE_SANITIZER_ADDRESS)
+    case iree_SanitizerKind_Address:
+      return iree_make_status(
+          IREE_STATUS_UNAVAILABLE,
+          "Executable library is compiled with ASAN support but the host "
+          "runtime is not compiled with it enabled; add -fsanitize=address to "
+          "the runtime compilation options");
+#endif  // !IREE_SANITIZER_ADDRESS
+    default:
+      return iree_make_status(
+          IREE_STATUS_UNAVAILABLE,
+          "Executable library requires a sanitizer the host runtime is not "
+          "compiled to enable/understand: %u",
+          (uint32_t)iree_DyLibExecutableDef_sanitized_kind_get(executable_def));
   }
 
   return iree_ok_status();