3d96927546f95ebca337a04f0a44ab8c5c9d2fba
Geoffrey Martin-Noble <gcmn@google.com>
2021-03-12 19:07:37 -0800

    Add documentation

--- integrations/tensorflow/e2e/iree_e2e_cartesian_product_test_suite.cmake
+++ integrations/tensorflow/e2e/iree_e2e_cartesian_product_test_suite.cmake
@@ -1,3 +1,15 @@
+
+# iree_py_test()
+#
+# Creates a test that executes a specified Python source file. The IREE Python
+# bindings are added to PYTHONPATH.
+#
+# Parameters:
+#   NAME: Name of the target
+#   SRC: Python source file to be executed.
+#   ARGS: Command line arguments to the Python source file.
+#   LABELS: Additional labels to apply to the test. The package path is added
+#       automatically.
 function(iree_py_test)
   cmake_parse_arguments(
     PARSE_ARGV 0
@@ -29,6 +41,81 @@ function(iree_py_test)
     "PYTHONPATH=${CMAKE_BINARY_DIR}/bindings/python")
 endfunction()
 
+# iree_e2e_cartesian_product_test_suite()
+#
+# Expands a testing matrix to create python test targets.
+#
+# Mirrors the bzl rule of the same name.
+#
+# This rule uses CMake in a way that it was never meant to be used. In
+# particular it uses custom hackery to enable nested data structures. The
+# original Starlark rule was written without the expectation that it would ever
+# need to be ported to CMake and so has some complex matrix expansion logic.
+# The author apologizes for what you are about to read.
+#
+# Parameters:
+#   NAME: Base name of the test targets.
+#   MATRIX_KEYS: Keys for the different dimensions of the matrix to be expanded.
+#       One of the keys must be `src` and is expanded to the source files for
+#       each test. One key must be `target_backends` and is expanded to the
+#       target backends for the IREE integration test. It is also used to derive
+#       a driver label to attach to the generated tests. All other keys are
+#       assumed to correspond to flag names that should be passed to the
+#       generated tests (with the corresponding values from MATRIX_VALUES).
+#   MATRIX_VALUES: Lists of values for each key of the matrix. Each element of
+#       this argument is interpreted as a list and the order of arguments must
+#       match MATRIX_KEYS.
+#   FAILING_CONFIGURATIONS: Configurations in the matrix expansion that are
+#       expected to fail. Each element of this argument is a *comma*-separated
+#       list of matrix values in the same order as MATRIX_KEYS (and having the
+#       same number of elements. An empty element is interpreted to correspond
+#       to all values for that key in the matrix.
+#   LABELS: Additional labels to apply to the test. The package path and
+#       "driver=${DRIVER}" based on the target_backends matrix key are added
+#       automatically.
+#
+#
+# Example:
+#   iree_e2e_cartesian_product_test_suite(
+#     NAME
+#       my_tests
+#     MATRIX_KEYS
+#       "src"
+#       "target_backend"
+#       "reference_backend"
+#       "magic_flag"
+#     MATRIX_VALUES
+#       "concat_test.py;range_test.py"
+#       "tf;tflite;iree_vmla;iree_llvmaot;iree_vulkan"
+#       "tf"
+#       "true;false"
+#    FAILING_CONFIGURATIONS
+#      "concat_test.py,,,true"
+#      "range_test.py,iree_vulkan,,"
+#   )
+#
+# Would expand to the following tests:
+#   DISABLED: python concat_test.py --target_backend=tf           --reference_backend=tf --magic_flag=true
+#   DISABLED: python concat_test.py --target_backend=tflite       --reference_backend=tf --magic_flag=true
+#   DISABLED: python concat_test.py --target_backend=iree_vmla    --reference_backend=tf --magic_flag=true
+#   DISABLED: python concat_test.py --target_backend=iree_llvmaot --reference_backend=tf --magic_flag=true
+#   DISABLED: python concat_test.py --target_backend=iree_vulkan  --reference_backend=tf --magic_flag=true
+#             python range_test.py  --target_backend=tf           --reference_backend=tf --magic_flag=true
+#             python range_test.py  --target_backend=tflite       --reference_backend=tf --magic_flag=true
+#             python range_test.py  --target_backend=iree_vmla    --reference_backend=tf --magic_flag=true
+#             python range_test.py  --target_backend=iree_llvmaot --reference_backend=tf --magic_flag=true
+#   DISABLED: python range_test.py  --target_backend=iree_vulkan  --reference_backend=tf --magic_flag=true
+#             python concat_test.py --target_backend=tf           --reference_backend=tf --magic_flag=false
+#             python concat_test.py --target_backend=tflite       --reference_backend=tf --magic_flag=false
+#             python concat_test.py --target_backend=iree_vmla    --reference_backend=tf --magic_flag=false
+#             python concat_test.py --target_backend=iree_llvmaot --reference_backend=tf --magic_flag=false
+#             python concat_test.py --target_backend=iree_vulkan  --reference_backend=tf --magic_flag=false
+#             python range_test.py  --target_backend=tf           --reference_backend=tf --magic_flag=false
+#             python range_test.py  --target_backend=tflite       --reference_backend=tf --magic_flag=false
+#             python range_test.py  --target_backend=iree_vmla    --reference_backend=tf --magic_flag=false
+#             python range_test.py  --target_backend=iree_llvmaot --reference_backend=tf --magic_flag=false
+#   DISABLED: python range_test.py  --target_backend=iree_vulkan  --reference_backend=tf --magic_flag=false
+#
 function(iree_e2e_cartesian_product_test_suite)
   if(NOT IREE_BUILD_TESTS)
     return()
