iree-org/iree

commit ce0285fe5996cf679bb667f5320869e55ddf1866

Continue pruning abseil usage: switch from absl::InlinedVector to std::vector. (#5302)
Also switched to using `IREE_ATTRIBUTE_NOINLINE` from `ABSL_ATTRIBUTE_NOINLINE`.

Generated via a pile of regex (code, includes, build deps) and manual cleanup until builds passed again.

* native modules needed some extra changes to work around calling `iree_hal_buffer_view_shape` with rank 0

iree/hal/vulkan/vulkan_device.cc
@@ -16,8 +16,8 @@
 
 #include <functional>
 #include <utility>
+#include <vector>
 
-#include "absl/container/inlined_vector.h"
 #include "absl/strings/str_cat.h"
 #include "iree/base/internal/math.h"
 #include "iree/base/status.h"
@@ -730,9 +730,9 @@ iree_status_t iree_hal_vulkan_device_create(
   // are of the same queue family as the dispatch queues: Vulkan requires that
   // all queues created from the same family are done in the same
   // VkDeviceQueueCreateInfo struct.
-  absl::InlinedVector<VkDeviceQueueCreateInfo, 2> queue_create_info;
-  absl::InlinedVector<float, 4> dispatch_queue_priorities;
-  absl::InlinedVector<float, 4> transfer_queue_priorities;
+  std::vector<VkDeviceQueueCreateInfo> queue_create_info;
+  std::vector<float> dispatch_queue_priorities;
+  std::vector<float> transfer_queue_priorities;
   queue_create_info.push_back({});
   auto& dispatch_queue_info = queue_create_info.back();
   dispatch_queue_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;

iree/base/BUILD
@@ -97,7 +97,6 @@ cc_library(
     srcs = ["signature_parser.cc"],
     hdrs = ["signature_parser.h"],
     deps = [
-        "@com_google_absl//absl/container:inlined_vector",
         "@com_google_absl//absl/strings",
         "@com_google_absl//absl/types:optional",
         "@com_google_absl//absl/types:span",

iree/base/CMakeLists.txt
@@ -93,7 +93,6 @@ iree_cc_library(
   SRCS
     "signature_parser.cc"
   DEPS
-    absl::inlined_vector
     absl::optional
     absl::span
     absl::strings

iree/base/signature_parser.h
@@ -19,8 +19,8 @@
 #include <cassert>
 #include <map>
 #include <string>
+#include <vector>
 
-#include "absl/container/inlined_vector.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/optional.h"
 #include "absl/types/span.h"
@@ -112,7 +112,7 @@ class SignatureParser {
 // Parses function signatures generated by RawSignatureMangler.
 class RawSignatureParser {
  public:
-  using DimVector = absl::InlinedVector<int, 4>;
+  using DimVector = std::vector<int>;
 
   enum class Type {
     kBuffer = 0,

iree/hal/BUILD
@@ -78,7 +78,6 @@ cc_library(
         "//iree/base:threading",
         "//iree/base:tracing",
         "//iree/base/internal",
-        "@com_google_absl//absl/container:inlined_vector",
         "@com_google_absl//absl/strings",
         "@com_google_absl//absl/types:span",
         "@half//:includes",
@@ -94,7 +93,7 @@ cc_test(
         "//iree/base:status",
         "//iree/testing:gtest",
         "//iree/testing:gtest_main",
-        "@com_google_absl//absl/container:inlined_vector",
         "@com_google_absl//absl/strings",
+        "@com_google_absl//absl/types:span",
     ],
 )

iree/hal/CMakeLists.txt
@@ -53,7 +53,6 @@ iree_cc_library(
     "string_util.cc"
     "string_util.h"
   DEPS
-    absl::inlined_vector
     absl::span
     absl::strings
     half::includes
@@ -73,7 +72,7 @@ iree_cc_test(
     "string_util_test.cc"
   DEPS
     ::api
-    absl::inlined_vector
+    absl::span
     absl::strings
     iree::base::core_headers
     iree::base::status

iree/hal/buffer_view.cc
@@ -17,8 +17,8 @@
 #include <cctype>
 #include <cinttypes>
 #include <cstdio>
+#include <vector>
 
-#include "absl/container/inlined_vector.h"
 #include "absl/strings/ascii.h"
 #include "absl/strings/match.h"
 #include "absl/strings/numbers.h"
@@ -75,7 +75,7 @@ IREE_API_EXPORT iree_status_t IREE_API_CALL iree_hal_buffer_view_parse(
   }
 
   // AxBxC...
-  absl::InlinedVector<iree_hal_dim_t, 6> shape(6);
+  std::vector<iree_hal_dim_t> shape(6);
   iree_host_size_t shape_rank = 0;
   iree_status_t shape_result =
       iree_hal_parse_shape({shape_str.data(), shape_str.length()}, shape.size(),

iree/hal/string_util.cc
@@ -17,8 +17,8 @@
 #include <cctype>
 #include <cinttypes>
 #include <cstdio>
+#include <vector>
 
-#include "absl/container/inlined_vector.h"
 #include "absl/strings/ascii.h"
 #include "absl/strings/match.h"
 #include "absl/strings/numbers.h"
@@ -44,7 +44,7 @@ IREE_API_EXPORT iree_status_t IREE_API_CALL iree_hal_parse_shape(
     return iree_ok_status();  // empty shape
   }
 
-  absl::InlinedVector<iree_hal_dim_t, 6> dims;
+  std::vector<iree_hal_dim_t> dims;
   for (auto dim_str : absl::StrSplit(str_value, 'x')) {
     int dim_value = 0;
     if (!absl::SimpleAtoi(dim_str, &dim_value)) {

commit 55d3715a896634f34f61be33e5fe389b54d9d10b

Fix vulkan_device struct manipulation issue with std::vector. (#5333)
More fallout from https://github.com/google/iree/pull/5302 :/

Actually caught by validation layer warnings about `pQueuePriorities` being null when it must not be.

In debug builds, this incorrect use of references sometimes allocated crazy amounts of memory by using uninitialized memory as the size to resize to... ouch.

iree/hal/vulkan/vulkan_device.cc
@@ -731,6 +731,9 @@ iree_status_t iree_hal_vulkan_device_create(
   // all queues created from the same family are done in the same
   // VkDeviceQueueCreateInfo struct.
   std::vector<VkDeviceQueueCreateInfo> queue_create_info;
+  // Reserve space for create infos. Note: must be the maximum used, or else
+  // references used below will be invalidated as the vector grows.
+  queue_create_info.reserve(2);
   std::vector<float> dispatch_queue_priorities;
   std::vector<float> transfer_queue_priorities;
   queue_create_info.push_back({});