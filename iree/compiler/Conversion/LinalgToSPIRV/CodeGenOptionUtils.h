23492ecb00a1aed2778a9dc45092411ae1c96dc6
Lei Zhang <antiagainst@google.com>
2021-04-23 10:06:28 -0400

    Delete unused vectorizeMemref option

--- iree/compiler/Conversion/LinalgToSPIRV/CodeGenOptionUtils.h
+++ iree/compiler/Conversion/LinalgToSPIRV/CodeGenOptionUtils.h
@@ -36,9 +36,9 @@ struct SPIRVCodegenOptions {
   llvm::SmallVector<unsigned, 3> workgroupSize = {};
   llvm::SmallVector<unsigned, 3> workgroupTileSizes = {};
   llvm::SmallVector<unsigned, 3> invocationTileSizes = {};
+
   bool enableVectorization = false;
   bool useWorkgroupMemory = false;
-  bool vectorizeMemref = false;
   bool usingLinalgOnTensors = false;
 };
 
