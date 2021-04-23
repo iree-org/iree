b6406ec1786ad40178d5f9e54e5d2b540f888538
Lei Zhang <antiagainst@google.com>
2021-04-23 09:25:07 -0400

    NFC: move ForOpCanonicalizationPass from CodegenUtils/ to Common/
    Common/ is the directory for hosting various passes applicable to
    multiple backends.

--- iree/compiler/Conversion/LinalgToSPIRV/Passes.cpp
+++ iree/compiler/Conversion/LinalgToSPIRV/Passes.cpp
@@ -20,7 +20,6 @@
 
 #include "iree/compiler/Conversion/LinalgToSPIRV/Passes.h"
 
-#include "iree/compiler/Conversion/CodegenUtils/ForOpCanonicalization.h"
 #include "iree/compiler/Conversion/Common/Passes.h"
 #include "iree/compiler/Conversion/HLOToHLO/Passes.h"
 #include "iree/compiler/Conversion/HLOToLinalg/Passes.h"
