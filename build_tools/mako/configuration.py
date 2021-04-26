a68e75746f1b20ea477c944b53561ce99ba874bd
Han-Chung Wang <hanchung@google.com>
2021-04-27 01:24:50 +0800

    Add CPU multi-threaded benchmarks to Mako benchmark targets. (#5613)
    Fixes https://github.com/google/iree/issues/4752
    
    Also makes flag style consistent.

--- build_tools/mako/configuration.py
+++ build_tools/mako/configuration.py
@@ -110,8 +110,20 @@ def get_pixel4_default_target_list(skipped_target=None, batch_config=None):
                  mako_tag="cpu",
                  compilation_flags=[
                      "--iree-llvm-target-triple=aarch64-none-linux-android29",
-                     "-iree-flow-inline-constants-max-byte-length=2048",
-                     "-iree-flow-dispatch-formation-enable-operand-fusion"
+                     "--iree-flow-inline-constants-max-byte-length=2048",
+                     "--iree-flow-dispatch-formation-enable-operand-fusion"
+                 ]),
+      TargetInfo(driver="dylib",
+                 hal_target_backend="dylib-llvm-aot",
+                 taskset="f0",
+                 mako_tag="cpu3t",
+                 compilation_flags=[
+                     "--iree-llvm-target-triple=aarch64-none-linux-android29",
+                     "--iree-flow-inline-constants-max-byte-length=2048",
+                     "--iree-flow-dispatch-formation-enable-operand-fusion"
+                 ],
+                 runtime_flags=[
+                     "--dylib_worker_count=3",
                  ]),
       TargetInfo(
           driver="vulkan",
@@ -120,8 +132,8 @@ def get_pixel4_default_target_list(skipped_target=None, batch_config=None):
           mako_tag="vlk",
           compilation_flags=[
               "--iree-vulkan-target-triple=qualcomm-adreno640-unknown-android10",
-              "-iree-flow-inline-constants-max-byte-length=2048",
-              "-iree-flow-dispatch-formation-enable-operand-fusion"
+              "--iree-flow-inline-constants-max-byte-length=2048",
+              "--iree-flow-dispatch-formation-enable-operand-fusion"
           ])
   ]
   targets = [elem for elem in targets if elem.mako_tag not in skipped_target]
@@ -147,8 +159,20 @@ def get_s20_default_target_list(skipped_target=None, batch_config=None):
                  mako_tag="cpu",
                  compilation_flags=[
                      "--iree-llvm-target-triple=aarch64-none-linux-android29",
-                     "-iree-flow-inline-constants-max-byte-length=2048",
-                     "-iree-flow-dispatch-formation-enable-operand-fusion"
+                     "--iree-flow-inline-constants-max-byte-length=2048",
+                     "--iree-flow-dispatch-formation-enable-operand-fusion"
+                 ]),
+      TargetInfo(driver="dylib",
+                 hal_target_backend="dylib-llvm-aot",
+                 taskset="f0",
+                 mako_tag="cpu3t",
+                 compilation_flags=[
+                     "--iree-llvm-target-triple=aarch64-none-linux-android29",
+                     "--iree-flow-inline-constants-max-byte-length=2048",
+                     "--iree-flow-dispatch-formation-enable-operand-fusion"
+                 ],
+                 runtime_flags=[
+                     "--dylib_worker_count=3",
                  ]),
       TargetInfo(
           driver="vulkan",
@@ -158,8 +182,8 @@ def get_s20_default_target_list(skipped_target=None, batch_config=None):
           compilation_flags=[
               "--iree-vulkan-target-triple=valhall-g77-unknown-android10",
               # TODO(GH-5330): Revisit the number or delete the flag.
-              "-iree-flow-inline-constants-max-byte-length=16",
-              "-iree-flow-dispatch-formation-enable-operand-fusion"
+              "--iree-flow-inline-constants-max-byte-length=16",
+              "--iree-flow-dispatch-formation-enable-operand-fusion"
           ])
   ]
   targets = [elem for elem in targets if elem.mako_tag not in skipped_target]
