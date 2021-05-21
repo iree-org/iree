6df555644efe0233138e3009212ce0fc84d25cf0
Han-Chung Wang <hanchung@google.com>
2021-05-22 05:34:02 +0800

    Revert "Handle removing unused results of a CloseureInterfaceOp (#5969)" (#5983)
    This reverts commit a869406d5198d5ddf8320474fbb2ff8b5df22a62.

--- iree/compiler/Dialect/Flow/IR/FlowOps.cpp
+++ iree/compiler/Dialect/Flow/IR/FlowOps.cpp
@@ -811,26 +811,6 @@ bool DispatchWorkgroupsOp::canClosureContainOp(Operation *op) {
   return canDispatchRegionContainOp(op);
 }
 
-bool DispatchWorkgroupsOp::isOutputReadWithinRegion(unsigned resultIndex) {
-  BlockArgument arg =
-      body().front().getArgument(operands().size() + resultIndex);
-  // If argument is of `writeonly` access, then it is not read by construction.
-  if (arg.getType().cast<DispatchTensorType>().getAccess() ==
-      TensorAccess::WriteOnly) {
-    return false;
-  }
-  // If the argument is a result with `readwrite` access, return false if the
-  // value is only written to. Check this by looking at the uses of the argument
-  // being only the `target` of `flow.dispatch.tensor.store` ops.
-  for (OpOperand &uses : arg.getUses()) {
-    auto storeOp = dyn_cast<DispatchTensorStoreOp>(uses.getOwner());
-    if (!(storeOp && storeOp.target() == uses.get())) {
-      return true;
-    }
-  }
-  return false;
-}
-
 ClosureOpInterface
 DispatchWorkgroupsOp::cloneReplacementExcludingOperandsAndResults(
     ArrayRef<unsigned> excludedOperandIndices,
@@ -865,31 +845,18 @@ DispatchWorkgroupsOp::cloneReplacementExcludingOperandsAndResults(
   excludeTiedOperandAndResultIndices(
       excludedOperandIndices, excludedResultIndices, newTiedOperandIndices);
 
-  OpBuilder builder(getContext());
-  auto newOp = builder.create<DispatchWorkgroupsOp>(
-      getLoc(), workgroup_count(), newResultTypes, newResultDims,
-      newOperandsValues, newOperandDims, newTiedOperandIndices,
-      getOperation()->getAttrs());
+  auto newOp = OpBuilder(getContext())
+                   .create<DispatchWorkgroupsOp>(
+                       getLoc(), workgroup_count(), newResultTypes,
+                       newResultDims, newOperandsValues, newOperandDims,
+                       newTiedOperandIndices, getOperation()->getAttrs());
   auto &newBody = newOp.getClosureBodyRegion();
   newBody.takeBody(getClosureBodyRegion());
-  unsigned baseResultIndex = newOperandsValues.size();
-  // For dropped results, erase all the store-op uses. It is a pre-requisite
-  // that the result can be dropped only if it is written within the dispatch
-  // region op.
-  auto erasedArguments = llvm::to_vector<4>(excludedOperandIndices);
-  for (unsigned i = baseResultIndex, e = newBody.getNumArguments(); i != e;
-       ++i) {
-    if (!is_contained(excludedResultIndices, i - baseResultIndex)) continue;
-    auto arg = newBody.front().getArgument(i);
-    for (OpOperand &user : llvm::make_early_inc_range(arg.getUses())) {
-      auto storeOp = dyn_cast<DispatchTensorStoreOp>(user.getOwner());
-      if (storeOp && storeOp.target() == user.get()) {
-        storeOp->erase();
-      }
-    }
-    erasedArguments.push_back(i);
-  }
-  newBody.front().eraseArguments(erasedArguments);
+  newBody.front().eraseArguments(excludedOperandIndices);
+  unsigned baseResultIndex = newBody.front().getNumArguments();
+  newBody.front().eraseArguments(llvm::to_vector<4>(llvm::map_range(
+      excludedResultIndices,
+      [&](unsigned index) { return baseResultIndex + index; })));
   return newOp;
 }
 
@@ -1398,10 +1365,6 @@ bool ExStreamFragmentOp::canClosureContainOp(Operation *op) {
         SymbolTable::lookupNearestSymbolFrom<VariableOp>(op, loadOp.variable());
     return variableOp.is_mutable() == false;
   }
-  return false;
-}
-
-bool ExStreamFragmentOp::isOutputReadWithinRegion(unsigned resultIndex) {
   return false;
 }
 
