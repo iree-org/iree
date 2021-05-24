ac77c20134aba889df417a04c4fb9854f5eba965
raikonenfnu <68087699+raikonenfnu@users.noreply.github.com>
2021-05-24 10:59:28 -0700

    Assemble, Linking BC, LLD Link in ROCm (#5962)
    -Generate object + Link with bitcode
    -ld.lld link object to generate code object
    -Added Target Chip and Link Bitcode Option for ROCm

--- iree/compiler/Dialect/HAL/Target/ROCM/ROCMTarget.h
+++ iree/compiler/Dialect/HAL/Target/ROCM/ROCMTarget.h
@@ -16,19 +16,31 @@
 #define IREE_COMPILER_DIALECT_HAL_TARGET_ROCM_ROCMTARGET_H_
 
 #include "iree/compiler/Dialect/HAL/Target/TargetBackend.h"
+#include "llvm/IR/Module.h"
 
 namespace mlir {
 namespace iree_compiler {
 namespace IREE {
 namespace HAL {
 
-struct ROCMTargetOptions {};
+struct ROCMTargetOptions {
+  // ROCm target Chip
+  std::string ROCMTargetChip;
+  // Whether to try Linking to AMD Bitcodes
+  bool ROCMLinkBC;
+};
 
 ROCMTargetOptions getROCMTargetOptionsFromFlags();
 
 // Registers the ROCM backend.
 void registerROCMTargetBackends(
     std::function<ROCMTargetOptions()> queryOptions);
+
+// Links LLVM module to ROC Device Library Bit Code
+void LinkROCDLIfNecessary(llvm::Module *module);
+
+// Compiles ISAToHsaco Code
+std::string createHsaco(const std::string isa, StringRef name);
 
 }  // namespace HAL
 }  // namespace IREE
