b98bf5d5152040d84ed5da9c1f04702dbb40b821
Geoffrey Martin-Noble <gcmn@google.com>
2021-05-25 14:33:05 -0700

    Fix license typos

--- iree/hal/cuda/cuda_allocator.c
+++ iree/hal/cuda/cuda_allocator.c
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations ufnder the License.
+// limitations under the License.
 
 #include "iree/hal/cuda/cuda_allocator.h"
 
