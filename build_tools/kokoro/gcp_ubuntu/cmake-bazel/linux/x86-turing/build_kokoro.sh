a6592c08141c52dd36b5bb469a541abf54c53737
Stella Laurenzo <stellaraccident@gmail.com>
2021-06-07 21:20:41 +0000

    Adds PyYAML to docker python images.
    Also adds a couple of other deps for consistency with build_requirements.txt and upstream. Will need them soon.

--- build_tools/kokoro/gcp_ubuntu/cmake-bazel/linux/x86-turing/build_kokoro.sh
+++ build_tools/kokoro/gcp_ubuntu/cmake-bazel/linux/x86-turing/build_kokoro.sh
@@ -30,7 +30,7 @@ docker_setup
 
 docker run "${DOCKER_RUN_ARGS[@]?}" \
   --gpus all \
-  gcr.io/iree-oss/cmake-bazel-frontends-nvidia@sha256:e5f15252212cda1c1952c23c7d0a31aa487d4ca56236b98054fcb5cb4f59cb88 \
+  gcr.io/iree-oss/cmake-bazel-frontends-nvidia@sha256:71eeb44ba014ee043ae2adeeb6458bc281444ee6f295b5ba7e4337a69a95f7df \
   build_tools/kokoro/gcp_ubuntu/cmake-bazel/linux/x86-turing/build.sh
 
 # Kokoro will rsync this entire directory back to the executor orchestrating the
