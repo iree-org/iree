5b53c538283832663833dde922d08edf9c5b72e3
Rob Suderman <rob.suderman@gmail.com>
2021-06-08 10:52:30 -0700

    git-clang-format

--- iree/compiler/Dialect/Flow/Transforms/PromoteI1ToI8Pass.cpp
+++ iree/compiler/Dialect/Flow/Transforms/PromoteI1ToI8Pass.cpp
@@ -59,7 +59,7 @@ class ConvertBoolConstantPattern : public OpRewritePattern<mlir::ConstantOp> {
     // needed as it is possible we are reusing an existing ConstantOp
     // containing the same values that occurs in a future line. Moving to the
     // first use case avoids declaring out of order operations.
-    Operation* firstUser = *op.getResult().getUsers().begin();
+    Operation *firstUser = *op.getResult().getUsers().begin();
     for (auto checkOp : op.getResult().getUsers()) {
       if (checkOp->isBeforeInBlock(firstUser)) {
         firstUser = checkOp;
