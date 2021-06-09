4a85781d5f07b4fee33f1187b69b6a05c5a5bc94
Ben Vanik <benvanik@google.com>
2021-06-09 09:38:42 -0700

    Compute stream buffer sizes based on the tensor shape we carry around. We know the full tensor type and can avoid the buffer view length query.

--- iree/compiler/Dialect/HAL/Conversion/FlowToHAL/ConvertStreamOps.cpp
+++ iree/compiler/Dialect/HAL/Conversion/FlowToHAL/ConvertStreamOps.cpp
@@ -1018,8 +1018,7 @@ class ExStreamFragmentOpConversion
                   streamOp.getLoc(),
                   IREE::HAL::BufferType::get(rewriter.getContext()),
                   bufferValue),
-              rewriter.createOrFold<IREE::HAL::BufferViewByteLengthOp>(
-                  streamOp.getLoc(), bufferValue)};
+              schedulingState.lookupOrComputeSize(streamValue, rewriter)};
         } else {
           bufferRange = BufferRange{
               bufferValue,
