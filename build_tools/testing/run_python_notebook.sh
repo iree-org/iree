d2fec5beb65490d4e134bf20ed77e8b3d6bfc222
Scott Todd <scotttodd@google.com>
2021-06-17 11:42:19 -0700

    Add scripts for testing Colab notebooks.

---
+++ build_tools/testing/run_python_notebook.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Copyright 2021 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# Run a Python (Colab/Jupyter) notebook in an isolated virtual environment.
+# Fails if the notebook has errors.
+# 
+# Usage: run_python_notebook.sh /path/to/notebook.ipynb
+# 
+# This is intended for use on continuous integration servers and as a reference
+# for users, but it can also be run manually.
+
+# set -x
+set -e
+
+# Run under a virtual environment to isolate Python packages.
+# TODO(scotttodd): clean the venv first? `rm -rf`?
+python -m venv .venv
+source .venv/bin/activate
+trap deactivate EXIT
+
+# Install general Jupyter notebook requirements.
+python -m pip install --upgrade pip
+python -m pip install --quiet jupyter_core nbconvert ipykernel
+
+# Install common notebook requirements.
+# TODO(scotttodd): refactor so not all deps are always installed
+python -m pip install --quiet \
+  numpy \
+  matplotlib \
+  tensorflow \
+  tensorflow_hub
+
+# Run the notebook, discarding output (still fails if an exception is thrown).
+jupyter nbconvert --to notebook --execute $1 --stdout > /dev/null
+printf "Notebook exit code: %d\n" $?
