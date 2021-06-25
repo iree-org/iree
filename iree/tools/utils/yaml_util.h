a903649bd230b9627afd02cbf58de79cdc4404cb
Ben Vanik <benvanik@google.com>
2021-06-25 09:39:53 -0700

    Adding libyaml submodule and getting it building. (#6319)
    The yaml_util.* files are here to ensure the cmake/bazel configuration is
    correct and don't yet do anything meaningful.

---
+++ iree/tools/utils/yaml_util.h
@@ -0,0 +1,16 @@
+// Copyright 2021 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef IREE_TOOLS_UTILS_YAML_UTIL_H_
+#define IREE_TOOLS_UTILS_YAML_UTIL_H_
+
+#include "iree/base/api.h"
+#include "iree/hal/api.h"
+#include "iree/vm/api.h"
+
+// TODO(benvanik): yaml parsing/printing to vm types.
+
+#endif  // IREE_TOOLS_UTILS_YAML_UTIL_H_
