5b961110596c220698c4b66e3f710ef4301c14e0
Cindy Liu <hcindyl@google.com>
2021-06-28 17:38:51 -0700

    Add riscv-32 baremetal kokoro scripts
    Test IREE build with riscv 32-bit for the bare-metal configuration. It tests
    * 32-bit code-gen flow.
    * build with the bare-metal config without threading and system library
    support.
    * Execute the 32-bit codegen workload with 32-bit linux user-space QEMU
    emulator.
    
    The CI setup for https://github.com/google/iree/issues/6322

---
+++ build_tools/kokoro/gcp_ubuntu/cmake/baremetal/riscv32/build_kokoro.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Copyright 2021 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# Cross-compile the project towards riscv with the
+# gcr.io/iree-oss/cmake-riscv image using Kokoro.
+# Requires the environment variables KOKORO_ROOT and KOKORO_ARTIFACTS_DIR, which
+# are set by Kokoro.
+
+set -x
+set -e
+set -o pipefail
+
+# Print the UTC time when set -x is on
+export PS4='[$(date -u "+%T %Z")] '
+
+source "${KOKORO_ARTIFACTS_DIR?}/github/iree/build_tools/kokoro/gcp_ubuntu/docker_common.sh"
+
+# Sets DOCKER_RUN_ARGS
+docker_setup
+
+docker run "${DOCKER_RUN_ARGS[@]?}" \
+  gcr.io/iree-oss/cmake-riscv@sha256:2ec67bdb5094323e1df45b88c088aeda435655a6543b6ee42db4a41adde3048d \
+  build_tools/kokoro/gcp_ubuntu/cmake/baremetal/riscv32/build.sh
+
+# Kokoro will rsync this entire directory back to the executor orchestrating the
+# build which takes forever and is totally useless.
+rm -rf "${KOKORO_ARTIFACTS_DIR?}"/*
+
+# Print out artifacts dir contents after deleting them as a coherence check.
+ls -1a "${KOKORO_ARTIFACTS_DIR?}/"
