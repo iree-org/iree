c7d86d31f56094cc6797c1a9dcd7a7e844f869d8
Geoffrey Martin-Noble <gcmn@google.com>
2021-06-30 14:40:08 -0700

    Actually run iree_tf_compiler tests (#6210)
    This configures the tests under iree_tf_compiler to actually get run as
    part of our CI. They had some non-functional CMake configuration that
    wasn't ever included anywhere. Some of these tests are currently failing
    due to the changes in SIP. I'll wait till those are fixed before syncing
    and merging this.
    
    This includes explicitly disabling the TFL import tests, which fail and
    appear to have never been run.

--- build_tools/kokoro/gcp_ubuntu/cmake-bazel/linux/x86-swiftshader/build.sh
+++ build_tools/kokoro/gcp_ubuntu/cmake-bazel/linux/x86-swiftshader/build.sh
@@ -31,11 +31,14 @@ cat /swiftshader/git-commit
 echo "Initializing submodules"
 ./scripts/git/submodule_versions.py init
 
-# BUILD the integrations binaries with Bazel
+# BUILD the integrations binaries with Bazel and run any lit tests
 pushd integrations/tensorflow
 BAZEL_CMD=(bazel --noworkspace_rc --bazelrc=build_tools/bazel/iree-tf.bazelrc)
 BAZEL_BINDIR="$(${BAZEL_CMD[@]?} info bazel-bin)"
-"${BAZEL_CMD[@]?}" build --config=generic_clang //iree_tf_compiler:all
+"${BAZEL_CMD[@]?}" query //iree_tf_compiler/... | \
+   xargs "${BAZEL_CMD[@]?}" test --config=generic_clang \
+      --test_tag_filters="-nokokoro" \
+      --build_tag_filters="-nokokoro"
 popd
 
 CMAKE_BUILD_DIR="$HOME/iree/build/tf"
