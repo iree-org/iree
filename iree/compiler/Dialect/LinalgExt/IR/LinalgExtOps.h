362fd520cf8bdc6f4a3134f5cf03a8ea3b1e77e7
MaheshRavishankar <1663364+MaheshRavishankar@users.noreply.github.com>
2021-07-13 16:36:28 -0700

    Initial Implementation of tile (+distribute) of LinalgExt ops using `TiledOpInterface` (#6423)
    This changes adds tiling transformations for LinalgExt using the
    TiledOpInterface. The linalg_ext.scatter and linalg_ext.sort
    (only parallel dims) operation is used as the candidate for tiling.
    The operations implements the TiledOpInterface
    which is used by the tiling transformation to tile + distribute the
    op.
    The tiling transformation is controlled similar to LinalgOps, using
    LinalgTilingOptions, LinalgTransformationFilter and
    LinalgLoopDistributionOptions.

--- iree/compiler/Dialect/LinalgExt/IR/LinalgExtOps.h
+++ iree/compiler/Dialect/LinalgExt/IR/LinalgExtOps.h
@@ -7,6 +7,8 @@
 #ifndef IREE_COMPILER_DIALECT_LINALGEXT_IR_LINALGEXTOPS_H_
 #define IREE_COMPILER_DIALECT_LINALGEXT_IR_LINALGEXTOPS_H_
 
+#include "iree/compiler/Dialect/LinalgExt/IR/LinalgExtInterfaces.h"
+#include "iree/compiler/Dialect/LinalgExt/IR/TiledOpInterface.h"
 #include "mlir/IR/Attributes.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
@@ -17,13 +19,18 @@
 namespace mlir {
 namespace iree_compiler {
 namespace linalg_ext {
-class LinalgExtOp;
+
+/// Returns a `memref.dim` or `tensor.dim` operation to get the shape of `v` at
+/// `dim`.
+Value getDimValue(OpBuilder &builder, Location loc, Value v, int64_t dim);
+
+/// Returns a `memref.dim` or `tensor.dim` operation to get the shape of `v` at
+/// `dim`. If the shape is constant, returns the shape as an `IntegerAttr`.
+OpFoldResult getDim(OpBuilder &builder, Location loc, Value v, int64_t dim);
 
 }  // namespace linalg_ext
 }  // namespace iree_compiler
 }  // namespace mlir
-
-#include "iree/compiler/Dialect/LinalgExt/IR/LinalgExtInterfaces.h"
 
 #define GET_OP_CLASSES
 #include "iree/compiler/Dialect/LinalgExt/IR/LinalgExtOps.h.inc"  // IWYU pragma: export
