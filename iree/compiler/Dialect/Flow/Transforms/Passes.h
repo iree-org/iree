fc3ebe253204bc229cd97bc4fb6e74971920af3d
Thomas <thomasraoux@google.com>
2021-07-19 14:30:45 -0700

    Interchange linalg generic op to move reduction loops to inner loops (#6470)
    During dispatchLinalgOnTensor we only tile outer parallel loops so if
    the parallel loops are not outer loop we may skip tiling some ops which
    will prevent them from being distributed onto several workgroups.

--- iree/compiler/Dialect/Flow/Transforms/Passes.h
+++ iree/compiler/Dialect/Flow/Transforms/Passes.h
@@ -57,6 +57,10 @@ std::unique_ptr<Pass> createPadTensorToSubTensorInsertPass();
 /// Creates a pass to fuse Linalg operations on tensors.
 std::unique_ptr<Pass> createFusionOfTensorOpsPass();
 
+/// Create a pass to interchange generic ops to force the reduction loop to be
+/// the most inner loops.
+std::unique_ptr<OperationPass<FuncOp>> createInterchangeLinalgGenericPass();
+
 // Convert operations to equivalent flow.tensor.* ops. This is run after
 // dispatch region creation to catch operations that were left outside of
 // dispatch regions and could be represented as flow.tensor.* ops.
