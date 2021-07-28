43530052d32ee79435c7b7a30221e5225c0076fd
niloysikdar <niloysikdar30@gmail.com>
2021-07-28 09:04:54 +0530

    Adding check_submodule_init.py

---
+++ scripts/git/check_submodule_init.py
@@ -0,0 +1,30 @@
+# Copyright 2021 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import os
+import sys
+
+
+def run():
+    # Checking the "git status"
+    git_check = os.popen("git status")
+    git_status = git_check.readline()
+
+    # no-op if not a git repository
+    if (git_status.startswith("On")):
+        output = os.popen("git submodule status")
+        submodules = output.readlines()
+
+        for submodule in submodules:
+            if (submodule.strip()[0] == "-"):
+                print(
+                    f"The git submodule '{submodule.split()[1]}' is not initialized. Please run `git submodule update --init`")
+
+            sys.exit(1)
+
+
+if __name__ == "__main__":
+    run()
