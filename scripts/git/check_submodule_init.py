96affe58165f9e399ee0535aebdac486db6f039a
Scott Todd <scotttodd@google.com>
2021-07-28 09:04:58 -0700

    Feedback changes: different git repo and python interpreter checks.

--- scripts/git/check_submodule_init.py
+++ scripts/git/check_submodule_init.py
@@ -5,27 +5,28 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 import os
+import subprocess
 import sys
 
 
 def run():
-  # Checking the "git status"
-  git_check = os.popen("git status")
-  git_status = git_check.readline()
+  # No-op if we're not in a git repository.
+  try:
+    subprocess.check_call(['git', 'rev-parse', '--is-inside-work-tree'],
+                          stdout=subprocess.DEVNULL,
+                          stderr=subprocess.DEVNULL)
+  except:
+    return
 
-  # no-op if not a git repository
-  if not git_status.startswith("On"):
-    sys.exit(1)
-  else:
-    output = os.popen("git submodule status")
-    submodules = output.readlines()
+  output = os.popen("git submodule status")
+  submodules = output.readlines()
 
-    for submodule in submodules:
-      if (submodule.strip()[0] == "-"):
-        print(
-            f"The git submodule '{submodule.split()[1]}' is not initialized. Please run `git submodule update --init`"
-        )
-        sys.exit(1)
+  for submodule in submodules:
+    if (submodule.strip()[0] == "-"):
+      print(
+          f"The git submodule '{submodule.split()[1]}' is not initialized. Please run `git submodule update --init`"
+      )
+      sys.exit(1)
 
 
 if __name__ == "__main__":
