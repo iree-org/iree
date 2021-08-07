fd767d07ebd40e20615909d18c40d6ada5505b52
Stella Laurenzo <stellaraccident@gmail.com>
2021-08-07 01:39:42 +0000

    Create a first-class IREE compiler C and Python API.
    * In-memory interop with MLIR C and Python API objects.
    * Exposes a low-level driver API for constructing pass pipelines, options, translating/emitting (expect a higher level user API similar to what we already have which shells out to iree-translate).
    * Includes access to all dialects that IREE can input.
    * Minimal options plumbed through.
    * Does not yet plumb through other exits (i.e. emit-c).
    * It would be nice to do some more code organization on the C++ side, but that doesn't change the C-API.
    * Will create a CLI later which uses the C-API (which can dynamic link and save us space).

--- llvm-projects/iree-compiler-api/build_tools/smoketest.py
+++ llvm-projects/iree-compiler-api/build_tools/smoketest.py
@@ -4,17 +4,17 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+import io
+
 from iree.compiler import ir
+from iree.compiler import passmanager
 from iree.compiler.dialects import chlo
 from iree.compiler.dialects import mhlo
 from iree.compiler.dialects import iree_public
 from iree.compiler.dialects import builtin
 from iree.compiler.dialects import std
 from iree.compiler.dialects import linalg
-try:
-  from iree.compiler.dialects import linalg
-except ImportError as e:
-  print("KNOWN ISSUE: Linalg has an absolute path dependency issue:", e)
+from iree.compiler.dialects import linalg
 from iree.compiler.dialects import math
 from iree.compiler.dialects import memref
 from iree.compiler.dialects import shape
@@ -22,17 +22,31 @@ from iree.compiler.dialects import tensor
 from iree.compiler.dialects import tosa
 from iree.compiler.dialects import vector
 
+from iree.compiler.api import driver
+
 with ir.Context() as ctx:
-  try:
-    chlo.register_chlo_dialect(ctx)
-  except ImportError as e:
-    print(
-        "KNOWN ISSUE: For hidden visibility builds extensions need "
-        "an explicit dep on LLVMSupport (chlo):", e)
-  try:
-    mhlo.register_mhlo_dialect(ctx)
-  except ImportError as e:
-    print(
-        "KNOWN ISSUE: For hidden visibility builds extensions need "
-        "an explicit dep on LLVMSupport (mhlo):", e)
+  chlo.register_chlo_dialect(ctx)
+  mhlo.register_mhlo_dialect(ctx)
   iree_public.register_iree_public_dialect(ctx)
+
+  input_module = ir.Module.parse(r"""
+    builtin.module  {
+      builtin.func @fabs(%arg0: tensor<1x4xf32>, %arg1: tensor<4x1xf32>) -> tensor<4x4xf32> {
+        %0 = chlo.broadcast_add %arg0, %arg1 : (tensor<1x4xf32>, tensor<4x1xf32>) -> tensor<4x4xf32>
+        %1 = "mhlo.abs"(%0) : (tensor<4x4xf32>) -> tensor<4x4xf32>
+        return %1 : tensor<4x4xf32>
+      }
+    }
+  """)
+
+  options = driver.CompilerOptions()
+  options.set_input_dialect_mhlo()
+  options.add_target_backend("cpu")
+  pm = passmanager.PassManager()
+  driver.build_iree_vm_pass_pipeline(options, pm)
+  pm.run(input_module)
+
+  print(input_module)
+  bytecode_io = io.BytesIO()
+  driver.translate_module_to_vm_bytecode(options, input_module, bytecode_io)
+  print(f"Bytecode module len = {len(bytecode_io.getbuffer())}")
