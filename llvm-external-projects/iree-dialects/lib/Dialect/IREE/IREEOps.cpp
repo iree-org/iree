d029202e5f15311d3b499f8c5372f1c15c4753c8
Stella Laurenzo <stellaraccident@gmail.com>
2021-08-10 23:17:44 -0700

    Start to populate the IREE dialect. (#6729)
    * Start to populate the IREE dialect.
    
    * A selection of types and ops that I have found useful to use from frontends.
    * Mostly 1:1 with internal dialects, with the exception of cast ops, which I disaggregated to simplify.
    * Stripped most builders, verifiers, canonicalizers, op interfaces, etc in favor of pure declarative.
    * No tests yet. Will test as part of the transforms to lower level.

--- llvm-external-projects/iree-dialects/lib/Dialect/IREE/IREEOps.cpp
+++ llvm-external-projects/iree-dialects/lib/Dialect/IREE/IREEOps.cpp
@@ -7,7 +7,13 @@
 #include "iree-dialects/Dialect/IREE/IREEOps.h"
 
 #include "iree-dialects/Dialect/IREE/IREEDialect.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/OpImplementation.h"
+#include "mlir/IR/TypeUtilities.h"
+
+using namespace mlir;
+using namespace mlir::iree;
 
 #define GET_OP_CLASSES
 #include "iree-dialects/Dialect/IREE/IREEOps.cpp.inc"
