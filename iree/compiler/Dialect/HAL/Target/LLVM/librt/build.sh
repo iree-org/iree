bb4f473fd1e3cdfd4b4a72f823172a8725ca49c7
MaheshRavishankar <ravishankarm@google.com>
2021-08-11 18:11:41 -0700

    Add musl suubmodule for math function implementation.
    This adds musl as a submodule of IREE. Using the source (with help of
    custom makefiles), the .ll file for math functions needed are
    generated and linked into `librt.bc`.

--- iree/compiler/Dialect/HAL/Target/LLVM/librt/build.sh
+++ iree/compiler/Dialect/HAL/Target/LLVM/librt/build.sh
@@ -4,6 +4,7 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+set -x
 set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
@@ -31,15 +32,40 @@ clang \
     -c \
     "${SRC}/libm.c"
 
-# Clang adds a bunch of bad attributes and host-specific information that we
-# don't want (so we get at least somewhat deterministic builds).
-sed -i 's/^;.*$//' ${LL_FILE}
-sed -i 's/^source_filename.*$//' ${LL_FILE}
-sed -i 's/^target datalayout.*$//' ${LL_FILE}
-sed -i 's/^target triple.*$//' ${LL_FILE}
-sed -i 's/^\(attributes #[0-9]* = {\).*$/\1 inlinehint }/' ${LL_FILE}
+## Generate the LLVM IR assembly for the required math files from muls
+IREE_SRC_DIR="$( cd ${SCRIPT_DIR} && cd ../../../../../../../ && pwd)"
+MUSL_DIR=${IREE_SRC_DIR}/third_party/musl
+cd ${MUSL_DIR}
+CC=clang CXX=clang++ ./configure
+MUSL_DIR=${MUSL_DIR} make -f ${SCRIPT_DIR}/Makefile_musl.iree iree
+MUSL_LL_FILES=`find obj/ -name *.ll`
+cp ${MUSL_LL_FILES} ${OUT}
+rm ${MUSL_LL_FILES}
+cd ${SCRIPT_DIR}
 
-# Generate a binary bitcode file embedded into the compiler binary.
-# NOTE: we do this from stdin so that the filename on the user's system is not
-# embedded in the bitcode file (making it non-deterministic).
-cat ${LL_FILE} | llvm-as -o=${BC_FILE}
+ALL_LL_FILES=`find ${OUT} -name *.ll`
+
+cd ${OUT}
+git restore ${BC_FILE}
+for file in ${ALL_LL_FILES}
+do
+  opt ${file} -O3 -S -o ${file}.opt.ll
+  # Clang adds a bunch of bad attributes and host-specific information that we
+  # don't want (so we get at least somewhat deterministic builds).
+  sed -i 's/^;.*$//' ${file}.opt.ll
+  sed -i 's/^source_filename.*$//' ${file}.opt.ll
+  sed -i 's/^target datalayout.*$//' ${file}.opt.ll
+  sed -i 's/^target triple.*$//' ${file}.opt.ll
+  sed -i 's/^\(attributes #[0-9]* = {\).*$/\1 inlinehint }/' ${file}.opt.ll
+
+  # Generate a binary bitcode file embedded into the compiler binary.
+  # NOTE: we do this from stdin so that the filename on the user's system is not
+  # embedded in the bitcode file (making it non-deterministic).
+  cat ${file}.opt.ll | llvm-as -o=${file}.opt.ll.bc
+done
+
+ALL_BC_FILES=`ls *.ll.bc`
+llvm-link ${ALL_BC_FILES} -o ${BC_FILE}
+rm ${ALL_BC_FILES}
+ALL_LL_FILES=`ls *.ll`
+rm ${ALL_LL_FILES}
