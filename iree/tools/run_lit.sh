c64624c8932039554d27f1fe63255fd0a33fd5fa
Geoffrey Martin-Noble <gcmn@google.com>
2021-08-23 14:47:53 -0700

    Remove runfiles nonsense and add llvm-symbolizer to lit tests (#6788)
    Related to https://github.com/google/iree/issues/818

--- iree/tools/run_lit.sh
+++ iree/tools/run_lit.sh
@@ -23,62 +23,9 @@ do
   EXPLICIT_PATH="${EXEDIR}:${EXPLICIT_PATH}"
 done
 
-#### TODO: Remove all of this implicit path/runfiles stuff. The CMake rules
-#### pass explicit tools on the command line.
-if [ -z "${RUNFILES_DIR}" ]; then
-  if [ -f "CMakeCache.txt" ]; then
-    # If running under CMake/CTest in the build directory, just scope to the
-    # iree directory to avoid blowing up the search through things like
-    # bazel out directories and the like.
-    RUNFILES_DIR="$PWD/iree"
-  else
-    # Some versions of bazel do not set RUNFILES_DIR. Instead they just cd
-    # into the directory.
-    RUNFILES_DIR="$PWD"
-  fi
-fi
-
-# Detect whether cygwin/msys2 paths need to be translated.
-set +e  # Ignore errors if not found.
-cygpath="$(which cygpath 2>/dev/null)"
-set -e
-
-function find_executables() {
-  set -e
-  local p="$1"
-  if [ "$(uname)" == "Darwin" ]; then
-    # For macOS, xtype isn't avaliable and perm can't use `/u=x,g=x,o=x` syntax.
-    find "${p}" -type l -perm +111
-  elif [ -z "$cygpath" ]; then
-    # For non-windows, use the perm based executable check, which has been
-    # supported by find for a very long time.
-    find "${p}" -xtype f -perm /u=x,g=x,o=x -print
-  else
-    # For windows, always use the newer -executable find predicate (which is
-    # not supported by ancient versions of find).
-    find "${p}" -xtype f -executable -print
-  fi
-}
-
-# Bazel helpfully puts all data deps in the ${RUNFILES_DIR}, but
-# it unhelpfully preserves the nesting with no way to reason about
-# it generically. run_lit expects that anything passed in the runfiles
-# can be found on the path for execution. So we just iterate over the
-# entries in the MANIFEST and extend the PATH.
-for runfile_path in $(find_executables "${RUNFILES_DIR}"); do
-  # Prepend so that local things override.
-  EXEDIR="$(dirname ${runfile_path})"
-  if ! [ -z "$cygpath" ]; then
-    EXEDIR="$($cygpath -u "$EXEDIR")"
-  fi
-  IMPLICIT_PATH="${EXEDIR}:$IMPLICIT_PATH"
-done
-#### END OF DEPRECATED IMPLICIT PATH DISCOVERY
-
 echo "run_lit.sh: $src_file"
 echo "PWD=$(pwd)"
 echo "EXPLICIT_PATH=$EXPLICIT_PATH"
-echo "IMPLICIT_PATH=$IMPLICIT_PATH"
 
 # For each "// RUN:" line, run the command.
 runline_matches="$(egrep "^// RUN: " "$src_file")"
@@ -101,7 +48,7 @@ do
   full_command="${command//\%s/$src_file}"
 
   # Run it.
-  export PATH="$EXPLICIT_PATH:$IMPLICIT_PATH:$PATH"
+  export PATH="$EXPLICIT_PATH:$PATH"
   echo "RUNNING TEST: $full_command"
   echo "----------------"
   if eval "$full_command"; then
