b3db3dd202669bf8b7b1adcbfe4b5d6d55eebdde
Geoffrey Martin-Noble <gcmn@google.com>
2021-08-25 15:32:16 -0700

    Use RBE to cache TF build results (#6867)
    TF makes it a total PITA to build it with remote execution, but we *can*
    get a bunch of the benefits (and with much less configuration) with
    remote caching. This has actions executed locally but cached remotely.
    
    Note that we need to ensure that the machines reading and writing from
    the cache are ~identical, so this should only be executed inside of a
    docker container and the docker image digest is used as the cache key.
    
    Not really sure why I didn't think of this earlier.
    
    Tested: observe that in the integrations build of the second commit
    here, the Bazel part of the build took 30 seconds.

--- build_tools/kokoro/gcp_ubuntu/cmake-bazel/linux/x86-swiftshader/build.sh
+++ build_tools/kokoro/gcp_ubuntu/cmake-bazel/linux/x86-swiftshader/build.sh
@@ -36,7 +36,9 @@ pushd integrations/tensorflow
 BAZEL_CMD=(bazel --noworkspace_rc --bazelrc=build_tools/bazel/iree-tf.bazelrc)
 BAZEL_BINDIR="$(${BAZEL_CMD[@]?} info bazel-bin)"
 "${BAZEL_CMD[@]?}" query //iree_tf_compiler/... | \
-   xargs "${BAZEL_CMD[@]?}" test --config=generic_clang \
+   xargs "${BAZEL_CMD[@]?}" test \
+      --config=remote_cache_tf_integrations \
+      --config=generic_clang \
       --test_tag_filters="-nokokoro" \
       --build_tag_filters="-nokokoro"
 popd
