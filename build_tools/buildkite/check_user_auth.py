3686d9684efff5916db88103d41b5f5db5c13bce
Geoffrey Martin-Noble <gcmn@google.com>
2021-08-25 19:22:01 -0700

    Format with yapf

--- build_tools/buildkite/check_user_auth.py
+++ build_tools/buildkite/check_user_auth.py
@@ -8,8 +8,9 @@
 import os
 import sys
 
-AUTHORIZED_USER_EMAILS=["gcmn@google.com", "marius.brehler@iml.fraunhofer.de"]
-CREATOR_EMAIL_ENV_VAR="BUILDKITE_BUILD_CREATOR_EMAIL"
+AUTHORIZED_USER_EMAILS = ["gcmn@google.com", "marius.brehler@iml.fraunhofer.de"]
+CREATOR_EMAIL_ENV_VAR = "BUILDKITE_BUILD_CREATOR_EMAIL"
+
 
 def main():
   creator_email = os.getenv(CREATOR_EMAIL_ENV_VAR)
@@ -17,17 +18,24 @@ def main():
     raise RuntimeError(f"{CREATOR_EMAIL_ENV_VAR} is not defined")
 
   if creator_email.endswith("@google.com"):
-    print(f"User '{creator_email}' is authorized because email ends in '@google.com'")
+    print(
+        f"User '{creator_email}' is authorized because email ends in '@google.com'"
+    )
     return 0
   print(f"User '{creator_email}' does not end in '@google.com'")
 
   if creator_email in AUTHORIZED_USER_EMAILS:
-    print(f"User '{creator_email}' is authorized because email is in list of authorized users")
+    print(
+        f"User '{creator_email}' is authorized because email is in list of authorized users"
+    )
     return 0
-  print(f"User '{creator_email}' is not in authorized user list: {AUTHORIZED_USER_EMAILS}")
+  print(
+      f"User '{creator_email}' is not in authorized user list: {AUTHORIZED_USER_EMAILS}"
+  )
 
   print("User is not authorized")
   return 1
+
 
 if __name__ == "__main__":
   sys.exit(main())
