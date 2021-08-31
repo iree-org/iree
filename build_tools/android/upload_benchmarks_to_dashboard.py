b12dca0017c3e7dc048812980526062791c76993
Lei Zhang <antiagainst@google.com>
2021-08-31 09:10:33 -0400

    Explain x/y axis in perf dashboard graphs

--- build_tools/android/upload_benchmarks_to_dashboard.py
+++ build_tools/android/upload_benchmarks_to_dashboard.py
@@ -33,6 +33,12 @@ IREE_GITHUB_COMMIT_URL_PREFIX = 'https://github.com/google/iree/commit'
 IREE_PROJECT_ID = 'IREE'
 THIS_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
+COMMON_DESCRIIPTION = """
+<br>
+For the graph, the x axis is the Git commit index, and the y axis is the
+measured latency in milliseconds.
+"""
+
 # A non-exhaustive list of models and their source URLs.
 # For models listed here we can provide a nicer description for them on
 # webpage.
@@ -292,11 +298,14 @@ def main(args):
 
   # Upload benchmark results to the dashboard.
   for series_id, (sample_value, benchmark_info) in aggregate_results.items():
-    model_description = get_model_description(benchmark_info)
+    description = get_model_description(benchmark_info)
+    if description is None:
+      description = ""
+    description += COMMON_DESCRIIPTION
 
     # Override by default to allow updates to the series.
     add_new_iree_series(series_id,
-                        model_description,
+                        description,
                         override=True,
                         dry_run=args.dry_run,
                         verbose=args.verbose)
