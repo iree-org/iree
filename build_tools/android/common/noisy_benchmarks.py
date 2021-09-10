ec69dae91e9f1c7b64f2d1d4cd68eb03c7792db5
Lei Zhang <antiagainst@google.com>
2021-09-10 14:32:34 -0400

    Update comment

--- build_tools/android/common/noisy_benchmarks.py
+++ build_tools/android/common/noisy_benchmarks.py
@@ -7,8 +7,17 @@
 
 import re
 
+# A list of noisy benchmarks. Each one is a tuple that contains the following
+# fields:
+# - A regular expression to match against the benchmark identifier.
+# - A threshold for computing the benchmark value average. Benchmark sample
+#   values from consecutive runs and within the given range will be considered
+#   as similar (with some noise). They will be used to compute the moving
+#   average. It is a string that sent to Dana as API call JSON payloadsdirectly.
+#   There are two formats supported: a percentage or an absolute value. So we
+#   use a string here. What value to set depends on the noise range of the
+#   particular benchmark.
 NOISY_BENCHMARKS = [
-    # (regular expression to match the benchmark name, average threshold)
     (re.compile(r"^PoseNet.*GPU-Mali-G77"), "100%"),
     (re.compile(r"^DeepLabV3.*GPU-Mali-G77"), "100%"),
     (re.compile(r"^MobileSSD.*GPU-Mali-G77"), "100%"),
