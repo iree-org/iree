0f6c264a5520946e9aaf3f6247c6fc0f0973f977
Lei Zhang <antiagainst@google.com>
2021-09-13 13:58:54 -0400

    Move Adreno convolution CodeGen config as default for all targets
    There is no change involved in the default logic; just moving the
    code. This also removes the hand picked known good configurations
    for Mali GPUs.

--- iree/compiler/Codegen/SPIRV/KernelConfig.cpp
+++ iree/compiler/Codegen/SPIRV/KernelConfig.cpp
@@ -14,6 +14,7 @@
 #include "llvm/Support/Debug.h"
 #include "mlir/Dialect/Linalg/IR/LinalgOps.h"
 #include "mlir/Dialect/SPIRV/IR/SPIRVAttributes.h"
+#include "mlir/Dialect/SPIRV/IR/SPIRVEnums.h"
 #include "mlir/Dialect/SPIRV/IR/TargetAndABI.h"
 
 #define DEBUG_TYPE "iree-spirv-kernel-config"
@@ -67,8 +68,15 @@ static LogicalResult setTranslationUsingDistributeToGlobalId(
   return defineWorkgroupCountRegion(builder, funcOp, numWorkgroupsFn);
 }
 
-namespace detail {
-LogicalResult defineConvWorkgroupCountRegion(
+//===----------------------------------------------------------------------===//
+// Convolution Default Configuration
+//===----------------------------------------------------------------------===//
+
+/// Lets the entry point region to return fully static number of workgroups.
+// This is needed for folding `affine.min` ops to expose static-shaped tiled
+// convolution for vectorization.
+// TODO(#5034): Use a proper way to prove tilability and fold `affine.min`s.
+static LogicalResult defineConvWorkgroupCountRegion(
     Operation *op, ArrayRef<int64_t> outputShape,
     ArrayRef<int64_t> workgroupTileSizes) {
   auto numWorkgroupsFn = [&](OpBuilder &b, Location loc, std::array<Value, 3>) {
@@ -83,6 +91,109 @@ LogicalResult defineConvWorkgroupCountRegion(
   return defineWorkgroupCountRegion(builder, op->getParentOfType<FuncOp>(),
                                     numWorkgroupsFn);
 }
+
+namespace detail {
+
+LogicalResult setConvOpConfig(linalg::LinalgOp linalgOp,
+                              const int64_t subgroupSize,
+                              const int64_t bestTilingFactor) {
+  ArrayRef<int64_t> inputShape = getUntiledShape(linalgOp.inputs()[0]);
+  ArrayRef<int64_t> outputShape = getUntiledResultShape(linalgOp, 0);
+  if (llvm::any_of(inputShape, ShapedType::isDynamic)) return success();
+  if (llvm::any_of(outputShape, ShapedType::isDynamic)) return success();
+
+  int64_t ic = inputShape[3];
+  int64_t oh = outputShape[1], ow = outputShape[2], oc = outputShape[3];
+
+  // The conversion pipeline requires the input channel dimension to be some
+  // multipler of four, or less than four.
+  if (!(ic % 4 == 0 || ic < 4)) return success();
+
+  // The core idea is to distribute the convolution OH/OW/OC dimension to the
+  // workgroup Z/Y/X dimension, with each thread in a workgroup handling
+  // multiple vector elements. We try to 1) utilize all threads in a subgroup,
+  // and 2) handle an optimal tile size along each dimension.
+
+  int64_t residualThreads = subgroupSize;
+  int64_t residualTilingFactor = bestTilingFactor;
+
+  SmallVector<int64_t, 3> workgroupSize(3, 1);        // (X, Y, Z)
+  SmallVector<int64_t, 4> workgroupTileSizes(4, 0);   // (N, OH, OW, OC)
+  SmallVector<int64_t, 4> invocationTileSizes(4, 0);  // (N, OH, OW, OC)
+
+  // Deduce the configuration for the OC dimension.
+  for (int64_t x = residualThreads; x >= 2; x >>= 1) {
+    // Handle 4 elements per thread for the innermost dimension. We need this
+    // for vectorized load.
+    int64_t chosenTileSize = 4;
+    if (oc % (x * chosenTileSize) == 0) {
+      workgroupSize[0] = x;
+      workgroupTileSizes[3] = x * chosenTileSize;
+      invocationTileSizes[3] = chosenTileSize;
+      residualThreads /= x;
+      residualTilingFactor /= chosenTileSize;
+      break;
+    }
+  }
+  if (workgroupTileSizes[3] == 0) return success();
+
+  // Deduce the configruation for the OW and OH dimension. Try to make them even
+  // if possible given we typically have images with the same height and width.
+  if (ow == oh && residualThreads != 1 &&
+      llvm::Log2_64(residualThreads) % 2 == 0) {
+    int64_t yz = 1 << (llvm::Log2_64(residualThreads) / 2);
+    int64_t chosenTileSize = 1 << (llvm::Log2_64(residualTilingFactor) / 2);
+    while (ow % chosenTileSize != 0) chosenTileSize >>= 1;
+    workgroupSize[1] = workgroupSize[2] = yz;
+    workgroupTileSizes[2] = workgroupTileSizes[1] = yz * chosenTileSize;
+    invocationTileSizes[2] = invocationTileSizes[1] = chosenTileSize;
+  } else {
+    auto decideOneDim = [&](int64_t inputDim, int64_t &wgDimSize,
+                            int64_t &wgTileSize, int64_t &invoTileSize) {
+      for (int64_t dim = residualThreads; dim >= 1; dim >>= 1) {
+        int64_t chosenTileSize = 0;
+        for (int64_t t = residualTilingFactor; t >= 1; t >>= 1) {
+          if (inputDim % (dim * t) == 0) {
+            chosenTileSize = t;
+            break;
+          }
+        }
+        if (chosenTileSize) {
+          wgDimSize = dim;
+          wgTileSize = dim * chosenTileSize;
+          invoTileSize = chosenTileSize;
+          residualThreads /= dim;
+          residualTilingFactor /= chosenTileSize;
+          return true;
+        }
+      }
+      return false;
+    };
+
+    if (!decideOneDim(ow, workgroupSize[1], workgroupTileSizes[2],
+                      invocationTileSizes[2]) ||
+        !decideOneDim(oh, workgroupSize[2], workgroupTileSizes[1],
+                      invocationTileSizes[1])) {
+      return success();
+    }
+  }
+
+  auto pipeline = IREE::HAL::DispatchLoweringPassPipeline::SPIRVVectorize;
+  TileSizesListType tileSizes;
+  tileSizes.push_back(workgroupTileSizes);
+  tileSizes.emplace_back();  // Subgroup level
+  tileSizes.push_back(invocationTileSizes);
+
+  auto funcOp = linalgOp->getParentOfType<FuncOp>();
+  if (failed(setOpConfigAndEntryPointFnTranslation(
+          funcOp, linalgOp, tileSizes, {}, pipeline, workgroupSize))) {
+    return failure();
+  }
+  return defineConvWorkgroupCountRegion(
+      linalgOp, llvm::makeArrayRef(outputShape).drop_front(),
+      llvm::makeArrayRef(workgroupTileSizes).drop_front());
+}
+
 }  // namespace detail
 
 //===----------------------------------------------------------------------===//
@@ -287,7 +398,12 @@ static LogicalResult setSPIRVOpConfig(const spirv::TargetEnv &targetEnv,
       .Case<linalg::BatchMatmulOp, linalg::MatmulOp>(
           [limits](auto op) { return setOpConfig(limits, op); })
       .Case<linalg::Conv2DNhwcHwcfOp, linalg::DepthwiseConv2DNhwOp>(
-          [limits](auto op) { return setDefaultOpConfig(limits, op); })
+          [](auto op) {
+            // It's common to see 32 threads per subgroup for GPUs.
+            // The tiling factor 32 is just a best guess.
+            return detail::setConvOpConfig(op, /*subgroupSize=*/32,
+                                           /*bestTilingFactor=*/32);
+          })
       .Default([](Operation *) { return success(); });
 };
 
