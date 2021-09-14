7bfb21a421892b52e3e3612ff12dabbb722cd7ba
Ben Vanik <benvanik@google.com>
2021-09-14 11:26:51 -0700

    Adding DFX (a TLA for something like DataFlow Framework). This is a port of the LLVM Attributor to MLIR/IREE concepts. Beyond using our Explorer it is relatively generic (unlike the Attributor, which is tied tightly to LLVM IR and their attributes) and something that we could try to upstream. For now it lives here.
    The overall design and functionality is nearly identical to the Attributor.
    The changes performed while porting were mostly around removing LLVM IR
    dependencies, matching MLIR style, anchoring on MLIR IR concepts (via our
    Position), and removing the manifesting phase (as we have better ways of
    doing that in MLIR).
    
    There are still some missing pieces - such as IntegerRangeState - that
    require additional porting of things like ConstantRange.

---
+++ iree/compiler/Dialect/Util/Analysis/DFX/Element.h
@@ -0,0 +1,191 @@
+// Copyright 2021 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef IREE_COMPILER_DIALECT_UTIL_ANALYSIS_DFX_ELEMENT_H_
+#define IREE_COMPILER_DIALECT_UTIL_ANALYSIS_DFX_ELEMENT_H_
+
+#include "iree/compiler/Dialect/Util/Analysis/DFX/DepGraph.h"
+#include "iree/compiler/Dialect/Util/Analysis/DFX/State.h"
+#include "llvm/Support/raw_ostream.h"
+#include "mlir/IR/AsmState.h"
+#include "mlir/Support/LLVM.h"
+
+namespace mlir {
+namespace iree_compiler {
+namespace DFX {
+
+class Solver;
+
+//===----------------------------------------------------------------------===//
+// AbstractElement
+//===----------------------------------------------------------------------===//
+
+// Base type for information in the solver framework.
+// Each element represents some assumed and known knowledge anchored on a
+// specific position in the IR such as a Value or Operation.
+class AbstractElement : public Position, public DepGraphNode {
+ public:
+  using StateType = AbstractState;
+
+  AbstractElement(const Position &pos) : Position(pos) {}
+  virtual ~AbstractElement() = default;
+
+  // Returns an IR position anchoring this element to the IR.
+  const Position &getPosition() const { return *this; };
+  Position &getPosition() { return *this; };
+
+  // Returns the internal abstract state for inspection.
+  virtual StateType &getState() = 0;
+  virtual const StateType &getState() const = 0;
+
+  // Initializes the state with the information in |solver|.
+  //
+  // This function is called by the solver once all abstract elements
+  // have been identified. It can and shall be used for tasks like:
+  //  - identify existing knowledge in the IR and use it for the "known state"
+  //  - perform any work that is not going to change over time, e.g., determine
+  //    a subset of the IR, or elements in-flight, that have to be looked at
+  //    in the `updateImpl` method.
+  virtual void initialize(Solver &solver) {}
+
+  // Returns the name of the AbstractElement for debug printing.
+  virtual const std::string getName() const = 0;
+  // Returns the address of the ID of the AbstractElement for type comparison.
+  virtual const char *getID() const = 0;
+
+  // Returns true if |node| is of type AbstractElement so that the dyn_cast and
+  // cast can use such information to cast an DepGraphNode to an
+  // AbstractElement.
+  //
+  // We eagerly return true here because all DepGraphNodes except for the
+  // synthethis node are of type AbstractElement.
+  static bool classof(const DepGraphNode *node) { return true; }
+
+  // Returns the human-friendly summarized assumed state as string for
+  // debugging.
+  virtual const std::string getAsStr() const = 0;
+
+  void print(llvm::raw_ostream &os, AsmState &asmState) const override;
+  virtual void printWithDeps(llvm::raw_ostream &os, AsmState &asmState) const;
+  void dump(AsmState &asmState) const;
+
+  friend class Solver;
+
+ protected:
+  // Hook for the solver to trigger an update of the internal state.
+  //
+  // If this attribute is already fixed this method will return UNCHANGED,
+  // otherwise it delegates to `AbstractElement::updateImpl`.
+  //
+  // Returns CHANGED if the internal state changed, otherwise UNCHANGED.
+  ChangeStatus update(Solver &solver);
+
+  // Update/transfer function which has to be implemented by the derived
+  // classes.
+  //
+  // When called the environment has changed and we have to determine if
+  // the current information is still valid or adjust it otherwise.
+  //
+  // Returns CHANGED if the internal state changed, otherwise UNCHANGED.
+  virtual ChangeStatus updateImpl(Solver &solver) = 0;
+};
+
+//===----------------------------------------------------------------------===//
+// OperationElement/TypedOperationElement<T>
+//===----------------------------------------------------------------------===//
+
+// An abstract element that represents an operation in the IR.
+struct OperationElement : public AbstractElement {
+  using AbstractElement::AbstractElement;
+
+  void initialize(Solver &solver) override {
+    if (isOperation()) {
+      auto &op = getOperation();
+      initializeOperation(&op, solver);
+    } else {
+      getState().indicatePessimisticFixpoint();
+    }
+  }
+
+  virtual void initializeOperation(Operation *op, Solver &solver) = 0;
+
+  ChangeStatus updateImpl(Solver &solver) override {
+    if (isOperation()) {
+      auto &op = getOperation();
+      return updateOperation(&op, solver);
+    } else {
+      return getState().indicatePessimisticFixpoint();
+    }
+  }
+
+  virtual ChangeStatus updateOperation(Operation *op, Solver &solver) = 0;
+};
+
+// An abstract element that represents an operation of type OpT in the IR.
+template <typename OpT>
+struct TypedOperationElement : public AbstractElement {
+  using AbstractElement::AbstractElement;
+
+  void initialize(Solver &solver) override {
+    if (isOperation()) {
+      auto op = dyn_cast<OpT>(getOperation());
+      if (op) {
+        initializeOperation(op, solver);
+        return;
+      }
+    }
+    getState().indicatePessimisticFixpoint();
+  }
+
+  virtual void initializeOperation(OpT op, Solver &solver) = 0;
+
+  ChangeStatus updateImpl(Solver &solver) override {
+    if (isOperation()) {
+      auto op = dyn_cast<OpT>(getOperation());
+      if (op) return updateOperation(op, solver);
+    }
+    return getState().indicatePessimisticFixpoint();
+  }
+
+  virtual ChangeStatus updateOperation(OpT op, Solver &solver) = 0;
+};
+
+//===----------------------------------------------------------------------===//
+// ValueElement
+//===----------------------------------------------------------------------===//
+
+// An abstract element that represents a value position in the IR.
+struct ValueElement : public AbstractElement {
+  using AbstractElement::AbstractElement;
+
+  void initialize(Solver &solver) override {
+    if (isValue()) {
+      auto value = getValue();
+      initializeValue(value, solver);
+    } else {
+      getState().indicatePessimisticFixpoint();
+    }
+  }
+
+  virtual void initializeValue(Value value, Solver &solver) = 0;
+
+  ChangeStatus updateImpl(Solver &solver) override {
+    if (isValue()) {
+      auto value = getValue();
+      return updateValue(value, solver);
+    } else {
+      return getState().indicatePessimisticFixpoint();
+    }
+  }
+
+  virtual ChangeStatus updateValue(Value value, Solver &solver) = 0;
+};
+
+}  // namespace DFX
+}  // namespace iree_compiler
+}  // namespace mlir
+
+#endif  // IREE_COMPILER_DIALECT_UTIL_ANALYSIS_DFX_ELEMENT_H_
---
+++ iree/compiler/Dialect/Util/Analysis/DFX/Solver.cpp
@@ -0,0 +1,221 @@
+// Copyright 2021 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "iree/compiler/Dialect/Util/Analysis/DFX/Solver.h"
+
+#include "llvm/ADT/SmallPtrSet.h"
+#include "llvm/Support/Debug.h"
+
+#define DEBUG_TYPE "iree-util-dfx"
+
+namespace mlir {
+namespace iree_compiler {
+namespace DFX {
+
+LogicalResult Solver::run() {
+  LLVM_DEBUG(llvm::dbgs() << "[Solver] identified and initialized "
+                          << depGraph.syntheticRoot.deps.size()
+                          << " abstract elements\n");
+
+  // Now that all abstract elements are collected and initialized we start
+  // the abstract analysis.
+  phase = Phase::UPDATE;
+  auto result = runTillFixpoint();
+  phase = Phase::DONE;
+
+  if (failed(result)) {
+    LLVM_DEBUG(llvm::dbgs()
+               << "[Solver] fixed point iteration failed to converge\n");
+  }
+  return result;
+}
+
+LogicalResult Solver::runTillFixpoint() {
+  unsigned iterationCounter = 1;
+  unsigned maxIterations = maxFixpointIterations.getValueOr(32);
+
+  SmallVector<AbstractElement *, 32> changedElements;
+  SetVector<AbstractElement *> worklist, invalidElements;
+  worklist.insert(depGraph.syntheticRoot.begin(), depGraph.syntheticRoot.end());
+
+  do {
+    // Remember the size to determine newly added elements in this iteration.
+    size_t elementCount = depGraph.syntheticRoot.deps.size();
+    LLVM_DEBUG(llvm::dbgs() << "\n\n[Solver] iteration#: " << iterationCounter
+                            << ", worklist size: " << worklist.size() << "\n");
+
+    // For invalid elements we can fix dependent elements that have a required
+    // dependence thereby folding long dependence chains in a single step
+    // without the need to run updates.
+    for (unsigned i = 0; i < invalidElements.size(); ++i) {
+      auto *invalidElement = invalidElements[i];
+
+      // Check the dependences to fast track invalidation.
+      LLVM_DEBUG(llvm::dbgs() << "[Solver] invalidElement: " << *invalidElement
+                              << " has " << invalidElement->deps.size()
+                              << " required & optional dependences\n");
+      while (!invalidElement->deps.empty()) {
+        const auto &dep = invalidElement->deps.back();
+        invalidElement->deps.pop_back();
+        auto *dependentElement = cast<AbstractElement>(dep.getPointer());
+        if (dep.getInt() == static_cast<unsigned>(Resolution::OPTIONAL)) {
+          worklist.insert(dependentElement);
+          continue;
+        }
+        dependentElement->getState().indicatePessimisticFixpoint();
+        assert(dependentElement->getState().isAtFixpoint() &&
+               "expected fixpoint state");
+        if (!dependentElement->getState().isValidState()) {
+          invalidElements.insert(dependentElement);
+        } else {
+          changedElements.push_back(dependentElement);
+        }
+      }
+    }
+
+    // Add all abstract elements that are potentially dependent on one that
+    // changed to the work list.
+    for (auto *changedElement : changedElements) {
+      while (!changedElement->deps.empty()) {
+        worklist.insert(
+            cast<AbstractElement>(changedElement->deps.back().getPointer()));
+        changedElement->deps.pop_back();
+      }
+    }
+
+    LLVM_DEBUG(llvm::dbgs()
+               << "[Solver] iteration#: " << iterationCounter
+               << ", worklist + dependent size: " << worklist.size() << "\n");
+
+    // Reset the changed and invalid set.
+    changedElements.clear();
+    invalidElements.clear();
+
+    // Update all abstract elements in the work list and record the ones that
+    // changed.
+    for (auto *element : worklist) {
+      const auto &elementState = element->getState();
+      if (!elementState.isAtFixpoint()) {
+        if (updateElement(*element) == ChangeStatus::CHANGED) {
+          changedElements.push_back(element);
+        }
+        LLVM_DEBUG(llvm::dbgs() << "\n");
+      }
+
+      // Use the invalidElements vector to propagate invalid states fast
+      // transitively without requiring updates.
+      if (!elementState.isValidState()) invalidElements.insert(element);
+    }
+
+    // Add elements to the changed set if they have been created in the last
+    // iteration.
+    changedElements.append(depGraph.syntheticRoot.begin() + elementCount,
+                           depGraph.syntheticRoot.end());
+
+    // Reset the work list and repopulate with the changed abstract elements.
+    // Note that dependent ones have already been added above.
+    worklist.clear();
+    worklist.insert(changedElements.begin(), changedElements.end());
+  } while (!worklist.empty() && (iterationCounter++ < maxIterations));
+
+  LLVM_DEBUG(llvm::dbgs() << "\n[Solver] fixpoint iteration done after: "
+                          << iterationCounter << "/" << maxIterations
+                          << " iterations\n");
+
+  // Reset abstract elements not settled in a sound fixpoint by now. This
+  // happens when we stopped the fixpoint iteration early. Note that only the
+  // ones marked as "changed" *and* the ones transitively depending on them
+  // need to be reverted to a pessimistic state. Others might not be in a
+  // fixpoint state but we can use the optimistic results for them anyway.
+  SmallPtrSet<AbstractElement *, 32> visitedElements;
+  for (unsigned i = 0; i < changedElements.size(); i++) {
+    auto *changedElement = changedElements[i];
+    if (!visitedElements.insert(changedElement).second) continue;
+
+    auto &elementState = changedElement->getState();
+    if (!elementState.isAtFixpoint()) {
+      elementState.indicatePessimisticFixpoint();
+    }
+
+    while (!changedElement->deps.empty()) {
+      changedElements.push_back(
+          cast<AbstractElement>(changedElement->deps.back().getPointer()));
+      changedElement->deps.pop_back();
+    }
+  }
+
+  LLVM_DEBUG({
+    if (!visitedElements.empty()) {
+      llvm::dbgs() << "\n[Solver] finalized " << visitedElements.size()
+                   << " abstract elements\n";
+    }
+  });
+
+  return iterationCounter < maxIterations ? success() : failure();
+}
+
+ChangeStatus Solver::updateElement(AbstractElement &element) {
+  assert(phase == Phase::UPDATE &&
+         "can update element only in the update stage");
+
+  // Use a new dependence vector for this update so we can possibly drop them
+  // all if we reach a fixpoint.
+  DependenceVector dependencies;
+  dependenceStack.push_back(&dependencies);
+
+  // Perform the abstract element update.
+  auto &elementState = element.getState();
+  ChangeStatus changeStatus = element.update(*this);
+
+  if (dependencies.empty()) {
+    // If the element did not query any non-fix information the state
+    // will not change and we can indicate that right away.
+    elementState.indicateOptimisticFixpoint();
+  }
+  if (!elementState.isAtFixpoint()) rememberDependences();
+
+  // Verify the stack is balanced by ensuring we pop the vector we pushed above.
+  auto *poppedDependencies = dependenceStack.pop_back_val();
+  (void)poppedDependencies;
+  assert(poppedDependencies == &dependencies &&
+         "inconsistent usage of the dependence stack");
+
+  return changeStatus;
+}
+
+void Solver::recordDependence(const AbstractElement &fromElement,
+                              const AbstractElement &toElement,
+                              Resolution resolution) {
+  if (resolution == Resolution::NONE) return;
+  // If we are outside of an update, thus before the actual fixpoint iteration
+  // started (= when we create elements), we do not track dependences because we
+  // will put all elements into the initial worklist anyway.
+  if (dependenceStack.empty()) return;
+  if (fromElement.getState().isAtFixpoint()) return;
+  dependenceStack.back()->push_back({&fromElement, &toElement, resolution});
+}
+
+void Solver::rememberDependences() {
+  assert(!dependenceStack.empty() && "no dependences to remember");
+  for (auto &depInfo : *dependenceStack.back()) {
+    assert((depInfo.resolution == Resolution::REQUIRED ||
+            depInfo.resolution == Resolution::OPTIONAL) &&
+           "expected required or optional dependence (1 bit)");
+    auto &dependentElements =
+        const_cast<AbstractElement &>(*depInfo.fromElement).deps;
+    dependentElements.push_back(
+        AbstractElement::DepTy(const_cast<AbstractElement *>(depInfo.toElement),
+                               static_cast<unsigned>(depInfo.resolution)));
+  }
+}
+
+void Solver::print(llvm::raw_ostream &os) { depGraph.print(os); }
+
+void Solver::dumpGraph() { depGraph.dumpGraph(); }
+
+}  // namespace DFX
+}  // namespace iree_compiler
+}  // namespace mlir
