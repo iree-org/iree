1d6f7fec8b82fe1ffbdacff52e8e2d314728a528
Stella Laurenzo <stellaraccident@gmail.com>
2021-09-24 23:38:01 +0000

    Bump cmake to 3.16.3.
    * The thing that tipped it was lack of NumPy configuration in 3.13.
    * Picked 3.16.3 because it is deployed with Ubuntu 20.04LTS, which at least gives it a pretty big fanout with respect to use (more as a "hey, this gets used a lot and may be ok" vs "we need to pin to Ubuntu").

--- build_tools/kokoro/gcp_ubuntu/cmake/linux/x86-turing/build_kokoro.sh
+++ build_tools/kokoro/gcp_ubuntu/cmake/linux/x86-turing/build_kokoro.sh
@@ -30,7 +30,7 @@ docker_setup
 
 docker run "${DOCKER_RUN_ARGS[@]?}" \
   --gpus all \
-  gcr.io/iree-oss/cmake-python-nvidia@sha256:90f0e35384f9218300e04c5d10c9385dd593c0dfef04eae0a44a59f6f1ca27bc \
+  gcr.io/iree-oss/cmake-python-nvidia@sha256:c5efaba9a104604c240541ae513b7de876e7406ef97661e5fe6844d19d4d1e69 \
   build_tools/kokoro/gcp_ubuntu/cmake/linux/x86-turing/build.sh
 
 # Kokoro will rsync this entire directory back to the executor orchestrating the
