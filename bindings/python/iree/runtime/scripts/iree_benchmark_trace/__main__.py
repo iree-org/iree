37444644dcdb44fb8d770245177fc268d65cc11c
Stella Laurenzo <stellaraccident@gmail.com>
2021-09-30 13:02:58 -0700

    Add iree-(run|benchmark)-trace console scripts to iree-runtime package. (#7227)
    * Now users of the python script have access to iree-run-trace and iree-benchmark-trace.
    * Going to be used for the new TFLite tests.

---
+++ bindings/python/iree/runtime/scripts/iree_benchmark_trace/__main__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import os
+import subprocess
+import sys
+
+
+def main(args=None):
+  if args is None:
+    args = sys.argv[1:]
+  exe = os.path.join(os.path.dirname(__file__), "..", "..",
+                     "iree-benchmark-trace")
+  return subprocess.call(args=[exe] + args)
+
+
+if __name__ == "__main__":
+  sys.exit(main())
