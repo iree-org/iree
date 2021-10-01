5d7fd45cce49ade15d37d58a90152d193aeb01a5
not-jenni <jennik@google.com>
2021-10-01 12:05:09 -0700

    Address comments

--- scripts/git/prune_old_tags.sh
+++ scripts/git/prune_old_tags.sh
@@ -6,24 +6,48 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-# Deletes the input number of tags and corresponding releases, starting with the
-# oldest tags.
+# Deletes all but the input number of tags and corresponding releases, starting
+# with the oldest tags.
 
-DELETE_TAGS_COUNT="$1"
+KEEP_TAGS_COUNT="$1"
+UPSTREAM_REMOTE="${UPSTREAM_REMOTE:-upstream}"
+DRY_RUN="${DRY_RUN:-0}"
 
-if [[ -z "$DELETE_TAGS_COUNT" ]]; then
-  echo "Must specify the number of tags to delete"
+if [[ -z "$KEEP_TAGS_COUNT" ]]; then
+  echo "Must specify the number of tags to keep"
   exit 1
 fi
 
-TOTAL_TAGS_COUNT=$(git tag | wc -l)
-
-if [[ "$DELETE_TAGS_COUNT" -ge "$TOTAL_TAGS_COUNT" ]]; then
+if [[ "$KEEP_TAGS_COUNT" -le 0 ]]; then
   echo "Cannot delete all remaining tags"
   exit 1
 fi
 
-TAGS=$(git for-each-ref --sort=creatordate --count="$DELETE_TAGS_COUNT" --format '%(refname:short)' refs/tags)
+git fetch --all --tags
+TOTAL_TAGS_COUNT=$(git tag | wc -l)
 
-git tag -d $TAGS
-git push upstream -d $TAGS
+if [[ "$KEEP_TAGS_COUNT" -ge "$TOTAL_TAGS_COUNT" ]]; then
+  echo "Only $TOTAL_TAGS_COUNT exist, so nothing to delete"
+  exit 1
+fi
+
+DELETE_TAGS_COUNT=$((TOTAL_TAGS_COUNT-KEEP_TAGS_COUNT))
+TAGS=($(git for-each-ref --sort=creatordate --count="$DELETE_TAGS_COUNT" --format '%(refname:short)' refs/tags))
+
+# Filter out tags that were not created by snapshot releases.
+for TAG_INDEX in "${!TAGS[@]}"; do
+  if [[ ! ${TAGS[TAG_INDEX]} =~ ^snapshot-* ]]; then
+    unset 'TAGS[TAG_INDEX]'
+  fi
+done
+
+if [[ $DRY_RUN -eq 1 ]]; then
+  echo "Dry run mode: these tags would be deleted"
+  for TAG in ${TAGS[@]}; do
+    echo $TAG
+  done
+  exit 1
+fi
+
+git tag -d ${TAGS[@]}
+git push "${UPSTREAM_REMOTE?}" -d ${TAGS[@]}
