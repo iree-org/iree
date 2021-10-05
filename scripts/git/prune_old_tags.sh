153c27259b0759fcc5fcfce797e4ec1f354462ec
not-jenni <jennik@google.com>
2021-10-05 06:29:30 -0700

    Address comments

--- scripts/git/prune_old_tags.sh
+++ scripts/git/prune_old_tags.sh
@@ -8,10 +8,21 @@
 
 # Deletes all but the input number of tags and corresponding releases, starting
 # with the oldest tags.
+#
+# Delete all but the 100 newest tags from the repository at the UPSTREAM_REMOTE
+# environment variable:
+#   ./prune_old_tags.sh 100
+#
+# List the tags that would be deleted when not performing a dry run:
+#   ./prune_old_tags.sh 100 DRY_RUN
+
+set -e
+set -u
+set -o pipefail
 
 KEEP_TAGS_COUNT="$1"
+DRY_RUN="${2:-}"
 UPSTREAM_REMOTE="${UPSTREAM_REMOTE:-upstream}"
-DRY_RUN="${DRY_RUN:-0}"
 
 if [[ -z "$KEEP_TAGS_COUNT" ]]; then
   echo "Must specify the number of tags to keep"
@@ -34,6 +45,19 @@ fi
 DELETE_TAGS_COUNT=$((TOTAL_TAGS_COUNT-KEEP_TAGS_COUNT))
 TAGS=($(git for-each-ref --sort=creatordate --count="$DELETE_TAGS_COUNT" --format '%(refname:short)' refs/tags))
 
+echo "$TOTAL_TAGS_COUNT tags available"
+echo "$DELETE_TAGS_COUNT tags will be deleted"
+
+if [[ "$DRY_RUN" != "DRY_RUN" ]]; then
+  read -p "Continue? (y/n) " -n 1 -r
+  echo
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+    echo "Exiting"
+    exit 0
+  fi
+fi
+
 # Filter out tags that were not created by snapshot releases.
 for TAG_INDEX in "${!TAGS[@]}"; do
   if [[ ! ${TAGS[TAG_INDEX]} =~ ^snapshot-* ]]; then
@@ -41,12 +65,12 @@ for TAG_INDEX in "${!TAGS[@]}"; do
   fi
 done
 
-if [[ $DRY_RUN -eq 1 ]]; then
-  echo "Dry run mode: these tags would be deleted"
+if [[ $DRY_RUN == "DRY_RUN" ]]; then
+  echo "Dry run mode, these tags would be deleted:"
   for TAG in ${TAGS[@]}; do
     echo $TAG
   done
-  exit 1
+  exit 0
 fi
 
 git tag -d ${TAGS[@]}
