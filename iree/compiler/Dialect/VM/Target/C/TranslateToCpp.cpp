ae861abe03ea0f8154feb76d663f08a558bf31ed
Geoffrey Martin-Noble <gcmn@google.com>
2021-10-19 10:43:54 -0700

    Update TranslateToCpp.cpp

--- iree/compiler/Dialect/VM/Target/C/TranslateToCpp.cpp
+++ iree/compiler/Dialect/VM/Target/C/TranslateToCpp.cpp
@@ -4,6 +4,7 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+// Formated in LLVM style. Avoid reformatting for upcoming upstreaming.
 // clang-format off
 
 #include "iree/compiler/Dialect/VM/Target/C/CppEmitter.h"
