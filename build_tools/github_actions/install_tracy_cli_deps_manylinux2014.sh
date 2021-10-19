31ebe92e50a2a4a00ceb0fa93e045f5542941378
Stella Laurenzo <laurenzo@google.com>
2021-10-18 22:41:59 -0700

    Update build_tools/github_actions/install_tracy_cli_deps_manylinux2014.sh
    Co-authored-by: Scott Todd <scotttodd@google.com>

--- build_tools/github_actions/install_tracy_cli_deps_manylinux2014.sh
+++ build_tools/github_actions/install_tracy_cli_deps_manylinux2014.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Copyright 2021 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 # Installs deps on a manylinux2014 CentOS docker container needed for
 # building Tracy CLI capture tool.
 
