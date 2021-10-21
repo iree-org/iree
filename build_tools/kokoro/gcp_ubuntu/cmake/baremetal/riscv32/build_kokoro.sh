ad8f733a3db8d057d9a5ced0353fd09784ed909a
Geoffrey Martin-Noble <gcmn@google.com>
2021-10-21 09:18:54 -0700

    Some cleanup to docker images (#7406)
    At this point all of our builds use Python. With
    https://github.com/google/iree/pull/7347 the last holdouts are growing a
    dep on yaml to be able to run traces. So I squashed `cmake-python` into
    `cmake` and shortened all the dependents by dropping `python` from their
    name.
    
    Some other cleanups, some of which were unfortunately necessary to be able to rebuild our images. Hooray unreproducible builds!
    1. The rbe-toolchain image was never actually updated to bump the swiftshader commit in https://github.com/google/iree/pull/7117 and
    https://github.com/google/iree/pull/6859, and the newer commits don't
    actually build there so I had to restore the commit in the Dockerfile to
    the one that has actually always been the one the image is using. See
    https://github.com/google/iree/issues/7419.
    2. The place we were fetching the Vulkan SDK from is no longer
    available, so I had to rebundle it and stash it in our GCS. Upgrading
    Vulkan is a huge undertaking that I'm not going to undertake right now.
    3. Deleted the `cmake-bazel-frontends-android` image, which is
    unused.

--- build_tools/kokoro/gcp_ubuntu/cmake/baremetal/riscv32/build_kokoro.sh
+++ build_tools/kokoro/gcp_ubuntu/cmake/baremetal/riscv32/build_kokoro.sh
@@ -24,7 +24,7 @@ source "${KOKORO_ARTIFACTS_DIR?}/github/iree/build_tools/kokoro/gcp_ubuntu/docke
 docker_setup
 
 docker run "${DOCKER_RUN_ARGS[@]?}" \
-  gcr.io/iree-oss/cmake-riscv@sha256:22a3aa7408c68188198f57b0af6bd38f364198cfd7ebade24fa09759445125cb \
+  gcr.io/iree-oss/cmake-riscv@sha256:2b8ade4c8aa47637160994f07134e51c1e68f5422118499f86236e255471b818 \
   build_tools/kokoro/gcp_ubuntu/cmake/baremetal/riscv32/build.sh
 
 # Kokoro will rsync this entire directory back to the executor orchestrating the
