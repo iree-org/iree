f3312c83fdebf6af4468177262f903133c393f5f
Han-Chung Wang <hanchung@google.com>
2021-10-25 14:04:25 -0700

    Dump more IRs for each stage of LLVMCPUTileAndVectorizePass in debug mode. (#7364)

--- iree/compiler/Codegen/LLVMCPU/LLVMCPUTileAndVectorizeLinalgTensorOps.cpp
+++ iree/compiler/Codegen/LLVMCPU/LLVMCPUTileAndVectorizeLinalgTensorOps.cpp
@@ -21,7 +21,7 @@
 #include "mlir/Pass/Pass.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
-#define DEBUG_TYPE "iree-linalg-to-llvm-tile-and-pad-workgroups"
+#define DEBUG_TYPE "iree-llvmcpu-tile-and-vectorize"
 
 namespace mlir {
 namespace iree_compiler {
@@ -73,7 +73,13 @@ void LLVMCPUTileAndVectorizePass::runOnOperation() {
   MLIRContext *context = &getContext();
   auto funcOp = getOperation();
 
-  // First level of tiling patterns {
+  DEBUG_WITH_TYPE(DEBUG_TYPE, {
+    llvm::dbgs() << "\n--- Before LLVMCPUTileAndVectorizePass ---\n";
+    funcOp.print(llvm::dbgs(), OpPrintingFlags().useLocalScope());
+    llvm::dbgs() << "\n\n";
+  });
+
+  // First level of tiling patterns
   {
     OwningRewritePatternList l1patterns(&getContext());
     l1patterns.insert<TileWorkgroups>(
@@ -90,6 +96,12 @@ void LLVMCPUTileAndVectorizePass::runOnOperation() {
     if (failed(applyPatternsAndFoldGreedily(funcOp, std::move(l1patterns)))) {
       return signalPassFailure();
     }
+
+    DEBUG_WITH_TYPE(DEBUG_TYPE, {
+      llvm::dbgs() << "\n--- After first level of tiling patterns ---\n";
+      funcOp.print(llvm::dbgs(), OpPrintingFlags().useLocalScope());
+      llvm::dbgs() << "\n\n";
+    });
   }
 
   // Apply canoncalization
@@ -104,6 +116,12 @@ void LLVMCPUTileAndVectorizePass::runOnOperation() {
             funcOp, std::move(canonicalizationPatterns)))) {
       return signalPassFailure();
     }
+
+    DEBUG_WITH_TYPE(DEBUG_TYPE, {
+      llvm::dbgs() << "\n--- After canonicalization ---\n";
+      funcOp.print(llvm::dbgs(), OpPrintingFlags().useLocalScope());
+      llvm::dbgs() << "\n\n";
+    });
   }
 
   // Second level of tiling patterns{
@@ -123,7 +141,14 @@ void LLVMCPUTileAndVectorizePass::runOnOperation() {
     if (failed(applyPatternsAndFoldGreedily(funcOp, std::move(l2patterns)))) {
       return signalPassFailure();
     }
+
+    DEBUG_WITH_TYPE(DEBUG_TYPE, {
+      llvm::dbgs() << "\n--- After second level of tiling patterns ---\n";
+      funcOp.print(llvm::dbgs(), OpPrintingFlags().useLocalScope());
+      llvm::dbgs() << "\n\n";
+    });
   }
+
   // Apply canoncalization
   {
     OwningRewritePatternList canonicalizationPatterns(&getContext());
@@ -136,6 +161,12 @@ void LLVMCPUTileAndVectorizePass::runOnOperation() {
             funcOp, std::move(canonicalizationPatterns)))) {
       return signalPassFailure();
     }
+
+    DEBUG_WITH_TYPE(DEBUG_TYPE, {
+      llvm::dbgs() << "\n--- After canonicalization ---\n";
+      funcOp.print(llvm::dbgs(), OpPrintingFlags().useLocalScope());
+      llvm::dbgs() << "\n\n";
+    });
   }
 
   if (!lowerToVectors) {
@@ -154,6 +185,12 @@ void LLVMCPUTileAndVectorizePass::runOnOperation() {
             funcOp, std::move(vectorizationPatterns)))) {
       return signalPassFailure();
     }
+
+    DEBUG_WITH_TYPE(DEBUG_TYPE, {
+      llvm::dbgs() << "\n--- After vectorization ---\n";
+      funcOp.print(llvm::dbgs(), OpPrintingFlags().useLocalScope());
+      llvm::dbgs() << "\n\n";
+    });
   }
 
   {
@@ -163,6 +200,14 @@ void LLVMCPUTileAndVectorizePass::runOnOperation() {
                                                        context);
     (void)applyPatternsAndFoldGreedily(funcOp,
                                        std::move(canonicalizationPatterns));
+
+    DEBUG_WITH_TYPE(DEBUG_TYPE, {
+      llvm::dbgs()
+          << "\n--- After folding consumer add ops into contraction op "
+             "iteself ---\n";
+      funcOp.print(llvm::dbgs(), OpPrintingFlags().useLocalScope());
+      llvm::dbgs() << "\n\n";
+    });
   }
 
   // Apply vector specific operation lowering.
@@ -181,6 +226,12 @@ void LLVMCPUTileAndVectorizePass::runOnOperation() {
             funcOp, std::move(vectorContractLoweringPatterns)))) {
       return signalPassFailure();
     }
+
+    DEBUG_WITH_TYPE(DEBUG_TYPE, {
+      llvm::dbgs() << "\n--- After vector specific operatrion lowering ---\n";
+      funcOp.print(llvm::dbgs(), OpPrintingFlags().useLocalScope());
+      llvm::dbgs() << "\n\n";
+    });
   }
 }
 
