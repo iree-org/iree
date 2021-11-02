21291b2c26bb6aad5bae0909b7a4023180dcc1e4
Geoffrey Martin-Noble <gcmn@google.com>
2021-11-02 15:08:46 -0700

    Enable TFLite benchmarks

--- build_tools/cmake/build_android.sh
+++ build_tools/cmake/build_android.sh
@@ -11,8 +11,7 @@
 # Host binaries (e.g. compiler tools) will be built and installed in build-host/
 # Android binaries (e.g. tests) will be built in build-android/.
 
-set -x
-set -e
+set -xeuo pipefail
 
 if [ "$#" -ne 1 ]; then
   echo "usage: $0 <android-abi>"
@@ -21,17 +20,30 @@ fi
 
 ANDROID_ABI=$1
 
-ROOT_DIR=$(git rev-parse --show-toplevel)
-
 CMAKE_BIN=${CMAKE_BIN:-$(which cmake)}
 
-"${CMAKE_BIN?}" --version
+"${CMAKE_BIN}" --version
+"${CC}" --version
+"${CXX}" --version
 ninja --version
+echo "Android NDK path: ${ANDROID_NDK}"
 
-cd ${ROOT_DIR?}
+ROOT_DIR=$(git rev-parse --show-toplevel)
+cd ${ROOT_DIR}
+
+# BUILD the iree-import-tflite binary for importing models to benchmark from
+# TFLite flatbuffers.
+cd "${ROOT_DIR}/integrations/tensorflow"
+BAZEL_CMD=(bazel --noworkspace_rc --bazelrc=build_tools/bazel/iree-tf.bazelrc)
+BAZEL_BINDIR="$(${BAZEL_CMD[@]} info bazel-bin)"
+"${BAZEL_CMD[@]}" build //iree_tf_compiler:iree-import-tflite \
+      --config=generic_clang \
+      --config=remote_cache_tf_integrations
 
 # --------------------------------------------------------------------------- #
 # Build for the host.
+
+cd "${ROOT_DIR}"
 
 if [ -d "build-host" ]
 then
@@ -43,22 +55,23 @@ fi
 cd build-host
 
 # Configure, build, install.
-"${CMAKE_BIN?}" -G Ninja .. \
+"${CMAKE_BIN}" -G Ninja .. \
   -DCMAKE_INSTALL_PREFIX=./install \
   -DIREE_ENABLE_ASSERTIONS=ON \
   -DIREE_BUILD_COMPILER=ON \
   -DIREE_BUILD_TESTS=OFF \
   -DIREE_BUILD_BENCHMARKS=ON \
+  -DIREE_BUILD_TFLITE_COMPILER=ON \
   -DIREE_BUILD_SAMPLES=OFF
-"${CMAKE_BIN?}" --build . --target install
+"${CMAKE_BIN}" --build . --target install
 # Also make sure that we can generate artifacts for benchmarking on Android.
-"${CMAKE_BIN?}" --build . --target iree-benchmark-suites
+"${CMAKE_BIN}" --build . --target iree-benchmark-suites
 # --------------------------------------------------------------------------- #
-
-cd ${ROOT_DIR?}
 
 # --------------------------------------------------------------------------- #
 # Build for the target (Android).
+
+cd ${ROOT_DIR}
 
 if [ -d "build-android" ]
 then
@@ -70,13 +83,13 @@ fi
 cd build-android
 
 # Configure towards 64-bit Android 10, then build.
-"${CMAKE_BIN?}" -G Ninja .. \
-  -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK?}/build/cmake/android.toolchain.cmake \
-  -DANDROID_ABI="${ANDROID_ABI?}" \
+"${CMAKE_BIN}" -G Ninja .. \
+  -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK}/build/cmake/android.toolchain.cmake" \
+  -DANDROID_ABI="${ANDROID_ABI}" \
   -DANDROID_PLATFORM=android-29 \
-  -DIREE_HOST_BINARY_ROOT=$PWD/../build-host/install \
+  -DIREE_HOST_BINARY_ROOT="${PWD}/../build-host/install" \
   -DIREE_ENABLE_ASSERTIONS=ON \
   -DIREE_BUILD_COMPILER=OFF \
   -DIREE_BUILD_TESTS=ON \
   -DIREE_BUILD_SAMPLES=OFF
-"${CMAKE_BIN?}" --build .
+"${CMAKE_BIN}" --build .
