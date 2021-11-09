12d6351c29ca9d74e1bee71159b05d2a839d3c98
thomasraoux <thomasraoux@google.com>
2021-11-09 08:01:31 -0800

    remove InterfaceWorkgroupSizeOp case

--- iree/compiler/Codegen/LLVMGPU/ConvertToLLVM.cpp
+++ iree/compiler/Codegen/LLVMGPU/ConvertToLLVM.cpp
@@ -382,8 +382,6 @@ void populateLowerHALInterfaceOp(RewritePatternSet &patterns) {
   patterns.insert<HALInterfaceWorkgroupOpsConverter<
                       IREE::HAL::InterfaceWorkgroupIDOp, gpu::BlockIdOp>,
                   HALInterfaceWorkgroupOpsConverter<
-                      IREE::HAL::InterfaceWorkgroupSizeOp, gpu::BlockDimOp>,
-                  HALInterfaceWorkgroupOpsConverter<
                       IREE::HAL::InterfaceWorkgroupCountOp, gpu::GridDimOp>>(
       patterns.getContext());
 }
