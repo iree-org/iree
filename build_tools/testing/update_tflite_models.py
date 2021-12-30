18a3e843fe98f206ba1e964d427b96ab62952393
Rob Suderman <rob.suderman@gmail.com>
2021-12-30 11:24:34 -0800

    Updated for first round of comments:
    - added specific file uploading to script
    - added license headers
    - fixed cloud bucket files for input and posenet model
    - added uploading posenet input to cloud bucket

--- build_tools/testing/update_tflite_models.py
+++ build_tools/testing/update_tflite_models.py
@@ -1,3 +1,22 @@
+# Copyright 2021 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# This tool handles mirroring tflite testing files from their source to the
+# the iree-model-artifacts test bucket. This avoids taking dependency on
+# external test files that may change or no longer be available.
+#
+# To update all files:
+#   python update_tflite_models.py --file all
+#
+# To update a specific file:
+#   python update_tflite_models.py --file posenet_i8_input.jpg
+#
+# Note you must have write permission to the iree-model-artifacts GCS bucket
+# with local gcloud authentication.
+
 from absl import app
 from absl import flags
 from google.cloud import storage
@@ -7,12 +26,15 @@ import tempfile
 import urllib
 
 FLAGS = flags.FLAGS
+flags.DEFINE_string('file', '', 'file to update')
 
-flatbuffers = dict({
+file_dict = dict({
     "mobilenet_v1.tflite":
         "https://tfhub.dev/tensorflow/lite-model/mobilenet_v1_1.0_160/1/default/1?lite-format=tflite",
     "posenet_i8.tflite":
         "https://tfhub.dev/google/lite-model/movenet/singlepose/lightning/tflite/int8/4?lite-format=tflite",
+    "posenet_i8_input.jpg":
+        "https://github.com/tensorflow/examples/raw/master/lite/examples/pose_estimation/raspberry_pi/test_data/image3.jpeg"
 })
 
 BUCKET_NAME = "iree-model-artifacts"
@@ -32,7 +54,15 @@ def upload_model(source, destination, tmpfile):
 def main(argv):
   tf = tempfile.NamedTemporaryFile()
 
-  for dst, src in flatbuffers.items():
+  items = file_dict.items()
+
+  if FLAGS.file in file_dict:
+    items = [(FLAGS.file, file_dict[FLAGS.file])]
+  elif FLAGS.file != "all":
+    print('Unknown file to upload: ', "\"" + FLAGS.file + "\"")
+    exit()
+
+  for dst, src in items:
     upload_model(src, dst, tf.name)
 
 
--- integrations/tensorflow/test/tflite_integration/posenet_i8_test.py
+++ integrations/tensorflow/test/tflite_integration/posenet_i8_test.py
@@ -1,3 +1,9 @@
+# Copyright 2020 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 import absl.testing
 import numpy
 import test_util
@@ -5,11 +11,10 @@ import urllib.request
 
 from PIL import Image
 
-model_path = "https://tfhub.dev/google/lite-model/movenet/singlepose/lightning/tflite/int8/4?lite-format=tflite"
+model_path = "https://storage.googleapis.com/iree-model-artifacts/tflite-integration-tests/posenet_i8.tflite"
+model_input = "https://storage.googleapis.com/iree-model-artifacts/tflite-integration-tests/posenet_i8_input.jpg"
 
 
-# Currently failing further in the linalg stack:
-#   Invalid cast from ui8 to f32 TODO: make tfl.cast insert a rescale for ui8
 class PosenetI8Test(test_util.TFLiteModelTest):
 
   def __init__(self, *args, **kwargs):
@@ -30,9 +35,8 @@ class PosenetI8Test(test_util.TFLiteModelTest):
                       atol=25e-3).all())
 
   def generate_inputs(self, input_details):
-    img_path = "https://github.com/tensorflow/examples/raw/master/lite/examples/pose_estimation/raspberry_pi/test_data/image3.jpeg"
     local_path = "/".join([self.workdir, "person.jpg"])
-    urllib.request.urlretrieve(img_path, local_path)
+    urllib.request.urlretrieve(model_input, local_path)
 
     shape = input_details[0]["shape"]
     im = numpy.array(Image.open(local_path).resize((shape[1], shape[2])))
