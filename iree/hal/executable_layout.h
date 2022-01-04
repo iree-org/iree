de91e46ad17c5aa25a396e7aa2dd257b97d964d8
Ben Vanik <benvanik@google.com>
2022-01-04 09:07:40 -0800

    Adding iree_hal_resource_retain/release. (#7994)
    This enables type-generic resource pooling.

--- iree/hal/executable_layout.h
+++ iree/hal/executable_layout.h
@@ -64,11 +64,9 @@ IREE_API_EXPORT void iree_hal_executable_layout_release(
 //===----------------------------------------------------------------------===//
 
 typedef struct iree_hal_executable_layout_vtable_t {
-  // << HAL C porting in progress >>
-  IREE_API_UNSTABLE
-
   void(IREE_API_PTR* destroy)(iree_hal_executable_layout_t* executable_layout);
 } iree_hal_executable_layout_vtable_t;
+IREE_HAL_ASSERT_VTABLE_LAYOUT(iree_hal_executable_layout_vtable_t);
 
 IREE_API_EXPORT void iree_hal_executable_layout_destroy(
     iree_hal_executable_layout_t* executable_layout);
--- iree/hal/resource.h
+++ iree/hal/resource.h
@@ -7,6 +7,7 @@
 #ifndef IREE_HAL_RESOURCE_H_
 #define IREE_HAL_RESOURCE_H_
 
+#include <assert.h>
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -33,6 +34,7 @@ typedef struct iree_hal_resource_t {
   iree_atomic_ref_count_t ref_count;
 
   // Opaque vtable for the resource object.
+  // Must start with iree_hal_resource_vtable_t at offset 0.
   //
   // NOTE: this field may be hidden in the future. Only use this for
   // IREE_HAL_VTABLE_DISPATCH and not equality/direct dereferencing.
@@ -41,10 +43,52 @@ typedef struct iree_hal_resource_t {
   // TODO(benvanik): debug string/logging utilities.
 } iree_hal_resource_t;
 
+// Base vtable for all resources.
+// This provides the base functions required to generically manipulate resources
+// of various types.
+//
+// This must be aliased at offset 0 of all typed vtables:
+//   typedef struct iree_hal_foo_vtable_t {
+//     void(IREE_API_PTR* destroy)(...);
+//     void(IREE_API_PTR* foo_method)(...);
+//   } iree_hal_foo_vtable_t;
+typedef struct iree_hal_resource_vtable_t {
+  // Destroys the resource upon the final reference being released.
+  // The resource pointer must be assumed invalid upon return from the function
+  // (even if in some implementations its returned to a pool and still live).
+  void(IREE_API_PTR* destroy)(iree_hal_resource_t* resource);
+} iree_hal_resource_vtable_t;
+
+// Verifies that the vtable has the right resource sub-vtable.
+#define IREE_HAL_ASSERT_VTABLE_LAYOUT(vtable_type)   \
+  static_assert(offsetof(vtable_type, destroy) == 0, \
+                "iree_hal_resource_vtable_t must be at offset 0");
+
+// Initializes the base resource type.
 static inline void iree_hal_resource_initialize(
     const void* vtable, iree_hal_resource_t* out_resource) {
   iree_atomic_ref_count_init(&out_resource->ref_count);
   out_resource->vtable = vtable;
+}
+
+// Retains a resource for the caller.
+static inline void iree_hal_resource_retain(const void* any_resource) {
+  iree_hal_resource_t* resource = (iree_hal_resource_t*)any_resource;
+  if (IREE_LIKELY(resource)) {
+    iree_atomic_ref_count_inc(&resource->ref_count);
+  }
+}
+
+// Releases a resource and destroys it if there are no more references.
+// This routes through the vtable and can disable optimizations; always prefer
+// to use the type-specific release functions (such as iree_hal_buffer_release)
+// to allow for more optimizations and better compile-time type safety.
+static inline void iree_hal_resource_release(const void* any_resource) {
+  iree_hal_resource_t* resource = (iree_hal_resource_t*)any_resource;
+  if (IREE_LIKELY(resource) &&
+      iree_atomic_ref_count_dec(&resource->ref_count) == 1) {
+    ((iree_hal_resource_vtable_t*)resource->vtable)->destroy(resource);
+  }
 }
 
 // Returns true if the |resource| has the given |vtable| type.
