8199f3b3218a931cd0a165440b0d99472f4697a4
Lei Zhang <antiagainst@google.com>
2022-01-05 20:58:13 -0500

    Adjust benchmark threshold for mobile GPUs (#8019)
    Now we have removed Galaxy S20 and rely on Pixel 6 Pro,
    which gives stable numbers. The threshold should be adjusted.

--- build_tools/benchmarks/common/benchmark_thresholds.py
+++ build_tools/benchmarks/common/benchmark_thresholds.py
@@ -40,21 +40,11 @@ class BenchmarkThreshold:
 # Order matters here: if multiple regexes match a single benchmark, the first
 # match is used.
 BENCHMARK_THRESHOLDS = [
-    # Unstable and noisy GPU benchmarks.
-    BenchmarkThreshold(re.compile(r"^DeepLabV3.*GPU-Mali"), 20,
-                       ThresholdUnit.PERCENTAGE),
-    BenchmarkThreshold(re.compile(r"^MobileNetV3Small.*GPU-Mali"), 30,
-                       ThresholdUnit.PERCENTAGE),
-    BenchmarkThreshold(re.compile(r"^MobileSSD.*GPU-Mali"), 50,
-                       ThresholdUnit.PERCENTAGE),
-    BenchmarkThreshold(re.compile(r"^PoseNet.*GPU-Mali"), 30,
-                       ThresholdUnit.PERCENTAGE),
-
-    # Fast GPU benchmarks that complete around 10ms; using percentage is
-    # not suitable.
-    BenchmarkThreshold(re.compile(r"^MobileNetV2.*kernel-execution.*GPU-Mali"),
-                       1, ThresholdUnit.VALUE_MS),
-    BenchmarkThreshold(re.compile(r"^MobileNetV3Small.*GPU-Adreno"), 1,
+    # Benchmarks that complete around 10ms on GPUs; using percentage is not
+    # suitable anymore.
+    BenchmarkThreshold(re.compile(r"^DeepLabV3.*GPU-Mali"), 1,
+                       ThresholdUnit.VALUE_MS),
+    BenchmarkThreshold(re.compile(r"^MobileNet.*GPU"), 1,
                        ThresholdUnit.VALUE_MS),
 
     # Default threshold for all benchmarks: 5%.
