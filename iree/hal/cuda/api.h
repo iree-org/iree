ed320c54d897d94b2f00e2ae99a760dd567885b8
Ben Vanik <benvanik@google.com>
2022-01-10 11:24:55 -0800

    Adding `--cuda_allow_inline_execution` flag. (#8058)
    This allows command buffers with the
    IREE_HAL_COMMAND_BUFFER_MODE_ALLOW_INLINE_EXECUTION flag set to execute
    directly against a CUDA stream to lower latency.
    
    Disabled by default because this is slower in almost all cases where the
    model actually uses the GPU to do work. simple_embedding_test (a single
    dispatch) for example goes 39.47us -> 39.32us with this enabled, while
    mobilessd (GPU-bound) goes 2.56s -> 2.69s.

--- iree/hal/cuda/api.h
+++ iree/hal/cuda/api.h
@@ -39,6 +39,11 @@ typedef struct iree_hal_cuda_device_params_t {
 
   // Specifies how command buffers are recorded and executed.
   iree_hal_cuda_command_buffer_mode_t command_buffer_mode;
+
+  // Allow executing command buffers against CUDA streams as they are recorded.
+  // Only command buffers produced by the compiler that have the
+  // IREE_HAL_COMMAND_BUFFER_MODE_ALLOW_INLINE_EXECUTION bit set will use this.
+  bool allow_inline_execution;
 } iree_hal_cuda_device_params_t;
 
 // Initializes |out_params| to default values.
