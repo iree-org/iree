899447b3584fd40d2eed353e960e2c8e335c0dc9
Geoffrey Martin-Noble <gcmn@google.com>
2022-01-21 19:58:50 -0800

    Use real lit testing

---
+++ iree/lit.cfg.py
@@ -0,0 +1,28 @@
+# Copyright 2022 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+"""Lit config for IREE."""
+
+# Lint for undefined variables is disabled as config is not defined inside this
+# file, instead config is injected by way of evaluating runlit.cfg.py from
+# runlit.site.cfg.py which in turn is evaluated by lit.py.
+# pylint: disable=undefined-variable
+
+import os
+import tempfile
+
+import lit.formats
+
+config.name = "IREE"
+config.suffixes = [".mlir", ".txt"]
+config.test_format = lit.formats.ShTest(execute_external=True)
+# Forward all IREE environment variables
+config.environment.update(
+    {k: v for k, v in os.environ.items() if k.startswith("IREE_")})
+
+# Use the most preferred temp directory.
+config.test_exec_root = (
+    os.environ.get("TEST_UNDECLARED_OUTPUTS_DIR") or
+    os.environ.get("TEST_TMPDIR") or os.path.join(tempfile.gettempdir(), "lit"))
