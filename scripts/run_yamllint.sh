cf9ee4487891ecf4f18d9bd70bbf08d81c52cb56
Geoffrey Martin-Noble <gcmn@google.com>
2022-01-25 17:28:09 -0800

    Make a few improvements to linting (#8184)
    Use our own script for the yamllint check. This allows us to only
    look at modified files and run it locally as part of lint.sh
    
    Made some misc cleanups to the other bash scripts along the way, like
    making check_tabs.sh not hang forever if there are no modified files
    (grep gets passed an empty list of files and then sits there waiting
    for input).

--- scripts/run_yamllint.sh
+++ scripts/run_yamllint.sh
@@ -1,48 +1,41 @@
 #!/bin/bash
 
-# Copyright 2020 The IREE Authors
+# Copyright 2021 The IREE Authors
 #
 # Licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-# Checks for tabs in files modified vs the specified reference commit (default
-# "main")
+# Runs yamllint on files modified vs the specified reference commit
+# (default "main")
 
-set -o pipefail
+set -uo pipefail
 
 BASE_REF="${1:-main}"
 
+declare -a included_files_patterns=(
+  "\.yaml$"
+  "\.yml$"
+)
+
 declare -a excluded_files_patterns=(
-  "^\.gitmodules$"
   "/third_party/"
   "^third_party/"
-  "*Makefile*"
-  # Symlinks make grep upset
-  "^integrations/tensorflow/iree-dialects$"
 )
 
 # Join on |
+included_files_pattern="$(IFS="|" ; echo "${included_files_patterns[*]?}")"
 excluded_files_pattern="$(IFS="|" ; echo "${excluded_files_patterns[*]?}")"
 
 readarray -t files < <(\
   (git diff --name-only --diff-filter=d "${BASE_REF}" || kill $$) \
+    | grep -E "${included_files_pattern?}" \
     | grep -v -E "${excluded_files_pattern?}")
 
 if (( ${#files[@]} == 0 )); then
+  echo "No Yaml files changed"
   exit 0
-fi;
-
-diff="$(grep --with-filename --line-number --perl-regexp --binary-files=without-match '\t' "${files[@]}")"
-
-grep_exit="$?"
-
-if (( "${grep_exit?}" >= 2 )); then
-  exit "${grep_exit?}"
 fi
 
-if (( "${grep_exit?}" == 0 )); then
-  echo "Changed files include tabs. Please use spaces.";
-  echo "$diff"
-  exit 1;
-fi
+
+yamllint --strict "${files[@]}"
