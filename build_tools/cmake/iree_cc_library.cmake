33439e84a2e3ae2f74037c5bd125f9593edc7e0e
Stella Laurenzo <stellaraccident@gmail.com>
2022-01-31 20:11:47 -0800

    Create unified runtime static library.
    * Reworks iree_cc_library to produce OBJECT libraries that underlie the static/shared libraries.
    * Uses property and generator magic to transitively stitch all object files and in the iree:: namespace for directly archiving together and collects libraries outside of this (i.e. cpuinfo, dl, flatbuffer, etc) to be linked to normally.
    * The result is a static archive of the IREE runtime. This is still not standalone as there are a few external deps. Within CMake, these are linked automatically. Outside, I will install the external libs individually (so they are replaceable) and add them as explicit deps in the package config file.
    * Forks the iree/runtime/demo build config: CMake diverges from bazel because it now builds against the unified library (which is an important test, since otherwise, this would only be used out of tree).
    
    There will be some followup work to install this archive properly and create unified headers. Also will be generating package descriptions and installing necessary dependencies.
    
    Supercedes: #7111
    Progress on: #7026

--- build_tools/cmake/iree_cc_library.cmake
+++ build_tools/cmake/iree_cc_library.cmake
@@ -56,6 +56,7 @@ include(CMakeParseArguments)
 #   DEPS
 #     iree::package::fantastic_lib
 # )
+
 function(iree_cc_library)
   cmake_parse_arguments(
     _RULE
@@ -76,6 +77,7 @@ function(iree_cc_library)
   # Prefix the library with the package name, so we get: iree_package_name.
   iree_package_name(_PACKAGE_NAME)
   set(_NAME "${_PACKAGE_NAME}_${_RULE_NAME}")
+  set(_OBJECTS_NAME ${_NAME}.objects)
 
   # Check if this is a header-only library.
   # Note that as of February 2019, many popular OS's (for example, Ubuntu
@@ -94,18 +96,55 @@ function(iree_cc_library)
   endif()
 
   if(NOT _RULE_IS_INTERFACE)
+    add_library(${_OBJECTS_NAME} OBJECT)
     if(_RULE_SHARED)
-      add_library(${_NAME} SHARED "")
+      add_library(${_NAME} SHARED "$<TARGET_OBJECTS:${_OBJECTS_NAME}>")
     else()
-      add_library(${_NAME} STATIC "")
+      add_library(${_NAME} STATIC "$<TARGET_OBJECTS:${_OBJECTS_NAME}>")
     endif()
 
-    target_sources(${_NAME}
+    # Sources get added to the object library.
+    target_sources(${_OBJECTS_NAME}
       PRIVATE
         ${_RULE_SRCS}
         ${_RULE_TEXTUAL_HDRS}
         ${_RULE_HDRS}
     )
+
+    # Keep track of objects transitively in our special property.
+    set_property(TARGET ${_NAME} PROPERTY
+      INTERFACE_IREE_TRANSITIVE_OBJECTS "$<TARGET_OBJECTS:${_OBJECTS_NAME}>")
+    _iree_cc_library_add_object_deps(${_NAME} ${_RULE_DEPS})
+
+    # We define everything else on the regular rule. However, the object
+    # library needs compiler definition related properties, so we forward them.
+    # We also forward link libraries -- not because the OBJECT libraries do
+    # linking but because they get transitive compile definitions from them.
+    # Yes. This is state of the art.
+    # Note that SYSTEM scope matches here, in the property name and in the
+    # include directories below on the main rule. If ever removing this,
+    # remove it from all places.
+    target_include_directories(${_OBJECTS_NAME} SYSTEM
+      PUBLIC
+        $<TARGET_PROPERTY:${_NAME},INTERFACE_SYSTEM_INCLUDE_DIRECTORIES>
+    )
+    target_include_directories(${_OBJECTS_NAME}
+      PUBLIC
+        $<TARGET_PROPERTY:${_NAME},INTERFACE_INCLUDE_DIRECTORIES>
+    )
+    target_compile_options(${_OBJECTS_NAME}
+      PRIVATE
+        $<TARGET_PROPERTY:${_NAME},COMPILE_OPTIONS>
+    )
+    target_compile_definitions(${_OBJECTS_NAME}
+      PUBLIC
+        $<TARGET_PROPERTY:${_NAME},INTERFACE_COMPILE_DEFINITIONS>
+    )
+    target_link_libraries(${_OBJECTS_NAME}
+      PUBLIC
+        $<TARGET_PROPERTY:${_NAME},INTERFACE_LINK_LIBRARIES>
+    )
+
     target_include_directories(${_NAME} SYSTEM
       PUBLIC
         "$<BUILD_INTERFACE:${IREE_SOURCE_DIR}>"
@@ -139,15 +178,19 @@ function(iree_cc_library)
     # Add all IREE targets to a folder in the IDE for organization.
     if(_RULE_PUBLIC)
       set_property(TARGET ${_NAME} PROPERTY FOLDER ${IREE_IDE_FOLDER})
+      set_property(TARGET ${_OBJECTS_NAME} PROPERTY FOLDER ${IREE_IDE_FOLDER})
     elseif(_RULE_TESTONLY)
       set_property(TARGET ${_NAME} PROPERTY FOLDER ${IREE_IDE_FOLDER}/test)
+      set_property(TARGET ${_OBJECTS_NAME} PROPERTY FOLDER ${IREE_IDE_FOLDER}/test)
     else()
       set_property(TARGET ${_NAME} PROPERTY FOLDER ${IREE_IDE_FOLDER}/internal)
+      set_property(TARGET ${_OBJECTS_NAME} PROPERTY FOLDER ${IREE_IDE_FOLDER}/internal)
     endif()
 
-    # INTERFACE libraries can't have the CXX_STANDARD property set.
-    set_property(TARGET ${_NAME} PROPERTY CXX_STANDARD ${IREE_CXX_STANDARD})
-    set_property(TARGET ${_NAME} PROPERTY CXX_STANDARD_REQUIRED ON)
+    # INTERFACE libraries can't have the CXX_STANDARD property set so only
+    # set here.
+    set_property(TARGET ${_OBJECTS_NAME} PROPERTY CXX_STANDARD ${IREE_CXX_STANDARD})
+    set_property(TARGET ${_OBJECTS_NAME} PROPERTY CXX_STANDARD_REQUIRED ON)
   else()
     # Generating header-only library.
     add_library(${_NAME} INTERFACE)
@@ -165,12 +208,140 @@ function(iree_cc_library)
       INTERFACE
         ${_RULE_DEPS}
     )
+    _iree_cc_library_add_object_deps(${_NAME} ${_RULE_DEPS})
     iree_add_data_dependencies(NAME ${_NAME} DATA ${_RULE_DATA})
     target_compile_definitions(${_NAME}
       INTERFACE
         ${_RULE_DEFINES}
     )
   endif()
+
+  # Alias the iree_package_name library to iree::package::name.
+  # This lets us more clearly map to Bazel and makes it possible to
+  # disambiguate the underscores in paths vs. the separators.
+  add_library(${_PACKAGE_NS}::${_RULE_NAME} ALIAS ${_NAME})
+
+  # If the library name matches the final component of the package then treat
+  # it as a default. For example, foo/bar/ library 'bar' would end up as
+  # 'foo::bar'.
+  iree_package_dir(_PACKAGE_DIR)
+  if(${_RULE_NAME} STREQUAL ${_PACKAGE_DIR})
+    add_library(${_PACKAGE_NS} ALIAS ${_NAME})
+  endif()
+endfunction()
+
+# _iree_cc_library_add_object_deps()
+#
+# Helper to add deps to an iree_cc_library. This only operates on the unaliased
+# raw name (i.e. 'iree_vm_vm'), not aliased names (i.e. 'iree::vm').
+#
+# This appends to two properties:
+#   INTERFACE_IREE_TRANSITIVE_OBJECTS: Transitive list of all objects from
+#     this library and all "iree::" prefixed dependent libraries. This will
+#     allow you to create mondo objects for any transtive libraries that are
+#     part of IREE, but it will not contain outside.
+#   INTERFACE_IREE_TRANSITIVE_OBJECT_LIBS: Transitive list of any dependency
+#     targets that are not under teh "iree::" namespace but are encountered
+#     in the dependency dag.
+function(_iree_cc_library_add_object_deps name)
+  foreach(dep_target ${ARGN})
+    if(dep_target MATCHES "^iree::")
+      set_property(TARGET ${name} APPEND PROPERTY
+        INTERFACE_IREE_TRANSITIVE_OBJECTS
+        "$<GENEX_EVAL:$<TARGET_PROPERTY:${dep_target},INTERFACE_IREE_TRANSITIVE_OBJECTS>>"
+      )
+      set_property(TARGET ${name} APPEND PROPERTY
+      INTERFACE_IREE_TRANSITIVE_OBJECT_LIBS
+        "$<GENEX_EVAL:$<TARGET_PROPERTY:${dep_target},INTERFACE_IREE_TRANSITIVE_OBJECT_LIBS>>"
+      )
+    else()
+      set_property(TARGET ${name} APPEND PROPERTY
+        INTERFACE_IREE_TRANSITIVE_OBJECT_LIBS
+        ${dep_target}
+      )
+    endif()
+  endforeach()
+endfunction()
+
+# iree_cc_unified_library()
+#
+# Creates a unified library out of the iree:: namespaced transitive deps+self
+# of some ROOT library. The resulting library will contain the union of all
+# objects from all transitive library-deps in the iree:: namespace. Such
+# libraries are typically the only libraries that we install for outside use
+# and they must only be used by leaf demos or out of tree libraries/executables.
+# Commingling with any regular libraries will result in duplicate symbols.
+#
+# Note that the resulting library will not contain any libraries outside of the
+# iree:: namespace but will be configured to link to them. For external use
+# it is expected that they will be installed and used separately as needed.
+#
+# Compile and link options are forwarded from the ROOT target non-transitively.
+# Ensure that this target directly references all definitions that need to
+# be exported to end consumers.
+#
+# Parameters:
+# NAME: name of target
+# ROOT: Root target library to extract objects and deps from.
+function(iree_cc_unified_library)
+  cmake_parse_arguments(
+    _RULE
+    "SHARED"
+    "NAME;ROOT"
+    ""
+    ${ARGN}
+  )
+
+  # Replace dependencies passed by ::name with iree::package::name
+  iree_package_ns(_PACKAGE_NS)
+  list(TRANSFORM _RULE_ROOT REPLACE "^::" "${_PACKAGE_NS}::")
+
+  # Prefix the library with the package name, so we get: iree_package_name.
+  iree_package_name(_PACKAGE_NAME)
+  set(_NAME "${_PACKAGE_NAME}_${_RULE_NAME}")
+
+  # Evaluate the object and libs.
+  set(_objects "$<REMOVE_DUPLICATES:$<GENEX_EVAL:$<TARGET_PROPERTY:${_RULE_ROOT},INTERFACE_IREE_TRANSITIVE_OBJECTS>>>")
+  set(_libs "$<REMOVE_DUPLICATES:$<GENEX_EVAL:$<TARGET_PROPERTY:${_RULE_ROOT},INTERFACE_IREE_TRANSITIVE_OBJECT_LIBS>>>")
+
+  # For debugging, write out evaluated objects to a file.
+  file(GENERATE OUTPUT "${_RULE_NAME}.contents.txt" CONTENT
+    "OBJECTS:\n${_objects}\n\nLIBS:\n${_libs}\n")
+  if(_RULE_SHARED)
+    add_library(${_NAME} SHARED ${_objects})
+  else()
+    add_library(${_NAME} STATIC ${_objects})
+  endif()
+
+  target_link_libraries(${_NAME}
+    PUBLIC
+      ${_libs}
+  )
+
+  # Forward compile usage requirements from the root library.
+  # Note that SYSTEM scope matches here, in the property name and in the
+  # include directories below on the main rule. If ever removing this,
+  # remove it from all places.
+  target_include_directories(${_NAME} SYSTEM
+    PUBLIC
+      $<TARGET_PROPERTY:${_RULE_ROOT},INTERFACE_SYSTEM_INCLUDE_DIRECTORIES>
+  )
+  target_include_directories(${_NAME}
+    PUBLIC
+      $<TARGET_PROPERTY:${_RULE_ROOT},INTERFACE_INCLUDE_DIRECTORIES>
+  )
+  target_compile_options(${_NAME}
+    PRIVATE
+      $<TARGET_PROPERTY:${_RULE_ROOT},COMPILE_OPTIONS>
+  )
+  target_compile_definitions(${_NAME}
+    PUBLIC
+      $<TARGET_PROPERTY:${_RULE_ROOT},INTERFACE_COMPILE_DEFINITIONS>
+  )
+  target_link_libraries(${_NAME}
+    PUBLIC
+      $<TARGET_PROPERTY:${_RULE_ROOT},INTERFACE_LINK_LIBRARIES>
+  )
 
   # Alias the iree_package_name library to iree::package::name.
   # This lets us more clearly map to Bazel and makes it possible to
