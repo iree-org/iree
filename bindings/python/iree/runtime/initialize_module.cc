e1e97a8fd917187b16f61b3309b0bca1089228d2
Stella Laurenzo <stellaraccident@gmail.com>
2022-02-07 19:24:37 -0800

    Add python-only and native flag parsing to the Python API.
    * Exposes one python flag `iree.runtime.flags.FUNCTION_INPUT_VALIDATION` (default True), which can be used to disable some expensive validation checks.
    * Adds `iree.runtime.flags.parse_flags("--flag1", "--flag2")` to set native flags from Python.

--- bindings/python/iree/runtime/initialize_module.cc
+++ bindings/python/iree/runtime/initialize_module.cc
@@ -8,6 +8,7 @@
 #include "bindings/python/iree/runtime/hal.h"
 #include "bindings/python/iree/runtime/status_utils.h"
 #include "bindings/python/iree/runtime/vm.h"
+#include "iree/base/internal/flags.h"
 #include "iree/base/status_cc.h"
 #include "iree/hal/drivers/init.h"
 
@@ -21,6 +22,26 @@ PYBIND11_MODULE(binding, m) {
   m.doc() = "IREE Binding Backend Helpers";
   SetupHalBindings(m);
   SetupVmBindings(m);
+
+  m.def("parse_flags", [](py::args py_flags) {
+    std::vector<std::string> alloced_flags;
+    alloced_flags.push_back("python");
+    for (auto &py_flag : py_flags) {
+      alloced_flags.push_back(py::cast<std::string>(py_flag));
+    }
+
+    // Must build pointer vector after filling so pointers are stable.
+    std::vector<char *> flag_ptrs;
+    for (auto &alloced_flag : alloced_flags) {
+      flag_ptrs.push_back(const_cast<char *>(alloced_flag.c_str()));
+    }
+
+    char **argv = &flag_ptrs[0];
+    int argc = flag_ptrs.size();
+    CheckApiStatus(
+        iree_flags_parse(IREE_FLAGS_PARSE_MODE_DEFAULT, &argc, &argv),
+        "Error parsing flags");
+  });
 }
 
 }  // namespace python
