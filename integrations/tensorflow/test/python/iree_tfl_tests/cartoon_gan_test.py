840940ba08e30cb25f629c40183823ce6840ae3b
not-jenni <jennik@google.com>
2022-02-08 08:11:38 -0800

    Add tflite tests to run on presubmits

---
+++ integrations/tensorflow/test/python/iree_tfl_tests/cartoon_gan_test.py
@@ -0,0 +1,20 @@
+# Copyright 2022 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import absl.testing
+import iree.tflite.support.test_util as test_util
+
+model_path = "https://tfhub.dev/sayakpaul/lite-model/cartoongan/dr/1?lite-format=tflite"
+
+class CartoonGanTest(test_util.TFLiteModelTest):
+  def __init__(self, *args, **kwargs):
+    super(CartoonGanTest, self).__init__(model_path, *args, **kwargs)
+
+  def test_compile_tflite(self):
+    self.compile_and_execute()
+
+if __name__ == '__main__':
+  absl.testing.absltest.main()
