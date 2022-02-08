529671ef5abcbdbd0f0a6b60789f6c47af025e7e
Benoit Jacob <benoitjacob@google.com>
2022-02-08 03:44:18 +0000

    clangformat

--- iree/compiler/Codegen/LLVMCPU/VectorContractCustomKernels.cpp
+++ iree/compiler/Codegen/LLVMCPU/VectorContractCustomKernels.cpp
@@ -192,7 +192,7 @@ int32_t operator*(int32_t i, PowerOfTwo p) {
   assert(i >= 0 && "only nonnegative values are supported");
   uint32_t u = i;
   assert(llvm::countLeadingZeros(u) > static_cast<unsigned>(p.getExponent()));
-  (void) u;
+  (void)u;
   return i << p.getExponent();
 }
 
