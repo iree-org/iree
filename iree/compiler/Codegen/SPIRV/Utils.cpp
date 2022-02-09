a4c1b2cc0285a76c03ebb2f61c9a69cd1ba62120
Lei Zhang <antiagainst@google.com>
2022-02-09 11:38:57 -0500

    [spirv] NFC: remove unused code in Utils.cpp (#8277)

--- iree/compiler/Codegen/SPIRV/Utils.cpp
+++ iree/compiler/Codegen/SPIRV/Utils.cpp
@@ -12,17 +12,11 @@
 
 #include "iree/compiler/Codegen/SPIRV/Utils.h"
 
-#include "iree/compiler/Codegen/SPIRV/MemorySpace.h"
-#include "iree/compiler/Codegen/Utils/MarkerUtils.h"
 #include "iree/compiler/Dialect/HAL/IR/HALOps.h"
 #include "mlir/Dialect/GPU/GPUDialect.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
 #include "mlir/Dialect/Linalg/Utils/Utils.h"
-#include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Dialect/SPIRV/IR/TargetAndABI.h"
-#include "mlir/IR/BuiltinOps.h"
-#include "mlir/IR/Matchers.h"
-#include "mlir/IR/Region.h"
 #include "mlir/Support/LogicalResult.h"
 
 namespace mlir {
@@ -40,25 +34,6 @@ spirv::TargetEnvAttr getSPIRVTargetEnvAttr(Operation *op) {
   return config.getAs<spirv::TargetEnvAttr>(spirv::getTargetEnvAttrName());
 }
 
-LogicalResult updateWorkGroupSize(FuncOp funcOp,
-                                  ArrayRef<int64_t> workGroupSize) {
-  // Need to update both the surrounding FuncOp that has the spv.entry_point_abi
-  // attribute, and the hal.executable.
-  Region &body = funcOp.getBody();
-  if (!llvm::hasSingleElement(body))
-    return funcOp.emitError("unhandled dispatch function with multiple blocks");
-
-  if (workGroupSize.size() != 3)
-    return funcOp.emitError("expected workgroup size to have three entries");
-  SmallVector<int32_t, 3> workGroupSizeVec = llvm::to_vector<3>(llvm::map_range(
-      workGroupSize, [](int64_t v) { return static_cast<int32_t>(v); }));
-
-  funcOp->setAttr(
-      spirv::getEntryPointABIAttrName(),
-      spirv::getEntryPointABIAttr(workGroupSizeVec, funcOp.getContext()));
-  return success();
-}
-
 template <typename GPUIdOp, typename GPUCountOp>
 static linalg::ProcInfo getGPUProcessorIdAndCountImpl(OpBuilder &builder,
                                                       Location loc,
@@ -70,31 +45,6 @@ static linalg::ProcInfo getGPUProcessorIdAndCountImpl(OpBuilder &builder,
   Type indexType = builder.getIndexType();
   return {builder.create<GPUIdOp>(loc, indexType, dimAttr[dim]),
           builder.create<GPUCountOp>(loc, indexType, dimAttr[dim])};
-}
-
-template <>
-linalg::ProcInfo getGPUProcessorIdAndCountImpl<GPUGlobalId, GPUGlobalCount>(
-    OpBuilder &builder, Location loc, unsigned dim) {
-  assert(dim < kNumGPUDims && "processor index out of range!");
-
-  std::array<gpu::Dimension, kNumGPUDims> dimAttr{
-      gpu::Dimension::x, gpu::Dimension::y, gpu::Dimension::z};
-  Type indexType = builder.getIndexType();
-  Value gridDim = builder.create<gpu::GridDimOp>(loc, indexType, dimAttr[dim]);
-  Value blockId = builder.create<gpu::BlockIdOp>(loc, indexType, dimAttr[dim]);
-  Value blockDim =
-      builder.create<gpu::BlockDimOp>(loc, indexType, dimAttr[dim]);
-  Value threadId =
-      builder.create<gpu::ThreadIdOp>(loc, indexType, dimAttr[dim]);
-  // TODO(ravishankarm): Using affine_maps here would be beneficial, and we can
-  // do this because the blockDim is constant. But this would lead to an
-  // ordering issue cause it assumes that the workgroup size has already been
-  // set. If using affine_map can help, make sure that the workgroup size is set
-  // before.
-  return {
-      builder.create<arith::AddIOp>(
-          loc, builder.create<arith::MulIOp>(loc, blockId, blockDim), threadId),
-      builder.create<arith::MulIOp>(loc, blockDim, gridDim)};
 }
 
 template <typename GPUIdOp, typename GPUCountOp>
@@ -118,214 +68,8 @@ SmallVector<linalg::ProcInfo, 2> getGPUProcessorIdsAndCounts(OpBuilder &builder,
 
 /// Explicit instantiation of gpuGPUProcessorIdsAndCounts.
 template SmallVector<linalg::ProcInfo, 2>
-getGPUProcessorIdsAndCounts<gpu::BlockIdOp, gpu::GridDimOp>(OpBuilder &builder,
-                                                            Location loc,
-                                                            unsigned numDims);
-template SmallVector<linalg::ProcInfo, 2>
 getGPUProcessorIdsAndCounts<gpu::ThreadIdOp, gpu::BlockDimOp>(
     OpBuilder &builder, Location loc, unsigned numDims);
-template SmallVector<linalg::ProcInfo, 2>
-getGPUProcessorIdsAndCounts<GPUGlobalId, GPUGlobalCount>(OpBuilder &builder,
-                                                         Location loc,
-                                                         unsigned numDims);
-
-scf::ParallelOp collapseParallelLoops(PatternRewriter &rewriter,
-                                      scf::ParallelOp pLoopOp) {
-  if (pLoopOp.getNumReductions()) return nullptr;
-
-  unsigned numLoops = pLoopOp.getNumLoops();
-  if (numLoops == 1) return pLoopOp;
-
-  // Compute the number of iterations of each loops starting from the innermost.
-  Location loc = pLoopOp.getLoc();
-  Value totalNumIterations = rewriter.create<arith::ConstantIndexOp>(loc, 1);
-
-  // Track the "stride" of each loop, i.e. product of the total number of
-  // iterations of the inner loops.
-  SmallVector<Value, 2> iterationStride;
-  iterationStride.resize(pLoopOp.getNumLoops());
-  auto lbs = pLoopOp.getLowerBound();
-  auto ubs = pLoopOp.getUpperBound();
-  auto steps = pLoopOp.getStep();
-  for (int i = numLoops - 1; i >= 0; --i) {
-    Value lb = lbs[i], ub = ubs[i], step = steps[i];
-    Value iterCount = rewriter.create<arith::DivSIOp>(
-        loc, rewriter.create<arith::SubIOp>(loc, ub, lb), step);
-    iterationStride[i] = totalNumIterations;
-    totalNumIterations =
-        rewriter.create<arith::MulIOp>(loc, totalNumIterations, iterCount);
-  }
-
-  // Create the collapsed parallel loop op with lowerbound 0, step 1 and upper
-  // bound being the totalNumIterations.
-  Value newLb = rewriter.create<arith::ConstantIndexOp>(loc, 0);
-  Value newStep = rewriter.create<arith::ConstantIndexOp>(loc, 1);
-  scf::ParallelOp newPLoopOp =
-      rewriter.create<scf::ParallelOp>(loc, newLb, totalNumIterations, newStep);
-
-  // Build the body of the collapsed loop by cloning the original loop body. The
-  // replacement value of the induction variables of the original loop body,
-  // from the induction variable of the new loop, using
-  //   origLoopIv[i] = loopIv / iterationStride[i]
-  //   loopIv = loopIv % iterationStride[i]
-  OpBuilder::InsertionGuard guard(rewriter);
-  Block &pLoopBody = pLoopOp.getLoopBody().front();
-  rewriter.setInsertionPointToStart(&newPLoopOp.getLoopBody().front());
-  Value loopIv = *newPLoopOp.getInductionVars().begin();
-  BlockAndValueMapping map;
-  for (int i : llvm::seq<int>(0, numLoops)) {
-    Value iterNum =
-        rewriter.create<arith::DivSIOp>(loc, loopIv, iterationStride[i]);
-    AffineExpr d0, d1;
-    bindDims(rewriter.getContext(), d0, d1);
-    AffineExpr s0 = getAffineSymbolExpr(0, rewriter.getContext());
-    Value newIv = makeComposedAffineApply(rewriter, loc, d0 + d1 * s0,
-                                          {lbs[i], iterNum, steps[i]});
-    map.map(pLoopBody.getArgument(i), newIv);
-    loopIv = rewriter.create<arith::RemSIOp>(loc, loopIv, iterationStride[i]);
-  }
-  for (Operation &op : pLoopBody.without_terminator()) {
-    rewriter.clone(op, map);
-  }
-  rewriter.eraseOp(pLoopOp);
-  return newPLoopOp;
-}
-
-/// Builds an empty scf.for operation. The default builder adds an entry basic
-/// block which needs to be avoided here.
-static scf::ForOp buildEmptyForOp(Location loc, OpBuilder &builder, Value lb,
-                                  Value ub, Value step) {
-  OperationState state(loc, scf::ForOp::getOperationName());
-  state.addOperands({lb, ub, step});
-  state.addRegion();
-  return cast<scf::ForOp>(builder.createOperation(state));
-}
-
-Operation *replacePLoopOp(ConversionPatternRewriter &rewriter,
-                          scf::ParallelOp pLoopOp,
-                          ArrayRef<LoopBounds> newPLoopBounds,
-                          ArrayRef<LoopBounds> forBounds,
-                          ArrayRef<unsigned> permutation) {
-  assert(!forBounds.empty() && "unhandled case of no scf.for created");
-  unsigned numLoops = pLoopOp.getNumLoops();
-  Location loc = pLoopOp.getLoc();
-  assert(forBounds.size() + newPLoopBounds.size() == numLoops &&
-         "cannot drop loops when splitting scf.parallel operation");
-  assert(permutation.size() == numLoops);
-  OpBuilder::InsertionGuard guard(rewriter);
-
-  // Need a signature conversion for the body of the scf.parallel operation,
-  // before can it can be used as the body of the innermost loop created here.
-  TypeConverter::SignatureConversion signatureConverter(numLoops);
-  Operation *outermostLoop = nullptr;
-  auto permuteIt = permutation.begin();
-
-  // Create the scf.parallel operation as the outermost loop, if specified.
-  if (!newPLoopBounds.empty()) {
-    auto lbs = llvm::to_vector<2>(llvm::map_range(
-        newPLoopBounds, [](LoopBounds bounds) -> Value { return bounds.lb; }));
-    auto ubs = llvm::to_vector<2>(llvm::map_range(
-        newPLoopBounds, [](LoopBounds bounds) { return bounds.ub; }));
-    auto steps = llvm::to_vector<2>(llvm::map_range(
-        newPLoopBounds, [](LoopBounds bounds) { return bounds.step; }));
-    auto newPLoop = rewriter.create<scf::ParallelOp>(loc, lbs, ubs, steps);
-    for (auto iv : newPLoop.getInductionVars()) {
-      signatureConverter.remapInput(*permuteIt, iv);
-      permuteIt++;
-    }
-    rewriter.setInsertionPointToStart(newPLoop.getBody());
-    outermostLoop = newPLoop.getOperation();
-  }
-
-  // Generate the nested scf.for operations with the bounds passed.
-  for (auto it : enumerate(forBounds)) {
-    Value lb = it.value().lb, ub = it.value().ub, step = it.value().step;
-    if (it.index() != forBounds.size() - 1) {
-      auto forOp = rewriter.create<scf::ForOp>(loc, lb, ub, step);
-      if (!outermostLoop) outermostLoop = forOp.getOperation();
-      signatureConverter.remapInput(*permuteIt, forOp.getInductionVar());
-      rewriter.setInsertionPointToStart(forOp.getBody());
-    } else {
-      // For the last loop, move the body of the scf.parallel op as the body of
-      // the loop after signature conversion.
-      auto forOp = buildEmptyForOp(loc, rewriter, lb, ub, step);
-      if (!outermostLoop) outermostLoop = forOp.getOperation();
-      signatureConverter.addInputs(*permuteIt, rewriter.getIndexType());
-      Region &pLoopOpRegion = pLoopOp.getLoopBody();
-      rewriter.applySignatureConversion(&pLoopOpRegion, signatureConverter);
-      Region &forOpRegion = forOp.getLoopBody();
-      rewriter.inlineRegionBefore(pLoopOpRegion, forOpRegion,
-                                  forOpRegion.begin());
-    }
-    permuteIt++;
-  }
-  rewriter.eraseOp(pLoopOp);
-  return outermostLoop;
-}
-
-/// Builds an empty scf.if operation without the then and else blocks.
-static scf::IfOp buildEmptyIfOp(Location loc, OpBuilder &builder, Value cond) {
-  OperationState state(loc, scf::IfOp::getOperationName());
-  state.addOperands(cond);
-  state.addRegion();
-  state.addRegion();
-  return cast<scf::IfOp>(builder.createOperation(state));
-}
-
-LogicalResult distributeSingleIterationPerProcessor(
-    ConversionPatternRewriter &rewriter, scf::ParallelOp pLoopOp,
-    ArrayRef<linalg::ProcInfo> procInfo, bool generateGuard) {
-  unsigned numLoops = pLoopOp.getNumLoops();
-  Location loc = pLoopOp.getLoc();
-  assert(numLoops == procInfo.size() &&
-         "expected as many ids as number of loops");
-
-  auto lbs = pLoopOp.getLowerBound();
-  auto step = pLoopOp.getStep();
-  SmallVector<Value, 2> ivReplacements;
-  for (unsigned i : llvm::seq<unsigned>(0, numLoops)) {
-    Value iterValue = rewriter.create<arith::AddIOp>(
-        loc, lbs[i],
-        rewriter.create<arith::MulIOp>(loc, procInfo[i].procId, step[i]));
-    ivReplacements.push_back(iterValue);
-  }
-  Region &pLoopOpRegion = pLoopOp.getLoopBody();
-
-  if (generateGuard) {
-    TypeConverter::SignatureConversion signatureConverter(numLoops);
-    Value cond = nullptr;
-    auto ubs = pLoopOp.getUpperBound();
-    for (unsigned i : llvm::seq<unsigned>(0, numLoops)) {
-      Value cmp = rewriter.create<arith::CmpIOp>(loc, arith::CmpIPredicate::slt,
-                                                 ivReplacements[i], ubs[i]);
-      cond = (cond ? rewriter.create<arith::AndIOp>(loc, cond, cmp) : cmp);
-      signatureConverter.remapInput(i, ivReplacements[i]);
-    }
-    rewriter.applySignatureConversion(&pLoopOpRegion, signatureConverter);
-    scf::IfOp ifOp = buildEmptyIfOp(loc, rewriter, cond);
-    Region &ifOpRegion = ifOp.getRegion(0);
-    rewriter.inlineRegionBefore(pLoopOpRegion, ifOpRegion, ifOpRegion.begin());
-  } else {
-    // The body of the scf.parallel needs to be moved into its parent
-    // operation.
-    // - Split the block just before the scf.parallel operation.
-    // - Move the only block of scf.parallel before the newly created block
-    //   (after signature conversion).
-    // - Add branch from the original block to the moved block of the
-    //   scf.parallel's region, and from the latter to the block created by the
-    //   split operation.
-    // - Canonicalization will fold these branches away.
-    Block *destBlock = pLoopOp.getOperation()->getBlock();
-    Block *remainingInst =
-        rewriter.splitBlock(destBlock, Block::iterator(pLoopOp));
-    Block *sourceBlock = &pLoopOpRegion.front();
-    rewriter.eraseOp(sourceBlock->getTerminator());
-    rewriter.mergeBlocks(&pLoopOpRegion.front(), destBlock, ivReplacements);
-    rewriter.mergeBlocks(remainingInst, destBlock, {});
-  }
-  rewriter.eraseOp(pLoopOp);
-  return success();
-}
 
 }  // namespace iree_compiler
 }  // namespace mlir
--- iree/compiler/Codegen/SPIRV/Utils.h
+++ iree/compiler/Codegen/SPIRV/Utils.h
@@ -14,25 +14,13 @@
 #define IREE_COMPILER_CODEGEN_SPIRV_UTILS_H_
 
 #include "mlir/Dialect/Linalg/Utils/Utils.h"
-#include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Dialect/SPIRV/IR/SPIRVAttributes.h"
-#include "mlir/Dialect/StandardOps/IR/Ops.h"
 #include "mlir/IR/Builders.h"
-#include "mlir/IR/BuiltinOps.h"
-#include "mlir/IR/Value.h"
-#include "mlir/Support/LLVM.h"
-#include "mlir/Support/LogicalResult.h"
-#include "mlir/Transforms/DialectConversion.h"
-#include "mlir/Transforms/FoldUtils.h"
 
 namespace mlir {
 namespace iree_compiler {
 
 static constexpr int kNumGPUDims = 3;
-
-//===----------------------------------------------------------------------===//
-// Attribute utils
-//===----------------------------------------------------------------------===//
 
 /// Given an operation, return the `spv.target_env` attribute.
 spirv::TargetEnvAttr getSPIRVTargetEnvAttr(Operation *op);
@@ -40,68 +28,12 @@ spirv::TargetEnvAttr getSPIRVTargetEnvAttr(Operation *op);
 /// Returns the attribute name carrying information about distribution.
 const char *getSPIRVDistributeAttrName();
 
-//===----------------------------------------------------------------------===//
-// Processor ID/size utils
-//===----------------------------------------------------------------------===//
-
 /// Generate the operations that compute the processor ID and number of
 /// processors. Used as the callback needed for LinalgDistributionOptions.
-class GPUGlobalId;
-class GPUGlobalCount;
 template <typename GPUIdOp, typename GPUCountOp>
 SmallVector<linalg::ProcInfo, 2> getGPUProcessorIdsAndCounts(OpBuilder &builder,
                                                              Location loc,
                                                              unsigned numDims);
-/// Updates the workgroup size used for the dispatch region.
-LogicalResult updateWorkGroupSize(FuncOp funcOp,
-                                  ArrayRef<int64_t> workGroupSize);
-
-//===----------------------------------------------------------------------===//
-// Loop utils
-//===----------------------------------------------------------------------===//
-
-/// Collapses all loops in a scf.parallel into one scf.parallel operation. This
-/// is done by
-/// 1) Normalize the loop bounds to be [0, (ub - lb) / step)
-/// 2) Compute the total number of iterations.
-/// 3) From the induction variable of the modified loop, compute the values of
-///    the original induction variables by de-linearization.
-scf::ParallelOp collapseParallelLoops(PatternRewriter &rewriter,
-                                      scf::ParallelOp pLoopOp);
-
-struct LoopBounds {
-  Value lb;
-  Value ub;
-  Value step;
-};
-
-/// Replaces a scf.parallelOp with an optional scf.parallel op and nested
-/// scf.for operations. To create the scf.parallel op as the outermost loop,
-/// pass the lower bound, upper bound and steps in `newPLoopLbs`, `newPLoopUbs`,
-/// and `newPLoopStep` respectively. The bounds of the inner scf.for operations
-/// to be created are passed in `forLbs`, `forUbs`, and `forStep`. The
-/// `permutation` vector contains a mapping from the original loop order, to the
-/// loop order to be generated.
-Operation *replacePLoopOp(ConversionPatternRewriter &rewriter,
-                          scf::ParallelOp pLoopOp,
-                          ArrayRef<LoopBounds> newPLoopBounds,
-                          ArrayRef<LoopBounds> forBounds,
-                          ArrayRef<unsigned> permutation);
-
-/// Distributes scf.parallel to processors with the processors logically
-/// arranged with same dimensionality as the number of loops, i.e. a
-/// scf.parallel with 2 loops to a 2D grid of processors. `processorIDs` must be
-/// of same size as the number of loops and are the values to use for process ID
-/// and number of processors along each dimension in the distributed code.  This
-/// method assumes that the number of processors is greater than or equal to the
-/// number of iterations. So just generates an if statement to mask of
-/// processors with no work. When the number of processors is known to be
-/// exactly equal to the number of iterations, the if statement is not needed as
-/// well. In such cases, `generateGuard` can be set to `false` to avoid
-/// generating the if statement.
-LogicalResult distributeSingleIterationPerProcessor(
-    ConversionPatternRewriter &rewriter, scf::ParallelOp pLoopOp,
-    ArrayRef<linalg::ProcInfo> procInfo, bool generateGuard = false);
 
 }  // namespace iree_compiler
 }  // namespace mlir
