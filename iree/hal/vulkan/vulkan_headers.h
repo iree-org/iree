224f41553bba56179ebca62f5032e597e76d58b1
Lei Zhang <antiagainst@google.com>
2022-02-14 21:12:05 -0500

    ifdef headers too

--- iree/hal/vulkan/vulkan_headers.h
+++ iree/hal/vulkan/vulkan_headers.h
@@ -33,7 +33,10 @@
 // https://djang86.blogspot.com/2019/01/what-is-vknoprototypes.html
 #define VK_NO_PROTOTYPES 1
 
-#include <vulkan/vulkan.h>       // IWYU pragma: export
+#include <vulkan/vulkan.h>  // IWYU pragma: export
+
+#ifdef IREE_PLATFORM_APPLE
 #include <vulkan/vulkan_beta.h>  // IWYU pragma: export
+#endif
 
 #endif  // IREE_HAL_VULKAN_VULKAN_HEADERS_H_
