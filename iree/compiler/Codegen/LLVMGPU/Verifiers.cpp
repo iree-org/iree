0d497b8447dc9d57ad4a16ff5e4308e70058de34
Kojo Acquah <kooljblack@google.com>
2022-02-22 09:02:07 -0800

    all tests run with batched matmul

--- iree/compiler/Codegen/LLVMGPU/Verifiers.cpp
+++ iree/compiler/Codegen/LLVMGPU/Verifiers.cpp
@@ -25,6 +25,10 @@ LogicalResult verifyGPUMatmulSimtPassPipeline(
   if (workgroupSize.empty()) {
     return op->emitOpError("expected workgroup size for GPU pipelines");
   }
+  linalg::MatmulOp matmulOp = dyn_cast<linalg::MatmulOp>(op);
+  if (!matmulOp) {
+    return success(); // Only verify matmul.
+  }
 
   // Verify the total workgroup size is <= 1024
   int64_t totalWorkgroupSize =
@@ -40,27 +44,25 @@ LogicalResult verifyGPUMatmulSimtPassPipeline(
            << pipelineName << ", got " << workgroupSize[2];
   }
 
-  // For linalg::MatmulOp, verify shared memory usage of operands after tiling
+  // Verify shared memory usage of operands after tiling
   // requires <= 64Kb combined space.
-  linalg::MatmulOp matmulOp = dyn_cast<linalg::MatmulOp>(op);
-  if (matmulOp) {
-    SmallVector<int64_t> firstLevelTileSizes =
-        loweringConfig.getTileSizeVals(kWorkgroupTileLevel);
-    MemRefType type =
-        matmulOp.getInputOperand(0)->get().getType().cast<mlir::MemRefType>();
-    unsigned bytesSize = type.getElementType().getIntOrFloatBitWidth() / 8;
+  SmallVector<int64_t> firstLevelTileSizes =
+      loweringConfig.getTileSizeVals(kWorkgroupTileLevel);
+  MemRefType type =
+      matmulOp.getInputOperand(0)->get().getType().cast<mlir::MemRefType>();
+  unsigned bytesSize = type.getElementType().getIntOrFloatBitWidth() / 8;
 
-    // Input shape sizes: A [ M x K],  B [ K x N]
-    unsigned totalSharedMemSizeBytes =
-        (firstLevelTileSizes[0] * firstLevelTileSizes[2] +
-         firstLevelTileSizes[1] * firstLevelTileSizes[2]) *
-        bytesSize;
+  // Input shape sizes: A [ M x K],  B [ K x N]
+  unsigned totalSharedMemSizeBytes =
+      (firstLevelTileSizes[0] * firstLevelTileSizes[2] +
+       firstLevelTileSizes[1] * firstLevelTileSizes[2]) *
+      bytesSize;
 
-    if (totalSharedMemSizeBytes > kSharedMemSizeBytes) {
-      return op->emitOpError("expected shared memory usage <= 64Kb for ")
-             << pipelineName << ", got " << totalSharedMemSizeBytes;
-    }
+  if (totalSharedMemSizeBytes > kSharedMemSizeBytes) {
+    return op->emitOpError("expected shared memory usage <= 64Kb for ")
+           << pipelineName << ", got " << totalSharedMemSizeBytes;
   }
+
   return success();
 }
 
@@ -73,6 +75,38 @@ LogicalResult verifyGPUMatmulTensorCorePipeline(
   StringRef pipelineName = stringifyEnum(pipeline);
   if (workgroupSize.empty()) {
     return op->emitOpError("expected workgroup size for GPU pipelines");
+  }
+
+  ArrayRef<int64_t> lhsShape;
+  ArrayRef<int64_t> rhsShape;
+  Type inputType;
+  SmallVector<int64_t> firstLevelTileSizes;
+  if (linalg::MatmulOp matmulOp = dyn_cast<linalg::MatmulOp>(op)) {
+    inputType = matmulOp.getInputOperand(0)->get().getType();
+    lhsShape = getUntiledShape(matmulOp.getInputOperand(0)->get());
+    rhsShape = getUntiledShape(matmulOp.getInputOperand(1)->get());
+    firstLevelTileSizes = loweringConfig.getTileSizeVals(kWorkgroupTileLevel);
+  } else if (linalg::BatchMatmulOp batchMatmulOp =
+                 dyn_cast<linalg::BatchMatmulOp>(op)) {
+    inputType = batchMatmulOp.getInputOperand(0)->get().getType();
+
+    // First dimension is the batch dimension. We don't check the shape batch.
+    lhsShape =
+        getUntiledShape(batchMatmulOp.getInputOperand(0)->get()).drop_front(1);
+    rhsShape =
+        getUntiledShape(batchMatmulOp.getInputOperand(1)->get()).drop_front(1);
+
+    // First tile dimensions should be 1 for batched, use remaining dimensions
+    // for comparisons.
+    firstLevelTileSizes = loweringConfig.getTileSizeVals(kWorkgroupTileLevel);
+    if (firstLevelTileSizes[0] != 1) {
+      op->emitError("Received first tile dimension of ")
+          << firstLevelTileSizes[0] << " instead of 1 for " << pipelineName;
+    }
+    firstLevelTileSizes = {firstLevelTileSizes[1], firstLevelTileSizes[2],
+                           firstLevelTileSizes[3]};
+  } else {
+    return success(); // Only verify batched and unbatched matmul.
   }
 
   // Verify the total workgroup size is <= 1024
@@ -95,14 +129,12 @@ LogicalResult verifyGPUMatmulTensorCorePipeline(
            << pipelineName << ", got " << workgroupSize[2];
   }
 
-  // Verify the second level of tiling = first level tile size divided by the
+  // The second level of tiling = first level tile size divided by the
   // warps per workgroup size
-  SmallVector<int64_t> firstLevelTileSizes =
-      loweringConfig.getTileSizeVals(kWorkgroupTileLevel);
-  std::array<int64_t, 3> warpsPerWorkgroup = {
+  SmallVector<int64_t, 3> warpsPerWorkgroup = {
       workgroupSize[0] / kWarpSize, workgroupSize[1], workgroupSize[2]};
   SmallVector<int64_t, 3> secondLevelTileSizes;
-  for (int i = 0; i < firstLevelTileSizes.size(); ++i) {
+  for (int i = 0; i < 3; ++i) {
     secondLevelTileSizes.push_back(firstLevelTileSizes[i] /
                                    warpsPerWorkgroup[i]);
   }
@@ -118,60 +150,47 @@ LogicalResult verifyGPUMatmulTensorCorePipeline(
            << pipelineName;
   }
 
-  // For linalg::MatmulOp, verify the first level tile size divides the matmul
+  // Verify the first level tile size divides the matmul
   // inputs A [M x K] & B [K x N]
-  linalg::MatmulOp matmulOp = dyn_cast<linalg::MatmulOp>(op);
-  if (matmulOp) {
-    ArrayRef<int64_t> lhsShape =
-        getUntiledShape(matmulOp.getInputOperand(0)->get());
-    ArrayRef<int64_t> rhsShape =
-        getUntiledShape(matmulOp.getInputOperand(1)->get());
+  if (lhsShape[0] % firstLevelTileSizes[0] != 0 ||
+      lhsShape[1] % firstLevelTileSizes[2] != 0) {
+    return op->emitOpError(
+               "lhsShape doesn't factor into first level tile size for ")
+           << pipelineName << " [ " << lhsShape[0] << ", " << lhsShape[1]
+           << "]";
+  }
+  if (rhsShape[0] % firstLevelTileSizes[2] != 0 ||
+      rhsShape[1] % firstLevelTileSizes[1] != 0) {
+    return op->emitOpError(
+               "rhsShape doesn't factor into first level tile size for ")
+           << pipelineName << " [ " << rhsShape[0] << ", " << rhsShape[1]
+           << "]";
+  }
 
-    if (lhsShape[0] % firstLevelTileSizes[0] != 0 ||
-        lhsShape[1] % firstLevelTileSizes[2] != 0) {
-      return op->emitOpError(
-                 "lhsShape doesn't factor into first level tile size for ")
-             << pipelineName;
-    }
+  // Verify shared memory usage of operands after tiling requires <= 64Kb
+  // combined space.
+  unsigned bytesSize;
+  if (MemRefType type = inputType.dyn_cast<mlir::MemRefType>()) {
+    bytesSize = type.getElementType().getIntOrFloatBitWidth() / 8;
+  } else if (RankedTensorType type = inputType.dyn_cast<RankedTensorType>()) {
+    bytesSize = type.getElementType().getIntOrFloatBitWidth() / 8;
+  } else if (UnrankedTensorType type =
+                 inputType.dyn_cast<UnrankedTensorType>()) {
+    bytesSize = type.getElementType().getIntOrFloatBitWidth() / 8;
+  } else {
+    // Unable to determine type, skip rest of verification.
+    return success();
+  }
 
-    if (rhsShape[0] % firstLevelTileSizes[2] != 0 ||
-        rhsShape[1] % firstLevelTileSizes[1] != 0) {
-      return op->emitOpError(
-                 "rhsShape doesn't factor into first level tile size for ")
-             << pipelineName;
-    }
+  // Input shape sizes: A [ M x K],  B [ K x N]
+  unsigned totalSharedMemSizeBytes =
+      (firstLevelTileSizes[0] * firstLevelTileSizes[2] +
+       firstLevelTileSizes[1] * firstLevelTileSizes[2]) *
+      bytesSize;
 
-    unsigned bytesSize;
-    if (MemRefType type = matmulOp.getInputOperand(0)
-                              ->get()
-                              .getType()
-                              .dyn_cast<mlir::MemRefType>()) {
-      bytesSize = type.getElementType().getIntOrFloatBitWidth() / 8;
-    } else if (RankedTensorType type = matmulOp.getInputOperand(0)
-                                           ->get()
-                                           .getType()
-                                           .dyn_cast<RankedTensorType>()) {
-      bytesSize = type.getElementType().getIntOrFloatBitWidth() / 8;
-    } else if (UnrankedTensorType type = matmulOp.getInputOperand(0)
-                                             ->get()
-                                             .getType()
-                                             .dyn_cast<UnrankedTensorType>()) {
-      bytesSize = type.getElementType().getIntOrFloatBitWidth() / 8;
-    } else {
-      // Unable to determine type, skip rest of verification.
-      return success();
-    }
-
-    // Input shape sizes: A [ M x K],  B [ K x N]
-    unsigned totalSharedMemSizeBytes =
-        (firstLevelTileSizes[0] * firstLevelTileSizes[2] +
-         firstLevelTileSizes[1] * firstLevelTileSizes[2]) *
-        bytesSize;
-
-    if (totalSharedMemSizeBytes > kSharedMemSizeBytes) {
-      return op->emitOpError("expected shared memory usage <= 64Kb for ")
-             << pipelineName << ", got " << totalSharedMemSizeBytes;
-    }
+  if (totalSharedMemSizeBytes > kSharedMemSizeBytes) {
+    return op->emitOpError("expected shared memory usage <= 64Kb for ")
+           << pipelineName << ", got " << totalSharedMemSizeBytes;
   }
   return success();
 }
