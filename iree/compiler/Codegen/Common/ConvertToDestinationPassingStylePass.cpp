add1285fe7cd8913d6b2f9f4a067954dbe2813a4
Hanhan Wang <hanchung@google.com>
2022-03-08 16:26:06 -0800

    combine two cases

--- iree/compiler/Codegen/Common/ConvertToDestinationPassingStylePass.cpp
+++ iree/compiler/Codegen/Common/ConvertToDestinationPassingStylePass.cpp
@@ -329,12 +329,8 @@ static bool isFromReadOnlyTensor(Value v) {
           [&](arith::ConstantOp constantOp) { return true; })
       .Case<tensor::CollapseShapeOp, tensor::ExpandShapeOp>(
           [&](auto op) { return isFromReadOnlyTensor(op.src()); })
-      .Case<tensor::ExtractSliceOp>([&](tensor::ExtractSliceOp sliceOp) {
-        return isFromReadOnlyTensor(sliceOp.source());
-      })
-      .Case<tensor::CastOp>([&](tensor::CastOp castOp) {
-        return isFromReadOnlyTensor(castOp.source());
-      })
+      .Case<tensor::CastOp, tensor::ExtractSliceOp>(
+          [&](auto op) { return isFromReadOnlyTensor(op.source()); })
       .Case<IREE::Flow::DispatchTensorLoadOp>(
           [&](IREE::Flow::DispatchTensorLoadOp loadOp) {
             return loadOp.source()
