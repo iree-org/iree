ccf4b1e86a48d0b42f5d7a768f86f051f1d62e55
Nicolas Vasilache <ntv@google.com>
2022-03-11 05:47:51 -0500

    Bring LinalgExtOps from the sandbox to iree-dialects.

--- llvm-external-projects/iree-dialects/lib/Dialect/LinalgExt/IR/LinalgExtOps.cpp
+++ llvm-external-projects/iree-dialects/lib/Dialect/LinalgExt/IR/LinalgExtOps.cpp
@@ -24,6 +24,7 @@
 #include "mlir/IR/Attributes.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/Diagnostics.h"
+#include "mlir/IR/FunctionImplementation.h"
 #include "mlir/IR/Matchers.h"
 #include "mlir/IR/OpImplementation.h"
 #include "mlir/IR/OperationSupport.h"
@@ -1240,6 +1241,388 @@ struct FoldTensorCastOp : public OpInterfaceRewritePattern<LinalgExtOp> {
   }
 };
 }  // namespace
+
+//===----------------------------------------------------------------------===//
+// TileOp
+//===----------------------------------------------------------------------===//
+
+void TileOp::build(mlir::OpBuilder &builder, mlir::OperationState &result,
+                   Value tileSize, ValueRange outs, int64_t tiledDim,
+                   TileOp::TileOpBodyBuilderFn bodyBuilder) {
+  result.addOperands(tileSize);
+  result.addOperands(outs);
+  result.addAttribute(TileOp::getTiledDimAttrName(),
+                      builder.getI64IntegerAttr(tiledDim));
+  result.addTypes(outs.getType());
+
+  Region *bodyRegion = result.addRegion();
+  bodyRegion->push_back(new Block);
+  Block &bodyBlock = bodyRegion->front();
+  // TODO: Pass a better location here.
+  Location loc = tileSize.getLoc();
+  bodyBlock.addArgument(builder.getIndexType(), loc);
+  bodyBlock.addArgument(builder.getIndexType(), loc);
+  // Handle the sliced out types in a conservative fashion: all dimensions
+  // become dynamic and a later canonicalization is expected to recover static
+  // types.
+  // TODO: should we relax this and use something less strict?
+  auto dynamicTypes =
+      llvm::to_vector(llvm::map_range(outs.getTypes(), [](Type t) -> Type {
+        auto rankedTensorType = t.cast<RankedTensorType>();
+        RankedTensorType::Builder rttb(rankedTensorType);
+        SmallVector<int64_t> dynamicShape(rankedTensorType.getRank(),
+                                          ShapedType::kDynamicSize);
+        return rttb.setShape(dynamicShape);
+      }));
+  SmallVector<Location> locs(dynamicTypes.size(), loc);
+  bodyBlock.addArguments(dynamicTypes, locs);
+
+  OpBuilder::InsertionGuard guard(builder);
+  builder.setInsertionPointToStart(&bodyBlock);
+  bodyBuilder(builder, result.location, bodyBlock.getArgument(0),
+              bodyBlock.getArgument(1), bodyBlock.getArguments().drop_front(2));
+}
+
+void TileOp::build(mlir::OpBuilder &builder, mlir::OperationState &result,
+                   Value tileSize, ValueRange outs,
+                   TileOp::TileOpBodyBuilderFn bodyBuilder) {
+  TileOp::build(builder, result, tileSize, outs, 0, bodyBuilder);
+}
+
+// TODO(#81): Impl me.
+LogicalResult TileOp::verify() { return success(); }
+
+void TileOp::print(OpAsmPrinter &p) {
+  p << ' ' << tile_size() << ' ';
+  if (tiled_dim() > 0) p << "tiled_dim = " << tiled_dim() << ' ';
+  if (!outs().empty()) {
+    p << "outs(";
+    llvm::interleaveComma(outs(), p,
+                          [&p](Value v) { p << v << ": " << v.getType(); });
+    p << ')';
+  }
+  p << " -> (" << getResultTypes() << ") ";
+  p.printRegion(region(),
+                /*printEntryBlockArgs=*/true,
+                /*printBlockTerminators=*/true);
+  p.printOptionalAttrDict(getOperation()->getAttrs(),
+                          /*elidedAttrs=*/{TileOp::getTiledDimAttrName()});
+}
+
+ParseResult TileOp::parse(OpAsmParser &parser, OperationState &result) {
+  auto &builder = parser.getBuilder();
+
+  OpAsmParser::OperandType tileSizes;
+  // TODO: also allow tensor<..xindex> and figure out a good syntax.
+  // Type tensorOfIndexType =
+  //     RankedTensorType::get({ShapedType::kDynamicSize}, indexType);
+  Type tileSizesType = builder.getIndexType();
+  SmallVector<Type> outsTypes;
+  SmallVector<OpAsmParser::OperandType, 4> outsOperands;
+
+  llvm::SMLoc outputsOperandsLoc;
+  if (parser.parseOperand(tileSizes) ||
+      parser.resolveOperand(tileSizes, tileSizesType, result.operands))
+    return failure();
+
+  // Parse the `tiled_dim` attribute or set it to 0 implicitly when elided.
+  if (succeeded(parser.parseOptionalKeyword(TileOp::getTiledDimAttrName()))) {
+    outputsOperandsLoc = parser.getCurrentLocation();
+    Attribute valueAttr;
+    parser.parseAttribute(valueAttr, TileOp::getTiledDimAttrName(),
+                          result.attributes);
+  } else {
+    result.attributes.append(TileOp::getTiledDimAttrName(),
+                             parser.getBuilder().getI64IntegerAttr(0));
+  }
+
+  if (succeeded(parser.parseOptionalKeyword("outs"))) {
+    bool _1;
+    SmallVector<NamedAttrList> _2;
+    SmallVector<Location> _3;
+    outputsOperandsLoc = parser.getCurrentLocation();
+    if (mlir::function_interface_impl::parseFunctionArgumentList(
+            parser,
+            /*allowAttributes=*/false,
+            /*allowVariadic=*/false, outsOperands, outsTypes, /*argAttrs=*/_2,
+            /*argLocations=*/_3,
+            /*isVariadic=*/_1) ||
+        parser.resolveOperands(outsOperands, outsTypes, outputsOperandsLoc,
+                               result.operands))
+      return failure();
+  }
+  if (parser.parseArrowTypeList(result.types)) return failure();
+
+  SmallVector<OpAsmParser::OperandType, 8> regionOperands;
+  std::unique_ptr<Region> region = std::make_unique<Region>();
+  SmallVector<Type, 8> operandTypes, regionTypes;
+  if (parser.parseRegion(*region, regionOperands, regionTypes))
+    return failure();
+
+  // Parse the optional attribute list.
+  if (parser.parseOptionalAttrDict(result.attributes)) return failure();
+
+  TileOp::ensureTerminator(*region, builder, result.location);
+  result.addRegion(std::move(region));
+
+  return success();
+}
+
+//===----------------------------------------------------------------------===//
+// InParallelOp
+//===----------------------------------------------------------------------===//
+
+LogicalResult InParallelOp::verify() {
+  // Check that the body defines as single block argument for the thread index.
+  auto *body = getBody();
+  if (body->getNumArguments() != 1)
+    return emitOpError("body expects exactly one argument");
+  if (!body->getArgument(0).getType().isIndex())
+    return emitOpError(
+        "expected body first argument to be an index argument for "
+        "the thread index");
+
+  // Verify consistency between the result types and the terminator.
+  auto terminatorTypes = getTerminator().yieldedTypes();
+  auto opResults = getResults();
+  if (opResults.size() != terminatorTypes.size())
+    return emitOpError("produces ")
+           << opResults.size() << " results, but its terminator yields "
+           << terminatorTypes.size() << " values";
+  unsigned i = 0;
+  for (auto e : llvm::zip(terminatorTypes, opResults)) {
+    if (std::get<0>(e) != std::get<1>(e).getType())
+      return emitOpError() << "type mismatch between " << i
+                           << "th result of in_parallel (" << std::get<0>(e)
+                           << ") and " << i << "th result yielded by its "
+                           << "terminator (" << std::get<1>(e).getType() << ")";
+    i++;
+  }
+
+  return success();
+}
+
+void InParallelOp::print(OpAsmPrinter &p) {
+  p << ' ' << num_threads() << ' ';
+  p << " -> (" << getResultTypes() << ") ";
+  p.printRegion(region(),
+                /*printEntryBlockArgs=*/true,
+                /*printBlockTerminators=*/true);
+  p.printOptionalAttrDict(getOperation()->getAttrs());
+}
+
+ParseResult InParallelOp::parse(OpAsmParser &parser, OperationState &result) {
+  auto &builder = parser.getBuilder();
+
+  OpAsmParser::OperandType numThreads;
+  Type indexType = builder.getIndexType();
+
+  if (parser.parseOperand(numThreads) ||
+      parser.resolveOperand(numThreads, indexType, result.operands))
+    return failure();
+  if (parser.parseArrowTypeList(result.types)) return failure();
+
+  SmallVector<OpAsmParser::OperandType, 8> regionOperands;
+  SmallVector<Type, 8> regionTypes;
+  std::unique_ptr<Region> region = std::make_unique<Region>();
+  if (parser.parseRegion(*region, regionOperands, regionTypes))
+    return failure();
+  InParallelOp::ensureTerminator(*region, builder, result.location);
+  result.addRegion(std::move(region));
+
+  // Parse the optional attribute list.
+  if (parser.parseOptionalAttrDict(result.attributes)) return failure();
+  return success();
+}
+
+// Bodyless builder, result types must be specified.
+void InParallelOp::build(mlir::OpBuilder &builder, mlir::OperationState &result,
+                         TypeRange resultTypes, Value numThreads) {
+  // TODO: Pass better location.
+  Location loc = numThreads.getLoc();
+  result.addOperands(numThreads);
+
+  Region *bodyRegion = result.addRegion();
+  bodyRegion->push_back(new Block);
+  Block &bodyBlock = bodyRegion->front();
+  bodyBlock.addArgument(builder.getIndexType(), loc);
+
+  // Create the default terminator if the builder is not provided and if the
+  // iteration arguments are not provided. Otherwise, leave this to the caller
+  // because we don't know which values to return from the loop.
+  InParallelOp::ensureTerminator(*bodyRegion, builder, result.location);
+  result.addTypes(resultTypes);
+}
+
+// Builder that takes a bodyBuilder lambda, result types are inferred from
+// the terminator.
+void InParallelOp::build(
+    mlir::OpBuilder &builder, mlir::OperationState &result, Value numThreads,
+    function_ref<void(OpBuilder &, Location, Value)> bodyBuilder) {
+  // TODO: Pass better location.
+  Location loc = numThreads.getLoc();
+  result.addOperands(numThreads);
+
+  Region *bodyRegion = result.addRegion();
+  bodyRegion->push_back(new Block);
+  Block &bodyBlock = bodyRegion->front();
+  bodyBlock.addArgument(builder.getIndexType(), loc);
+
+  OpBuilder::InsertionGuard guard(builder);
+  builder.setInsertionPointToStart(&bodyBlock);
+  bodyBuilder(builder, result.location, bodyBlock.getArgument(0));
+  auto terminator =
+      llvm::cast<PerformConcurrentlyOp>(bodyBlock.getTerminator());
+  result.addTypes(terminator.yieldedTypes());
+}
+
+// The ensureTerminator method generated by SingleBlockImplicitTerminator is
+// unaware of the fact that our terminator also needs a region to be well
+// formed. We override it here to ensure that we do the right thing.
+void InParallelOp::ensureTerminator(Region &region, Builder &builder,
+                                    Location loc) {
+  OpTrait::SingleBlockImplicitTerminator<PerformConcurrentlyOp>::Impl<
+      InParallelOp>::ensureTerminator(region, builder, loc);
+  auto terminator =
+      llvm::dyn_cast<PerformConcurrentlyOp>(region.front().getTerminator());
+  PerformConcurrentlyOp::ensureTerminator(terminator.getRegion(), builder, loc);
+}
+
+PerformConcurrentlyOp InParallelOp::getTerminator() {
+  return cast<PerformConcurrentlyOp>(getBody()->getTerminator());
+}
+
+//===----------------------------------------------------------------------===//
+// ParallelInsertSliceOp
+//===----------------------------------------------------------------------===//
+
+// Build a ParallelInsertSliceOp with mixed static and dynamic entries.
+void ParallelInsertSliceOp::build(OpBuilder &b, OperationState &result,
+                                  Value source, Value dest,
+                                  ArrayRef<OpFoldResult> offsets,
+                                  ArrayRef<OpFoldResult> sizes,
+                                  ArrayRef<OpFoldResult> strides,
+                                  ArrayRef<NamedAttribute> attrs) {
+  SmallVector<int64_t> staticOffsets, staticSizes, staticStrides;
+  SmallVector<Value> dynamicOffsets, dynamicSizes, dynamicStrides;
+  dispatchIndexOpFoldResults(offsets, dynamicOffsets, staticOffsets,
+                             ShapedType::kDynamicStrideOrOffset);
+  dispatchIndexOpFoldResults(sizes, dynamicSizes, staticSizes,
+                             ShapedType::kDynamicSize);
+  dispatchIndexOpFoldResults(strides, dynamicStrides, staticStrides,
+                             ShapedType::kDynamicStrideOrOffset);
+  build(b, result, {}, source, dest, dynamicOffsets, dynamicSizes,
+        dynamicStrides, b.getI64ArrayAttr(staticOffsets),
+        b.getI64ArrayAttr(staticSizes), b.getI64ArrayAttr(staticStrides));
+  result.addAttributes(attrs);
+}
+
+// Build a ParallelInsertSliceOp with dynamic entries.
+void ParallelInsertSliceOp::build(OpBuilder &b, OperationState &result,
+                                  Value source, Value dest, ValueRange offsets,
+                                  ValueRange sizes, ValueRange strides,
+                                  ArrayRef<NamedAttribute> attrs) {
+  SmallVector<OpFoldResult> offsetValues = llvm::to_vector<4>(
+      llvm::map_range(offsets, [](Value v) -> OpFoldResult { return v; }));
+  SmallVector<OpFoldResult> sizeValues = llvm::to_vector<4>(
+      llvm::map_range(sizes, [](Value v) -> OpFoldResult { return v; }));
+  SmallVector<OpFoldResult> strideValues = llvm::to_vector<4>(
+      llvm::map_range(strides, [](Value v) -> OpFoldResult { return v; }));
+  build(b, result, source, dest, offsetValues, sizeValues, strideValues);
+}
+
+namespace {
+/// Pattern to rewrite a parallel_insert_slice op with constant arguments.
+class ParallelInsertSliceOpConstantArgumentFolder final
+    : public OpRewritePattern<ParallelInsertSliceOp> {
+ public:
+  using OpRewritePattern<ParallelInsertSliceOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(ParallelInsertSliceOp insertSliceOp,
+                                PatternRewriter &rewriter) const override {
+    // No constant operand, just return.
+    if (llvm::none_of(insertSliceOp.getOperands(), [](Value operand) {
+          return matchPattern(operand, matchConstantIndex());
+        }))
+      return failure();
+
+    // At least one of offsets/sizes/strides is a new constant.
+    // Form the new list of operands and constant attributes from the
+    // existing.
+    SmallVector<OpFoldResult> mixedOffsets(insertSliceOp.getMixedOffsets());
+    SmallVector<OpFoldResult> mixedSizes(insertSliceOp.getMixedSizes());
+    SmallVector<OpFoldResult> mixedStrides(insertSliceOp.getMixedStrides());
+    canonicalizeSubViewPart(mixedOffsets, ShapedType::isDynamicStrideOrOffset);
+    canonicalizeSubViewPart(mixedSizes, ShapedType::isDynamic);
+    canonicalizeSubViewPart(mixedStrides, ShapedType::isDynamicStrideOrOffset);
+
+    // Create the new op in canonical form.
+    rewriter.replaceOpWithNewOp<ParallelInsertSliceOp>(
+        insertSliceOp, insertSliceOp.source(), insertSliceOp.dest(),
+        mixedOffsets, mixedSizes, mixedStrides);
+    return success();
+  }
+};
+}  // namespace
+
+void ParallelInsertSliceOp::getCanonicalizationPatterns(
+    RewritePatternSet &results, MLIRContext *context) {
+  results.add<ParallelInsertSliceOpConstantArgumentFolder>(context);
+}
+
+//===----------------------------------------------------------------------===//
+// PerformConcurrentlyOp
+//===----------------------------------------------------------------------===//
+
+// TODO(ntv,apaszke): Implement this
+LogicalResult PerformConcurrentlyOp::verify() { return success(); }
+
+void PerformConcurrentlyOp::print(OpAsmPrinter &p) {
+  p << " ";
+  p.printRegion(region(),
+                /*printEntryBlockArgs=*/false,
+                /*printBlockTerminators=*/false);
+  p.printOptionalAttrDict(getOperation()->getAttrs());
+}
+
+ParseResult PerformConcurrentlyOp::parse(OpAsmParser &parser,
+                                         OperationState &result) {
+  auto &builder = parser.getBuilder();
+
+  SmallVector<OpAsmParser::OperandType, 8> regionOperands;
+  SmallVector<Type, 8> regionTypes;
+  std::unique_ptr<Region> region = std::make_unique<Region>();
+  if (parser.parseRegion(*region, regionOperands, regionTypes))
+    return failure();
+  PerformConcurrentlyOp::ensureTerminator(*region, builder, result.location);
+  result.addRegion(std::move(region));
+
+  // Parse the optional attribute list.
+  if (parser.parseOptionalAttrDict(result.attributes)) return failure();
+  return success();
+}
+
+SmallVector<Type> PerformConcurrentlyOp::yieldedTypes() {
+  return llvm::to_vector(llvm::map_range(
+      this->yieldingOps(),
+      [](ParallelInsertSliceOp op) { return op.yieldedType(); }));
+}
+
+SmallVector<ParallelInsertSliceOp> PerformConcurrentlyOp::yieldingOps() {
+  SmallVector<ParallelInsertSliceOp> ret;
+  for (Operation &op : *getBody()) {
+    // TODO: interface when this grows up.
+    if (auto sliceOp = llvm::dyn_cast<ParallelInsertSliceOp>(op)) {
+      ret.push_back(sliceOp);
+      continue;
+    }
+    if (auto endPerformOp = llvm::dyn_cast<EndPerformConcurrentlyOp>(op)) {
+      continue;
+    }
+    llvm_unreachable("Unexpected operation in perform_concurrently");
+  }
+  return ret;
+}
 
 //===----------------------------------------------------------------------===//
 // LinalgExtDialect
--- llvm-external-projects/iree-dialects/test/iree_linalgext/canonicalize.mlir
+++ llvm-external-projects/iree-dialects/test/iree_linalgext/canonicalize.mlir
@@ -19,3 +19,24 @@ func @tensor.cast(%arg0: tensor<3x5xi32>) -> tensor<3x5xi32> {
 
   return %1: tensor<3x5xi32>
 }
+
+// CHECK-LABEL: func @canonicalize_insert_slice_indices(
+//  CHECK-SAME:     %[[arg0:.*]]: tensor<?x?xf32>, %[[arg1:.*]]: tensor<?x?xf32>,
+//  CHECK-SAME:     %[[idx:.*]]: index
+func @canonicalize_insert_slice_indices(
+    %arg0 : tensor<?x?xf32>, %arg1: tensor<?x?xf32>,
+    %idx : index) -> tensor<?x?xf32>
+{
+  %cst = arith.constant 4.200000e+01 : f32
+  %c0 = arith.constant 0 : index
+  %c1 = arith.constant 1 : index
+
+  %2 = iree_linalg_ext.in_parallel %idx  -> (tensor<?x?xf32>) {
+    ^bb0(%arg3: index):  // no predecessors
+      iree_linalg_ext.perform_concurrently {
+        // CHECK: iree_linalg_ext.parallel_insert_slice %[[arg0]] into %arg1[%[[idx]], 0] [1, 5] [1, 1]
+        iree_linalg_ext.parallel_insert_slice %arg0 into %arg1[%idx, %c0] [%c1, 5] [%c1, %c1] : tensor<?x?xf32> into tensor<?x?xf32>
+      }
+  }
+  return %2 : tensor<?x?xf32>
+}
