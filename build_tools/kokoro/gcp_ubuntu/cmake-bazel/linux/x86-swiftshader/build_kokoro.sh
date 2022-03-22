f10b758e1037ff3e8b1679c2f70a5ab7badb82e0
Rob Suderman <suderman@google.com>
2022-03-22 15:32:22 -0700

    Fix Lambda layers test and bump Swiftshader (#8605)
    Changes to swiftshader head caused an accuracy regression. This is
    becase x**2 is numerically computationally complex. Changed the
    computation to be x*x which is computationally identical while not
    depending on an exponential operator.
    
    Bumped swiftshader version and rebuilt docker images.

--- build_tools/kokoro/gcp_ubuntu/cmake-bazel/linux/x86-swiftshader/build_kokoro.sh
+++ build_tools/kokoro/gcp_ubuntu/cmake-bazel/linux/x86-swiftshader/build_kokoro.sh
@@ -24,7 +24,7 @@ source "${KOKORO_ARTIFACTS_DIR?}/github/iree/build_tools/kokoro/gcp_ubuntu/docke
 docker_setup
 
 docker run "${DOCKER_RUN_ARGS[@]?}" \
-  gcr.io/iree-oss/frontends-swiftshader@sha256:ba1dd452360a8942316b6f095abd555e4214ed5c3cdc892038d38eaf4bb8d162 \
+  gcr.io/iree-oss/frontends-swiftshader@sha256:ab8f7280bb183f272c14479b4a90d8bf3a3185ff1f5bb4872fbea8f232fd1bee \
   build_tools/kokoro/gcp_ubuntu/cmake-bazel/linux/x86-swiftshader/build.sh
 
 # Kokoro will rsync this entire directory back to the executor orchestrating the
