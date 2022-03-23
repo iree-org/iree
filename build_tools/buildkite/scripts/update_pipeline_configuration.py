d7089530f49c1390095763659fa2c75cebf22741
Geoffrey Martin-Noble <gcmn@google.com>
2022-03-23 13:56:32 -0700

    Lint

--- build_tools/buildkite/scripts/update_pipeline_configuration.py
+++ build_tools/buildkite/scripts/update_pipeline_configuration.py
@@ -18,6 +18,7 @@ check simultaneously.
 import argparse
 import os
 from pybuildkite import buildkite
+import requests
 import sys
 
 PIPELINE_ROOT_PATH = "build_tools/buildkite/pipelines"
@@ -32,26 +33,14 @@ def parse_args():
             f" file at {os.path.join(PIPELINE_ROOT_PATH, '<pipeline>.yml')}"))
   return parser.parse_args()
 
-def should_update(pipeline_to_update):
-  # A token for the Buildkite API. Needs read/write privileges on builds to
-  # watch and create builds. Within our pipelines we fetch this from secret
-  # manager: https://cloud.google.com/secret-manager. Users can create a
-  # personal token for running this script locally:
-  # https://buildkite.com/docs/apis/managing-api-tokens
-  access_token = os.environ["BUILDKITE_ACCESS_TOKEN"]
 
+def should_update(bk, organization, pipeline_to_update):
   # Buildkite sets these environment variables. See
   # https://buildkite.com/docs/pipelines/environment-variables. If running
   # locally you can set locally or use the simulate_buildkite.sh script.
-  organization = os.environ["BUILDKITE_ORGANIZATION_SLUG"]
-
-  organization = os.environ["BUILDKITE_ORGANIZATION_SLUG"]
   running_pipeline = os.environ["BUILDKITE_PIPELINE_SLUG"]
   build_number = int(os.environ["BUILDKITE_BUILD_NUMBER"])
   step_key = os.environ["BUILDKITE_STEP_KEY"]
-
-  bk = buildkite.Buildkite()
-  bk.set_access_token(access_token)
 
   next_build_number = build_number + 1
   try:
@@ -59,12 +48,11 @@ def should_update(pipeline_to_update):
                                             next_build_number)
   except requests.exceptions.HTTPError as e:
     if e.response.status_code == 404:
-      print(f"Did not find later run of '{pipeline_to_update}' with build number"
-            f" '{next_build_number}'. Proceeding with update.")
+      print(f"Did not find later run of '{pipeline_to_update}' with"
+            f" build number '{next_build_number}'. Proceeding with update.")
       return True
     else:
       raise e
-
 
   jobs = build["jobs"]
   matching_job = next((j for j in jobs if j["step_key"] == step_key), None)
@@ -98,7 +86,22 @@ def should_update(pipeline_to_update):
 
 
 def main(args):
-  if should_update(args.pipeline):
+  # A token for the Buildkite API. Needs read/write privileges on builds to
+  # watch and create builds. Within our pipelines we fetch this from secret
+  # manager: https://cloud.google.com/secret-manager. Users can create a
+  # personal token for running this script locally:
+  # https://buildkite.com/docs/apis/managing-api-tokens
+  access_token = os.environ["BUILDKITE_ACCESS_TOKEN"]
+
+  # Buildkite sets these environment variables. See
+  # https://buildkite.com/docs/pipelines/environment-variables. If running
+  # locally you can set locally or use the simulate_buildkite.sh script.
+  organization = os.environ["BUILDKITE_ORGANIZATION_SLUG"]
+
+  bk = buildkite.Buildkite()
+  bk.set_access_token(access_token)
+
+  if should_update(bk, organization, args.pipeline):
     pipeline_file = os.path.join(PIPELINE_ROOT_PATH, f"{args.pipeline}.yml")
     with open(pipeline_file) as f:
       current_pipeline_yaml = f.read()
