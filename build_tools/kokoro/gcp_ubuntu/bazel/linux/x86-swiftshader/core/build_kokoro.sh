2325782b402c50441d8b3e6d1502b841978f3669
Lei Zhang <antiagainst@google.com>
2022-04-07 18:44:47 -0400

    Update SwiftShader to d15c42482560fba311e3cac90203438ad972df55

--- build_tools/kokoro/gcp_ubuntu/bazel/linux/x86-swiftshader/core/build_kokoro.sh
+++ build_tools/kokoro/gcp_ubuntu/bazel/linux/x86-swiftshader/core/build_kokoro.sh
@@ -28,7 +28,7 @@ docker_setup
 # and the cache key is the docker container it's run in (to ensure correct cache
 # hits).
 docker run "${DOCKER_RUN_ARGS[@]?}" \
-  gcr.io/iree-oss/frontends-swiftshader@sha256:d94e374295b0ea9e4b3ccf1cbc2325ca8e3c02aceab849a7609ecbad29a99981 \
+  gcr.io/iree-oss/frontends-swiftshader@sha256:650ccdc4a6f82c443f07618ee874fbca3bcc9b52049533c78eff882d51557989 \
   build_tools/kokoro/gcp_ubuntu/bazel/linux/x86-swiftshader/core/build.sh
 
 # Kokoro will rsync this entire directory back to the executor orchestrating the
--- build_tools/kokoro/gcp_ubuntu/cmake-bazel/linux/x86-swiftshader/build_kokoro.sh
+++ build_tools/kokoro/gcp_ubuntu/cmake-bazel/linux/x86-swiftshader/build_kokoro.sh
@@ -24,7 +24,7 @@ source "${KOKORO_ARTIFACTS_DIR?}/github/iree/build_tools/kokoro/gcp_ubuntu/docke
 docker_setup
 
 docker run "${DOCKER_RUN_ARGS[@]?}" \
-  gcr.io/iree-oss/frontends-swiftshader@sha256:d94e374295b0ea9e4b3ccf1cbc2325ca8e3c02aceab849a7609ecbad29a99981 \
+  gcr.io/iree-oss/frontends-swiftshader@sha256:650ccdc4a6f82c443f07618ee874fbca3bcc9b52049533c78eff882d51557989 \
   build_tools/kokoro/gcp_ubuntu/cmake-bazel/linux/x86-swiftshader/build.sh
 
 # Kokoro will rsync this entire directory back to the executor orchestrating the
