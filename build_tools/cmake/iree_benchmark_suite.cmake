944afa17b001150561a673268ab84c6ac87deae0
Scott Todd <scotttodd@google.com>
2022-04-12 14:57:24 -0700

    Rework meta benchmark import target and drop new option.

--- build_tools/cmake/iree_benchmark_suite.cmake
+++ build_tools/cmake/iree_benchmark_suite.cmake
@@ -160,15 +160,9 @@ function(iree_benchmark_suite)
           COMMENT
             "Importing ${_TFLITE_FILE_BASENAME} into MLIR"
         )
+        add_dependencies(iree-benchmark-import-models "${_TFLITE_IMPORT_TARGET}")
       endif()
       set(_MODULE_SOURCE_TARGET "${_TFLITE_IMPORT_TARGET}")
-    endif()
-
-    # Allow an early exit for developers that just want to import module
-    # sources rather than fully compile to IREE modules for each configuration.
-    if(IREE_BUILD_BENCHMARKS_IMPORT_ONLY)
-      add_dependencies(iree-benchmark-suites "${_MODULE_SOURCE_TARGET}")
-      continue()
     endif()
 
     # Next create the command and target for compiling the input module into
