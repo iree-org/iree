45e1a8d8163ccf5b8b800ab99add5c76ffc1853d
Lei Zhang <antiagainst@google.com>
2022-04-14 11:37:48 -0400

    Remove unused header file

--- iree/compiler/Codegen/Utils/GPUUtils.h
+++ iree/compiler/Codegen/Utils/GPUUtils.h
@@ -4,10 +4,9 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#ifndef IREE_COMPILER_CODEGEN_COMMON_GPUUTILS_H_
-#define IREE_COMPILER_CODEGEN_COMMON_GPUUTILS_H_
+#ifndef IREE_COMPILER_CODEGEN_UTILS_GPUUTILS_H_
+#define IREE_COMPILER_CODEGEN_UTILS_GPUUTILS_H_
 
-#include "iree/compiler/Codegen/Transforms/Transforms.h"
 #include "iree/compiler/Codegen/Utils/Utils.h"
 
 namespace mlir {
@@ -16,8 +15,8 @@ namespace iree_compiler {
 static constexpr int32_t kNumGPUDims = 3;
 static constexpr int32_t kWarpSize = 32;
 
-llvm::SmallVector<mlir::linalg::ProcInfo, 2> getGPUThreadIdsAndCounts(
-    mlir::OpBuilder &builder, mlir::Location loc, unsigned numDims,
+llvm::SmallVector<linalg::ProcInfo, 2> getGPUThreadIdsAndCounts(
+    OpBuilder &builder, Location loc, unsigned numDims,
     llvm::ArrayRef<int64_t> workgroupSize);
 
 /// Compute subgroup ID. CUDA doesn't have a subgroupId equivalent so we are are
@@ -25,12 +24,12 @@ llvm::SmallVector<mlir::linalg::ProcInfo, 2> getGPUThreadIdsAndCounts(
 /// When tiling to warp we assume each warp is full and we pick a workgroup
 /// size so that `workgroupSize.x % warpSize == 0`. This is why we can have
 /// warpId = { threadId.x / warpSize, threadId.y, threadId.z }.
-llvm::SmallVector<mlir::linalg::ProcInfo, 2> getSubgroupIdsAndCounts(
-    mlir::OpBuilder &builder, mlir::Location loc, unsigned numDims,
+llvm::SmallVector<linalg::ProcInfo, 2> getSubgroupIdsAndCounts(
+    OpBuilder &builder, Location loc, unsigned numDims,
     llvm::ArrayRef<int64_t> numSubgroups);
 
 /// Return the workgroup size associated to the funcOp entry point.
-std::array<int64_t, 3> getWorkgroupSize(mlir::func::FuncOp funcOp);
+std::array<int64_t, 3> getWorkgroupSize(func::FuncOp funcOp);
 
 /// Return true if we can use all threads to perform vectorized load/store of
 /// the given `shape`.
@@ -51,4 +50,4 @@ LogicalResult deallocateWorkgroupMemory(OpBuilder &, Value buffer);
 }  // namespace iree_compiler
 }  // namespace mlir
 
-#endif  // IREE_COMPILER_CODEGEN_COMMON_GPUUTILS_H_
+#endif  // IREE_COMPILER_CODEGEN_UTILS_GPUUTILS_H_
