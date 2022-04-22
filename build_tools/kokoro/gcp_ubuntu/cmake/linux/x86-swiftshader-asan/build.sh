761ba9c5fb6771d95621ddbd183ceb0708aecf6b
Stella Laurenzo <laurenzo@google.com>
2022-04-22 13:00:19 -0700

    Rename failing test exclude (missed after samples/ move).
    Fixes #8973
    Documented in #5715

--- build_tools/kokoro/gcp_ubuntu/cmake/linux/x86-swiftshader-asan/build.sh
+++ build_tools/kokoro/gcp_ubuntu/cmake/linux/x86-swiftshader-asan/build.sh
@@ -113,7 +113,7 @@ label_exclude_regex="($(IFS="|" ; echo "${label_exclude_args[*]?}"))"
 # These tests currently have asan failures
 # TODO(#5715): Fix these
 declare -a excluded_tests=(
-  "iree/samples/simple_embedding/simple_embedding_vulkan_test"
+  "iree/samples/iree_simple_embedding/simple_embedding_vulkan_test"
 )
 
 # Prefix with `^` anchor
