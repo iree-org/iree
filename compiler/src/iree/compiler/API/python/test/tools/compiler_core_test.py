a67faa41bb341750bd717a60fa5b451f916036bb
Hanhan Wang <hanchung@google.com>
2022-05-11 05:52:43 -0700

    fix iree/compiler/API/python/test/tools/compiler_core_test

--- compiler/src/iree/compiler/API/python/test/tools/compiler_core_test.py
+++ compiler/src/iree/compiler/API/python/test/tools/compiler_core_test.py
@@ -14,7 +14,7 @@ import unittest
 import iree.compiler.tools
 
 SIMPLE_MUL_ASM = """
-func @simple_mul(%arg0: tensor<4xf32>, %arg1: tensor<4xf32>) -> tensor<4xf32> {
+func.func @simple_mul(%arg0: tensor<4xf32>, %arg1: tensor<4xf32>) -> tensor<4xf32> {
     %0 = "mhlo.multiply"(%arg0, %arg1) {name = "mul.1"} : (tensor<4xf32>, tensor<4xf32>) -> tensor<4xf32>
     return %0 : tensor<4xf32>
 }
