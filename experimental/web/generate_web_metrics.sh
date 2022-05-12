b53b6ca19be4e18dd76db3baa91b08841aac3d01
Scott Todd <scotttodd@google.com>
2022-05-12 10:05:58 -0700

    Address bash script comments.
    * Quote variable expansions
    * Document hardcoded paths
    * Other style tweaks

--- experimental/web/generate_web_metrics.sh
+++ experimental/web/generate_web_metrics.sh
@@ -10,6 +10,9 @@
 #
 # Intended for interactive developer use, so this generates lots of output
 # files for manual inspection outside of the script.
+# Some hardcoded paths should be edited before use, and you might want to
+# comment out specific portions (such as python package installs) depending
+# on your environment or workflow choices.
 #
 # Script steps:
 #   * Install IREE tools into a Python virtual environment (venv)
@@ -33,27 +36,23 @@
 #   then open http://localhost:8000/benchmarks.html (for automated benchmarks)
 #   or open http://localhost:8000/ (for interactive benchmarks)
 
-set -e
+set -eo pipefail
 
 TARGET_DIR="$1"
-if [ -z "$TARGET_DIR" ]; then
-  echo "ERROR: Expected target directory (e.g. /tmp/iree/web_metrics/)"
+if [[ -z "$TARGET_DIR" ]]; then
+  >&2 echo "ERROR: Expected target directory (e.g. /tmp/iree/web_metrics/)"
   exit 1
 fi
 
 # If used in conjunction with sample_dynamic/build_sample.sh, this script can
 # copy compiled programs into that sample's build directory for benchmarking.
-ROOT_DIR=$(git rev-parse --show-toplevel)
-BUILD_DIR=${ROOT_DIR?}/build-emscripten
-SAMPLE_BINARY_DIR=${BUILD_DIR}/experimental/web/sample_dynamic/
+ROOT_DIR="$(git rev-parse --show-toplevel)"
+BUILD_DIR="${ROOT_DIR?}"/build-emscripten
+SAMPLE_BINARY_DIR="${BUILD_DIR}"/experimental/web/sample_dynamic/
 
-echo "Working in directory '$TARGET_DIR'"
-mkdir -p ${TARGET_DIR}
-cd ${TARGET_DIR}
-
-# Print all commands and the UTC time.
-# set -xeuo pipefail
-# export PS4='[$(date -u "+%T %Z")] '
+echo "Working in directory '${TARGET_DIR}'"
+mkdir -p "${TARGET_DIR}"
+cd "${TARGET_DIR}"
 
 ###############################################################################
 # Set up Python virtual environment                                           #
@@ -96,7 +95,7 @@ IREE_IMPORT_TFLITE_PATH=iree-import-tflite
 function import_program {
   OUTPUT_FILE=./$1.tflite.mlir
   echo "Importing '$1' to '${OUTPUT_FILE}'..."
-  ${IREE_IMPORT_TFLITE_PATH?} $2 -o ${OUTPUT_FILE}
+  "${IREE_IMPORT_TFLITE_PATH?}" "$2" -o "${OUTPUT_FILE}"
 }
 
 import_program "deeplabv3" "deeplabv3.tflite"
@@ -112,40 +111,40 @@ import_program "MobileNetV3SmallStaticBatch" "MobileNetV3SmallStaticBatch.tflite
 
 # Either build from source (setting this path), or use from the python packages.
 # IREE_COMPILE_PATH=~/code/iree-build/iree/tools/iree-compile
-IREE_COMPILE_PATH="D:\dev\projects\iree-build\iree\tools\iree-compile"
-# IREE_COMPILE_PATH=iree-compile
+# IREE_COMPILE_PATH="D:\dev\projects\iree-build\iree\tools\iree-compile"
+IREE_COMPILE_PATH=iree-compile
 
 # compile_program_wasm helper
 #   Args: program_name
 #   Compiles program_name.tflite.mlir to program_name_wasm.vmfb, dumping
 #   statistics and intermediate files to disk.
 function compile_program_wasm {
-  INPUT_FILE=./$1.tflite.mlir
-  OUTPUT_FILE=./$1_wasm.vmfb
+  INPUT_FILE=./"$1".tflite.mlir
+  OUTPUT_FILE=./"$1"_wasm.vmfb
   echo "Compiling '${INPUT_FILE}' to '${OUTPUT_FILE}'..."
 
-  ARTIFACTS_DIR=./$1-wasm_artifacts/
-  mkdir -p ${ARTIFACTS_DIR}
+  ARTIFACTS_DIR=./"$1"-wasm_artifacts/
+  mkdir -p "${ARTIFACTS_DIR}"
 
   # Compile from .mlir to .vmfb, dumping all the intermediate files and
   # compile-time statistics that we can.
-  ${IREE_COMPILE_PATH?} ${INPUT_FILE} \
+  "${IREE_COMPILE_PATH?}" "${INPUT_FILE}" \
     --iree-input-type=tosa \
     --iree-hal-target-backends=llvm \
     --iree-llvm-target-triple=wasm32-unknown-emscripten \
-    --iree-hal-dump-executable-sources-to=${ARTIFACTS_DIR} \
-    --iree-hal-dump-executable-binaries-to=${ARTIFACTS_DIR} \
+    --iree-hal-dump-executable-sources-to="${ARTIFACTS_DIR}" \
+    --iree-hal-dump-executable-binaries-to="${ARTIFACTS_DIR}" \
     --iree-scheduling-dump-statistics-format=csv \
     --iree-scheduling-dump-statistics-file=${ARTIFACTS_DIR}/$1_statistics.csv \
     --o ${OUTPUT_FILE}
 
   # Compress the .vmfb file (ideally it would be compressed already, but we can
   # expect some compression support from platforms like the web).
-  gzip -k -f ${OUTPUT_FILE}
+  gzip -k -f "${OUTPUT_FILE}"
 
-  if [ -d "$SAMPLE_BINARY_DIR" ]; then
+  if [[ -d "$SAMPLE_BINARY_DIR" ]]; then
     echo "Copying '${OUTPUT_FILE}' to '${SAMPLE_BINARY_DIR}' for benchmarking"
-    cp ${OUTPUT_FILE} ${SAMPLE_BINARY_DIR}
+    cp "${OUTPUT_FILE}" "${SAMPLE_BINARY_DIR}"
   fi
 }
 
@@ -154,27 +153,27 @@ function compile_program_wasm {
 #   Compiles program_name.tflite.mlir to program_name_native.vmfb, dumping
 #   statistics and intermediate files to disk.
 function compile_program_native {
-  INPUT_FILE=./$1.tflite.mlir
-  OUTPUT_FILE=./$1_native.vmfb
+  INPUT_FILE=./"$1".tflite.mlir
+  OUTPUT_FILE=./"$1"_native.vmfb
   echo "Compiling '${INPUT_FILE}' to '${OUTPUT_FILE}'..."
 
-  ARTIFACTS_DIR=./$1-native_artifacts/
-  mkdir -p ${ARTIFACTS_DIR}
+  ARTIFACTS_DIR=./"$1"-native_artifacts/
+  mkdir -p "${ARTIFACTS_DIR}"
 
   # Compile from .mlir to .vmfb, dumping all the intermediate files and
   # compile-time statistics that we can.
-  ${IREE_COMPILE_PATH?} ${INPUT_FILE} \
+  "${IREE_COMPILE_PATH?}" "${INPUT_FILE}" \
     --iree-input-type=tosa \
     --iree-hal-target-backends=llvm \
-    --iree-hal-dump-executable-sources-to=${ARTIFACTS_DIR} \
-    --iree-hal-dump-executable-binaries-to=${ARTIFACTS_DIR} \
+    --iree-hal-dump-executable-sources-to="${ARTIFACTS_DIR}" \
+    --iree-hal-dump-executable-binaries-to="${ARTIFACTS_DIR}" \
     --iree-scheduling-dump-statistics-format=csv \
-    --iree-scheduling-dump-statistics-file=${ARTIFACTS_DIR}/$1_statistics.csv \
-    --o ${OUTPUT_FILE}
+    --iree-scheduling-dump-statistics-file="${ARTIFACTS_DIR}"/$1_statistics.csv \
+    --o "${OUTPUT_FILE}"
 
   # Compress the .vmfb file (ideally it would be compressed already, but we can
   # expect some compression support from platforms like the web).
-  gzip -k -f ${OUTPUT_FILE}
+  gzip -k -f "${OUTPUT_FILE}"
 }
 
 # compile_program helper
--- experimental/web/run_native_benchmarks.sh
+++ experimental/web/run_native_benchmarks.sh
@@ -11,20 +11,16 @@
 # Sample usage (after generate_web_metrics.sh):
 #   run_native_benchmarks.sh /tmp/iree/web_metrics/
 
-set -e
+set -euo pipefail
 
 TARGET_DIR="$1"
-if [ -z "$TARGET_DIR" ]; then
-  echo "ERROR: Expected target directory (e.g. /tmp/iree/web_metrics/)"
+if [[ -z "$TARGET_DIR" ]]; then
+  >&2 echo "ERROR: Expected target directory (e.g. /tmp/iree/web_metrics/)"
   exit 1
 fi
 echo "Working in directory '$TARGET_DIR'"
-mkdir -p ${TARGET_DIR}
-cd ${TARGET_DIR}
-
-# Print all commands and the UTC time.
-# set -xeuo pipefail
-# export PS4='[$(date -u "+%T %Z")] '
+mkdir -p "${TARGET_DIR}"
+cd "${TARGET_DIR}"
 
 ###############################################################################
 # Run benchmarks                                                              #
@@ -32,37 +28,44 @@ cd ${TARGET_DIR}
 
 # Either build from source (setting this path), or use from the python packages.
 # IREE_BENCHMARK_MODULE_PATH=~/code/iree-build/iree/tools/iree-benchmark-module
-IREE_BENCHMARK_MODULE_PATH="D:\dev\projects\iree-build\iree\tools\iree-benchmark-module"
-# IREE_BENCHMARK_MODULE_PATH=iree-benchmark-module
+# IREE_BENCHMARK_MODULE_PATH="D:\dev\projects\iree-build\iree\tools\iree-benchmark-module"
+IREE_BENCHMARK_MODULE_PATH=iree-benchmark-module
 
 echo "Benchmarking DeepLabV3..."
-${IREE_BENCHMARK_MODULE_PATH?} \
+"${IREE_BENCHMARK_MODULE_PATH?}" \
     --module_file=./deeplabv3_native.vmfb \
-    --driver=dylib --task_topology_group_count=1 \
-    --entry_function=main --function_input=1x257x257x3xf32 \
+    --driver=dylib \
+    --task_topology_group_count=1 \
+    --entry_function=main \
+    --function_input=1x257x257x3xf32 \
     --benchmark_min_time=3
 
 echo ""
 echo "Benchmarking MobileSSD..."
-${IREE_BENCHMARK_MODULE_PATH?} \
+"${IREE_BENCHMARK_MODULE_PATH?}" \
     --module_file=./mobile_ssd_v2_float_coco_native.vmfb \
-    --driver=dylib --task_topology_group_count=1 \
-    --entry_function=main --function_input=1x320x320x3xf32 \
+    --driver=dylib \
+    --task_topology_group_count=1 \
+    --entry_function=main \
+    --function_input=1x320x320x3xf32 \
     --benchmark_min_time=3
 
 echo ""
 echo "Benchmarking PoseNet..."
-${IREE_BENCHMARK_MODULE_PATH?} \
+"${IREE_BENCHMARK_MODULE_PATH?}" \
     --module_file=./posenet_native.vmfb \
-    --driver=dylib --task_topology_group_count=1 \
-    --entry_function=main --function_input=1x353x257x3xf32 \
+    --driver=dylib \
+    --task_topology_group_count=1 \
+    --entry_function=main \
+    --function_input=1x353x257x3xf32 \
     --benchmark_min_time=3
 
 echo ""
 echo "Benchmarking MobileBertSquad..."
-${IREE_BENCHMARK_MODULE_PATH?} \
+"${IREE_BENCHMARK_MODULE_PATH?}" \
     --module_file=./mobilebertsquad_native.vmfb \
-    --driver=dylib --task_topology_group_count=1 \
+    --driver=dylib \
+    --task_topology_group_count=1 \
     --entry_function=main \
     --function_input=1x384xi32 \
     --function_input=1x384xi32 \
@@ -71,16 +74,20 @@ ${IREE_BENCHMARK_MODULE_PATH?} \
 
 echo ""
 echo "Benchmarking MobileNetV2..."
-${IREE_BENCHMARK_MODULE_PATH?} \
+"${IREE_BENCHMARK_MODULE_PATH?}" \
     --module_file=./mobilenet_v2_1.0_224_native.vmfb \
-    --driver=dylib --task_topology_group_count=1 \
-    --entry_function=main --function_input=1x224x224x3xf32 \
+    --driver=dylib \
+    --task_topology_group_count=1 \
+    --entry_function=main \
+    --function_input=1x224x224x3xf32 \
     --benchmark_min_time=3
 
 echo ""
 echo "Benchmarking MobileNetV3Small..."
-${IREE_BENCHMARK_MODULE_PATH?} \
+"${IREE_BENCHMARK_MODULE_PATH?}" \
     --module_file=./MobileNetV3SmallStaticBatch_native.vmfb \
-    --driver=dylib --task_topology_group_count=1 \
-    --entry_function=main --function_input=1x224x224x3xf32 \
+    --driver=dylib \
+    --task_topology_group_count=1 \
+    --entry_function=main \
+    --function_input=1x224x224x3xf32 \
     --benchmark_min_time=3
