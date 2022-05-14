011f3a0852e8b8a6200b50e75d8ee3d1dfd742b2
Stella Laurenzo <stellaraccident@gmail.com>
2022-05-14 13:35:47 -0700

    Add pydm project boilerplate.

---
+++ experimental/pydm_compiler/README.md
@@ -0,0 +1,39 @@
+# PyDM compiler framework
+
+PyDM ("Python DataModel") is an IREE dialect which models the Python
+language in a way suitable for compilation and offline execution. While the
+core compiler itself is encapsulated in the `iree_pydm` dialect, this
+directory contains "the rest of the story", consisting of:
+
+* User-level APIs
+* Example integrations
+* Test suite
+
+This is only loosely coupled to IREE and should be seen as an incubation
+staging area vs a signal that the end result will be tightly bound to IREE
+itself.
+
+## Development
+
+This package is pure Python and can be installed via:
+
+```
+pip install -e experimental/pydm_compiler/
+```
+
+The usual cautions about using a virtual environment, etc are encouraged.
+
+The above presumes that the `iree-compiler` and `iree-runtime` dependencies
+can be resolved. Typically, this is done by installing from nightlies:
+
+```
+pip install -e experimental/pydm_compiler/ \
+  -f https://github.com/google/iree/releases
+```
+
+If you are already using IREE, setup for Python development, then you should
+install without deps:
+
+```
+pip install --no-deps -e experimental/pydm_compiler/
+```
---
+++ experimental/pydm_compiler/setup.py
@@ -0,0 +1,42 @@
+# Copyright 2022 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import json
+import os
+from setuptools import find_namespace_packages, setup
+
+# Setup and get version information.
+THIS_DIR = os.path.realpath(os.path.dirname(__file__))
+IREESRC_DIR = os.path.join(THIS_DIR, "..", "..")
+VERSION_INFO_FILE = os.path.join(IREESRC_DIR, "version_info.json")
+
+
+def load_version_info():
+  with open(VERSION_INFO_FILE, "rt") as f:
+    return json.load(f)
+
+
+try:
+  version_info = load_version_info()
+except FileNotFoundError:
+  print("version_info.json not found. Using defaults")
+  version_info = {}
+
+PACKAGE_SUFFIX = version_info.get("package-suffix") or "-dev"
+PACKAGE_VERSION = version_info.get("package-version") or "0.1dev1"
+
+setup(
+    name=f"iree-pydm{PACKAGE_SUFFIX}",
+    version=f"{PACKAGE_VERSION}",
+    packages=find_namespace_packages(include=[
+        "iree.pydm",
+        "iree.pydm.*",
+    ],),
+    install_requires=[
+        "iree-compiler",
+        "iree-runtime",
+    ]
+)
