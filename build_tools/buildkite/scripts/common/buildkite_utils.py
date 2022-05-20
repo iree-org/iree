0d1be3efa37bc8dc9cdb0bd0b391ba31413b9476
Jerry Wu <cheyuw@google.com>
2022-05-20 13:46:42 +0000

    Dump build JSON object after the pipeline finished (#9129)
    Co-authored-by: Geoffrey Martin-Noble <gcmn@google.com>

---
+++ build_tools/buildkite/scripts/common/buildkite_utils.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# Copyright 2022 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+from typing import Any, Dict, Optional
+from pybuildkite import buildkite
+
+# Type signature of Buildkite build object.
+BuildObject = Dict[str, Any]
+
+
+def get_build_number(build: BuildObject) -> int:
+  return build["number"]
+
+
+def get_build_state(build: BuildObject) -> buildkite.BuildState:
+  return buildkite.BuildState(build.get("state"))
+
+
+def linkify(url: str, text: Optional[str] = None):
+  """Make a link clickable using ANSI escape sequences.
+    See https://buildkite.com/docs/pipelines/links-and-images-in-log-output
+  """
+
+  if text is None:
+    text = url
+
+  return f"\033]1339;url={url};content={text}\a"
