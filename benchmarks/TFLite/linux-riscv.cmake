1dcc7a0043749991fa6453a0846649aff59f9480
Che-Yu Wu <cheyuw@google.com>
2022-05-26 23:14:07 +0000

    Update config. Add RV32.

--- benchmarks/TFLite/linux-riscv.cmake
+++ benchmarks/TFLite/linux-riscv.cmake
@@ -16,15 +16,27 @@
 #                                                                              #
 ################################################################################
 
-set(LINUX_RISCV64_GENERIC_RV64_CPU_TRANSLATION_FLAGS
+set(LINUX_RV64_GENERIC_CPU_TRANSLATION_FLAGS
   "--iree-input-type=tosa"
   "--iree-llvm-target-triple=riscv64"
   "--iree-llvm-target-cpu=generic-rv64"
   "--iree-llvm-target-abi=lp64d"
-  "--iree-llvm-target-cpu-features=+m,+a,+f,+d,+c"
+  "--iree-llvm-target-cpu-features=+m,+a,+f,+d,+v"
+  "--riscv-v-vector-bits-min=256"
+  "--riscv-v-fixed-length-vector-lmul-max=8"
 )
 
-# CPU, Dylib-Sync, RISCV64, full-inference
+set(LINUX_RV32_GENERIC_CPU_TRANSLATION_FLAGS
+  "--iree-input-type=tosa"
+  "--iree-llvm-target-triple=riscv32-pc-linux-elf"
+  "--iree-llvm-target-cpu=generic-rv32"
+  "--iree-llvm-target-abi=ilp32"
+  "--iree-llvm-target-cpu-features=+m,+a,+f,+zvl512b,+zve32x"
+  "--riscv-v-vector-bits-min=512"
+  "--riscv-v-fixed-length-vector-lmul-max=8"
+)
+
+# CPU, Dylib-Sync, RV64-Generic, full-inference
 iree_benchmark_suite(
   MODULES
     "${DEEPLABV3_FP32_MODULE}"
@@ -35,39 +47,15 @@ iree_benchmark_suite(
     "${MOBILENET_V3SMALL_MODULE}"
 
   BENCHMARK_MODES
-    "dylib-sync,full-inference,default-flags"
+    "full-inference,default-flags"
   TARGET_BACKEND
     "dylib-llvm-aot"
   TARGET_ARCHITECTURE
-    "CPU-RISCV64-Generic"
+    "CPU-RV64-Generic"
   TRANSLATION_FLAGS
-    ${LINUX_RISCV64_GENERIC_RV64_CPU_TRANSLATION_FLAGS}
+    ${LINUX_RV64_GENERIC_CPU_TRANSLATION_FLAGS}
   BENCHMARK_TOOL
     iree-benchmark-module
   DRIVER
     "dylib-sync"
-)
-
-# CPU, Dylib, RISCV64, full-inference
-iree_benchmark_suite(
-  MODULES
-    "${DEEPLABV3_FP32_MODULE}"
-    "${MOBILESSD_FP32_MODULE}"
-    "${POSENET_FP32_MODULE}"
-    "${MOBILEBERT_FP32_MODULE}"
-    "${MOBILENET_V2_MODULE}"
-    "${MOBILENET_V3SMALL_MODULE}"
-
-  BENCHMARK_MODES
-    "dylib,full-inference,default-flags"
-  TARGET_BACKEND
-    "dylib-llvm-aot"
-  TARGET_ARCHITECTURE
-    "CPU-RISCV64-Generic"
-  TRANSLATION_FLAGS
-    ${LINUX_RISCV64_GENERIC_RV64_CPU_TRANSLATION_FLAGS}
-  BENCHMARK_TOOL
-    iree-benchmark-module
-  DRIVER
-    "dylib"
 )
