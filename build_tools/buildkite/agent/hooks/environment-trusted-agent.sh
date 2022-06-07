bb1fef2d2844645d61815de797fa853025734dea
Geoffrey Martin-Noble <gcmn@google.com>
2022-06-06 17:53:20 -0700

    Move the setting of environment variables to environment agent hook

---
+++ build_tools/buildkite/agent/hooks/environment-trusted-agent.sh
@@ -0,0 +1,9 @@
+# Copyright 2022 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+export BUILDKITE_ANNOTATION_CONTEXT="$${BUILDKITE_STEP_ID}"
+export IREE_BUILDKITE_ACCESS_TOKEN="$(gcloud secrets versions access latest \
+          --secret=iree-buildkite-privileged)"
