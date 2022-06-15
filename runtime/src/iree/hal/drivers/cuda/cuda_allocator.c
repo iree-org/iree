04788d1016bed3c111b182ff08d15eb69e67bc55
Ben Vanik <ben.vanik@gmail.com>
2022-06-14 22:19:25 -0700

    Mocking up enough of the CUDA semaphore to run in coroutines.

--- runtime/src/iree/hal/drivers/cuda/cuda_allocator.c
+++ runtime/src/iree/hal/drivers/cuda/cuda_allocator.c
@@ -200,6 +200,7 @@ static iree_status_t iree_hal_cuda_allocator_allocate_buffer(
   void* host_ptr = NULL;
   CUdeviceptr device_ptr = 0;
   IREE_TRACE_ZONE_BEGIN_NAMED(z0, "iree_hal_cuda_buffer_allocate");
+  IREE_TRACE_ZONE_APPEND_VALUE(z0, allocation_size);
   if (iree_all_bits_set(memory_type, IREE_HAL_MEMORY_TYPE_DEVICE_LOCAL)) {
     // Device local case.
     if (iree_all_bits_set(memory_type, IREE_HAL_MEMORY_TYPE_HOST_VISIBLE)) {
