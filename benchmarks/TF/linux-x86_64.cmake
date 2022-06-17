999c6755822a1794bffd68c63e880f48bcf1a4d1
Che-Yu Wu <cheyuw@google.com>
2022-06-17 17:30:59 +0000

    Add headers.

--- benchmarks/TF/linux-x86_64.cmake
+++ benchmarks/TF/linux-x86_64.cmake
@@ -1,3 +1,21 @@
+# Copyright 2022 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+################################################################################
+#                                                                              #
+# Default benchmark configurations                                             #
+#                                                                              #
+# Each suite benchmarks a list of modules with configurations specifying a     #
+# target architecture and runtime characteristics (e.g. threads/cores). These  #
+# benchmarks only configure IREE compilation and runtime flags for the target  #
+# architecture and do *not* include any non-default flags. No non-default      #
+# flags should be added here.                                                  #
+#                                                                              #
+################################################################################
+
 set(LINUX_X86_64_CASCADELAKE_CPU_COMPILATION_FLAGS
   "--iree-input-type=mhlo"
   "--iree-llvm-target-cpu=cascadelake"
