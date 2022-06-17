1f38dba88201cd6755ac6002941c60879830cd41
Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>
2022-06-17 21:53:34 +0200

    Add LLVMGPU transform to rewrite scf.foreach_thread to gpu.thread + translation_info attr. (#9545)

--- compiler/src/iree/compiler/Codegen/LLVMGPU/TransformDialectExtensions/TransformDialectLLVMGPUExtensions.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/TransformDialectExtensions/TransformDialectLLVMGPUExtensions.cpp
@@ -6,6 +6,14 @@
 
 #include "TransformDialectLLVMGPUExtensions.h"
 
+#include "iree-dialects/Transforms/Functional.h"
+#include "iree/compiler/Dialect/HAL/IR/HALOps.h"
+#include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/GPU/IR/GPUDialect.h"
+#include "mlir/Dialect/SCF/SCF.h"
+#include "mlir/IR/OpImplementation.h"
+
 using namespace mlir;
 using namespace mlir::iree_compiler;
 using namespace mlir::iree_compiler::IREE;
@@ -26,6 +34,119 @@ void mlir::iree_compiler::registerTransformDialectLLVMGPUExtension(
 
 // TODO: Maybe we need both a transform.iree.cpu.bufferize and a
 // transform.iree.gpu.bufferize rather than a single common bufferize op?
+
+//===---------------------------------------------------------------------===//
+// Patterns for ForeachThreadToGpu rewrite.
+//===---------------------------------------------------------------------===//
+
+struct ForeachThreadToGpuRewriter
+    : public OpRewritePattern<scf::ForeachThreadOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  FailureOr<SmallVector<Value>> returningMatchAndRewrite(
+      scf::ForeachThreadOp foreachThreadOp, PatternRewriter &rewriter) const;
+
+  LogicalResult matchAndRewrite(scf::ForeachThreadOp foreachThreadOp,
+                                PatternRewriter &rewriter) const override {
+    return returningMatchAndRewrite(foreachThreadOp, rewriter);
+  }
+};
+
+FailureOr<SmallVector<Value>>
+ForeachThreadToGpuRewriter::returningMatchAndRewrite(
+    scf::ForeachThreadOp foreachThreadOp, PatternRewriter &rewriter) const {
+  if (foreachThreadOp.getNumResults() > 0)
+    return foreachThreadOp->emitError(
+        "only bufferized scf.foreach_thread lowers to gpu.thread");
+  if (foreachThreadOp.getNumThreads().size() > 3)
+    return foreachThreadOp->emitError(
+        "scf.foreach_thread with rank > 3 does not lower to gpu.thread");
+
+  // Step 1. Create the gpu.thread ops
+  Location loc = foreachThreadOp.getLoc();
+  IndexType indexType = rewriter.getIndexType();
+  SmallVector<Value> threadCount = foreachThreadOp.getNumThreads();
+  SmallVector<gpu::Dimension, 3> gpuDims{gpu::Dimension::x, gpu::Dimension::y,
+                                         gpu::Dimension::z};
+  SmallVector<Value> threadOps;
+  threadOps.reserve(3);
+  for (int64_t idx : llvm::seq<int64_t>(0, threadCount.size())) {
+    threadOps.push_back(
+        rewriter.create<gpu::ThreadIdOp>(loc, indexType, gpuDims[idx]));
+  }
+  threadCount.resize(3, rewriter.create<arith::ConstantIndexOp>(loc, 1));
+
+  // Step 2. Move the body of foreachThreadOp after the op.
+  // Erase the terminator first, it will not be used.
+  rewriter.eraseOp(foreachThreadOp.getTerminator());
+  Block *block = foreachThreadOp->getBlock();
+  block->getOperations().splice(
+      Block::iterator(foreachThreadOp),
+      foreachThreadOp.getRegion().front().getOperations());
+
+  // Step 3. RAUW thread indices to thread ops.
+  for (auto it : llvm::zip(foreachThreadOp.getThreadIndices(), threadOps))
+    std::get<0>(it).replaceAllUsesWith(std::get<1>(it));
+
+  rewriter.eraseOp(foreachThreadOp);
+
+  return threadCount;
+}
+
+//===---------------------------------------------------------------------===//
+// IREE-specific LLVMGPU transformations.
+//===---------------------------------------------------------------------===//
+
+// TODO: if the number of threads was wired like the workgroup_count, we could
+// reuse most of the code and not require a static number of threads.
+// TODO: synchronizations for imperfectly nested stuff.
+DiagnosedSilenceableFailure
+transform_dialect::ForeachThreadToGpuAndTranslationInfo::apply(
+    transform::TransformResults &results, transform::TransformState &state) {
+  llvm::StringMap<IREE::HAL::ExecutableExportOp> exportOps;
+  state.getTopLevel()->walk(
+      [&](IREE::HAL::ExecutableExportOp op) { exportOps[op.sym_name()] = op; });
+
+  if (state.getTopLevel()
+          ->walk<WalkOrder::PostOrder>([&](scf::ForeachThreadOp op) {
+            auto funcOp = op->getParentOfType<func::FuncOp>();
+            auto exportOp = exportOps.lookup(funcOp.getName());
+            // Step 1. Apply the rewrite.
+            auto workgroupSize = functional::applyReturningPatternAt(
+                ForeachThreadToGpuRewriter(getContext()), op);
+            if (failed(workgroupSize)) return WalkResult::interrupt();
+
+            // Step 2. Ensure the workgroupSize is static and extract it.
+            SmallVector<int64_t, 3> staticThreadCount;
+            for (auto en : llvm::enumerate(*workgroupSize)) {
+              auto maybeInt = getConstantIntValue(en.value());
+              if (!maybeInt) {
+                op->emitError("scf.foreach_thread rank #")
+                    << en.index() << " is not a constant and cannot be lowered "
+                    << "into the translation_info attr";
+                return WalkResult::interrupt();
+              }
+              staticThreadCount.push_back(*maybeInt);
+            }
+
+            // Step 3. Check and fill the attribute that requires
+            OpBuilder b(exportOp);
+            auto maybeExistingAttr =
+                exportOp->getAttr(exportOp.workgroup_sizeAttrName());
+            auto newAttr = b.getIndexArrayAttr(staticThreadCount);
+            if (maybeExistingAttr && maybeExistingAttr != newAttr) {
+              exportOp->emitError("multiple mismatching workgroup_size ")
+                  << "attributes found: " << maybeExistingAttr << " and "
+                  << newAttr;
+              return WalkResult::interrupt();
+            }
+            exportOp->setAttr(exportOp.workgroup_sizeAttrName(), newAttr);
+            return WalkResult::advance();
+          })
+          .wasInterrupted())
+    return DiagnosedSilenceableFailure::definiteFailure();
+  return DiagnosedSilenceableFailure::success();
+}
 
 #define GET_OP_CLASSES
 #include "iree/compiler/Codegen/LLVMGPU/TransformDialectExtensions/TransformDialectLLVMGPUExtensionsOps.cpp.inc"
--- compiler/src/iree/compiler/Codegen/LLVMGPU/test/linalg_transform.mlir
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/test/linalg_transform.mlir
@@ -1,4 +1,10 @@
-// RUN: iree-opt %s  --pass-pipeline='hal.executable(hal.executable.variant(iree-llvmgpu-lower-executable-target-pass))' --iree-codegen-llvmgpu-use-transform-dialect=%p/linalg_transform_spec.mlir | FileCheck %s
+// RUN: iree-opt %s  --pass-pipeline='hal.executable(hal.executable.variant(iree-llvmgpu-lower-executable-target-pass))' \
+// RUN: --iree-codegen-llvmgpu-use-transform-dialect=%p/transform_dialect_codegen_bufferize_spec.mlir | \
+// RUN: FileCheck %s
+
+// RUN: iree-opt %s  --pass-pipeline='hal.executable(hal.executable.variant(iree-llvmgpu-lower-executable-target-pass))' \
+// RUN: --iree-codegen-llvmgpu-use-transform-dialect=%p/transform_dialect_codegen_foreach_to_gpu_spec.mlir | \
+// RUN: FileCheck %s --check-prefix=FOREACH-TO-GPU
 
 #device_target_cuda = #hal.device.target<"cuda", {executable_targets = [#hal.executable.target<"cuda", "cuda-nvptx-fb", {target_arch = "sm_35"}>]}>
 #executable_layout = #hal.executable.layout<push_constants = 0, sets = [#hal.descriptor_set.layout<0, bindings = [#hal.descriptor_set.binding<0, storage_buffer>]>]>
@@ -6,6 +12,8 @@
 module attributes {hal.device.targets = [#device_target_cuda]} {
   hal.executable private @matmul_static_dispatch_0 {
     hal.executable.variant public @cuda_nvptx_fb, target = #executable_target_cuda_nvptx_fb {
+
+      // FOREACH-TO-GPU: hal.executable.export {{.*}} workgroup_size = [7 : index, 9 : index, 1 : index]
       hal.executable.export public @matmul_static_dispatch_0 ordinal(0) layout(#executable_layout)
       builtin.module {
         func.func @matmul_static_dispatch_0() {
@@ -24,6 +32,18 @@ module attributes {hal.device.targets = [#device_target_cuda]} {
           // CHECK-NEXT: linalg.fill ins(%{{.*}} : f32) outs(%{{.*}} : memref<250x1020xf32>)
           // CHECK-NEXT: linalg.matmul{{.*}}ins(%{{.*}} : memref<250x500xf32>, memref<500x1020xf32>) outs(%{{.*}} : memref<250x1020xf32>)
           // CHECK-NEXT: return
+
+
+          // FOREACH-TO-GPU: %[[TIDX:.*]] = gpu.thread_id  x
+          // FOREACH-TO-GPU: %[[TIDY:.*]] = gpu.thread_id  y
+          // FOREACH-TO-GPU: affine.min #{{.*}}()[%[[TIDX]]]
+          // FOREACH-TO-GPU: affine.min #{{.*}}()[%[[TIDY]]]
+          // FOREACH-TO-GPU: affine.apply #{{.*}}()[%[[TIDX]]]
+          // FOREACH-TO-GPU: %[[svA:.*]] = memref.subview {{.*}} : memref<250x500xf32> to memref<?x500xf32
+          // FOREACH-TO-GPU: affine.apply #{{.*}}()[%[[TIDY]]]
+          // FOREACH-TO-GPU: %[[svB:.*]] = memref.subview {{.*}} : memref<500x1020xf32> to memref<500x?xf32
+          // FOREACH-TO-GPU: %[[svC:.*]] = memref.subview {{.*}} : memref<250x1020xf32> to memref<?x?xf32
+          // FOREACH-TO-GPU: linalg.matmul ins(%[[svA]], %[[svB]] : memref<?x500xf32{{.*}}>, memref<500x?xf32{{.*}}>) outs(%[[svC]] : memref<?x?xf32{{.*}}>)
 
           %6 = linalg.matmul ins(%3, %4 : tensor<250x500xf32>, tensor<500x1020xf32>) outs(%5 : tensor<250x1020xf32>) -> tensor<250x1020xf32>
           flow.dispatch.tensor.store %6, %2, offsets = [0, 0], sizes = [250, 1020], strides = [1, 1] : tensor<250x1020xf32> -> !flow.dispatch.tensor<readwrite:250x1020xf32>
