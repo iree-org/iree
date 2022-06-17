fc9670e2a198b96c5aaaa4962ed56d71a7db41db
Jacques Pienaar <jpienaar@google.com>
2022-06-17 09:51:27 -0700

    Add control flow canonicalization along MHLO import path too (#9525)
    Previously this was only done on XLA import path.
    
    I considered adding canonicalization too, but wanted to evaluate first with just control flow support added. Keeping some of the XLA legacy work there for now.
    
    Fixes #9499

--- compiler/src/iree/compiler/InputConversion/MHLO/Passes.cpp
+++ compiler/src/iree/compiler/InputConversion/MHLO/Passes.cpp
@@ -52,6 +52,9 @@ void registerMHLOConversionPassPipeline() {
 
 // Prepare HLO for use as an input to the Flow dialect.
 void buildMHLOInputConversionPassPipeline(OpPassManager &passManager) {
+  passManager.addNestedPass<func::FuncOp>(
+      mhlo::createLegalizeControlFlowPass());
+
   // Currently we don't handle SCF ops well and have to convert them all to CFG.
   // In the future it would be nice if we could have all of flow be both scf
   // and cfg compatible.
