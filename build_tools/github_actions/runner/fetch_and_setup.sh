ef4aa07b21c03274d820291699629f4c2cb546ab
Geoffrey Martin-Noble <gcmn@google.com>
2022-07-18 09:56:45 -0700

    Avoid overwriting fetch script

--- build_tools/github_actions/runner/fetch_and_setup.sh
+++ build_tools/github_actions/runner/fetch_and_setup.sh
@@ -12,18 +12,19 @@
 # scripts instead of fetching them directly from HEAD.
 
 set -euo pipefail
+shopt -s extglob
+
+SCRIPT="$( readlink -f -- "$0"; )"
+SCRIPT_BASENAME="$(basename ${SCRIPT})"
 
 if [[ "$(whoami)" != "runner" ]]; then
   echo "Current user is not 'runner'. Rerunning script as 'runner'."
-  SCRIPT="$( readlink -f -- "$0"; )"
   sudo su runner --shell /bin/bash --command "${SCRIPT}"
   exit
 fi
 
 cd "${HOME}"
-shopt -s extglob
-rm -rf -v !("actions-runner"|"fetch_and_setup.sh")
-shopt -u extglob
+rm -rf -v !("actions-runner"|"${SCRIPT_BASENAME}")
 
 cd /tmp/
 rm -rf /tmp/iree
@@ -35,7 +36,10 @@ git fetch origin runner-setup
 git checkout runner-setup
 cd ..
 
-cp -r iree/build_tools/github_actions/runner/* "${HOME}/"
+cd iree/build_tools/github_actions/runner/
+# Bash doesn't read the whole script into memory when executing, so if you
+# overwrite it during execution, madness ensues.
+cp -r !("${SCRIPT_BASENAME}") "${HOME}/"
 
 cd "${HOME}"
 rm -rf /tmp/iree
