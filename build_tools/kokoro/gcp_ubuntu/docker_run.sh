5dcde951d8b748d9b53e62f8978f728e95f88728
Geoffrey Martin-Noble <gcmn@google.com>
2022-07-19 15:08:13 -0700

    Use common scripting for Docker in GitHub actions (#9842)

--- build_tools/kokoro/gcp_ubuntu/docker_run.sh
+++ build_tools/kokoro/gcp_ubuntu/docker_run.sh
@@ -4,9 +4,8 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-# Runs docker configured for usage with Kokoro actions, translating Kokoro
-# environment variables into generic ones and then invoking the generic
-# docker_run script.
+# Runs docker configured for usage with Kokoro, translating Kokoro environment
+# variables into generic ones and then invoking the generic docker_run script.
 
 set -euo pipefail
 
