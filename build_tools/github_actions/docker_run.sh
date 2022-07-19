4c08bd07e68096c0d85d98a5c3cde5f45e3f9935
Geoffrey Martin-Noble <gcmn@google.com>
2022-07-19 13:54:07 -0700

    Use common docker scripting in GitHub actions

---
+++ build_tools/github_actions/docker_run.sh
@@ -0,0 +1,16 @@
+# Copyright 2020 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# Runs docker configured for usage with Kokoro, translating Kokoro-specific
+# environment variables into generic ones and then invoking `docker run` with
+# some arguments to make it behave itself.
+
+set -euo pipefail
+
+export DOCKER_WORKDIR="${GITHUB_WORKSPACE}"
+export DOCKER_TMPDIR="${RUNNER_TEMP}"
+
+"${GITHUB_WORKSPACE}/build_tools/docker/docker_run.sh" "$@"
