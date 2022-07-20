414a308964f333faae77c85ef21b3b95f7c36bff
Jacques Pienaar <jpienaar@google.com>
2022-07-19 23:11:58 -0700

    Update to using public header (NFC) (#9847)
    assert.h is not a public header of the corresponding cc_library.

--- runtime/bindings/tflite/java/org/tensorflow/lite/native/interpreter_jni.cc
+++ runtime/bindings/tflite/java/org/tensorflow/lite/native/interpreter_jni.cc
@@ -6,7 +6,7 @@
 
 #include <jni.h>
 
-#include "iree/base/assert.h"
+#include "iree/base/api.h"
 
 // NOTE: we pull in our own copy here in case the tflite API changes upstream.
 #define TFL_COMPILE_LIBRARY 1
