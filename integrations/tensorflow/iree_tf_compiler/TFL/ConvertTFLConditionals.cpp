7a1e5f28e036e787206d835775b855178d034ae9
Scott Reid <scott@femtosense.ai>
2022-07-22 21:40:09 +0000

    Copyright?

--- integrations/tensorflow/iree_tf_compiler/TFL/ConvertTFLConditionals.cpp
+++ integrations/tensorflow/iree_tf_compiler/TFL/ConvertTFLConditionals.cpp
@@ -1,4 +1,4 @@
-// Copyright 2022 Scott Reid
+// Copyright 2022 Scott Reid, Femtosense
 //
 // Licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
