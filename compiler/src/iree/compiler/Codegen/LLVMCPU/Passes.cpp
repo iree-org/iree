09074f582d49309bcf605cf5a573de4c73fdb43a
Diego Caballero <diegocaballero@google.com>
2022-07-25 05:45:41 +0000

    clang-format

--- compiler/src/iree/compiler/Codegen/LLVMCPU/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMCPU/Passes.cpp
@@ -66,7 +66,6 @@ struct LinalgCPUVectorLoweringPassOptions : LinalgVectorLoweringPassOptions {
   }
 };
 
-
 //===---------------------------------------------------------------------===//
 // Default allocation functions for CPU backend
 //===---------------------------------------------------------------------===//
