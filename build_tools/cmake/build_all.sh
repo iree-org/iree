b6ddd1c8b8f2188fbd399cd82430d4501074b1bb
Geoffrey Martin-Noble <gcmn@google.com>
2022-07-26 14:10:20 -0700

    Add multi-stage full build and test (#9877)
    This creates jobs to build all of IREE on one machine and then test on
    another, passing the build directory around. This same build directory
    can be used in other jobs also (e.g. for GPU testing, which I'm punting
    on for now).
    
    I ran into issues using the GitHub action artifacts for this. I knew
    we'd get suboptimal transfer speeds since it's hosted on Azure and
    we'd probably want another solution eventually, but they were much
    worse than I expected. So this uses a GCS bucket for the transfer
    instead.
    
    We don't want presubmit jobs to be able to affect builds from other
    commits, so that's a bit tricky. To get around this, I have separate
    buckets for presubmit and postsubmit. The runners only have "create"
    and "read" permissions, not "delete" permissions, which means they
    cannot remove or overwrite existing artifacts. They upload to a file
    including the commit name in its path. If a file is already there, the
    upload (and therefore the job) would fail. I think this avoids the
    possibility of one build affecting another, even if someone were trying
    to do so intentionally. If we move to avoiding rerunning a job if the
    artifact for it already exists, we'll need to revisit this.
    
    Part of https://github.com/iree-org/iree/issues/9881

---
+++ build_tools/cmake/build_all.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Copyright 2019 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# Build "all" of the IREE project. Designed for CI, but can be run locally.
+
+set -xeuo pipefail
+
+ROOT_DIR="${ROOT_DIR:-$(git rev-parse --show-toplevel)}"
+cd "${ROOT_DIR}"
+
+CMAKE_BIN=${CMAKE_BIN:-$(which cmake)}
+BUILD_DIR="${1:-${IREE_BUILD_DIR:-build}}"
+IREE_ENABLE_ASSERTIONS="${IREE_ENABLE_ASSERTIONS:-ON}"
+IREE_ENABLE_CCACHE="${IREE_ENABLE_CCACHE:-OFF}"
+
+"$CMAKE_BIN" --version
+ninja --version
+
+if [[ -d "${BUILD_DIR}" ]]; then
+  echo "Build directory '${BUILD_DIR}' already exists. Will use cached results there."
+else
+  echo "Build directory '${BUILD_DIR}' does not already exist. Creating a new one."
+  mkdir "${BUILD_DIR}"
+fi
+
+declare -a CMAKE_ARGS=(
+  "-G" "Ninja"
+  "-B" "${BUILD_DIR}"
+  # Let's make linking fast
+  "-DIREE_ENABLE_LLD=ON"
+  "-DCMAKE_BUILD_TYPE=RelWithDebInfo"
+
+  # Enable docs build on the CI. The additional builds are pretty fast and
+  # give us early warnings for some types of website publication errors.
+  "-DIREE_BUILD_DOCS=ON"
+
+  # Enable building the python bindings on CI.
+  "-DIREE_BUILD_PYTHON_BINDINGS=ON"
+
+  "-DIREE_ENABLE_ASSERTIONS=${IREE_ENABLE_ASSERTIONS}"
+  "-DIREE_ENABLE_CCACHE=${IREE_ENABLE_CCACHE}"
+
+  # Enable CUDA compiler and runtime builds unconditionally. Our CI images all
+  # have enough deps to at least build CUDA support and compile CUDA binaries
+  # (but not necessarily test on real hardware).
+  "-DIREE_HAL_DRIVER_CUDA=ON"
+  "-DIREE_TARGET_BACKEND_CUDA=ON"
+  "${ROOT_DIR}"
+)
+
+"$CMAKE_BIN" "${CMAKE_ARGS[@]}"
+echo "Building all"
+echo "------------"
+"$CMAKE_BIN" --build "${BUILD_DIR}" -- -k 0
+
+echo "Building test deps"
+echo "------------------"
+"$CMAKE_BIN" --build "${BUILD_DIR}" --target iree-test-deps -- -k 0
