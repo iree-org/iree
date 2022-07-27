5be200eea817529cc65e807f7406e7570793e53a
Geoffrey Martin-Noble <gcmn@google.com>
2022-07-27 14:01:36 -0700

    Job to build TF binaries

--- integrations/tensorflow/symlink_binaries.sh
+++ integrations/tensorflow/symlink_binaries.sh
@@ -7,15 +7,18 @@
 # Symlinks built binaries from the bazel-bin/ directory into the corresponding
 # python packages.
 
-set -e
+set -euo pipefail
 
 cd "$(dirname $0)"
-if [ -f bazel-bin/iree_tf_compiler/iree-import-tf ]; then
-  ln -sf $PWD/bazel-bin/iree_tf_compiler/iree-import-tf python_projects/iree_tf/iree/tools/tf/
+
+BINARIES_DIR="${1:-${PWD}/bazel-bin/iree_tf_compiler}"
+
+if [ -f "${BINARIES_DIR}/iree-import-tf" ]; then
+  ln -sf "${BINARIES_DIR}/iree-import-tf" python_projects/iree_tf/iree/tools/tf/
 fi
-if [ -f bazel-bin/iree_tf_compiler/iree-import-tflite ]; then
-  ln -sf $PWD/bazel-bin/iree_tf_compiler/iree-import-tflite python_projects/iree_tflite/iree/tools/tflite/
+if [ -f "${BINARIES_DIR}/iree-import-tflite" ]; then
+  ln -sf "${BINARIES_DIR}/iree-import-tflite" python_projects/iree_tflite/iree/tools/tflite/
 fi
-if [ -f bazel-bin/iree_tf_compiler/iree-import-xla ]; then
-  ln -sf $PWD/bazel-bin/iree_tf_compiler/iree-import-xla python_projects/iree_xla/iree/tools/xla/
+if [ -f "${BINARIES_DIR}/iree-import-xla" ]; then
+  ln -sf "${BINARIES_DIR}/iree-import-xla" python_projects/iree_xla/iree/tools/xla/
 fi
