bd61a78442f53a47a9170991e8450ed1f55867ac
Geoffrey Martin-Noble <gcmn@google.com>
2022-08-01 14:18:26 -0700

    Add IREE_HOST_BINARY_ROOT

--- build_tools/kokoro/gcp_ubuntu/cmake/linux/riscv64/build.sh
+++ build_tools/kokoro/gcp_ubuntu/cmake/linux/riscv64/build.sh
@@ -26,7 +26,8 @@ git submodule update --init --jobs 8 --depth 1
 export ROOT_DIR="$(git rev-parse --show-toplevel)"
 export BUILD_HOST_DIR="${ROOT_DIR}/build-host"
 export BUILD_RISCV_DIR="${ROOT_DIR}/build-riscv"
-export LLVM_BINDIR="${BUILD_HOST_DIR}/third_party/llvm-project/llvm/bin"
+export LLVM_BIN_DIR="${BUILD_HOST_DIR}/third_party/llvm-project/llvm/bin"
+export IREE_HOST_BINARY_ROOT="${ROOT_DIR}/build-host/install"
 
 # BUILD the integrations iree-import-tflite with Bazel
 echo "Build iree-import-tflite with Bazel"
@@ -34,13 +35,12 @@ pushd "${ROOT_DIR}/integrations/tensorflow"
 BAZEL_CMD=(bazel --noworkspace_rc --bazelrc=build_tools/bazel/iree-tf.bazelrc)
 # Yes this is really the best way to get the path to a single binary
 export IREE_IMPORT_TFLITE_BIN="$(
-"${BAZEL_CMD[@]}" run --run_under=echo \
-  --config=remote_cache_bazel_ci \
-  --config=generic_clang \
-  //iree_tf_compiler:iree-import-tflite
+  "${BAZEL_CMD[@]}" run --run_under=echo \
+    --config=remote_cache_bazel_ci \
+    --config=generic_clang \
+    //iree_tf_compiler:iree-import-tflite
 )"
 popd
-
 
 echo "Cross-compiling with cmake"
 ./build_tools/cmake/build_host_and_riscv.sh
