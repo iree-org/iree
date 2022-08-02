e203a21d6e89c728dd3932ca38b1d8ef8f707b25
Cindy Liu <hcindyl@google.com>
2022-08-02 11:05:29 -0700

    Clean up RISC-V CI RVV config
    * Remove QEMU vector setting for non-vector emulation in rv32
    * Use vlen=512 for v-ext emulation to align with `benchmarks/` setup
    * Update the e2e model path to align with `benchmarks/` setup

--- tests/riscv32/smoke.sh
+++ tests/riscv32/smoke.sh
@@ -16,17 +16,14 @@ export PS4='[$(date -u "+%T %Z")] '
 # Run the embedded_library module loader and simple_embedding under QEMU.
 echo "Test elf_module_test_binary"
 pushd "${BUILD_RISCV_DIR}/runtime/src/iree/hal/local/elf" > /dev/null
-"${QEMU_RV32_BIN}" -cpu rv32,x-v=true,x-k=true,vlen=256,elen=64,vext_spec=v1.0 \
-elf_module_test_binary
+"${QEMU_RV32_BIN}" -cpu rv32 elf_module_test_binary
 popd > /dev/null
 
 echo "Test simple_embedding binaries"
 pushd "${BUILD_RISCV_DIR}/samples/simple_embedding" > /dev/null
 
-"${QEMU_RV32_BIN}" -cpu rv32,x-v=true,x-k=true,vlen=256,elen=64,vext_spec=v1.0 \
-simple_embedding_embedded_sync
+"${QEMU_RV32_BIN}" -cpu rv32 simple_embedding_embedded_sync
 
-"${QEMU_RV32_BIN}" -cpu rv32,x-v=true,x-k=true,vlen=256,elen=64,vext_spec=v1.0 \
-simple_embedding_vmvx_sync
+"${QEMU_RV32_BIN}" -cpu rv32 simple_embedding_vmvx_sync
 
 popd > /dev/null
