e7531fceb8ab7c02862e3cf3f191f55251f9a5ba
Anush Elangovan <anush@nod-labs.com>
2022-08-05 06:39:45 +0000

    Add ROCM builds to the the nightly
    Build Experimental ROCM builds

--- runtime/setup.py
+++ runtime/setup.py
@@ -237,6 +237,8 @@ def prepare_installation():
         "-DPython3_EXECUTABLE={}".format(sys.executable),
         "-DCMAKE_BUILD_TYPE={}".format(cfg),
         get_env_cmake_option("IREE_HAL_DRIVER_CUDA"),
+        get_env_cmake_option("IREE_EXTERNAL_HAL_DRIVERS",
+                             "" if platform.system() == "Darwin" else "rocm"),
         get_env_cmake_option("IREE_HAL_DRIVER_VULKAN",
                              "OFF" if platform.system() == "Darwin" else "ON"),
         get_env_cmake_option("IREE_ENABLE_RUNTIME_TRACING"),
