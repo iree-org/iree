40b51f9b32a788b00ad525b911fe7faa7d87dd18
Scott Todd <scotttodd@google.com>
2022-08-08 08:58:04 -0700

    Bump "last updated time" on version prior to merge.

--- compiler/src/iree/compiler/Dialect/HAL/Target/LLVM/LibraryBuilder.h
+++ compiler/src/iree/compiler/Dialect/HAL/Target/LLVM/LibraryBuilder.h
@@ -48,7 +48,7 @@ class LibraryBuilder {
     // NOTE: until we hit v1 the versioning scheme here is not set in stone.
     // We may want to make this major release number, date codes (0x20220307),
     // or some semantic versioning we track in whatever spec we end up having.
-    V_0_3 = 0x0000'0003u,  // v0.3 - ~2022-08-03
+    V_0_3 = 0x0000'0003u,  // v0.3 - ~2022-08-08
 
     // Pinned to the latest version.
     // Requires that the runtime be compiled with the same version.
