9931cb4dc926aa76e47e1d167fc156b244075428
Scott Todd <scotttodd@google.com>
2022-08-10 10:16:58 -0700

    Pass vm instance instead of runtime instance in web samples.

--- experimental/web/sample_dynamic/main.c
+++ experimental/web/sample_dynamic/main.c
@@ -131,7 +131,8 @@ iree_program_state_t* load_program(iree_sample_state_t* sample_state,
     // Take ownership of the FlatBuffer data so JavaScript doesn't need to
     // explicitly call `Module._free()`.
     status = iree_vm_bytecode_module_create(
-        sample_state->instance, iree_make_const_byte_span(vmfb_data, length),
+        iree_runtime_instance_vm_instance(sample_state->instance),
+        iree_make_const_byte_span(vmfb_data, length),
         /*flatbuffer_allocator=*/iree_allocator_system(),
         iree_allocator_system(), &program_state->module);
   } else {
