2a6f4bd5ce233f62301b7a9a79e7d43e54b519ab
Bob Muniz <bob@bobmuniz.com>
2022-08-10 12:21:39 -0700

    use abs path and add line break

--- build_tools/github_actions/runner/config/register.sh
+++ build_tools/github_actions/runner/config/register.sh
@@ -114,4 +114,4 @@ declare -a args=(
 # functionality.
 (set -x; : Running configuration with additional args: "${args[@]}")
 
-~runner/actions-runner/config.sh --token "${REGISTER_TOKEN}" "${args[@]}"
+/home/runner/actions-runner/config.sh --token "${REGISTER_TOKEN}" "${args[@]}"
