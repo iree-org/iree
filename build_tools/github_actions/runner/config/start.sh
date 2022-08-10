5807f152c7dcf113db66bfccb1311e3b1868587e
Bob Muniz <bob@bobmuniz.com>
2022-08-10 10:52:24 -0700

    Update build_tools/github_actions/runner/config/start.sh
    Co-authored-by: Geoffrey Martin-Noble <gcmn@google.com>

--- build_tools/github_actions/runner/config/start.sh
+++ build_tools/github_actions/runner/config/start.sh
@@ -6,7 +6,11 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-# Starts the GitHub actions runner as a custom service.
+# Starts the GitHub actions runner in service mode.
+# We don't use runsvc.sh here because it's a shell wrapper around a
+# nodejs listener that calls the .NET runner program 😱 in startup mode
+# and at the exit codes so it can retry if the runner is busy updating.
+# We disable updating, so can avoid those layers of cruft.
 
 set -euo pipefail
 
