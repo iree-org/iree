ab1695aa350afda4971919ecd68fe4eaea5b7ae3
Bob Muniz <bob@bobmuniz.com>
2022-08-11 08:27:02 -0700

    Recommended fetch script

--- build_tools/github_actions/runner/fetch_and_setup.sh
+++ build_tools/github_actions/runner/fetch_and_setup.sh
@@ -6,33 +6,24 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-# This script is actually part of the VM image and fetches the rest of the
-# configuration. It is invoked on startup through a one-line startup script that
-# calls it. Longer term, we may want to have an explicit deployment of new
-# scripts instead of fetching them directly from HEAD.
+# This script is put into the VM metadata as the startup script. It fetches the 
+# rest of the configuration from the repo. Longer term, we may want to have an
+# explicit deployment of new scripts instead of fetching them directly from HEAD.
+# Note that the startup script runs as root.
 
 set -euo pipefail
 
-
-# Change config-ref metadata to a different git reference to fetch from somewhere else.
+# Change this to a different git reference to fetch from somewhere else.
 # For PRs, that would be refs/pull/<pr_number>/merge
-gihub_config_ref=$(curl --silent --fail "http://metadata.google.internal/computeMetadata/v1/instance/attributes/github-config-ref" -H "Metadata-Flavor: Google")
-
-if [[ -z "${gihub_config_ref}" ]]; then
-  echo "Using default 'main' config as no github-config-ref was specified."
-  github_config_ref="main"
-fi
-
-cd /home/runner
-rm -rf config/
-mkdir config/
+CONFIG_REF=main
+ 
+cd ~runner
+rm -rf config
 curl --silent --fail --show-error --location \
-  "https://github.com/iree-org/iree/archive/${gihub_config_ref}.tar.gz" \
+  "https://github.com/iree-org/iree/archive/${CONFIG_REF}.tar.gz" \
   | tar -zx -f - \
   --strip-components=4  --wildcards \
   */build_tools/github_actions/runner/config/
 
-chown -R runner:runner /home/runner/config/
-cd config
-
-./start.sh
+chown -R runner:runner config
+~runner/config/setup.sh
