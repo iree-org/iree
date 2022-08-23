7b1d3bb7d3d64fd5cd603aff65fcd09c65467abb
Geoffrey Martin-Noble <gcmn@google.com>
2022-08-23 13:41:04 -0700

    Change into git directory before finding top level

--- build_tools/python_deploy/build_linux_packages.sh
+++ build_tools/python_deploy/build_linux_packages.sh
@@ -41,7 +41,7 @@ set -eu -o errtrace
 
 this_dir="$(cd $(dirname $0) && pwd)"
 script_name="$(basename $0)"
-repo_root="$(git rev-parse --show-toplevel)"
+repo_root="$(cd "${this_dir}" && git rev-parse --show-toplevel)"
 manylinux_docker_image="${manylinux_docker_image:-gcr.io/iree-oss/manylinux2014_x86_64-release@sha256:b09c10868f846308bad2eab253a77d0a3f097816c40342bc289d8e62509bc5f9}"
 python_versions="${override_python_versions:-cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310}"
 output_dir="${output_dir:-${this_dir}/wheelhouse}"
