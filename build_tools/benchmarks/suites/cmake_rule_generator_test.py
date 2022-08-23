8b3b214d2e1d57ccd01464669302afcda8b26727
Che-Yu Wu <cheyuw@google.com>
2022-08-23 19:07:08 +0000

    Fix license.

--- build_tools/benchmarks/suites/cmake_rule_generator_test.py
+++ build_tools/benchmarks/suites/cmake_rule_generator_test.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+## Copyright 2022 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 from suites.definitions import common_definitions
 from suites import cmake_rule_generator
 import unittest
