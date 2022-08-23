91812c1a24800531a3d4cbdc5a0f4f5a081ed29f
Thomas Raoux <thomasraoux@google.com>
2022-08-23 05:38:42 +0000

    revert unnecessary changes

--- compiler/src/iree/compiler/Codegen/LLVMGPU/TransformExtensions/LLVMGPUExtensions.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/TransformExtensions/LLVMGPUExtensions.cpp
@@ -113,10 +113,15 @@ mlir::iree_compiler::rewriteForeachThreadToGpu(
         "scf.foreach_thread with rank > 3 does not lower to gpu.thread");
 
   auto maybeWorkgroupSizes = getNumThreads(rewriter, foreachThreadOp);
-  if (failed(maybeWorkgroupSizes))
-    return foreachThreadOp->emitError("can't extract number of threads");
+  if (failed(maybeWorkgroupSizes) ||
+      llvm::any_of(*maybeWorkgroupSizes, [](OpFoldResult ofr) {
+        return !getConstantIntValue(ofr).has_value();
+      }))
+    return foreachThreadOp->emitError("unsupported dynamic workgroup size");
 
-  SmallVector<OpFoldResult> workgroupSizes = *maybeWorkgroupSizes;
+  SmallVector<int64_t> workgroupSizes;
+  for (OpFoldResult ofr : *maybeWorkgroupSizes)
+    workgroupSizes.push_back(getConstantIntValue(ofr).value());
 
   // Step 1. Create the gpu.thread ops
   Location loc = foreachThreadOp.getLoc();
@@ -136,14 +141,11 @@ mlir::iree_compiler::rewriteForeachThreadToGpu(
     auto threadId = std::get<0>(it);
     auto workgroupSize = std::get<1>(it);
     auto globalWorkgroupSize = std::get<2>(it);
-    auto cstWgSize = getConstantIntValue(workgroupSize);
-    if (cstWgSize) {
-      assert(*cstWgSize <= globalWorkgroupSize && "workgroup size overflow");
-      if (*cstWgSize == globalWorkgroupSize) continue;
-    }
+    assert(workgroupSize <= globalWorkgroupSize && "workgroup size overflow");
+    if (workgroupSize == globalWorkgroupSize) continue;
     Value tmpPredicate = rewriter.create<arith::CmpIOp>(
         loc, arith::CmpIPredicate::ult, threadId,
-        linalg::materializeOpFoldResult(rewriter, loc, workgroupSize));
+        rewriter.create<arith::ConstantIndexOp>(loc, workgroupSize));
     predicate =
         predicate ? rewriter.create<arith::AndIOp>(loc, predicate, tmpPredicate)
                   : tmpPredicate;
