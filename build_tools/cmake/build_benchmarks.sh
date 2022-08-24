c06dd4283980097dcbb056084485e6b8402689e6
Geoffrey Martin-Noble <gcmn@google.com>
2022-08-24 12:50:24 -0700

    Build benchmarks on the CI using host tools

--- build_tools/cmake/build_benchmarks.sh
+++ build_tools/cmake/build_benchmarks.sh
@@ -16,20 +16,20 @@ ROOT_DIR="${ROOT_DIR:-$(git rev-parse --show-toplevel)}"
 cd "${ROOT_DIR}"
 
 BUILD_DIR="${1:-${IREE_BUILD_DIR:-build}}"
+IREE_HOST_BINARY_ROOT="${IREE_HOST_BINARY_ROOT:-build-host/install}"
 CMAKE_BIN=${CMAKE_BIN:-$(which cmake)}
 IREE_TF_BINARIES_DIR="${IREE_TF_BINARIES_DIR:-integrations/tensorflow/bazel-bin/iree_tf_compiler}"
 
 "$CMAKE_BIN" --version
 ninja --version
 
-if ! [[ -d "${BUILD_DIR}" ]]; then
-  echo "Build directory '${BUILD_DIR}' does not exist. Aborting"
-  exit 1
-fi
-
 echo "Reconfiguring to enable benchmarks"
 # Note that we're relying on CMake caching here
 "${CMAKE_BIN}" -B "${BUILD_DIR}" \
+  -G Ninja \
+  -DIREE_BUILD_COMPILER=OFF \
+  -DIREE_BUILD_TESTS=OFF \
+  -DIREE_HOST_BINARY_ROOT="${IREE_HOST_BINARY_ROOT}" \
   -DIREE_BUILD_BENCHMARKS=ON \
   -DIREE_BUILD_MICROBENCHMARKS=ON \
   -DIREE_IMPORT_TFLITE_PATH="${IREE_TF_BINARIES_DIR}/iree-import-tflite" \
