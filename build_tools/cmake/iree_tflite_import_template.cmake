fd5bbdf1f2213d0706d64c3cb6bda96cd6832a3a
Che-Yu Wu <cheyuw@google.com>
2022-08-29 17:02:47 +0000

    Remove extra files.

--- build_tools/cmake/iree_tflite_import_template.cmake
+++
@@ -1,6 +0,0 @@
-# Import the TFLite model "$__SOURCE_MODEL_PATH"
-iree_import_tflite_model(
-  TARGET_NAME "$${_PACKAGE_NAME}_$__TARGET_NAME"
-  SOURCE "$__SOURCE_MODEL_PATH"
-  OUTPUT_MLIR_FILE "$__OUTPUT_PATH"
-)
