da21e839865abb47fe00ab9df2e1178c78e981e2
Han-Chung Wang <hanchung@google.com>
2022-08-30 02:29:41 +0800

    Optimize tiling sizes heuristics for elementwise dispatches. (#10179)
    In the past, small numbers could be picked because we want vectorization
    enabled for all the kernels. The PR picks a more reasonable tiling sizes
    and addresses tiny dispatch issues.
    
    The peeling pipeline works in IREE and the PR moves elementwise dispatches
    (and copy only dispatches) to use peeling approach. In this case, we're
    still able to vectorize the dispatches.
    
    This PR changes the logic to limit the unroll factor when computing the
    vector level tiling sizes. It avoids generating many operations, which saves
    many compilation time and binary size for quantized models. It also
    improves models performance that IREE is tracking for all CPU backends.
    
    Fixes https://github.com/iree-org/iree/issues/9660

--- compiler/src/iree/compiler/Codegen/LLVMCPU/KernelDispatch.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMCPU/KernelDispatch.cpp
@@ -1106,11 +1106,102 @@ static LogicalResult setTransposeLikeOpRootConfig(func::FuncOp entryPointFn,
                                                tileSizes, passPipeline);
 }
 
+/// Sets elementwise dispatches to use peeling approach. It scales the number of
+/// workload per workgroup to a larger number, which prevents runtime overheads
+/// from tiny dispatches.
+static LogicalResult setElementwiseGenericOpRootConfig(
+    func::FuncOp entryPointFn, linalg::GenericOp genericOp) {
+  if (getLoweringConfig(genericOp)) {
+    return success();
+  }
+
+  unsigned numLoops = genericOp.getNumLoops();
+  if (numLoops == 0) return success();
+  if (!linalg::isElementwise(genericOp)) return success();
+
+  // Set the flow level tiling to the default.
+  SmallVector<int64_t> minTileSizes =
+      getMinTilingSizesForEachDim(entryPointFn, genericOp);
+  SmallVector<int64_t> maxTileSizes(numLoops, defaultWorkgroupTileSize);
+  SmallVector<int64_t> flowTileSizes =
+      getDefaultDistributedLevelTileSizes(genericOp, minTileSizes, maxTileSizes,
+                                          /*allowIncompleteTile=*/true);
+
+  // Adjust the number of workload per workgroup to at least 4096. This
+  // prevents the runtime overheads domiating the execution time. The number is
+  // derived from experimients. We should be able to make it related to target.
+  constexpr int64_t kMinimumWorkload = 4096;
+  auto shape = genericOp.getStaticLoopRanges();
+  int64_t numWorkload = 1;
+  for (auto en : llvm::enumerate(shape)) {
+    int64_t size = en.value();
+    if (size == ShapedType::kDynamicSize) {
+      numWorkload = ShapedType::kDynamicSize;
+      break;
+    }
+    int index = en.index();
+    if (flowTileSizes[index]) {
+      size = flowTileSizes[index];
+    }
+    numWorkload *= size;
+  }
+  for (unsigned currDim = 0;
+       numWorkload < kMinimumWorkload && currDim < numLoops;) {
+    int64_t currSize = flowTileSizes[currDim];
+    if (currSize == shape[currDim] || currSize == 0 ||
+        shape[currDim] == ShapedType::kDynamicSize ||
+        numWorkload == ShapedType::kDynamicSize) {
+      currDim++;
+      continue;
+    }
+    int64_t newSize = std::min<int64_t>(currSize * 2, shape[currDim]);
+    numWorkload = numWorkload / currSize * newSize;
+    flowTileSizes[currDim] = newSize;
+  }
+
+  // Adjust tiling sizes of vector levels to avoid large unroll factors.
+  SmallVector<int64_t> vecTileSizes(minTileSizes.begin(), minTileSizes.end());
+  for (auto operand : genericOp.getOutputOperands()) {
+    constexpr int64_t kMaxUnrollFactor = 8;
+    AffineMap map = genericOp.getTiedIndexingMap(operand);
+    int64_t vecSize = getVectorSize(entryPointFn, operand->get().getType());
+    int64_t currSize = 1;
+    for (auto dimExpr : llvm::reverse(map.getResults().drop_back())) {
+      unsigned pos = dimExpr.cast<AffineDimExpr>().getPosition();
+      if (vecTileSizes[pos] * currSize > vecSize * kMaxUnrollFactor) {
+        vecTileSizes[pos] = 1;
+        currSize = vecSize * kMaxUnrollFactor;
+      }
+    }
+    int fastestPos =
+        map.getResults().back().cast<AffineDimExpr>().getPosition();
+    vecTileSizes[fastestPos] =
+        std::min<int64_t>(vecTileSizes[fastestPos], kMaxUnrollFactor);
+  }
+
+  // Setting reduction tile sizes is a workaround to kick in peeling transform.
+  // The tiling won't happen because the sizes are zeros.
+  SmallVector<int64_t> zeros(numLoops, 0);
+
+  TileSizesListType tileSizes;
+  tileSizes.push_back(flowTileSizes);
+  tileSizes.push_back(vecTileSizes);
+  tileSizes.push_back(zeros);
+
+  auto passPipeline =
+      genericOp.hasTensorSemantics()
+          ? DispatchLoweringPassPipeline::CPUDoubleTilingPeelingExpert
+          : DispatchLoweringPassPipeline::CPUBufferOpsTileAndVectorize;
+  return setOpConfigAndEntryPointFnTranslation(entryPointFn, genericOp,
+                                               tileSizes, passPipeline);
+}
+
 /// Sets the lowering configuration for a generic op to use
 /// CPUDoubleTilingExpert pipeline.
 static LogicalResult setRootConfig(func::FuncOp entryPointFn,
                                    linalg::GenericOp genericOp) {
   if (failed(setTransposeLikeOpRootConfig(entryPointFn, genericOp)) ||
+      failed(setElementwiseGenericOpRootConfig(entryPointFn, genericOp)) ||
       failed(setDefaultGenericOpRootConfig(entryPointFn, genericOp))) {
     return failure();
   }
