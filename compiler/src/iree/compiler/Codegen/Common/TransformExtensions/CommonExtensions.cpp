ff0422027b0bad9edbfab03138ece543cc33342d
Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>
2022-08-31 17:09:18 +0200

    Add a foreach_thread_to_workgroup transform (#10131)
    This revision adds a transform dialect op that is very similar to `foreach_thread_to_gpu` to allow distributing to the first level of parallelism during codegen.
    
    This is subject to conventions of what values are captured in the workgroup_count region and how these values are passed around in the `stream.cmd.dispatch` op.
    
    For now the convention choosen is that `0` values are passed and the `tile_to_foreach_thread` may only use static sizes.
    
    A followup PR will introduce a new transform dialect op to allow creating the sizes dynamically based on other conventions.

--- compiler/src/iree/compiler/Codegen/Common/TransformExtensions/CommonExtensions.cpp
+++ compiler/src/iree/compiler/Codegen/Common/TransformExtensions/CommonExtensions.cpp
@@ -6,11 +6,15 @@
 
 #include "CommonExtensions.h"
 
+#include <iree/compiler/Dialect/HAL/IR/HALOps.h>
+
+#include "iree-dialects/Dialect/LinalgTransform/SimplePatternRewriter.h"
 #include "iree-dialects/Dialect/LinalgTransform/StructuredTransformOpsExt.h"
 #include "iree-dialects/Transforms/ListenerGreedyPatternRewriteDriver.h"
 #include "iree/compiler/Codegen/Interfaces/BufferizationInterfaces.h"
 #include "iree/compiler/Codegen/Passes.h"
 #include "llvm/ADT/StringSet.h"
+#include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
 #include "mlir/Pass/PassManager.h"
 
 using namespace mlir;
@@ -76,6 +80,9 @@ DiagnosedSilenceableFailure transform_dialect::ApplyPatternsOp::applyToOne(
 // IREEBufferizeOp
 //===---------------------------------------------------------------------===//
 
+// TODO: Maybe we need both a transform.iree.cpu.bufferize and a
+// transform.iree.gpu.bufferize rather than a single common bufferize op?
+//
 //===---------------------------------------------------------------------===//
 // Default allocation functions for CPU backend
 // TODO: register the bufferization behavior in a target-specific way.
@@ -171,6 +178,229 @@ DiagnosedSilenceableFailure transform_dialect::IREEBufferizeOp::apply(
     return WalkResult::advance();
   });
   return DiagnosedSilenceableFailure(failure(res.wasInterrupted()));
+}
+/// Populate the workgroup_count region of `dispatchOp`.
+/// For now, this only supports constant index ops and empty workload operands.
+/// Assumes the HAL::ExecutableExportOp is built with an empty region.
+static LogicalResult populateWorkgroupCountComputingRegion(
+    PatternRewriter &rewriter, scf::ForeachThreadOp foreachThreadOp,
+    HAL::ExecutableExportOp exportOp) {
+  Location loc = foreachThreadOp.getLoc();
+  OpBuilder::InsertionGuard g(rewriter);
+  Region &r = exportOp.getWorkgroupCount();
+  assert(r.empty() && "expected block-less workgroup_count region");
+  Block *block = rewriter.createBlock(&r);
+  // The HAL::DeviceType argument is always the first argument.
+  block->addArgument(HAL::DeviceType::get(rewriter.getContext()), loc);
+  rewriter.setInsertionPointToStart(block);
+
+  SmallVector<Value> results;
+  // For now, this assumes that we only pull in constants.
+  // TODO: Iteratively pull required operations.
+  for (Value v : foreachThreadOp.getNumThreads()) {
+    auto op = dyn_cast_or_null<arith::ConstantIndexOp>(v.getDefiningOp());
+    if (!op) return failure();
+    results.push_back(
+        cast<arith::ConstantIndexOp>(rewriter.clone(*op)).getResult());
+  }
+  // Pad to `3` to match assumptions hardcoded in IREE.
+  for (unsigned i = results.size(); i < 3; ++i) {
+    results.push_back(rewriter.create<arith::ConstantIndexOp>(loc, 1));
+  }
+  rewriter.create<HAL::ReturnOp>(loc, results);
+
+  return success();
+}
+
+/// Apply the permutation `perm` to `vals.
+/// Return failure if perm is not a permutation.
+// TODO: upstream as extraClassDeclaration once stabilized.
+template <typename T>
+static FailureOr<SmallVector<T>> permute(const SmallVector<T> &vals,
+                                         ArrayRef<int64_t> perm) {
+  if (vals.size() != perm.size()) return failure();
+  SmallVector<T> result(vals.size());
+  SmallVector<bool> seen(vals.size());
+  for (const auto &it : llvm::zip(perm, vals)) {
+    // Already seen, invalid thread_dim_mapping.
+    if (seen[std::get<0>(it)]) return failure();
+    result[std::get<0>(it)] = std::get<1>(it);
+    seen[std::get<0>(it)] = true;
+  }
+  // Some not seen, invalid thread_dim_mapping.
+  if (!llvm::all_of(seen, [](bool b) { return b; })) return failure();
+  return result;
+}
+
+/// Helper to get apply the `thread_dim_mapping` permutation of a
+/// `foreachThreadOp` to `values`.
+// TODO: upstream as extraClassDeclaration once stabilized.
+template <typename T>
+static FailureOr<SmallVector<T>> getPermuted(
+    scf::ForeachThreadOp foreachThreadOp, const SmallVector<T> &values) {
+  // Apply mapping permutation if specified.
+  auto mapping = foreachThreadOp.getThreadDimMapping();
+  if (mapping && !mapping.empty()) {
+    auto maybePermuted = permute(values, extractFromI64ArrayAttr(mapping));
+    if (failed(maybePermuted))
+      return foreachThreadOp->emitError("invalid permutation");
+    return *maybePermuted;
+  }
+  return values;
+}
+
+/// Helper to get the `num_threads` of a `foreachThreadOp` after applying the
+/// `thread_dim_mapping` permutation.
+// TODO: upstream as extraClassDeclaration once stabilized.
+static FailureOr<SmallVector<OpFoldResult>> getNumThreads(
+    OpBuilder &b, scf::ForeachThreadOp foreachThreadOp) {
+  SmallVector<OpFoldResult> threadCount = foreachThreadOp.getNumThreads();
+  threadCount.resize(3, b.getIndexAttr(1));
+  return getPermuted(foreachThreadOp, threadCount);
+}
+
+/// Helper to get the thread indices of a `foreachThreadOp` after applying the
+/// `thread_dim_mapping` permutation.
+// TODO: upstream as extraClassDeclaration once stabilized.
+static FailureOr<SmallVector<Value>> getThreadIndices(
+    OpBuilder &b, scf::ForeachThreadOp foreachThreadOp) {
+  SmallVector<Value> threadCount = foreachThreadOp.getThreadIndices();
+  threadCount.resize(3, Value());
+  return getPermuted(foreachThreadOp, threadCount);
+}
+
+//===---------------------------------------------------------------------===//
+// Patterns for ForeachThreadToWorkgroup rewrite.
+//===---------------------------------------------------------------------===//
+
+LogicalResult rewriteForeachThreadToWorkgroup(
+    scf::ForeachThreadOp foreachThreadOp,
+    IREE::HAL::ExecutableExportOp exportOp, PatternRewriter &rewriter) {
+  if (foreachThreadOp.getNumResults() > 0)
+    return foreachThreadOp->emitError(
+        "only bufferized scf.foreach_thread lowers to workgroup");
+  if (foreachThreadOp.getNumThreads().size() > 3)
+    return foreachThreadOp->emitError(
+        "scf.foreach_thread with rank > 3 does not lower to workgroup");
+
+  // Step 0. Outline the compute workload region and set up the workload
+  // operands.
+  auto maybeWorkgroupCounts = getNumThreads(rewriter, foreachThreadOp);
+  if (failed(maybeWorkgroupCounts) ||
+      llvm::any_of(*maybeWorkgroupCounts, [](OpFoldResult ofr) {
+        return !getConstantIntValue(ofr).has_value();
+      }))
+    return foreachThreadOp->emitError(
+        "unsupported dynamic workgroup_count atm --- need to slice out "
+        "workgroup_count computation into ExecutableExport::workgroup_count. "
+        "This region may require arbitrary computations and cannot magically "
+        "match what the `stream.cmd.dispatch` has already imposed on us at a "
+        "distance. For now we must specify the number of values properly when "
+        "applying the topLevel tile_to_foreach_thread_op");
+
+  SmallVector<int64_t> workgroupCounts;
+  for (OpFoldResult ofr : *maybeWorkgroupCounts)
+    workgroupCounts.push_back(getConstantIntValue(ofr).value());
+  if (failed(populateWorkgroupCountComputingRegion(rewriter, foreachThreadOp,
+                                                   exportOp)))
+    return foreachThreadOp->emitOpError(
+               "failed to populate workload region for dispatchOp: ")
+           << exportOp;
+
+  // Step 1. Create the workgroup id and count ops.
+  Location loc = foreachThreadOp.getLoc();
+  BlockAndValueMapping bvm;
+  SmallVector<Value, 8> workgroupIdOps, workgroupCountOps;
+  for (int64_t rank :
+       llvm::seq<int64_t>(0, foreachThreadOp.getThreadIndices().size())) {
+    workgroupIdOps.push_back(
+        rewriter.create<HAL::InterfaceWorkgroupIDOp>(loc, rank));
+    workgroupCountOps.push_back(
+        rewriter.create<HAL::InterfaceWorkgroupCountOp>(loc, rank));
+  }
+  bvm.map(foreachThreadOp.getThreadIndices(), workgroupIdOps);
+  bvm.map(foreachThreadOp.getNumThreads(), workgroupCountOps);
+
+  // Step 2. Predicate omitted given unique topLevel scf::ForeachThreadOp.
+
+  // Step 3. Move the body of foreachThreadOp.
+  // Erase the terminator first, it will not be used since we are on buffers.
+  rewriter.eraseOp(foreachThreadOp.getTerminator());
+  Block *targetBlock;
+  Block::iterator insertionPoint;
+  targetBlock = foreachThreadOp->getBlock();
+  insertionPoint = Block::iterator(foreachThreadOp);
+  Block &sourceBlock = foreachThreadOp.getRegion().front();
+  targetBlock->getOperations().splice(insertionPoint,
+                                      sourceBlock.getOperations());
+
+  // Step 4. RAUW thread indices to thread ops.
+  SmallVector<Value> threadIndices =
+      *getThreadIndices(rewriter, foreachThreadOp);
+  for (auto it : llvm::zip(threadIndices, workgroupIdOps)) {
+    if (!std::get<0>(it)) continue;
+    std::get<0>(it).replaceAllUsesWith(std::get<1>(it));
+  }
+
+  // Step 5. Barriers omitted given unique topLevel scf::ForeachThreadOp.
+
+  // Step 6. Erase old op.
+  rewriter.eraseOp(foreachThreadOp);
+
+  return success();
+}
+
+//===---------------------------------------------------------------------===//
+// IREE-specific transformations defined outside of iree_linalg_transform.
+//===---------------------------------------------------------------------===//
+
+DiagnosedSilenceableFailure
+transform_dialect::ForeachThreadToWorkgroupOp::applyToOne(
+    func::FuncOp target, SmallVectorImpl<Operation *> &results,
+    transform::TransformState &state) {
+  if (!isa<HAL::ExecutableOp, HAL::ExecutableVariantOp>(state.getTopLevel())) {
+    state.getTopLevel()->emitOpError(
+        "requires HAL::ExecutableOp or HAL::ExecutableVariantOp toplevel to "
+        "attach the workgroup size information to a nested ExecutableExportOp");
+    return DiagnosedSilenceableFailure(reportUnknownTransformError(target));
+  }
+
+  IREE::HAL::ExecutableExportOp exportOp;
+  state.getTopLevel()->walk([&](IREE::HAL::ExecutableExportOp op) {
+    if (op.getSymName() == target.getName()) exportOp = op;
+  });
+  if (!exportOp) {
+    state.getTopLevel()->emitOpError("no IREE::HAL::ExecutableExportOp found");
+    return DiagnosedSilenceableFailure(reportUnknownTransformError(target));
+  }
+  if (!exportOp.getWorkgroupCount().empty())
+    return emitDefaultSilenceableFailure(target)
+           << "export op must have an empty workgroup count region that the "
+              "transform fills --- the transform is not applied";
+
+  scf::ForeachThreadOp topLevelForeachThreadOp;
+  auto walkResult = target->walk([&](scf::ForeachThreadOp foreachThreadOp) {
+    if (foreachThreadOp->getParentOfType<scf::ForeachThreadOp>())
+      return WalkResult::advance();
+    if (topLevelForeachThreadOp) return WalkResult::interrupt();
+    topLevelForeachThreadOp = foreachThreadOp;
+    return WalkResult::advance();
+  });
+
+  if (walkResult.wasInterrupted()) {
+    state.getTopLevel()->emitOpError(
+        "could not find a unique topLevel scf.foreach_thread");
+    return DiagnosedSilenceableFailure(reportUnknownTransformError(target));
+  }
+
+  SimplePatternRewriter rewriter(topLevelForeachThreadOp);
+  if (failed(rewriteForeachThreadToWorkgroup(topLevelForeachThreadOp, exportOp,
+                                             rewriter)))
+    return DiagnosedSilenceableFailure(reportUnknownTransformError(target));
+
+  results.assign({target});
+
+  return DiagnosedSilenceableFailure(success());
 }
 
 #define GET_OP_CLASSES
--- tests/transform_dialect/cpu/matmul.mlir
+++ tests/transform_dialect/cpu/matmul.mlir
@@ -1,17 +1,94 @@
-// RUN: iree-compile --iree-hal-target-backends=llvm-cpu \
-// RUN:     --iree-flow-dispatch-use-transform-dialect=%p/matmul_dispatch_spec.mlir \
-// RUN:     --iree-flow-export-benchmark-funcs %s | \
-// RUN: iree-benchmark-module --device=local-task | \
-// RUN: FileCheck %s
 
-!A_size = tensor<50x100xf32>
-!B_size = tensor<100x50xf32>
-!C_size = tensor<50x50xf32>
+!A_size = tensor<3x5xf32>
+!B_size = tensor<5x3xf32>
+!C_size = tensor<3x3xf32>
 
-// CHECK: tile_matmul_with_constant
-func.func @tile_matmul_with_constant(
+func.func @matmul_static(
     %A : !A_size, %B : !B_size, %C : !C_size) -> !C_size {
   %0 = linalg.matmul ins(%A, %B : !A_size, !B_size)
                      outs(%C : !C_size) -> !C_size
   return %0 : !C_size
 }
+
+// RUN: iree-opt %s --iree-hal-target-backends=llvm-cpu \
+// RUN:   --iree-abi-transformation-pipeline \
+// RUN:   --iree-flow-transformation-pipeline \
+// RUN:   --iree-flow-dispatch-use-transform-dialect=%p/matmul_dispatch_spec.mlir | \
+// RUN: FileCheck %s --check-prefixes=DISPATCH
+
+// TODO: make this test drop transform dialect usage at the flow level and use:
+//   --iree-flow-transformation-pipeline --iree-flow-convert-region-to-workgroups
+// Atm the 3rd flow.dispatch.tensor.load shows as readonly instead of readwrite.
+
+// DISPATCH: flow.executable private @matmul_static_dispatch_0 {
+// DISPATCH:   flow.executable.export public @matmul_static_dispatch_0_matmul_3x3x5 
+// DISPATCH:     builtin.module {
+// DISPATCH:       func.func @matmul_static_dispatch_0_matmul_3x3x5
+// DISPATCH:         flow.dispatch.tensor.load {{.*}}, offsets = [0, 0], sizes = [3, 5], strides = [1, 1] : !flow.dispatch.tensor<readonly:3x5xf32> -> tensor<3x5xf32>
+// DISPATCH:         flow.dispatch.tensor.load {{.*}}, offsets = [0, 0], sizes = [5, 3], strides = [1, 1] : !flow.dispatch.tensor<readonly:5x3xf32> -> tensor<5x3xf32>
+// DISPATCH:         flow.dispatch.tensor.load {{.*}}, offsets = [0, 0], sizes = [3, 3], strides = [1, 1] : !flow.dispatch.tensor<readwrite:3x3xf32> -> tensor<3x3xf32>
+// DISPATCH:         linalg.matmul ins({{.*}} : tensor<3x5xf32>, tensor<5x3xf32>) outs({{.*}} : tensor<3x3xf32>) -> tensor<3x3xf32>
+// DISPATCH:         flow.dispatch.tensor.store {{.*}} offsets = [0, 0], sizes = [3, 3], strides = [1, 1] : tensor<3x3xf32> -> !flow.dispatch.tensor<readwrite:3x3xf32>
+// DISPATCH:         return
+
+// RUN: iree-opt %s --iree-hal-target-backends=llvm-cpu \
+// RUN:   --iree-abi-transformation-pipeline \
+// RUN:   --iree-flow-transformation-pipeline \
+// RUN:   --iree-flow-dispatch-use-transform-dialect=%p/matmul_dispatch_spec.mlir \
+// RUN:   --iree-stream-transformation-pipeline \
+// RUN:    --iree-hal-configuration-pipeline | \
+// RUN: iree-opt --pass-pipeline='hal.executable(hal.executable.variant(iree-llvmcpu-lower-executable-target))' \
+// RUN:   --iree-codegen-llvmcpu-use-transform-dialect=%p/matmul_codegen_spec.mlir | \
+// RUN: FileCheck %s --check-prefixes=CODEGEN
+
+// CODEGEN: hal.executable private @matmul_static_dispatch_0 {
+// CODEGEN:   hal.executable.variant public @embedded_elf_x86_64, target = #executable_target_embedded_elf_x86_64_ {
+//
+// The signature of the hal.executable.export region is subject to conventions
+// at the flow level. These conventions are materialized in IR e.g. into 
+// stream.cmd.dispatch before codegen gets invoked.
+// As a consequence, the tile_size/num_threads/workgroup_count passed to 
+// transform.tile_to_foreach_thread needs to be aware of this convention.
+// For now we use our own convention that sizes are static and no other bbArg
+// than !hal.device is present.
+// 
+// CODEGEN:     hal.executable.export public @matmul_static_dispatch_0_matmul_3x3x5 ordinal(0) layout(#{{.*}}) attributes {translation_info = #translation} {
+// CODEGEN:       ^bb0(%{{.*}}: !hal.device):
+// CODEGEN:         arith.constant 2 : index
+// CODEGEN:         arith.constant 1 : index
+// CODEGEN:         hal.return %{{.*}}, %{{.*}}, %{{.*}} : index, index, index
+// CODEGEN:       }
+// CODEGEN:       builtin.module {
+// CODEGEN:         func.func @matmul_static_dispatch_0_matmul_3x3x5() {
+// CODEGEN:           arith.constant 0 : index
+// CODEGEN:           hal.interface.binding.subspan set(0) binding(0) type(storage_buffer) offset({{.*}}) alignment(64) : memref<3x5xf32>
+// CODEGEN:           memref.assume_alignment %{{.*}}, 64 : memref<3x5xf32>
+// CODEGEN:           hal.interface.binding.subspan set(0) binding(1) type(storage_buffer) offset({{.*}}) alignment(64) : memref<5x3xf32>
+// CODEGEN:           memref.assume_alignment %{{.*}}, 64 : memref<5x3xf32>
+// CODEGEN:           hal.interface.binding.subspan set(0) binding(2) type(storage_buffer) offset({{.*}}) alignment(64) : memref<3x3xf32>
+// CODEGEN:           memref.assume_alignment %{{.*}}, 64 : memref<3x3xf32>
+// CODEGEN:           %[[workgroup_id_x:.*]] = hal.interface.workgroup.id[0] : index
+// CODEGEN:           affine.apply {{.*}}()[%workgroup_id_x]
+// CODEGEN:           memref.subview %{{.*}}[%{{.*}}, 0] [%{{.*}}, 5] [1, 1] : memref<3x5xf32> to memref<?x5xf32, #{{.*}}>
+// CODEGEN:           memref.subview %{{.*}}[%{{.*}}, 0] [%{{.*}}, 3] [1, 1] : memref<3x3xf32> to memref<?x3xf32, #{{.*}}>
+// CODEGEN:           linalg.matmul ins(%{{.*}}, %{{.*}} : memref<?x5xf32, #map3>, memref<5x3xf32>) outs(%{{.*}} : memref<?x3xf32, #{{.*}}>)
+
+// RUN: iree-compile %s --iree-hal-target-backends=llvm-cpu \
+// RUN:   --iree-flow-dispatch-use-transform-dialect=%p/matmul_dispatch_spec.mlir \
+// RUN:   --iree-codegen-llvmcpu-use-transform-dialect=%p/matmul_codegen_spec.mlir | \
+// RUN: iree-run-module --entry_function=matmul_static \
+// RUN:   --function_input="3x5xf32=1 1 1 1 1 1 1 1 1 1 1 1 1 1 1" \
+// RUN:   --function_input="5x3xf32=1 1 1 1 1 1 1 1 1 1 1 1 1 1 1" \
+// RUN:   --function_input="3x3xf32=0 0 0 0 0 0 0 0 0"| \
+// RUN: FileCheck %s --check-prefixes=EXEC
+
+// EXEC: 3x3xf32=[5 5 5][5 5 5][5 5 5]
+
+// RUN: iree-compile --iree-hal-target-backends=llvm-cpu \
+// RUN:     --iree-flow-dispatch-use-transform-dialect=%p/matmul_tiled_dispatch_spec.mlir \
+// RUN:     --iree-flow-export-benchmark-funcs %s | \
+// RUN: iree-benchmark-module --device=local-task | \
+// RUN: FileCheck %s --check-prefixes=BENCHMARK-MODULE
+
+// When running iree-benchmark-module, we only check the existence of the func.
+// BENCHMARK-MODULE: matmul_static
