5089c944a013f953c7c387174f13c40ab1f365c4
Benoit Jacob <benoitjacob@google.com>
2022-09-01 14:52:29 +0000

    Initial mmt4d builtins - reference code move out of vmvx/module.c

--- runtime/src/iree/builtins/ukernel/mmt4d.h
+++ runtime/src/iree/builtins/ukernel/mmt4d.h
@@ -13,25 +13,55 @@
 extern "C" {
 #endif  // __cplusplus
 
-//===----------------------------------------------------------------------===//
-// Public API
-//===----------------------------------------------------------------------===//
+struct iree_ukernel_mmt4d_f32f32f32_params_t {
+  const float* lhs_buffer;
+  const float* rhs_buffer;
+  float* out_buffer;
+  iree_ukernel_size_t lhs_stride;
+  iree_ukernel_size_t rhs_stride;
+  iree_ukernel_size_t out_stride;
+  iree_ukernel_size_t M;
+  iree_ukernel_size_t N;
+  iree_ukernel_size_t K;
+  int32_t M0;
+  int32_t N0;
+  int32_t K0;
+  uint32_t flags;
+};
 
-// Example matmul tile. This should be removed after we have any real methods
-// that can be used to demonstrate how this all fits together. The shape of the
-// method is close to what we need (proper buffer types, using the
-// iree_mmt4d_size_t type for strides, and taking the minimal required metadata)
-// but what we pass is dependent on the what the compiler produces.
-//
-// Returns 0 if the parameters were valid and the operation was performed.
-// Non-zero results will fail the entire submission or lose the device and
-// should be used as if an abort() ("no correct execution is possible after
-// this point").
-IREE_UKERNEL_EXPORT int iree_mmt4d_example_matmul_f32(
-    const float* lhs, iree_ukernel_size_t lhs_stride, const float* rhs,
-    iree_ukernel_size_t rhs_stride, float* IREE_RESTRICT out,
-    iree_ukernel_size_t out_stride, int32_t m, int32_t n, int32_t k,
-    float alpha, float beta);
+struct iree_ukernel_mmt4d_i8i8i32_params_t {
+  const int8_t* lhs_buffer;
+  const int8_t* rhs_buffer;
+  int32_t* out_buffer;
+  iree_ukernel_size_t lhs_stride;
+  iree_ukernel_size_t rhs_stride;
+  iree_ukernel_size_t out_stride;
+  iree_ukernel_size_t M;
+  iree_ukernel_size_t N;
+  iree_ukernel_size_t K;
+  int32_t M0;
+  int32_t N0;
+  int32_t K0;
+  uint32_t flags;
+};
+
+typedef struct iree_ukernel_mmt4d_f32f32f32_params_t
+    iree_ukernel_mmt4d_f32f32f32_params_t;
+typedef struct iree_ukernel_mmt4d_i8i8i32_params_t
+    iree_ukernel_mmt4d_i8i8i32_params_t;
+
+#define IREE_UKERNEL_MMT4D_ERROR_UNIMPLEMENTED 1
+#define IREE_UKERNEL_MMT4D_ERROR_BAD_FLAGS 2
+
+// TODO: move these flags to a header file shared with compiler/.
+#define IREE_VMVX_MATMUL_FLAG_ACCUMULATE 1
+
+IREE_UKERNEL_EXPORT int iree_ukernel_mmt4d_f32f32f32(
+    const iree_ukernel_mmt4d_f32f32f32_params_t* params);
+IREE_UKERNEL_EXPORT int iree_ukernel_mmt4d_i8i8i32(
+    const iree_ukernel_mmt4d_i8i8i32_params_t* params);
+
+IREE_UKERNEL_EXPORT const char* iree_ukernel_mmt4d_error_message(int retcode);
 
 #ifdef __cplusplus
 }  // extern "C"
