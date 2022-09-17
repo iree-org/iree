eebcd384f8cdf9e8f65c5447aa31ee1790f3338a
Cindy Liu <hcindyl@google.com>
2022-09-16 17:22:20 -0700

    Fix RISC-V 64 Linux test script
    The tests can actually be run via QEMU
    
    * Fix iree_hal_cts_test_suite to support RISC-V cross compile
    * Need to disable
      tests/e2e/regression/check_regression_llvm-cpu_lowering_config.mlir in
      the CI because it hangs indefinitely

--- build_tools/cmake/test_riscv64.sh
+++ build_tools/cmake/test_riscv64.sh
@@ -79,8 +79,9 @@ ctest --test-dir ${BUILD_RISCV_DIR}/runtime/ --timeout 900 --output-on-failure \
   --no-tests=error --label-exclude \
   '(^nokokoro$|^driver=vulkan$|^driver=cuda$|^vulkan_uses_vk_khr_shader_float16_int8$|^requires-filesystem$|^requires-dtz$)'
 
-# Test e2e models. Excluding mobilebert and fp16 for now.
+# Test e2e models. Excluding mobilebert, fp16, and lowering_config regression
+# tests for now.
 ctest --test-dir ${BUILD_RISCV_DIR}/tests/e2e --timeout 900 --output-on-failure \
   --no-tests=error --label-exclude \
   '(^nokokoro$|^driver=vulkan$|^driver=cuda$|^vulkan_uses_vk_khr_shader_float16_int8$)' \
-  -E '(bert|fp16)'
+  -E '(bert|fp16|regression_llvm-cpu_lowering_config)'
