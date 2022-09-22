25d21114267d23a2aacd8e81eba8b781ced45d9f
Jerry Wu <cheyuw@google.com>
2022-09-22 20:56:21 +0000

    Fix comments

--- build_tools/python/e2e_test_framework/models/tf_models.py
+++ build_tools/python/e2e_test_framework/models/tf_models.py
@@ -3,7 +3,7 @@
 # Licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-"""Defines TFLite models."""
+"""Defines Tensorflow models."""
 
 from e2e_test_framework import unique_ids
 from e2e_test_framework.definitions import common_definitions
