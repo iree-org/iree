0dd745ad06c16d166ccb789272d0606adf700e02
Hanhan Wang <hanchung@google.com>
2022-09-28 15:28:46 -0700

    format

--- llvm-external-projects/iree-dialects/lib/Dialect/LinalgExt/IR/LinalgExtOps.cpp
+++ llvm-external-projects/iree-dialects/lib/Dialect/LinalgExt/IR/LinalgExtOps.cpp
@@ -1640,9 +1640,8 @@ LogicalResult PackOp::verify() {
   // semantics.
   if (!getPaddingValue() &&
       areNotFullTiles(getInputShape(), getDimAndTileMapping())) {
-    return op->emitError(
-        "invalid tile factor provided. Only full tiles are "
-        "supported when padding_value is not set");
+    return op->emitError("invalid tile factor provided. Only full tiles are "
+                         "supported when padding_value is not set");
   }
 
   // Verify result type against inferred type.
