b16747867b1ab43c59042de0a430f60bc12636ce
Quinn Dawkins <quinn.dawkins@gmail.com>
2022-09-29 15:44:59 -0400

    [Flow] Pass to convert NCHW convolutions to NHWC
    The conversion pass is enabled with `--iree-flow-enable-conv-nchw-to-nhwc-transform`
    
    Includes partial support for propagating and cancelling transposes generated
    when converting from nchw to nhwc. The high level strategy for this pass is
    as follows:
        1. Do the conversions for all conv_nchw_fchw ops (and pooling ops) and
        wrap the converted convolutions in transposes. Each transpose is tagged
        to indicate which direction the transpose should propagate through the
        graph.
        2. Traverse the ops in the function in reverse to propagate transposes
        marked for upwards propagation to their parents. Ideally just before ops
        such as arith.constant or function arguments.
        3. Propagate the transposes marked for downward propagation to its users,
        ideally to just before return.
        4. Canonicalize out all adjacent cancelling transposes and generalize the
        remaining transposes to allow for fusing them with nearby ops.

--- compiler/src/iree/compiler/Preprocessing/Common/Passes.h
+++ compiler/src/iree/compiler/Preprocessing/Common/Passes.h
@@ -9,6 +9,7 @@
 
 #include <functional>
 
+#include "iree/compiler/Dialect/Flow/IR/FlowOps.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Pass/PassManager.h"
 
@@ -19,6 +20,10 @@ namespace IREE {
 // Creates a pass to convert linalg convolution ops into linalg.matmul ops
 // using im2col tranformation.
 std::unique_ptr<Pass> createConvertConv2DToImg2ColPass();
+
+// Creates a pass to convert linalg NCHW Convolutions to NHWC.
+std::unique_ptr<InterfacePass<mlir::FunctionOpInterface>>
+createConvertConvNchwToNhwcPass();
 
 // A pass to pad linalg ops to the next integer multiple of `paddingSize`.
 std::unique_ptr<Pass> createPadLinalgOpsToIntegerMultiplePass();
