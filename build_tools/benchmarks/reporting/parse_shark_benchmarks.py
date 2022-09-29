5f73a3cf61ca58b239a16033b38f4636b1f85681
mariecwhite <mariewhite@google.com>
2022-09-28 23:50:45 -0700

    yapf

--- build_tools/benchmarks/reporting/parse_shark_benchmarks.py
+++ build_tools/benchmarks/reporting/parse_shark_benchmarks.py
@@ -3,7 +3,6 @@
 # Licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
 r"""Script to generate a HTML summary of SHARK Tank benchmarks.
 
 Example usage:
