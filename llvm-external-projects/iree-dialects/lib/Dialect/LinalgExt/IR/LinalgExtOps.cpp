8047410536f3f18b9371f40bbfd4a7de72b6835b
Han-Chung Wang <hanchung@google.com>
2022-10-05 02:56:18 +0800

    Add support for tiling linalg_ext.pack op (#10600)
    Also fixed a bug in reifyResultShapes. The output should be derived from input shapes.

--- llvm-external-projects/iree-dialects/lib/Dialect/LinalgExt/IR/LinalgExtOps.cpp
+++ llvm-external-projects/iree-dialects/lib/Dialect/LinalgExt/IR/LinalgExtOps.cpp
@@ -1874,6 +1874,125 @@ LogicalResult PackOp::generateScalarImplementation(OpBuilder &builder,
   return success();
 }
 
+SmallVector<Operation *>
+PackOp::getTiledImplementation(OpBuilder &builder,
+                               ArrayRef<OpFoldResult> offsets,
+                               ArrayRef<OpFoldResult> sizes) {
+  Location loc = getLoc();
+  auto ctx = builder.getContext();
+
+  // Take the minimum of two integers.
+  auto idMap = AffineMap::getMultiDimIdentityMap(2, ctx);
+  auto min = [&](OpFoldResult v1, OpFoldResult v2) -> OpFoldResult {
+    return makeComposedFoldedAffineMin(builder, loc, idMap, {v1, v2});
+  };
+  // Subtract two integers.
+  AffineExpr dim0, dim1;
+  bindDims(ctx, dim0, dim1);
+  auto subMap = AffineMap::get(2, 0, {dim0 - dim1});
+  auto sub = [&](OpFoldResult v1, OpFoldResult v2) -> OpFoldResult {
+    return makeComposedFoldedAffineApply(builder, loc, subMap, {v1, v2});
+  };
+
+  int64_t inputRank = getInputRank();
+  DenseMap<int64_t, OpFoldResult> dimAndTileMapping = getDimAndTileMapping();
+  SmallVector<OpFoldResult> inputIndices, inputSizes;
+  for (auto dim : llvm::seq<int64_t>(0, inputRank)) {
+    if (dimAndTileMapping.count(dim)) {
+      // If the dimension is tiled, the i-th index is the product of offset_i
+      // and tile_i, and the i-th size is the product of sizes_i and tile_i.
+      AffineExpr i, tile;
+      bindDims(ctx, i);
+      bindSymbols(ctx, tile);
+      OpFoldResult inputIndex = makeComposedFoldedAffineApply(
+          builder, loc, i * tile,
+          ArrayRef<OpFoldResult>{offsets[dim], dimAndTileMapping[dim]});
+      inputIndices.push_back(inputIndex);
+
+      OpFoldResult inputSize = makeComposedFoldedAffineApply(
+          builder, loc, i * tile,
+          ArrayRef<OpFoldResult>{sizes[dim], dimAndTileMapping[dim]});
+      inputSizes.push_back(inputSize);
+    } else {
+      inputIndices.push_back(offsets[dim]);
+      inputSizes.push_back(sizes[dim]);
+    }
+
+    // Limit the size of the input operand for incomplet tiles.
+    OpFoldResult dimSize = getDim(builder, loc, getInput(), dim);
+    inputSizes.back() =
+        min(inputSizes.back(), sub(dimSize, inputIndices.back()));
+  }
+
+  auto oneAttr = builder.getI64IntegerAttr(1);
+  SmallVector<OpFoldResult> strides(inputRank, oneAttr);
+
+  SmallVector<Value> tiledOperands;
+  tiledOperands.push_back(
+      getSlice(builder, loc, getInput(), inputIndices, inputSizes, strides));
+
+  SmallVector<OpFoldResult> outputOffsets, outputSizes;
+  if (failed(getResultTilePosition(builder, 0, offsets, sizes, outputOffsets,
+                                   outputSizes))) {
+    return {};
+  }
+  strides.append(getOutputRank() - inputRank, oneAttr);
+  tiledOperands.push_back(
+      getSlice(builder, loc, getOutput(), outputOffsets, outputSizes, strides));
+
+  for (auto tile : getInnerTiles()) {
+    tiledOperands.push_back(tile);
+  }
+  if (auto val = getPaddingValue()) {
+    tiledOperands.push_back(val);
+  }
+
+  // There are exactly one input and one output, the output is the second
+  // operand.
+  SmallVector<Type, 4> tiledResultTypes;
+  if (hasTensorSemantics()) {
+    tiledResultTypes.push_back(tiledOperands[1].getType());
+  }
+
+  Operation *tiledPackOp =
+      cast<LinalgExtOp>(getOperation())
+          .clone(builder, loc, tiledResultTypes, tiledOperands);
+
+  return {tiledPackOp};
+}
+
+LogicalResult PackOp::getResultTilePosition(
+    OpBuilder &builder, unsigned resultNumber, ArrayRef<OpFoldResult> offsets,
+    ArrayRef<OpFoldResult> sizes, SmallVector<OpFoldResult> &resultOffsets,
+    SmallVector<OpFoldResult> &resultSizes) {
+  // The tiling is applied on outer dimensions. In this context, the outer
+  // dimenensions of result tile position is the same. The inner offsets are
+  // zeros becuase tiling is not applied to them.
+  auto zeroAttr = builder.getI64IntegerAttr(0);
+  resultOffsets.assign(offsets.begin(), offsets.end());
+  resultOffsets.append(getOutputRank() - getInputRank(), zeroAttr);
+
+  ReifiedRankedShapedTypeDims outputShape;
+  if (failed(reifyResultShapes(builder, outputShape)))
+    return getOperation()->emitOpError("failed to reify result shape");
+  if (outputShape.size() != 1 || outputShape[0].size() != getOutputRank()) {
+    return getOperation()->emitOpError(
+               "expected shape of one result value of rank")
+           << getOutputRank();
+  }
+
+  // The outer sizes are the same because the iteration space is over outer
+  // dimensions. The inner sizes are whole sizes because tiling is not applied
+  // on them.
+  resultSizes.assign(sizes.begin(), sizes.end());
+  for (auto dataTileDim :
+       llvm::seq<unsigned>(getInputRank(), getOutputRank())) {
+    resultSizes.push_back(getAsOpFoldResult(outputShape[0][dataTileDim]));
+  }
+
+  return success();
+}
+
 LogicalResult
 PackOp::reifyResultShapes(OpBuilder &builder,
                           ReifiedRankedShapedTypeDims &reifiedReturnShapes) {
@@ -1896,8 +2015,12 @@ PackOp::reifyResultShapes(OpBuilder &builder,
     };
     // If we are dealing with a tiled dimension compose the map otherwise
     // return the dimension extracted with `memref.dim`.
+    // TODO(hanchung): Should bind inner size to inner_tiles if they are
+    // dynamic.
     OpFoldResult dimBound =
-        getDim(builder, getOperation()->getLoc(), getOutput(), dimIdx);
+        dimIdx < getInputRank()
+            ? getDim(builder, getOperation()->getLoc(), getInput(), dimIdx)
+            : getDim(builder, getOperation()->getLoc(), getOutput(), dimIdx);
     return (dimAndTileMapping.count(dimIdx))
                ? apply(dim.ceilDiv(tile),
                        ArrayRef<OpFoldResult>{dimBound,
