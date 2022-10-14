699f35bb3b32463eea433dd1062d233cc6eb20ac
Okwan Kwon <okwan@google.com>
2022-10-14 15:18:00 -0700

    Do not promote to sharemem for dynamic tensors (#10649)
    Specialization helped the performance too and now the sharemem promotion
    actually slows down the partial tile portion. Needs a broad evaluation.
    
    For example, distillbert shows improvement from 6.21ms to 5.29ms.

--- compiler/src/iree/compiler/Codegen/LLVMGPU/LLVMGPUTensorAlloc.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/LLVMGPUTensorAlloc.cpp
@@ -22,6 +22,12 @@ namespace iree_compiler {
 static bool contractOpFilter(Operation *op) {
   auto linalgOp = dyn_cast<linalg::LinalgOp>(op);
   if (!linalgOp) return false;
+
+  // The workgroup specialization already makes static shapes available for the
+  // main tile part and makes the partial tile computation small, so promoting
+  // to shared memory for the partial tile actually hurts the performance.
+  if (linalgOp.hasDynamicShape()) return false;
+
   SmallVector<unsigned> dims;
   linalgOp.getParallelDims(dims);
   SmallVector<int64_t, 4> shapes = linalgOp.getStaticLoopRanges();
