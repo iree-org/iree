043402e0fd77e3a9d5a72bd2eca52850627d86d4
Jerry Wu <cheyuw@google.com>
2022-10-20 23:19:04 +0000

    Remove unused files.

--- build_tools/python/e2e_test_framework/iree_tf_import_template.cmake
+++
@@ -1,9 +0,0 @@
-# Import the Tensorflow model "$__SOURCE_MODEL_PATH"
-iree_import_tf_model(
-  TARGET_NAME "$${_PACKAGE_NAME}_$__TARGET_NAME"
-  SOURCE "$__SOURCE_MODEL_PATH"
-  ENTRY_FUNCTION "$__ENTRY_FUNCTION"
-  OUTPUT_MLIR_FILE "$__OUTPUT_PATH"
-)
-# Mark dependency so users can import models without compiling them.
-add_dependencies(iree-benchmark-import-models "$${_PACKAGE_NAME}_$__TARGET_NAME")
