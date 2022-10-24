ab3fd462683fb0ac49cee039eb49cb783c6d7569
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-24 17:37:26 +0000

    Adding tarfile member sanitization to extractall()

--- build_tools/scripts/download_file.py
+++ build_tools/scripts/download_file.py
@@ -61,7 +61,26 @@ def main(args):
           if os.path.exists(args.output):
             shutil.rmtree(args.output)
           os.makedirs(args.output)
-          tar_file.extractall(args.output)
+          def is_within_directory(directory, target):
+              
+              abs_directory = os.path.abspath(directory)
+              abs_target = os.path.abspath(target)
+          
+              prefix = os.path.commonprefix([abs_directory, abs_target])
+              
+              return prefix == abs_directory
+          
+          def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+          
+              for member in tar.getmembers():
+                  member_path = os.path.join(path, member.name)
+                  if not is_within_directory(path, member_path):
+                      raise Exception("Attempted Path Traversal in Tar File")
+          
+              tar.extractall(path, members, numeric_owner=numeric_owner) 
+              
+          
+          safe_extract(tar_file, args.output)
         return
       elif args.source_url.endswith(".gz"):
         # Open gzip from a file-like object, which will be in the streaming mode.
