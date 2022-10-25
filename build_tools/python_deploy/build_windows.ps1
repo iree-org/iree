7d14f3b2b635c6138d59852be43729224427fde8
Anush Elangovan <anush@nod-labs.com>
2022-10-24 23:17:43 -0700

    Add a Windows Build Powershell script

---
+++ build_tools/python_deploy/build_windows.ps1
@@ -0,0 +1,25 @@
+#Write-Host "Installing python"
+
+#Start-Process winget install Python.Python.3.10 '/quiet InstallAllUsers=1 PrependPath=1' -wait -NoNewWindow
+
+#Write-Host "python installation completed successfully"
+
+#Write-Host "Reload environment variables"
+#$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+#Write-Host "Reloaded environment variables"
+
+Write-Host "Installing Build Dependencies"
+python -m venv .\iree_venv\
+.\iree_venv\Scripts\activate
+pip install -r runtime\bindings\python\iree\runtime\build_requirements.txt
+Write-Host "Build Deps installation completed successfully" 
+
+Write-Host "Building ..." 
+$env:CMAKE_GENERATOR='Ninja'
+$env:IREE_HAL_DRIVER_CUDA = 'ON'
+$env:IREE_HAL_DRIVER_VULKAN = 'ON'
+$env:IREE_EXTERNAL_HAL_DRIVERS = 'OFF'
+pip wheel -v -w wheelhouse compiler/
+pip wheel -v -w wheelhouse runtime/
+
+Write-Host "Build completed successfully" 
