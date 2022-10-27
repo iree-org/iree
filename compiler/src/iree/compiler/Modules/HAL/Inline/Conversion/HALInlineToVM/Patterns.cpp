e23046ca1aa730ea4b3e2c2974415d40e81aa8b5
Ben Vanik <ben.vanik@gmail.com>
2022-10-27 14:08:25 -0700

    Renaming some more conversion patterns files missed in #10921.

--- compiler/src/iree/compiler/Modules/HAL/Inline/Conversion/HALInlineToVM/Patterns.cpp
+++ compiler/src/iree/compiler/Modules/HAL/Inline/Conversion/HALInlineToVM/Patterns.cpp
@@ -4,7 +4,7 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#include "iree/compiler/Modules/HAL/Inline/Conversion/HALInlineToVM/ConvertHALInlineToVM.h"
+#include "iree/compiler/Modules/HAL/Inline/Conversion/HALInlineToVM/Patterns.h"
 
 #include "iree/compiler/Dialect/Util/IR/UtilTypes.h"
 #include "iree/compiler/Dialect/VM/Conversion/ConversionTarget.h"
