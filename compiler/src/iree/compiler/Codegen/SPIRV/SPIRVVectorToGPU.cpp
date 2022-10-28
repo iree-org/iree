357537c0b703a53971e5f94ec3df322480f9ae47
Lei Zhang <antiagainst@google.com>
2022-10-28 10:25:12 -0700

    Trim unnecessary headers

--- compiler/src/iree/compiler/Codegen/SPIRV/SPIRVVectorToGPU.cpp
+++ compiler/src/iree/compiler/Codegen/SPIRV/SPIRVVectorToGPU.cpp
@@ -7,13 +7,9 @@
 #include "iree/compiler/Codegen/Common/GPUPatterns.h"
 #include "iree/compiler/Codegen/PassDetail.h"
 #include "iree/compiler/Codegen/Passes.h"
-#include "llvm/ADT/SetVector.h"
 #include "mlir/Conversion/VectorToGPU/VectorToGPU.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
-#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/GPU/IR/GPUDialect.h"
-#include "mlir/Dialect/NVGPU/IR/NVGPUDialect.h"
-#include "mlir/Dialect/NVGPU/Transforms/Transforms.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
 namespace mlir {
