0fdf7466b796bf17256cd9f700b250362ddac6ed
Jerry Wu <cheyuw@google.com>
2022-10-31 04:50:22 +0000

    Fix comments

--- build_tools/python/e2e_model_tests/cmake_generator.py
+++ build_tools/python/e2e_model_tests/cmake_generator.py
@@ -3,7 +3,7 @@
 # Licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-"""Defines e2e model tests on benchmark models."""
+"""Generates e2e model tests from benchmark models."""
 
 from dataclasses import dataclass
 from typing import List
