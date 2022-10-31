c22d048eeb07684cb08d1b839796b98a1a5fc13a
Benoit Jacob <benoitjacob@google.com>
2022-10-31 16:11:39 +0000

    Fix GCC build and move this code to separate static_assert.h

--- runtime/src/iree/builtins/ukernel/exported_flag_bits.h
+++ runtime/src/iree/builtins/ukernel/exported_flag_bits.h
@@ -7,13 +7,15 @@
 #ifndef IREE_BUILTINS_UKERNEL_EXPORTED_FLAG_BITS_H_
 #define IREE_BUILTINS_UKERNEL_EXPORTED_FLAG_BITS_H_
 
+#include "iree/builtins/ukernel/static_assert.h"
+
 // This header is shared across:
 //
-// * compiler/ C++ code
-// * ukernel/ C code
-// * ukernel/ asm code
+// * C++ code under compiler/
+// * C code   under runtime/.../ukernel/
+// * asm code under runtime/.../ukernel/
 //
-// Being shared with the compiler means that we should treat these flags as set
+// Being shared with compiler/ means that we should treat these flags as set
 // in stone. Don't count on being able to remove or change the numerical value
 // of an existing flag.
 //
@@ -43,25 +45,10 @@
 #define IREE_UKERNEL_FLAG_PACK_TRANSPOSE_OUTER_BIT_POS 17
 
 // Static assertions ensuring consistency of the above flag values.
-//
-#if defined(__ASSEMBLER__)
-// Note that __STDC__ may also be defined here (when the assembler driver is the
-// C compiler driver). So it's easiest to get this case out of the way first.
-//
-// Leave IREE_UKERNEL_STATIC_ASSERT undefined.
-#elif defined(__STDC__)  // Really C, as __ASSEMBLER__ is not defined.
-#define IREE_UKERNEL_STATIC_ASSERT(COND) _Static_assert(COND, #COND)
-#elif defined(__cplusplus)
-#define IREE_UKERNEL_STATIC_ASSERT(COND) static_assert(COND, #COND)
-#else
-// Leave IREE_UKERNEL_STATIC_ASSERT undefined. Typically the case in .S files.
-#endif
-#if defined(IREE_UKERNEL_STATIC_ASSERT)
 #define IREE_UKERNEL_ENSURE_CONSISTENT_FLAG(F) \
   IREE_UKERNEL_STATIC_ASSERT((F) == (1u << (F##_BIT_POS)))
 IREE_UKERNEL_ENSURE_CONSISTENT_FLAG(IREE_UKERNEL_FLAG_ACCUMULATE);
 IREE_UKERNEL_ENSURE_CONSISTENT_FLAG(IREE_UKERNEL_FLAG_PACK_TRANSPOSE_INNER);
 IREE_UKERNEL_ENSURE_CONSISTENT_FLAG(IREE_UKERNEL_FLAG_PACK_TRANSPOSE_OUTER);
-#endif  // defined(IREE_UKERNEL_STATIC_ASSERT)
 
 #endif  // IREE_BUILTINS_UKERNEL_EXPORTED_FLAG_BITS_H_
