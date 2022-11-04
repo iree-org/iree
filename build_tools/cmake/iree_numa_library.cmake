5dcb6a3b4d9e779271fdb5ac16acf73dc351aac3
aviator19941 <aviator1994@gmail.com>
2022-11-04 23:45:33 +0000

    Fix configure numactl before build

--- build_tools/cmake/iree_numa_library.cmake
+++ build_tools/cmake/iree_numa_library.cmake
@@ -1,13 +1,7 @@
-include(ExternalProject)
-
 set(NUMA_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/third_party/numactl")
-ExternalProject_Add(
-  numa
-  PREFIX ${NUMA_SOURCE_DIR}
-  STAMP_DIR ${NUMA_SOURCE_DIR}
-  SOURCE_DIR ${NUMA_SOURCE_DIR}
-  CONFIGURE_COMMAND cd ${NUMA_SOURCE_DIR} && ${NUMA_SOURCE_DIR}/autogen.sh
-  COMMAND ${NUMA_SOURCE_DIR}/configure
-  INSTALL_COMMAND cmake -E echo "Skipping install step."
-  BUILD_IN_SOURCE 1
+execute_process(COMMAND ./autogen.sh
+  WORKING_DIRECTORY ${NUMA_SOURCE_DIR}
+)
+execute_process(COMMAND ./configure
+  WORKING_DIRECTORY ${NUMA_SOURCE_DIR}
 )
