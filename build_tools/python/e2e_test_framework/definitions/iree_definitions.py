020ca0ec7387d74cf8034faf8a1178b8ae87d1dd
Jerry Wu <cheyuw@google.com>
2022-11-11 19:38:42 +0000

    Cleanup

--- build_tools/python/e2e_test_framework/definitions/iree_definitions.py
+++ build_tools/python/e2e_test_framework/definitions/iree_definitions.py
@@ -7,7 +7,7 @@
 
 from dataclasses import dataclass
 from enum import Enum
-from typing import List, Optional
+from typing import List
 import dataclasses
 
 from e2e_test_framework.definitions import common_definitions
@@ -36,8 +36,6 @@ class RuntimeLoader(Enum):
   EMBEDDED_ELF = "embedded-elf"
   VMVX_MODULE = "vmvx-module"
   SYSTEM_LIBRARY = "system-library"
-  CUDA = "cuda"
-  VULKAN = "vulkan"
 
 
 class RuntimeDriver(Enum):
@@ -127,31 +125,3 @@ class E2EModelRunConfig(object):
   module_execution_config: ModuleExecutionConfig
   target_device_spec: common_definitions.DeviceSpec
   input_data: common_definitions.ModelInputData
-
-
-def get_run_flags_of_model(
-    model: common_definitions.Model,
-    model_input_data: common_definitions.ModelInputData) -> List[str]:
-  """Returns the IREE run module flags for the model and its inputs."""
-
-  args = [f"--entry_function={model.entry_function}"]
-  if model_input_data != common_definitions.ZEROS_MODEL_INPUT_DATA:
-    raise ValueError("Currently only support all-zeros data.")
-  args += [
-      f"--function_input={input_type}=0" for input_type in model.input_types
-  ]
-  return args
-
-
-def get_run_flags_of_execution_config(
-    module_execution_config: ModuleExecutionConfig,
-    gpu_id: str = "0") -> List[str]:
-  """Returns the IREE run module flags of the execution config."""
-
-  run_flags = module_execution_config.extra_flags
-  driver = module_execution_config.driver
-  if driver == RuntimeDriver.CUDA:
-    run_flags.append(f"--device=cuda://{gpu_id}")
-  else:
-    run_flags.append(f"--device={driver.value}")
-  return run_flags
