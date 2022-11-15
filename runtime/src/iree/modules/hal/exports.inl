AaronStGeorge/hrx-system

Adding hal.command_buffer.collective & co runtime imports.

runtime/src/iree/modules/hal/exports.inl
@@ -43,7 +43,11 @@ EXPORT_FN("buffer_view.encoding_type", iree_hal_module_buffer_view_encoding_type
 EXPORT_FN("buffer_view.rank", iree_hal_module_buffer_view_rank, r, i)
 EXPORT_FN("buffer_view.trace", iree_hal_module_buffer_view_trace, rCrD, v)
 
+EXPORT_FN("channel.create", iree_hal_module_channel_create, rIii, r)
+EXPORT_FN("channel.rank_and_count", iree_hal_module_channel_rank_and_count, r, ii)
+
 EXPORT_FN("command_buffer.begin_debug_group", iree_hal_module_command_buffer_begin_debug_group, rr, v)
+EXPORT_FN("command_buffer.collective", iree_hal_module_command_buffer_collective, rriirIIrIII, v)
 EXPORT_FN("command_buffer.copy_buffer", iree_hal_module_command_buffer_copy_buffer, rrIrII, v)
 EXPORT_FN("command_buffer.create", iree_hal_module_command_buffer_create, riii, r)
 EXPORT_FN("command_buffer.dispatch", iree_hal_module_command_buffer_dispatch, rriiii, v)

iree-org/iree

runtime/src/iree/modules/hal/exports.inl
@@ -43,7 +43,11 @@ EXPORT_FN("buffer_view.encoding_type", iree_hal_module_buffer_view_encoding_type
 EXPORT_FN("buffer_view.rank", iree_hal_module_buffer_view_rank, r, i)
 EXPORT_FN("buffer_view.trace", iree_hal_module_buffer_view_trace, rCrD, v)
 
+EXPORT_FN("channel.create", iree_hal_module_channel_create, rIi, r)
+EXPORT_FN("channel.rank_and_count", iree_hal_module_channel_rank_and_count, r, ii)
+
 EXPORT_FN("command_buffer.begin_debug_group", iree_hal_module_command_buffer_begin_debug_group, rr, v)
+EXPORT_FN("command_buffer.collective", iree_hal_module_command_buffer_collective, rriirIIrIII, v)
 EXPORT_FN("command_buffer.copy_buffer", iree_hal_module_command_buffer_copy_buffer, rrIrII, v)
 EXPORT_FN("command_buffer.create", iree_hal_module_command_buffer_create, riii, r)
 EXPORT_FN("command_buffer.dispatch", iree_hal_module_command_buffer_dispatch, rriiii, v)