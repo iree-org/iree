006e77656b89eafc3e9ad1f26e219d767dea066b
Cindy Liu <hcindyl@google.com>
2022-11-15 11:40:09 -0800

    Fix windows build

--- build_tools/embed_data/c_embed_data_test.cc
+++ build_tools/embed_data/c_embed_data_test.cc
@@ -10,21 +10,35 @@
 
 namespace {
 
+#if defined(_MSC_VER)
+#define NEWLINE "\r\n"
+#else
+#define NEWLINE "\n"
+#endif
+
 TEST(Generator, TestContents) {
   auto* toc1 = testembed1_create();
   ASSERT_EQ("file1.bin", std::string(toc1->name));
   EXPECT_EQ(R"(Are you '"Still"' here?)"
-            "\n",
+            NEWLINE,
             std::string(toc1->data));
+#if defined(_MSC_VER)
+  EXPECT_EQ(25, toc1->size);
+#else
   EXPECT_EQ(24, toc1->size);
+#endif
   ASSERT_EQ(0, *(toc1->data + toc1->size));
 
   ++toc1;
   ASSERT_EQ("file2.bin", std::string(toc1->name));
   EXPECT_EQ(R"(¯\_(ツ)_/¯)"
-            "\n",
+            NEWLINE,
             std::string(toc1->data));
+#if defined(_MSC_VER)
+  EXPECT_EQ(15, toc1->size);
+#else
   ASSERT_EQ(14, toc1->size);
+#endif
   ASSERT_EQ(0, *(toc1->data + toc1->size));
 
   ++toc1;
@@ -34,9 +48,13 @@ TEST(Generator, TestContents) {
   auto* toc2 = testembed2_create();
   ASSERT_EQ("file3.bin", std::string(toc2->name));
   EXPECT_EQ(R"(ᕕ( ᐛ )ᕗ)"
-            "\n",
+            NEWLINE,
             std::string(toc2->data));
+#if defined(_MSC_VER)
+  EXPECT_EQ(15, toc2->size);
+#else
   EXPECT_EQ(14, toc2->size);
+#endif
   ASSERT_EQ(0, *(toc2->data + toc2->size));
 }
 
