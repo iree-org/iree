077168f6c8b2bff9eda724f7d2a764c909d8458c
Han-Chung Wang <hanchung@google.com>
2022-11-17 07:47:34 +0800

    [LLVMCPU] Enable vectorization for pack ops. (#11178)
    It introduces `SplitFullPartialTransferPass` to hook up upstream
    VectorTransferFullPartialRewriter pattern. The pattern splits a
    vector.transfer operation into an in-bounds (i.e., no out-of-bounds
    masking) fastpath and a slowpath. This needs to be applied before
    converting to LLVM dialect.
    
    Verified that we're getting 2x-3x performance improvements from Tracy.
    
    For LHS packing, i.e.,
    
    ```mlir
    lhs_4d = iree_linalg_ext.pack %lhs
      inner_dims_pos = [0, 1]
      inner_tiles = [8, 8]
      into %lhs_4d_init
      : (tensor<256x256xi8> tensor<32x32x8x8xi8>) -> tensor<32x32x8x8xi8>
    ```
    
    The performance is improved from `32.59` us to `10.01` us.
    
    
    For RHS packing, i.e.,
    
    ```mlir
    %rhs_4d = iree_linalg_ext.pack %rhs
      outer_dims_perm = [1, 0]
      inner_dims_pos = [1, 0]
      inner_tiles = [8, 8]
      into %rhs_4d_init
      : (tensor<256x256xi8> tensor<32x32x8x8xi8>) -> tensor<32x32x8x8xi8>
    ```
    
    The performance is improved from `32.35` us to `16.11` us.
    
    Fixes https://github.com/iree-org/iree/issues/10889

---
+++ compiler/src/iree/compiler/Codegen/Common/SplitFullPartialTransferPass.cpp
@@ -0,0 +1,59 @@
+// Copyright 2022 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <string>
+
+#include "iree/compiler/Codegen/PassDetail.h"
+#include "iree/compiler/Codegen/Passes.h"
+#include "mlir/Dialect/Vector/Transforms/VectorRewritePatterns.h"
+#include "mlir/Dialect/Vector/Transforms/VectorTransforms.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+
+namespace mlir {
+namespace iree_compiler {
+namespace {
+
+struct SplitFullPartialTransferPass
+    : public SplitFullPartialTransferBase<SplitFullPartialTransferPass> {
+  SplitFullPartialTransferPass() = default;
+  SplitFullPartialTransferPass(StringRef option) {
+    this->splitVectorTransfersTo = std::string(option);
+  }
+
+  void runOnOperation() override {
+    MLIRContext *ctx = &getContext();
+    RewritePatternSet patterns(ctx);
+
+    auto options = vector::VectorTransformsOptions().setVectorTransferSplit(
+        llvm::StringSwitch<vector::VectorTransferSplit>(
+            splitVectorTransfersTo.getValue())
+            .Case("none", vector::VectorTransferSplit::None)
+            .Case("linalg-copy", vector::VectorTransferSplit::LinalgCopy)
+            .Case("vector-transfers",
+                  vector::VectorTransferSplit::VectorTransfer)
+            .Default(vector::VectorTransferSplit::None));
+    patterns.add<vector::VectorTransferFullPartialRewriter>(ctx, options);
+    if (failed(applyPatternsAndFoldGreedily(getOperation(),
+                                            std::move(patterns)))) {
+      return signalPassFailure();
+    }
+  }
+};
+
+}  // namespace
+
+std::unique_ptr<OperationPass<func::FuncOp>>
+createSplitFullPartialTransferPass() {
+  return std::make_unique<SplitFullPartialTransferPass>();
+}
+std::unique_ptr<OperationPass<func::FuncOp>> createSplitFullPartialTransferPass(
+    StringRef option) {
+  return std::make_unique<SplitFullPartialTransferPass>(option);
+}
+
+}  // namespace iree_compiler
+}  // namespace mlir
