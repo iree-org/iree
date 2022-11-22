3448a892a6aba3b9eec7a97eceab1a35e86441a4
Jerry Wu <cheyuw@google.com>
2022-11-22 01:24:20 +0000

    Fix

--- build_tools/testing/generate_cmake_e2e_test_artifacts_suite.py
+++ build_tools/testing/generate_cmake_e2e_test_artifacts_suite.py
@@ -40,17 +40,16 @@ def main(args: argparse.Namespace):
   artifacts_root = (
       e2e_test_artifacts.artifacts.generate_default_artifacts_root())
 
-  root_path = pathlib.PurePath(f"${{{ROOT_ARTIFACTS_DIR_CMAKE_VARIABLE}}}")
+  root_path = pathlib.PurePath("${%s}" % ROOT_ARTIFACTS_DIR_CMAKE_VARIABLE)
   package_name = f"${{{PACKAGE_NAME_CMAKE_VARIABLE}}}"
   model_rule_map = model_rule_generator.generate_model_rule_map(
       root_path=root_path, artifacts_root=artifacts_root.model_artifacts_root)
 
   output_dir = pathlib.Path(args.output_dir)
   fetch_models_cmake_file = output_dir / GENERATED_E2E_TEST_FETCH_MODELS_CMAKE_FILENAMAE
-  with fetch_models_cmake_file.open("w") as output_file:
-    cmake_rules = itertools.chain.from_iterable(
-        rule.cmake_rules for rule in model_rule_map.values())
-    output_file.write("\n".join(cmake_rules))
+  cmake_rules = itertools.chain.from_iterable(
+      rule.cmake_rules for rule in model_rule_map.values())
+  fetch_models_cmake_file.write_text("\n".join(cmake_rules))
 
   iree_cmake_rules = iree_rule_generator.generate_rules(
       package_name=package_name,
@@ -58,9 +57,8 @@ def main(args: argparse.Namespace):
       artifacts_root=artifacts_root.iree_artifacts_root,
       model_rule_map=model_rule_map)
 
-  iree_artifacts_cmake_file = output_dir / GENERATED_E2E_TEST_IREE_ARTIFACTS_CMAKE_FILENAME
-  with iree_artifacts_cmake_file.open("w") as output_file:
-    output_file.write("\n".join(iree_cmake_rules))
+  (output_dir / GENERATED_E2E_TEST_IREE_ARTIFACTS_CMAKE_FILENAME).write_text(
+      "\n".join(iree_cmake_rules))
 
 
 if __name__ == "__main__":
