ae0e293a9bac9123f977e63081599c98d7a5a8eb
Jerry Wu <cheyuw@google.com>
2022-11-22 22:37:21 +0000

    Option to ignore dynamic dims in stack allocation check (#11254)

--- compiler/src/iree/compiler/Codegen/LLVMCPU/LLVMCPUCheckIRBeforeLLVMConversion.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMCPU/LLVMCPUCheckIRBeforeLLVMConversion.cpp
@@ -17,6 +17,11 @@ static llvm::cl::opt<int> clMaxAllocationSizeInBytes(
     "iree-llvmcpu-stack-allocation-limit",
     llvm::cl::desc("maximum allowed stack allocation size in bytes"),
     llvm::cl::init(32768));
+static llvm::cl::opt<bool> clFailUnboundDynamicStackAllocation(
+    "iree-llvmcpu-fail-unbound-dynamic-stack-allocation",
+    llvm::cl::desc("fail if the upper bound of dynamic stack allocation cannot "
+                   "be solved"),
+    llvm::cl::init(true));
 
 namespace {
 struct LLVMCPUCheckIRBeforeLLVMConversionPass
@@ -38,11 +43,12 @@ void LLVMCPUCheckIRBeforeLLVMConversionPass::runOnOperation() {
     }
     for (auto operand : allocaOp.getDynamicSizes()) {
       auto ub = linalg::getConstantUpperBoundForIndex(operand);
-      if (failed(ub)) {
+      if (succeeded(ub)) {
+        size *= *ub;
+      } else if (clFailUnboundDynamicStackAllocation) {
         return allocaOp.emitOpError(
             "expected no stack allocations without upper bound shapes");
       }
-      size *= *ub;
     }
     size *= type.getElementType().getIntOrFloatBitWidth();
     if (allocaOp.getAlignment()) {
