9519d2b80e37f91ce9e24cbabba800ccb0c63e73
Jerry Wu <cheyuw@google.com>
2022-11-28 20:04:56 +0000

    Add tests

--- build_tools/python/e2e_model_tests/run_module_utils_test.py
+++ build_tools/python/e2e_model_tests/run_module_utils_test.py
@@ -1,7 +1,15 @@
-# Copyright 2022 The IREE Authors
+## Copyright 2022 The IREE Authors
 #
 # Licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-iree_add_all_subdirs()
+import unittest
+
+
+class RunModuleTuilsTest(unittest.TestCase):
+  pass
+
+
+if __name__ == "__main__":
+  unittest.main()
