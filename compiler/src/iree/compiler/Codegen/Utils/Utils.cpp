3236f510e4fff146ac9d70be79c4c02481db5d33
bjacob <benoitjacob@google.com>
2022-11-30 16:45:23 -0500

    Port the AArch64 tile size selection from the old `matmul-to-mmt4d` pass. (#11366)

--- compiler/src/iree/compiler/Codegen/Utils/Utils.cpp
+++ compiler/src/iree/compiler/Codegen/Utils/Utils.cpp
@@ -129,8 +129,7 @@ bool isVMVXBackend(IREE::HAL::ExecutableTargetAttr targetAttr) {
 // features in the future, we may want to consider a persistent state to carry
 // over processed HAL information or keeping the TTI instance alive and query
 // subtarget features data structure.
-static bool hasFeature(IREE::HAL::ExecutableTargetAttr targetAttr,
-                       StringRef feature) {
+bool hasFeature(IREE::HAL::ExecutableTargetAttr targetAttr, StringRef feature) {
   Optional<StringRef> features = getCpuFeatures(targetAttr);
   if (!features) {
     return false;
