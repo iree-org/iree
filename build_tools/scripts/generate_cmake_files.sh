bb283ce34cb1497c3c91fcc43fe6a8ff51674ac0
Jerry Wu <cheyuw@google.com>
2022-12-02 17:06:28 +0000

    Remove redundant comments

--- build_tools/scripts/generate_cmake_files.sh
+++ build_tools/scripts/generate_cmake_files.sh
@@ -13,10 +13,8 @@ set -euo pipefail
 ROOT_DIR=$(git rev-parse --show-toplevel)
 cd "${ROOT_DIR}"
 
-# Generates E2E test artifacts cmake files
 ./build_tools/testing/generate_cmake_e2e_test_artifacts_suite.py \
   --output_dir ./tests/e2e/test_artifacts
 
-# Generates E2E model test cmake files
 ./build_tools/testing/generate_cmake_e2e_model_tests.py \
   --output ./tests/e2e/models/generated_e2e_model_tests.cmake
