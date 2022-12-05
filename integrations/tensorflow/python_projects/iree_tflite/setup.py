3e4ac93b0da65687bb3522e327db7f461a31b4f9
Scott Todd <scotttodd@google.com>
2022-12-05 09:31:49 -0800

    Include Python 3.7-3.10 in all package 'classifiers'.

--- integrations/tensorflow/python_projects/iree_tflite/setup.py
+++ integrations/tensorflow/python_projects/iree_tflite/setup.py
@@ -94,6 +94,10 @@ setup(
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     python_requires=">=3.7",
     packages=find_namespace_packages(include=[
