9d8ecbf1cbf6f4f0c1b82d69db7e4f52d8562b86
Jerry Wu <cheyuw@google.com>
2022-12-15 06:01:35 +0000

    Support using e2e test artifacts directory path in benchmark tool (#11542)

--- build_tools/benchmarks/common/common_arguments_test.py
+++ build_tools/benchmarks/common/common_arguments_test.py
@@ -9,39 +9,44 @@ import unittest
 import shutil
 import tempfile
 
-from common.common_arguments import build_common_argument_parser
+import common.common_arguments
 
 
 class CommonArgumentsTest(unittest.TestCase):
 
-  def test_build_common_argument_parser(self):
+  def test_parser(self):
     with tempfile.TemporaryDirectory() as tempdir:
-      arg_parser = build_common_argument_parser()
-      arg_parser.parse_args([
+      common.common_arguments.Parser().parse_args([
           "--normal_benchmark_tool_dir=" + tempdir,
           "--traced_benchmark_tool_dir=" + tempdir,
           "--trace_capture_tool=" + shutil.which("ls"), "."
       ])
 
-  def test_build_common_argument_parser_check_build_dir(self):
-    arg_parser = build_common_argument_parser()
+  def test_parser_check_build_dir(self):
+    arg_parser = common.common_arguments.Parser()
     with self.assertRaises(SystemExit):
       arg_parser.parse_args(["nonexistent"])
 
-  def test_build_common_argument_parser_check_normal_benchmark_tool(self):
-    arg_parser = build_common_argument_parser()
+  def test_parser_check_normal_benchmark_tool(self):
+    arg_parser = common.common_arguments.Parser()
     with self.assertRaises(SystemExit):
       arg_parser.parse_args(["--normal_benchmark_tool_dir=nonexistent", "."])
 
-  def test_build_common_argument_parser_check_traced_benchmark_tool(self):
-    arg_parser = build_common_argument_parser()
+  def test_parser_check_traced_benchmark_tool(self):
+    arg_parser = common.common_arguments.Parser()
     with self.assertRaises(SystemExit):
       arg_parser.parse_args(["--traced_benchmark_tool_dir=nonexistent", "."])
 
-  def test_build_common_argument_parser_check_trace_capture_tool(self):
-    arg_parser = build_common_argument_parser()
+  def test_parser_check_trace_capture_tool(self):
+    arg_parser = common.common_arguments.Parser()
     with self.assertRaises(SystemExit):
       arg_parser.parse_args(["--trace_capture_tool=nonexistent", "."])
+
+  def test_parser_e2e_test_artifacts_dir_requires_run_config(self):
+    arg_parser = common.common_arguments.Parser()
+    with tempfile.TemporaryDirectory() as tempdir:
+      with self.assertRaises(SystemExit):
+        arg_parser.parse_args([f"--e2e_test_artifacts_dir={tempdir}"])
 
 
 if __name__ == "__main__":
