964ce618ae7c6de6a4deaac71bd358e07eb0001d
Ben Vanik <ben.vanik@gmail.com>
2022-12-19 10:21:24 -0800

    Fix implementations of allocators' query_compatibility (#11599)
    Buffers should have compatibility
    IREE_HAL_BUFFER_COMPATIBILITY_QUEUE_TRANSFER if their usage is either
    IREE_HAL_BUFFER_USAGE_TRANSFER_SOURCE or
    IREE_HAL_BUFFER_USAGE_TRANSFER_TARGET.
    
    Buffers should have compatibility
    IREE_HAL_BUFFER_COMPATIBILITY_QUEUE_DISPATCH if their usage is either
    IREE_HAL_BUFFER_USAGE_DISPATCH_STORAGE_READ or
    IREE_HAL_BUFFER_USAGE_DISPATCH_STORAGE_WRITE.
    
    (fixes #11463; authored by @sogartar)
    
    Co-authored-by: Boian Petkantchin <boian@nod-labs.com>

--- runtime/src/iree/hal/drivers/cuda/cuda_allocator.c
+++ runtime/src/iree/hal/drivers/cuda/cuda_allocator.c
@@ -139,15 +139,14 @@ iree_hal_cuda_allocator_query_compatibility(
   iree_hal_buffer_compatibility_t compatibility =
       IREE_HAL_BUFFER_COMPATIBILITY_ALLOCATABLE;
 
-  // CUDA supports host <-> device for all copies.
-  if (iree_all_bits_set(params->usage, IREE_HAL_BUFFER_USAGE_TRANSFER)) {
+  if (iree_any_bit_set(params->usage, IREE_HAL_BUFFER_USAGE_TRANSFER)) {
     compatibility |= IREE_HAL_BUFFER_COMPATIBILITY_QUEUE_TRANSFER;
   }
 
   // Buffers can only be used on the queue if they are device visible.
   if (iree_all_bits_set(params->type, IREE_HAL_MEMORY_TYPE_DEVICE_VISIBLE)) {
-    if (iree_all_bits_set(params->usage,
-                          IREE_HAL_BUFFER_USAGE_DISPATCH_STORAGE)) {
+    if (iree_any_bit_set(params->usage,
+                         IREE_HAL_BUFFER_USAGE_DISPATCH_STORAGE)) {
       compatibility |= IREE_HAL_BUFFER_COMPATIBILITY_QUEUE_DISPATCH;
     }
   }
