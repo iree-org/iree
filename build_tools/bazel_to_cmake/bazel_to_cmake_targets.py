607f32a8e0bf4fa19330cf3b28295ef6dba474d3
Stella Laurenzo <stellaraccident@gmail.com>
2022-12-27 13:10:53 -0800

    Fix bazel2cmake

--- build_tools/bazel_to_cmake/bazel_to_cmake_targets.py
+++ build_tools/bazel_to_cmake/bazel_to_cmake_targets.py
@@ -111,6 +111,9 @@ EXPLICIT_TARGET_MAPPING = {
         "TorchMLIRTMTensorDialect"
     ],
 
+    # Tracy.
+    "@tracy_client//:runtime_impl": ["tracy_client::runtime_impl"],
+
     # Vulkan
     "@vulkan_headers": ["Vulkan::Headers"],
     # Misc single targets
