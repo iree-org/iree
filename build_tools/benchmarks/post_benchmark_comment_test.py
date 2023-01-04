8cccdb40e6184ff886d2dfd49c195fc248ab5a62
Jerry Wu <cheyuw@google.com>
2023-01-04 06:54:59 +0000

    Revert back the mock changes

--- build_tools/benchmarks/post_benchmark_comment_test.py
+++ build_tools/benchmarks/post_benchmark_comment_test.py
@@ -5,38 +5,30 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-from typing import Any
-from unittest import mock
 import http.client
 import requests
 import unittest
+from unittest import mock
+from typing import Any
 
 import post_benchmark_comment
 
 
 class GithubClientTest(unittest.TestCase):
 
-  def setUp(self):
-    self._mock_response = mock.create_autospec(requests.Response)
-    self._mock_requester = mock.create_autospec(
-        post_benchmark_comment.APIRequester)
-    self._mock_requester.get.return_value = self._mock_response
-    self._mock_requester.post.return_value = self._mock_response
-    self._mock_requester.patch.return_value = self._mock_response
-
   def test_post_to_gist(self):
     gist_url = "https://example.com/123455/1234.md"
-    self._mock_response.status_code = http.client.CREATED
-    self._mock_response.json.return_value = {
-        "html_url": gist_url,
-        "truncated": False
-    }
-    client = post_benchmark_comment.GithubClient(self._mock_requester)
+    response = mock.create_autospec(requests.Response)
+    response.status_code = http.client.CREATED
+    response.json.return_value = {"html_url": gist_url, "truncated": False}
+    requester = mock.create_autospec(post_benchmark_comment.APIRequester)
+    requester.post.return_value = response
+    client = post_benchmark_comment.GithubClient(requester)
 
     url = client.post_to_gist(filename="1234.md", content="xyz")
 
     self.assertEqual(url, gist_url)
-    self._mock_requester.post.assert_called_once_with(
+    requester.post.assert_called_once_with(
         endpoint=post_benchmark_comment.GITHUB_GIST_API,
         payload={
             "public": True,
@@ -49,12 +41,12 @@ class GithubClientTest(unittest.TestCase):
 
   def test_post_to_gist_truncated(self):
     gist_url = "example.com/123455/1234.md"
-    self._mock_response.status_code = http.client.CREATED
-    self._mock_response.json.return_value = {
-        "html_url": gist_url,
-        "truncated": True
-    }
-    client = post_benchmark_comment.GithubClient(self._mock_requester)
+    response = mock.create_autospec(requests.Response)
+    response.status_code = http.client.CREATED
+    response.json.return_value = {"html_url": gist_url, "truncated": True}
+    requester = mock.create_autospec(post_benchmark_comment.APIRequester)
+    requester.post.return_value = response
+    client = post_benchmark_comment.GithubClient(requester)
 
     with self.assertRaises(RuntimeError) as _:
       client.post_to_gist(filename="1234.md", content="xyz")
@@ -94,7 +86,7 @@ class GithubClientTest(unittest.TestCase):
 
     requester = mock.create_autospec(post_benchmark_comment.APIRequester)
     requester.get.side_effect = _handle_get
-    client = post_benchmark_comment.GithubClient(requester=requester)
+    client = post_benchmark_comment.GithubClient(requester)
 
     comment_id = client.get_previous_comment_on_pr(pr_number=23,
                                                    gist_bot_user="bot",
@@ -118,16 +110,19 @@ class GithubClientTest(unittest.TestCase):
                                   })
 
   def test_get_previous_comment_on_pr_not_found(self):
-    self._mock_response.status_code = http.client.OK
-    self._mock_response.json.return_value = []
-    client = post_benchmark_comment.GithubClient(self._mock_requester)
+    response = mock.create_autospec(requests.Response)
+    response.status_code = http.client.OK
+    response.json.return_value = []
+    requester = mock.create_autospec(post_benchmark_comment.APIRequester)
+    requester.get.return_value = response
+    client = post_benchmark_comment.GithubClient(requester)
 
     comment_id = client.get_previous_comment_on_pr(pr_number=23,
                                                    gist_bot_user="bot",
                                                    comment_type_id="1234")
 
     self.assertIsNone(comment_id)
-    self._mock_requester.get.assert_any_call(
+    requester.get.assert_any_call(
         endpoint=
         f"{post_benchmark_comment.GITHUB_IREE_API_PREFIX}/issues/23/comments",
         payload={
@@ -136,7 +131,7 @@ class GithubClientTest(unittest.TestCase):
             "sort": "updated",
             "direction": "desc"
         })
-    self._mock_requester.get.assert_any_call(
+    requester.get.assert_any_call(
         endpoint=
         f"{post_benchmark_comment.GITHUB_IREE_API_PREFIX}/issues/23/comments",
         payload={
@@ -147,23 +142,29 @@ class GithubClientTest(unittest.TestCase):
         })
 
   def test_update_comment_on_pr(self):
-    self._mock_response.status_code = http.client.OK
-    client = post_benchmark_comment.GithubClient(self._mock_requester)
+    response = mock.create_autospec(requests.Response)
+    response.status_code = http.client.OK
+    requester = mock.create_autospec(post_benchmark_comment.APIRequester)
+    requester.patch.return_value = response
+    client = post_benchmark_comment.GithubClient(requester)
 
     client.update_comment_on_pr(comment_id=123, content="xyz")
 
-    self._mock_requester.patch.assert_called_once_with(
+    requester.patch.assert_called_once_with(
         endpoint=
         f"{post_benchmark_comment.GITHUB_IREE_API_PREFIX}/issues/comments/123",
         payload={"body": "xyz"})
 
   def test_create_comment_on_pr(self):
-    self._mock_response.status_code = http.client.CREATED
-    client = post_benchmark_comment.GithubClient(self._mock_requester)
+    response = mock.create_autospec(requests.Response)
+    response.status_code = http.client.CREATED
+    requester = mock.create_autospec(post_benchmark_comment.APIRequester)
+    requester.post.return_value = response
+    client = post_benchmark_comment.GithubClient(requester)
 
     client.create_comment_on_pr(pr_number=1234, content="xyz")
 
-    self._mock_requester.post.assert_called_once_with(
+    requester.post.assert_called_once_with(
         endpoint=
         f"{post_benchmark_comment.GITHUB_IREE_API_PREFIX}/issues/1234/comments",
         payload={"body": "xyz"})
