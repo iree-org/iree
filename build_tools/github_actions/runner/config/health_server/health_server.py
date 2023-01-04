8bd2cbd5263714d90d37ab747f8c453caac02944
Jerry Wu <cheyuw@google.com>
2023-01-04 19:30:33 +0000

    Revert "Cloud Run Function for instance self-deletion (#11610)"
    This reverts commit d985a1b8faf7242a61dd1e289bbddd8e3780c3d0.

--- build_tools/github_actions/runner/config/health_server/health_server.py
+++ build_tools/github_actions/runner/config/health_server/health_server.py
@@ -21,7 +21,7 @@ import argparse
 import http.server
 
 
-class HealthCheckHandler(http.server.BaseHTTPRequestHandler):
+class HealthCheckServer(http.server.BaseHTTPRequestHandler):
 
   def do_GET(self):
     self.send_response(200)
@@ -30,7 +30,7 @@ class HealthCheckHandler(http.server.BaseHTTPRequestHandler):
 
 
 def main(args):
-  webServer = http.server.HTTPServer(("", args.port), HealthCheckHandler)
+  webServer = http.server.HTTPServer(("", args.port), HealthCheckServer)
   print(f"Server started on port {args.port}. Ctrl+C to stop.")
 
   try:
