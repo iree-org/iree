416bdd54dc48acb5f4ce1235b28d73ad086b349c
Jerry Wu <cheyuw@google.com>
2023-01-04 19:30:32 +0000

    Revert "Make health check look at runner service status (#11673)"
    This reverts commit 56e40f6505bfd5cf9d9b42377e7960fade6a955d.

--- build_tools/github_actions/runner/config/health_server/health_server.py
+++ build_tools/github_actions/runner/config/health_server/health_server.py
@@ -19,40 +19,17 @@ all but a few IPs and even those are internal to the network.
 
 import argparse
 import http.server
-import subprocess
-from http.client import INTERNAL_SERVER_ERROR, NOT_FOUND, OK
-
-RUNNER_SERVICE_NAME = "gh-runner"
-CHECK_SERVICE_CMD = ["systemctl", "is-active", RUNNER_SERVICE_NAME]
-CHECK_SERVICE_TIMEOUT = 10
 
 
 class HealthCheckHandler(http.server.BaseHTTPRequestHandler):
 
-  def send_success(self):
-    self.send_response(OK)
+  def do_GET(self):
+    self.send_response(200)
     self.send_header("Content-type", "text/html")
     self.end_headers()
 
-  def do_GET(self):
-    try:
-      subprocess.run(CHECK_SERVICE_CMD,
-                     check=True,
-                     text=True,
-                     stdout=subprocess.PIPE,
-                     timeout=CHECK_SERVICE_TIMEOUT)
-    except subprocess.TimeoutExpired as e:
-      msg = f"'{' '.join(e.cmd)}' timed out: {e.stdout}"
-      return self.send_error(INTERNAL_SERVER_ERROR, msg)
-    except subprocess.CalledProcessError as e:
-      return self.send_error(
-          NOT_FOUND, f"Runner service not found: '{' '.join(e.cmd)}' returned"
-          f" '{e.stdout.strip()}' (exit code {e.returncode})")
 
-    return self.send_success()
-
-
-def main(args: argparse.Namespace):
+def main(args):
   webServer = http.server.HTTPServer(("", args.port), HealthCheckHandler)
   print(f"Server started on port {args.port}. Ctrl+C to stop.")
 
@@ -62,6 +39,7 @@ def main(args: argparse.Namespace):
     # Don't print an exception on interrupt. Add a newline to handle printing of
     # "^C"
     print()
+    pass
 
   webServer.server_close()
   print("Server stopped.")
