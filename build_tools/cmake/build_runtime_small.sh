348f0edce7f2b4108ea080cda0655d6d36c956d3
Jerry Wu <cheyuw@google.com>
2023-01-11 18:36:47 +0000

    Revert "Specify Python3 executable path in CI (#11696)"
    This reverts commit 56d65bfc33ef22abb7bb617d573db5b085232bda.

--- build_tools/cmake/build_runtime_small.sh
+++ build_tools/cmake/build_runtime_small.sh
@@ -18,8 +18,6 @@ source build_tools/cmake/setup_ccache.sh
 
 "${CMAKE_BIN?}" -B "${BUILD_DIR}" \
   -G Ninja . \
-  -DPython3_EXECUTABLE="${IREE_PYTHON3_EXECUTABLE}" \
-  -DPYTHON_EXECUTABLE="${IREE_PYTHON3_EXECUTABLE}" \
   -DCMAKE_BUILD_TYPE=MinSizeRel \
   -DIREE_SIZE_OPTIMIZED=ON \
   -DIREE_BUILD_COMPILER=OFF
