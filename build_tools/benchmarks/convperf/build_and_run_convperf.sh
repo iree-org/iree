5a0ced2c2e0aef9fc5e89185ee962145b867a0a6
Jerry Wu <cheyuw@google.com>
2023-01-11 18:36:47 +0000

    Revert "Use Clang as the compiler for ConvPerf (#11691)"
    This reverts commit 07c3c7224b76f800945ebbe5558b0af7ab0fce0e.

--- build_tools/benchmarks/convperf/build_and_run_convperf.sh
+++ build_tools/benchmarks/convperf/build_and_run_convperf.sh
@@ -47,10 +47,7 @@ popd # external/iree
 popd # ${REPO_DIR}
 
 # Build ConvPerf.
-cmake -GNinja \
-  -DCMAKE_C_COMPILER="${CC:-clang}" \
-  -DCMAKE_CXX_COMPILER="${CXX:-clang++}" \
-  -B "${BUILD_DIR}" "${REPO_DIR}"
+cmake -GNinja -B "${BUILD_DIR}" "${REPO_DIR}"
 cmake --build "${BUILD_DIR}"
 
 # Run ConvPerf for several threading configurations.
