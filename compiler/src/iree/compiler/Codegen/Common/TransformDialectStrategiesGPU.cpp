44b39ff7a84d7a27b4fea6b070c27fed221e6d83
Jerry Wu <cheyuw@google.com>
2023-01-11 18:36:47 +0000

    Revert "Fix compilation issue on macOS due to mismatched std::min types (#11717)"
    This reverts commit a13b60a3ad2257a901068e08ba95014d3caca06f.

--- compiler/src/iree/compiler/Codegen/Common/TransformDialectStrategiesGPU.cpp
+++ compiler/src/iree/compiler/Codegen/Common/TransformDialectStrategiesGPU.cpp
@@ -277,9 +277,9 @@ void ReductionStrategyStagedThreadDistribution::compute(
   // TODO: Split to ensure 4 on most of the problem and use a 1-epilogue.
   int64_t reductionDimensionSize = captures.reductionOpSizes.back();
   if (reductionDimensionSize > 0 && reductionDimensionSize % 4 == 0)
-    vectorSize = std::min(maxVectorSize, int64_t(4));
+    vectorSize = std::min(maxVectorSize, 4l);
   else if (reductionDimensionSize > 0 && reductionDimensionSize % 2 == 0)
-    vectorSize = std::min(maxVectorSize, int64_t(2));
+    vectorSize = std::min(maxVectorSize, 2l);
   else
     vectorSize = 1;
   // Tile reduction to the maximal multiple `vectorSize` allowed.
