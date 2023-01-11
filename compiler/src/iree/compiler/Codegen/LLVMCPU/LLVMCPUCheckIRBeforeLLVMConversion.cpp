899f56cf44b10d4681625af2adbd002c931aa929
Jerry Wu <cheyuw@google.com>
2023-01-11 18:37:41 +0000

    Revert "Add a test that compiles softmax under aggressive fusion. (#11362)"
    This reverts commit 634ca1af3ecc282294eebcd00bc2cf18b70d283e.

--- compiler/src/iree/compiler/Codegen/LLVMCPU/LLVMCPUCheckIRBeforeLLVMConversion.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMCPU/LLVMCPUCheckIRBeforeLLVMConversion.cpp
@@ -17,8 +17,8 @@ static llvm::cl::opt<int> clMaxAllocationSizeInBytes(
     "iree-llvmcpu-stack-allocation-limit",
     llvm::cl::desc("maximum allowed stack allocation size in bytes"),
     llvm::cl::init(32768));
-static llvm::cl::opt<bool> clFailOnOutOfBoundsStackAllocation(
-    "iree-llvmcpu-fail-on-out-of-bounds-stack-allocation",
+static llvm::cl::opt<bool> clFailUnboundDynamicStackAllocation(
+    "iree-llvmcpu-fail-unbound-dynamic-stack-allocation",
     llvm::cl::desc("fail if the upper bound of dynamic stack allocation cannot "
                    "be solved"),
     llvm::cl::init(true));
@@ -45,7 +45,7 @@ void LLVMCPUCheckIRBeforeLLVMConversionPass::runOnOperation() {
       auto ub = linalg::getConstantUpperBoundForIndex(operand);
       if (succeeded(ub)) {
         size *= *ub;
-      } else if (clFailOnOutOfBoundsStackAllocation) {
+      } else if (clFailUnboundDynamicStackAllocation) {
         return allocaOp.emitOpError(
             "expected no stack allocations without upper bound shapes");
       }
@@ -62,8 +62,7 @@ void LLVMCPUCheckIRBeforeLLVMConversionPass::runOnOperation() {
     return signalPassFailure();
   }
   int maxAllocationSizeInBits = clMaxAllocationSizeInBytes * 8;
-  if (clFailOnOutOfBoundsStackAllocation &&
-      totalBits > maxAllocationSizeInBits) {
+  if (totalBits > maxAllocationSizeInBits) {
     moduleOp.emitOpError(
         "expected total size of stack allocation is not greater than ")
         << clMaxAllocationSizeInBytes.getValue() << " bytes, but got "
--- compiler/src/iree/compiler/Codegen/LLVMCPU/test/check_ir_before_llvm_conversion_not_fail_unbound.mlir
+++ compiler/src/iree/compiler/Codegen/LLVMCPU/test/check_ir_before_llvm_conversion_not_fail_unbound.mlir
@@ -1,4 +1,4 @@
-// RUN: iree-opt --iree-llvmcpu-check-ir-before-llvm-conversion --iree-llvmcpu-fail-on-out-of-bounds-stack-allocation=false %s --verify-diagnostics -split-input-file
+// RUN: iree-opt --iree-llvmcpu-check-ir-before-llvm-conversion --iree-llvmcpu-fail-unbound-dynamic-stack-allocation=false %s --verify-diagnostics -split-input-file
 
 module {
   func.func @dynamic_allocas(%arg0: index) {
@@ -6,11 +6,11 @@ module {
     return
   }
 }
-// CHECK-LABEL: func @dynamic_allocas(
 
 // -----
 
 #map = affine_map<(d0) -> (-d0, 16384)>
+// expected-error @+1 {{expected total size of stack allocation is not greater than 32768 bytes, but got 65536 bytes}}
 module {
   func.func @dynamic_big_allocas(%arg0: index, %arg1: index) {
     %0 = affine.min #map(%arg0)
@@ -18,14 +18,13 @@ module {
     return
   }
 }
-// CHECK-LABEL: func @dynamic_big_allocas(
 
 // -----
 
+// expected-error @+1 {{expected total size of stack allocation is not greater than 32768 bytes, but got 65536 bytes}}
 module {
   func.func @mix_static_and_unbound_dynamic_allocas(%arg0: index) {
     %0 = memref.alloca(%arg0) : memref<?x16384xf32>
     return
   }
 }
-// CHECK-LABEL: func @mix_static_and_unbound_dynamic_allocas(
