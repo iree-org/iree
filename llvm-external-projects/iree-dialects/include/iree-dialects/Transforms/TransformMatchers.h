33cb4e70514ffb04a6a5611924fca33d0aff0f9d
Jerry Wu <cheyuw@google.com>
2023-01-11 18:36:47 +0000

    Revert "Add a second CUDA transform reduction strategy for small reduction sizes (#11646)"
    This reverts commit 715b11d710c313f032ceb2a92bd28accb8cf5148.

--- llvm-external-projects/iree-dialects/include/iree-dialects/Transforms/TransformMatchers.h
+++ llvm-external-projects/iree-dialects/include/iree-dialects/Transforms/TransformMatchers.h
@@ -570,8 +570,6 @@ private:
 
 struct MatchedReductionCaptures {
   int64_t reductionRank = 0;
-  // TODO: capture all dims.
-  int64_t mostMinorParallelDimensionSize = 0;
   int64_t reductionDimensionSize = 0;
   int64_t maybeLeadingRank = 0;
   int64_t maybeTrailingRank = 0;
