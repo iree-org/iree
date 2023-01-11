e65b12eedd01f663018cd4619ef34384402dc55e
Jerry Wu <cheyuw@google.com>
2023-01-11 18:36:47 +0000

    Revert "Adds MIG template for x86_64 benchmarking in Github CI (#11645)"
    This reverts commit 99fc4f97e11f4ac9c28c98ff722085d649f11ecc.

--- build_tools/github_actions/runner/gcp/create_templates.sh
+++ build_tools/github_actions/runner/gcp/create_templates.sh
@@ -149,12 +149,6 @@ function create_template() {
       --maintenance-policy=MIGRATE
       --create-disk="auto-delete=yes,boot=yes,image=projects/iree-oss/global/images/${CPU_IMAGE},mode=rw,size=${CPU_DISK_SIZE_GB},type=pd-balanced"
     )
-  elif [[ "${type}" == cpu-c2-16 ]]; then
-    cmd+=(
-      --machine-type=c2-standard-16
-      --maintenance-policy=MIGRATE
-      --create-disk="auto-delete=yes,boot=yes,image=projects/iree-oss/global/images/${CPU_IMAGE},mode=rw,size=${CPU_DISK_SIZE_GB},type=pd-balanced"
-    )
   else
     echo "Got unrecognized type '${type}'" >2
     exit 1
@@ -169,7 +163,7 @@ function create_template() {
 }
 
 for group in presubmit postsubmit; do
-  for type in gpu cpu cpu-c2-16; do
+  for type in gpu cpu; do
     create_template "${group}" "${type}"
   done
 done
