d79c0a93b2037575ab1dc8b10fe4e86ce01d0eee
Jerry Wu <cheyuw@google.com>
2023-01-11 18:36:46 +0000

    Revert "[TransformDialectStrategies] Carve out a place for current and future… (#11738)"
    This reverts commit fc6377ceb3fa9f88fae8c466a5e865c662a33b48.

--- compiler/src/iree/compiler/Codegen/Common/TransformDialectStrategies.h
+++ compiler/src/iree/compiler/Codegen/Common/TransformDialectStrategies.h
@@ -4,8 +4,7 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#ifndef IREE_COMPILER_CODEGEN_TRANSFORM_DIALECT_STRATEGIES_COMMON_COMMON_H_
-#define IREE_COMPILER_CODEGEN_TRANSFORM_DIALECT_STRATEGIES_COMMON_COMMON_H_
+#ifndef IREE_COMPILER_CODEGEN_COMMON_TRANSFORMDIALECT_STRATEGIES_H_
 
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
@@ -14,15 +13,10 @@
 namespace mlir {
 namespace iree_compiler {
 
-class AbstractReductionStrategy;
-
-//===----------------------------------------------------------------------===//
-// General helpers.
-//===----------------------------------------------------------------------===//
-
-/// Return the greatest value smaller or equal to `val` that is a multiple
-/// of `multiple`. Asserts that all quantities are nonnegative. I.e. returns
-/// `(val / multiple) * multiple` a.k.a `floordiv(val, multiple) * multiple`.
+/// Return the greatest value smaller or equal to `val` that is a multiple of
+/// `multiple`. Asserts that all quantities are nonnegative.
+/// I.e. returns `(val / multiple) * multiple`
+///        a.k.a `floordiv(val, multiple) * multiple`.
 int64_t previousMultipleOf(int64_t val, int64_t multiple);
 
 /// Return the smallest value greater or equal to `val` that is a multiple of
@@ -38,15 +32,6 @@ int64_t nextMultipleOf(int64_t val, int64_t multiple);
 // TODO: approximate with a faster implementation based on a few desirable
 // primes.
 FailureOr<int64_t> maxDivisorOfValueBelowLimit(int64_t value, int64_t limit);
-
-using StrategyBuilderFn = std::function<void(ImplicitLocOpBuilder &, Value)>;
-
-/// Use `buildStrategy` to build a ModuleOp containing transform dialect IR,
-/// right after func::FuncOp `entryPoint`.
-/// This embed the transform into the IR and allows applying it either in debug
-/// mode or within the IREE pipeline.
-void createTransformRegion(func::FuncOp entryPoint,
-                           StrategyBuilderFn buildStrategy);
 
 //===----------------------------------------------------------------------===//
 // Low-level reusable builder APIs, these should follow MLIR-style builders.
@@ -124,7 +109,6 @@ buildTileFuseDistToForeachThreadWithTileSizes(ImplicitLocOpBuilder &b,
                                               ValueRange opsHToFuse,
                                               ArrayRef<OpFoldResult> tileSizes,
                                               ArrayAttr threadDimMapping);
-
 TileToForeachThreadAndFuseAndDistributeResult
 buildTileFuseDistToForeachThreadAndWorkgroupCountWithTileSizes(
     ImplicitLocOpBuilder &b, Value rootH, ValueRange opsHToFuse,
@@ -135,7 +119,6 @@ TileToForeachThreadAndFuseAndDistributeResult
 buildTileFuseDistToForeachThreadWithNumThreads(
     ImplicitLocOpBuilder &b, Value rootH, ValueRange opsHToFuse,
     ArrayRef<OpFoldResult> numThreads, ArrayAttr threadDimMapping);
-
 TileToForeachThreadAndFuseAndDistributeResult
 buildTileFuseDistToForeachThreadAndWorgroupCountWithNumThreads(
     ImplicitLocOpBuilder &b, Value rootH, ValueRange opsHToFuse,
@@ -146,31 +129,23 @@ buildTileFuseDistToForeachThreadAndWorgroupCountWithNumThreads(
 /// func.func.
 Value buildVectorize(ImplicitLocOpBuilder &b, Value funcH);
 
-/// Bufferize and drop HAL descriptor from memref ops.
+/// Bufferize and drop HAL decriptor from memref ops.
 /// Takes a handle variantOp and returns a handle to the same variant op.
 Value buildBufferize(ImplicitLocOpBuilder &b, Value variantH,
                      bool targetGpu = false);
 
-/// Uses TileFuseDistToForeachThreadAndWorkgroupCountWithTileSizes to create a
-/// top-level `scf.foreach_thread` tiled by `strategy.workgroupTileSizes`. All
-/// of `maybeLeadingH`, `fillH`, `reductionH` and `maybeTrailingH` are fused
-/// into the top-level `scf.foreach_thread`.
-/// Handles are returned to the fused versions of `maybeLeadingH`, `fillH`,
-/// `reductionH` and `maybeTrailingH` that are all tiled and distributed
-/// accordingly.
-/// The mapping of the `scf.foreach_thread` dimensions is to the first
-/// dimensions of `strategy.allBlockAttrs`.
-std::tuple<Value, Value, Value, Value> buildReductionStrategyBlockDistribution(
-    ImplicitLocOpBuilder &b, Value maybeLeadingH, Value fillH, Value reductionH,
-    Value maybeTrailingH, const AbstractReductionStrategy &strategy);
+using StrategyBuilderFn = std::function<void(ImplicitLocOpBuilder &, Value)>;
+
+void createTransformRegion(func::FuncOp entryPoint,
+                           StrategyBuilderFn buildStrategy);
 
 //===----------------------------------------------------------------------===//
 // Higher-level problem-specific strategy creation APIs, these should favor
 // user-friendliness.
 //===----------------------------------------------------------------------===//
-/// Distribute the reduction referred to by `reductionH` to blocks using the
-/// current IREE lowering config.
-Value buildReductionStrategyBlockDistributionPart(
+/// Distribute to blocks using the current IREE lowering config.
+// TODO: consider passing a problem-specific struct to control information.
+Value createReductionStrategyBlockDistributionPart(
     ImplicitLocOpBuilder &b, Value variantH, Value originalFillH,
     Value reductionH, Value optionalFusionRootH,
     ArrayRef<OpFoldResult> tileSizes0Generic, bool hasLeadingEltwise = false,
@@ -179,4 +154,4 @@ Value buildReductionStrategyBlockDistributionPart(
 }  // namespace iree_compiler
 }  // namespace mlir
 
-#endif  // IREE_COMPILER_CODEGEN_TRANSFORM_DIALECT_STRATEGIES_COMMON_COMMON_H_
+#endif  // IREE_COMPILER_CODEGEN_COMMON_TRANSFORMDIALECT_STRATEGIES_H_
