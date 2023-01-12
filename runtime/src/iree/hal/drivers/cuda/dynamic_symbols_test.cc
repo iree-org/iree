7f07a22933c2f3b651f5a08c8f9cf4b46a531a3a
Okwan Kwon <okkwon@gmail.com>
2023-01-12 10:47:27 -0800

    move the nccl dynamic symbol test

--- runtime/src/iree/hal/drivers/cuda/dynamic_symbols_test.cc
+++ runtime/src/iree/hal/drivers/cuda/dynamic_symbols_test.cc
@@ -10,6 +10,7 @@
 
 #include "iree/base/api.h"
 #include "iree/testing/gtest.h"
+#include "third_party/nccl/nccl.h"
 
 namespace iree {
 namespace hal {
@@ -41,6 +42,29 @@ TEST(DynamicSymbolsTest, CreateFromSystemLoader) {
     CUDA_CHECK_ERRORS(symbols.cuDeviceGet(&device, /*ordinal=*/0));
   }
 
+  iree_hal_cuda_dynamic_symbols_deinitialize(&symbols);
+}
+
+#define NCCL_CHECK_ERRORS(expr)     \
+  {                                 \
+    ncclResult_t status = expr;     \
+    ASSERT_EQ(ncclSuccess, status); \
+  }
+
+TEST(NCCLDynamicSymbolsTest, CreateFromSystemLoader) {
+  iree_hal_cuda_dynamic_symbols_t symbols;
+  iree_status_t status = iree_hal_cuda_dynamic_symbols_initialize(
+      iree_allocator_system(), &symbols);
+  if (!iree_status_is_ok(status)) {
+    iree_status_fprint(stderr, status);
+    iree_status_ignore(status);
+    std::cerr << "Symbols cannot be loaded, skipping test.";
+    GTEST_SKIP();
+  }
+
+  int nccl_version = 0;
+  NCCL_CHECK_ERRORS(symbols.ncclGetVersion(&nccl_version));
+  ASSERT_EQ(NCCL_VERSION_CODE, nccl_version);
   iree_hal_cuda_dynamic_symbols_deinitialize(&symbols);
 }
 
