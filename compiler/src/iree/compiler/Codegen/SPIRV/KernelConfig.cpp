b9b2720f2cbc2873b68fee6d12f68dea157c2891
Mahesh Ravishankar <ravishankarm@google.com>
2023-01-18 11:57:36 -0800

    Remove use `llvm::makeMutableArrayRef`.

--- compiler/src/iree/compiler/Codegen/SPIRV/KernelConfig.cpp
+++ compiler/src/iree/compiler/Codegen/SPIRV/KernelConfig.cpp
@@ -222,8 +222,8 @@ LogicalResult setConvOpConfig(linalg::LinalgOp linalgOp,
     // and width.
     const bool tileToSquare = tileConvSquare(
         oh, ow, residualThreads, residualTilingFactor,
-        llvm::makeMutableArrayRef(workgroupSize).drop_front(),
-        llvm::makeMutableArrayRef(workgroupTileSizes).drop_front().drop_back());
+        llvm::MutableArrayRef(workgroupSize).drop_front(),
+        llvm::MutableArrayRef(workgroupTileSizes).drop_front().drop_back());
 
     // Otherwise treat OW and OH separately to allow them to have different
     // number of threads and tiling size.
