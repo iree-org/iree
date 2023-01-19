117bc65bfc4f831fa640d7f9ab952713d6f37553
Okwan Kwon <okkwon@gmail.com>
2023-01-18 16:18:40 -0800

    fix the wrong starting value for reduction op enum

--- runtime/src/iree/hal/command_buffer.h
+++ runtime/src/iree/hal/command_buffer.h
@@ -297,7 +297,7 @@ typedef uint8_t iree_hal_collective_kind_t;
 // Specifies the reduction operator of a collective reduction operation.
 enum iree_hal_collective_reduction_e {
   // Specifies that the reduction operation computes a sum (addition).
-  IREE_HAL_COLLECTIVE_REDUCTION_SUM = 0,
+  IREE_HAL_COLLECTIVE_REDUCTION_SUM = 1,
   // Specifies that the reduction operation computes a product (multiplication).
   IREE_HAL_COLLECTIVE_REDUCTION_PRODUCT,
   // Specifies that the reduction operation computes a minimum (min).
