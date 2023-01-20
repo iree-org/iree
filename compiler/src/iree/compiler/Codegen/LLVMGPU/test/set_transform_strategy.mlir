iree-org/iree

commit d782f54f56def442105656cace5643666acd9b89

Refactor strategy configuration and add a primitive hook for known good reduction configurations. (#11774)
Improve doc for matchAndSetReductionStrategy.
Refactor strategy configuration and introduce a hardware model shim.
Add a primitive hook for known good reduction configurations.

compiler/src/iree/compiler/Codegen/TransformDialectStrategies/GPU/Common.cpp
@@ -14,6 +14,7 @@
 #include "iree/compiler/Codegen/TransformDialectStrategies/GPU/SmallReductionStrategy.h"
 #include "iree/compiler/Codegen/TransformDialectStrategies/GPU/StagedReductionStrategy.h"
 #include "llvm/Support/Debug.h"
+#include "llvm/Support/ErrorHandling.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/Linalg/TransformOps/LinalgTransformOps.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
@@ -50,14 +51,16 @@ using iree_compiler::maxDivisorOfValueBelowLimit;
 using iree_compiler::gpu::AbstractReductionStrategy;
 using iree_compiler::gpu::build1DSplittingStrategyWithOptionalThreadMapping;
 using iree_compiler::gpu::buildCommonTrailingStrategy;
-using iree_compiler::gpu::buildGpuSmallReductionStrategy;
 using iree_compiler::gpu::buildMapToBlockAndThreads;
+using iree_compiler::gpu::buildSmallReductionStrategy;
 using iree_compiler::gpu::buildStagedReductionStrategy;
-using iree_compiler::gpu::getSmallReductionConfig;
+using iree_compiler::gpu::GPUModel;
 using iree_compiler::gpu::kCudaMaxNumThreads;
 using iree_compiler::gpu::kCudaMaxVectorLoadBitWidth;
 using iree_compiler::gpu::kCudaWarpSize;
 using iree_compiler::gpu::ReductionConfig;
+using iree_compiler::gpu::ReductionStrategy;
+using iree_compiler::gpu::scaleUpByBitWidth;
 using iree_compiler::gpu::SmallReductionStrategy;
 using iree_compiler::gpu::StagedReductionStrategy;
 
@@ -243,11 +246,104 @@ std::pair<Value, Value> mlir::iree_compiler::gpu::buildCommonTrailingStrategy(
 // Higher-level problem-specific strategy creation APIs, these should favor
 // user-friendliness.
 //===----------------------------------------------------------------------===//
-/// Return success if the IR matches what the GPU reduction strategy can
-/// handle. If it is success it will append the transform dialect after the
-/// entry point module.
+
+/// Placeholder to encode fixed reductions that should take finer-grained
+/// precedence over other heuristics. In the future, this could be lifted to
+/// e.g. `gpuModel` or higher up in some transform dialect database summary of
+/// "known good things".
+static FailureOr<ReductionConfig> applyKnownGoodReductionConfigurations(
+    const transform_ext::MatchedReductionCaptures &captures,
+    const GPUModel &gpuModel) {
+  auto staged = ReductionStrategy::Staged;
+  int64_t reductionSize = captures.reductionOpSizes.back();
+  if (gpuModel.model == GPUModel::kDefaultGPU) {
+    if (captures.reductionOutputElementalTypeBitWidth == 32) {
+      if (reductionSize == 64) return ReductionConfig{64, 1, staged};
+      if (reductionSize == 128) return ReductionConfig{32, 4, staged};
+      if (reductionSize == 512) return ReductionConfig{256, 2, staged};
+    }
+  }
+  return failure();
+}
+
+/// The configurations below have been determined empirically by performing a
+/// manual tradeoff between problem size, amount of parallelism and vector
+/// size on a particular NVIDIA RTX2080Ti 12GB card. This is a coarse tradeoff
+/// that should generally give reasonably good results but that begs to be
+/// complemented by hardcoded known good configurations and ultimately a
+/// database and/or a random forest compression of configurations with
+/// guaranteed performance.
+// TODO: Lift some of the strategy sizing logic as hints and/or heuristics to
+// also work properly in the dynamic case.
+// TODO: Support more HW configs and make it more pluggable.
+static ReductionConfig getReductionConfig(
+    const transform_ext::MatchedReductionCaptures &captures,
+    const GPUModel &gpuModel) {
+  auto maybeHardcodedConfiguration =
+      applyKnownGoodReductionConfigurations(captures, gpuModel);
+  if (succeeded(maybeHardcodedConfiguration))
+    return *maybeHardcodedConfiguration;
+
+  //===--------------------------------------------------------------------===//
+  // Small reduction strategy.
+  //===--------------------------------------------------------------------===//
+  // Dynamic reductions are never supported by default because we can
+  // never know offhand whether we are in a small-reduction regime mode.
+  // Since this mode does not coalesce reads, perf will suffer
+  // catastrophically on larger runtime reduction.
+  // TODO: explicit hint from above that we really want to do that.
+  int64_t reductionDimensionSize = captures.reductionOpSizes.back();
+  bool isDynamicReduction = ShapedType::isDynamic(reductionDimensionSize);
+  // Otherwise, still only support the small cases for now and fall back to
+  // other strategies otherwise.
+  bool isSmallReduction = (reductionDimensionSize < 2 * kCudaWarpSize);
+  if (!isDynamicReduction && isSmallReduction) {
+    int64_t maxNumThreads = 4 * kCudaWarpSize;
+    return ReductionConfig{maxNumThreads, 0, ReductionStrategy::Small};
+  }
+  //===--------------------------------------------------------------------===//
+  // Staged reduction strategy.
+  //===--------------------------------------------------------------------===//
+  int64_t bitWidth = captures.reductionOutputElementalTypeBitWidth;
+  int64_t vectorSize = scaleUpByBitWidth(4, bitWidth);
+  int64_t maxNumThreads = 8 * kCudaWarpSize;
+  // No adjustments in the dynamic case, we need extra information to make a
+  // good decision.
+  int64_t redSize = captures.reductionOpSizes.back();
+  if (ShapedType::isDynamic(redSize))
+    return ReductionConfig{maxNumThreads, vectorSize};
+  // Scale down to smaller sizes (4, 8, 16)-warps.
+  if (scaleUpByBitWidth(redSize, bitWidth) <= 4 * kCudaWarpSize) {
+    vectorSize = scaleUpByBitWidth(1, bitWidth);
+    maxNumThreads = 4 * kCudaWarpSize;
+  } else if (scaleUpByBitWidth(redSize, bitWidth) <= 8 * kCudaWarpSize) {
+    vectorSize = scaleUpByBitWidth(2, bitWidth);
+    maxNumThreads = 4 * kCudaWarpSize;
+  } else if (scaleUpByBitWidth(redSize, bitWidth) <= 8 * 2 * kCudaWarpSize) {
+    vectorSize = scaleUpByBitWidth(4, bitWidth);
+    maxNumThreads = 4 * kCudaWarpSize;
+  }
+  // Scale up to larger sizes (32, 64, 128+)-warps, using vector-4.
+  if (!captures.trailingOpSizes.empty()) {
+    if (scaleUpByBitWidth(redSize, bitWidth) >= 128 * 4 * kCudaWarpSize) {
+      vectorSize = scaleUpByBitWidth(4, bitWidth);
+      maxNumThreads = 32 * kCudaWarpSize;
+    } else if (scaleUpByBitWidth(redSize, bitWidth) >= 64 * 4 * kCudaWarpSize) {
+      vectorSize = scaleUpByBitWidth(4, bitWidth);
+      maxNumThreads = 16 * kCudaWarpSize;
+    } else if (scaleUpByBitWidth(redSize, bitWidth) >= 32 * 4 * kCudaWarpSize) {
+      vectorSize = scaleUpByBitWidth(4, bitWidth);
+      maxNumThreads = 8 * kCudaWarpSize;
+    } else if (scaleUpByBitWidth(redSize, bitWidth) >= 16 * 4 * kCudaWarpSize) {
+      vectorSize = scaleUpByBitWidth(4, bitWidth);
+      maxNumThreads = 4 * kCudaWarpSize;
+    }
+  }
+  return ReductionConfig{maxNumThreads, vectorSize, ReductionStrategy::Staged};
+}
+
 LogicalResult mlir::iree_compiler::gpu::matchAndSetReductionStrategy(
-    func::FuncOp entryPoint, linalg::LinalgOp op) {
+    func::FuncOp entryPoint, linalg::LinalgOp op, const GPUModel &gpuModel) {
   // 1. Match a reduction and surrounding ops.
   StructuredOpMatcher reduction, fill, leading, trailing;
   transform_ext::MatchedReductionCaptures captures;
@@ -257,14 +353,19 @@ LogicalResult mlir::iree_compiler::gpu::matchAndSetReductionStrategy(
   // 2. Construct the configuration and the strategy builder.
   // TODO: Generalize along the HW axis.
   auto strategyBuilder = [&](ImplicitLocOpBuilder &b, Value variant) {
-    // Take the small strategy if it is deemed profitable.
-    auto maybeSmallStrategy =
-        SmallReductionStrategy::create(op->getContext(), captures);
-    if (succeeded(maybeSmallStrategy))
-      return buildGpuSmallReductionStrategy(b, variant, *maybeSmallStrategy);
-    // Otherwise, always fallback to the staged strategy.
-    auto strategy = StagedReductionStrategy::create(op->getContext(), captures);
-    return buildStagedReductionStrategy(b, variant, strategy);
+    ReductionConfig reductionConfig = getReductionConfig(captures, gpuModel);
+    if (reductionConfig.strategy == ReductionStrategy::Small) {
+      auto strategy = SmallReductionStrategy::create(op->getContext(), captures,
+                                                     reductionConfig);
+      return buildSmallReductionStrategy(b, variant, strategy);
+    } else if (reductionConfig.strategy == ReductionStrategy::Staged) {
+      // Otherwise, always fallback to the staged strategy.
+      auto strategy = StagedReductionStrategy::create(
+          op->getContext(), captures, reductionConfig);
+      return buildStagedReductionStrategy(b, variant, strategy);
+    } else {
+      return llvm_unreachable("Unknown strategy");
+    }
   };
 
   // 3. Build strategy embedded into the IR.

compiler/src/iree/compiler/Codegen/LLVMGPU/test/set_transform_strategy.mlir
@@ -90,8 +90,8 @@ hal.executable.variant public @cuda_nvptx_fb, target = <"cuda", "cuda-nvptx-fb",
 
 //   CHECK-LABEL: func.func @group_reduction_128
 //         CHECK:   transform.structured.canonicalized_sequence failures(propagate)
-//         CHECK:   transform.structured.tile_reduction_using_foreach_thread %{{.*}} by num_threads = [0, 128], tile_sizes = [0, 1], mapping = [#gpu.thread<x>]
-//         CHECK:   transform.iree.map_nested_foreach_thread_to_gpu_threads %{{.*}} {workgroup_size = [128, 1, 1]}
+//         CHECK:   transform.structured.tile_reduction_using_foreach_thread %{{.*}} by num_threads = [0, 32], tile_sizes = [0, 4], mapping = [#gpu.thread<x>]
+//         CHECK:   transform.iree.map_nested_foreach_thread_to_gpu_threads %{{.*}} {workgroup_size = [32, 1, 1]}
 //         CHECK:   transform.iree.vector.to_warp_execute_on_lane_0 %{{.*}} {warp_size = 32 : i64}
 
 // -----

compiler/src/iree/compiler/Codegen/TransformDialectStrategies/CPU/Common.cpp
@@ -29,6 +29,8 @@ using namespace mlir;
 #define DBGS() (llvm::dbgs() << "[" DEBUG_TYPE "]: ")
 
 // TODO: significantly better namespacing.
+using iree_compiler::cpu::CPUModel;
+using iree_compiler::cpu::ReductionConfig;
 using iree_compiler::cpu::ReductionStrategy;
 using iree_compiler::IREE::transform_dialect::ForeachThreadToWorkgroupOp;
 using transform::LowerVectorsOp;
@@ -42,8 +44,7 @@ using transform_ext::ShapeKind;
 using transform_ext::StructuredOpMatcher;
 
 //===----------------------------------------------------------------------===//
-// Higher-level problem-specific strategy creation APIs, these should favor
-// user-friendliness.
+// Mid-level problem-specific strategy builder APIs, follow MLIR-style builders.
 //===----------------------------------------------------------------------===//
 
 /// Take care of the last common steps in a CPU strategy (i.e. vectorize,
@@ -70,8 +71,37 @@ std::pair<Value, Value> mlir::iree_compiler::cpu::buildCommonTrailingStrategy(
   return std::make_pair(variantH, funcH);
 }
 
+//===----------------------------------------------------------------------===//
+// Higher-level problem-specific strategy creation APIs, these should favor
+// user-friendliness.
+//===----------------------------------------------------------------------===//
+
+/// Placeholder to encode fixed reductions that should take finer-grained
+/// precedence over other heuristics. In the future, this could be lifted to
+/// e.g. `cpuModel` or higher up in some transform dialect database summary of
+/// "known good things".
+static FailureOr<ReductionConfig> applyKnownGoodReductionConfigurations(
+    const transform_ext::MatchedReductionCaptures &captures,
+    const CPUModel &cpuModel) {
+  std::optional<int64_t> vectorSize;
+  int64_t reductionSize = captures.reductionOpSizes.back();
+  if (cpuModel.model == CPUModel::kDefaultCPU) {
+    if (captures.reductionOutputElementalTypeBitWidth == 32) {
+      if (reductionSize == 32) vectorSize = 32;
+    }
+  }
+  if (!vectorSize.has_value()) return failure();
+  return ReductionConfig{vectorSize.value()};
+}
+
+static ReductionConfig getReductionConfig(
+    const transform_ext::MatchedReductionCaptures &captures,
+    const CPUModel &cpuModel) {
+  return ReductionConfig{16};
+}
+
 LogicalResult iree_compiler::cpu::matchAndSetReductionStrategy(
-    func::FuncOp entryPoint, linalg::LinalgOp op) {
+    func::FuncOp entryPoint, linalg::LinalgOp op, const CPUModel &cpuModel) {
   // 1. Match a reduction and surrounding ops.
   StructuredOpMatcher reduction, fill, leading, trailing;
   transform_ext::MatchedReductionCaptures captures;
@@ -81,8 +111,9 @@ LogicalResult iree_compiler::cpu::matchAndSetReductionStrategy(
   // 2. Construct the configuration and the strategy builder.
   // TODO: Generalize along the HW axis.
   auto strategyBuilder = [&](ImplicitLocOpBuilder &b, Value variant) {
-    // Otherwise, always fallback to the staged strategy.
-    auto strategy = ReductionStrategy::create(op->getContext(), captures);
+    ReductionConfig reductionConfig = getReductionConfig(captures, cpuModel);
+    auto strategy =
+        ReductionStrategy::create(op->getContext(), captures, reductionConfig);
     return buildReductionStrategy(b, variant, strategy);
   };
 

compiler/src/iree/compiler/Codegen/LLVMCPU/KernelDispatch.cpp
@@ -1225,7 +1225,9 @@ static LogicalResult setTransformStrategyRootConfig(
     const TargetMLTransformInfo &targetMLTransInfo) {
   if (!clCPUEnableTransformDialectJit) return failure();
   if (getLoweringConfig(genericOp)) return failure();
-  if (failed(cpu::matchAndSetReductionStrategy(entryPointFn, genericOp)))
+  cpu::CPUModel cpuModel;
+  if (failed(
+          cpu::matchAndSetReductionStrategy(entryPointFn, genericOp, cpuModel)))
     return failure();
   auto translationInfo = IREE::Codegen::TranslationInfoAttr::get(
       entryPointFn->getContext(),

compiler/src/iree/compiler/Codegen/LLVMGPU/KernelConfig.cpp
@@ -540,7 +540,9 @@ static LogicalResult setReductionTransformDialectConfig(
     return setTranslationInfo(entryPoint, translationInfo);
   }
 
-  if (failed(iree_compiler::gpu::matchAndSetReductionStrategy(entryPoint, op)))
+  iree_compiler::gpu::GPUModel gpuModel;
+  if (failed(iree_compiler::gpu::matchAndSetReductionStrategy(entryPoint, op,
+                                                              gpuModel)))
     return failure();
 
   return setTranslationInfo(entryPoint, translationInfo);

compiler/src/iree/compiler/Codegen/TransformDialectStrategies/CPU/Common.h
@@ -15,19 +15,42 @@ namespace mlir {
 namespace iree_compiler {
 namespace cpu {
 
+//===----------------------------------------------------------------------===//
+// Mid-level problem-specific strategy builder APIs, follow MLIR-style builders.
+//===----------------------------------------------------------------------===//
 /// Take care of the last common steps in a CPU strategy (i.e. vectorize,
 /// bufferize, maps to blocks/workgroups and lower vectors).
 /// Return the handles to the updated variant and the func::FuncOp ops under
 /// the variant op.
 // TODO: pass control to LowerVectorsOp once the builder allows it.
-std::pair<Value, Value> buildCommonTrailingStrategy(ImplicitLocOpBuilder &b,
+std::pair<Value, Value> buildCommonTrailingStrategy(ImplicitLocOpBuilder& b,
                                                     Value variantH);
 
-/// Return success if the IR matches what the GPU reduction strategy can
-/// handle. If it is success it will append the transform dialect after the
-/// entry point module.
+//===----------------------------------------------------------------------===//
+// Higher-level problem-specific strategy creation APIs, these should favor
+// user-friendliness.
+//===----------------------------------------------------------------------===//
+/// Placeholder for some hardware model proxy that contains relevant information
+/// to configure the reduction strategy. In the future, this will need to be
+/// driven by some contract with the runtime.
+struct CPUModel {
+  static constexpr StringLiteral kDefaultCPU = "DefaultCPU";
+  StringRef model = kDefaultCPU;
+};
+
+/// Map an N-D parallel, 1-D reduction operation with optional leading and
+/// optional trailing elementwise operations.
+/// The 1-D reduction dimension must be in the most minor dimension.
+/// The innermost dimensions of the leading and trailing operations must be most
+/// minor along all accesses.
+/// Return failure if matching fails.
+/// On a successful match, configure a reduction strategy based on a proxy model
+/// of the hardware and construct transform dialect IR that implements the
+/// reduction strategy. The transform dialect IR is added in a top-level
+/// ModuleOp after the `entryPoint` func::FuncOp.
 LogicalResult matchAndSetReductionStrategy(func::FuncOp entryPoint,
-                                           linalg::LinalgOp op);
+                                           linalg::LinalgOp op,
+                                           const CPUModel& cpuModel);
 }  // namespace cpu
 }  // namespace iree_compiler
 }  // namespace mlir

compiler/src/iree/compiler/Codegen/TransformDialectStrategies/CPU/ReductionStrategy.cpp
@@ -35,8 +35,8 @@ using transform_ext::RegisterMatchCallbacksOp;
 
 ReductionStrategy mlir::iree_compiler::cpu::ReductionStrategy::create(
     MLIRContext *context,
-    const transform_ext::MatchedReductionCaptures &captures) {
-  ReductionConfig reductionConfig = getReductionConfig(captures);
+    const transform_ext::MatchedReductionCaptures &captures,
+    const ReductionConfig &reductionConfig) {
   ReductionStrategy strategy(context, captures);
   strategy.configure(reductionConfig);
   LLVM_DEBUG(DBGS() << "use CPU reduction strategy\n");
@@ -67,8 +67,7 @@ void mlir::iree_compiler::cpu::buildReductionStrategy(
           b, "reduction", transform::FailurePropagationMode::Propagate,
           variantH);
 
-  // Step 2. Use tiling to introduce a single-iteration loop mapped to a
-  // single block/workgroup. Keep everything fused.
+  // Step 2. Tiling to the block/workgroup level. Keep everything fused.
   auto [maybeLeadingHBlock, gridFillH, gridReductionH,
         maybeTiledTrailingHBlock] =
       buildReductionStrategyBlockDistribution(
@@ -89,9 +88,4 @@ void mlir::iree_compiler::cpu::buildReductionStrategy(
 
   // Step 4-6. Common trailing steps.
   buildCommonTrailingStrategy(b, variantH);
-}
-
-ReductionConfig mlir::iree_compiler::cpu::getReductionConfig(
-    const transform_ext::MatchedReductionCaptures &captures) {
-  return ReductionConfig{16};
 }

compiler/src/iree/compiler/Codegen/TransformDialectStrategies/CPU/ReductionStrategy.h
@@ -13,6 +13,8 @@ namespace mlir {
 namespace iree_compiler {
 namespace cpu {
 
+struct CPUModel;
+
 /// Structure to hold a summary of HW-derived properties to configure the
 /// reduction strategy.
 /// The objective of this struct is to act as a minimal summary of key
@@ -29,13 +31,11 @@ class ReductionStrategy : public iree_compiler::AbstractReductionStrategy {
  public:
   static ReductionStrategy create(
       MLIRContext *context,
-      const transform_ext::MatchedReductionCaptures &captures);
+      const transform_ext::MatchedReductionCaptures &captures,
+      const ReductionConfig &reductionConfig);
 
   ReductionStrategy(const ReductionStrategy &) = default;
   ReductionStrategy &operator=(const ReductionStrategy &) = default;
-
-  // Always profitable.
-  bool isProfitable() override { return true; }
 
   int64_t getVectorSize() const { return vectorSize; }
 
@@ -50,16 +50,14 @@ class ReductionStrategy : public iree_compiler::AbstractReductionStrategy {
   int64_t vectorSize;
 };
 
-/// Entry point to build the transform IR corresponding to a reduction
-/// strategy. This is used for mapping a N-D parallel, 1-D reduction
-/// operation. The 1-D reduction dimensions must be in the most minor
-/// dimension. Supports an optional leading and an optional trailing
-/// elementwise operation.
+/// Entry point to build the transform IR corresponding to a reduction strategy.
+/// This is used to map an N-D parallel, 1-D reduction operation with optional
+/// leading and optional trailing elementwise operations.
+/// The 1-D reduction dimension must be in the most minor dimension.
+/// The innermost dimensions of the leading and trailing operations must be most
+/// minor along all accesses.
 void buildReductionStrategy(ImplicitLocOpBuilder &b, Value variantH,
                             const ReductionStrategy &strategy);
-
-ReductionConfig getReductionConfig(
-    const transform_ext::MatchedReductionCaptures &captures);
 
 }  // namespace cpu
 }  // namespace iree_compiler

commit 6aeec828213997364b31693e79fd9c58fdc9907f

[Transform] Fix perf regression in the dynamic case and add a test
d782f54f56def442105656cace5643666acd9b89 introduced a regression while refactoring:
the SmallStrategy would be selected by default for dynamic cases.

Fix and add a test.

Ideally we should also have some microbenchmarkings part of presubmit.

Repro:
```
(benchmark-transform-create -r -b cuda  ../iree-samples/transform_dialect/benchmark_linalg_reductions.stub.mlir   reduction_2d_dynamic  f32 128 4096)
```

Before:
```
403.76ms  202.76us              (2 1 1)        (64 1 1)        56        0B        0B  NVIDIA GeForce          1        13                     -                -  reduction_2d_dynamic_dispatch_0_generic_DxD [33]
With transform dialect: reduction_2d_dynamic --function_input="128x4096xf32=1" P50: 202800.00 ns 2.64836291913214990138 GElements/s
```

After:
```
449.43ms  4.8640us            (128 1 1)       (256 1 1)        22        0B  1.0078KB  NVIDIA GeForce          1        13                     -                -  reduction_2d_dynamic_dispatch_0_generic_DxD [33]
With transform dialect: reduction_2d_dynamic --function_input="128x4096xf32=1" P50: 4608.0000 ns 116.55555555555555555555 GElements/s
```

compiler/src/iree/compiler/Codegen/TransformDialectStrategies/GPU/Common.cpp
@@ -325,11 +325,11 @@ static ReductionConfig getReductionConfig(
   // Since this mode does not coalesce reads, perf will suffer
   // catastrophically on larger runtime reduction.
   // TODO: explicit hint from above that we really want to do that.
-  int64_t reductionDimensionSize = captures.reductionOpSizes.back();
-  bool isDynamicReduction = ShapedType::isDynamic(reductionDimensionSize);
+  int64_t redSize = captures.reductionOpSizes.back();
+  bool isDynamicReduction = ShapedType::isDynamic(redSize);
   // Otherwise, still only support the small cases for now and fall back to
   // other strategies otherwise.
-  bool isSmallReduction = (reductionDimensionSize < 2 * kCudaWarpSize);
+  bool isSmallReduction = (redSize < 2 * kCudaWarpSize);
   if (!isDynamicReduction && isSmallReduction) {
     int64_t maxNumThreads = 4 * kCudaWarpSize;
     return ReductionConfig{maxNumThreads, 0, ReductionStrategy::Small};
@@ -343,9 +343,9 @@ static ReductionConfig getReductionConfig(
   int64_t maxNumThreads = 8 * kCudaWarpSize;
   // No adjustments in the dynamic case, we need extra information to make a
   // good decision.
-  int64_t redSize = captures.reductionOpSizes.back();
   if (ShapedType::isDynamic(redSize))
-    return ReductionConfig{maxNumThreads, vectorSize};
+    return ReductionConfig{maxNumThreads, vectorSize,
+                           ReductionStrategy::Staged};
   // Scale down to smaller sizes (4, 8, 16)-warps.
   if (scaleUpByBitWidth(redSize, bitWidth) <= 4 * kCudaWarpSize) {
     vectorSize = scaleUpByBitWidth(1, bitWidth);

compiler/src/iree/compiler/Codegen/LLVMGPU/test/set_transform_strategy.mlir
@@ -97,6 +97,48 @@ hal.executable.variant public @cuda_nvptx_fb, target = <"cuda", "cuda-nvptx-fb",
 // -----
 
 
+hal.executable @group_reduction_D {
+hal.executable.variant public @cuda_nvptx_fb, target = <"cuda", "cuda-nvptx-fb", {target_arch = "sm_35"}> {
+  hal.executable.export public @group_reduction_D ordinal(0) layout(#hal.pipeline.layout<push_constants = 0, sets = [<0, bindings = [<0, storage_buffer, ReadOnly>, <1, storage_buffer>]>]>) {
+  ^bb0(%arg0: !hal.device, %arg1: index, %arg2: index):
+    %x, %y, %z = flow.dispatch.workgroup_count_from_dag_root %arg1, %arg2
+    hal.return %x, %y, %z : index, index, index
+  }
+  builtin.module {
+    func.func @group_reduction_D() {
+      %c0 = arith.constant 0 : index
+      %cst = arith.constant -0.000000e+00 : f32
+      %d0i = hal.interface.constant.load[0] : i32
+      %d0 = arith.index_castui %d0i : i32 to index
+      %0 = hal.interface.binding.subspan set(0) binding(0) type(storage_buffer) offset(%c0) alignment(64) : !flow.dispatch.tensor<readonly:tensor<8x?xf32>>{%d0}
+      %1 = hal.interface.binding.subspan set(0) binding(1) type(storage_buffer) offset(%c0) alignment(64) : !flow.dispatch.tensor<writeonly:tensor<8xf32>>
+      %2 = flow.dispatch.tensor.load %0, offsets = [0, 0], sizes = [8, %d0], strides = [1, 1] : !flow.dispatch.tensor<readonly:tensor<8x?xf32>>{%d0} -> tensor<8x?xf32>
+      %3 = tensor.empty() : tensor<8xf32>
+      %4 = linalg.fill ins(%cst : f32) outs(%3 : tensor<8xf32>) -> tensor<8xf32>
+      %5 = linalg.generic {indexing_maps = [affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0)>], iterator_types = ["parallel", "reduction"]} ins(%2 : tensor<8x?xf32>) outs(%4 : tensor<8xf32>) {
+      ^bb0(%in: f32, %out: f32):
+        %6 = arith.addf %in, %out : f32
+        linalg.yield %6 : f32
+      } -> tensor<8xf32>
+      flow.dispatch.tensor.store %5, %1, offsets = [0], sizes = [8], strides = [1] : tensor<8xf32> -> !flow.dispatch.tensor<writeonly:tensor<8xf32>>
+      return
+    }
+  }
+}
+}
+
+// Overall, the schedule is same as above, but with larger tile sizes.
+// Checking only the tile sizes.
+
+//   CHECK-LABEL: func.func @group_reduction_D
+//         CHECK:   transform.structured.canonicalized_sequence failures(propagate)
+//         CHECK:   transform.structured.tile_reduction_using_foreach_thread %{{.*}} by num_threads = [0, 256], tile_sizes = [0, 1], mapping = [#gpu.thread<x>]
+//         CHECK:   transform.iree.map_nested_foreach_thread_to_gpu_threads %{{.*}} {workgroup_size = [256, 1, 1]}
+//         CHECK:   transform.iree.vector.to_warp_execute_on_lane_0 %{{.*}} {warp_size = 64 : i64}
+
+// -----
+
+
 hal.executable @group_reduction_34 {
 hal.executable.variant public @cuda_nvptx_fb, target = <"cuda", "cuda-nvptx-fb", {target_arch = "sm_35"}> {
   hal.executable.export public @group_reduction_34 ordinal(0) layout(#hal.pipeline.layout<push_constants = 0, sets = [<0, bindings = [<0, storage_buffer, ReadOnly>, <1, storage_buffer>]>]>) {