c03a9c8a0e69536764bf066cf50690d93defa783
Ben Vanik <ben.vanik@gmail.com>
2023-01-23 11:46:26 -0800

    Adding CUDA device-side tracing support (`--cuda_tracing`). This uses a pool of CUevents and trace zones inserted during stream execution. Graphs will require a bit more plumbing to allow for updating the events/trace zones used during launches but since most performance work is being done with streams this is deferred.

--- runtime/src/iree/hal/drivers/cuda/native_executable.h
+++ runtime/src/iree/hal/drivers/cuda/native_executable.h
@@ -10,6 +10,7 @@
 #include <stdint.h>
 
 #include "iree/base/api.h"
+#include "iree/base/tracing.h"
 #include "iree/hal/api.h"
 #include "iree/hal/drivers/cuda/context_wrapper.h"
 #include "iree/hal/drivers/cuda/cuda_headers.h"
@@ -18,6 +19,14 @@
 extern "C" {
 #endif  // __cplusplus
 
+typedef struct iree_hal_cuda_kernel_params_t {
+  iree_hal_pipeline_layout_t* layout;
+  CUfunction function;
+  uint32_t block_size[3];
+  uint32_t shared_memory_size;
+  IREE_TRACE(iree_string_view_t function_name);
+} iree_hal_cuda_kernel_params_t;
+
 // Creates an executable from a PTX module. The module may contain several
 // kernels that can be extracted along with the associated block size.
 iree_status_t iree_hal_cuda_native_executable_create(
@@ -25,23 +34,10 @@ iree_status_t iree_hal_cuda_native_executable_create(
     const iree_hal_executable_params_t* executable_params,
     iree_hal_executable_t** out_executable);
 
-CUfunction iree_hal_cuda_native_executable_for_entry_point(
-    iree_hal_executable_t* executable, int32_t entry_point);
-
-// Return the block size of the given |entry_point| within the executable.
-iree_status_t iree_hal_cuda_native_executable_block_size(
-    iree_hal_executable_t* executable, int32_t entry_point, uint32_t* x,
-    uint32_t* y, uint32_t* z);
-
-// Return the shared memory size of the given |entry_point| within the
-// executable.
-iree_status_t iree_hal_cuda_native_executable_shared_memory_size(
+// Returns the kernel launch parameters for the given |entry_point|.
+iree_status_t iree_hal_cuda_native_executable_entry_point_kernel_params(
     iree_hal_executable_t* executable, int32_t entry_point,
-    uint32_t* shared_memory_size);
-
-/// Return the layout associated with the entry point.
-iree_hal_pipeline_layout_t* iree_hal_cuda_executable_get_layout(
-    iree_hal_executable_t* executable, int32_t entry_point);
+    iree_hal_cuda_kernel_params_t* out_params);
 
 #ifdef __cplusplus
 }  // extern "C"
