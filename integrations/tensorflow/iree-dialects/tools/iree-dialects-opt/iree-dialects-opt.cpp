167c492d649f5f757adfe8217204aa2fde55d276
Diego Caballero <diegocaballero@google.com>
2023-01-27 21:12:59 +0000

    Revert "Try updating iree-dialects"
    This reverts commit 1df4823b7aff302c6b1538dab03f7b101aadfa56.

--- integrations/tensorflow/iree-dialects/tools/iree-dialects-opt/iree-dialects-opt.cpp
+++ integrations/tensorflow/iree-dialects/tools/iree-dialects-opt/iree-dialects-opt.cpp
@@ -5,12 +5,6 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include "iree-dialects/Dialect/Input/InputDialect.h"
-#include "iree-dialects/Dialect/LinalgExt/IR/LinalgExtDialect.h"
-#include "iree-dialects/Dialect/LinalgExt/Passes/Passes.h"
-#include "iree-dialects/Dialect/LinalgExt/TransformOps/LinalgExtTransformOps.h"
-#include "iree-dialects/Dialect/LinalgTransform/LinalgTransformOps.h"
-#include "iree-dialects/Dialect/LinalgTransform/Passes.h"
-#include "iree-dialects/Dialect/LinalgTransform/StructuredTransformOpsExt.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Async/IR/Async.h"
@@ -28,7 +22,6 @@
 #include "mlir/Dialect/SCF/Transforms/Passes.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Dialect/Transform/IR/TransformDialect.h"
-#include "mlir/Dialect/Vector/TransformOps/VectorTransformOps.h"
 #include "mlir/IR/AsmState.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
@@ -41,7 +34,6 @@ namespace mlir {
 namespace test_ext {
 /// Test passes, do not deserve an include.
 void registerTestLinalgTransformWrapScope();
-void registerTestListenerPasses();
 } // namespace test_ext
 } // namespace mlir
 
@@ -54,8 +46,6 @@ int main(int argc, char **argv) {
       // clang-format off
       // Local dialects
       mlir::iree_compiler::IREE::Input::IREEInputDialect,
-      mlir::iree_compiler::IREE::LinalgExt::IREELinalgExtDialect,
-      mlir::linalg::transform::LinalgTransformDialect,
       // Upstream dialects
       mlir::async::AsyncDialect,
       mlir::arith::ArithDialect,
@@ -68,8 +58,7 @@ int main(int argc, char **argv) {
       mlir::pdl_interp::PDLInterpDialect,
       mlir::scf::SCFDialect,
       mlir::tensor::TensorDialect,
-      mlir::transform::TransformDialect,
-      mlir::vector::VectorDialect
+      mlir::transform::TransformDialect
       // clang-format on
       >();
 
@@ -77,23 +66,12 @@ int main(int argc, char **argv) {
   memref::registerMemRefPasses();
   registerTransformsPasses();
   registerSCFPasses();
-  // Local dialect passes.
-  mlir::iree_compiler::IREE::LinalgExt::registerPasses();
-  mlir::linalg::transform::registerTransformDialectInterpreterPass();
-  mlir::linalg::transform::registerLinalgTransformExpertExpansionPass();
-  mlir::linalg::transform::registerDropSchedulePass();
-  // Local test passes.
-  mlir::test_ext::registerTestLinalgTransformWrapScope();
-  mlir::test_ext::registerTestListenerPasses();
 
   // External models.
   mlir::linalg::registerTilingInterfaceExternalModels(registry);
 
-  registry.addExtensions<IREE::LinalgExt::LinalgExtTransformOpsExtension,
-                         transform_ext::StructuredTransformOpsExtension>();
   mlir::linalg::registerTransformDialectExtension(registry);
   mlir::scf::registerTransformDialectExtension(registry);
-  mlir::vector::registerTransformDialectExtension(registry);
 
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "MLIR modular optimizer driver\n", registry,
