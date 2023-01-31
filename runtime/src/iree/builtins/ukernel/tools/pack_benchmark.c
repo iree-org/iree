iree-org/iree

commit b14aaf486b4408b8bbbd6454b0247b105849a6e4

pack microkernel padding improvements (#11987)
- More correct: the old code was incorrect for really large padding
(test expanded).
- Faster: padding is handled with an intermediate buffer, which allows
padded tiles to still be packed by the fast target-specific tile func,
as opposed to a super slow generic fallback. Moreover, we optimize the
common case where the padding byte pattern is a single-byte pattern so
we can use memset.

Even with that, padding is still surprisingly slow. This means that
compiler-level ways to remove padding overhead (pad fusions) are
important in a way that is not diminished by the ability to call
microkernels. @MaheshRavishankar @hanhanW

Note: in ruy and XNNPACK, padding is handled by having separate source
pointers for N strided rows being packed (so padding rows can just have
their pointer point at some zeros). This does not scale well to the
degree of generality and runtime-flexibility that we are aiming for here
(this essentially requires knowing some tile dimensions at compile time,
and being able to design the whole system around a restricted range of
values for these tile sizes). While not the fastest, the
intermediate-buffer approach taken here is not that slow in principle
(that it happens to be less fast than I hoped seems to be an accident of
how it's compiled by Clang, but I haven't been able to dig to the bottom
of that), and at least it makes for smaller code and insulates the
target-optimized kernels from any padding consideration.

At first I thought that in large-stride cases, L1 cache aliasing would
be a major issue, so I thought that the temporary buffer would be useful
to deal with that. But it almost never was a net win in benchmarks. So
the PR evolved towards its current state where the temp buffer is only
used to deal with padding. But at least it's still easy to revisit that
later.

runtime/src/iree/builtins/ukernel/tools/pack_benchmark.c
@@ -109,7 +109,7 @@ static iree_status_t iree_pack_benchmark(
   }
   params.in_size0 = iree_max(0, out_size0 * out_size2 - FLAG_padding_size);
   params.in_size1 = iree_max(0, out_size1 * out_size3 - FLAG_padding_size);
-  params.in_stride0 = params.in_size1;
+  params.in_stride0 = out_size0 * out_size2;
   params.out_stride0 = params.out_size1 * params.out_size2 * params.out_size3;
   iree_uk_ssize_t in_buffer_size = iree_uk_test_2d_buffer_length(
       in_type, params.in_size0, params.in_stride0);
@@ -126,8 +126,8 @@ static iree_status_t iree_pack_benchmark(
   iree_uk_test_write_random_buffer(in_buffer, in_buffer_size, in_type, engine);
   iree_uk_test_write_random_buffer(out_buffer, out_buffer_size, out_type,
                                    engine);
-  iree_uk_test_write_random_buffer(padding_value_buffer, out_type_size,
-                                   out_type, engine);
+  // Test single-byte padding pattern, most common use case as 0.0f is 0 bytes.
+  memset(padding_value_buffer, 0, out_type_size);
   iree_uk_test_random_engine_destroy(engine);
   params.in_buffer = in_buffer;
   params.out_buffer = out_buffer;

runtime/src/iree/builtins/ukernel/arch/arm_64/pack_tile_arm_64.c
@@ -8,14 +8,14 @@
 
 #include <arm_neon.h>
 
-void* iree_uk_pack_tile_8x1_x32_arm_64_direct(
+void iree_uk_pack_tile_8x1_x32_arm_64_direct(
     void* restrict out_tile_ptr, const void* restrict in_tile_ptr,
-    iree_uk_ssize_t outer_size1, iree_uk_ssize_t out_stride_l1,
+    iree_uk_ssize_t outer_size1, iree_uk_ssize_t out_stride1,
     iree_uk_ssize_t in_stride0, iree_uk_ssize_t elem_size_unused,
     iree_uk_ssize_t tile_size0_unused, iree_uk_ssize_t tile_size1_unused) {
-  return iree_uk_pack_tile_8x4_x8_arm_64_direct(out_tile_ptr, in_tile_ptr,
-                                                outer_size1, out_stride_l1 * 4,
-                                                in_stride0 * 4, 1, 8, 4);
+  iree_uk_pack_tile_8x4_x8_arm_64_direct(out_tile_ptr, in_tile_ptr, outer_size1,
+                                         out_stride1 * 4, in_stride0 * 4, 1, 8,
+                                         4);
 }
 
 static inline int8x8_t iree_uk_neon_load_8xi8_strided(const iree_uk_int8_t* src,
@@ -181,9 +181,9 @@ static inline void iree_uk_neon_copy_8x8xi8_rowmajor_to_colmajor_tiled_1x4(
   vst1q_s8(out_ptr + 16 + 1 * out_stride, vreinterpretq_s8_s32(c1.val[1]));
 }
 
-void* iree_uk_pack_tile_8x1_x8_arm_64_direct(
+void iree_uk_pack_tile_8x1_x8_arm_64_direct(
     void* restrict out_tile_ptr, const void* restrict in_tile_ptr,
-    iree_uk_ssize_t outer_size1, iree_uk_ssize_t out_stride_l1,
+    iree_uk_ssize_t outer_size1, iree_uk_ssize_t out_stride1,
     iree_uk_ssize_t in_stride0, iree_uk_ssize_t elem_size_unused,
     iree_uk_ssize_t tile_size0_unused, iree_uk_ssize_t tile_size1_unused) {
   iree_uk_ssize_t outer_i1 = 0;
@@ -193,22 +193,21 @@ void* iree_uk_pack_tile_8x1_x8_arm_64_direct(
   // thanks to using 16-byte loads. Is it worth the code size? This 8x1 tile is
   // used on baseline aarch64 where the matmul kernel is slow anyway.
   for (; outer_i1 <= outer_size1 - 8; outer_i1 += 8) {
-    iree_uk_neon_copy_8x8xi8_rowmajor_to_colmajor(out_ptr, in_ptr,
-                                                  out_stride_l1, in_stride0);
-    out_ptr += 8 * out_stride_l1;
+    iree_uk_neon_copy_8x8xi8_rowmajor_to_colmajor(out_ptr, in_ptr, out_stride1,
+                                                  in_stride0);
+    out_ptr += 8 * out_stride1;
     in_ptr += 8;
   }
   for (; outer_i1 < outer_size1; ++outer_i1) {
     iree_uk_neon_copy_8x1xi8_strided_to_unstrided(out_ptr, in_ptr, in_stride0);
-    out_ptr += out_stride_l1;
+    out_ptr += out_stride1;
     in_ptr += 1;
   }
-  return out_ptr;
 }
 
-void* iree_uk_pack_tile_8x4_x8_arm_64_direct(
+void iree_uk_pack_tile_8x4_x8_arm_64_direct(
     void* restrict out_tile_ptr, const void* restrict in_tile_ptr,
-    iree_uk_ssize_t outer_size1, iree_uk_ssize_t out_stride_l1,
+    iree_uk_ssize_t outer_size1, iree_uk_ssize_t out_stride1,
     iree_uk_ssize_t in_stride0, iree_uk_ssize_t elem_size_unused,
     iree_uk_ssize_t tile_size0_unused, iree_uk_ssize_t tile_size1_unused) {
   iree_uk_ssize_t outer_i1 = 0;
@@ -216,21 +215,20 @@ void* iree_uk_pack_tile_8x4_x8_arm_64_direct(
   const iree_uk_int8_t* restrict in_ptr = in_tile_ptr;
   for (; outer_i1 <= outer_size1 - 2; outer_i1 += 2) {
     iree_uk_neon_copy_8x8xi8_rowmajor_to_colmajor_tiled_1x4(
-        out_ptr, in_ptr, out_stride_l1, in_stride0);
-    out_ptr += 2 * out_stride_l1;
+        out_ptr, in_ptr, out_stride1, in_stride0);
+    out_ptr += 2 * out_stride1;
     in_ptr += 8;
   }
   for (; outer_i1 < outer_size1; outer_i1++) {
     iree_uk_neon_copy_8x4xi8_rowmajor_strided_to_unstrided(out_ptr, in_ptr,
                                                            in_stride0);
-    out_ptr += out_stride_l1;
+    out_ptr += out_stride1;
     in_ptr += 4;
   }
-  return out_ptr;
 }
-void* iree_uk_pack_tile_8x8_x8_arm_64_direct(
+void iree_uk_pack_tile_8x8_x8_arm_64_direct(
     void* restrict out_tile_ptr, const void* restrict in_tile_ptr,
-    iree_uk_ssize_t outer_size1, iree_uk_ssize_t out_stride_l1,
+    iree_uk_ssize_t outer_size1, iree_uk_ssize_t out_stride1,
     iree_uk_ssize_t in_stride0, iree_uk_ssize_t elem_size_unused,
     iree_uk_ssize_t tile_size0_unused, iree_uk_ssize_t tile_size1_unused) {
   iree_uk_int8_t* restrict out_ptr = out_tile_ptr;
@@ -238,54 +236,51 @@ void* iree_uk_pack_tile_8x8_x8_arm_64_direct(
   for (iree_uk_ssize_t outer_i1 = 0; outer_i1 < outer_size1; ++outer_i1) {
     iree_uk_neon_copy_8x8xi8_rowmajor_strided_to_unstrided(out_ptr, in_ptr,
                                                            in_stride0);
-    out_ptr += out_stride_l1;
+    out_ptr += out_stride1;
     in_ptr += 8;
   }
-  return out_ptr;
 }
 
-void* iree_uk_pack_tile_8x1_x32_arm_64_transpose(
+void iree_uk_pack_tile_8x1_x32_arm_64_transpose(
     void* restrict out_tile_ptr, const void* restrict in_tile_ptr,
-    iree_uk_ssize_t outer_size1, iree_uk_ssize_t out_stride_l1,
+    iree_uk_ssize_t outer_size1, iree_uk_ssize_t out_stride1,
     iree_uk_ssize_t in_stride0, iree_uk_ssize_t elem_size_unused,
     iree_uk_ssize_t tile_size0_unused, iree_uk_ssize_t tile_size1_unused) {
   const iree_uk_int32_t* restrict in_tile_ptr_i32 = in_tile_ptr;
   iree_uk_int32_t* restrict out_tile_i32_ptr = out_tile_ptr;
   for (iree_uk_ssize_t outer_i1 = 0; outer_i1 < outer_size1; ++outer_i1) {
     iree_uk_memcpy(out_tile_i32_ptr, in_tile_ptr_i32, 32);
-    out_tile_i32_ptr += out_stride_l1;
+    out_tile_i32_ptr += out_stride1;
     in_tile_ptr_i32 += 8;
   }
-  return out_tile_i32_ptr;
 }
 
-void* iree_uk_pack_tile_8x1_x8_arm_64_transpose(
+void iree_uk_pack_tile_8x1_x8_arm_64_transpose(
     void* restrict out_tile_ptr, const void* restrict in_tile_ptr,
-    iree_uk_ssize_t outer_size1, iree_uk_ssize_t out_stride_l1,
+    iree_uk_ssize_t outer_size1, iree_uk_ssize_t out_stride1,
     iree_uk_ssize_t in_stride0, iree_uk_ssize_t elem_size_unused,
     iree_uk_ssize_t tile_size0_unused, iree_uk_ssize_t tile_size1_unused) {
   const iree_uk_int8_t* restrict in_ptr = in_tile_ptr;
   iree_uk_int8_t* restrict out_ptr = out_tile_ptr;
   iree_uk_ssize_t outer_i1 = 0;
   for (; outer_i1 <= outer_size1 - 4; outer_i1 += 4) {
-    iree_uk_memcpy(out_ptr + 0 * out_stride_l1, in_ptr + 0, 8);
-    iree_uk_memcpy(out_ptr + 1 * out_stride_l1, in_ptr + 8, 8);
-    iree_uk_memcpy(out_ptr + 2 * out_stride_l1, in_ptr + 16, 8);
-    iree_uk_memcpy(out_ptr + 3 * out_stride_l1, in_ptr + 24, 8);
-    out_ptr += 4 * out_stride_l1;
+    iree_uk_memcpy(out_ptr + 0 * out_stride1, in_ptr + 0, 8);
+    iree_uk_memcpy(out_ptr + 1 * out_stride1, in_ptr + 8, 8);
+    iree_uk_memcpy(out_ptr + 2 * out_stride1, in_ptr + 16, 8);
+    iree_uk_memcpy(out_ptr + 3 * out_stride1, in_ptr + 24, 8);
+    out_ptr += 4 * out_stride1;
     in_ptr += 32;
   }
   for (; outer_i1 < outer_size1; ++outer_i1) {
     iree_uk_memcpy(out_ptr, in_ptr, 8);
-    out_ptr += out_stride_l1;
+    out_ptr += out_stride1;
     in_ptr += 8;
   }
-  return out_ptr;
 }
 
-void* iree_uk_pack_tile_8x4_x8_arm_64_transpose(
+void iree_uk_pack_tile_8x4_x8_arm_64_transpose(
     void* restrict out_tile_ptr, const void* restrict in_tile_ptr,
-    iree_uk_ssize_t outer_size1, iree_uk_ssize_t out_stride_l1,
+    iree_uk_ssize_t outer_size1, iree_uk_ssize_t out_stride1,
     iree_uk_ssize_t in_stride0, iree_uk_ssize_t elem_size_unused,
     iree_uk_ssize_t tile_size0_unused, iree_uk_ssize_t tile_size1_unused) {
   const iree_uk_int8_t* restrict in_ptr = in_tile_ptr;
@@ -301,15 +296,14 @@ void* iree_uk_pack_tile_8x4_x8_arm_64_transpose(
         iree_uk_neon_zip_8xi16_as_4xi32(zip_i16.val[0], zip_i16.val[1]);
     vst1q_s8(out_ptr, vreinterpretq_s8_s32(zip_i32.val[0]));
     vst1q_s8(out_ptr + 16, vreinterpretq_s8_s32(zip_i32.val[1]));
-    out_ptr += out_stride_l1;
+    out_ptr += out_stride1;
     in_ptr += 8;
   }
-  return out_ptr;
 }
 
-void* iree_uk_pack_tile_8x8_x8_arm_64_transpose(
+void iree_uk_pack_tile_8x8_x8_arm_64_transpose(
     void* restrict out_tile_ptr, const void* restrict in_tile_ptr,
-    iree_uk_ssize_t outer_size1, iree_uk_ssize_t out_stride_l1,
+    iree_uk_ssize_t outer_size1, iree_uk_ssize_t out_stride1,
     iree_uk_ssize_t in_stride0, iree_uk_ssize_t elem_size_unused,
     iree_uk_ssize_t tile_size0_unused, iree_uk_ssize_t tile_size1_unused) {
   const iree_uk_int8_t* restrict in_ptr = in_tile_ptr;
@@ -317,8 +311,7 @@ void* iree_uk_pack_tile_8x8_x8_arm_64_transpose(
   for (iree_uk_ssize_t outer_i1 = 0; outer_i1 < outer_size1; ++outer_i1) {
     iree_uk_neon_copy_8x8xi8_rowmajor_to_colmajor(out_ptr, in_ptr, 8,
                                                   in_stride0);
-    out_ptr += out_stride_l1;
+    out_ptr += out_stride1;
     in_ptr += 8;
   }
-  return out_ptr;
 }

runtime/src/iree/builtins/ukernel/common.h
@@ -102,6 +102,14 @@
 // Include IREE_UK_STATIC_ASSERT.
 #include "iree/builtins/ukernel/static_assert.h"
 
+// Clang on Windows has __builtin_clzll; otherwise we need to use the
+// windows intrinsic functions.
+#if defined(_MSC_VER)
+#include <intrin.h>
+#pragma intrinsic(_BitScanReverse)
+#pragma intrinsic(_BitScanForward)
+#endif  // defined(_MSC_VER)
+
 #ifdef __cplusplus
 extern "C" {
 #endif  // __cplusplus
@@ -195,6 +203,21 @@ extern "C" {
 #define IREE_UK_ATTRIBUTE_NOINLINE
 #endif  // IREE_UK_HAVE_ATTRIBUTE(noinline)
 
+#if defined(__GNUC__) || defined(__clang__)
+#define IREE_UK_LIKELY(x) (__builtin_expect(!!(x), 1))
+#define IREE_UK_UNLIKELY(x) (__builtin_expect(!!(x), 0))
+#else
+#define IREE_UK_LIKELY(x) (x)
+#define IREE_UK_UNLIKELY(x) (x)
+#endif  // IREE_HAVE_ATTRIBUTE(likely)
+
+#if IREE_UK_HAVE_ATTRIBUTE(aligned) || \
+    (defined(__GNUC__) && !defined(__clang__))
+#define IREE_UK_ATTRIBUTE_ALIGNED(N) __attribute__((aligned(N)))
+#else
+#define IREE_UK_ATTRIBUTE_ALIGNED(N)
+#endif  // IREE_UK_HAVE_ATTRIBUTE(noinline)
+
 //===----------------------------------------------------------------------===//
 // Local replacements for stdint.h types and constants
 // Refer to the comment at the top of this file for why we can't include
@@ -261,6 +284,14 @@ static inline void iree_uk_ssize_swap(iree_uk_ssize_t* a, iree_uk_ssize_t* b) {
   iree_uk_ssize_t t = *a;
   *a = *b;
   *b = t;
+}
+
+static inline iree_uk_ssize_t iree_uk_ssize_clamp(iree_uk_ssize_t val,
+                                                  iree_uk_ssize_t min,
+                                                  iree_uk_ssize_t max) {
+  if (val < min) val = min;
+  if (val > max) val = max;
+  return val;
 }
 
 //===----------------------------------------------------------------------===//
@@ -458,8 +489,7 @@ static inline iree_uk_type_t iree_uk_untie_type(int pos,
 //===----------------------------------------------------------------------===//
 
 // The `restrict` here have the effect of enabling the compiler to rewrite this
-// as a memcpy call, shrinking code size of the (slow anyway) generic code paths
-// that would use this.
+// as a memcpy call.
 static inline void iree_uk_memcpy(void* IREE_UK_RESTRICT dst,
                                   const void* IREE_UK_RESTRICT src,
                                   iree_uk_ssize_t size) {
@@ -468,12 +498,63 @@ static inline void iree_uk_memcpy(void* IREE_UK_RESTRICT dst,
 }
 
 static inline void iree_uk_memset(void* buf, int val, iree_uk_ssize_t n) {
-  // No need for memset builtins: this naive loop is already transformed into a
-  // memset by both clang and gcc on ARM64. As __builtin_memset_inline requires
-  // a compile-time-constant size, it would require writing more complex code,
-  // which could actually prevent the optimization matching it as a single
-  // memset!
+  // This naive loop is lifted to a memset by both clang and gcc on ARM64.
   for (iree_uk_ssize_t i = 0; i < n; ++i) ((char*)buf)[i] = val;
+}
+
+//===----------------------------------------------------------------------===//
+// Count leading zeros (extracted from base/internal/math.h and adapted
+// to be able to be used standalone).
+//===----------------------------------------------------------------------===//
+
+static inline int iree_uk_count_leading_zeros_u32(const iree_uk_uint32_t n) {
+#if defined(_MSC_VER)
+  unsigned long result = 0;  // NOLINT(runtime/int)
+  if (_BitScanReverse(&result, n)) {
+    return (int)(31 - result);
+  }
+  return 32;
+#elif defined(__GNUC__) || defined(__clang__)
+#if defined(__LCZNT__)
+  // NOTE: LZCNT is a risky instruction; it is not supported on architectures
+  // before Haswell, yet it is encoded as 'rep bsr', which typically ignores
+  // invalid rep prefixes, and interprets it as the 'bsr' instruction, which
+  // returns the index of the value rather than the count, resulting in
+  // incorrect code.
+  return (int)__lzcnt32(n);
+#endif  // defined(__LCZNT__)
+
+  // Handle 0 as a special case because __builtin_clz(0) is undefined.
+  if (n == 0) return 32;
+  // Use __builtin_clz, which uses the following instructions:
+  //  x86: bsr
+  //  ARM64: clz
+  //  PPC: cntlzd
+  return (int)__builtin_clz(n);
+#else
+#error No clz for this arch.
+#endif  // MSVC / GCC / CLANG
+}
+
+//===----------------------------------------------------------------------===//
+// Power-of-two math helpers
+//===----------------------------------------------------------------------===//
+
+static inline bool iree_uk_is_po2_u32(const iree_uk_uint32_t n) {
+  return !(n & (n - 1));
+}
+
+static inline int iree_uk_floor_log2_u32(const iree_uk_uint32_t n) {
+  return 31 - iree_uk_count_leading_zeros_u32(n);
+}
+
+static inline int iree_uk_po2_log2_u32(const iree_uk_uint32_t n) {
+  IREE_UK_ASSERT(iree_uk_is_po2_u32(n));
+  return iree_uk_floor_log2_u32(n);
+}
+
+static inline int iree_uk_ceil_log2_u32(const iree_uk_uint32_t n) {
+  return n <= 1 ? 0 : (1 + iree_uk_floor_log2_u32(n - 1));
 }
 
 #ifdef __cplusplus

runtime/src/iree/builtins/ukernel/elementwise_impl.c.inc
@@ -61,48 +61,6 @@ typedef enum {
 #define ASSI32(ptr) *((iree_uk_int32_t*)ptr)
 
 //===----------------------------------------------------------------------===//
-// Math helper functions (extracted from base/internal/math.h and adapted
-// to be able to be used standalone).
-//===----------------------------------------------------------------------===//
-
-// Clang on Windows has __builtin_clzll; otherwise we need to use the
-// windows intrinsic functions.
-#if defined(_MSC_VER)
-#include <intrin.h>
-#pragma intrinsic(_BitScanReverse)
-#pragma intrinsic(_BitScanForward)
-#endif  // IREE_COMPILER_MSVC
-
-static inline int iree_uk_count_leading_zeros_u32(const iree_uk_uint32_t n) {
-#if defined(_MSC_VER)
-  unsigned long result = 0;  // NOLINT(runtime/int)
-  if (_BitScanReverse(&result, n)) {
-    return (int)(31 - result);
-  }
-  return 32;
-#elif defined(__GNUC__) || defined(__clang__)
-#if defined(__LCZNT__)
-  // NOTE: LZCNT is a risky instruction; it is not supported on architectures
-  // before Haswell, yet it is encoded as 'rep bsr', which typically ignores
-  // invalid rep prefixes, and interprets it as the 'bsr' instruction, which
-  // returns the index of the value rather than the count, resulting in
-  // incorrect code.
-  return (int)__lzcnt32(n);
-#endif  // defined(__LCZNT__)
-
-  // Handle 0 as a special case because __builtin_clz(0) is undefined.
-  if (n == 0) return 32;
-  // Use __builtin_clz, which uses the following instructions:
-  //  x86: bsr
-  //  ARM64: clz
-  //  PPC: cntlzd
-  return (int)__builtin_clz(n);
-#else
-#error No clz for this arch.
-#endif  // MSVC / GCC / CLANG
-}
-
-//===----------------------------------------------------------------------===//
 // Implementation macros.
 //===----------------------------------------------------------------------===//
 

runtime/src/iree/builtins/ukernel/mmt4d.c
@@ -101,8 +101,7 @@ static void iree_uk_mmt4d_zero_out(const iree_uk_mmt4d_params_t* params) {
 // Early-return code paths, including trivial or near-trivial cases (when one
 // of the dimensions is 0) and in the future, hardware ports that specialize
 // the entire loop nest.
-// The value |true| is written to the out-param |*done| if an early-return path
-// was taken and the mmt4d work is already done.
+// Returns true if already done.
 static bool iree_uk_mmt4d_early(const iree_uk_mmt4d_params_t* params) {
   // Trivial cases
   if (params->M == 0 || params->N == 0) {

commit df0db843401a3053e111fc69798f4e04f4c2efa2

fix bad stride in pack_benchmark (#12009)
Fixes #12008
Regression introduced in #11987

I had tweaked the strides in the benchmark as I thought for a moment
that it was more realistic to benchmark round (power of two) strides
even when padding makes sizes a little smaller. But now I'm not so sure
anymore what's the more important case in practice, so this just reverts
to the old state.

The crash was because instead of the correct ` out_size1 * out_size3`
(value for `in_size1` before adjustment for padding) I had incorrectly
typed `out_size0 * out_size2`.

runtime/src/iree/builtins/ukernel/tools/pack_benchmark.c
@@ -109,7 +109,7 @@ static iree_status_t iree_pack_benchmark(
   }
   params.in_size0 = iree_max(0, out_size0 * out_size2 - FLAG_padding_size);
   params.in_size1 = iree_max(0, out_size1 * out_size3 - FLAG_padding_size);
-  params.in_stride0 = out_size0 * out_size2;
+  params.in_stride0 = params.in_size1;
   params.out_stride0 = params.out_size1 * params.out_size2 * params.out_size3;
   iree_uk_ssize_t in_buffer_size = iree_uk_test_2d_buffer_length(
       in_type, params.in_size0, params.in_stride0);