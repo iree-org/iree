6dec38bc14dd3348b45df912100c588079bb89dc
Scott Todd <scotttodd@google.com>
2023-02-02 11:25:42 -0800

    Append op breadcrumb text for all passes when tracing.

--- compiler/src/iree/compiler/Utils/TracingUtils.cpp
+++ compiler/src/iree/compiler/Utils/TracingUtils.cpp
@@ -6,6 +6,8 @@
 
 #include "iree/compiler/Utils/TracingUtils.h"
 
+#include "mlir/IR/SymbolTable.h"
+
 namespace mlir {
 namespace iree_compiler {
 
@@ -16,11 +18,28 @@ namespace {
 thread_local llvm::SmallVector<iree_zone_id_t, 8> passTraceZonesStack;
 }  // namespace
 
+static void prettyPrintOpBreadcrumb(Operation *op, llvm::raw_ostream &os) {
+  auto parentOp = op->getParentOp();
+  if (parentOp) {
+    prettyPrintOpBreadcrumb(parentOp, os);
+    os << " > ";
+  }
+  os << op->getName();
+  if (auto symbolOp = dyn_cast<SymbolOpInterface>(op)) {
+    os << " @" << symbolOp.getName();
+  }
+}
+
 void PassTracing::runBeforePass(Pass *pass, Operation *op) {
   IREE_TRACE_ZONE_BEGIN_EXTERNAL(z0, __FILE__, strlen(__FILE__), __LINE__,
                                  pass->getName().data(), pass->getName().size(),
                                  NULL, 0);
   passTraceZonesStack.push_back(z0);
+
+  std::string breadcrumbStorage;
+  llvm::raw_string_ostream os(breadcrumbStorage);
+  prettyPrintOpBreadcrumb(op, os);
+  IREE_TRACE_ZONE_APPEND_TEXT(z0, os.str().data());
 }
 void PassTracing::runAfterPass(Pass *pass, Operation *op) {
   IREE_TRACE_ZONE_END(passTraceZonesStack.back());
