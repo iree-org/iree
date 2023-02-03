f1beb06b25fc24d2893a870ea7a2a83d3096a015
harsh <harsh@nod-labs.com>
2023-02-03 19:36:09 +0000

    Add promote operands op to transform dialect
    This op promotes the given target's operands specified by
    the indices.

--- compiler/src/iree/compiler/Codegen/LLVMGPU/TransformExtensions/LLVMGPUExtensions.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/TransformExtensions/LLVMGPUExtensions.cpp
@@ -11,6 +11,7 @@
 #include "iree/compiler/Dialect/HAL/IR/HALOps.h"
 #include "mlir/Conversion/VectorToGPU/VectorToGPU.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Bufferization/IR/Bufferization.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/GPU/IR/GPUDialect.h"
 #include "mlir/Dialect/GPU/TransformOps/GPUTransformOps.h"
@@ -652,6 +653,34 @@ transform_dialect::VectorToMMAConversionOp::applyToOne(
   convertVectorToMMAOps(target);
 
   results.push_back(target);
+  return DiagnosedSilenceableFailure::success();
+}
+
+DiagnosedSilenceableFailure transform_dialect::PromoteOperandsOp::applyToOne(
+    Operation *target, transform::ApplyToEachResultList &results,
+    transform::TransformState &state) {
+  OpBuilder builder(getContext());
+  OpBuilder::InsertionGuard g(builder);
+  builder.setInsertionPoint(target);
+  SmallVector<int64_t> indices = llvm::to_vector(getIndices());
+  auto numOperands = target->getNumOperands();
+  bufferization::BufferizationOptions options;
+  for (int64_t index : indices) {
+    if ((index >= 0) && (index < numOperands)) {
+      FailureOr<Value> ret = bufferization::allocateTensorForShapedValue(
+          builder, target->getLoc(), target->getOperand(index), false, options,
+          true);
+      if (failed(ret)) {
+        target->emitOpError("failed to promote operand");
+        return emitDefaultDefiniteFailure(target);
+      }
+      target->setOperand(index, ret.value());
+      results.push_back(ret.value().getDefiningOp());
+    } else {
+      target->emitOpError("invalid index specified");
+      return emitDefaultDefiniteFailure(target);
+    }
+  }
   return DiagnosedSilenceableFailure::success();
 }
 
