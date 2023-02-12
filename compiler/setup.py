ed105519431aaca11afb85a29d6fddd54e3999b5
powderluv <powderluv@users.noreply.github.com>
2023-02-11 23:01:43 -0800

    Add "Programming Language :: Python :: 3.11" classifier (#12163)
    Add setup.py classifiers for Python 3.11

--- compiler/setup.py
+++ compiler/setup.py
@@ -389,6 +389,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     ext_modules=[
         CMakeExtension("iree.compiler._mlir_libs._mlir"),
