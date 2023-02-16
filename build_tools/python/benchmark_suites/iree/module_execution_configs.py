9a8bc68a49cccda71b937660fa931e8eb81658b2
Jerry Wu <cheyuw@google.com>
2023-02-16 20:51:46 +0000

    Revert tags

--- build_tools/python/benchmark_suites/iree/module_execution_configs.py
+++ build_tools/python/benchmark_suites/iree/module_execution_configs.py
@@ -29,32 +29,32 @@ def _with_caching_allocator(
 
 ELF_LOCAL_SYNC_CONFIG = _with_caching_allocator(
     id=unique_ids.IREE_MODULE_EXECUTION_CONFIG_LOCAL_SYNC,
-    tags=["full-inference", "default-flags", "caching-allocator"],
+    tags=["full-inference", "default-flags"],
     loader=iree_definitions.RuntimeLoader.EMBEDDED_ELF,
     driver=iree_definitions.RuntimeDriver.LOCAL_SYNC)
 
 CUDA_CONFIG = _with_caching_allocator(
     id=unique_ids.IREE_MODULE_EXECUTION_CONFIG_CUDA,
-    tags=["full-inference", "default-flags", "caching-allocator"],
+    tags=["full-inference", "default-flags"],
     loader=iree_definitions.RuntimeLoader.NONE,
     driver=iree_definitions.RuntimeDriver.CUDA)
 
 VULKAN_CONFIG = _with_caching_allocator(
     id=unique_ids.IREE_MODULE_EXECUTION_CONFIG_VULKAN,
-    tags=["full-inference", "default-flags", "caching-allocator"],
+    tags=["full-inference", "default-flags"],
     loader=iree_definitions.RuntimeLoader.NONE,
     driver=iree_definitions.RuntimeDriver.VULKAN)
 
 VULKAN_BATCH_SIZE_16_CONFIG = _with_caching_allocator(
     id=unique_ids.IREE_MODULE_EXECUTION_CONFIG_VULKAN_BATCH_SIZE_16,
-    tags=["full-inference", "experimental-flags", "caching-allocator"],
+    tags=["full-inference", "experimental-flags"],
     loader=iree_definitions.RuntimeLoader.NONE,
     driver=iree_definitions.RuntimeDriver.VULKAN,
     extra_flags=["--batch_size=16"])
 
 VULKAN_BATCH_SIZE_32_CONFIG = _with_caching_allocator(
     id=unique_ids.IREE_MODULE_EXECUTION_CONFIG_VULKAN_BATCH_SIZE_32,
-    tags=["full-inference", "experimental-flags", "caching-allocator"],
+    tags=["full-inference", "experimental-flags"],
     loader=iree_definitions.RuntimeLoader.NONE,
     driver=iree_definitions.RuntimeDriver.VULKAN,
     extra_flags=["--batch_size=32"])
@@ -64,10 +64,7 @@ def get_elf_local_task_config(thread_num: int):
   config_id = f"{unique_ids.IREE_MODULE_EXECUTION_CONFIG_LOCAL_TASK_BASE}-{thread_num}"
   return _with_caching_allocator(
       id=config_id,
-      tags=[
-          f"{thread_num}-thread", "full-inference", "default-flags",
-          "caching-allocator"
-      ],
+      tags=[f"{thread_num}-thread", "full-inference", "default-flags"],
       loader=iree_definitions.RuntimeLoader.EMBEDDED_ELF,
       driver=iree_definitions.RuntimeDriver.LOCAL_TASK,
       extra_flags=[f"--task_topology_group_count={thread_num}"])
@@ -77,10 +74,7 @@ def get_vmvx_local_task_config(thread_num: int):
   config_id = f"{unique_ids.IREE_MODULE_EXECUTION_CONFIG_VMVX_LOCAL_TASK_BASE}-{thread_num}"
   return _with_caching_allocator(
       id=config_id,
-      tags=[
-          f"{thread_num}-thread", "full-inference", "default-flags",
-          "caching-allocator"
-      ],
+      tags=[f"{thread_num}-thread", "full-inference", "default-flags"],
       loader=iree_definitions.RuntimeLoader.VMVX_MODULE,
       driver=iree_definitions.RuntimeDriver.LOCAL_TASK,
       extra_flags=[f"--task_topology_group_count={thread_num}"])
