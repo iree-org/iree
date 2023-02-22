cbfec79733b41d53a5ce3ef2dfc66e86c6b318df
Jerry Wu <cheyuw@google.com>
2023-02-22 05:28:17 +0000

    Test notice

--- build_tools/github_actions/wait_ci.py
+++ build_tools/github_actions/wait_ci.py
@@ -10,10 +10,29 @@
 import os
 import requests
 
+GITHUB_IREE_API_PREFIX = "https://api.github.com/repos/openxla/iree"
+GITHUB_API_VERSION = "2022-11-28"
+
+NEEDS_CI_JOB = {"build_all", "build_e2e_test_artifacts"}
+
 
 def main():
   github_token = os.environ["GITHUB_TOKEN"]
   commit_sha = os.environ["GITHUB_SHA"]
+
+  session = requests.session()
+  api_headers = {
+      "Accept": "application/vnd.github+json",
+      "Authorization": f"token {github_token}",
+      "X-GitHub-Api-Version": GITHUB_API_VERSION,
+  }
+  resp = session.get(
+      f"{GITHUB_IREE_API_PREFIX}/commits/{commit_sha}/check-runs",
+      headers=api_headers)
+  needs_runs = [
+      run for run in resp.json()["check_runs"] if run["name"] in NEEDS_CI_JOB
+  ]
+  print(needs_runs)
   print("Done")
 
 
