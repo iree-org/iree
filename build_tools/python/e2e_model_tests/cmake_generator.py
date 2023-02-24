79c172f4377cfb0c75d8bcf090f0fdd8a76c0aa3
Jerry Wu <cheyuw@google.com>
2023-02-24 23:31:06 +0000

    Fix e2e tests

--- build_tools/python/e2e_model_tests/cmake_generator.py
+++ build_tools/python/e2e_model_tests/cmake_generator.py
@@ -18,13 +18,13 @@ def generate_rules() -> List[str]:
   for platform in test_definitions.CMakePlatform:
     cmake_rule = cmake_builder.rules.build_set(
         variable_name=f"IREE_MODULE_COMPILE_CONFIG_ID_{platform.value.upper()}",
-        value=f'"{test_definitions.PLATFORM_COMPILE_CONFIG_MAP[platform].id}"')
+        value=f'"{test_definitions.PLATFORM_COMPILE_CONFIG_MAP[platform]}"')
     cmake_rules.append(cmake_rule)
 
   for test_config in test_definitions.TEST_CONFIGS:
-    model = test_config.model
+    imported_model = test_config.imported_model
     runner_args = run_module_utils.build_run_flags_for_model(
-        model=model,
+        model=imported_model.model,
         model_input_data=test_config.input_data) + test_config.extra_test_flags
     # TODO(#11136): Currently the DRIVER is a separate field in the CMake rule (
     # and has effect on test labels). Rules should be generated in another way
@@ -33,7 +33,7 @@ def generate_rules() -> List[str]:
         test_config.execution_config, with_driver=False)
     cmake_rule = cmake_builder.rules.build_iree_benchmark_suite_module_test(
         target_name=test_config.name,
-        model=f"{model.id}_{model.name}",
+        model=f"{imported_model}",
         driver=test_config.execution_config.driver.value,
         expected_output=test_config.expected_output,
         runner_args=runner_args,
--- tests/e2e/models/generated_e2e_model_tests.cmake
+++ tests/e2e/models/generated_e2e_model_tests.cmake
@@ -5,26 +5,26 @@
 ################################################################################
 
 set(IREE_MODULE_COMPILE_CONFIG_ID_ANDROID-ARM64-V8A
-  "1f2adf49-282e-4aff-9d4f-e63b1621f1e8"
+  "[cpu-armv8.2-a-generic-linux-android29-llvm-cpu][default-flags]"
 )
 
 set(IREE_MODULE_COMPILE_CONFIG_ID_RISCV32-LINUX
-  "6d9ce240-ec14-4d8f-a8e4-1b20aa17b4e4"
+  "[cpu-riscv_32-generic-linux-gnu-llvm-cpu][default-flags]"
 )
 
 set(IREE_MODULE_COMPILE_CONFIG_ID_RISCV64-LINUX
-  "cdf579a9-5446-403b-a991-802a6c702e65"
+  "[cpu-riscv_64-generic-linux-gnu-llvm-cpu][default-flags]"
 )
 
 set(IREE_MODULE_COMPILE_CONFIG_ID_X86_64
-  "e7e18b0f-c72d-4f1c-89b1-5afee70df6e9"
+  "[cpu-x86_64-cascadelake-linux-gnu-llvm-cpu][default-flags]"
 )
 
 iree_benchmark_suite_module_test(
   NAME
     "mobilenet_v1_fp32_correctness_test"
   MODEL
-    "78eab9e5-9ff1-4769-9b55-933c81cc9a0f_MobileNetV1_fp32"
+    "MobileNetV1_fp32[fp32,imagenet][tosa]"
   DRIVER
     "local-sync"
   EXPECTED_OUTPUT
@@ -42,7 +42,7 @@ iree_benchmark_suite_module_test(
   NAME
     "efficientnet_int8_correctness_test"
   MODEL
-    "4a6f545e-1b4e-41a5-9236-792aa578184b_EfficientNet_int8"
+    "EfficientNet_int8[int8][tosa]"
   DRIVER
     "local-sync"
   EXPECTED_OUTPUT
@@ -59,7 +59,7 @@ iree_benchmark_suite_module_test(
   NAME
     "deeplab_v3_fp32_correctness_test"
   MODEL
-    "c36c63b0-220a-4d78-8ade-c45ce47d89d3_DeepLabV3_fp32"
+    "DeepLabV3_fp32[fp32][tosa]"
   DRIVER
     "local-sync"
   EXPECTED_OUTPUT
@@ -77,7 +77,7 @@ iree_benchmark_suite_module_test(
   NAME
     "person_detect_int8_correctness_test"
   MODEL
-    "bc1338be-e3df-44fd-82e4-40ba9560a073_PersonDetect_int8"
+    "PersonDetect_int8[int8][tosa]"
   DRIVER
     "local-sync"
   EXPECTED_OUTPUT
