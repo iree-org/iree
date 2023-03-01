bb63885c7acd6a3983e3d64d4de43c23932b7459
Jakub Kuderski <kubak@google.com>
2023-03-01 18:02:36 -0500

    Revert "Work around test failures in `mobilenetv3_fake_weights` (#12445)"
    This reverts commit 51bdf0a9c58b1374f605f7aec3495919f94b2890.

--- compiler/src/iree/compiler/Codegen/LLVMGPU/KernelConfig.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/KernelConfig.cpp
@@ -37,11 +37,10 @@ llvm::cl::opt<std::string> clGPUCodegenTransformDialectFileName(
         "MLIR file containing a transform dialect specification to apply"),
     llvm::cl::init(""));
 
-// TODO(#12444): Re-enable once test failure are resolved.
 llvm::cl::opt<bool> clGPUEnableTransformDialectJit(
     "iree-codegen-llvmgpu-enable-transform-dialect-jit",
     llvm::cl::desc("enable the usage of the transform dialect JIT"),
-    llvm::cl::init(false));
+    llvm::cl::init(true));
 
 llvm::cl::opt<std::string> clGPUCodegenTransformDialectDebugPayloadTag(
     "iree-codegen-llvmgpu-transform-dialect-debug-payload-tag",
--- compiler/src/iree/compiler/Codegen/LLVMGPU/test/reduction_pipeline.mlir
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/test/reduction_pipeline.mlir
@@ -1,6 +1,4 @@
-// RUN: iree-opt --split-input-file \
-// RUN:   --pass-pipeline="builtin.module(hal.executable(hal.executable.variant(iree-llvmgpu-lower-executable-target)))" \
-// RUN:   --iree-codegen-llvmgpu-enable-transform-dialect-jit %s | FileCheck %s
+// RUN: iree-opt --split-input-file --pass-pipeline="builtin.module(hal.executable(hal.executable.variant(iree-llvmgpu-lower-executable-target)))" %s | FileCheck %s
 
 #pipeline_layout = #hal.pipeline.layout<push_constants = 0, sets = [
   #hal.descriptor_set.layout<0, bindings = [
