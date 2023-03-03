bc2b44ebd731550f280b1098407e8e2d68892dbc
Daniel Ellis <1346302+dellis23@users.noreply.github.com>
2023-03-03 15:47:17 +0000

    Replace internal TF function names

--- integrations/tensorflow/iree_tf_compiler/iree-import-tf-main.py
+++ integrations/tensorflow/iree_tf_compiler/iree-import-tf-main.py
@@ -1,3 +1,5 @@
+import re
+
 import tensorflow as tf
 from tensorflow.python import pywrap_mlir
 from pathlib import Path
@@ -5,6 +7,12 @@ from pathlib import Path
 def convert_to_hlo(model_path: str, use_stablehlo=True):
   result = pywrap_mlir.experimental_convert_saved_model_to_mlir(
       model_path, "", show_debug_info=False)
+
+  # See:
+  # * https://github.com/tensorflow/tensorflow/issues/59685
+  # * https://github.com/tensorflow/tensorflow/blob/cd5667ec9d8af787a18c5b1ae239f060e9fa6fdd/tensorflow/python/saved_model/function_deserialization.py#L641-L653
+  result = re.sub(r"__inference_(.*)_\d+", r"\1", result)
+
   pipeline = ["tf-lower-to-mlprogram-and-hlo"]
   if not use_stablehlo:
     pipeline.append("stablehlo-legalize-to-hlo")
