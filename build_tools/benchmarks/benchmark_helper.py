d4cc2b851fa434379878e1140a0e433224b628e9
Che-Yu Wu <cheyuw@google.com>
2023-03-06 21:14:27 +0000

    Add benchmark helper tool

---
+++ build_tools/benchmarks/benchmark_helper.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+# Copyright 2023 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+"""Miscellaneous tool to help work with benchmark suite and benchmark CI."""
+
+import pathlib
+import sys
+
+# Add build_tools python dir to the search path.
+sys.path.insert(0, str(pathlib.Path(__file__).parent.with_name("python")))
+
+import argparse
+import json
+from typing import List
+
+from benchmark_suites.iree import export_definitions
+from e2e_test_framework import serialization
+from e2e_test_artifacts import iree_artifacts
+from e2e_test_framework.definitions import iree_definitions
+
+
+def _dump_flags_of_generation_config(
+    module_generation_config: iree_definitions.ModuleGenerationConfig):
+  imported_model_path = iree_artifacts.get_imported_model_path(
+      imported_model=module_generation_config.imported_model)
+  return {
+      "composite_id":
+          module_generation_config.composite_id,
+      "compile_flags":
+          module_generation_config.materialize_compile_flags() +
+          [str(imported_model_path)]
+  }
+
+
+def _dump_flags_from_run_config(
+    e2e_model_run_config: iree_definitions.E2EModelRunConfig):
+  gen_config = e2e_model_run_config.module_generation_config
+  module_path = iree_artifacts.get_module_dir_path(
+      module_generation_config=gen_config) / iree_artifacts.MODULE_FILENAME
+  return {
+      "composite_id":
+          e2e_model_run_config.composite_id,
+      "run_flags":
+          e2e_model_run_config.materialize_run_flags() +
+          [f"--module={module_path}"],
+      "module_generation_config":
+          _dump_flags_of_generation_config(module_generation_config=gen_config)
+  }
+
+
+def _dump_flags_handler(args: argparse.Namespace):
+  dump_configs = {}
+  if args.execution_benchmark_config is not None:
+    benchmark_groups = json.loads(args.execution_benchmark_config.read_text())
+    for target_device, group in benchmark_groups.items():
+      benchmark_group = export_definitions.ExecutionBenchmarkGroup(**group)
+      run_configs = serialization.unpack_and_deserialize(
+          data=benchmark_group.run_configs,
+          root_type=List[iree_definitions.E2EModelRunConfig])
+      dump_configs[target_device] = dict(
+          (run_config.composite_id,
+           _dump_flags_from_run_config(e2e_model_run_config=run_config))
+          for run_config in run_configs)
+
+  elif args.compilation_benchmark_config is not None:
+    benchmark_group = export_definitions.CompilationBenchmarkGroup(
+        **json.loads(args.compilation_benchmark_config.read_text()))
+    gen_configs = serialization.unpack_and_deserialize(
+        data=benchmark_group.generation_configs,
+        root_type=List[iree_definitions.ModuleGenerationConfig])
+    dump_configs = dict(
+        (gen_config.composite_id,
+         _dump_flags_of_generation_config(module_generation_config=gen_config))
+        for gen_config in gen_configs)
+
+  else:
+    raise AssertionError("No benchmark config is set.")
+
+  print(json.dumps(dump_configs, indent=2))
+
+
+def _parse_arguments() -> argparse.Namespace:
+  parser = argparse.ArgumentParser(
+      description=
+      "Miscellaneous tool to help work with benchmark suite and benchmark CI.")
+
+  subparser = parser.add_subparsers(required=True, title="operation")
+  dump_flags_parser = subparser.add_parser(
+      "dump-flags",
+      help="Dump the flags to compile and run benchmarks manually.")
+  dump_flags_input_parser = dump_flags_parser.add_mutually_exclusive_group(
+      required=True)
+  dump_flags_input_parser.add_argument(
+      "--execution_benchmark_config",
+      type=pathlib.Path,
+      help="Config file exported from export_benchmark_config.py execution")
+  dump_flags_input_parser.add_argument(
+      "--compilation_benchmark_config",
+      type=pathlib.Path,
+      help="Config file exported from export_benchmark_config.py compilation")
+  dump_flags_parser.set_defaults(handler=_dump_flags_handler)
+
+  return parser.parse_args()
+
+
+def main(args: argparse.Namespace):
+  args.handler(args)
+
+
+if __name__ == "__main__":
+  main(_parse_arguments())
