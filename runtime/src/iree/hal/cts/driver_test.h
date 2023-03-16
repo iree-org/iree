c213d358593e1f7a0bd9abbf6837d35f5b2a1521
Lei Zhang <antiagainst@google.com>
2023-03-16 19:56:39 +0000

    Address comments

--- runtime/src/iree/hal/cts/driver_test.h
+++ runtime/src/iree/hal/cts/driver_test.h
@@ -20,7 +20,31 @@ namespace iree {
 namespace hal {
 namespace cts {
 
-class driver_test : public CtsTestBase {};
+class driver_test : public CtsTestBase {
+ protected:
+  void CheckCreateDeviceViaPath(iree_string_view_t name,
+                                iree_string_view_t path) {
+    std::cout << "  Creating device '" << std::string(name.data, name.size)
+              << "' with path '" << std::string(path.data, path.size) << "'\n";
+    iree_hal_device_t* device = NULL;
+    iree_status_t status = iree_hal_driver_create_device_by_path(
+        driver_, name, path, /*param_count=*/0, /*params=*/NULL,
+        iree_allocator_system(), &device);
+
+    // Creation via path is HAL driver specific. Allow unimplemented cases.
+    if (iree_status_is_not_found(status)) {
+      iree_status_consume_code(status);
+      iree_hal_device_release(device);
+      return;
+    }
+
+    IREE_ASSERT_OK(status);
+    iree_string_view_t device_id = iree_hal_device_id(device);
+    std::cout << "    Created device with id: '"
+              << std::string(device_id.data, device_id.size) << "'\n";
+    iree_hal_device_release(device);
+  }
+};
 
 TEST_P(driver_test, QueryAndCreateAvailableDevicesByID) {
   iree_host_size_t device_info_count = 0;
@@ -33,7 +57,7 @@ TEST_P(driver_test, QueryAndCreateAvailableDevicesByID) {
     std::cout << "  Creating device '"
               << std::string(device_infos[i].name.data,
                              device_infos[i].name.size)
-              << "'..\n";
+              << "'\n";
     iree_hal_device_t* device = NULL;
     IREE_ASSERT_OK(iree_hal_driver_create_device_by_id(
         driver_, device_infos[i].device_id, /*param_count=*/0, /*params=*/NULL,
@@ -58,7 +82,7 @@ TEST_P(driver_test, QueryAndCreateAvailableDevicesByOrdinal) {
     std::cout << "  Creating device '"
               << std::string(device_infos[i].name.data,
                              device_infos[i].name.size)
-              << "'..\n";
+              << "'\n";
     iree_hal_device_t* device = NULL;
     IREE_ASSERT_OK(iree_hal_driver_create_device_by_ordinal(
         driver_, i, /*param_count=*/0, /*params=*/NULL, iree_allocator_system(),
@@ -79,43 +103,17 @@ TEST_P(driver_test, QueryAndCreateAvailableDevicesByPath) {
       driver_, iree_allocator_system(), &device_info_count, &device_infos));
 
   std::cout << "Driver has " << device_info_count << " device(s)\n";
+  if (device_info_count == 0) GTEST_SKIP() << "No available devices";
 
-  if (device_info_count > 0) {
-    iree_string_view_t name = device_infos[0].name;
-    std::cout << "  Creating device '" << std::string(name.data, name.size)
-              << "' with empty path..\n";
-    iree_hal_device_t* device = NULL;
-    IREE_ASSERT_OK(iree_hal_driver_create_device_by_path(
-        driver_, name, iree_string_view_empty(),
-        /*param_count=*/0, /*params=*/NULL, iree_allocator_system(), &device));
-    iree_string_view_t device_id = iree_hal_device_id(device);
-    std::cout << "    Created device with id: '"
-              << std::string(device_id.data, device_id.size) << "'\n";
-    iree_hal_device_release(device);
-  }
+  // Check creation via empty path.
+  iree_string_view_t name = device_infos[0].name;
+  CheckCreateDeviceViaPath(device_infos[0].name, iree_string_view_empty());
 
-  // Creation via normal index or UUID path is HAL driver specific.
-  // Allow unimplemented cases for these.
-
+  // Check creation via index path.
   for (iree_host_size_t i = 0; i < device_info_count; ++i) {
-    iree_string_view_t name = device_infos[i].name;
-    std::cout << "  Creating device '" << std::string(name.data, name.size)
-              << "' with index #" << i << "..\n";
-    iree_hal_device_t* device = NULL;
     char index[8];
     snprintf(index, 8, "%d", i);
-    iree_status_t status = iree_hal_driver_create_device_by_path(
-        driver_, name, IREE_SV(index), /*param_count=*/0, /*params=*/NULL,
-        iree_allocator_system(), &device);
-    if (iree_status_is_not_found(status)) {
-      iree_status_consume_code(status);
-    } else {
-      IREE_ASSERT_OK(status);
-      iree_string_view_t device_id = iree_hal_device_id(device);
-      std::cout << "    Created device with id: '"
-                << std::string(device_id.data, device_id.size) << "'\n";
-    }
-    iree_hal_device_release(device);
+    CheckCreateDeviceViaPath(device_infos[i].name, IREE_SV(index));
   }
 
   iree_allocator_free(iree_allocator_system(), device_infos);
