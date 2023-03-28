788f6d551bd82961ed33aacd95a3a4677db81936
bjacob <benoitjacob@google.com>
2023-03-28 10:09:27 -0400

    Ukernel cleanups (standard CPU feature sets, consistently including config.h files) (#12790)

--- runtime/src/iree/builtins/ukernel/arch/x86_64/mmt4d_x86_64.c
+++ runtime/src/iree/builtins/ukernel/arch/x86_64/mmt4d_x86_64.c
@@ -1,4 +1,4 @@
-// Copyright 2022 The IREE Authors
+// Copyright 2023 The IREE Authors
 //
 // Licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
