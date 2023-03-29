2e8788d9db3e472bf0140407ccdc6e3db1998a4d
Daniel Ellis <1346302+dellis23@users.noreply.github.com>
2023-03-29 17:20:20 +0000

    Migrate contents of compiler tool to iree_import_tf script.

--- integrations/tensorflow/python_projects/iree_tf/iree/tools/tf/scripts/iree_import_tf/__main__.py
+++ integrations/tensorflow/python_projects/iree_tf/iree/tools/tf/scripts/iree_import_tf/__main__.py
@@ -3,20 +3,26 @@
 # Licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+import argparse
 
-import subprocess
-import sys
-
-import iree.tools.tf
+import iree.compiler.tf
+from pathlib import Path
 
 
-def main(args=None):
-  if args is None:
-    args = sys.argv[1:]
-  # BEFORE SUBMIT: Add optional arg to use old tool
-  exe = iree.tools.tf.get_tool("iree-import-tf")
-  return subprocess.call(args=[exe] + args)
+parser = argparse.ArgumentParser()
+parser.add_argument('-p',
+                    '--saved_model_path',
+                    dest='saved_model_path',
+                    required=True,
+                    help='Path to the saved model directory to import.')
+parser.add_argument('-o',
+                    '--output_path',
+                    dest='output_path',
+                    required=True,
+                    help='Path to the mlir file name to output.')
+args = parser.parse_args()
 
 
 if __name__ == "__main__":
-  sys.exit(main())
+  Path(args.output_path).write_text(
+    iree.compiler.tf.get_mlir(args.saved_model_path))
