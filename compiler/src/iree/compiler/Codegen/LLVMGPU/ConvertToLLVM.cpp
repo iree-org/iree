774079f927c410beb219761f75bd2225969e5ec3
Thomas Raoux <thomasraoux@google.com>
2023-03-29 17:42:53 +0000

    [LLVMGPU] Allow complex as buffer element type.
    Complex are now lowered later and can be buffer element types therefore
    relax the verification.

--- compiler/src/iree/compiler/Codegen/LLVMGPU/ConvertToLLVM.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/ConvertToLLVM.cpp
@@ -25,25 +25,6 @@
 namespace mlir {
 namespace iree_compiler {
 
-LogicalResult verifyLLVMConversionCompatibility(ModuleOp moduleOp) {
-  LogicalResult compatible = success();
-
-  moduleOp.walk([&](IREE::HAL::InterfaceBindingSubspanOp subspanOp) {
-    auto memrefType = subspanOp.getType().dyn_cast<MemRefType>();
-    if (memrefType) {
-      Type elType = memrefType.getElementType();
-      if (!elType.isa<FloatType, IntegerType>()) {
-        subspanOp.emitError()
-            << "only integer and floating point element types "
-               "are supported at interface boundary";
-        compatible = failure();
-      }
-    }
-  });
-
-  return compatible;
-}
-
 void ConvertToDynamicSharedMemory(ModuleOp moduleOp) {
   SymbolTableCollection symbolTableCollection;
   // Collect all the adressOfOps to static shared memory globals.
