d7e26d804e72b4437b9b7e2ed110ef25e6302a87
mariecwhite <mariewhite@google.com>
2023-03-30 07:33:17 +1100

    Reconfigure x86 CPU benchmarks (#12816)
    Reconfigures x86 CPU benchmarks to:
    1. Benchmark with threads 1 and 8 for small workloads (models <10M
    params).
    2. Benchmark with threads 1, 8 and 13 threads for large workloads
    (models >= 10M params or large batch size). We use 13 because a
    c2-standard-60 machine will be used which has 15 cores and MAX_CORES - 2
    has empirically shown to be an optimum configuration.
    
    Before this change, configurations used sync, thread 1, 4, and 8 for all
    models.
    
    x86 benchmarks completed in 21 mins (from 30 mins).

--- tests/e2e/test_artifacts/generated_e2e_test_fetch_models.cmake
+++ tests/e2e/test_artifacts/generated_e2e_test_fetch_models.cmake
@@ -120,26 +120,6 @@ iree_fetch_artifact(
 
 iree_fetch_artifact(
   NAME
-    "model-39d157ad-f0ec-4a76-963b-d783beaed60f"
-  SOURCE_URL
-    "https://storage.googleapis.com/iree-model-artifacts/bert-for-masked-lm-seq512-tf-model.tar.gz"
-  OUTPUT
-    "${ROOT_ARTIFACTS_DIR}/model_39d157ad-f0ec-4a76-963b-d783beaed60f_BertForMaskedLMTF"
-  UNPACK
-)
-
-iree_fetch_artifact(
-  NAME
-    "model-8871f602-571c-4eb8-b94d-554cc8ceec5a"
-  SOURCE_URL
-    "https://storage.googleapis.com/iree-model-artifacts/bert-large-seq384-tf-model.tar.gz"
-  OUTPUT
-    "${ROOT_ARTIFACTS_DIR}/model_8871f602-571c-4eb8-b94d-554cc8ceec5a_BertLargeTF"
-  UNPACK
-)
-
-iree_fetch_artifact(
-  NAME
     "model-ebe7897f-5613-435b-a330-3cb967704e5e"
   SOURCE_URL
     "https://storage.googleapis.com/iree-model-artifacts/efficientnet-v2-s-tf-model.tar.gz"
@@ -155,6 +135,26 @@ iree_fetch_artifact(
     "https://storage.googleapis.com/iree-model-artifacts/minilm-l12-h384-uncased-seqlen128-tf-model.tar.gz"
   OUTPUT
     "${ROOT_ARTIFACTS_DIR}/model_ecf5c970-ee97-49f0-a4ed-df1f34e9d493_MiniLML12H384Uncased"
+  UNPACK
+)
+
+iree_fetch_artifact(
+  NAME
+    "model-39d157ad-f0ec-4a76-963b-d783beaed60f"
+  SOURCE_URL
+    "https://storage.googleapis.com/iree-model-artifacts/bert-for-masked-lm-seq512-tf-model.tar.gz"
+  OUTPUT
+    "${ROOT_ARTIFACTS_DIR}/model_39d157ad-f0ec-4a76-963b-d783beaed60f_BertForMaskedLMTF"
+  UNPACK
+)
+
+iree_fetch_artifact(
+  NAME
+    "model-8871f602-571c-4eb8-b94d-554cc8ceec5a"
+  SOURCE_URL
+    "https://storage.googleapis.com/iree-model-artifacts/bert-large-seq384-tf-model.tar.gz"
+  OUTPUT
+    "${ROOT_ARTIFACTS_DIR}/model_8871f602-571c-4eb8-b94d-554cc8ceec5a_BertLargeTF"
   UNPACK
 )
 
