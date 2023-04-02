e7f98e28db7617bc043a3241a02a06550d04f42b
Lei Zhang <antiagainst@google.com>
2023-04-02 18:47:21 -0400

    [ci] Adjust CPU benchmark flagging thresholds (#12881)
    CPU benchmark reported numbers have stability issues and that's unlikely
    to be addressed anytime soon. For now adjust the flagging threshold to
    avoid being noisy.
    
    Fixes https://github.com/openxla/iree/issues/12330

--- build_tools/benchmarks/common/benchmark_thresholds.py
+++ build_tools/benchmarks/common/benchmark_thresholds.py
@@ -40,35 +40,57 @@ class BenchmarkThreshold:
 # Order matters here: if multiple regexes match a single benchmark, the first
 # match is used.
 BENCHMARK_THRESHOLDS = [
-    # Fluctuating benchmarks on CPUs.
+    # Fluctuating benchmarks on ARM64 CPUs.
     BenchmarkThreshold(re.compile(r"^DeepLabV3.*big-core.*LLVM-CPU.* @ Pixel"),
                        20, ThresholdUnit.PERCENTAGE),
     BenchmarkThreshold(
-        re.compile(r"^MobileBertSquad.*big-core.*LLVM-CPU-Sync @ Pixel-4"), 50,
+        re.compile(r"^MobileBertSquad.*big-core.*LLVM-CPU-Sync @ Pixel-4"), 20,
         ThresholdUnit.PERCENTAGE),
-    BenchmarkThreshold(re.compile(r"^MobileNetV2.*LLVM-CPU @ Pixel"), 15,
+    BenchmarkThreshold(re.compile(r"^MobileNetV2.*LLVM-CPU.* @ Pixel"), 15,
                        ThresholdUnit.PERCENTAGE),
-    BenchmarkThreshold(
-        re.compile(r"^MobileNetV3Small.*LLVM-CPU-Sync @ Pixel-6"), 20,
-        ThresholdUnit.PERCENTAGE),
-    BenchmarkThreshold(
-        re.compile(r"^MobileNetV3Small.*big-core.*LLVM-CPU @ Pixel-6"), 20,
-        ThresholdUnit.PERCENTAGE),
-    BenchmarkThreshold(
-        re.compile(r"^MobileNetV3Small.*little-core.*LLVM-CPU @ Pixel"), 20,
-        ThresholdUnit.PERCENTAGE),
+    BenchmarkThreshold(re.compile(r"^MobileNetV3Small.*LLVM-CPU.* @ Pixel"), 25,
+                       ThresholdUnit.PERCENTAGE),
     BenchmarkThreshold(
         re.compile(r"^MobileSSD.*little-core.*LLVM-CPU.* @ Pixel-6"), 20,
         ThresholdUnit.PERCENTAGE),
-    BenchmarkThreshold(re.compile(r"^PoseNet.*big-core.*LLVM-CPU.* @ Pixel-6"),
-                       20, ThresholdUnit.PERCENTAGE),
+    BenchmarkThreshold(re.compile(r"^PoseNet.*big-core.*LLVM-CPU.* @ Pixel"),
+                       15, ThresholdUnit.PERCENTAGE),
 
-    # Fluctuating benchmarks on GPUs.
+    # Fluctuating benchmarks on X86_64 CPUs.
+    BenchmarkThreshold(re.compile(r"^BertForMaskedLMTF.*x86_64"), 10,
+                       ThresholdUnit.PERCENTAGE),
+    BenchmarkThreshold(re.compile(r"^BertLargeTF.*x86_64"), 10,
+                       ThresholdUnit.PERCENTAGE),
+    BenchmarkThreshold(re.compile(r"^MobileBertSquad_fp32.*x86_64"), 10,
+                       ThresholdUnit.PERCENTAGE),
+    BenchmarkThreshold(re.compile(r"^MobileSSD_fp32.*x86_64"), 10,
+                       ThresholdUnit.PERCENTAGE),
+    BenchmarkThreshold(re.compile(r"^Resnet50TF.*x86_64"), 10,
+                       ThresholdUnit.PERCENTAGE),
+
+    # Benchmarks that complete <= 10ms on X86_64 CPUs; using percentage is not
+    # suitable anymore.
+    BenchmarkThreshold(re.compile(r"^DeepLabV3_fp32.*x86_64"), 1 * 10**6,
+                       ThresholdUnit.VALUE_NS),
+    BenchmarkThreshold(re.compile(r"^EfficientNet_int8.*x86_64"), 1 * 10**6,
+                       ThresholdUnit.VALUE_NS),
+    BenchmarkThreshold(re.compile(r"^MobileNetV1_fp32.*x86_64"), 1 * 10**6,
+                       ThresholdUnit.VALUE_NS),
+    BenchmarkThreshold(re.compile(r"^MobileNetV2_fp32.*x86_64"), 2 * 10**6,
+                       ThresholdUnit.VALUE_NS),
+    BenchmarkThreshold(re.compile(r"^MobileNetV3Small_fp32.*x86_64"), 1 * 10**6,
+                       ThresholdUnit.VALUE_NS),
+    BenchmarkThreshold(re.compile(r"^PersonDetect_int8.*x86_64"), 5 * 10**5,
+                       ThresholdUnit.VALUE_NS),
+    BenchmarkThreshold(re.compile(r"^PoseNet_fp32.*x86_64"), 1 * 10**6,
+                       ThresholdUnit.VALUE_NS),
+
+    # Fluctuating benchmarks on mobile GPUs.
     BenchmarkThreshold(
         re.compile(r"^MobileNetV3Small.*full-inference.*GPU-Mali"), 2 * 10**6,
         ThresholdUnit.VALUE_NS),
 
-    # Benchmarks that complete around 10ms on GPUs; using percentage is not
+    # Benchmarks that complete <= 10ms on GPUs; using percentage is not
     # suitable anymore.
     BenchmarkThreshold(re.compile(r"^DeepLabV3.*GPU-Mali"), 1 * 10**6,
                        ThresholdUnit.VALUE_NS),
