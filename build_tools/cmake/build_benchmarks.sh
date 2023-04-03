23b879c509299fcdbde9b2de32d6da030b949db6
Daniel Ellis <1346302+dellis23@users.noreply.github.com>
2023-04-03 17:21:17 +0000

    Don't look for iree-import-tf on binaries path.

--- build_tools/cmake/build_benchmarks.sh
+++ build_tools/cmake/build_benchmarks.sh
@@ -38,7 +38,7 @@ echo "Configuring to build benchmarks"
   -DIREE_BUILD_SAMPLES=OFF \
   -DIREE_BUILD_TESTS=OFF \
   -DIREE_IMPORT_TFLITE_PATH="${IREE_TF_BINARIES_DIR}/iree-import-tflite" \
-  -DIREE_IMPORT_TF_PATH="${IREE_TF_BINARIES_DIR}/iree-import-tf"
+  -DIREE_IMPORT_TF_PATH="iree-import-tf"
 
 echo "Building benchmark artifacts"
 "${CMAKE_BIN}" \
