0d789084bc84d6d49117e115122c01d36684b61f
Daniel Ellis <1346302+dellis23@users.noreply.github.com>
2023-04-04 15:24:32 +0000

    Install compiler.

--- build_tools/cmake/build_benchmarks.sh
+++ build_tools/cmake/build_benchmarks.sh
@@ -27,6 +27,7 @@ IREE_TF_BINARIES_DIR="${IREE_TF_BINARIES_DIR:-integrations/tensorflow/bazel-bin/
 source build_tools/cmake/setup_build.sh
 
 # Install iree-import-tf
+pip install compiler/
 pip install integrations/tensorflow/python_projects/iree_tf
 
 # DEBUG: Where was it installed?
