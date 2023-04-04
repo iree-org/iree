696bd5ed01c77b7a0194905ed9bb3a2e79abe45e
mariecwhite <mariewhite@google.com>
2023-04-03 18:48:42 -0700

    Refine benchmark model groups

--- build_tools/python/e2e_test_framework/models/model_groups.py
+++ build_tools/python/e2e_test_framework/models/model_groups.py
@@ -7,35 +7,62 @@
 
 from e2e_test_framework.models import tf_models, tflite_models, torch_models
 
-# yapf: disable
-# Small models that require less computational resources.
-SMALL = [
-    tflite_models.DEEPLABV3_FP32,
-    tflite_models.EFFICIENTNET_INT8,
-    tflite_models.MOBILEBERT_FP16,
-    tflite_models.MOBILEBERT_FP32,
-    tflite_models.MOBILEBERT_INT8,
-    tflite_models.MOBILENET_V1,
-    tflite_models.MOBILENET_V2,
-    tflite_models.MOBILENET_V2_INT8,
-    tflite_models.MOBILENET_V3SMALL,
-    tflite_models.MOBILESSD_FP32,
-    tflite_models.PERSON_DETECT_INT8,
-    tflite_models.POSENET_FP32,
-    tf_models.EFFICIENTNET_V2_S_FP32,
-    tf_models.MINILM_L12_H384_UNCASED_INT32_SEQLEN128,
-    torch_models.EFFICIENTNET_V2_S_FP32_TORCH
-]
-# yapf: enable
 
-# Large models that require more computational resources.
-LARGE = [
-    tf_models.BERT_FOR_MASKED_LM_FP32_SEQLEN512,
-    tf_models.BERT_LARGE_TF_FP32_SEQLEN384,
-    tf_models.RESNET50_TF_FP32,
-    torch_models.MODEL_CLIP_TEXT_SEQLEN64_FP32_TORCH,
-    torch_models.MODEL_UNET_2D_FP32_TORCH,
-    torch_models.EFFICIENTNET_B7_FP32_TORCH,
+# A list of models with thread configurations.
+# Note `0` represents sync execution.
+x86_64_MODELS_AND_THREADS = [
+    # Tiny models.
+    (tflite_models.PERSON_DETECT_INT8, [0, 1]),
+    (tflite_models.MOBILENET_V3SMALL, [0, 1]),
+    # Small models.
+    (tflite_models.DEEPLABV3_FP32, [1, 8]),
+    (tflite_models.EFFICIENTNET_INT8, [1, 8]),
+    (tflite_models.MOBILENET_V1, [1, 8]),
+    (tflite_models.MOBILENET_V2, [1, 8]),
+    (tflite_models.MOBILENET_V2_INT8, [1, 8]),
+    (tflite_models.MOBILESSD_FP32, [1, 8]),
+    (tflite_models.POSENET_FP32, [1, 8]),
+    # Medium models.
+    # TODO: Add 13 threads once we move to new hardware.
+    (tflite_models.MOBILEBERT_FP16, [1, 8]),
+    (tflite_models.MOBILEBERT_FP32, [1, 8]),
+    (tflite_models.MOBILEBERT_INT8, [1, 8]),
+    (tf_models.EFFICIENTNET_V2_S_FP32, [1, 8]),
+    (tf_models.MINILM_L12_H384_UNCASED_INT32_SEQLEN128, [1, 8]),
+    (tf_models.RESNET50_TF_FP32, [1, 8]),
+    (torch_models.EFFICIENTNET_V2_S_FP32_TORCH, [1, 8]),
+    # Large models.
+    # TODO: These models should be running at 8, 13, 28 threads but we use 8 for now until new hardware becomes available.
+    (tf_models.BERT_FOR_MASKED_LM_FP32_SEQLEN512, [8]),
+    (tf_models.BERT_LARGE_TF_FP32_SEQLEN384, [8]),
+    (torch_models.EFFICIENTNET_B7_FP32_TORCH, [8]),
+]
+
+# A subset of `x86_64_MODELS_AND_THREADS`.
+x86_64_MODELS_AND_THREADS_EXPERIMENTAL = [
+    # Tiny models.
+    (tflite_models.PERSON_DETECT_INT8, [1]),
+    (tflite_models.MOBILENET_V3SMALL, [1]),
+    # Small models.
+    (tflite_models.DEEPLABV3_FP32, [8]),
+    (tflite_models.EFFICIENTNET_INT8, [8]),
+    (tflite_models.MOBILENET_V2, [8]),
+    (tflite_models.MOBILENET_V2_INT8, [8]),
+    (tflite_models.MOBILESSD_FP32, [8]),
+    (tflite_models.POSENET_FP32, [8]),
+    # Medium models.
+    (tflite_models.MOBILEBERT_FP32, [8]),
+    (tflite_models.MOBILEBERT_INT8, [8]),
+    (tf_models.EFFICIENTNET_V2_S_FP32, [8]),
+    (tf_models.MINILM_L12_H384_UNCASED_INT32_SEQLEN128, [8]),
+    # Disabled due to https://github.com/openxla/iree/issues/11174.
+    # (tf_models.RESNET50_TF_FP32, [8]),
+    # Disabled due to https://github.com/openxla/iree/issues/12772.
+    # (torch_models.EFFICIENTNET_V2_S_FP32_TORCH, [8]),
+    # Large models.
+    (tf_models.BERT_LARGE_TF_FP32_SEQLEN384, [8]),
+    # Disabled due to https://github.com/openxla/iree/issues/12772.
+    # (torch_models.EFFICIENTNET_B7_FP32_TORCH, [8]),
 ]
 
 # BERT-Large in various batch sizes.
@@ -62,4 +89,13 @@ RESNET50_TORCH_BATCHES = [
     #torch_models.RESNET50_2048X3X224X224_FP32_TORCH,
 ]
 
-ALL = SMALL + LARGE + BERT_LARGE_TORCH_BATCHES + RESNET50_TORCH_BATCHES
+CUDA_MODELS = BERT_LARGE_TORCH_BATCHES + RESNET50_TORCH_BATCHES + [
+    tf_models.EFFICIENTNET_V2_S_FP32,
+    tf_models.MINILM_L12_H384_UNCASED_INT32_SEQLEN128,
+    tf_models.RESNET50_TF_FP32,
+    tf_models.BERT_FOR_MASKED_LM_FP32_SEQLEN512,
+    tf_models.BERT_LARGE_TF_FP32_SEQLEN384,
+    torch_models.MODEL_CLIP_TEXT_SEQLEN64_FP32_TORCH,
+    torch_models.MODEL_UNET_2D_FP32_TORCH,
+    torch_models.EFFICIENTNET_B7_FP32_TORCH,
+]
