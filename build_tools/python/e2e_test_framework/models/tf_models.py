f2ac7a3d7988ddf3a3a87ff40cb50a4e146cd8c9
Daniel Ellis <1346302+dellis23@users.noreply.github.com>
2023-04-05 14:49:36 +0000

    Attempt to retrigger CI.

--- build_tools/python/e2e_test_framework/models/tf_models.py
+++ build_tools/python/e2e_test_framework/models/tf_models.py
@@ -4,6 +4,3 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 """Defines Tensorflow models."""
-
-from e2e_test_framework import unique_ids
-from e2e_test_framework.definitions import common_definitions
