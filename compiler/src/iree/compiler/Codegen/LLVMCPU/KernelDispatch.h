5309c604e309c7d3c249dbfeafb031a2cd4895c9
Hanhan Wang <hanchung@google.com>
2023-04-10 13:38:06 -0700

    [NFC] Update comments and var naming after CodegenStrategy deprecation.
    - Unify TilingLevel enum class.
    - Update comments about "Sandbox".

--- compiler/src/iree/compiler/Codegen/LLVMCPU/KernelDispatch.h
+++ compiler/src/iree/compiler/Codegen/LLVMCPU/KernelDispatch.h
@@ -13,30 +13,20 @@
 namespace mlir {
 namespace iree_compiler {
 
-enum class TilingLevel : unsigned {
-  // Tile linalg operations to threads.
-  WorkGroupTiles = 0,
-  // Tile linalg operation on workgroup thread into L1 block tiles.
-  L1Tiles = 1,
-  // Tile linalg operations on L1 block tiles into vector tiles.
-  VectorTiles = 2,
-  NumTileLevels = 3
-};
-
 // TODO(hanchung): Create a pass to handle detailed logic about splitting tiling
 // sizes for parallel dims and reduction dims.
 // We have to fuse the fill + named_op + generic ops along parallel dims
 // firstly. At this stage, we do not apply vectorization. The reduction dim
 // won't get tiled if the case is matmul + generic op. In this case, we have to
-// tile along reduction dim again, which needs them to be Linalg ops form.
-enum class StrategyTilingLevel : unsigned {
-  // Tile linalg operations to threads.
+// tile along reduction dim again, which needs them to be TilingInterface ops.
+enum class TilingLevel : unsigned {
+  // Tile TilingInterface operations to threads.
   WorkGroupTiles = 0,
-  // Tile linalg operation on workgroup thread for parallel dims.
+  // Tile TilingInterface operation on workgroup thread for parallel dims.
   ParallelTiles = 1,
-  // Tile linalg operations on workgroup thread for reduction dims.
+  // Tile TilingInterface operations on workgroup thread for reduction dims.
   ReductionTiles = 2,
-  NumStrategyTileLevels = 3
+  NumTileLevels = 3
 };
 
 LogicalResult initCPULaunchConfig(ModuleOp moduleOp);
