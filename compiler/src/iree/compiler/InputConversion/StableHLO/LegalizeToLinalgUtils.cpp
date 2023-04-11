ecd9d37efab4f23eab0361a51a4e62be246fbb92
Jakub Kuderski <kubak@google.com>
2023-04-11 09:27:28 -0400

    Update copyright headers

--- compiler/src/iree/compiler/InputConversion/StableHLO/LegalizeToLinalgUtils.cpp
+++ compiler/src/iree/compiler/InputConversion/StableHLO/LegalizeToLinalgUtils.cpp
@@ -1,5 +1,4 @@
-// Copyright 2022 The TensorFlow Authors. All Rights Reserved.
-// Copyright 2023 The IREE Authors
+// Copyright 2022 The IREE Authors
 //
 // Licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- compiler/src/iree/compiler/InputConversion/StableHLO/TypeConversion.cpp
+++ compiler/src/iree/compiler/InputConversion/StableHLO/TypeConversion.cpp
@@ -1,5 +1,4 @@
-// Copyright 2021 The TensorFlow Authors. All Rights Reserved.
-// Copyright 2023 The IREE Authors
+// Copyright 2021 The IREE Authors
 //
 // Licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
