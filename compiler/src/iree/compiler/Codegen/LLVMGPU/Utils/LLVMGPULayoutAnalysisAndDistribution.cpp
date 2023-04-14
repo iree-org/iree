5bdaa4f8290cd2c4c7bdb97efa3e31d029aafbc8
Harsh Menon <harsh@nod-labs.com>
2023-04-14 00:56:14 -0700

    Compute reduction across batch dims
    This existing code only computes the reduction across
    the vector dims. This patch fixes it to compute across
    the batch dims as well.

--- compiler/src/iree/compiler/Codegen/LLVMGPU/Utils/LLVMGPULayoutAnalysisAndDistribution.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/Utils/LLVMGPULayoutAnalysisAndDistribution.cpp
@@ -819,10 +819,11 @@ static void distributeReductionBroadcastTranspose(
 
       // Since this is a broadcasted tensor, we only need to extract the 0th
       // element
-      result = rewriter.create<vector::ExtractOp>(
-          loc, simdToSimtMap.at(acc),
-          SmallVector<int64_t>{state[DimType::Batch0], state[DimType::Batch1],
-                               vectorOffset, 0});
+      if (!result)
+        result = rewriter.create<vector::ExtractOp>(
+            loc, simdToSimtMap.at(acc),
+            SmallVector<int64_t>{state[DimType::Batch0], state[DimType::Batch1],
+                                 vectorOffset, 0});
 
       for (int i = 0; i < vShape[0]; i++) {
         Value v = rewriter.create<vector::ExtractOp>(loc, vector,
