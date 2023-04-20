6164b586858b18ce58466b19a134ba5083ea615c
Natasha Kononenko <natashaknk@google.com>
2023-04-20 23:50:31 +0000

    Split mhlo optimization_barrier lowering into several util.optimization_barrier ops if there are multiple inputs

--- compiler/src/iree/compiler/InputConversion/MHLO/MHLOToLinalgOnTensors.cpp
+++ compiler/src/iree/compiler/InputConversion/MHLO/MHLOToLinalgOnTensors.cpp
@@ -228,8 +228,11 @@ struct OptimizationBarrierOpConversion
   LogicalResult matchAndRewrite(
       mhlo::OptimizationBarrierOp op, OpAdaptor adaptor,
       ConversionPatternRewriter &rewriter) const override {
-    rewriter.replaceOpWithNewOp<IREE::Util::OptimizationBarrierOp>(
-        op, op.getOperands());
+    SmallVector<Value> outputs;
+    for (unsigned i = 0, e = adaptor.getOperands().size(); i < e; ++i) {
+      outputs.push_back(rewriter.create<IREE::Util::OptimizationBarrierOp>(op.getLoc(), adaptor.getOperands()[i]).getResult(0));
+    }
+    rewriter.replaceOp(op, outputs);
     return success();
   }
 };
