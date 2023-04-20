4880d0046ab2e4644aa83404069823f87ea23fc8
Jonathan DEKHTIAR <jdekhtiar@nvidia.com>
2023-04-19 23:39:11 -0400

    [Dev Container] - Adding VSCode Dev Container Capability (#12921)
    This PR aims to add support for VS Code development container -
    https://github.com/openxla/iree/issues/12907
    
    Full Documentation: .devcontainer/README.md
    
    ---------
    
    Co-authored-by: Scott Todd <scotttodd@google.com>
    Co-authored-by: Geoffrey Martin-Noble <gcmn@google.com>

--- build_tools/docker/get_image_name.py
+++ build_tools/docker/get_image_name.py
@@ -20,19 +20,24 @@ version without adding fully referencing sha256 hashes, etc.
 from pathlib import Path
 import sys
 
+
+def find_image_by_name(img_name):
+  this_dir = Path(__file__).resolve().parent
+
+  with open(this_dir / "prod_digests.txt", "rt") as f:
+    for line in f.readlines():
+      line = line.strip()
+      if line.startswith(f"gcr.io/iree-oss/{img_name}@"):
+        return line
+    else:
+      raise ValueError(
+          f"ERROR: Image name {img_name} not found in prod_digests.txt")
+
+
 if __name__ == "__main__":
   if len(sys.argv) != 2:
     print("ERROR: Expected image short name", file=sys.stderr)
     sys.exit(1)
   short_name = sys.argv[1]
-  this_dir = Path(__file__).resolve().parent
-  with open(this_dir / "prod_digests.txt", "rt") as f:
-    for line in f.readlines():
-      line = line.strip()
-      if line.startswith(f"gcr.io/iree-oss/{short_name}@"):
-        print(line)
-        break
-    else:
-      print(f"ERROR: Image name {short_name} not found in prod_digests.txt",
-            file=sys.stderr)
-      sys.exit(1)
+  image_name = find_image_by_name(short_name)
+  print(image_name)
