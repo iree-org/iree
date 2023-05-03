6020ab3b8d3349027d3c38a3ae8159bd5bfe2630
Jakub Kuderski <kubak@google.com>
2023-05-02 21:27:48 -0400

    [StableHLO] Add initial preprocessing passes (#13372)
    This the first batch of StableHLO-StableHLO preprocessing passes. It
    only covers the eninsum and batch_norm preprocessing patterns, ported
    from mlir-hlo. See the initial import for more context:
    https://github.com/openxla/iree/pull/12957.
    
    The changes compared to the mlir-hlo code are:
    -  Code was ported to work on StableHLO ops.
    - Test were ported to match against how stablehlo is printed. Unlike
    mhlo, `stablehlo.(dyanmic_)broadcast_in_dim` does not use generic
    syntax, and epsilon starts as a 0-d tensor constant that is immediately
    broadcast.
    - Slightly different code organization, inc., directories and
    `populate*Patterns` functions, to match the other StableHLO code.
    
    Issue: https://github.com/openxla/iree/issues/12678

---
+++ compiler/src/iree/compiler/InputConversion/StableHLO/Preprocessing/Rewriters.h
@@ -0,0 +1,28 @@
+// Copyright 2023 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef IREE_COMPILER_INPUTCONVERSION_STABLEHLO_PREPROCESSING_REWRITERS_H_
+#define IREE_COMPILER_INPUTCONVERSION_STABLEHLO_PREPROCESSING_REWRITERS_H_
+
+#include "mlir/Transforms/DialectConversion.h"
+
+namespace mlir::iree_compiler::stablehlo {
+
+//===----------------------------------------------------------------------===//
+// General StableHLO/CHLO preprocessing patterns.
+//===----------------------------------------------------------------------===//
+
+/// Collection of rewrite patterns for lowering of StableHLO dim operations.
+void populatePreprocessingEinsumToDotGeneralPatterns(
+    MLIRContext *context, RewritePatternSet *patterns);
+
+/// Collection of rewrite patterns to materialize 'batch_dimension' attributes.
+void populatePreprocessingUnfuseBatchNormPatterns(MLIRContext *context,
+                                                  RewritePatternSet *patterns);
+
+}  // namespace mlir::iree_compiler::stablehlo
+
+#endif  // IREE_COMPILER_INPUTCONVERSION_STABLEHLO_PREPROCESSING_REWRITERS_H_
