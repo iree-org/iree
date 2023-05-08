f7c36ae2b0d5376c865749796372009297b79074
mariecwhite <mariewhite@google.com>
2023-05-07 17:54:55 -0700

    Use lowered mlir instead of SavedModel for TF benchmarks

--- build_tools/python/e2e_test_artifacts/cmake_generator/model_rule_generator_test.py
+++ build_tools/python/e2e_test_artifacts/cmake_generator/model_rule_generator_test.py
@@ -27,8 +27,8 @@ class CommonGeneratorsTest(unittest.TestCase):
         id="5678",
         name="tf_m",
         tags=[],
-        source_type=common_definitions.ModelSourceType.EXPORTED_TF_V2,
-        source_url="https://example.com/xyz_saved_model",
+        source_type=common_definitions.ModelSourceType.EXPORTED_MHLO_MLIR,
+        source_url="https://example.com/xyz_mlirl",
         entry_function="predict",
         input_types=["2xf32"])
     root_path = pathlib.PurePath("model_root")
