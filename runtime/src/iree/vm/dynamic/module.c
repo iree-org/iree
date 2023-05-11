ef2bb52317eed3b90618a6c16616ae14e33b7803
Ben Vanik <ben.vanik@gmail.com>
2023-05-11 11:43:20 -0700

    Adding a VM implementation detail around expected import signatures. (#13562)
    This allows modules to snoop what importers are expecting the exporters
    to provide. This information is optional and not included in all cases
    as not all importers have the information available.

--- runtime/src/iree/vm/dynamic/module.c
+++ runtime/src/iree/vm/dynamic/module.c
@@ -126,10 +126,12 @@ static iree_status_t IREE_API_PTR iree_vm_dynamic_module_get_function_attr(
 
 static iree_status_t IREE_API_PTR iree_vm_dynamic_module_lookup_function(
     void* self, iree_vm_function_linkage_t linkage, iree_string_view_t name,
+    const iree_vm_function_signature_t* expected_signature,
     iree_vm_function_t* out_function) {
   iree_vm_dynamic_module_t* module = (iree_vm_dynamic_module_t*)self;
   IREE_RETURN_IF_ERROR(module->user_module->lookup_function(
-      module->user_module->self, linkage, name, out_function));
+      module->user_module->self, linkage, name, expected_signature,
+      out_function));
   out_function->module = (iree_vm_module_t*)self;
   return iree_ok_status();
 }
