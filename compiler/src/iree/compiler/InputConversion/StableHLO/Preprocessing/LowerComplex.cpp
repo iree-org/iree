451fa60e1a40b0c6330427f9b83864554e3b584f
Jakub Kuderski <kubak@google.com>
2023-05-11 10:20:12 -0400

    Port new mhlo complex lowering pattern

--- compiler/src/iree/compiler/InputConversion/StableHLO/Preprocessing/LowerComplex.cpp
+++ compiler/src/iree/compiler/InputConversion/StableHLO/Preprocessing/LowerComplex.cpp
@@ -20,6 +20,54 @@ namespace mlir::iree_compiler::stablehlo {
 #include "iree/compiler/InputConversion/StableHLO/Preprocessing/Passes.h.inc"
 
 namespace {
+
+struct ConvertComplexDot final : OpRewritePattern<mlir::stablehlo::DotOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  // Will decompose stablehlo::DotOp with complex parameters down to
+  // four Dot operations in the following fashion:
+  //   result.real = lhs.real <DOT> rhs.real - lhs.imag <DOT> rhs.imag
+  //   result.imag = lhs.imag <DOT> rhs.real + lhs.real <DOT> rhs.imag
+  //   result = complex(result.real, result.imag)
+  LogicalResult matchAndRewrite(mlir::stablehlo::DotOp dot,
+                                PatternRewriter &rewriter) const override {
+    ArrayAttr precision = dot.getPrecisionConfigAttr();
+    TypedValue<TensorType> lhs = dot.getLhs();
+    TypedValue<TensorType> rhs = dot.getRhs();
+    ShapedType lhsType = lhs.getType();
+    ShapedType rhsType = rhs.getType();
+    if (!isa<ComplexType>(lhsType.getElementType()) ||
+        !isa<ComplexType>(rhsType.getElementType())) {
+      return rewriter.notifyMatchFailure(dot, "lhs/rhs types are not complex");
+    }
+
+    Location loc = dot.getLoc();
+    Value lhsReal = rewriter.create<mlir::stablehlo::RealOp>(loc, lhs);
+    Value lhsImag = rewriter.create<mlir::stablehlo::ImagOp>(loc, lhs);
+    Value rhsReal = rewriter.create<mlir::stablehlo::RealOp>(loc, rhs);
+    Value rhsImag = rewriter.create<mlir::stablehlo::ImagOp>(loc, rhs);
+    TensorType resultType = dot.getType();
+    Type newType = mlir::hlo::createRealType(resultType);
+
+    Value realComponent = rewriter.create<mlir::stablehlo::SubtractOp>(
+        loc,
+        rewriter.create<mlir::stablehlo::DotOp>(loc, newType, lhsReal, rhsReal,
+                                                precision),
+        rewriter.create<mlir::stablehlo::DotOp>(loc, newType, lhsImag, rhsImag,
+                                                precision));
+    Value imagComponent = rewriter.create<mlir::stablehlo::AddOp>(
+        loc,
+        rewriter.create<mlir::stablehlo::DotOp>(loc, newType, lhsReal, rhsImag,
+                                                precision),
+        rewriter.create<mlir::stablehlo::DotOp>(loc, newType, lhsImag, rhsReal,
+                                                precision));
+    Value result = rewriter.create<mlir::stablehlo::ComplexOp>(
+        loc, resultType, realComponent, imagComponent);
+    rewriter.replaceOp(dot, result);
+    return success();
+  }
+};
+
 struct LowerComplex final : impl::LowerComplexBase<LowerComplex> {
   void runOnOperation() override {
     MLIRContext *ctx = &getContext();
@@ -69,8 +117,9 @@ namespace {
 #include "iree/compiler/InputConversion/StableHLO/Preprocessing/ComplexLoweringPatterns.h.inc"
 }  // end anonymous namespace
 
-void populatePreprocessingComplexPatterns(MLIRContext * /*context*/,
+void populatePreprocessingComplexPatterns(MLIRContext *context,
                                           RewritePatternSet *patterns) {
+  patterns->add<ConvertComplexDot>(context);
   populateWithGenerated(*patterns);
 }
 }  // namespace mlir::iree_compiler::stablehlo
