efb889bc99c04a675128e7eb24ca0e7f28584598
Julian Walker <julianreidwalker@gmail.com>
2023-05-17 19:26:06 -0700

    respond to code review

--- experimental/dispatch_profiler/profile_all.sh
+++ experimental/dispatch_profiler/profile_all.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Copyright 2022 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 # Sets up a venv suitable for running IREE Dispatch Profiler and executes
 # a suite of runs. This is invoked by a Github workflow and can be invoked
 # locally.
@@ -7,43 +14,37 @@
 #   sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 # Or launch with python=/some/path
 
+set -euo pipefail
+
 TD="$(cd $(dirname $0) && pwd)"
 
 PYTHON="${PYTHON:-python3}"
 DISPATCH_PROFILER_IREE_BIN_DIR="${1:-$TD/../../tools}"
 DISPATCH_PROFILER_OUTPUT_DIR="${2:-$TD/dispatch_profiler_output}"
 
-VENV_DIR="$TD/dispatch-profiler.venv"
+VENV_DIR="${TD}/dispatch-profiler.venv"
 
-echo "Setting up venv dir: $VENV_DIR"
-echo "Python: $PYTHON"
-echo "Python version: $("$PYTHON" --version)"
-echo "Dispatch Profiler IREE bin dir: $DISPATCH_PROFILER_IREE_BIN_DIR"
-echo "Dispatch Profiler output dir: $DISPATCH_PROFILER_OUTPUT_DIR"
+echo "Setting up venv dir: ${VENV_DIR}"
+echo "Python: ${PYTHON}"
+echo "Python version: $("${PYTHON}" --version)"
+echo "Dispatch Profiler IREE bin dir: ${DISPATCH_PROFILER_IREE_BIN_DIR}"
+echo "Dispatch Profiler output dir: ${DISPATCH_PROFILER_OUTPUT_DIR}"
 
-function die() {
-  echo "Error executing command: $*"
-  exit 1
-}
-
-$PYTHON -m venv "$VENV_DIR" || die "Could not create venv."
-source "$VENV_DIR/bin/activate" || die "Could not activate venv"
+${PYTHON} -m venv "${VENV_DIR}"
+source "${VENV_DIR}/bin/activate"
 
 # Upgrade pip and install requirements. 'python' is used here in order to
 # reference to the python executable from the venv.
-python -m pip install --upgrade pip || die "Could not upgrade pip"
-python -m pip install --upgrade -r "$TD/requirements.txt"
+python -m pip install --upgrade pip
+python -m pip install --upgrade -r "${TD}/requirements.txt"
 
 python "${TD}/generator.py" \
-  --generated-dir "${TD}" \
-  || die "Dispatch profiler failed to generate"
+  --generated-dir "${TD}"
 python "${TD}/compile.py" \
   --iree-bin-dir "${DISPATCH_PROFILER_IREE_BIN_DIR}" \
-  --generated-dir "${TD}" \
-  || die "Dispatch profiler failed to compile"
+  --generated-dir "${TD}"
 python "${TD}/profiler.py" \
   --iree-bin-dir "${DISPATCH_PROFILER_IREE_BIN_DIR}" \
   --generated-dir "${TD}" \
   --dispatches="matmul_3456x1024x2048_f16t_f16t_f16t_tile_config_128x128_32x5_tensorcore_mmasync,matmul_3456x1024x2048_f32t_f32t_f32t_tile_config_128x128_16x5_tensorcore_mmasync" \
-  --output "${DISPATCH_PROFILER_OUTPUT_DIR}/matmul_perf_tensor_core_a100.csv" \
-  || die "Dispatch profiler failed to profile"
+  --output "${DISPATCH_PROFILER_OUTPUT_DIR}/matmul_perf_tensor_core_a100.csv"
