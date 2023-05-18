01e34e985d3facce6cbd06df8b8639149f5e4143
Ben Vanik <ben.vanik@gmail.com>
2023-05-17 17:10:46 -0700

    Adding the inliner interface to the linalg_ext dialect.

--- llvm-external-projects/iree-dialects/lib/Dialect/LinalgExt/IR/LinalgExtDialect.cpp
+++ llvm-external-projects/iree-dialects/lib/Dialect/LinalgExt/IR/LinalgExtDialect.cpp
@@ -11,6 +11,7 @@
 #include "mlir/IR/DialectImplementation.h"
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/IR/OpImplementation.h"
+#include "mlir/Transforms/InliningUtils.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/TypeSwitch.h"
 #include "llvm/Support/SourceMgr.h"
@@ -23,9 +24,30 @@ using namespace mlir::iree_compiler::IREE::LinalgExt;
 #define GET_ATTRDEF_CLASSES
 #include "iree-dialects/Dialect/LinalgExt/IR/LinalgExtAttrs.cpp.inc" // IWYU pragma: keep
 
+// Used to control inlining behavior.
+struct IREELinalgExtInlinerInterface : public DialectInlinerInterface {
+  using DialectInlinerInterface::DialectInlinerInterface;
+
+  bool isLegalToInline(Operation *call, Operation *callable,
+                       bool wouldBeCloned) const final {
+    // Sure!
+    return true;
+  }
+  bool isLegalToInline(Region *dest, Region *src, bool wouldBeCloned,
+                       IRMapping &valueMapping) const final {
+    // Sure!
+    return true;
+  }
+
+  bool isLegalToInline(Operation *op, Region *dest, bool wouldBeCloned,
+                       IRMapping &valueMapping) const final {
+    // Sure!
+    return true;
+  }
+};
+
 void IREELinalgExtDialect::initialize() {
-  // TODO(hanchung): Add interface to the dialect.
-  // addInterfaces<IREEInlinerInterface>();
+  addInterfaces<IREELinalgExtInlinerInterface>();
 
   addAttributes<
 #define GET_ATTRDEF_LIST
