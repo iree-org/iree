4795765b831d56d326fdc6de07e73e74dc7e405c
Quinn Dawkins <quinn@nod-labs.com>
2023-05-18 23:42:51 -0400

    Only apply WMMA unrolling to elementwise ops of sufficient rank (#13627)
    In the future WMMA unrolling needs to somehow restrict unrolling to ops
    that are intended to be later lowered to the opaque WMMA types. For now
    check the rank of elementwise to prevent crashes when there are flat
    elementwise vector operations present.

--- compiler/src/iree/compiler/Codegen/Utils/GPUUtils.cpp
+++ compiler/src/iree/compiler/Codegen/Utils/GPUUtils.cpp
@@ -587,6 +587,25 @@ std::optional<SmallVector<int64_t>> getWmmaNativeVectorSize(Operation *op) {
   }
   if ((OpTrait::hasElementwiseMappableTraits(op) && op->getNumResults() == 1)) {
     if (auto vecType = op->getResultTypes()[0].dyn_cast<VectorType>()) {
+      // TODO: The condition for unrolling elementwise should be restricted
+      // only to operations that need unrolling (connected to the contract).
+      if (vecType.getRank() < 2) return std::nullopt;
+
+      // First check whether there is a slice to infer the shape from. This is
+      // required for cases where the accumulator type differs from the input
+      // types, in which case we will see an `arith.ext_` between the contract
+      // and transfer_read which needs to be unrolled.
+      VectorType sliceType;
+      for (Operation *users : op->getUsers()) {
+        auto extract = dyn_cast<vector::ExtractStridedSliceOp>(users);
+        if (!extract) return std::nullopt;
+        auto vecType = extract.getResult().getType().cast<VectorType>();
+        if (sliceType && sliceType != vecType) return std::nullopt;
+        sliceType = vecType;
+      }
+      if (sliceType) return llvm::to_vector(sliceType.getShape());
+
+      // Else unroll for trailing elementwise.
       SmallVector<int64_t> nativeSize(vecType.getRank() - 2, 1);
       // Map elementwise ops to the output shape.
       nativeSize.append({m, n});
