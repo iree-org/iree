d8e16ea7ab76c5c0da87948d1ffe40a6d0d9eee8
Che-Yu Wu <cheyuw@google.com>
2023-05-22 14:46:50 +0000

    Rename all to defaults

---
+++ build_tools/github_actions/configure_ci_test.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+# Copyright 2023 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import unittest
+
+import configure_ci
+
+SORTED_DEFAULT_BENCHMARK_PRESETS = ",".join([
+    "android-cpu",
+    "android-gpu",
+    "comp-stats",
+    "cuda",
+    "vulkan-nvidia",
+    "x86_64",
+])
+
+
+class GetBenchmarkPresetsTest(unittest.TestCase):
+
+  def test_get_benchmark_presets_from_pr_labels(self):
+    presets = configure_ci.get_benchmark_presets(
+        trailers={},
+        labels=["benchmarks:x86_64", "benchmarks:cuda"],
+        is_pr=True,
+        is_llvm_integrate_pr=False)
+
+    self.assertEqual(presets, "cuda,x86_64")
+
+  def test_get_benchmark_presets_from_trailers_and_labels(self):
+    presets = configure_ci.get_benchmark_presets(
+        trailers={"benchmark-extra": "android-cpu,android-gpu"},
+        labels=["benchmarks:x86_64", "benchmarks:cuda"],
+        is_pr=True,
+        is_llvm_integrate_pr=False)
+
+    self.assertEqual(presets, "android-cpu,android-gpu,cuda,x86_64")
+
+  def test_get_benchmark_presets_from_default_group(self):
+    presets = configure_ci.get_benchmark_presets(
+        trailers={"benchmark-extra": "defaults"},
+        labels=[],
+        is_pr=True,
+        is_llvm_integrate_pr=False)
+
+    self.assertEqual(presets, SORTED_DEFAULT_BENCHMARK_PRESETS)
+
+  def test_get_benchmark_presets_for_non_pr(self):
+    presets = configure_ci.get_benchmark_presets(trailers={},
+                                                 labels=[],
+                                                 is_pr=False,
+                                                 is_llvm_integrate_pr=False)
+
+    self.assertEqual(presets, SORTED_DEFAULT_BENCHMARK_PRESETS)
+
+  def test_get_benchmark_presets_for_llvm_integrate_pr(self):
+    presets = configure_ci.get_benchmark_presets(trailers={},
+                                                 labels=[],
+                                                 is_pr=True,
+                                                 is_llvm_integrate_pr=True)
+
+    self.assertEqual(presets, SORTED_DEFAULT_BENCHMARK_PRESETS)
+
+
+if __name__ == "__main__":
+  unittest.main()
