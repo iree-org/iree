3fc85e33b918ba1771628b2f8e1dcec76ab0a93f
Che-Yu Wu <cheyuw@google.com>
2023-05-22 17:05:32 +0000

    Refactor

--- build_tools/github_actions/configure_ci_test.py
+++ build_tools/github_actions/configure_ci_test.py
@@ -10,62 +10,65 @@ import unittest
 
 import configure_ci
 
-SORTED_DEFAULT_BENCHMARK_PRESETS = ",".join(
+SORTED_DEFAULT_BENCHMARK_PRESETS_STR = ",".join(
     sorted(configure_ci.DEFAULT_BENCHMARK_PRESET_GROUP))
 
 
 class GetBenchmarkPresetsTest(unittest.TestCase):
 
   def test_get_benchmark_presets_no_preset(self):
-    presets = configure_ci.get_benchmark_presets(trailers={},
-                                                 labels=["unrelated-labels"],
-                                                 is_pr=True,
-                                                 is_llvm_integrate_pr=False)
+    presets_str = configure_ci.get_benchmark_presets(
+        trailers={},
+        labels=["unrelated-labels"],
+        is_pr=True,
+        is_llvm_integrate_pr=False)
 
-    self.assertEqual(presets, "")
+    self.assertEqual(presets_str, "")
 
   def test_get_benchmark_presets_from_pr_labels(self):
-    presets = configure_ci.get_benchmark_presets(
+    presets_str = configure_ci.get_benchmark_presets(
         trailers={},
         labels=["benchmarks:x86_64", "benchmarks:cuda"],
         is_pr=True,
         is_llvm_integrate_pr=False)
 
-    self.assertEqual(presets, "cuda,x86_64")
+    self.assertEqual(presets_str, "cuda,x86_64")
 
   def test_get_benchmark_presets_from_trailers_and_labels(self):
-    presets = configure_ci.get_benchmark_presets(
+    presets_str = configure_ci.get_benchmark_presets(
         trailers={"benchmark-extra": "android-cpu,android-gpu"},
         labels=["benchmarks:x86_64", "benchmarks:cuda"],
         is_pr=True,
         is_llvm_integrate_pr=False)
 
-    self.assertEqual(presets, "android-cpu,android-gpu,cuda,x86_64")
+    self.assertEqual(presets_str, "android-cpu,android-gpu,cuda,x86_64")
 
   def test_get_benchmark_presets_from_default_group(self):
-    presets = configure_ci.get_benchmark_presets(
+    presets_str = configure_ci.get_benchmark_presets(
         trailers={"benchmark-extra": "defaults"},
         labels=[],
         is_pr=True,
         is_llvm_integrate_pr=False)
 
-    self.assertEqual(presets, SORTED_DEFAULT_BENCHMARK_PRESETS)
+    self.assertEqual(presets_str, SORTED_DEFAULT_BENCHMARK_PRESETS_STR)
+    # Sanity check to ensure no `*-long` preset in the default group.
+    self.assertNotIn("-long", presets_str)
 
   def test_get_benchmark_presets_for_non_pr(self):
-    presets = configure_ci.get_benchmark_presets(trailers={},
-                                                 labels=[],
-                                                 is_pr=False,
-                                                 is_llvm_integrate_pr=False)
+    presets_str = configure_ci.get_benchmark_presets(trailers={},
+                                                     labels=[],
+                                                     is_pr=False,
+                                                     is_llvm_integrate_pr=False)
 
-    self.assertEqual(presets, SORTED_DEFAULT_BENCHMARK_PRESETS)
+    self.assertEqual(presets_str, SORTED_DEFAULT_BENCHMARK_PRESETS_STR)
 
   def test_get_benchmark_presets_for_llvm_integrate_pr(self):
-    presets = configure_ci.get_benchmark_presets(trailers={},
-                                                 labels=[],
-                                                 is_pr=True,
-                                                 is_llvm_integrate_pr=True)
+    presets_str = configure_ci.get_benchmark_presets(trailers={},
+                                                     labels=[],
+                                                     is_pr=True,
+                                                     is_llvm_integrate_pr=True)
 
-    self.assertEqual(presets, SORTED_DEFAULT_BENCHMARK_PRESETS)
+    self.assertEqual(presets_str, SORTED_DEFAULT_BENCHMARK_PRESETS_STR)
 
   # Sample PR description:
   # ```
@@ -77,13 +80,13 @@ class GetBenchmarkPresetsTest(unittest.TestCase):
   # ```
   # Result: No benchmark is automatically enabled on the LLVM integrate PR.
   def test_get_benchmark_presets_skip_llvm_integrate_benchmark(self):
-    presets = configure_ci.get_benchmark_presets(
+    presets_str = configure_ci.get_benchmark_presets(
         trailers={"skip-llvm-integrate-benchmark": "some good reasons"},
         labels=[],
         is_pr=True,
         is_llvm_integrate_pr=True)
 
-    self.assertEqual(presets, "")
+    self.assertEqual(presets_str, "")
 
   def test_get_benchmark_presets_unknown_preset(self):
     self.assertRaises(
