655be2bf9434b53695b8765f108b441a8bd45e42
Geoffrey Martin-Noble <gcmn@google.com>
2023-05-23 11:36:11 -0700

    Add Bazel remote cache to package build (#128)
    This uses a GCS bucket as a remote cache partitioned by docker image
    name and digest.
    
    This can be used for other builds as well using the same process.
    Speedups here are not spectacular, but it helps a bit.
    
    Tested:
    
    https://github.com/openxla/openxla-pjrt-plugin/actions/workflows/build_packages.yml
    Run 1 was writing the cache only and run 2 was with caching.

--- build_tools/ci/build_linux_packages.sh
+++ build_tools/ci/build_linux_packages.sh
@@ -49,6 +49,7 @@ output_dir="${output_dir:-${repo_root}/bindist}"
 plugins="${plugins:-iree/integrations/pjrt/cpu/pjrt_plugin_iree_cpu.so iree/integrations/pjrt/cuda/pjrt_plugin_iree_cuda.so}"
 packages="${packages:-plugins python-cpu-wheel python-cuda-wheel}"
 package_suffix="${package_suffix:-}"
+write_caches="${write_caches:-0}"
 
 function run_on_host() {
   echo "Running on host"
@@ -62,6 +63,15 @@ function run_on_host() {
     fi
     echo "Using default docker image: $manylinux_docker_image"
   fi
+
+  # Strip off the rest of the URL. Only the digest is load bearing anyway. If
+  # someone's not specifying the digest they may have a bad time, but the most
+  # likely scenario is just that the cache hits won't return anything. Actually
+  # writing to the cache requires access. It's surprisingly hard to get docker
+  # to tell you "give me the fully qualified name to the image you would use if
+  # I told you to `docker run` this", which is what we'd want to do this
+  # properly.
+  cache_key="${manylinux_docker_image##*/}"
 
   # Canonicalize paths.
   mkdir -p "${output_dir}"
@@ -79,6 +89,9 @@ function run_on_host() {
     -e "packages=${packages}" \
     -e "package_suffix=${package_suffix}" \
     -e "output_dir=${output_dir}" \
+    -e "write_caches=${write_caches}" \
+    -e "cache_key=${cache_key}" \
+    -e "GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS:-}" \
     "${manylinux_docker_image}" \
     -- ${this_dir}/${script_name}
 
@@ -114,6 +127,15 @@ function run_in_docker() {
       echo "::: Configuring bazel"
       python configure.py --cuda-sdk-dir=${cuda_sdk_dir}
       # replace dashes with underscores
+      declare -a bazel_flags=(
+        --compilation_mode=opt
+        --remote_cache="https://storage.googleapis.com/openxla-bazel-cache/${cache_key}"
+      )
+      if (( write_caches == 1 )); then
+        bazel_flags+=(--google_default_credentials)
+      else
+        bazel_flags+=(--noremote_upload_local_results)
+      fi
       package_suffix="${package_suffix//-/_}"
       case "${package}" in
         plugins)
@@ -145,7 +167,7 @@ function build_plugins() {
   local f
   local dest="${output_dir}/pjrt_plugins"
   mkdir -p $dest
-  bazel build -c opt ${plugins}
+  bazel build "${bazel_flags[@]}" ${plugins}
   for f in ${plugins}; do
     cp -fv bazel-bin/$f $dest
   done
@@ -156,7 +178,7 @@ function build_plugins_instrumented() {
   local f
   local dest="${output_dir}/pjrt_plugins_instrumented"
   mkdir -p $dest
-  bazel build -c opt --iree_enable_runtime_tracing ${plugins}
+  bazel build "${bazel_flags[@]}" --iree_enable_runtime_tracing ${plugins}
   for f in ${plugins}; do
     cp -fv bazel-bin/$f $dest
   done
@@ -166,7 +188,7 @@ function build_python_cpu_wheel() {
   # Note that these wheels are Python version independent. We build them
   # with each listed Python version to make sure that the setup machinery
   # works, but the most recent one persists.
-  bazel build -c opt iree/integrations/pjrt/cpu/pjrt_plugin_iree_cpu.so
+  bazel build "${bazel_flags[@]}" iree/integrations/pjrt/cpu/pjrt_plugin_iree_cpu.so
   clean_wheels "openxla_pjrt_plugin_cpu${package_suffix}" "py3-none"
   build_wheel python_packages/openxla_cpu_plugin
   run_audit_wheel "openxla_pjrt_plugin_cpu${package_suffix}" "py3-none"
