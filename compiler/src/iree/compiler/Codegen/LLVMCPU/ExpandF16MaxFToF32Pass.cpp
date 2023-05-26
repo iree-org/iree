7997d712edbc5783be1446ba071fa880a1ad052d
Ranvirsv <ranvirsv@gmail.com>
2023-05-26 16:15:56 -0700

    Adding tests and passes

--- compiler/src/iree/compiler/Codegen/LLVMCPU/ExpandF16MaxFToF32Pass.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMCPU/ExpandF16MaxFToF32Pass.cpp
@@ -32,32 +32,22 @@ struct ExpandF16MaxFToF32Pattern : public OpRewritePattern<arith::MaxFOp> {
     LogicalResult matchAndRewrite(arith::MaxFOp op, 
                                   PatternRewriter &rewriter) const override{
       Type resultType = op.getLhs().getType();
-      llvm::outs()<<"\n\n Added \n\n";
       if (getElementTypeOrSelf(resultType).getIntOrFloatBitWidth() != 16) {
         return failure();
       }
-      llvm::outs()<<"\n\n Pass \n\n";
 
       Location loc = op.getLoc();
 
       Type wideType = rewriter.getF32Type();
-      llvm::outs()<<"helloA!\n";
       if(auto vecTy = resultType.dyn_cast<VectorType>()) {
         wideType = VectorType::get(vecTy.getShape(), wideType);
       }
-      llvm::outs()<<"helloB!\n";
 
       Value lhsExt = rewriter.create<arith::ExtFOp>(loc, wideType, op.getLhs());
       Value rhsExt = rewriter.create<arith::ExtFOp>(loc, wideType, op.getRhs());
       Value maxExt = 
           rewriter.create<arith::MaxFOp>(loc, wideType, lhsExt, rhsExt);
       Value result = rewriter.create<arith::TruncFOp>(loc, resultType, maxExt);
-      llvm::outs()<<"helloC!\n";
-      // llvm::outs()<<"op found:"<<op<<"\n";
-      // llvm::outs()<<"lhs:"<<lhsExt<<"\n";
-      // llvm::outs()<<"rhs:"<<rhsExt<<"\n";
-      // llvm::outs()<<"max:"<<maxExt<<"\n";
-      // llvm::outs()<<"\nres:"<<result<<"\n";
 
       rewriter.replaceOp(op, result);
       return success();
---
+++ tests/transform_dialect/cpu/fmaxf_16bit_expantion.mlir
@@ -0,0 +1,21 @@
+
+
+func.func @test_expand_f16_maxf(%arg0: tensor<4xf16>, %arg1: tensor<4xf16>) -> tensor<4xf16>{
+    %1 = arith.maxf %arg0, %arg1 : tensor<4xf16>
+    return %1: tensor<4xf16>
+}
+
+// CHECK-LABEL: func @test_expand_f16_maxf
+// CHECK: %[[EXT1:.*]] = arith.extf %arg0 : tensor<4xf16> to tensor<4xf32> 
+// CHECK: %[[EXT2:.*]] = arith.extf %arg1 : tensor<4xf16> to tensor<4xf32>
+// CHECK: %[[MAX:.*]] = arith.maxf %[[EXT1]], %[[EXT2]] : tensor<4xf32>
+// CHECK: %[[TRUNC:.*]] = arith.truncf %[[MAX]] : tensor<4xf16>
+// CHECK: return %[[TRUNC]] : tensor<4xf16>
+
+// CHECK-LABEL: func @test_expand_f16_maxf
+// CHECK: %[[EXT1:.*]] = arith.extf %arg0 : vector<4xf16> to vector<4xf32> 
+// CHECK: %[[EXT2:.*]] = arith.extf %arg1 : vector<4xf16> to vector<4xf32>
+// CHECK: %[[MAX:.*]] = arith.maxf %[[EXT1]], %[[EXT2]] : vector<4xf32>
+// CHECK: %[[TRUNC:.*]] = arith.truncf %[[MAX]] : vector<4xf16>
+// CHECK: return %[[TRUNC]] : vector<4xf16>
+
