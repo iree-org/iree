iree-org/iree

commit bd5174b66f7f17933ef6dcdf8d3bc89204dda410

`iree_c_embed_data` improvements (#13814)
* Reconcile CMake with Bazel by dropping the CMake-specific
`GENERATED_SRCS` argument and instead just putting everything in `SRCS`,
like in Bazel.
* To bridge that difference, `bazel_to_cmake_converter.py` was facing
the impossible task of choosing, for each `srcs` entry, whether to put
it in `SRCS` or `GENERATED_SRCS`. It was simply deciding that based on
whether the leading character was a `:`, offering a way to distinguish
generated files in the case where they were generated within the current
package. But that didn't generalize to generated files from other
packages.
* In `SRCS` support various syntaxes for files in the source or in the
build directory (generated files) and automatically do the right thing
to match Bazel. In particular, make it simple to reference generated
files in a subdirectory.
* Concretely, to address #13804, ukernel bitcode will need to reference
generated bitcode in architecture-specific subdirectories.
* In `generate_embed_data_main.cc`, generate clear errors on failure to
open an input/output file (would've saved some head scratching).

For some reason, this exposed an issue in `hal/cts`. The filenames were
missing a `"${TARGET_BACKEND}_"` prefix and somehow that was still
working; now with my changes that wasn't working anymore. Fixed by
adding the missing prefixes.

(This commit is one of a chain in completing
https://github.com/openxla/iree/issues/13804).

build_tools/cmake/iree_c_embed_data.cmake
@@ -13,10 +13,8 @@ include(CMakeParseArguments)
 # Parameters:
 # PACKAGE: Name of the package (overrides actual path)
 # NAME: Name of target (see Note).
-# SRCS: List of source files to embed (non-absolute paths will be resolved
-#     relative to CMAKE_CURRENT_SRC_DIR).
+# SRCS: List of files to embed (in source or build directory).
 # INCLUDES: Include directories to add to dependencies
-# GENERATED_SRCS: List of generated source files to embed.
 # C_FILE_OUTPUT: The C implementation file to output.
 # H_FILE_OUTPUT: The H header file to output.
 # STRIP_PREFIX: Strips this verbatim prefix from filenames (in the TOC).
@@ -34,7 +32,7 @@ function(iree_c_embed_data)
     _RULE
     "PUBLIC;TESTONLY;FLATTEN"
     "PACKAGE;NAME;IDENTIFIER;STRIP_PREFIX;C_FILE_OUTPUT;H_FILE_OUTPUT"
-    "DEPS;SRCS;GENERATED_SRCS;INCLUDES"
+    "DEPS;SRCS;INCLUDES"
     ${ARGN}
   )
 
@@ -59,21 +57,48 @@ function(iree_c_embed_data)
     list(APPEND _ARGS "--flatten")
   endif()
 
+  set(_RELATIVE_BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}")
+  cmake_path(RELATIVE_PATH _RELATIVE_BINARY_DIR BASE_DIRECTORY "${IREE_BINARY_DIR}")
+
   foreach(_SRC ${_RULE_SRCS})
-    if(IS_ABSOLUTE "${_SRC}")
-      list(APPEND _ARGS "${_SRC}")
+    if(_SRC MATCHES "^/")
+      # _SRC is an absolute path (starts with `/`).
+      list(APPEND _RESOLVED_SRCS "${_SRC}")
+    elseif(_SRC MATCHES "^[$]<")
+      # _SRC is a CMake generator expression (starts with `$<`).
+      list(APPEND _RESOLVED_SRCS "${_SRC}")
+    elseif(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${_SRC}")
+      # _SRC path exists relatively to current source dir.
+      list(APPEND _RESOLVED_SRCS "${CMAKE_CURRENT_SOURCE_DIR}/${_SRC}")
+    elseif(EXISTS "${IREE_SOURCE_DIR}/${_SRC}")
+      # _SRC path exists relatively to root source dir.
+      list(APPEND _RESOLVED_SRCS "${IREE_SOURCE_DIR}/${_SRC}")
     else()
-      list(APPEND _ARGS "${CMAKE_CURRENT_SOURCE_DIR}/${_SRC}")
+      # All else having failed, interpret _SRC as a path in the binary dir,
+      # that is, a generated file. As the present logic executes before
+      # that file would be generated, we can't perform a if(EXISTS) test to
+      # validate that hypothesis.
+      #
+      # Additional difficulty: _SRC could be a path relative to either the
+      # current binary dir, or the root binary dir. Again, it's too early here
+      # to use a if(EXISTS) test to determine that. Instead, the following
+      # regex replacement strips the current binary dir as a prefix from _SRC.
+      # So if _SRC was relative to the root binary dir, now it is relative to
+      # the current binary dir. And if it was already relative to the current
+      # binary dir, then the regex should fail to match (unless we're very
+      # unlucky and we have paths of the from root/a/b/a/b, but that's not a
+      # problem that we have today. And if that ever happens, people will still
+      # be able to get the correct behavior by passing a path relative to the
+      # root dir).
+      string(REGEX REPLACE "^${_RELATIVE_BINARY_DIR}/" "" _SRC_RELATIVE "${_SRC}")
+      list(APPEND _RESOLVED_SRCS "${CMAKE_CURRENT_BINARY_DIR}/${_SRC_RELATIVE}")
     endif()
-  endforeach(_SRC)
-  foreach(_SRC ${_RULE_GENERATED_SRCS})
-    list(APPEND _ARGS "${_SRC}")
   endforeach(_SRC)
 
   add_custom_command(
     OUTPUT "${_RULE_H_FILE_OUTPUT}" "${_RULE_C_FILE_OUTPUT}"
-    COMMAND generate_embed_data ${_ARGS}
-    DEPENDS generate_embed_data ${_RULE_SRCS} ${_RULE_GENERATED_SRCS}
+    COMMAND generate_embed_data ${_ARGS} ${_RESOLVED_SRCS}
+    DEPENDS generate_embed_data ${_RESOLVED_SRCS}
   )
 
   if(_RULE_TESTONLY)

build_tools/bazel_to_cmake/bazel_to_cmake_converter.py
@@ -111,19 +111,17 @@ class BuildFileFunctions(object):
     return self._convert_string_arg_block(name, target, quote=False)
 
   def _convert_srcs_block(self, srcs):
-    if srcs is None:
+    if not srcs:
       return ""
-    generated_srcs = [src for src in srcs if src.startswith(":")]
-    srcs = [src for src in srcs if src not in generated_srcs]
-    sets = []
-    if srcs:
-      sets.append(self._convert_string_list_block("SRCS", srcs, sort=True))
-    if generated_srcs:
-      sets.append(
-          self._convert_string_list_block("GENERATED_SRCS",
-                                          [src[1:] for src in generated_srcs],
-                                          sort=True))
-    return "\n".join(sets)
+    # Bazel allows srcs to reference targets in the current package (leading
+    # ':') or in other packages (leading '//'). We map that to paths by:
+    # - dropping any leading ':' as in:
+    #      ':generated.c' -> 'generated.c'
+    # - dropping any leading '//', and internal ':' by '/', as in:
+    #      '//path/to/package:generated.c' ->  'path/to/package/generated.c'
+    srcs = [s.lstrip('//').lstrip(':').replace(':', '/') for s in srcs]
+
+    return self._convert_string_list_block("SRCS", srcs, sort=True)
 
   def _convert_td_file_block(self, td_file):
     if td_file.startswith("//iree"):

build_tools/cmake/iree_bytecode_module.cmake
@@ -158,7 +158,7 @@ function(iree_bytecode_module)
         "${_RULE_NAME}_c"
       IDENTIFIER
         "${_RULE_C_IDENTIFIER}"
-      GENERATED_SRCS
+      SRCS
         "${_RULE_NAME}.vmfb"
       C_FILE_OUTPUT
         "${_RULE_NAME}_c.c"

build_tools/cmake/iree_hal_cts_test_suite.cmake
@@ -111,7 +111,7 @@ function(iree_hal_cts_test_suite)
       iree_c_embed_data(
         NAME
           ${_EXECUTABLES_TESTDATA_NAME}_c
-        GENERATED_SRCS
+        SRCS
           ${_EMBED_DATA_SOURCES}
         C_FILE_OUTPUT
           "${_EXECUTABLES_TESTDATA_NAME}_c.c"
@@ -180,6 +180,7 @@ function(iree_hal_cts_test_suite)
     set(IREE_CTS_DRIVER_REGISTRATION_FN "${_RULE_DRIVER_REGISTRATION_FN}")
     set(IREE_CTS_TEST_CLASS_NAME "${_TEST_NAME}_test")
     set(IREE_CTS_DRIVER_NAME "${_RULE_DRIVER_NAME}")
+    set(IREE_CTS_TARGET_BACKEND "${_RULE_COMPILER_TARGET_BACKEND}")
 
     configure_file(
       "${IREE_ROOT_DIR}/runtime/src/iree/hal/cts/cts_test_template.cc.in"

build_tools/embed_data/generate_embed_data_main.cc
@@ -90,6 +90,11 @@ static bool GenerateHeader(const std::string& identifier,
                            const std::string& header_file,
                            const std::vector<std::string>& toc_files) {
   std::ofstream f(header_file, std::ios::out | std::ios::trunc);
+  if (!f) {
+    fprintf(stderr, "Failed to open '%s' for write.\n", header_file.c_str());
+    exit(EXIT_FAILURE);
+  }
+
   f << "#pragma once\n";  // Pragma once isn't great but is the best we can do.
   f << "#include <stddef.h>\n";
   GenerateTocStruct(f);
@@ -106,6 +111,10 @@ static bool GenerateHeader(const std::string& identifier,
 static bool SlurpFile(const std::string& file_name, std::string* contents) {
   constexpr std::streamoff kMaxSize = 100000000;
   std::ifstream f(file_name, std::ios::in | std::ios::binary);
+  if (!f) {
+    fprintf(stderr, "Failed to open '%s' for read.\n", file_name.c_str());
+    exit(EXIT_FAILURE);
+  }
   // get length of file:
   f.seekg(0, f.end);
   std::streamoff length = f.tellg();
@@ -133,6 +142,11 @@ static bool GenerateImpl(const std::string& identifier,
                          const std::vector<std::string>& input_files,
                          const std::vector<std::string>& toc_files) {
   std::ofstream f(impl_file, std::ios::out | std::ios::trunc);
+  if (!f) {
+    fprintf(stderr, "Failed to open '%s' for write.\n", impl_file.c_str());
+    exit(EXIT_FAILURE);
+  }
+
   f << "#include <stddef.h>\n";
   f << "#include <stdint.h>\n";
   f << R"(

runtime/src/iree/base/testing/CMakeLists.txt
@@ -19,7 +19,7 @@ iree_cc_library(
 iree_c_embed_data(
   NAME
     dynamic_library_test_library
-  GENERATED_SRCS
+  SRCS
     "$<TARGET_FILE:iree::base::testing::dynamic_library_test_library.so>"
   C_FILE_OUTPUT
     "dynamic_library_test_library_embed.c"

runtime/src/iree/builtins/device/CMakeLists.txt
@@ -71,7 +71,7 @@ iree_bitcode_library(
 iree_c_embed_data(
   NAME
     libdevice_bitcode
-  GENERATED_SRCS
+  SRCS
     "libdevice_wasm32_generic.bc"
     "libdevice_wasm64_generic.bc"
   DEPS

runtime/src/iree/builtins/ukernel/CMakeLists.txt
@@ -172,7 +172,7 @@ iree_link_bitcode(
 iree_c_embed_data(
   NAME
     libukernel_bitcode
-  GENERATED_SRCS
+  SRCS
     "ukernel_bitcode.bc"
   DEPS
 

commit 5b4719709980161e82800f86ec4e2c7cc17115b6

Handle Windows `C:/`-style absolute paths in `iree_c_embed_data` (#13852)
Fixed breakage
https://github.com/openxla/iree/actions/runs/5125490369/jobs/9218738653
caused by https://github.com/openxla/iree/pull/13814:

```
FindFirstFileExA(build_tools/third_party/cuda/c:/a/iree/iree/build-windows/build_tools/third_party/cuda/11.6.2/windows-x86_64/nvvm/libdevice)
```

If my MS-DOS memory serves, the c:/ indicates the start of an absolute
path. my PR had logic to recognize absolute paths.... by looking for '/'
prefix. this adds logic to handle the Windows flavor of absolute paths.

See https://github.com/openxla/iree/pull/13814 for why we need a
heuristic to tell whether a path is absolute.

build_tools/cmake/iree_c_embed_data.cmake
@@ -62,7 +62,10 @@ function(iree_c_embed_data)
 
   foreach(_SRC ${_RULE_SRCS})
     if(_SRC MATCHES "^/")
-      # _SRC is an absolute path (starts with `/`).
+      # _SRC is a Unix-style absolute path (starts with `/`).
+      list(APPEND _RESOLVED_SRCS "${_SRC}")
+    elseif(_SRC MATCHES "^[a-zA-Z]:/")
+      # _SRC is a Windows-style absolute path (starts with `X:/`).
       list(APPEND _RESOLVED_SRCS "${_SRC}")
     elseif(_SRC MATCHES "^[$]<")
       # _SRC is a CMake generator expression (starts with `$<`).