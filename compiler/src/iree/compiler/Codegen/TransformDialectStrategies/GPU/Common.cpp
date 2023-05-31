e50592d443a9419302e85a73e2a1e147bb92bef1
Diego Caballero <diegocaballero@google.com>
2023-05-31 05:34:38 +0000

    Fix wrong use of llvm_unreachable

--- compiler/src/iree/compiler/Codegen/TransformDialectStrategies/GPU/Common.cpp
+++ compiler/src/iree/compiler/Codegen/TransformDialectStrategies/GPU/Common.cpp
@@ -770,7 +770,7 @@ static LogicalResult matchAndSetReductionStrategy(func::FuncOp entryPoint,
       StagedReductionStrategy strategy(captures, reductionConfig);
       return buildStagedReductionStrategy(b, variant, strategy);
     } else {
-      return llvm_unreachable("Unknown strategy");
+      llvm_unreachable("Unknown strategy");
     }
   };
 
