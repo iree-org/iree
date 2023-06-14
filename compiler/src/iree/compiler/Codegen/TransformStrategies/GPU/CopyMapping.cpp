432099c22ec38da0c6c6fde869d9586684c2b88f
Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>
2023-06-14 12:49:50 +0000

    Reduce maxVectorSize to 1

--- compiler/src/iree/compiler/Codegen/TransformStrategies/GPU/CopyMapping.cpp
+++ compiler/src/iree/compiler/Codegen/TransformStrategies/GPU/CopyMapping.cpp
@@ -45,7 +45,12 @@ iree_compiler::gpu::CopyMapping::numThreadsForCopy(int totalNumThreads,
   for (auto s : sizes) numElements *= s;
   LDBG("--numElements: " << numElements);
 
-  int64_t actualVectorSize = maxVectorSize;
+  // TODO: this implementation exhibits and issue with mapping to cp.async zfill
+  // in the presence of vectors of size > 1. This could also be related to the
+  // lack of substantial perf gains between masked vector<4> and masked
+  // vector<1>.
+  // int64_t actualVectorSize = maxVectorSize;
+  int64_t actualVectorSize = 1;
   if (!favorPredication) {
     // Bias towards reducing the vector size to avoid predication.
     // Predication occurs if we end up using fewer than totalNumThreads for a
