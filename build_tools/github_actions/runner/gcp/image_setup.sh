bb10bfa202f8b2a3fad54043323754ca0289330b
Jerry Wu <cheyuw@google.com>
2023-06-21 19:37:44 -0400

    Regenerate bleeding-edge images (#14177)

--- build_tools/github_actions/runner/gcp/image_setup.sh
+++ build_tools/github_actions/runner/gcp/image_setup.sh
@@ -316,7 +316,7 @@ EOF
 
     check_docker gcr.io/iree-oss/nvidia@sha256:1717431fd46b8b1e96d95fa72508e3e3eacb5c95f1245b9b7dbeec23ae823d02
     check_docker gcr.io/iree-oss/frontends-nvidia@sha256:bc217d1a226f778e44874b989d6ead3a3085f04643a40aa301f5d30661356d07
-    check_docker gcr.io/iree-oss/nvidia-bleeding-edge@sha256:a14a1c9442a40f02023c8a30a025902f4c8cd06bf37dac605e48521296fb23be
+    check_docker gcr.io/iree-oss/nvidia-bleeding-edge@sha256:234f6efb1e9fd2eea67d40effd7d9cfe8725c5e5ba7a58c0428200a54493637a
 
     # Remove the docker images we've fetched. We might want to pre-fetch Docker
     # images into the VM image, but that should be a separate decision.
