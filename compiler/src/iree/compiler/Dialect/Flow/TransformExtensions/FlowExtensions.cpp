226e3f928fd8186235f101cf39e6d0be7ebd88db
Alex Zinenko <zinenko@google.com>
2023-06-22 13:52:25 +0000

    Fix memory effects of transform flow extension ops
    The transform ops in the Flow dialect extension should consume their
    handles. Their implementaiton uses the confusingly named `move*Op` API
    that in fact does clone+erase instead of the proper move, which is not
    guaranteed to preserve operation pointers. This leads to memory
    corruption.

--- compiler/src/iree/compiler/Dialect/Flow/TransformExtensions/FlowExtensions.cpp
+++ compiler/src/iree/compiler/Dialect/Flow/TransformExtensions/FlowExtensions.cpp
@@ -588,7 +588,7 @@ transform_dialect::MovePrecedingOpIntoDispatchRegionOp::apply(
 
 void transform_dialect::MovePrecedingOpIntoDispatchRegionOp::getEffects(
     SmallVectorImpl<MemoryEffects::EffectInstance> &effects) {
-  transform::onlyReadsHandle(getTarget(), effects);
+  transform::consumesHandle(getTarget(), effects);
   transform::consumesHandle(getDispatchRegion(), effects);
   transform::producesHandle(getTransformed(), effects);
   transform::modifiesPayload(effects);
@@ -818,7 +818,7 @@ transform_dialect::MoveSucceedingOpIntoDispatchRegionOp::apply(
 
 void transform_dialect::MoveSucceedingOpIntoDispatchRegionOp::getEffects(
     SmallVectorImpl<MemoryEffects::EffectInstance> &effects) {
-  transform::onlyReadsHandle(getTarget(), effects);
+  transform::consumesHandle(getTarget(), effects);
   transform::consumesHandle(getDispatchRegion(), effects);
   transform::producesHandle(getTransformed(), effects);
   transform::modifiesPayload(effects);
