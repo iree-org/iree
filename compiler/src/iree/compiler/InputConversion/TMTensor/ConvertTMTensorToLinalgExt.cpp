c2e602dff3c8e6999241903e9920a4b4c400771f
gpetters94 <gpetters@protonmail.com>
2023-06-23 16:17:18 -0400

    Add support for tm_tensor.attention (#13777)
    This adds a mapping from tm_tensor.attention to linalg_ext.attention. The extra
    batch dimensions allowed in tm_tensor.attention are planned to be dropped in a
    later revision.

--- compiler/src/iree/compiler/InputConversion/TMTensor/ConvertTMTensorToLinalgExt.cpp
+++ compiler/src/iree/compiler/InputConversion/TMTensor/ConvertTMTensorToLinalgExt.cpp
@@ -4,16 +4,16 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#include <algorithm>
+#include <cstdint>
+#include <numeric>
 
 #include "iree-dialects/Dialect/LinalgExt/IR/LinalgExtDialect.h"
 #include "iree-dialects/Dialect/LinalgExt/IR/LinalgExtOps.h"
 #include "iree/compiler/InputConversion/TMTensor/PassDetail.h"
 #include "iree/compiler/InputConversion/TMTensor/Passes.h"
-#include "mlir/IR/PatternMatch.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Transforms/DialectConversion.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
-#include "torch-mlir-dialects/Dialect/TMTensor/IR/TMTensorDialect.h"
 #include "torch-mlir-dialects/Dialect/TMTensor/IR/TMTensorOps.h"
 
 namespace mlir {
@@ -69,6 +69,93 @@ struct ScatterOpConversion
 };
 }  // namespace
 
+static Value collapseBatches(PatternRewriter &rewriter, Location loc,
+                             Value val) {
+  auto valSizes = val.getType().cast<RankedTensorType>().getShape();
+  int64_t newBatch =
+      std::accumulate(valSizes.begin(), valSizes.end() - 2, 1,
+                      [](int64_t x, int64_t y) { return x * y; });
+  Type elementType = val.getType().cast<RankedTensorType>().getElementType();
+  SmallVector<int64_t> newSizes{newBatch};
+  newSizes.append(valSizes.end() - 2, valSizes.end());
+  Type newType = RankedTensorType::get(newSizes, elementType);
+
+  auto rank = valSizes.size();
+  SmallVector<int64_t> collapsed;
+  for (auto i = 0; i < rank - 2; i++) collapsed.push_back(i);
+
+  SmallVector<ReassociationIndices> reassociation(3);
+  reassociation[0].append(collapsed);
+  reassociation[1].push_back(rank - 2);
+  reassociation[2].push_back(rank - 1);
+
+  return rewriter
+      .create<tensor::CollapseShapeOp>(loc, newType, val, reassociation)
+      .getResult();
+}
+static Value expandBatches(PatternRewriter &rewriter, Location loc,
+                           SmallVector<int64_t> batchSizes, Value val) {
+  auto valSizes = val.getType().cast<RankedTensorType>().getShape();
+  Type elementType = val.getType().cast<RankedTensorType>().getElementType();
+  SmallVector<int64_t> newSizes(batchSizes);
+  newSizes.append(valSizes.end() - 2, valSizes.end());
+  auto rank = newSizes.size();
+  Type newType = RankedTensorType::get(newSizes, elementType);
+
+  SmallVector<ReassociationIndices> reassociation(3);
+  for (auto i = 0; i < batchSizes.size(); i++) reassociation[0].push_back(i);
+  reassociation[1].push_back(rank - 2);
+  reassociation[2].push_back(rank - 1);
+
+  return rewriter
+      .create<tensor::ExpandShapeOp>(loc, newType, val, reassociation)
+      .getResult();
+}
+struct AttentionOpConversion
+    : public OpRewritePattern<mlir::torch::TMTensor::AttentionOp> {
+  using OpRewritePattern<mlir::torch::TMTensor::AttentionOp>::OpRewritePattern;
+  LogicalResult matchAndRewrite(mlir::torch::TMTensor::AttentionOp op,
+                                PatternRewriter &rewriter) const override {
+    Location loc = op->getLoc();
+    Value query = op.getQuery();
+    Value key = op.getKey();
+    Value value = op.getValue();
+    auto sizes = query.getType().cast<RankedTensorType>().getShape();
+    SmallVector<int64_t> batchSizes(sizes.begin(), sizes.end() - 2);
+
+    if (sizes.size() > 3) {
+      query = collapseBatches(rewriter, loc, query);
+      key = collapseBatches(rewriter, loc, key);
+      value = collapseBatches(rewriter, loc, value);
+    }
+
+    SmallVector<int64_t> resultShape(
+        op->getResultTypes()[0].cast<RankedTensorType>().getShape());
+    SmallVector<int64_t> collapsedResultShape;
+    collapsedResultShape.push_back(
+        std::accumulate(resultShape.begin(), resultShape.end() - 2, 1,
+                        [](int64_t x, int64_t y) { return x * y; }));
+    collapsedResultShape.append(resultShape.end() - 2, resultShape.end());
+    Type elementType =
+        query.getType().cast<RankedTensorType>().getElementType();
+    auto collapsedResultType =
+        RankedTensorType::get(collapsedResultShape, elementType);
+    Value collapsedResult = rewriter.create<tensor::EmptyOp>(
+        loc, collapsedResultShape, elementType);
+
+    auto attention = rewriter.create<IREE::LinalgExt::AttentionOp>(
+        loc, collapsedResultType, SmallVector<Value>{query, key, value},
+        collapsedResult);
+
+    if (sizes.size() > 3)
+      rewriter.replaceOp(op, expandBatches(rewriter, loc, batchSizes,
+                                           attention.getResult()[0]));
+    else
+      rewriter.replaceOp(op, attention.getResult()[0]);
+    return success();
+  }
+};
+
 namespace {
 
 //===----------------------------------------------------------------------===//
@@ -79,6 +166,7 @@ struct ConvertTMTensorToLinalgExtPass
     : public ConvertTMTensorToLinalgExtBase<ConvertTMTensorToLinalgExtPass> {
   void getDependentDialects(DialectRegistry &registry) const override {
     registry.insert<IREE::LinalgExt::IREELinalgExtDialect>();
+    registry.insert<tensor::TensorDialect>();
   }
 
   void runOnOperation() override {
@@ -98,6 +186,7 @@ struct ConvertTMTensorToLinalgExtPass
 #undef INSERT_TMTENSOR_CONVERSION_PATTERN
 
     patterns.add<ScatterOpConversion>(context);
+    patterns.add<AttentionOpConversion>(context);
 
     if (failed(applyPatternsAndFoldGreedily(getOperation(),
                                             std::move(patterns)))) {
