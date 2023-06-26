abe91fa6361e6b471f611cb3620607ecfc23c854
Stella Laurenzo <stellaraccident@gmail.com>
2023-06-26 16:57:30 -0700

    Port iree.runtime to nanobind. (#14214)
    I believe that this should be a no-op for users. There is one minor API
    change (the MappedMemory class no longer implements the buffer protocol,
    but I've seen no evidence that this was actually used since it was a
    less functional way to get a host ndarray).
    
    More adventurous use of nanobind is possible in the future (i.e. using
    `ndarray` and `dlpack` interop for sharing across frameworks), but using
    that will most likely necessitate API changes, which I was working to
    avoid.
    
    Aside from relatively mechanical differences from pybind11, the main
    issues were that the buffer protocol and array support was dropped in
    nanobind. This required some direct coding against the C API to achieve
    the same characteristics. I think this is actually an improvement as the
    pybind11 implementations of these features was neither efficient nor
    obvious what it was doing.
    
    A build time dependency on `nanobind` is added. When building Python
    wheels, this gets satisfied automatically. Otherwise, the docker images
    have been updated to pre-install the necessary Python package. In
    addition, there is now a build time dependency on NumPy headers, which
    should already be installed (pybind11 vendored stripped down copies of
    these headers in an effort to avoid this, but I opted to just do the
    normal thing).
    
    Nanobind's performance is [quite
    compelling](https://nanobind.readthedocs.io/en/latest/benchmark.html)
    and owes to a combination of favoring more efficient binding styles that
    would basically be a rewrite in pybind11 and exclusive use of the new
    Python 3.8+ vectorcall ABI. Since the runtime is performance critical
    and the cost of Python calls is already quite visibly adding overhead on
    traces, it makes sense to baseline on the most efficient implementation.
    In addition, the compile-time savings seem to be real and the build is
    noticeably faster (this was not a primary consideration, just a nice
    bonus).

--- runtime/bindings/python/invoke.cc
+++ runtime/bindings/python/invoke.cc
@@ -6,6 +6,8 @@
 
 #include "./invoke.h"
 
+#include <functional>
+
 #include "./hal.h"
 #include "./vm.h"
 #include "iree/base/api.h"
@@ -74,18 +76,19 @@ class InvokeStatics {
   py::str kAttrBufferView = py::str("_buffer_view");
 
   // Module 'numpy'.
-  py::module &numpy_module() { return numpy_module_; }
+  py::module_ &numpy_module() { return numpy_module_; }
 
   py::object &runtime_module() {
     if (!runtime_module_) {
-      runtime_module_ = py::module::import("iree.runtime");
+      runtime_module_ = py::module_::import_("iree.runtime");
     }
     return *runtime_module_;
   }
 
-  py::module &array_interop_module() {
+  py::module_ &array_interop_module() {
     if (!array_interop_module_) {
-      array_interop_module_ = py::module::import("iree.runtime.array_interop");
+      array_interop_module_ =
+          py::module_::import_("iree.runtime.array_interop");
     }
     return *array_interop_module_;
   }
@@ -97,7 +100,7 @@ class InvokeStatics {
     return *device_array_type_;
   }
 
-  py::type &hal_buffer_view_type() { return hal_buffer_view_type_; }
+  py::type_object &hal_buffer_view_type() { return hal_buffer_view_type_; }
 
   py::object MapElementAbiTypeToDtype(py::object &element_abi_type) {
     try {
@@ -165,12 +168,13 @@ class InvokeStatics {
           IREE_TRACE_SCOPE_NAMED("ArgumentPacker::ReflectionNdarray");
           HalBufferView *bv = nullptr;
           py::object retained_bv;
-          if (py::isinstance(py_value, device_array_type())) {
+          if (is_instance_of_type_object(py_value, device_array_type())) {
             // Short-circuit: If a DeviceArray is provided, assume it is
             // correct.
             IREE_TRACE_SCOPE_NAMED("PackDeviceArray");
             bv = py::cast<HalBufferView *>(py_value.attr(kAttrBufferView));
-          } else if (py::isinstance(py_value, hal_buffer_view_type())) {
+          } else if (is_instance_of_type_object(py_value,
+                                                hal_buffer_view_type())) {
             // Short-circuit: If a HalBufferView is provided directly.
             IREE_TRACE_SCOPE_NAMED("PackBufferView");
             bv = py::cast<HalBufferView *>(py_value);
@@ -363,7 +367,7 @@ class InvokeStatics {
 
   PackCallback GetGenericPackCallbackFor(py::handle arg) {
     PopulatePyTypeToPackCallbacks();
-    py::type clazz = py::type::of(arg);
+    py::handle clazz = arg.type();
     auto found_it = py_type_to_pack_callbacks_.find(clazz.ptr());
     if (found_it == py_type_to_pack_callbacks_.end()) {
       // Probe to see if we have a host array.
@@ -419,7 +423,7 @@ class InvokeStatics {
     // has no further refinement of these, just treat them as vm 64 bit int and
     // floats and let the VM take care of it. There isn't much else we can do.
     AddPackCallback(
-        py::type::of(py::cast(1)),
+        py::cast(1).type(),
         [](InvokeContext &c, iree_vm_list_t *list, py::handle py_value) {
           iree_vm_value_t vm_value =
               iree_vm_value_make_i64(py::cast<int64_t>(py_value));
@@ -428,7 +432,7 @@ class InvokeStatics {
         });
 
     AddPackCallback(
-        py::type::of(py::cast(1.0)),
+        py::cast(1.0).type(),
         [](InvokeContext &c, iree_vm_list_t *list, py::handle py_value) {
           iree_vm_value_t vm_value =
               iree_vm_value_make_f64(py::cast<double>(py_value));
@@ -441,7 +445,7 @@ class InvokeStatics {
                                     py::handle py_value) {
       auto py_seq = py::cast<py::sequence>(py_value);
       VmVariantList item_list = VmVariantList::Create(py::len(py_seq));
-      for (py::object py_item : py_seq) {
+      for (py::handle py_item : py_seq) {
         PackCallback sub_packer = GetGenericPackCallbackFor(py_item);
         if (!sub_packer) {
           std::string message("could not convert python value to VM: ");
@@ -454,8 +458,8 @@ class InvokeStatics {
       iree_vm_ref_t retained = iree_vm_list_move_ref(item_list.steal_raw_ptr());
       iree_vm_list_push_ref_move(list, &retained);
     };
-    AddPackCallback(py::type::of(py::list{}), sequence_callback);
-    AddPackCallback(py::type::of(py::tuple{}), sequence_callback);
+    AddPackCallback((py::list{}).type(), sequence_callback);
+    AddPackCallback((create_empty_tuple()).type(), sequence_callback);
 
     // Dict.
     auto dict_callback = [this](InvokeContext &c, iree_vm_list_t *list,
@@ -483,11 +487,11 @@ class InvokeStatics {
       iree_vm_ref_t retained = iree_vm_list_move_ref(item_list.steal_raw_ptr());
       iree_vm_list_push_ref_move(list, &retained);
     };
-    AddPackCallback(py::type::of(py::dict{}), dict_callback);
+    AddPackCallback((py::dict{}).type(), dict_callback);
 
     // HalBufferView.
     AddPackCallback(
-        py::type::of<HalBufferView>(),
+        py::type<HalBufferView>(),
         [](InvokeContext &c, iree_vm_list_t *list, py::handle py_value) {
           HalBufferView *bv = py::cast<HalBufferView *>(py_value);
           iree_vm_ref_t buffer_view_ref =
@@ -529,11 +533,12 @@ class InvokeStatics {
 
   // Cached modules and types. Those that involve recursive lookup within
   // our top level module, we defer. Those outside, we cache at creation.
-  py::module numpy_module_ = py::module::import("numpy");
+  py::module_ numpy_module_ = py::module_::import_("numpy");
   std::optional<py::object> runtime_module_;
-  std::optional<py::module> array_interop_module_;
+  std::optional<py::module_> array_interop_module_;
   std::optional<py::object> device_array_type_;
-  py::type hal_buffer_view_type_ = py::type::of<HalBufferView>();
+  py::type_object hal_buffer_view_type_ =
+      py::cast<py::type_object>(py::type<HalBufferView>());
 
   // Maps Python type to a PackCallback that can generically code it.
   // This will have inc_ref() called on them when added.
@@ -594,12 +599,12 @@ class ArgumentPacker {
     // Dynamic dispatch.
     if (dynamic_dispatch_) {
       IREE_TRACE_SCOPE_NAMED("ArgumentPacker::PackDynamic");
-      if (!kw_args.empty()) {
+      if (kw_args.size() != 0) {
         throw std::invalid_argument(
             "kwargs not supported for dynamic dispatch functions");
       }
 
-      VmVariantList arg_list = VmVariantList::Create(pos_args.size());
+      VmVariantList arg_list = VmVariantList::Create(py::len(pos_args));
       for (py::handle py_arg : pos_args) {
         PackCallback packer = statics_.GetGenericPackCallbackFor(py_arg);
         if (!packer) {
@@ -618,11 +623,12 @@ class ArgumentPacker {
       // Reflection based dispatch.
       std::vector<py::handle> py_args(flat_arg_packers_.size());
 
-      if (pos_args.size() > pos_only_arg_count_) {
+      auto pos_args_size = py::len(pos_args);
+      if (pos_args_size > pos_only_arg_count_) {
         std::string message("mismatched call arity: expected ");
         message.append(std::to_string(pos_only_arg_count_));
         message.append(" got ");
-        message.append(std::to_string(pos_args.size()));
+        message.append(std::to_string(pos_args_size));
         throw std::invalid_argument(std::move(message));
       }
 
@@ -639,14 +645,14 @@ class ArgumentPacker {
           found_index = py::cast<int>(kwarg_to_index_[it.first]);
         } catch (std::exception &) {
           std::string message("specified kwarg '");
-          message.append(py::cast<py::str>(it.first));
+          message.append(py::cast<std::string>(it.first));
           message.append("' is unknown");
           throw std::invalid_argument(std::move(message));
         }
         if (py_args[found_index]) {
           std::string message(
               "mismatched call arity: duplicate keyword argument '");
-          message.append(py::cast<py::str>(it.first));
+          message.append(py::cast<std::string>(it.first));
           message.append("'");
           throw std::invalid_argument(std::move(message));
         }
@@ -693,11 +699,12 @@ class ArgumentPacker {
 
 }  // namespace
 
-void SetupInvokeBindings(pybind11::module &m) {
+void SetupInvokeBindings(nanobind::module_ &m) {
   py::class_<InvokeStatics>(m, "_InvokeStatics");
   py::class_<InvokeContext>(m, "InvokeContext").def(py::init<HalDevice &>());
   py::class_<ArgumentPacker>(m, "ArgumentPacker")
-      .def(py::init<InvokeStatics &, std::optional<py::list>>())
+      .def(py::init<InvokeStatics &, std::optional<py::list>>(),
+           py::arg("statics"), py::arg("arg_descs") = py::none())
       .def("pack", &ArgumentPacker::Pack);
 
   m.attr("_invoke_statics") = py::cast(InvokeStatics());
