5e6deb1bc8fcee34f5427b0c01d56f6502cd8eb3
Eugene Zhulenev <ezhulenev@google.com>
2023-06-26 18:01:21 -0700

    [HAL] Fix a bug in buffer_view.buffer folder

--- compiler/src/iree/compiler/Dialect/HAL/IR/HALOpFolders.cpp
+++ compiler/src/iree/compiler/Dialect/HAL/IR/HALOpFolders.cpp
@@ -165,17 +165,24 @@ void BufferViewCreateOp::getCanonicalizationPatterns(RewritePatternSet &results,
 namespace {
 
 /// Skips a hal.buffer_view.buffer accessor when the buffer view was created in
-/// the same scope and we know the origin buffer.
+/// the same scope at zero offset and we know the origin buffer.
 struct SkipBufferViewBufferOp : public OpRewritePattern<BufferViewBufferOp> {
   using OpRewritePattern<BufferViewBufferOp>::OpRewritePattern;
 
   LogicalResult matchAndRewrite(BufferViewBufferOp op,
                                 PatternRewriter &rewriter) const override {
-    if (auto createOp = dyn_cast_or_null<BufferViewCreateOp>(
-            op.getBufferView().getDefiningOp())) {
+    auto createOp = dyn_cast_or_null<BufferViewCreateOp>(
+        op.getBufferView().getDefiningOp());
+    if (!createOp)
+      return failure();
+
+    APInt offset;
+    if (matchPattern(createOp.getSourceOffset(), m_ConstantInt(&offset)) &&
+        offset.isZero()) {
       rewriter.replaceOp(op, createOp.getSourceBuffer());
       return success();
     }
+
     return failure();
   }
 };
