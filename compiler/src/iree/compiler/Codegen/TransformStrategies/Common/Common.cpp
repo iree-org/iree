4306175a3fef8965342ab1e42827835f2b911317
Matthias Springer <me@m-sp.org>
2023-07-06 08:35:38 +0200

    fix transform dialect build

--- compiler/src/iree/compiler/Codegen/TransformStrategies/Common/Common.cpp
+++ compiler/src/iree/compiler/Codegen/TransformStrategies/Common/Common.cpp
@@ -272,11 +272,14 @@ Value mlir::iree_compiler::buildPad(
   SmallVector<Attribute> transposeAttrs;
   for (auto &transp : transposePaddings)
     transposeAttrs.push_back(b.getI64ArrayAttr(transp));
-  return b.create<transform::PadOp>(
-      opH.getType(), opH, b.getArrayAttr(paddingValues),
-      b.getI64ArrayAttr(paddingDimensions), b.getI64ArrayAttr(padToMultipleOf),
-      b.getI64ArrayAttr(packingDimensions), b.getArrayAttr(transposeAttrs),
-      /*copyBack=*/b.getBoolAttr(false));
+  return b
+      .create<transform::PadOp>(
+          opH.getType(), opH, b.getArrayAttr(paddingValues),
+          b.getI64ArrayAttr(paddingDimensions),
+          b.getI64ArrayAttr(padToMultipleOf),
+          b.getI64ArrayAttr(packingDimensions), b.getArrayAttr(transposeAttrs),
+          /*copyBack=*/b.getBoolAttr(false))
+      ->getResult(0);
 }
 
 /// Apply patterns and vectorize.
