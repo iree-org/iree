51ffef9b2d79cb9bfc1293c4d151a4408cbca7ca
Jerry Wu <cheyuw@google.com>
2023-07-06 17:51:58 +0000

    Add comments

--- build_tools/python/e2e_test_artifacts/utils.py
+++ build_tools/python/e2e_test_artifacts/utils.py
@@ -7,7 +7,10 @@
 
 import re
 
-# The characters unsafe for CMake and file systems.
+# The characters unsafe for CMake and file systems. It's the negation of the
+# allowed list, which is derived from the join of
+# https://cmake.org/cmake/help/v3.27/policy/CMP0037.html and
+# https://www.mtu.edu/umc/services/websites/writing/characters-avoid/.
 UNSAFE_CHARACTERS = re.compile(r"[^0-9a-zA-Z\-_.]")
 
 
