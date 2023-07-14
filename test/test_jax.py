d2c05f61366d4dd327c62c478f0c40f08528489d
Rob Suderman <suderman@google.com>
2023-07-14 09:39:31 -0700

    Include a full triage with multiprocessing improvements (#227)

--- test/test_jax.py
+++ test/test_jax.py
@@ -5,9 +5,14 @@
 import argparse
 import multiprocessing
 import os
+import random
 import re
 import subprocess
 import sys
+import time
+
+from collections import namedtuple
+from enum import Enum
 
 parser = argparse.ArgumentParser(prog='test_jax.py',
                                  description='Run jax testsuite hermetically')
@@ -27,54 +32,87 @@ PYTEST_CMD = [
 ]
 
 
+def get_test(test):
+  print("Fetching from:", test)
+  stdout = subprocess.run(PYTEST_CMD + ["--setup-only", test],
+                          capture_output=True)
+  lst = re.findall('::[^ ]*::[^ ]*', str(stdout))
+  return [test + func for func in lst]
+
 def get_tests(tests):
   fulltestlist = []
-  for test in sorted(tests):
-    print("Fetching from:", test)
-    stdout = subprocess.run(PYTEST_CMD + ["--setup-only", test],
-                            capture_output=True)
-    testlist = re.findall('::[^ ]*::[^ ]*', str(stdout))
-    fulltestlist += [test + func for func in testlist]
+  with multiprocessing.Pool(os.cpu_count()) as p:
+    fulltestlist = p.map(get_test, tests)
+  fulltestlist = sorted([i for lst in fulltestlist for i in lst])
   return fulltestlist
 
 
-def generate_test_commands(tests, timeout=False):
-  cmd = ["timeout", f"{args.timeout}"] if timeout else []
-  cmd += PYTEST_CMD
+def generate_test_commands(tests):
   cmds = []
   for test in tests:
-    test_cmd = cmd + [test]
+    test_cmd = PYTEST_CMD + [test]
     cmds.append(test_cmd)
 
   return cmds
 
+TestCase = namedtuple('TestCase', ['test', 'timeout'])
+TestResult = Enum('TestResult', ['SUCCESS', 'FAILURE', 'TIMEOUT'])
 
-def exec_test(command):
+def exec_test(testcase):
+  command, timeout = testcase
+  if float(timeout) > 0:
+    command = ["timeout", f"{timeout}"] + command
+
+  start = time.perf_counter()
   result = subprocess.run(command, capture_output=True)
+  end = time.perf_counter()
+  ellapsed = end - start
+  timedout = (float(timeout) > 0) and (ellapsed > float(timeout))
+
   if result.returncode == 0:
     sys.stdout.write(".")
-  else:
-    sys.stdout.write("f")
+    sys.stdout.flush()
+    return TestResult.SUCCESS
+
+  if timedout:
+    sys.stdout.write("t")
+    sys.stdout.flush()
+    return TestResult.TIMEOUT
+
+  sys.stdout.write("f")
   sys.stdout.flush()
-  return result.returncode
+  return TestResult.FAILURE
 
 
-def exec_testsuite(commands):
-  returncodes = []
-  with multiprocessing.Pool(int(args.jobs) if args.jobs else args.jobs) as p:
-    returncodes = p.map(exec_test, commands)
-    print("")
-  passing = []
-  failing = []
-  for code, cmd in zip(returncodes, commands):
-    testname = " ".join(cmd)
+def exec_testsuite(commands, jobs, timeout):
+  random.shuffle(commands)
+  withTimeout = list(map(lambda x : TestCase(x, timeout), commands))
+
+  results = []
+  with multiprocessing.Pool(int(jobs)) as p:
+    results = p.map(exec_test, withTimeout)
+
+  passing, timeout, failing = [], [], []
+  for result, cmd in zip(results, commands):
+    if result == TestResult.SUCCESS:
+      passing.append(cmd)
+
+    if result == TestResult.TIMEOUT:
+      timeout.append(cmd)
+
+    if result == TestResult.FAILURE:
+      failing.append(cmd)
+  print("")
+
+  return passing, timeout, failing
+
+def get_testnames(cmd):
+  names = []
+  for c in cmd:
+    testname = " ".join(c)
     testname = re.search("[^ /]*::[^ ]*::[^ ]*", testname)[0]
-
-    if code == 0:
-      passing.append(testname)
-    else:
-      failing.append(testname)
-  return passing, failing
+    names.append(testname)
+  return names
 
 
 def write_results(filename, results):
@@ -106,18 +144,27 @@ print("Querying All Tests")
 tests = get_tests(args.testfiles)
 
 print("Generating test suite")
-test_commands = generate_test_commands(tests, timeout=True)
+commands = generate_test_commands(tests)
 
-print(f"Executing {len(test_commands)} tests hermetically")
-passing, failing = exec_testsuite(test_commands)
+print(f"Executing {len(commands)} tests across {args.jobs} threads with timeout = {args.timeout}")
+passing, timeout, failing = exec_testsuite(commands, jobs=args.jobs, timeout=args.timeout)
+
 expected = load_results(args.expected)
+
+# Break into passing vs failing
+failing = failing + timeout
+
+# Get the testnames
+passing = get_testnames(passing)
+failing = get_testnames(failing)
 
 write_results(args.passing, passing)
 write_results(args.failing, failing)
 
-print("Total:", len(test_commands))
+print("Total:", len(commands))
 print("Passing:", len(passing))
 print("Failing:", len(failing))
+print("Failing (timed out):", len(timeout))
 
 if expected:
   new_passing, new_failures = compare_results(expected, passing)
--- test/triage_jaxtest.py
+++ test/triage_jaxtest.py
@@ -5,6 +5,7 @@
 import argparse
 import jaxlib.mlir.ir as mlir_ir
 import jax._src.interpreters.mlir as mlir
+import multiprocessing
 import os
 import re
 
@@ -12,6 +13,7 @@ parser = argparse.ArgumentParser(prog='triage_jaxtest.py',
                                  description='Triage the jax tests')
 parser.add_argument('-l', '--logdir', default="/tmp/jaxtest")
 parser.add_argument('-d', '--delete', default=False)
+parser.add_argument('-j', '--jobs', default=None)
 args = parser.parse_args()
 
 tests = set(os.listdir(args.logdir))
@@ -28,7 +30,7 @@ def filter_to_failures(tests):
 
 
 def check_custom_call(errortxt, mlirbc, __):
-  return "mhlo.custom_call" in errortxt or "stablehlo.custom_call" in mlirbc
+  return "stablehlo.custom_call" in errortxt or "stablehlo.custom_call" in mlirbc
 
 
 def check_load_ui(errortxt, _, __):
@@ -52,15 +54,7 @@ def check_cross_replica(errortxt, _, __):
 
 
 def check_collective(errortxt, _, __):
-  return "mhlo.collective" in errortxt
-
-
-def check_optimization_barrier(errortxt, _, __):
-  return "util.optimization_barrier" in errortxt
-
-
-def check_sort_i1(errortxt, _, __):
-  return "'iree_linalg_ext.sort' op region block argument #" in errortxt and " should be of type 'i8' but got 'i1'" in errortxt
+  return "stablehlo.collective" in errortxt or "UNIMPLEMENTED; collectives not implemented" in errortxt
 
 
 def check_sort_shape(errortxt, _, __):
@@ -78,11 +72,21 @@ def check_complex(errortxt, mlirbc, __):
   return "complex<" in errortxt or "complex<" in mlirbc
 
 
-def check_rng_bit(errortxt, _, __):
-  return "mhlo.rng_bit_generator" in errortxt
+def check_timeout(errortxt, _, __):
+  return "jaxlib.xla_extension.XlaRuntimeError: ABORTED: ABORTED" in errortxt
+
+
+def check_rng_bit_i8(_, mlirbc, __):
+  lines = mlirbc.split("\n")
+  for line in lines:
+    if "stablehlo.rng_bit_generator" in line and "i8" in line:
+      return True
+  return False
 
 
 def check_min_max_f16(errortxt, _, __):
+  if "undefined symbol: fminf" in errortxt:
+    return True
   lines = errortxt.split("\n")
   for line in lines:
     has_fmax = "llvm.intr.vector.reduce.fmax" in line
@@ -104,12 +108,8 @@ def check_scatter_ui(errortxt, _, __):
   return False
 
 
-def check_scatter_i1(errortxt, _, __):
-  return "'iree_linalg_ext.scatter' op mismatch in argument 0 of region 'i1' and element type of update value 'i8'" in errortxt
-
-
 def check_bitcast_bf16(errortxt, _, __):
-  return "bf16" in errortxt and "`arith.bitcast` op operand type"
+  return "bf16" in errortxt and "`arith.bitcast` op operand type" in errortxt
 
 
 def check_constant_bf16(errortxt, _, __):
@@ -117,11 +117,15 @@ def check_constant_bf16(errortxt, _, __):
 
 
 def check_triangular_solve(errortxt, _, __):
-  return "mhlo.triangular_solve" in errortxt
+  return "stablehlo.triangular_solve" in errortxt
 
 
 def check_cholesky(errortxt, _, __):
-  return "mhlo.cholesky" in errortxt
+  return "stablehlo.cholesky" in errortxt
+
+
+def check_fft(_, mlirbc, __):
+  return "stablehlo.fft" in mlirbc
 
 
 def check_schedule_allocation(errortxt, _, __):
@@ -157,57 +161,194 @@ def check_scatter(errortxt, _, __):
   return "'iree_linalg_ext.scatter' op mismatch in shape of indices and update value at dim#0" in errortxt
 
 
+def check_shape_cast(errortxt, _, __):
+  return "'vector.shape_cast' op source/result number of elements must match" in errortxt
+
+
 def check_scatter_crash(_, mlirbc, runtime_crash):
   return "stablehlo.scatter" in mlirbc and runtime_crash
 
 
-def check_runtime_crash(errortxt, _, runtime_crash):
+def check_eigen_decomposition(errortxt, _, __):
+  return "Nonsymmetric eigendecomposition is only implemented on the CPU backend" in errortxt
+
+
+def check_jax_unimplemented(errortxt, _, __):
+  return "NotImplementedError: MLIR translation rule for primitive" in errortxt
+
+
+def check_serialize_exe(errortxt, _, __):
+  return "UNIMPLEMENTED; PJRT_Executable_Serialize" in errortxt
+
+
+def check_optimized_prgrm(errortxt, _, __):
+  return "UNIMPLEMENTED; PJRT_Executable_OptimizedProgram" in errortxt
+
+
+def check_optimized_program(errortxt, _, __):
+  return "UNIMPLEMENTED; PJRT_Executable_OptimizedProgram" in errortxt
+
+
+def check_donation(errortxt, _, __):
+  return "Donation is not implemented for iree_cpu" in errortxt
+
+
+def check_semaphore_overload(errortxt, _, __):
+  return "OUT_OF_RANGE; semaphore values must be monotonically increasing;" in errortxt
+
+
+def check_python_callback(errortxt, _, __):
+  return "ValueError: `EmitPythonCallback` not supported" in errortxt
+
+
+def check_complex_convolution(errortxt, mlirbc, __):
+  if "failed to legalize operation 'complex.constant'" in errortxt:
+    return True
+
+  for line in mlirbc.split("\n"):
+    has_i1 = re.search("tensor<([0-9]*x)*complex<f[0-9]*>>", line)
+    has_conv = re.search("stablehlo.convolution", line)
+    has_dot = re.search("stablehlo.dot", line)
+    if has_i1 and (has_conv or has_dot):
+      return True
+  return False
+
+
+def check_subspan(errortxt, _, __):
+  return "failed to legalize operation 'hal.interface.binding.subspan' that was explicitly marked illegal" in errortxt
+
+
+def check_from_tensor(errortxt, _, __):
+  return "error: 'tensor.from_elements' op unhandled tensor operation" in errortxt
+
+
+def check_unknown_backend(errortxt, _, __):
+  return "RuntimeError: Unknown backend" in errortxt
+
+
+def check_unsigned_topk(_, mlirbc, __):
+  for line in mlirbc.split("\n"):
+    if "xui" in line and "chlo.top_k" in line:
+      return True
+  return False
+
+
+def check_runtime_crash(__, _, runtime_crash):
   return runtime_crash
 
 
+def check_aborted(errortxt, _, __):
+  return "ABORTED" in errortxt
+
+
+def check_bounds_indexing(errortxt, _, __):
+  return "out-of-bounds indexing for array of shape" in errortxt
+
+
+def check_nan_correctness(errortxt, _, __):
+  return "nan location mismatch" in errortxt
+
+
+def check_pointer_mismatch(errortxt, _, __):
+  return "unsafe_buffer_pointer()" in errortxt
+
+
+def check_select_and_scatter(errortxt, _, __):
+  return "failed to legalize operation 'stablehlo.select_and_scatter'" in errortxt
+
+
+def check_degenerate_scatter(errortxt, _, __):
+  return "'iree_linalg_ext.scatter' op operand #2 must be ranked tensor or memref of any type values" in errortxt
+
+
+def check_cost_analysis(errortxt, _, __):
+  return "cost_analysis()" in errortxt
+
+
+def check_invalid_option(errortxt, _, __):
+  return "No such compile option: 'invalid_key'" in errortxt
+
+
+def check_inf_mismatch(errortxt, _, __):
+  return "inf location mismatch" in errortxt
+
+
+def check_shape_assertion(errortxt, _, __):
+  for line in errortxt.split("\n"):
+    if "assertEqual" in line and ".shape" in line:
+      return True
+  return False
+
+
+def check_vector_contract(errortxt, _, __):
+  return "'vector.contract' op failed to verify that lhs and rhs have same element type" in errortxt
+
+
+def check_subbyte_read(errortxt, _, __):
+  return "opaque and sub-byte aligned element types cannot be indexed" in errortxt
+
+
+def check_buffer_usage(errortxt, _, __):
+  return "requested buffer usage is not supported" in errortxt or "tensor requested usage was not specified when the buffer" in errortxt or "PERMISSION_DENIED; requested usage was not specified when the buffer was allocated; buffer allows DISPATCH_INDIRECT_PARAMS" in errortxt
+
+
+def check_subbyte_singleton(errortxt, _, __):
+  return "does not have integral number of total bytes" in errortxt
+
+
+def check_max_arg(errortxt, _, __):
+  return "max() arg is an empty sequence" in errortxt
+
+
+def check_double_support(errortxt, _, __):
+  return "expected f32 (21000020) but have f64 (21000040)" in errortxt
+
+
+def check_stablehlo_degenerate(_, mlirbc, __):
+  for line in mlirbc.split("\n"):
+    if "stablehlo" in line and ("x0x" in line or "<0x" in line):
+      return True
+  return False
+
+
+def check_stablehlo_allreduce(errortxt, _, __):
+  return "failed to legalize operation 'stablehlo.all_reduce'" in errortxt
+
+
+def check_dot_shape(errortxt, _, __):
+  for line in errortxt.split("\n"):
+    if "error: inferred shape" in line and "is incompatible with return type of operation " in line:
+      return True
+  return False
+
+
 KnownChecks = {
-    "https://github.com/openxla/iree/issues/13544 (complex)":
-        check_complex,
-    "https://github.com/openxla/iree/issues/11761 (rng bit gen)":
-        check_rng_bit,
-    "https://github.com/openxla/iree/issues/13427 (scatter i1)":
-        check_scatter_i1,
+    "https://github.com/openxla/iree/issues/14255 (detensoring)":
+        check_from_tensor,
+    "https://github.com/openxla/iree/issues/????? (unknown)":
+        check_jax_unimplemented,
+    "https://github.com/openxla/iree/issues/13726 (collective)":
+        check_collective,
     "https://github.com/openxla/iree/issues/12410 (custom call)":
         check_custom_call,
     "https://github.com/openxla/iree/issues/11018 (triangle)":
         check_triangular_solve,
+    "https://github.com/openxla/iree/issues/12263 (fft)":
+        check_fft,
+    "https://github.com/openxla/iree/issues/14072 (complex convolution)":
+        check_complex_convolution,
     "https://github.com/openxla/iree/issues/10816 (cholesky)":
         check_cholesky,
-    "https://github.com/openxla/iree/issues/13720 (topk bf16)":
-        check_topk_bf16,
-    "https://github.com/openxla/iree/issues/13723 (reverse ui)":
-        check_reverse_i1,
+    "https://github.com/openxla/iree/issues/11761 (rng bit gen i8)":
+        check_rng_bit_i8,
+    "https://github.com/openxla/iree/issues/????? (eigen decomp)":
+        check_eigen_decomposition,
     "https://github.com/openxla/iree/issues/13579 (scatter ui)":
         check_scatter_ui,
-    "https://github.com/openxla/iree/issues/13576 (flow.load ui)":
-        check_load_ui,
-    "https://github.com/openxla/iree/issues/13724 (flow.splat ui)":
-        check_splat_ui,
     "https://github.com/openxla/iree/issues/13725 (cross repl)":
         check_cross_replica,
-    "https://github.com/openxla/iree/issues/13726 (collective)":
-        check_collective,
-    "https://github.com/openxla/iree/issues/13727 (optbarrier)":
-        check_optimization_barrier,
-    "https://github.com/openxla/iree/issues/13723 (sort i1)":
-        check_sort_i1,
-    "https://github.com/openxla/iree/issues/13728 (sort shape)":
-        check_sort_shape,
-    "https://github.com/openxla/iree/issues/13722 (bitcast bf16)":
-        check_bitcast_bf16,
-    "https://github.com/openxla/iree/issues/13721 (constant bf16)":
-        check_constant_bf16,
-    "https://github.com/openxla/iree/issues/13729 (schedule)":
-        check_schedule_allocation,
     "https://github.com/openxla/iree/issues/13493 (dot i1)":
         check_dot_i1,
-    "https://github.com/openxla/iree/issues/13507 (vectorize)":
-        check_vectorize,
     "https://github.com/openxla/iree/issues/13522 (roundeven)":
         check_roundeven,
     "https://github.com/openxla/iree/issues/13577 (max/min f16)":
@@ -216,6 +357,60 @@ KnownChecks = {
         check_scatter,
     "https://github.com/openxla/iree/issues/13580 (scatter crash)":
         check_scatter_crash,
+    "https://github.com/openxla/iree/issues/14079 (shape_cast)":
+        check_shape_cast,
+    "https://github.com/openxla/iree/issues/????? (optimized prgrm)":
+        check_optimized_program,
+    "https://github.com/openxla/iree/issues/????? (donation)":
+        check_donation,
+    "https://github.com/openxla/iree/issues/????? (python callback)":
+        check_python_callback,
+    "https://github.com/openxla/iree/issues/????? (subspan)":
+        check_subspan,
+    "https://github.com/openxla/iree/issues/14098 (unsigned topk)":
+        check_unsigned_topk,
+    "https://github.com/openxla/iree/issues/????? (bounds indexing)":
+        check_bounds_indexing,
+    "https://github.com/openxla/iree/issues/????? (nan correctness)":
+        check_nan_correctness,
+    "https://github.com/openxla/iree/issues/????? (pointer mismatch)":
+        check_pointer_mismatch,
+    "https://github.com/openxla/iree/issues/10841 (select and scatter)":
+        check_select_and_scatter,
+    "https://github.com/openxla/iree/issues/????? (degenerate scatter)":
+        check_degenerate_scatter,
+    "https://github.com/openxla/iree/issues/????? (cost analysis)":
+        check_cost_analysis,
+    "https://github.com/openxla/iree/issues/????? (invalid option)":
+        check_invalid_option,
+    "https://github.com/openxla/iree/issues/????? (inf mismatch)":
+        check_inf_mismatch,
+    "https://github.com/openxla/iree/issues/????? (shape assertion)":
+        check_shape_assertion,
+    "https://github.com/openxla/iree/issues/????? (vector contract)":
+        check_vector_contract,
+    "https://github.com/openxla/iree/issues/????? (subbyte indexed)":
+        check_subbyte_read,
+    "https://github.com/openxla/iree/issues/????? (buffer usage)":
+        check_buffer_usage,
+    "https://github.com/openxla/iree/issues/????? (subbyte singleton)":
+        check_subbyte_singleton,
+    "https://github.com/openxla/iree/issues/????? (max arg)":
+        check_max_arg,
+    "https://github.com/openxla/iree/issues/????? (double support)":
+        check_double_support,
+    "https://github.com/openxla/iree/issues/????? (zero extent)":
+        check_stablehlo_degenerate,
+    "https://github.com/openxla/iree/issues/????? (all reduce)":
+        check_stablehlo_allreduce,
+    "https://github.com/openxla/iree/issues/????? (stablehlo dot_general)":
+        check_dot_shape,
+    "(unknown backend)":
+        check_unknown_backend,
+    "(semaphore)":
+        check_semaphore_overload,
+    "Aborted (possible timeout)":
+        check_aborted,
     "Runtime Crash":
         check_runtime_crash,
     "Compilation Failure":
@@ -227,35 +422,42 @@ KnownChecks = {
 }
 
 
+def triage_test(test):
+  files = sorted(os.listdir(f'{args.logdir}/{test}'))
+  # Load the error.txt if it is available.
+  error = ""
+  if "error.txt" in files:
+    with open(f'{args.logdir}/{test}/error.txt') as f:
+      error = "".join(f.readlines())
+
+  # Load the last bytecode file that was attempted to be compiled:
+  mlirbc_count = len([f for f in files if "mlirbc" in f])
+  mlirbc_name = f'{mlirbc_count - 1}-program.mlirbc'
+  vmfb_name = f'{mlirbc_count - 1}-program.vmfb'
+
+  runtime_crash = "CRASH_MARKER" in files
+
+  mlirbc = ""
+  if mlirbc_count > 0:
+    with mlir.make_ir_context() as ctx:
+      with open(f'{args.logdir}/{test}/{mlirbc_name}', 'rb') as f:
+        mlirbc = f.read()
+      mlirbc = str(mlir_ir.Module.parse(mlirbc))
+
+  for checkname in KnownChecks:
+    if KnownChecks[checkname](error, mlirbc, runtime_crash):
+      return checkname
+
+  return "unknown error"
+
+
 def filter_error_mapping(tests):
   error_mapping = {}
-  for test in tests:
-    files = sorted(os.listdir(f'{args.logdir}/{test}'))
-    # Load the error.txt if it is available.
-    error = ""
-    if "error.txt" in files:
-      with open(f'{args.logdir}/{test}/error.txt') as f:
-        error = "".join(f.readlines())
+  with multiprocessing.Pool(int(args.jobs) if args.jobs else args.jobs) as p:
+    results = p.map(triage_test, tests)
 
-    # Load the last bytecode file that was attempted to be compiled:
-    mlirbc_count = len([f for f in files if "mlirbc" in f])
-    mlirbc_name = f'{mlirbc_count - 1}-program.mlirbc'
-    vmfb_name = f'{mlirbc_count - 1}-program.vmfb'
-
-    runtime_crash = "CRASH_MARKER" in files and vmfb_name in files
-
-    mlirbc = ""
-    if mlirbc_count > 0:
-      with mlir.make_ir_context() as ctx:
-        with open(f'{args.logdir}/{test}/{mlirbc_name}', 'rb') as f:
-          mlirbc = f.read()
-        mlirbc = str(mlir_ir.Module.parse(mlirbc))
-
-    error_mapping[test] = "unknown error"
-    for checkname in KnownChecks:
-      if KnownChecks[checkname](error, mlirbc, runtime_crash):
-        error_mapping[test] = checkname
-        break
+  for test, result in zip(tests, results):
+    error_mapping[test] = result
   return error_mapping
 
 
@@ -269,13 +471,19 @@ def generate_summary(mapping):
 
 
 def print_summary(summary):
+  maxlen = 0
   for error in summary:
-    print(f'{error:<60} : {len(summary[error])}')
+    maxlen = max(len(error), maxlen)
+  for error in summary:
+    print(f'{error:<{maxlen}} : {len(summary[error])}')
+
+  passstr = "Passing"
+  failstr = "Failing"
+  print(f'{passstr:<{maxlen}} : {len(tests) - len(failing)}')
+  print(f'{failstr:<{maxlen}} : {len(failing)}')
 
 
 failing = filter_to_failures(tests)
 mapping = filter_error_mapping(failing)
 summary = generate_summary(mapping)
 print_summary(summary)
-print("{:<60} : {}".format("Passing", len(tests) - len(failing)))
-print("{:<60} : {}".format("Failing", len(failing)))
