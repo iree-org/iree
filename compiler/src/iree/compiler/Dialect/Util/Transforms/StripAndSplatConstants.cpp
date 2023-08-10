ca51caa593e727adf1a995d69c5ed94b95da8bdb
Ben Vanik <ben.vanik@gmail.com>
2023-08-10 14:39:57 -0700

    Making `iree-util-strip-and-splat-constants` use byte_patterns. (#14627)
    Had to remove the path someone added for arith.constant as that can't
    take byte patterns (and it wasn't the intent of the pass). If someone
    wants that they can hoist into globals first before running this pass
    with `--iree-util-outline-constants`.
    
    A common usage of this would be to take a giant mlir file and run it
    through iree-opt with
    `--iree-util-outline-constants --iree-util-strip-and-splat-constants` to
    outline any arith.constant values into globals and then strip them.
    
    Note that the final VMFB will contain the same number of bytes as the
    original unstripped file.

---
+++ compiler/src/iree/compiler/Dialect/Util/Transforms/StripAndSplatConstants.cpp
@@ -0,0 +1,64 @@
+// Copyright 2020 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <utility>
+
+#include "iree/compiler/Dialect/Util/IR/UtilDialect.h"
+#include "iree/compiler/Dialect/Util/IR/UtilOps.h"
+#include "iree/compiler/Dialect/Util/Transforms/PassDetail.h"
+#include "iree/compiler/Dialect/Util/Transforms/Passes.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/Pass/Pass.h"
+
+namespace mlir {
+namespace iree_compiler {
+namespace IREE {
+namespace Util {
+
+class StripAndSplatConstantsPass
+    : public StripAndSplatConstantsBase<StripAndSplatConstantsPass> {
+public:
+  StripAndSplatConstantsPass() = default;
+
+  void getDependentDialects(DialectRegistry &registry) const override {
+    registry.insert<IREE::Util::UtilDialect>();
+  }
+
+  void runOnOperation() override {
+    auto moduleOp = getOperation();
+
+    // Give each splatted value a module-unique byte value so that it's easier
+    // to track back to where it came from in the final output.
+    int replaceIndex = 1;
+    auto getSplatAttr = [&](ShapedType type) {
+      return IREE::Util::BytePatternAttr::get(moduleOp.getContext(), type,
+                                              replaceIndex++);
+    };
+
+    moduleOp.walk([&](Operation *op) {
+      if (auto globalOp = dyn_cast<Util::GlobalOp>(op)) {
+        if (auto initialValue = globalOp.getInitialValueAttr()) {
+          if (auto shapedType = dyn_cast<ShapedType>(initialValue.getType())) {
+            globalOp.setInitialValueAttr(getSplatAttr(shapedType));
+          }
+        }
+      }
+    });
+  }
+};
+
+std::unique_ptr<OperationPass<mlir::ModuleOp>>
+createStripAndSplatConstantsPass() {
+  return std::make_unique<StripAndSplatConstantsPass>();
+}
+
+} // namespace Util
+} // namespace IREE
+} // namespace iree_compiler
+} // namespace mlir
