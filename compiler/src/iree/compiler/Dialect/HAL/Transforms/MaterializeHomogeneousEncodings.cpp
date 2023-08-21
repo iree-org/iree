bf889bdb8062c74d337deb63f518e76999db2a6d
Hanhan Wang <hanchung@google.com>
2023-08-21 16:20:53 -0700

    do not include unused header

--- compiler/src/iree/compiler/Dialect/HAL/Transforms/MaterializeHomogeneousEncodings.cpp
+++ compiler/src/iree/compiler/Dialect/HAL/Transforms/MaterializeHomogeneousEncodings.cpp
@@ -4,7 +4,6 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#include "iree/compiler/Codegen/Common/CPU/Passes.h"
 #include "iree/compiler/Dialect/HAL/IR/HALDialect.h"
 #include "iree/compiler/Dialect/HAL/IR/HALOps.h"
 #include "iree/compiler/Dialect/HAL/Target/TargetBackend.h"
