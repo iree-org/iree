iree-org/iree

commit 617a1d8fc39d22af233616b406c0f6f85568eef6

Fixing GCC warning on pointer passed into tracy on frees. (#14721)

runtime/src/iree/base/allocator.c
@@ -94,6 +94,7 @@ static iree_status_t iree_allocator_system_alloc(
     }
   });
 
+  void* existing_ptr_value = iree_tracing_obscure_ptr(existing_ptr);
   void* new_ptr = NULL;
   if (existing_ptr && command == IREE_ALLOCATOR_COMMAND_REALLOC) {
     new_ptr = realloc(existing_ptr, byte_length);
@@ -110,8 +111,8 @@ static iree_status_t iree_allocator_system_alloc(
                             "system allocator failed the request");
   }
 
-  if (existing_ptr) {
-    IREE_TRACE_FREE(existing_ptr);
+  if (existing_ptr_value) {
+    IREE_TRACE_FREE(existing_ptr_value);
   }
   IREE_TRACE_ALLOC(new_ptr, byte_length);
 

runtime/src/iree/base/tracing/console.h
@@ -233,6 +233,8 @@ void iree_tracing_message_string_view(const char* value, size_t value_length,
 
 #if IREE_TRACING_FEATURES & IREE_TRACING_FEATURE_ALLOCATION_TRACKING
 
+static inline void* iree_tracing_obscure_ptr(void* ptr) { return ptr; }
+
 // TODO(benvanik): console tracing allocation support. We could just print out
 // pointers or keep track of statistics and dump them in IREE_TRACE_APP_EXIT.
 #define IREE_TRACE_ALLOC(ptr, size)

runtime/src/iree/base/tracing.h
@@ -306,6 +306,8 @@ enum {
 
 #if !(IREE_TRACING_FEATURES & IREE_TRACING_FEATURE_ALLOCATION_TRACKING)
 
+static void* iree_tracing_obscure_ptr(void* ptr) { return ptr; }
+
 // Traces a new memory allocation with host |ptr| and the given |size|.
 // A balanced IREE_TRACE_FREE on the same |ptr| is required for proper memory
 // tracking. Allocations will be attributed to their parent zone.

runtime/src/iree/base/tracing/tracy.cc
@@ -321,6 +321,8 @@ void iree_tracing_gpu_zone_notify(uint8_t context_id, uint16_t query_id,
 
 #endif  // IREE_TRACING_FEATURE_INSTRUMENTATION_DEVICE
 
+void* iree_tracing_obscure_ptr(void* ptr) { return ptr; }
+
 #endif  // IREE_TRACING_FEATURES
 
 #ifdef __cplusplus

runtime/src/iree/base/tracing/tracy.h
@@ -169,6 +169,8 @@ void iree_tracing_gpu_zone_notify(uint8_t context_id, uint16_t query_id,
 
 #endif  // IREE_TRACING_FEATURE_INSTRUMENTATION_DEVICE
 
+void* iree_tracing_obscure_ptr(void* ptr);
+
 #endif  // IREE_TRACING_FEATURES
 
 #ifdef __cplusplus

commit 308637a1b8b48c9004787828a7776f6ad5c62587

Fixing tracing balance bug introduced in #14721. (#14801)
Also adding allocation logging to the console tracing provider.

runtime/src/iree/base/allocator.c
@@ -94,9 +94,10 @@ static iree_status_t iree_allocator_system_alloc(
     }
   });
 
-  void* existing_ptr_value = iree_tracing_obscure_ptr(existing_ptr);
+  void* existing_ptr_value = NULL;
   void* new_ptr = NULL;
   if (existing_ptr && command == IREE_ALLOCATOR_COMMAND_REALLOC) {
+    existing_ptr_value = iree_tracing_obscure_ptr(existing_ptr);
     new_ptr = realloc(existing_ptr, byte_length);
   } else {
     existing_ptr = NULL;

runtime/src/iree/base/tracing/console.c
@@ -209,4 +209,25 @@ void iree_tracing_message_string_view(const char* value, size_t value_length,
 #endif  // IREE_TRACING_CONSOLE_FLUSH
 }
 
+void iree_tracing_memory_alloc(const char* name, size_t name_length, void* ptr,
+                               size_t size) {
+  fprintf(_console.file, "[%08" PRIX64 "][%.*s%*s] %*s● %.*s alloc %p (%zu)\n",
+          iree_thread_id(), (int)_thread.name_length, _thread.name,
+          (int)(IREE_TRACING_MAX_THREAD_LENGTH - _thread.name_length), "",
+          _thread.depth, "", (int)name_length, name, ptr, size);
+#if IREE_TRACING_CONSOLE_FLUSH
+  fflush(_console.file);
+#endif  // IREE_TRACING_CONSOLE_FLUSH
+}
+
+void iree_tracing_memory_free(const char* name, size_t name_length, void* ptr) {
+  fprintf(_console.file, "[%08" PRIX64 "][%.*s%*s] %*s◌ %.*s free %p\n",
+          iree_thread_id(), (int)_thread.name_length, _thread.name,
+          (int)(IREE_TRACING_MAX_THREAD_LENGTH - _thread.name_length), "",
+          _thread.depth, "", (int)name_length, name, ptr);
+#if IREE_TRACING_CONSOLE_FLUSH
+  fflush(_console.file);
+#endif  // IREE_TRACING_CONSOLE_FLUSH
+}
+
 #endif  // IREE_TRACING_FEATURES

runtime/src/iree/base/tracing/console.h
@@ -21,9 +21,10 @@
 
 // Filter to only supported features.
 #if !defined(IREE_TRACING_FEATURES)
-#define IREE_TRACING_FEATURES          \
-  ((IREE_TRACING_FEATURES_REQUESTED) & \
-   (IREE_TRACING_FEATURE_INSTRUMENTATION | IREE_TRACING_FEATURE_LOG_MESSAGES))
+#define IREE_TRACING_FEATURES                                                  \
+  ((IREE_TRACING_FEATURES_REQUESTED) &                                         \
+   (IREE_TRACING_FEATURE_INSTRUMENTATION | IREE_TRACING_FEATURE_LOG_MESSAGES | \
+    IREE_TRACING_FEATURE_ALLOCATION_TRACKING))
 #endif  // !IREE_TRACING_FEATURES
 
 // File handle that tracing information will be dumped to.
@@ -94,6 +95,10 @@ void iree_tracing_message_cstring(const char* value, const char* symbol,
                                   uint32_t color);
 void iree_tracing_message_string_view(const char* value, size_t value_length,
                                       const char* symbol, uint32_t color);
+
+void iree_tracing_memory_alloc(const char* name, size_t name_length, void* ptr,
+                               size_t size);
+void iree_tracing_memory_free(const char* name, size_t name_length, void* ptr);
 
 #endif  // IREE_TRACING_FEATURES
 
@@ -237,10 +242,15 @@ static inline void* iree_tracing_obscure_ptr(void* ptr) { return ptr; }
 
 // TODO(benvanik): console tracing allocation support. We could just print out
 // pointers or keep track of statistics and dump them in IREE_TRACE_APP_EXIT.
-#define IREE_TRACE_ALLOC(ptr, size)
-#define IREE_TRACE_FREE(ptr)
-#define IREE_TRACE_ALLOC_NAMED(name_literal, ptr, size)
-#define IREE_TRACE_FREE_NAMED(name_literal, ptr)
+#define IREE_TRACE_ALLOC(ptr, size) \
+  iree_tracing_memory_alloc("heap", 4, (ptr), (size))
+#define IREE_TRACE_FREE(ptr) iree_tracing_memory_free("heap", 4, (ptr))
+#define IREE_TRACE_ALLOC_NAMED(name_literal, ptr, size)                      \
+  iree_tracing_memory_alloc((name_literal), IREE_TRACE_STRLEN(name_literal), \
+                            (ptr), (size))
+#define IREE_TRACE_FREE_NAMED(name_literal, ptr)                            \
+  iree_tracing_memory_free((name_literal), IREE_TRACE_STRLEN(name_literal), \
+                           (ptr))
 
 #endif  // IREE_TRACING_FEATURE_ALLOCATION_TRACKING
 