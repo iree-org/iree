b76b6df4902c6665227c33f5ec458001021fb89c
Stella Laurenzo <stellaraccident@gmail.com>
2023-08-24 19:38:26 -0700

    Initial commit of package based CI and regression testing. (#14793)
    This patch lays a lot of new track:
    
    * New CI jobs that are rooted in building deployable packages and having
    those be used by subsequent jobs (for testing).
    * New variant of the building scripts that will eventually subsume the
    old once we rebase package building on top of this workflow (like it is
    done for downstreams).
    * A new `regression_test` project (currently under experimental) that
    has the seeds for being able to handle "big" tests and a wider variety
    of combinations than we presently have in other places like `e2e`.
    * An initial set of tests targeting both CPU and AMD/Vulkan for llama2
    7b f16/i4 to commemorate the current work the team has been doing. The
    tests are not yet super inspired, just verifying that it compiles and
    does in fact run, but I will expand them in a followup once the CI can
    guide me.
    * A regression testing job for CPU. Will add one for AMD GPU shortly
    once I finish setting up the runner.
    
    The regression_test project should be suitable for the development
    workflow too, but it needs a bit more turns and mileage on it. Consider
    this a WIP that I'll be holding carefully for some days to get it ready
    for general use.

---
+++ experimental/regression_suite/ireers/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2023 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+from .fixtures import (
+    fetch_source_fixture,
+    iree_compile,
+    iree_benchmark_module,
+    iree_run_module,
+)
---
+++ experimental/regression_suite/tests/pregenerated/test_llama2.py
@@ -0,0 +1,102 @@
+# Copyright 2023 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import pytest
+from ireers import *
+
+###############################################################################
+# Fixtures
+###############################################################################
+
+COMMON_FLAGS = [
+    "--iree-input-type=none",
+    "--iree-stream-resource-index-bits=64",
+    "--iree-vm-target-index-bits=64",
+    "--iree-opt-const-expr-hoisting=false",
+    "--iree-stream-resource-max-allocation-size=3221225472",
+]
+
+llama2_7b_f16qi4_source = fetch_source_fixture(
+    "https://storage.googleapis.com/shark_tank/stella/llama2_7b_int4_stripped.mlir",
+    group="llama2_7b_f16qi4",
+)
+
+
+@pytest.fixture
+def llama2_7b_f16qi4_rdna3_vulkan_vmfb(llama2_7b_f16qi4_source):
+    return iree_compile(
+        llama2_7b_f16qi4_source,
+        "rdna3_vulkan",
+        flags=COMMON_FLAGS
+        + [
+            "--iree-hal-target-backends=vulkan",
+            "--iree-vulkan-target-triple=rdna3-unknown-linux",
+        ],
+    )
+
+
+@pytest.fixture
+def llama2_7b_f16qi4_host_cpu_vmfb(llama2_7b_f16qi4_source):
+    return iree_compile(
+        llama2_7b_f16qi4_source,
+        "host_cpu",
+        flags=COMMON_FLAGS
+        + [
+            "--iree-hal-target-backends=llvm-cpu",
+            "--iree-llvmcpu-target-cpu-features=host",
+        ],
+    )
+
+
+###############################################################################
+# Tests
+###############################################################################
+
+
+@pytest.mark.presubmit
+@pytest.mark.unstable_linalg
+@pytest.mark.plat_rdna3_vulkan
+def test_step_rdna3_vulkan_stripped(llama2_7b_f16qi4_rdna3_vulkan_vmfb):
+    iree_benchmark_module(
+        llama2_7b_f16qi4_rdna3_vulkan_vmfb,
+        device="vulkan",
+        function="first_vicuna_forward",
+        args=[
+            "--input=1x1xi64",
+        ],
+    )
+    iree_benchmark_module(
+        llama2_7b_f16qi4_rdna3_vulkan_vmfb,
+        device="vulkan",
+        function="second_vicuna_forward",
+        args=[
+            "--input=1x1xi64",
+        ]
+        + (["--input=1x32x1x128xf16"] * 64),
+    )
+
+
+@pytest.mark.presubmit
+@pytest.mark.unstable_linalg
+@pytest.mark.plat_host_cpu
+def test_step_host_cpu_stripped(llama2_7b_f16qi4_host_cpu_vmfb):
+    iree_benchmark_module(
+        llama2_7b_f16qi4_host_cpu_vmfb,
+        device="local-task",
+        function="first_vicuna_forward",
+        args=[
+            "--input=1x1xi64",
+        ],
+    )
+    iree_benchmark_module(
+        llama2_7b_f16qi4_host_cpu_vmfb,
+        device="local-task",
+        function="second_vicuna_forward",
+        args=[
+            "--input=1x1xi64",
+        ]
+        + (["--input=1x32x1x128xf16"] * 64),
+    )
