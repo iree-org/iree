801356fa200419eaa0626db52dc7511494ae9038
bjacob <benoitjacob@google.com>
2023-09-10 20:58:26 -0400

    Un-unroll ukernel C+intrinsics code. (#14908)
    Fully unrolling C+intrinsics code is a common defensive practice vs. the
    tendency of compilers to miss good codegen of SIMD code. It's not just
    about the loops, it's about using arrays of vector-variables, which is
    necessary to be able to write loop. A sufficiently naive compiler will
    literally take that to mean that the vectors are memory objects. Several
    years ago I had filed https://bugs.llvm.org/show_bug.cgi?id=34945 and
    never heard back about it. To this day, XNNPACK sticks to this practice,
    e.g.
    https://github.com/google/XNNPACK/blob/master/src/f32-gemm/gen/f32-gemm-8x8s4-minmax-neon.c#L238-L269
    .
    
    This prompts the question of how to manage the resulting verbose code,
    how to scale to supporting many variants of ukernels. The immediate
    motivation for us here is as we are about to introduce narrow variants
    of matmul kernels. XNNPACK deals with that with a Python-based generator
    of unrolled C code. In our case, as our primary deployment path for
    ukernels is to compile them to LLVM bitcode that IREE can then inline at
    each call site and "LTO", where it should be able to perform loop
    unrolling and dead code optimization, it would be neat to simply take
    advantage of that, instead of inventing a new way to unroll loops and
    skip over dead code, or carry verbose source code.
    
    The danger is regressing performance in the native-toolchain,
    non-bitcode builds of ukernels. That's only used in VMVX, and in
    ukernel's own micro benchmarks (and unit tests). Performance of that
    isn't really critical. We want to make sure that we build correctly
    there, but it's OK to have suboptimal performance. To be clear, to
    preserve performance in the native build, we will still instantiate
    functions with the loop size known at compile time (calling into the
    shared loop impl, inlined into each case). The only question is whether
    the native toolchain will handle that inlining as well as Clang does.
    Concretely, I tried one case, and found that GCC generates ~ 2x slower
    code, while Clang and MSVC did fine. https://godbolt.org/z/WsbW487ze
    
    Just look at the code shrink here. And this is only a first step. As a
    next PR will introduce variants for narrow M0 dimensions, they will be
    able to all share the same loop implementation, both in source code and
    in embedded bitcode in the bitcode build.

--- runtime/src/iree/builtins/ukernel/arch/arm_64/mmt4d_arm_64_i8mm.c
+++ runtime/src/iree/builtins/ukernel/arch/arm_64/mmt4d_arm_64_i8mm.c
@@ -34,121 +34,52 @@ void iree_uk_mmt4d_tile_i8i8i32_8x8x8_arm_64_i8mm(
   const iree_uk_int8_t* IREE_UK_RESTRICT lhs_ptr = lhs_panel;
   const iree_uk_int8_t* IREE_UK_RESTRICT rhs_ptr = rhs_panel;
   iree_uk_int32_t* IREE_UK_RESTRICT out_ptr = out_tile;
-  int32x4_t acc_01_01, acc_01_23, acc_01_45, acc_01_67;
-  int32x4_t acc_23_01, acc_23_23, acc_23_45, acc_23_67;
-  int32x4_t acc_45_01, acc_45_23, acc_45_45, acc_45_67;
-  int32x4_t acc_67_01, acc_67_23, acc_67_45, acc_67_67;
+  // Accumulator 2x2 register tiles.
+  int32x4_t acc[4][4];
   if (params->flags & IREE_UK_FLAG_MMT4D_ACCUMULATE) {
-    int32x4_t acc_0_0123 = vld1q_s32(out_ptr + 8 * 0 + 0);
-    int32x4_t acc_0_4567 = vld1q_s32(out_ptr + 8 * 0 + 4);
-    int32x4_t acc_1_0123 = vld1q_s32(out_ptr + 8 * 1 + 0);
-    int32x4_t acc_1_4567 = vld1q_s32(out_ptr + 8 * 1 + 4);
-    int32x4_t acc_2_0123 = vld1q_s32(out_ptr + 8 * 2 + 0);
-    int32x4_t acc_2_4567 = vld1q_s32(out_ptr + 8 * 2 + 4);
-    int32x4_t acc_3_0123 = vld1q_s32(out_ptr + 8 * 3 + 0);
-    int32x4_t acc_3_4567 = vld1q_s32(out_ptr + 8 * 3 + 4);
-    int32x4_t acc_4_0123 = vld1q_s32(out_ptr + 8 * 4 + 0);
-    int32x4_t acc_4_4567 = vld1q_s32(out_ptr + 8 * 4 + 4);
-    int32x4_t acc_5_0123 = vld1q_s32(out_ptr + 8 * 5 + 0);
-    int32x4_t acc_5_4567 = vld1q_s32(out_ptr + 8 * 5 + 4);
-    int32x4_t acc_6_0123 = vld1q_s32(out_ptr + 8 * 6 + 0);
-    int32x4_t acc_6_4567 = vld1q_s32(out_ptr + 8 * 6 + 4);
-    int32x4_t acc_7_0123 = vld1q_s32(out_ptr + 8 * 7 + 0);
-    int32x4_t acc_7_4567 = vld1q_s32(out_ptr + 8 * 7 + 4);
-    acc_01_01 = iree_uk_neon_zip1_s32_as_s64(acc_0_0123, acc_1_0123);
-    acc_01_23 = iree_uk_neon_zip2_s32_as_s64(acc_0_0123, acc_1_0123);
-    acc_01_45 = iree_uk_neon_zip1_s32_as_s64(acc_0_4567, acc_1_4567);
-    acc_01_67 = iree_uk_neon_zip2_s32_as_s64(acc_0_4567, acc_1_4567);
-    acc_23_01 = iree_uk_neon_zip1_s32_as_s64(acc_2_0123, acc_3_0123);
-    acc_23_23 = iree_uk_neon_zip2_s32_as_s64(acc_2_0123, acc_3_0123);
-    acc_23_45 = iree_uk_neon_zip1_s32_as_s64(acc_2_4567, acc_3_4567);
-    acc_23_67 = iree_uk_neon_zip2_s32_as_s64(acc_2_4567, acc_3_4567);
-    acc_45_01 = iree_uk_neon_zip1_s32_as_s64(acc_4_0123, acc_5_0123);
-    acc_45_23 = iree_uk_neon_zip2_s32_as_s64(acc_4_0123, acc_5_0123);
-    acc_45_45 = iree_uk_neon_zip1_s32_as_s64(acc_4_4567, acc_5_4567);
-    acc_45_67 = iree_uk_neon_zip2_s32_as_s64(acc_4_4567, acc_5_4567);
-    acc_67_01 = iree_uk_neon_zip1_s32_as_s64(acc_6_0123, acc_7_0123);
-    acc_67_23 = iree_uk_neon_zip2_s32_as_s64(acc_6_0123, acc_7_0123);
-    acc_67_45 = iree_uk_neon_zip1_s32_as_s64(acc_6_4567, acc_7_4567);
-    acc_67_67 = iree_uk_neon_zip2_s32_as_s64(acc_6_4567, acc_7_4567);
+    // Load row-major accumulator and swizzle into 2x2 register tiles.
+    for (int i = 0; i < 4; ++i) {
+      for (int j = 0; j < 2; ++j) {
+        int32x4_t acc_1x4_0 = vld1q_s32(out_ptr + 8 * (2 * i + 0) + 4 * j);
+        int32x4_t acc_1x4_1 = vld1q_s32(out_ptr + 8 * (2 * i + 1) + 4 * j);
+        acc[i][2 * j + 0] = iree_uk_neon_zip1_s32_as_s64(acc_1x4_0, acc_1x4_1);
+        acc[i][2 * j + 1] = iree_uk_neon_zip2_s32_as_s64(acc_1x4_0, acc_1x4_1);
+      }
+    }
   } else {
-    acc_01_01 = vdupq_n_s32(0);
-    acc_01_23 = vdupq_n_s32(0);
-    acc_01_45 = vdupq_n_s32(0);
-    acc_01_67 = vdupq_n_s32(0);
-    acc_23_01 = vdupq_n_s32(0);
-    acc_23_23 = vdupq_n_s32(0);
-    acc_23_45 = vdupq_n_s32(0);
-    acc_23_67 = vdupq_n_s32(0);
-    acc_45_01 = vdupq_n_s32(0);
-    acc_45_23 = vdupq_n_s32(0);
-    acc_45_45 = vdupq_n_s32(0);
-    acc_45_67 = vdupq_n_s32(0);
-    acc_67_01 = vdupq_n_s32(0);
-    acc_67_23 = vdupq_n_s32(0);
-    acc_67_45 = vdupq_n_s32(0);
-    acc_67_67 = vdupq_n_s32(0);
-  }
-  IREE_UK_ASSUME(params->K >= 1);
-  for (int k = 0; k < params->K; ++k) {
-    int8x16_t lhs01 = vld1q_s8(lhs_ptr + 0);
-    int8x16_t lhs23 = vld1q_s8(lhs_ptr + 16);
-    int8x16_t lhs45 = vld1q_s8(lhs_ptr + 32);
-    int8x16_t lhs67 = vld1q_s8(lhs_ptr + 48);
-    lhs_ptr += 64;
-    int8x16_t rhs01 = vld1q_s8(rhs_ptr + 0);
-    int8x16_t rhs23 = vld1q_s8(rhs_ptr + 16);
-    int8x16_t rhs45 = vld1q_s8(rhs_ptr + 32);
-    int8x16_t rhs67 = vld1q_s8(rhs_ptr + 48);
-    rhs_ptr += 64;
-    acc_01_01 = vmmlaq_s32(acc_01_01, lhs01, rhs01);
-    acc_01_23 = vmmlaq_s32(acc_01_23, lhs01, rhs23);
-    acc_01_45 = vmmlaq_s32(acc_01_45, lhs01, rhs45);
-    acc_01_67 = vmmlaq_s32(acc_01_67, lhs01, rhs67);
-    acc_23_01 = vmmlaq_s32(acc_23_01, lhs23, rhs01);
-    acc_23_23 = vmmlaq_s32(acc_23_23, lhs23, rhs23);
-    acc_23_45 = vmmlaq_s32(acc_23_45, lhs23, rhs45);
-    acc_23_67 = vmmlaq_s32(acc_23_67, lhs23, rhs67);
-    acc_45_01 = vmmlaq_s32(acc_45_01, lhs45, rhs01);
-    acc_45_23 = vmmlaq_s32(acc_45_23, lhs45, rhs23);
-    acc_45_45 = vmmlaq_s32(acc_45_45, lhs45, rhs45);
-    acc_45_67 = vmmlaq_s32(acc_45_67, lhs45, rhs67);
-    acc_67_01 = vmmlaq_s32(acc_67_01, lhs67, rhs01);
-    acc_67_23 = vmmlaq_s32(acc_67_23, lhs67, rhs23);
-    acc_67_45 = vmmlaq_s32(acc_67_45, lhs67, rhs45);
-    acc_67_67 = vmmlaq_s32(acc_67_67, lhs67, rhs67);
+    for (int i = 0; i < 4; ++i) {
+      for (int j = 0; j < 4; ++j) {
+        acc[i][j] = vdupq_n_s32(0);
+      }
+    }
   }
 
-  int32x4_t acc_0_0123 = iree_uk_neon_uzp1_s32_as_s64(acc_01_01, acc_01_23);
-  int32x4_t acc_0_4567 = iree_uk_neon_uzp1_s32_as_s64(acc_01_45, acc_01_67);
-  int32x4_t acc_1_0123 = iree_uk_neon_uzp2_s32_as_s64(acc_01_01, acc_01_23);
-  int32x4_t acc_1_4567 = iree_uk_neon_uzp2_s32_as_s64(acc_01_45, acc_01_67);
-  int32x4_t acc_2_0123 = iree_uk_neon_uzp1_s32_as_s64(acc_23_01, acc_23_23);
-  int32x4_t acc_2_4567 = iree_uk_neon_uzp1_s32_as_s64(acc_23_45, acc_23_67);
-  int32x4_t acc_3_0123 = iree_uk_neon_uzp2_s32_as_s64(acc_23_01, acc_23_23);
-  int32x4_t acc_3_4567 = iree_uk_neon_uzp2_s32_as_s64(acc_23_45, acc_23_67);
-  int32x4_t acc_4_0123 = iree_uk_neon_uzp1_s32_as_s64(acc_45_01, acc_45_23);
-  int32x4_t acc_4_4567 = iree_uk_neon_uzp1_s32_as_s64(acc_45_45, acc_45_67);
-  int32x4_t acc_5_0123 = iree_uk_neon_uzp2_s32_as_s64(acc_45_01, acc_45_23);
-  int32x4_t acc_5_4567 = iree_uk_neon_uzp2_s32_as_s64(acc_45_45, acc_45_67);
-  int32x4_t acc_6_0123 = iree_uk_neon_uzp1_s32_as_s64(acc_67_01, acc_67_23);
-  int32x4_t acc_6_4567 = iree_uk_neon_uzp1_s32_as_s64(acc_67_45, acc_67_67);
-  int32x4_t acc_7_0123 = iree_uk_neon_uzp2_s32_as_s64(acc_67_01, acc_67_23);
-  int32x4_t acc_7_4567 = iree_uk_neon_uzp2_s32_as_s64(acc_67_45, acc_67_67);
-  vst1q_s32(out_ptr + 8 * 0 + 0, acc_0_0123);
-  vst1q_s32(out_ptr + 8 * 0 + 4, acc_0_4567);
-  vst1q_s32(out_ptr + 8 * 1 + 0, acc_1_0123);
-  vst1q_s32(out_ptr + 8 * 1 + 4, acc_1_4567);
-  vst1q_s32(out_ptr + 8 * 2 + 0, acc_2_0123);
-  vst1q_s32(out_ptr + 8 * 2 + 4, acc_2_4567);
-  vst1q_s32(out_ptr + 8 * 3 + 0, acc_3_0123);
-  vst1q_s32(out_ptr + 8 * 3 + 4, acc_3_4567);
-  vst1q_s32(out_ptr + 8 * 4 + 0, acc_4_0123);
-  vst1q_s32(out_ptr + 8 * 4 + 4, acc_4_4567);
-  vst1q_s32(out_ptr + 8 * 5 + 0, acc_5_0123);
-  vst1q_s32(out_ptr + 8 * 5 + 4, acc_5_4567);
-  vst1q_s32(out_ptr + 8 * 6 + 0, acc_6_0123);
-  vst1q_s32(out_ptr + 8 * 6 + 4, acc_6_4567);
-  vst1q_s32(out_ptr + 8 * 7 + 0, acc_7_0123);
-  vst1q_s32(out_ptr + 8 * 7 + 4, acc_7_4567);
+  IREE_UK_ASSUME(params->K >= 1);
+  for (int k = 0; k < params->K; ++k) {
+    int8x16_t lhs[4];
+    int8x16_t rhs[4];
+    for (int i = 0; i < 4; ++i) {
+      lhs[i] = vld1q_s8(lhs_ptr + 16 * i);
+      rhs[i] = vld1q_s8(rhs_ptr + 16 * i);
+    }
+    lhs_ptr += 64;
+    rhs_ptr += 64;
+    for (int i = 0; i < 4; ++i) {
+      for (int j = 0; j < 4; ++j) {
+        acc[i][j] = vmmlaq_s32(acc[i][j], lhs[i], rhs[j]);
+      }
+    }
+  }
+
+  // Swizzle accumulator 2x2 register tiles back to row-major and store.
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      int32x4_t acc_1x4_0 =
+          iree_uk_neon_uzp1_s32_as_s64(acc[i][2 * j + 0], acc[i][2 * j + 1]);
+      int32x4_t acc_1x4_1 =
+          iree_uk_neon_uzp2_s32_as_s64(acc[i][2 * j + 0], acc[i][2 * j + 1]);
+      vst1q_s32(out_ptr + 8 * (2 * i + 0) + 4 * j, acc_1x4_0);
+      vst1q_s32(out_ptr + 8 * (2 * i + 1) + 4 * j, acc_1x4_1);
+    }
+  }
 }
