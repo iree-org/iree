e23de71b9c0639ddb79820e9b2eaae3a3c020757
Groverkss <groverkss@gmail.com>
2023-09-12 00:31:12 +0530

    Make sample work for vmvx

--- samples/iree-reduce/interesting.sh
+++ samples/iree-reduce/interesting.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OUT1=`./tools/iree-compile $1 --iree-hal-target-backends=cuda --iree-opt-const-expr-hoisting=false --iree-opt-const-eval=false --compile-to=executable-targets | grep "llvm.intr.vector.reduce.fmax"`
+OUT1=`iree-compile $1 --iree-hal-target-backends=vmvx --iree-opt-const-expr-hoisting=false --iree-opt-const-eval=false --compile-to=executable-targets | grep "vm.mul"`
 
 OUT=$(($?))
 if [[ $OUT -eq 0 ]]; then
