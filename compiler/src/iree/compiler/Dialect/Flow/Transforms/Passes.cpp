fd9b43d0bba52480db32c4e686d4f7625cacc189
Rob Suderman <suderman@google.com>
2023-09-20 13:48:19 -0700

    Move SCFToCFG earlier in pipeline
    There appears to be a bug in the Stream pipeline's SCF support. Moving
    the SCFToCFG back to an earlier spot to avoid.

--- compiler/src/iree/compiler/Dialect/Flow/Transforms/Passes.cpp
+++ compiler/src/iree/compiler/Dialect/Flow/Transforms/Passes.cpp
@@ -182,6 +182,9 @@ void buildFlowTransformPassPipeline(OpPassManager &passManager,
         return createFormDispatchWorkgroupsPass(
             clDispatchGenerateWorkloadRegion);
       })
+      // TODO(#15003): SCF support appears to be insufficient for stream work.
+      // Need to debug the cause.
+      .addPass(IREE::Flow::createTopLevelSCFToCFGPass)
       ////////////////////////////////////////////////////////////////////////
       .addPass(createCaptureDispatchDynamicDimsPass)
       .addPass(mlir::createCanonicalizerPass)
