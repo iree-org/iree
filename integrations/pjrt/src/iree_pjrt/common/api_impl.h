1206e60009d89d6a239ee87688b9ea8635b84014
Rob Suderman <suderman@google.com>
2023-10-25 13:30:15 -0700

    Refactor HostToDevice Transfer to seperate 0-dim and splat cases (#15285)
    Transfer is an intermixing of cases. Best to refactor so that we
    separate splatting/0-dim/transfer cases
    to cleanup the branching behavior.

--- integrations/pjrt/src/iree_pjrt/common/api_impl.h
+++ integrations/pjrt/src/iree_pjrt/common/api_impl.h
@@ -214,6 +214,16 @@ class DeviceInstance {
   bool is_addressable() { return true; }
   int local_hardware_id() { return -1; }
 
+  iree_status_t HostBufferToDeviceZeroDim(
+      PJRT_Buffer_Type type, const int64_t* dims, size_t num_dims,
+      EventInstance** out_done_with_host_buffer_event,
+      BufferInstance** out_buffer);
+
+  iree_status_t HostBufferToDeviceSplat(
+      const void* data, PJRT_Buffer_Type type, const int64_t* dims,
+      size_t num_dims, EventInstance** out_done_with_host_buffer_event,
+      BufferInstance** out_buffer);
+
   // Copies a host buffer to the device.
   // See PJRT_Client_BufferFromHostBuffer
   iree_status_t HostBufferToDevice(
