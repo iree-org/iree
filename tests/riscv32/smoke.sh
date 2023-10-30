8223344a7c08af5566922c2d39fb8d8dd310746d
CindyLiu <hcindyl@google.com>
2023-10-30 09:32:48 -0700

    Update RISCV-QEMU emulator to 8.2.1 (#15320)
    Supports hwcap
    
    Update the RISCV CI docker and run_riscv_test.sh QEMU flags
    
    For RISCV bare-metal smoke tests, the docker still uses the QEMU v5.2.0,
    for QEMU v8.1.2 RISC-V has more restricted memory protection by default.

--- tests/riscv32/smoke.sh
+++ tests/riscv32/smoke.sh
@@ -19,14 +19,15 @@ BUILD_DIR="${1:-${IREE_TARGET_BUILD_DIR:-build-riscv}}"
 # Run the embedded_library module loader and simple_embedding under QEMU.
 echo "Test elf_module_test_binary"
 pushd "${BUILD_DIR}/runtime/src/iree/hal/local/elf" > /dev/null
-"${QEMU_RV32_BIN}" -cpu rv32 elf_module_test_binary
+# Need to run qemu without memory protection for embedded elf (code in .data)
+"${QEMU_RV32_V5_2_BIN}" -cpu rv32 elf_module_test_binary
 popd > /dev/null
 
 echo "Test simple_embedding binaries"
 pushd "${BUILD_DIR}/samples/simple_embedding" > /dev/null
 
-"${QEMU_RV32_BIN}" -cpu rv32 simple_embedding_embedded_sync
+"${QEMU_RV32_V5_2_BIN}" -cpu rv32 simple_embedding_embedded_sync
 
-"${QEMU_RV32_BIN}" -cpu rv32 simple_embedding_vmvx_sync
+"${QEMU_RV32_V5_2_BIN}" -cpu rv32 simple_embedding_vmvx_sync
 
 popd > /dev/null
