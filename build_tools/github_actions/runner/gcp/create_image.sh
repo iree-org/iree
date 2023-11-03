904dfab4af8d29c3d21d795d4ca0e21f720a772a
Fredrik Knutsson <fredrik.knutsson@arm.com>
2023-11-03 17:22:23 +0100

    Add Arm64 variants of build_all and test_all CI jobs (#15237)
    These jobs will run on the T2A runner.
    
    ---------
    
    Co-authored-by: Jerry Wu <cheyuw@google.com>

--- build_tools/github_actions/runner/gcp/create_image.sh
+++ build_tools/github_actions/runner/gcp/create_image.sh
@@ -38,8 +38,8 @@ BASE_IMAGE="${BASE_IMAGE:-projects/ubuntu-os-cloud/global/images/ubuntu-2204-jam
 # We create the image using n1 machines with attached T4 GPUs. This image works
 # for the A100 machines as well though.
 GPU_MACHINE_TYPE="n1-standard-16"
-CPU_MACHINE_TYPE="e2-medium"
-ARMM64_MACHINE_TYPE="t2a-standard-8"
+X86_64_MACHINE_TYPE="e2-medium"
+ARM64_MACHINE_TYPE="t2a-standard-8"
 CPU_IMAGE_SIZE_GB=10
 # We need enough space to fetch Docker images that we test with
 # TODO(gcmn): See if we can make the image smaller, e.g. by resizing after setup
@@ -154,13 +154,13 @@ function create_image() {
     echo "Creating instance '${INSTANCE_NAME}' for boot disk"
     case "${RUNNER_TYPE}" in
       cpu)
-        local machine_type="${CPU_MACHINE_TYPE}"
+        local machine_type="${X86_64_MACHINE_TYPE}"
         local image_size_gb="${CPU_IMAGE_SIZE_GB}"
         local maintenance_policy=MIGRATE
         local -a extra_args=()
         ;;
       arm64)
-        local machine_type="${ARMM64_MACHINE_TYPE}"
+        local machine_type="${ARM64_MACHINE_TYPE}"
         local image_size_gb="${CPU_IMAGE_SIZE_GB}"
         local maintenance_policy=MIGRATE
         local -a extra_args=()
