e3671a59e1b1e80e04c6dee2e4c99b4ed9dfcfd6
Lei Zhang <antiagainst@gmail.com>
2023-11-02 21:37:39 -0700

    NFC: Rename cuda to cuda2

--- experimental/cuda2/stream_command_buffer.h
+++ experimental/cuda2/stream_command_buffer.h
@@ -1,11 +1,11 @@
-// Copyright 2021 The IREE Authors
+// Copyright 2023 The IREE Authors
 //
 // Licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#ifndef IREE_HAL_DRIVERS_CUDA_STREAM_COMMAND_BUFFER_H_
-#define IREE_HAL_DRIVERS_CUDA_STREAM_COMMAND_BUFFER_H_
+#ifndef EXPERIMENTAL_CUDA2_STREAM_COMMAND_BUFFER_H_
+#define EXPERIMENTAL_CUDA2_STREAM_COMMAND_BUFFER_H_
 
 #include "iree/base/internal/arena.h"
 #include "iree/hal/api.h"
@@ -29,9 +29,9 @@ extern "C" {
 // perform inline execution. When replaying the scratch data required for things
 // like buffer updates is retained by the source deferred command buffer and as
 // such the |block_pool| and can be NULL to avoid a double copy.
-iree_status_t iree_hal_cuda_stream_command_buffer_create(
-    iree_hal_device_t* device, iree_hal_cuda_context_wrapper_t* context,
-    iree_hal_cuda_tracing_context_t* tracing_context,
+iree_status_t iree_hal_cuda2_stream_command_buffer_create(
+    iree_hal_device_t* device, iree_hal_cuda2_context_wrapper_t* context,
+    iree_hal_cuda2_tracing_context_t* tracing_context,
     iree_hal_command_buffer_mode_t mode,
     iree_hal_command_category_t command_categories,
     iree_host_size_t binding_capacity, CUstream stream,
@@ -39,11 +39,11 @@ iree_status_t iree_hal_cuda_stream_command_buffer_create(
     iree_hal_command_buffer_t** out_command_buffer);
 
 // Returns true if |command_buffer| is a CUDA stream-based command buffer.
-bool iree_hal_cuda_stream_command_buffer_isa(
+bool iree_hal_cuda2_stream_command_buffer_isa(
     iree_hal_command_buffer_t* command_buffer);
 
 #ifdef __cplusplus
 }  // extern "C"
 #endif  // __cplusplus
 
-#endif  // IREE_HAL_DRIVERS_CUDA_STREAM_COMMAND_BUFFER_H_
+#endif  // EXPERIMENTAL_CUDA2_STREAM_COMMAND_BUFFER_H_
