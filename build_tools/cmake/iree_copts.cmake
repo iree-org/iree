3534c9929ff3060589a1f3c14f8f7ef999221159
Scott Todd <scotttodd@google.com>
2023-11-17 15:22:19 -0800

    Ignore overloaded-virtual warning on gcc.

--- build_tools/cmake/iree_copts.cmake
+++ build_tools/cmake/iree_copts.cmake
@@ -212,6 +212,7 @@ iree_select_compiler_opts(IREE_DEFAULT_COPTS
     "-Wno-comment"
     "-Wno-format-zero-length"
     "-Wno-uninitialized"
+    "-Wno-overloaded-virtual"  # gcc reports more warnings than clang
     # Technically UB but needed for intrusive ptrs
     $<$<COMPILE_LANGUAGE:CXX>:-Wno-invalid-offsetof>
     $<$<COMPILE_LANGUAGE:C>:-Wno-pointer-sign>
