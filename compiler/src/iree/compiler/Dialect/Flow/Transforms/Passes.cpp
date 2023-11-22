83da98d4f76807ed204853af0396eb2913601f61
Max Dawkins <max.dawkins@gmail.com>
2023-11-22 16:10:48 -0500

    [Flow] CollapseDimensions after CloneProducersIntoDispatchRegions

--- compiler/src/iree/compiler/Dialect/Flow/Transforms/Passes.cpp
+++ compiler/src/iree/compiler/Dialect/Flow/Transforms/Passes.cpp
@@ -169,14 +169,14 @@ void buildFlowTransformPassPipeline(OpPassManager &passManager,
             clEnableFusePaddingIntoLinalgConsumerOps,
             clEnableFusePaddingIntoLinalgProducerOps});
       })
-      // Collapse dimensions of linalg Ops.
-      .addPass(createCollapseDimensionsPass)
       // Clone all producers into the dispatch region to perpare for being
       // isolated from above. This enables running additional transformations
       // afterwards that would need the full dispatch content but don't want to
       // handle explicit captures as materialized as dispatch workgroup operands
       // and block arguments.
       .addPass(createCloneProducersIntoDispatchRegionsPass)
+      // Collapse dimensions of linalg Ops.
+      .addPass(createCollapseDimensionsPass)
       // Form dispatch region into dispatch workgroups
       .addPass([&]() {
         return createFormDispatchWorkgroupsPass(
