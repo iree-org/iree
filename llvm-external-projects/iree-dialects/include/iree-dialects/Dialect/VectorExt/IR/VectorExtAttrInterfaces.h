81877778bef61a3606e95f08965ecdf09740ce19
Kunwar Grover <groverkss@gmail.com>
2023-11-28 21:41:38 +0530

    wip

---
+++ llvm-external-projects/iree-dialects/include/iree-dialects/Dialect/VectorExt/IR/VectorExtAttrInterfaces.h
@@ -0,0 +1,17 @@
+// Copyright 2023 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef IREE_DIALECTS_DIALECT_VECTOREXT_ATTR_INTERFACES_H_
+#define IREE_DIALECTS_DIALECT_VECTOREXT_ATTR_INTERFACES_H_
+
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/Support/LLVM.h"
+
+/// Include the generated interface declarations.
+#include "iree-dialects/Dialect/VectorExt/IR/VectorExtAttrInterfaces.h.inc"
+
+#endif // IREE_DIALECTS_DIALECT_VECTOREXT_ATTR_INTERFACES_H
