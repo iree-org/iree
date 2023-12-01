70b075bc124f13b3e8f84cb8fd6fac81868b79fd
Jakub Kuderski <jakub@nod-labs.com>
2023-12-01 01:26:58 -0500

    nit

--- compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
@@ -5,7 +5,6 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include "iree-dialects/Dialect/LinalgExt/Passes/Passes.h"
-#include <iree/compiler/Dialect/HAL/IR/HALOps.h>
 #include <cstdint>
 
 #include "iree-dialects/Dialect/LinalgTransform/Passes.h"
