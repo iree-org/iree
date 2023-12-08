54ced7f056f012f3ced51b74682bbe810210b839
hanhanW <hanhan0912@gmail.com>
2023-12-08 23:12:37 +0000

    [SPIRV][NFC] Switch tiling to use scf::tileUsingSCFForOp.
    It is a cleanup towards removing LinalgExt::TilingPatterns. It also
    moves the setSCFTileSizes util to Codegen/Utils, so it can be reused by
    other backend.

--- compiler/src/iree/compiler/Codegen/LLVMCPU/Utils.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMCPU/Utils.cpp
@@ -8,7 +8,6 @@
 
 #include "iree/compiler/Codegen/Utils/Utils.h"
 #include "llvm/ADT/STLExtras.h"
-#include "mlir/Dialect/SCF/Transforms/TileUsingInterface.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Dialect/Vector/IR/VectorOps.h"
 
@@ -52,37 +51,6 @@ bool hasAnySVEFeature(IREE::HAL::ExecutableTargetAttr targetAttr) {
 
 bool hasSMEFeature(IREE::HAL::ExecutableTargetAttr targetAttr) {
   return hasFeature(targetAttr, "+sme");
-}
-
-void setSCFTileSizes(scf::SCFTilingOptions &options, TilingInterface consumerOp,
-                     SmallVector<int64_t> tileSizes,
-                     SmallVector<bool> tileScalableFlags) {
-  // scf::tileUsingSCFForOp expects the num of tile sizes = num of loops.
-  int numLoops = consumerOp.getLoopIteratorTypes().size();
-  tileSizes.resize(numLoops, /*default=*/0);
-  tileScalableFlags.resize(numLoops, /*default=*/false);
-  if (!llvm::is_contained(tileScalableFlags, true)) {
-    // Non-scalable case: All constant tile sizes.
-    options.setTileSizes(
-        getAsIndexOpFoldResult(consumerOp.getContext(), tileSizes));
-  } else {
-    // Scalable case: Multiply scalable tile sizes by a vector.vscale op.
-    options.setTileSizeComputationFunction(
-        [=](OpBuilder &b, Operation *op) -> SmallVector<OpFoldResult> {
-          auto loc = op->getLoc();
-          return llvm::map_to_vector(
-              llvm::zip(tileSizes, tileScalableFlags),
-              [&](auto pair) -> OpFoldResult {
-                auto [t, isScalable] = pair;
-                Value size = b.create<arith::ConstantIndexOp>(loc, t);
-                if (isScalable) {
-                  Value vscale = b.create<vector::VectorScaleOp>(loc);
-                  size = b.create<arith::MulIOp>(loc, size, vscale);
-                }
-                return size;
-              });
-        });
-  }
 }
 
 } // namespace mlir::iree_compiler
