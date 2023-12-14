28ad76aa9c6c5d9d59234b2cdf0196d57af51eba
Scott Todd <scotttodd@google.com>
2023-12-14 15:08:57 -0800

    Log full output from vulkaninfo.

--- build_tools/scripts/check_vulkan.sh
+++ build_tools/scripts/check_vulkan.sh
@@ -10,17 +10,16 @@
 
 set -xeuo pipefail
 
-# Print Vulkan related information: SDK version and GPU ICD version
 vulkaninfo 2> /tmp/vulkaninfo.stderr 1> /tmp/vulkaninfo.stdout
-VULKAN_INSTANCE="$(grep "Vulkan Instance" /tmp/vulkaninfo.stdout)"
-VK_PHYSICAL_DEVICE_PROPERTIES="$(grep -A7 "VkPhysicalDeviceProperties" /tmp/vulkaninfo.stdout)"
 
-if [[ -z "${VULKAN_INSTANCE?}" ]] || [[ -z "${VK_PHYSICAL_DEVICE_PROPERTIES?}" ]]; then
+VULKAN_INSTANCE="$(grep "Vulkan Instance" /tmp/vulkaninfo.stdout)"
+
+if [[ -z "${VULKAN_INSTANCE?}" ]]; then
   echo "Vulkan not found!"
   cat /tmp/vulkaninfo.stdout
   cat /tmp/vulkaninfo.stderr
   exit 1
 fi
 
-echo "${VULKAN_INSTANCE?}"
-echo "${VK_PHYSICAL_DEVICE_PROPERTIES?}"
+cat /tmp/vulkaninfo.stdout
+cat /tmp/vulkaninfo.stderr
