39576d0a6e72c3e2060abddf8f5a58479da400eb
Lei Zhang <antiagainst@gmail.com>
2023-12-19 13:07:29 -0800

    [spirv] Trim provided executable target before linking (#15888)
    This commit adds a pass to trim the provided target environment to the
    minimal required one to run the compiled spirv.module op. This increases
    the chance of linking different executable variants together when giving
    multiple targets. For the same provided target, it may actually generate
    more exectuables, given that now each dispatch may see a different set
    of capability requirements. So we may need to be smarter when linking
    executables--simply relying on exact target attribute match, which is
    the current state, is not the best way. But this pass alone should be a
    good cleanup.

---
+++ compiler/src/iree/compiler/Codegen/SPIRV/SPIRVTrimExecutableTargetEnv.cpp
@@ -0,0 +1,83 @@
+// Copyright 2023 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "iree/compiler/Codegen/SPIRV/PassDetail.h"
+#include "iree/compiler/Codegen/SPIRV/Passes.h"
+#include "iree/compiler/Dialect/HAL/IR/HALOps.h"
+#include "mlir/Dialect/SPIRV/IR/SPIRVAttributes.h"
+#include "mlir/Dialect/SPIRV/IR/SPIRVOps.h"
+#include "mlir/Dialect/SPIRV/IR/TargetAndABI.h"
+#include "mlir/Pass/Pass.h"
+
+namespace mlir::iree_compiler {
+
+namespace {
+
+bool IsSPIRVBasedBackend(StringRef backend) {
+  return backend.starts_with("vulkan") || backend.starts_with("metal") ||
+         backend.starts_with("webgpu");
+}
+
+struct SPIRVTrimExecutableTargetEnvPass final
+    : SPIRVTrimExecutableTargetEnvBase<SPIRVTrimExecutableTargetEnvPass> {
+  void runOnOperation() override {
+    IREE::HAL::ExecutableVariantOp variant = getOperation();
+    if (!IsSPIRVBasedBackend(variant.getTarget().getBackend())) {
+      return;
+    }
+    if (variant.getObjects().has_value()) {
+      // Ignore external executable variants. We need to read spirv.module
+      // ops to get the deduced minimal list of required capability and
+      // extension. External source executables won't have any spirv.module
+      // ops inside.
+      return;
+    }
+
+    mlir::ModuleOp innerModule = variant.getInnerModule();
+    auto spirvModuleOps = innerModule.getOps<spirv::ModuleOp>();
+    // The SPIR-V CodeGen flow in IREE guarantees that we have exactly one
+    // spirv.module op inside the variant op.
+    if (!llvm::hasSingleElement(spirvModuleOps)) {
+      variant.emitOpError("should contain exactly one spirv.module op");
+      return signalPassFailure();
+    }
+
+    spirv::ModuleOp spvModule = *spirvModuleOps.begin();
+    std::optional<spirv::VerCapExtAttr> vceTriple = spvModule.getVceTriple();
+    // The SPIR-V CodeGen flow in IREE also guarantees that we have deduced
+    // the minimal version/capability/extension requirement.
+    if (!vceTriple) {
+      spvModule.emitError("should have deduced vce triple");
+      return signalPassFailure();
+    }
+    auto minimalTarget = spirv::TargetEnvAttr::get(
+        vceTriple.value(),
+        spirv::getDefaultResourceLimits(vceTriple->getContext()));
+
+    // Replace the provided allow list to the minimal requirement deduced
+    // from compilation.
+    IREE::HAL::ExecutableTargetAttr providedTarget = variant.getTarget();
+    auto deducedConfig = providedTarget.getConfiguration().replace(
+        [&](spirv::TargetEnvAttr attr) { return minimalTarget; });
+    auto deducedTarget = IREE::HAL::ExecutableTargetAttr::get(
+        providedTarget.getContext(), providedTarget.getBackend(),
+        providedTarget.getFormat(), cast<DictionaryAttr>(deducedConfig));
+    variant.setTargetAttr(deducedTarget);
+
+    // Clean up the spirv.target_env attribute on inner module, which was
+    // used to drive compilation.
+    innerModule->removeAttr(spirv::getTargetEnvAttrName());
+  }
+};
+
+} // namespace
+
+std::unique_ptr<OperationPass<IREE::HAL::ExecutableVariantOp>>
+createSPIRVTrimExecutableTargetEnvPass() {
+  return std::make_unique<SPIRVTrimExecutableTargetEnvPass>();
+}
+
+} // namespace mlir::iree_compiler
