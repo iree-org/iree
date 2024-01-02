dce349e2e3a37a2a96f30d274a57c067ac549ca8
Byungchul Kim <byungchul@google.com>
2024-01-02 09:09:34 -0800

    Output to xplane protobuf file.

--- tools/iree-tracy-print.cc
+++ tools/iree-tracy-print.cc
@@ -11,12 +11,14 @@
 #include <unistd.h>
 
 #include <atomic>
+#include <fstream>
 #include <iostream>
 #include <chrono>
 #include <map>
 #include <thread>
 
 #include "iree/base/internal/flags.h"
+#include "build_tools/third_party/tsl/xplane.pb.h"
 #include "third_party/tracy/public/common/TracyProtocol.hpp"
 #include "third_party/tracy/server/TracyFileRead.hpp"
 #include "third_party/tracy/server/TracyFileWrite.hpp"
@@ -26,6 +28,8 @@ IREE_FLAG(string, input_tracy_file, "",
           "Tracy file to read. Ignored if executable command is given.");
 IREE_FLAG(string, output_tracy_file, "",
           "Tracy file to write as the output of the given executable command.");
+IREE_FLAG(string, output_xplane_file, "",
+          "Xplane file to write as the output of execution or conversion.");
 IREE_FLAG(int32_t, tracy_port, 18086, "TCP port number for tracy profiler.");
 
 namespace {
@@ -77,12 +81,6 @@ struct Zone {
 };
 
 template <typename T>
-const char* GetZoneName(const tracy::Worker& worker, const Zone<T>& zone) {
-  const auto& srcloc = worker.GetSourceLocation(zone.source_location_id);
-  return worker.GetString(srcloc.name.active ? srcloc.name : srcloc.function);
-}
-
-template <typename T>
 std::vector<Zone<T>> GetTopZones(
     const tracy::unordered_flat_map<int16_t, T>& zones,
     int num_zones_to_return) {
@@ -100,6 +98,12 @@ std::vector<Zone<T>> GetTopZones(
   }
   return vector_to_return;
 };
+
+const char* GetZoneName(const tracy::Worker& worker,
+                        int16_t source_location_id) {
+  const auto& srcloc = worker.GetSourceLocation(source_location_id);
+  return worker.GetString(srcloc.name.active ? srcloc.name : srcloc.function);
+}
 
 const char* ArchToString(tracy::CpuArchitecture arch) {
   switch (arch) {
@@ -120,7 +124,7 @@ void PrintWorker(const tracy::Worker& worker) {
   std::cout << "[TRACY]   CPU Zones = " << worker.GetZoneCount() << "\n";
   auto cpu_zones = GetTopZones(worker.GetSourceLocationZones(), 10);
   for (const auto& z : cpu_zones) {
-    std::cout << "[TRACY]         " << GetZoneName(worker, z)
+    std::cout << "[TRACY]         " << GetZoneName(worker, z.source_location_id)
               << ": total_ns=" << z.zone->total
               << ", count=" << z.zone->zones.size() << "\n";
   }
@@ -128,10 +132,80 @@ void PrintWorker(const tracy::Worker& worker) {
   std::cout << "[TRACY]   GPU Zones = " << worker.GetGpuZoneCount() << "\n";
   auto gpu_zones = GetTopZones(worker.GetGpuSourceLocationZones(), 10);
   for (const auto& z : gpu_zones) {
-    std::cout << "[TRACY]         " << GetZoneName(worker, z)
+    std::cout << "[TRACY]         " << GetZoneName(worker, z.source_location_id)
               << ": total_ns=" << z.zone->total
               << ", count=" << z.zone->zones.size() << "\n";
   }
+}
+
+tensorflow::profiler::XSpace ToXplane(const tracy::Worker& worker) {
+  tensorflow::profiler::XSpace xspace;
+  auto* xplane = xspace.add_planes();
+  xplane->set_id(0);
+  xplane->set_name(worker.GetCaptureName());
+
+  // XLine corresponds to each Thread.
+  // XEvent corresponds to each Zone.
+  std::unordered_map<uint16_t, tensorflow::profiler::XLine*> xlines;
+
+  for (const auto& z : worker.GetSourceLocationZones()) {
+    auto& event_metadata = (*xplane->mutable_event_metadata())[z.first];
+    event_metadata.set_id(z.first);
+    event_metadata.set_name(GetZoneName(worker, z.first));
+    event_metadata.set_display_name(event_metadata.name());
+
+    for (const auto& t : z.second.zones) {
+      auto it = xlines.find(t.Thread());
+      if (it == xlines.end()) {
+        it = xlines.insert(std::make_pair(t.Thread(), xplane->add_lines()))
+             .first;
+        auto* xline = it->second;
+        xline->set_id(t.Thread());
+        xline->set_display_id(t.Thread());
+        xline->set_name(worker.GetThreadName(
+            worker.DecompressThread(t.Thread())));
+        xline->set_display_name(xline->name());
+        //xline->set_timestamp_ns();
+        //xline->set_duration_ps();
+      }
+
+      auto* event = it->second->add_events();
+      event->set_metadata_id(z.first);
+      event->set_offset_ps(t.Zone()->Start() * 1000);
+      event->set_duration_ps((t.Zone()->End() - t.Zone()->Start()) * 1000);
+    }
+  }
+
+  for (const auto& z : worker.GetGpuSourceLocationZones()) {
+    auto& event_metadata = (*xplane->mutable_event_metadata())[z.first];
+    event_metadata.set_id(z.first);
+    event_metadata.set_name(GetZoneName(worker, z.first));
+    event_metadata.set_display_name(event_metadata.name());
+
+    for (const auto& t : z.second.zones) {
+      auto it = xlines.find(t.Thread());
+      if (it == xlines.end()) {
+        it = xlines.insert(std::make_pair(t.Thread(), xplane->add_lines()))
+             .first;
+        auto* xline = it->second;
+        xline->set_id(t.Thread());
+        xline->set_display_id(t.Thread());
+        xline->set_name(worker.GetThreadName(
+            worker.DecompressThread(t.Thread())));
+        xline->set_display_name(xline->name());
+        //xline->set_timestamp_ns();
+        //xline->set_duration_ps();
+      }
+
+      auto* event = it->second->add_events();
+      event->set_metadata_id(z.first);
+      event->set_offset_ps(t.Zone()->GpuStart() * 1000);
+      event->set_duration_ps(
+          (t.Zone()->GpuEnd() - t.Zone()->GpuStart()) * 1000);
+    }
+  }
+
+  return xspace;
 }
 
 }  // namespace
@@ -211,10 +285,18 @@ int main(int argc, char** argv) {
     auto f = std::unique_ptr<tracy::FileWrite>(
         tracy::FileWrite::Open(FLAG_output_tracy_file));
     if (f) {
-      worker->Write(*f, false);
+      worker->Write(*f, /*fiDict=*/false);
     } else {
       std::cerr << "[ERROR] Could not write tracy file "
                 << FLAG_output_tracy_file << "\n";
+    }
+  }
+
+  if (strlen(FLAG_output_xplane_file) > 0) {
+    std::fstream f(FLAG_output_xplane_file, std::ios::out|std::ios::binary);
+    if (!ToXplane(*worker).SerializeToOstream(&f)) {
+      std::cerr << "[ERROR] Could not write xplane file "
+                << FLAG_output_xplane_file << "\n";
     }
   }
 
