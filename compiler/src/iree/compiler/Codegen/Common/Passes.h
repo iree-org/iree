e4aa589c9d5e848620abb8141563512ac2bcc089
Lubomir Litchev <llitchev@live.com>
2024-01-03 09:20:24 -0800

    [CodeGen] Add aflag to allow potentially to remove unnecessary code to improve performance. (#15862)
    - Added a new flag to allow to test and evaluate performance, so we can
    evaluate the removal of inter-pass fusion code in the DecomposeSoftmax
    iree pass that duplicated a sub and exp operation, causing perf
    degradation.
    - Clened up (removed) some of never used/stale code from the pass. It
    looks like the functinality has been upstreamed to linalg already, but
    some of the code has remained in the pass.
    - Updated the LIT tests as necessary.
    
    ---------
    
    Co-authored-by: Lubo Litchev <lubol@google.com>

--- compiler/src/iree/compiler/Codegen/Common/Passes.h
+++ compiler/src/iree/compiler/Codegen/Common/Passes.h
@@ -28,7 +28,8 @@ void registerTransformDialectTranslationDependentDialects(
 
 /// Passes that are done on all backends before target-specific code-generation
 /// kicks in.
-void addCommonTargetExecutablePreprocessingPasses(OpPassManager &passManager);
+void addCommonTargetExecutablePreprocessingPasses(
+    OpPassManager &passManager, bool useDecomposeSoftmaxFusion = true);
 
 /// Post-bufferization passes run to cleanup the IR
 /// (ResolveShapedTypeResultDims, Canonicalization/CSE and
@@ -100,7 +101,7 @@ createDecomposePackUnPackOpsPass(bool tileOuterToOne = false);
 
 /// Creates a pass to convert the softmax op into a sequence of linalg generic
 /// ops.
-std::unique_ptr<Pass> createDecomposeSoftmaxPass();
+std::unique_ptr<Pass> createDecomposeSoftmaxPass(bool useFusion = true);
 
 /// A pass to eliminate tensor.empty ops that could turn into allocations
 /// during bufferization.
