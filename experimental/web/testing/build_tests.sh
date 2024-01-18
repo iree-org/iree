020b7876e1643ffe17f2a3b891db98405f05f75e
Scott Todd <scott.todd0@gmail.com>
2024-01-18 10:42:28 -0800

    Tweak a few uses of ghost.svg.

--- experimental/web/testing/build_tests.sh
+++ experimental/web/testing/build_tests.sh
@@ -100,4 +100,4 @@ echo "=== Copying static files to the build directory ==="
 
 cp ${SOURCE_DIR?}/test-runner.html ${BINARY_DIR}
 cp ${SOURCE_DIR?}/*.js ${BINARY_DIR}
-cp ${ROOT_DIR?}/docs/website/overrides/.icons/iree/ghost.svg ${BINARY_DIR}
+cp ${ROOT_DIR?}/docs/website/docs/assets/images/ghost.svg ${BINARY_DIR}
