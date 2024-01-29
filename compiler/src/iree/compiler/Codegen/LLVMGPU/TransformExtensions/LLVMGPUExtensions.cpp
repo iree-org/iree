8efc168270a1f1bd64e4775e29febeb31fa49f06
Kunwar Grover <groverkss@gmail.com>
2024-01-30 01:47:59 +0530

    [Codegen][LLVMGPU] Add option to control sync after map_nested_for_all_to_gpu_threads (#16247)
    map_nested_for_all_to_gpu_threads needs to synchronize because the the
    number of threads being distributed may not be equal to workgroup size.
    This patch adds an option to disable these barriers, which is useful in
    cases when the number of threads are equal to workgroup size.

--- compiler/src/iree/compiler/Codegen/LLVMGPU/TransformExtensions/LLVMGPUExtensions.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/TransformExtensions/LLVMGPUExtensions.cpp
@@ -97,7 +97,7 @@ transform_dialect::MapNestedForallToGpuThreadsOp::applyToOne(
   DiagnosedSilenceableFailure diag =
       mlir::transform::gpu::mapNestedForallToThreadsImpl(
           rewriter, transformOp, target, getWorkgroupDims(), getSubgroupSize(),
-          true);
+          getSyncAfterDistribution());
   if (!diag.succeeded())
     return diag;
   auto newAttr = rewriter.getIndexArrayAttr(getWorkgroupDims());
