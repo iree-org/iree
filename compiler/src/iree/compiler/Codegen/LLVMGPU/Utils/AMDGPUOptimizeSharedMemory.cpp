75b16b6f4bdea6a256ef859516b6b97df327c7a5
erman-gurses <erman@nod-labs.com>
2024-01-29 17:42:29 -0600

    Fix CI build issue

--- compiler/src/iree/compiler/Codegen/LLVMGPU/Utils/AMDGPUOptimizeSharedMemory.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/Utils/AMDGPUOptimizeSharedMemory.cpp
@@ -4,20 +4,8 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#include "iree/compiler/Codegen/Common/GPU/GPUPatterns.h"
-#include "iree/compiler/Codegen/LLVMGPU/PassDetail.h"
-#include "iree/compiler/Codegen/LLVMGPU/Passes.h"
-#include "iree/compiler/Codegen/LLVMGPU/Utils/LLVMGPUUtils.h"
 #include "iree/compiler/Codegen/Utils/GPUUtils.h"
-#include "mlir/Conversion/VectorToGPU/VectorToGPU.h"
-#include "mlir/Dialect/AMDGPU/IR/AMDGPUDialect.h"
 #include "mlir/Dialect/AMDGPU/Transforms/Transforms.h"
-#include "mlir/Dialect/Affine/IR/AffineOps.h"
-#include "mlir/Dialect/Arith/IR/Arith.h"
-#include "mlir/Dialect/GPU/IR/GPUDialect.h"
-#include "mlir/Dialect/MemRef/Transforms/Transforms.h"
-#include "mlir/Dialect/Vector/Transforms/VectorRewritePatterns.h"
-#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
 namespace mlir::iree_compiler {
 
