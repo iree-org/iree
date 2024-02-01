fe4e1ceab8eaebd36955c638a699dbd4ad63fd84
Jakub Kuderski <jakub@nod-labs.com>
2024-01-31 20:17:02 -0500

    [ROCM] Fix `addPreloadKernArgHint`
    Use `size_t` to avoid type mismatches on Windows. Do not recalculate the
    trip count.

--- compiler/plugins/target/ROCM/ROCMTarget.cpp
+++ compiler/plugins/target/ROCM/ROCMTarget.cpp
@@ -119,7 +119,7 @@ static std::string translateModuleToISA(llvm::Module &module,
 // TODO: Query max number of user SGPRs from target machine.
 static void addPreloadKernArgHint(llvm::Function *F) {
   constexpr unsigned long maxSGPRs = 16;
-  for (unsigned i = 0; i < std::min(F->arg_size(), maxSGPRs); ++i) {
+  for (size_t i = 0, e = std::min(F->arg_size(), maxSGPRs); i != e; ++i) {
     llvm::Argument *Arg = F->getArg(i);
     // Check for incompatible attributes.
     if (Arg->hasByRefAttr() || Arg->hasNestAttr())
