3b79d8172144b4c628c2ad0c8d95a320c5f34677
Han-Chung Wang <hanchung@google.com>
2024-02-01 05:51:25 -0800

    only benchmark batch_size=1

--- build_tools/python/e2e_test_framework/models/model_groups.py
+++ build_tools/python/e2e_test_framework/models/model_groups.py
@@ -22,12 +22,12 @@ X86_64_BENCHMARK_CONFIG = [
     common_definitions.CpuBenchmarkConfig(
         model=jax_models.T5_4CG_LARGE_FP32_JAX_512XI32_BATCHES[1], threads=[30]
     ),
-    common_definitions.CpuBenchmarkConfig(
-        model=jax_models.T5_4CG_LARGE_FP32_JAX_512XI32_BATCHES[16], threads=[30]
-    ),
-    common_definitions.CpuBenchmarkConfig(
-        model=jax_models.T5_4CG_LARGE_FP32_JAX_512XI32_BATCHES[32], threads=[30]
-    ),
+    #common_definitions.CpuBenchmarkConfig(
+    #    model=jax_models.T5_4CG_LARGE_FP32_JAX_512XI32_BATCHES[16], threads=[30]
+    #),
+    #common_definitions.CpuBenchmarkConfig(
+    #    model=jax_models.T5_4CG_LARGE_FP32_JAX_512XI32_BATCHES[32], threads=[30]
+    #),
     # Tiny models.
     common_definitions.CpuBenchmarkConfig(
         model=tflite_models.PERSON_DETECT_INT8, threads=[0, 1]
