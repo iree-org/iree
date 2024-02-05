15da5ec0c3ee6b74a39847efb13c5c407a29a7e1
Jerry Wu <cheyuw@google.com>
2024-02-05 19:03:39 +0000

    Tmp

--- compiler/src/iree/compiler/GlobalOptimization/GlobalLoopInvariantCodeMotion.cpp
+++ compiler/src/iree/compiler/GlobalOptimization/GlobalLoopInvariantCodeMotion.cpp
@@ -20,117 +20,155 @@ struct GlobalLoopInvariantCodeMotionPass
     : public GlobalLoopInvariantCodeMotionBase<
           GlobalLoopInvariantCodeMotionPass> {
   void runOnOperation() override {
-    MLIRContext *context = &getContext();
     FunctionOpInterface funcOp = getOperation();
     if (funcOp.isDeclaration())
       return;
 
-    SmallVector<scf::WhileOp> worklist;
+    SmallVector<LoopLikeOpInterface> worklist;
     for (Operation &op : funcOp.getCallableRegion()->getOps()) {
-      if (auto whileOp = llvm::dyn_cast<scf::WhileOp>(op)) {
-        worklist.push_back(whileOp);
+      if (auto loopOp = llvm::dyn_cast<LoopLikeOpInterface>(op)) {
+        worklist.push_back(loopOp);
       }
     }
 
-    for (auto whileOp : worklist) {
-      SmallVector<Operation *> hoistedOps;
+    MLIRContext *context = &getContext();
+    IRRewriter rewriter(context);
+    for (auto loopOp : worklist) {
+      llvm::SmallSetVector<Operation *, 8> toBeHoistedOps;
       moveLoopInvariantCode(
-          whileOp.getLoopRegions(),
+          loopOp.getLoopRegions(),
           [&](Value value, Region *region) {
-            return whileOp.isDefinedOutsideOfLoop(value);
-          },
-          [&](Operation *invOp, Region *region) {
-            return isMemoryEffectFree(invOp) && isSpeculatable(invOp);
-          },
-          [&](Operation *invOp, Region *region) {
-            if (region == &whileOp.getAfter()) {
-              hoistedOps.push_back(invOp);
+            if (auto op = value.getDefiningOp()) {
+              if (toBeHoistedOps.contains(op)) {
+                return true;
+              }
             }
-            whileOp.moveOutOfLoop(invOp);
+            return loopOp.isDefinedOutsideOfLoop(value);
+          },
+          [&](Operation *op, Region *region) {
+            return !toBeHoistedOps.contains(op) && isMemoryEffectFree(op) &&
+                   isSpeculatable(op);
+          },
+          [&](Operation *op, Region *region) {
+            toBeHoistedOps.insert(op);
             return;
           });
-
-      if (hoistedOps.empty()) {
+      if (toBeHoistedOps.empty()) {
         continue;
       }
 
-      IRRewriter rewriter(context);
-      rewriter.setInsertionPoint(whileOp);
-
-      IRMapping mapper;
-      Block *srcBlock = whileOp.getBeforeBody();
-      for (auto [arg, init] :
-           llvm::zip_equal(srcBlock->getArguments(), whileOp.getInits())) {
-        mapper.map(arg, init);
+      FailureOr<LoopLikeOpInterface> wrappedLoopOp =
+          loopOp.replaceWithZeroTripCheck(rewriter);
+      if (failed(wrappedLoopOp)) {
+        continue;
       }
 
-      auto cloneOptions =
-          Operation::CloneOptions::all().cloneRegions(false).cloneOperands(
-              false);
-      SmallVector<Operation *> clonedOps;
-      for (auto &op : *srcBlock) {
-        if (isa<scf::ConditionOp>(op)) {
-          break;
-        }
-        auto clonedOp = op.clone(mapper, cloneOptions);
-        rewriter.insert(clonedOp);
-        clonedOps.push_back(clonedOp);
-      }
-
-      SmallVector<Value> operands;
-      for (auto [clonedOp, srcOp] : llvm::zip(clonedOps, *srcBlock)) {
-        operands.resize(srcOp.getNumOperands());
-        llvm::transform(
-            srcOp.getOperands(), operands.begin(),
-            [&](Value operand) { return mapper.lookupOrDefault(operand); });
-        clonedOp->setOperands(operands);
-        for (auto [srcRegion, clonedRegion] :
-             llvm::zip_equal(srcOp.getRegions(), clonedOp->getRegions())) {
-          srcRegion.cloneInto(&clonedRegion, mapper);
+      for (auto op : toBeHoistedOps) {
+        if (op->getParentOp() == wrappedLoopOp->getOperation()) {
+          wrappedLoopOp->moveOutOfLoop(op);
         }
       }
-
-      auto condOp = cast<scf::ConditionOp>(srcBlock->getTerminator());
-      auto clonedCondition = mapper.lookupOrDefault(condOp.getCondition());
-      SmallVector<Value> clonedArgs =
-          llvm::map_to_vector(condOp.getArgs(), [&](Value result) {
-            return mapper.lookupOrDefault(result);
-          });
-      auto loc = condOp.getLoc();
-      auto ifOp = rewriter.create<scf::IfOp>(
-          loc, clonedCondition,
-          [&](OpBuilder &builder, Location loc) {
-            auto rotatedWhileOp = builder.create<scf::WhileOp>(
-                whileOp.getLoc(), whileOp.getResultTypes(), clonedArgs,
-                [&](OpBuilder &builder, Location loc, ValueRange args) {
-                  // Before block is created below.
-                },
-                [&](OpBuilder &builder, Location loc, ValueRange args) {
-                  // Pass-through values to the before block.
-                  builder.create<scf::YieldOp>(loc, args);
-                });
-
-            IRRewriter rewriter(builder);
-            Block *beforeBlock = rotatedWhileOp.getBeforeBody();
-            rewriter.mergeBlocks(whileOp.getAfterBody(), beforeBlock,
-                                 beforeBlock->getArguments());
-            auto yieldOp = cast<scf::YieldOp>(beforeBlock->getTerminator());
-            rewriter.mergeBlocks(whileOp.getBeforeBody(), beforeBlock,
-                                 yieldOp.getResults());
-            rewriter.eraseOp(yieldOp);
-
-            for (auto hoistedOp : hoistedOps) {
-              rewriter.moveOpBefore(hoistedOp, rotatedWhileOp);
-            }
-
-            builder.create<scf::YieldOp>(loc, rotatedWhileOp->getResults());
-          },
-          [&](OpBuilder &builder, Location loc) {
-            builder.create<scf::YieldOp>(loc, clonedArgs);
-          });
-
-      rewriter.replaceOp(whileOp, ifOp);
     }
+
+    // for (auto whileOp : worklist) {
+    //   SmallVector<Operation *> hoistedOps;
+    //   moveLoopInvariantCode(
+    //       whileOp.getLoopRegions(),
+    //       [&](Value value, Region *region) {
+    //         return whileOp.isDefinedOutsideOfLoop(value);
+    //       },
+    //       [&](Operation *invOp, Region *region) {
+    //         return isMemoryEffectFree(invOp) && isSpeculatable(invOp);
+    //       },
+    //       [&](Operation *invOp, Region *region) {
+    //         if (region == &whileOp.getAfter()) {
+    //           hoistedOps.push_back(invOp);
+    //         }
+    //         whileOp.moveOutOfLoop(invOp);
+    //         return;
+    //       });
+
+    //   if (hoistedOps.empty()) {
+    //     continue;
+    //   }
+
+    //   IRRewriter rewriter(context);
+    //   rewriter.setInsertionPoint(whileOp);
+
+    //   IRMapping mapper;
+    //   Block *srcBlock = whileOp.getBeforeBody();
+    //   for (auto [arg, init] :
+    //        llvm::zip_equal(srcBlock->getArguments(), whileOp.getInits())) {
+    //     mapper.map(arg, init);
+    //   }
+
+    //   auto cloneOptions =
+    //       Operation::CloneOptions::all().cloneRegions(false).cloneOperands(
+    //           false);
+    //   SmallVector<Operation *> clonedOps;
+    //   for (auto &op : *srcBlock) {
+    //     if (isa<scf::ConditionOp>(op)) {
+    //       break;
+    //     }
+    //     auto clonedOp = op.clone(mapper, cloneOptions);
+    //     rewriter.insert(clonedOp);
+    //     clonedOps.push_back(clonedOp);
+    //   }
+
+    //   SmallVector<Value> operands;
+    //   for (auto [clonedOp, srcOp] : llvm::zip(clonedOps, *srcBlock)) {
+    //     operands.resize(srcOp.getNumOperands());
+    //     llvm::transform(
+    //         srcOp.getOperands(), operands.begin(),
+    //         [&](Value operand) { return mapper.lookupOrDefault(operand); });
+    //     clonedOp->setOperands(operands);
+    //     for (auto [srcRegion, clonedRegion] :
+    //          llvm::zip_equal(srcOp.getRegions(), clonedOp->getRegions())) {
+    //       srcRegion.cloneInto(&clonedRegion, mapper);
+    //     }
+    //   }
+
+    //   auto condOp = cast<scf::ConditionOp>(srcBlock->getTerminator());
+    //   auto clonedCondition = mapper.lookupOrDefault(condOp.getCondition());
+    //   SmallVector<Value> clonedArgs =
+    //       llvm::map_to_vector(condOp.getArgs(), [&](Value result) {
+    //         return mapper.lookupOrDefault(result);
+    //       });
+    //   auto loc = condOp.getLoc();
+    //   auto ifOp = rewriter.create<scf::IfOp>(
+    //       loc, clonedCondition,
+    //       [&](OpBuilder &builder, Location loc) {
+    //         auto rotatedWhileOp = builder.create<scf::WhileOp>(
+    //             whileOp.getLoc(), whileOp.getResultTypes(), clonedArgs,
+    //             [&](OpBuilder &builder, Location loc, ValueRange args) {
+    //               // Before block is created below.
+    //             },
+    //             [&](OpBuilder &builder, Location loc, ValueRange args) {
+    //               // Pass-through values to the before block.
+    //               builder.create<scf::YieldOp>(loc, args);
+    //             });
+
+    //         IRRewriter rewriter(builder);
+    //         Block *beforeBlock = rotatedWhileOp.getBeforeBody();
+    //         rewriter.mergeBlocks(whileOp.getAfterBody(), beforeBlock,
+    //                              beforeBlock->getArguments());
+    //         auto yieldOp = cast<scf::YieldOp>(beforeBlock->getTerminator());
+    //         rewriter.mergeBlocks(whileOp.getBeforeBody(), beforeBlock,
+    //                              yieldOp.getResults());
+    //         rewriter.eraseOp(yieldOp);
+
+    //         for (auto hoistedOp : hoistedOps) {
+    //           rewriter.moveOpBefore(hoistedOp, rotatedWhileOp);
+    //         }
+
+    //         builder.create<scf::YieldOp>(loc, rotatedWhileOp->getResults());
+    //       },
+    //       [&](OpBuilder &builder, Location loc) {
+    //         builder.create<scf::YieldOp>(loc, clonedArgs);
+    //       });
+
+    //   rewriter.replaceOp(whileOp, ifOp);
+    // }
   }
 };
 
