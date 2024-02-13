91ee20f9895c55ddc8fba3f788fc31d854fef3e7
Lei Zhang <antiagainst@gmail.com>
2024-02-13 10:45:48 -0800

    [rocdl] Create basic code structure for ROCDL CodeGen (#16342)
    This commit starts the rework of LLVMGPU compiler backend for future
    production. Specifically it starts a set of new source code files for
    ROCDL CodeGen configuration. The goal is to start with a minimal working
    structure and gradually port over relevant pass pipelines with proper
    target handling and heuristics. So in this commit we only pick the warp
    reduction pipeline which sort of matches the criteria here. Also we
    don't shuffle the passes around yet--the new ROCDL library just depends
    on the existing LLVMGPU library and its passes directly to get started.
    This is meant to avoid churns and we can do the mechanical change once
    everything is sorted.
    
    Progress towards https://github.com/openxla/iree/issues/16341

---
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/ROCDLSelectLoweringStrategy.cpp
@@ -0,0 +1,51 @@
+// Copyright 2024 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "iree/compiler/Codegen/Dialect/Codegen/IR/IREECodegenAttrs.h"
+#include "iree/compiler/Codegen/Dialect/Codegen/IR/IREECodegenDialect.h"
+#include "iree/compiler/Codegen/LLVMGPU/ROCDLKernelConfig.h"
+#include "iree/compiler/Codegen/LLVMGPU/ROCDLPassDetail.h"
+#include "iree/compiler/Codegen/LLVMGPU/ROCDLPasses.h"
+#include "iree/compiler/Dialect/HAL/IR/HALOps.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+
+namespace mlir::iree_compiler {
+
+namespace {
+/// Selects a strategy for lowering an IREE hal.executable.variant to ROCDL.
+class ROCDLSelectLoweringStrategyPass
+    : public ROCDLSelectLoweringStrategyBase<ROCDLSelectLoweringStrategyPass> {
+public:
+  void getDependentDialects(DialectRegistry &registry) const override {
+    registry.insert<IREE::Codegen::IREECodegenDialect>();
+  }
+
+  void runOnOperation() override {
+    IREE::HAL::ExecutableVariantOp variantOp = getOperation();
+    ModuleOp moduleOp = variantOp.getInnerModule();
+
+    if (failed(initROCDLLaunchConfig(moduleOp))) {
+      return signalPassFailure();
+    }
+
+    std::optional<IREE::Codegen::TranslationInfoAttr> translationInfo =
+        getIdenticalTranslationInfo(variantOp);
+    if (!translationInfo) {
+      moduleOp.emitError(
+          "unsupported entry point functions with different translation info");
+      return signalPassFailure();
+    }
+  }
+};
+} // namespace
+
+std::unique_ptr<OperationPass<IREE::HAL::ExecutableVariantOp>>
+createROCDLSelectLoweringStrategyPass() {
+  return std::make_unique<ROCDLSelectLoweringStrategyPass>();
+}
+
+} // namespace mlir::iree_compiler
