4aa7fcc4dab798e370d91b20e3d8a5df8bc4d203
Jerry Wu <cheyuw@google.com>
2024-02-13 00:29:38 +0000

    Change pass add point

--- compiler/src/iree/compiler/GlobalOptimization/Passes.cpp
+++ compiler/src/iree/compiler/GlobalOptimization/Passes.cpp
@@ -145,11 +145,15 @@ void buildGlobalOptimizationPassPipeline(
     mainPassManager.addPass(createCSEPass());
     mainPassManager.addPass(createSimplifyPackUnpackPass());
   }
+  // Generalize transposes and any other remaining named linalg ops that can
+  // now be represented as generics.
+  FunctionLikeNest(mainPassManager).addPass(createGeneralizeLinalgNamedOpsPass);
+
+  // Hoist loop invariants (e.g. from scf loops) with zero-trip-check.
   FunctionLikeNest(mainPassManager)
-      // Generalize transposes and any other remaining named linalg ops that can
-      // now be represented as generics.
-      .addPass(createGeneralizeLinalgNamedOpsPass)
-      .addPass(createGlobalLoopInvariantCodeMotionPass);
+      .addPass(createGlobalLoopInvariantCodeMotionPass)
+      .addPass(mlir::createCanonicalizerPass)
+      .addPass(mlir::createCSEPass);
 
   OpPassManager pipeline(ModuleOp::getOperationName());
   FunctionLikeNest(pipeline)
