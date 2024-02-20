83c1744dd306232c154b8723b78843f4074afb19
Jerry Wu <cheyuw@google.com>
2024-02-20 22:03:05 +0000

    Address comments

--- compiler/src/iree/compiler/GlobalOptimization/GlobalLoopInvariantCodeMotion.cpp
+++ compiler/src/iree/compiler/GlobalOptimization/GlobalLoopInvariantCodeMotion.cpp
@@ -11,7 +11,6 @@
 #include "mlir/Dialect/SCF/Transforms/Transforms.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/IR/PatternMatch.h"
-#include "mlir/Transforms/TopologicalSortUtils.h"
 
 #include "llvm/Support/Debug.h"
 
@@ -20,29 +19,23 @@ using namespace mlir;
 #define DEBUG_TYPE "global-loop-invariant-code-motion"
 #define LICM_DBGS() (llvm::dbgs() << '[' << DEBUG_TYPE << "] ")
 
-// Check if the op and its producers are loop invariants and hoistable. Results
-// are cached in hoistableOpMap.
-//
-// It expects the op's producers have been checked and the results exist in
-// `hoistableOpMap`.
-static bool
-isHoistableOp(LoopLikeOpInterface loopOp, Operation *op,
-              llvm::SmallDenseMap<Operation *, bool> &hoistableOpMap) {
+// Check if the op and its producers are loop invariants and hoistable. Using
+// `hoistableOps` to check if producers are hoistable. It expects the op's
+// producers have been checked and the results exist in `hoistableOps`.
+static bool isHoistableOp(LoopLikeOpInterface loopOp, Operation *op,
+                          const llvm::SetVector<Operation *> &hoistableOps) {
   // Currently we don't handle implicit captures, so don't hoist ops with
   // regions.
   if (op->getNumRegions() > 0) {
-    hoistableOpMap[op] = false;
     return false;
   }
 
   // Check if the op type is hoistable.
   if (!isa<tensor::EmptyOp, tensor::PackOp>(op)) {
-    hoistableOpMap[op] = false;
     return false;
   }
 
   // Check if all producers are hoistable.
-  bool hoistable = true;
   for (OpOperand &operand : op->getOpOperands()) {
     Value value = operand.get();
     // Ignore values defined outside the loop.
@@ -52,46 +45,41 @@ isHoistableOp(LoopLikeOpInterface loopOp, Operation *op,
     Operation *producer = value.getDefiningOp();
     // If the producer is not an operation, can't hoist it.
     if (!producer) {
-      hoistable = false;
-      break;
+      return false;
     }
-
-    auto producerResult = hoistableOpMap.find(producer);
-    assert(producerResult != hoistableOpMap.end() &&
-           "producer should have been checked");
-    if (!producerResult->second) {
-      hoistable = false;
-      break;
+    if (!hoistableOps.contains(producer)) {
+      return false;
     }
   }
 
-  hoistableOpMap[op] = hoistable;
-  return hoistable;
+  return true;
 }
 
 static LogicalResult hoistLoopInvariants(LoopLikeOpInterface loopOp,
                                          RewriterBase &rewriter) {
-  // First find the root ops can be hoisted. The root op needs to satisfy:
-  // 1. It is a root op having benefits to be hoisted (e.g. tensor.pack)
-  // 2. Its backward slice can be hoisted (e.g. they are loop invariant)
-  SmallVector<Operation *> opsToHoist;
-  llvm::SmallDenseMap<Operation *, bool> hoistableOpMap;
+  // Find hoistable ops in the loop.
+  llvm::SetVector<Operation *> hoistableOps;
   for (Region *region : loopOp.getLoopRegions()) {
-    // Consider only the top-level ops in the region.
-    // The forward visiting also ensures we are check and add ops in topological
-    // order.
-    for (Operation &op : region->getOps()) {
-      if (isHoistableOp(loopOp, &op, hoistableOpMap)) {
+    // Skip loops with multi-block regions to simplify op's dependency.
+    if (!region->hasOneBlock())
+      return failure();
+
+    // Consider only the top-level ops in the region. The forward visiting in a
+    // single block ensures we are check and add ops in topological order.
+    for (Operation &op : region->front()) {
+      if (isHoistableOp(loopOp, &op, hoistableOps)) {
         LLVM_DEBUG(LICM_DBGS() << "Found hoistable op: " << op << "\n");
-        opsToHoist.push_back(&op);
+        hoistableOps.insert(&op);
       }
     }
   }
-  if (opsToHoist.empty())
+  if (hoistableOps.empty())
     return success();
 
   // Wrap the loop in zero-trip-check so the hoisted ops will only run when the
   // loop condition is ever satisfied.
+  // Assume the transformation only moves ops around in loop instead of clone
+  // them.
   FailureOr<LoopLikeOpInterface> wrappedLoop =
       TypeSwitch<Operation *, FailureOr<LoopLikeOpInterface>>(
           loopOp.getOperation())
@@ -102,9 +90,10 @@ static LogicalResult hoistLoopInvariants(LoopLikeOpInterface loopOp,
   if (failed(wrappedLoop))
     return failure();
 
-  // Hoist ops in their original insertion order, which is the topological
-  // order.
-  for (Operation *op : opsToHoist) {
+  // Hoist ops out of the loop in topological order.
+  for (Operation *op : hoistableOps) {
+    assert(wrappedLoop->getOperation()->isAncestor(op) &&
+           "assume hoistable ops still stay in the loop after transformation");
     wrappedLoop->moveOutOfLoop(op);
   }
   return success();
