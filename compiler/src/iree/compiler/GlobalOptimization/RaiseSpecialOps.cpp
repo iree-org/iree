876380d8c5a71c11995276dec64378ff6f836b23
hanhanW <hanhan0912@gmail.com>
2024-02-22 10:02:10 -0800

    fix include

--- compiler/src/iree/compiler/GlobalOptimization/RaiseSpecialOps.cpp
+++ compiler/src/iree/compiler/GlobalOptimization/RaiseSpecialOps.cpp
@@ -4,6 +4,7 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include "iree-dialects/Transforms/TransformMatchers.h"
 #include "iree/compiler/Dialect/Flow/Transforms/RegionOpUtils.h"
 #include "iree/compiler/Dialect/LinalgExt/IR/LinalgExtDialect.h"
 #include "iree/compiler/Dialect/LinalgExt/IR/LinalgExtInterfaces.h"
