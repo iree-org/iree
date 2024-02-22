599a3d16a97d5f05784ea1fa6b4a53d55e31c720
Diego Caballero <diegocaballero@google.com>
2024-02-22 13:26:12 -0800

    [CPU] Enable DT for [i8, i4 -> i32/f32] mmt4d (#16302)
    This only provides some tile sizes to target SMLAL instructions using DT
    only (no UK). We can enable the UK side targeting SMMLA once we have one
    written for that mmt4d configuration.

--- compiler/src/iree/compiler/Codegen/Common/CPU/CPUMaterializeEncodingPass.cpp
+++ compiler/src/iree/compiler/Codegen/Common/CPU/CPUMaterializeEncodingPass.cpp
@@ -113,8 +113,8 @@ enumerateMatmulTileArm64(TypeRange elementTypes, ExecutableTargetAttr target) {
     }
   }
 
-  if (lhs.isSignlessInteger(8) && rhs.isSignlessInteger(8) &&
-      out.isSignlessInteger(32)) {
+  if (hasUkernel(target) && lhs.isSignlessInteger(8) &&
+      (rhs.isSignlessInteger(8)) && out.isSignlessInteger(32)) {
     if (hasFeature(target, "+i8mm")) {
       return {
           TileMxNxK{8, 8, 8}, // Aim to use SMMLA.
@@ -131,12 +131,27 @@ enumerateMatmulTileArm64(TypeRange elementTypes, ExecutableTargetAttr target) {
           TileMxNxK{1, 8, 4}, // Truncation of the above.
       };
     }
-    return {
-        TileMxNxK{8, 8, 1}, // Aim to use SMLAL.
-        TileMxNxK{4, 8, 1}, // Truncation of the above.
-        TileMxNxK{2, 8, 1}, // Truncation of the above.
-        TileMxNxK{1, 8, 1}, // Truncation of the above.
-    };
+  }
+
+  if (!hasUkernel(target)) {
+    if (lhs.isSignlessInteger(8) && rhs.isSignlessInteger(8) &&
+        (out.isSignlessInteger(32) || out.isF32())) {
+      return {
+          TileMxNxK{8, 8, 1}, // Aim to use SMLAL.
+          TileMxNxK{4, 8, 1}, // Truncation of the above.
+          TileMxNxK{2, 8, 1}, // Truncation of the above.
+          TileMxNxK{1, 8, 1}, // Truncation of the above.
+      };
+    }
+    if (lhs.isSignlessInteger(8) && rhs.isSignlessInteger(4) &&
+        (out.isSignlessInteger(32) || out.isF32())) {
+      return {
+          TileMxNxK{4, 16, 1}, // Aim to use SMLAL.
+          TileMxNxK{4, 16, 1}, // Truncation of the above.
+          TileMxNxK{2, 32, 1}, // Truncation of the above.
+          TileMxNxK{1, 64, 1}, // Truncation of the above.
+      };
+    }
   }
 
   // Fallback - no architecture-optimized tile size for this case.
