7bed8595b029763788562aee9e0d38e3ffdc292d
Lei Zhang <antiagainst@gmail.com>
2024-02-23 17:43:03 -0800

    Fix CUDA test
    Do not overwrite existing translation info from transform
    diaelct

--- compiler/src/iree/compiler/Codegen/LLVMGPU/KernelConfig.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/KernelConfig.cpp
@@ -1516,24 +1516,27 @@ LogicalResult initGPULaunchConfig(ModuleOp moduleOp) {
     if (!exportOp)
       continue;
 
-    // First check whether we already have workgroup count set--it's a
-    // "contract" to indicate that we should bypass all tiling and
-    // distribution to go down just the most basic lowering flow.
-    if (Block *body = exportOp.getWorkgroupCountBody()) {
-      auto retOp = cast<IREE::HAL::ReturnOp>(body->getTerminator());
-      // For scalar dispatch cases--using just one thread of one workgroup.
-      auto isOne = [](Value value) { return matchPattern(value, m_One()); };
-      if (llvm::all_of(retOp.getOperands(), isOne)) {
-        std::array<int64_t, 3> workgroupSize = {1, 1, 1};
-        if (failed(setDispatchConfig(funcOp, workgroupSize, std::nullopt)))
-          return failure();
-        auto translationInfo = IREE::Codegen::TranslationInfoAttr::get(
-            funcOp.getContext(),
-            IREE::Codegen::DispatchLoweringPassPipeline::LLVMGPUBaseLowering);
-        if (failed(setTranslationInfo(funcOp, translationInfo))) {
-          return failure();
+    if (!getTranslationInfo(funcOp)) {
+      // If no translation info set, first check whether we already have
+      // workgroup count set--it's a "contract" to indicate that we should
+      // bypass all tiling and distribution to go down just the most basic
+      // lowering flow.
+      if (Block *body = exportOp.getWorkgroupCountBody()) {
+        auto retOp = cast<IREE::HAL::ReturnOp>(body->getTerminator());
+        // For scalar dispatch cases--using just one thread of one workgroup.
+        auto isOne = [](Value value) { return matchPattern(value, m_One()); };
+        if (llvm::all_of(retOp.getOperands(), isOne)) {
+          std::array<int64_t, 3> workgroupSize = {1, 1, 1};
+          if (failed(setDispatchConfig(funcOp, workgroupSize, std::nullopt)))
+            return failure();
+          auto translationInfo = IREE::Codegen::TranslationInfoAttr::get(
+              funcOp.getContext(),
+              IREE::Codegen::DispatchLoweringPassPipeline::LLVMGPUBaseLowering);
+          if (failed(setTranslationInfo(funcOp, translationInfo))) {
+            return failure();
+          }
+          continue;
         }
-        continue;
       }
     }
 
