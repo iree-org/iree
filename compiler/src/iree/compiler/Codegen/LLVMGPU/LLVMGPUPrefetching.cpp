dcef18df410fd9e558ffe84f4ca4781c2415e104
Lei Zhang <antiagainst@gmail.com>
2024-03-10 17:44:08 -0700

    Revert "Clean up the pass"
    This reverts commit ede214040fa1f066415f08cf534e0533ce745b21.

--- compiler/src/iree/compiler/Codegen/LLVMGPU/LLVMGPUPrefetching.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/LLVMGPUPrefetching.cpp
@@ -7,7 +7,6 @@
 #include "iree/compiler/Codegen/LLVMGPU/PassDetail.h"
 #include "iree/compiler/Codegen/LLVMGPU/Passes.h"
 #include "iree/compiler/Codegen/LLVMGPU/Utils/LLVMGPUUtils.h"
-#include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/SmallVector.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/Dialect/SCF/Transforms/Transforms.h"
@@ -26,17 +25,14 @@ struct LLVMGPUPrefetchSharedMemoryPass final
 
     SmallVector<scf::ForOp> loops;
     funcOp.walk([&loops](scf::ForOp forOp) { loops.push_back(forOp); });
-    if (!llvm::hasSingleElement(loops)) {
-      // Right now we only support non-nested single loop. Don't touch the code
-      // for other cases.
-      return;
-    }
 
-    FailureOr<scf::ForOp> newLoop =
-        prefetchSharedMemoryCopy(rewriter, loops[0]);
-    // The only possible failure is the analysis failure, which does not cause
-    // the pass to fail. Therefore we discard any failures at this point.
-    (void)newLoop;
+    for (scf::ForOp forOp : loops) {
+      FailureOr<scf::ForOp> newLoop = prefetchSharedMemoryCopy(rewriter, forOp);
+      // The only possible failure is the analysis failure, which does not cause
+      // the pass to fail. Therefore we discard any failures at this point.
+      (void)newLoop;
+      break; // TODO: Fix nested loop handling.
+    }
   }
 };
 
