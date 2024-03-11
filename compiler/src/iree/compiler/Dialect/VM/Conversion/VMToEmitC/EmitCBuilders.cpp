6d703d41f2bca36cf4e91ae9972573cd2aed9140
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2024-03-11 15:26:53 +0000

    Drop mapBinaryOperator enum

--- compiler/src/iree/compiler/Dialect/VM/Conversion/VMToEmitC/EmitCBuilders.cpp
+++ compiler/src/iree/compiler/Dialect/VM/Conversion/VMToEmitC/EmitCBuilders.cpp
@@ -58,50 +58,6 @@ std::string mapUnaryOperator(UnaryOperator op) {
     return "XXX";
   }
 }
-
-std::string mapBinaryOperator(BinaryOperator op) {
-  switch (op) {
-  case BinaryOperator::ADDITION:
-    return "+";
-  case BinaryOperator::SUBTRACTION:
-    return "-";
-  case BinaryOperator::PRODUCT:
-    return "*";
-  case BinaryOperator::DIVISION:
-    return "/";
-  case BinaryOperator::REMAINDER:
-    return "%";
-  case BinaryOperator::BITWISE_AND:
-    return "&";
-  case BinaryOperator::BITWISE_OR:
-    return "|";
-  case BinaryOperator::BITWISE_XOR:
-    return "^";
-  case BinaryOperator::BITWISE_LEFT_SHIFT:
-    return "<<";
-  case BinaryOperator::BITWISE_RIGHT_SHIFT:
-    return ">>";
-  case BinaryOperator::LOGICAL_AND:
-    return "&&";
-  case BinaryOperator::LOGICAL_OR:
-    return "||";
-  case BinaryOperator::EQUAL_TO:
-    return "==";
-  case BinaryOperator::NOT_EQUAL_TO:
-    return "!=";
-  case BinaryOperator::LESS_THAN:
-    return "<";
-  case BinaryOperator::GREATER_THAN:
-    return ">";
-  case BinaryOperator::LESS_THAN_OR_EQUAL:
-    return "<=";
-  case BinaryOperator::GREATER_THAN_OR_EQUAL:
-    return ">=";
-  default:
-    llvm_unreachable("unsupported binary operator");
-    return "XXX";
-  }
-}
 } // namespace
 
 Value unaryOperator(OpBuilder builder, Location location, UnaryOperator op,
