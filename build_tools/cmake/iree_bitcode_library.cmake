3b30ab4e53ee85eb48b2a56081a25f87cc4b2a6d
Benoit Jacob <benoitjacob@google.com>
2024-03-14 16:49:13 -0400

    Revert "Ukernels: enable limited debug information that is useful in profilers like Tracy." (#16779)
    Reverts openxla/iree#15756
    
    Reason: more models have the same issue,
    https://github.com/openxla/iree/actions/runs/8284380403/attempts/1

--- build_tools/cmake/iree_bitcode_library.cmake
+++ build_tools/cmake/iree_bitcode_library.cmake
@@ -55,9 +55,6 @@ function(iree_bitcode_library)
     "-fno-ident"
     "-fdiscard-value-names"
 
-    # Limited debug information that is useful in profilers like Tracy.
-    "-gline-tables-only"
-
     # Set the size of wchar_t to 4 bytes (instead of 2 bytes).
     # This must match what the runtime is built with.
     "-fno-short-wchar"
