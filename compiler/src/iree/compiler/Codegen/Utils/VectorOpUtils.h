17484ad7081b6638a9abc238c61ec587859d0e0a
Quinn Dawkins <quinn.dawkins@gmail.com>
2024-03-17 10:52:00 -0400

    [LLVMGPU] allow multiple m and n dims in contraction distribution (#16808)
    This adjusts the layout generation logic to allow distribution of
    contractions with multiple m and n dimensions by greedily using the
    subgroup/tile_counts of the mma_schedule with the outer dims. The inner
    most m/n dimensions are still required to be divisible by the intrinsic
    shape. (and this only supports a single k dimension).
    
    This also decouples the ordering logic of the batch/subgroup
    distribution from the lane distribution for the intrinsics. Currently it
    assumes intrinsics can only specify three important sizes, an M, N, and
    K size. To support distributed batches this would require adding a
    fourth dim type.
    
    I'm checkpointing this work to allow further evolution of the set of
    intrinsics.

--- compiler/src/iree/compiler/Codegen/Utils/VectorOpUtils.h
+++ compiler/src/iree/compiler/Codegen/Utils/VectorOpUtils.h
@@ -4,7 +4,9 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include "mlir/Dialect/Linalg/IR/LinalgInterfaces.h"
 #include "mlir/Dialect/Vector/IR/VectorOps.h"
+#include "mlir/IR/BuiltinTypes.h"
 
 namespace mlir::iree_compiler {
 
@@ -14,6 +16,7 @@ public:
   enum class OpKind { MK_KN_MN, MK_NK_MN, KM_NK_MN, UNKNOWN };
 
   explicit VectorContractOpInfo(vector::ContractionOp op) {
+    contractionDims = *linalg::inferContractionDims(op.getIndexingMapsArray());
     opKind = inferOpKind(op.getContext(), op.getIndexingMapsArray());
   }
 
@@ -21,18 +24,44 @@ public:
 
   // Returns the (LHS M, RHS N) dimension index pair.
   std::optional<std::pair<int, int>> getOperandMNIndex() const;
+  std::pair<int, int> getOperandFullMNIndex() const;
 
   // Returns the (LHS K, RHS K) dimension index pair.
   std::optional<std::pair<int, int>> getOperandKIndex() const;
+  std::pair<int, int> getOperandFullKIndex() const;
 
   // Returns the result (M, N) dimension index pair.
   std::optional<std::pair<int, int>> getResultMNIndex() const;
+  std::pair<int, int> getResultFullMNIndex() const;
+
+  SmallVector<unsigned, 2> getMDims() const { return contractionDims.m; }
+
+  SmallVector<unsigned, 2> getNDims() const { return contractionDims.n; }
+
+  int64_t getARank() {
+    return contractionDims.m.size() + contractionDims.k.size();
+  }
+  int64_t getBRank() {
+    return contractionDims.k.size() + contractionDims.n.size();
+  }
+  int64_t getCRank() {
+    return contractionDims.m.size() + contractionDims.n.size();
+  }
+
+  SmallVector<int64_t> lhsMDims;
+  int64_t lhsKDim;
+  SmallVector<int64_t> rhsNDims;
+  int64_t rhsKDim;
+  SmallVector<int64_t> outMDims;
+  SmallVector<int64_t> outNDims;
 
 private:
   // Gets the kind of a contract op with the given indexing |maps|.
-  OpKind inferOpKind(MLIRContext *ctx, SmallVector<AffineMap> maps) const;
+  OpKind inferOpKind(MLIRContext *ctx, SmallVector<AffineMap> maps);
 
   OpKind opKind = OpKind::UNKNOWN;
+
+  linalg::ContractionDimensions contractionDims;
 };
 
 } // namespace mlir::iree_compiler
