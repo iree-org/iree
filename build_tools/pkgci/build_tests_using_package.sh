5f2743baaa79160a3883854e60e5188822dceeb1
Lei Zhang <antiagainst@gmail.com>
2024-03-20 16:39:24 -0700

    [hip] Move into hal/drivers and build by default  (#16706)
    This commit moves the hip HAL driver into the
    `hal/drivers` directory and starts to build by
    default. Right now only CMake build is supported. 
    
    ci-extra: test_nvidia_gpu, test_nvidia_a100, build_test_all_windows,
    build_test_all_macos_arm64, build_test_all_macos_x86_64,
    build_test_all_arm64
    
    ---------
    
    Co-authored-by: Scott Todd <scott.todd0@gmail.com>

--- build_tools/pkgci/build_tests_using_package.sh
+++ build_tools/pkgci/build_tests_using_package.sh
@@ -38,6 +38,7 @@ export IREE_CPU_DISABLE="${IREE_CPU_DISABLE:-0}"
 export IREE_VULKAN_DISABLE="${IREE_VULKAN_DISABLE:-1}"
 export IREE_METAL_DISABLE="${IREE_METAL_DISABLE:-1}"
 export IREE_CUDA_DISABLE="${IREE_CUDA_DISABLE:-1}"
+export IREE_HIP_DISABLE="${IREE_HIP_DISABLE:-1}"
 
 # Set cmake options based on disabled features.
 declare -a cmake_config_options=()
@@ -53,6 +54,9 @@ if (( IREE_METAL_DISABLE == 1 )); then
 fi
 if (( IREE_CUDA_DISABLE == 1 )); then
   cmake_config_options+=("-DIREE_HAL_DRIVER_CUDA=OFF")
+fi
+if (( IREE_HIP_DISABLE == 1 )); then
+  cmake_config_options+=("-DIREE_HAL_DRIVER_HIP=OFF")
 fi
 
 ###############################################################################
---
+++ runtime/src/iree/hal/drivers/hip/dynamic_symbols.h
@@ -0,0 +1,61 @@
+// Copyright 2023 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef IREE_HAL_DRIVERS_HIP_DYNAMIC_SYMBOLS_H_
+#define IREE_HAL_DRIVERS_HIP_DYNAMIC_SYMBOLS_H_
+
+#include "iree/base/api.h"
+#include "iree/base/internal/dynamic_library.h"
+#include "iree/hal/drivers/hip/hip_headers.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+// iree_dynamic_library_t allows dynamically loading a subset of HIP driver API.
+// We load all the symbols in `dynamic_symbol_tables.h` and fail if any of the
+// symbol is not available. The functions signatures are matching the
+// declarations in `hip_runtime_api.h`.
+
+//===----------------------------------------------------------------------===//
+// HIP dynamic symbols
+//===----------------------------------------------------------------------===//
+
+// HIP driver API dynamic symbols.
+typedef struct iree_hal_hip_dynamic_symbols_t {
+  // The dynamic library handle.
+  iree_dynamic_library_t* dylib;
+
+  // Concrete HIP symbols defined by including the `dynamic_symbol_tables.h`.
+#define IREE_HAL_HIP_REQUIRED_PFN_DECL(hipSymbolName, ...) \
+  hipError_t (*hipSymbolName)(__VA_ARGS__);
+#define IREE_HAL_HIP_REQUIRED_PFN_STR_DECL(hipSymbolName, ...) \
+  const char* (*hipSymbolName)(__VA_ARGS__);
+#define IREE_HAL_HIP_OPTIONAL_PFN_DECL(hipSymbolName, ...) \
+  hipError_t (*hipSymbolName)(__VA_ARGS__);
+#include "iree/hal/drivers/hip/dynamic_symbol_tables.h"  // IWYU pragma: export
+#undef IREE_HAL_HIP_REQUIRED_PFN_DECL
+#undef IREE_HAL_HIP_REQUIRED_PFN_STR_DECL
+#undef IREE_HAL_HIP_OPTIONAL_PFN_DECL
+} iree_hal_hip_dynamic_symbols_t;
+
+// Initializes |out_syms| in-place with dynamically loaded HIP symbols.
+// iree_hal_hip_dynamic_symbols_deinitialize must be used to release the
+// library resources.
+iree_status_t iree_hal_hip_dynamic_symbols_initialize(
+    iree_allocator_t host_allocator, iree_hal_hip_dynamic_symbols_t* out_syms);
+
+// Deinitializes |syms| by unloading the backing library. All function pointers
+// will be invalidated. They _may_ still work if there are other reasons the
+// library remains loaded so be careful.
+void iree_hal_hip_dynamic_symbols_deinitialize(
+    iree_hal_hip_dynamic_symbols_t* syms);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // IREE_HAL_DRIVERS_HIP_DYNAMIC_SYMBOLS_H_
