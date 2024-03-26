a92a775758c8978382719ed336dc3173b3d6b363
Benjamin Maxwell <benjamin.maxwell@arm.com>
2024-03-26 12:23:47 +0000

    Fixups (renaming + comments)

--- compiler/src/iree/compiler/Codegen/LLVMCPU/LLVMCPU2DScalableTo1DScalable.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMCPU/LLVMCPU2DScalableTo1DScalable.cpp
@@ -13,19 +13,70 @@
 #include "mlir/Pass/Pass.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
-#define DEBUG_TYPE "iree-llvmcpu-unsupported-scalability-to-loops"
-#define VEC_DBGS() (llvm::dbgs() << '[' << DEBUG_TYPE << "] ")
-
 namespace mlir::iree_compiler {
 
 namespace {
 
-class LLVMCPUUnsupportedScalabilityToLoopsPass
-    : public LLVMCPUUnsupportedScalabilityToLoopsBase<
-          LLVMCPUUnsupportedScalabilityToLoopsPass> {
+/// Currently, IREE requires `lowering_config`s to be propagated to all compute
+/// ops within a dispatch region. This can be problematic for SME, as it does
+/// not make sense to use 2D-scalability for all ops within a dispatch region.
+/// The Arm SME target only supports 2D scalable outer products, so if it's not
+/// an outer product, we can only scalably vectorize in one dimension.
+///
+/// The solution here is this pass (`2d-scalable-to-1d-scalable`) that runs just
+/// before vectorization, that drops unsupported scalable tile/vector sizes,
+/// producing loops of ops that will only be vectorized scalably in one
+/// dimension. This allows earlier passes like `tile-and-fuse` to still function
+/// correctly.
+///
+/// Take this simple example:
+///
+/// ```mlir
+/// // Lowering configs propagated (from matmul):
+/// linalg.fill {lowering_config = [[4], [4]]
+/// linalg.matmul {lowering_config = [[4], [4], 1]
+/// linalg.generic {lowering_config = [[4], [4]]
+/// ```
+/// Here the `linalg.generic` cannot be vectorized with 2D scalable vectors.
+///
+/// After `tile-and-fuse` (which requires consistent lowering configs):
+/// ```mlir
+/// scf.for i in range(0, 1000) step 4 x vscale {
+///   scf.for j in range(0, 2000) step 4 x vscale {
+///     linalg.fill {lowering_config = [[4], [4]]
+///     for k in range(0, 100) step 1 {
+///       linalg.matmul {lowering_config = [[4], [4], 1]
+///     }
+///     // 2D scalable vectorization unsupported here:
+///     linalg.generic {lowering_config = [[4], [4]]
+///   }
+/// }
+/// ```
+///
+/// Unsupported scalability removed (by `2d-scalable-to-1d-scalable`):
+/// ```mlir
+/// scf.for i in range(0, 1000) step 4 x vscale {
+///   scf.for j in range(0, 2000) step 4 x vscale {
+///     linalg.fill {lowering_config = [[4], [4]]
+///     for k in range(0, 100) step 1 {
+///       linalg.matmul {lowering_config = [[4], [4], 1]
+///     }
+///     // Insert a new loop:
+///     for n in range(0, 4 x vscale) step 4 {
+///        // Drop a scalable dim:
+///        linalg.generic {lowering_config = [4, [4]]
+///     }
+///   }
+/// }
+/// ```
+///
+/// This can now be vectorized and lowered successfully, which produces a
+/// dispatch that mixes SME and SVE.
+class LLVMCPU2DScalableTo1DScalablePass
+    : public LLVMCPU2DScalableTo1DScalableBase<
+          LLVMCPU2DScalableTo1DScalablePass> {
 public:
-  using LLVMCPUUnsupportedScalabilityToLoopsBase::
-      LLVMCPUUnsupportedScalabilityToLoopsBase;
+  using LLVMCPU2DScalableTo1DScalableBase::LLVMCPU2DScalableTo1DScalableBase;
 
   void getDependentDialects(DialectRegistry &registry) const override {
     registry
@@ -39,6 +90,9 @@ static bool opKnownToSupport2DScalableVectorizationWithArmSME(Operation *op) {
   return isa<linalg::MatmulOp, linalg::MatmulTransposeAOp, linalg::FillOp>(op);
 }
 
+// Note: It would be easy to parameterize this rewrite to convert N-D scalable
+// operations to M-D scalable ones (where M < N). However this is currently not
+// needed.
 struct DropUnsupportedScalableDimsFromTilingInterfaceOps
     : public OpInterfaceRewritePattern<TilingInterface> {
   DropUnsupportedScalableDimsFromTilingInterfaceOps(MLIRContext *context,
@@ -47,8 +101,9 @@ struct DropUnsupportedScalableDimsFromTilingInterfaceOps
 
   LogicalResult matchAndRewrite(TilingInterface op,
                                 PatternRewriter &rewriter) const override {
-    // Note: This rewrite is currently only required for ArmSME (which is the
-    // only target that currently has some concept of 2D scalability).
+    // Note: Which operations should have scalability dropped is specific to
+    // ArmSME. The rest of this rewrite could be generic (though currently
+    // there's no other targets that support > 1D scalability).
     auto targetAttr = IREE::HAL::ExecutableTargetAttr::lookup(op);
     bool isArmSME = assumeArmSME || hasSMEFeature(targetAttr);
     if (!isArmSME || opKnownToSupport2DScalableVectorizationWithArmSME(op))
@@ -61,7 +116,7 @@ struct DropUnsupportedScalableDimsFromTilingInterfaceOps
     auto tileSizes = loweringConfigAttr.getTileSizeVals();
     auto scalableFlags = loweringConfigAttr.getScalableTileFlagVals();
 
-    // Drop scalable dimensions from leading tiling levels first (and leading
+    // 1. Drop scalable dimensions from leading tiling levels first (and leading
     // dimensions within tiling levels). This works out as dropping scalability
     // from leading dimensions of the vector type.
     int64_t firstTilingLevelWithScalableDims = -1;
@@ -91,7 +146,7 @@ struct DropUnsupportedScalableDimsFromTilingInterfaceOps
       }
     }
 
-    // Re-tile the operation with some scalability dropped. This introduces
+    // 2. Re-tile the operation with some scalability dropped. This introduces
     // loops for previously scalable vector/tile sizes.
     scf::SCFTilingOptions options{};
     setSCFTileSizes(options, op, loopTileSizes, {});
@@ -100,7 +155,7 @@ struct DropUnsupportedScalableDimsFromTilingInterfaceOps
     if (failed(tilingResult))
       return failure();
 
-    // Update the lowering config of the new tiled operations.
+    // 3. Update the lowering config of the new tiled operations.
     scalableFlags[firstTilingLevelWithScalableDims] = newScalableFlags;
     auto newLoweringConfig = IREE::Codegen::LoweringConfigAttr::get(
         getContext(), tileSizes, scalableFlags);
@@ -117,7 +172,7 @@ private:
   bool assumeArmSME{false};
 };
 
-void LLVMCPUUnsupportedScalabilityToLoopsPass::runOnOperation() {
+void LLVMCPU2DScalableTo1DScalablePass::runOnOperation() {
   RewritePatternSet patterns(&getContext());
   patterns.add<DropUnsupportedScalableDimsFromTilingInterfaceOps>(
       patterns.getContext(), assumeArmSME);
@@ -131,8 +186,8 @@ void LLVMCPUUnsupportedScalabilityToLoopsPass::runOnOperation() {
 } // namespace
 
 std::unique_ptr<InterfacePass<mlir::FunctionOpInterface>>
-createLLVMCPUUnsupportedScalabilityToLoopsPass() {
-  return std::make_unique<LLVMCPUUnsupportedScalabilityToLoopsPass>();
+createLLVMCPU2DScalableTo1DScalablePass() {
+  return std::make_unique<LLVMCPU2DScalableTo1DScalablePass>();
 }
 
 } // namespace mlir::iree_compiler
