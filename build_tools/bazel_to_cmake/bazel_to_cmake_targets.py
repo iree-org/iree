54e84d83d30011254c33b356bbd2aa6b5a8e5ff4
Scott Todd <scott.todd0@gmail.com>
2024-03-27 17:30:33 +0000

    Bazel and CMake whack-a-mole.

--- build_tools/bazel_to_cmake/bazel_to_cmake_targets.py
+++ build_tools/bazel_to_cmake/bazel_to_cmake_targets.py
@@ -64,6 +64,7 @@ class TargetConverter:
                 "@llvm-project//mlir:DialectUtils": [""],
                 "@llvm-project//mlir:GPUDialect": ["MLIRGPUDialect"],
                 "@llvm-project//mlir:GPUTransforms": ["MLIRGPUTransforms"],
+                "@llvm-project//mlir:InliningUtils": [""],
                 "@llvm-project//mlir:LinalgOpsIncGen": ["MLIRLinalgOpsIncGenLib"],
                 "@llvm-project//mlir:LinalgStructuredOpsIncGen": [
                     "MLIRLinalgStructuredOpsIncGenLib"
