e19a34373b7a7d73f1d980ec1e88156d7e39a04f
hanhanW <hanhan0912@gmail.com>
2024-04-15 22:29:55 +0000

    Revert "[HAL] Remove the CPUMaterializeUpperBoundTileSizePass from HAL pipeline."
    This reverts commit bfc6a36d85661f1e444239f96a26ec58076df275.

--- compiler/src/iree/compiler/Dialect/HAL/Transforms/Passes.cpp
+++ compiler/src/iree/compiler/Dialect/HAL/Transforms/Passes.cpp
@@ -8,6 +8,7 @@
 
 #include <memory>
 
+#include "iree/compiler/Codegen/Common/CPU/Passes.h"
 #include "iree/compiler/Dialect/HAL/IR/HALDialect.h"
 #include "iree/compiler/Dialect/HAL/IR/HALOps.h"
 #include "iree/compiler/Dialect/Util/Transforms/Passes.h"
@@ -275,6 +276,10 @@ void buildHALTransformPassPipeline(OpPassManager &passManager,
   if (compileFrom < PipelinePhase::ExecutableSources) {
     buildHALConfigurationPassPipeline(passManager, targetRegistry,
                                       targetOptions, hooks);
+
+    FunctionLikeNest(passManager).addPass([]() {
+      return createCPUMaterializeUpperBoundTileSizePass();
+    });
 
     // Preprocess executables using an external tool. The tool may mutate one or
     // more variants and even insert or remove variants.
