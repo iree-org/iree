e646ef8ba5c8b61222af961321a1a05d3d2745d9
Balaji V. Iyer <43187390+bviyer@users.noreply.github.com>
2024-04-18 14:03:07 -0500

    Update compiler/src/iree/compiler/Pipelines/Options.cpp

--- compiler/src/iree/compiler/Pipelines/Options.cpp
+++ compiler/src/iree/compiler/Pipelines/Options.cpp
@@ -44,8 +44,8 @@ void InputDialectOptions::bindOptions(OptionsBinder &binder) {
 // messages, so we err on the side of being helpful and populating plugin
 // options here, even though it is a layering violation.
 #ifdef IREE_COMPILER_PLUGIN_HAVE_STATIC_INPUT_STABLEHLO
-          "  =stablehlo     - Legalize from StableHLO ops (Do VHLO to StableHLO conversion ap priori, if necessary).\n"
-          "  =stablehlo_xla - Legalize from StableHLO ops (with XLA cleanup preprocessing and VHLO to StableHLO conversion, if necessary).\n"
+          "  =stablehlo     - Legalize from StableHLO ops (including VHLO deserialization).\n"
+          "  =stablehlo_xla - Legalize from StableHLO ops (including VHLO deserialization and XLA de-tupling).\n"
 #endif // IREE_COMPILER_PLUGIN_HAVE_STATIC_INPUT_STABLEHLO
 #ifdef IREE_COMPILER_PLUGIN_HAVE_STATIC_INPUT_TOSA
           "  =tosa          - Legalize from TOSA ops.\n"
