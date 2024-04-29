e264282751ea3f0c42bf02299eb476ca271ed091
Quinn Dawkins <quinn@nod-labs.com>
2024-04-29 19:32:53 -0400

    [Codegen] Add lane distribution for scf.forall
    This adds a new type of mapping id, iree_gpu.lane_id, and an op to
    distribute forall ops mapping to it. Lane mapped forall ops require that
    the thread count exactly matches with the subgroup count, so this is
    added downstream due to incompatibility with upstream lowerings, however
    this should be upstreamed in the future.

--- compiler/src/iree/compiler/Codegen/Dialect/GPU/IR/IREEGPUAttrs.cpp
+++ compiler/src/iree/compiler/Codegen/Dialect/GPU/IR/IREEGPUAttrs.cpp
@@ -1001,6 +1001,16 @@ MMAScheduleAttr::getContractionLayout(vector::ContractionOp contractOp) const {
 }
 
 //===----------------------------------------------------------------------===//
+// LaneIdAttr
+//===----------------------------------------------------------------------===//
+
+int64_t LaneIdAttr::getMappingId() const { return getDim(); }
+
+bool LaneIdAttr::isLinearMapping() const { return true; }
+
+int64_t LaneIdAttr::getRelativeIndex() const { return getDim(); }
+
+//===----------------------------------------------------------------------===//
 // Attribute Registration
 //===----------------------------------------------------------------------===//
 
