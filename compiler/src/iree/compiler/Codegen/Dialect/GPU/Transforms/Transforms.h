a36773a2420b48f0906c710389b0f9965504c10f
Quinn Dawkins <quinn.dawkins@gmail.com>
2024-05-21 12:08:09 -0400

    [Codegen][GPU] Add vectorization pattern for iree_gpu.multi_mma (#17453)
    This allows vectorizing a statically shaped `iree_gpu.multi_mma` op. To
    use this pattern with dynamically shaped tensors, a tiling
    implementation for this op will need to be added in the future.
    
    Additionally this adds a new set of `IREEGPUTransformExtensions` because
    `Codegen/CommonExtensions` has become a rather large file and this is
    specific to the dialect.

---
+++ compiler/src/iree/compiler/Codegen/Dialect/GPU/Transforms/Transforms.h
@@ -0,0 +1,25 @@
+// Copyright 2024 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+//===- Transforms.h - Transformations for the IREE GPU dialect ------------===//
+//
+// Defines transformations that apply to IREE GPU ops for use in multiple
+// places.
+//
+//===----------------------------------------------------------------------===//
+#ifndef IREE_COMPILER_CODEGEN_DIALECT_GPU_TRANSFORMS_TRANSFORMS_H_
+#define IREE_COMPILER_CODEGEN_DIALECT_GPU_TRANSFORMS_TRANSFORMS_H_
+
+#include "iree/compiler/Codegen/Dialect/GPU/IR/IREEGPUOps.h"
+#include "mlir/IR/PatternMatch.h"
+
+namespace mlir::iree_compiler::IREE::GPU {
+
+void populateIREEGPUVectorizationPatterns(RewritePatternSet &patterns);
+
+} // namespace mlir::iree_compiler::IREE::GPU
+
+#endif // IREE_COMPILER_CODEGEN_DIALECT_GPU_TRANSFORMS_TRANSFORMS_H_
