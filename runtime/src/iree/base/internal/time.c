49c76acf7e288a6d4c9d317fd4884e8048ac7b99
Scott Todd <scott.todd0@gmail.com>
2024-05-23 23:03:39 +0000

    Fix copyright year from 2014 to 2024, lol.
    Signed-off-by: Scott Todd <scott.todd0@gmail.com>

--- runtime/src/iree/base/internal/time.c
+++ runtime/src/iree/base/internal/time.c
@@ -1,4 +1,4 @@
-// Copyright 2014 The IREE Authors
+// Copyright 2024 The IREE Authors
 //
 // Licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
