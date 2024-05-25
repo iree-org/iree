282c3b8d232be164d7c9d9828c3ff53659de0834
stanley-nod <stanley@nod-labs.com>
2024-05-25 00:47:41 -0700

    [Backend][ROCM] Add gfx1150 support.
    Enable support on ROCMTarget to compile gfx1150.
    
    Signed-off-by: stanley-nod <stanley@nod-labs.com>

--- compiler/src/iree/compiler/Codegen/Dialect/GPU/TargetUtils/KnownTargets.cpp
+++ compiler/src/iree/compiler/Codegen/Dialect/GPU/TargetUtils/KnownTargets.cpp
@@ -213,7 +213,7 @@ std::optional<TargetDetails> getAMDGPUTargetDetails(StringRef target) {
       .Case("rx7800xt", TargetDetails{rdna3Wgp, &rx7800xtChip})
       // https://www.techpowerup.com/gpu-specs/radeon-rx-7700-xt.c3911
       .Case("rx7700xt", TargetDetails{rdna3Wgp, &rx7700xtChip})
-      .Cases("rdna3", "gfx1100", "gfx1101", "gfx1102", "gfx1103",
+      .Cases("rdna3", "gfx1100", "gfx1101", "gfx1102", "gfx1103", "gfx1150",
              TargetDetails{rdna3Wgp, nullptr})
       .Default(std::nullopt);
 }
