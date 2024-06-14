c5d4b96a4aadbc86c6b63052e900adbc88037d38
Dave Liddell <dave.liddell@amd.com>
2024-06-14 10:53:31 -0600

    Allow flags to be set with greater flexibility (#17659)
    Changes to the python binding to allow iree.runtime.flags.parse_flags to
    take effect at times other than before the first time a driver is
    created. Also includes fixes for bugs exposed during the development of
    this feature.
    
    - Added "internal" API functions `create_hal_driver()` and
    `clear_hal_driver_cache()` to create a driver object independent of the
    cache, and to clear the cache, respectively
    - Added `HalDriver` class implementation functions for the above new API
    functions. Refactored class to share as much common code as possible.
    - Factored out driver URI processing into its own nested class for
    easier handling of URI components
    - Fixed dangling pointer bug. In the C layer flags are being kept by
    reference as string views, requiring the caller to keep the original
    flag strings (argc, argv) around for as long as the flags are being
    used. However, the python binding was using a local variable for those
    strings, letting them go out of scope and causing garbage values later
    on. The fix is to move the strings to a file scope variable. Flag
    handling does not appear to be getting used in a multi-threaded
    environment, as other aspects of flag handling use static variables with
    no mutex guarding that I could find.
    - Fixed runtime assert in Windows debug build for the improper use of
    std::vector<>::front() on an empty vector. The code never used the value
    of front(), as it was guarded by a check for the vector's size, but the
    assert prevents the debug build from running.
    
    ---------
    
    Signed-off-by: Dave Liddell <dave.liddell@amd.com>
    Signed-off-by: daveliddell <dave.liddell@amd.com>

--- runtime/bindings/python/hal.cc
+++ runtime/bindings/python/hal.cc
@@ -905,29 +905,40 @@ std::vector<std::string> HalDriver::Query() {
   return driver_names;
 }
 
-py::object HalDriver::Create(const std::string& device_uri,
-                             py::dict& driver_cache) {
-  iree_string_view_t driver_name, device_path, params_str;
+HalDriver::DeviceUri::DeviceUri(const std::string& device_uri) {
   iree_string_view_t device_uri_sv{
       device_uri.data(), static_cast<iree_host_size_t>(device_uri.size())};
   iree_uri_split(device_uri_sv, &driver_name, &device_path, &params_str);
+}
 
-  // Check cache.
-  py::str cache_key(driver_name.data, driver_name.size);
+py::object HalDriver::Create(const DeviceUri& device_uri) {
+  iree_hal_driver_t* driver;
+  CheckApiStatus(iree_hal_driver_registry_try_create(
+                     iree_hal_driver_registry_default(), device_uri.driver_name,
+                     iree_allocator_system(), &driver),
+                 "Error creating driver");
+
+  py::object driver_obj = py::cast(HalDriver::StealFromRawPtr(driver));
+  return driver_obj;
+}
+
+py::object HalDriver::Create(const std::string& device_uri) {
+  DeviceUri parsed_uri(device_uri);
+  return HalDriver::Create(parsed_uri);
+}
+
+py::object HalDriver::Create(const std::string& device_uri,
+                             py::dict& driver_cache) {
+  // Look up the driver by driver name in the cache, and return it if found.
+  DeviceUri parsed_uri(device_uri);
+  py::str cache_key(parsed_uri.driver_name.data, parsed_uri.driver_name.size);
   py::object cached = driver_cache.attr("get")(cache_key);
   if (!cached.is_none()) {
     return cached;
   }
 
-  // Create.
-  iree_hal_driver_t* driver;
-  CheckApiStatus(iree_hal_driver_registry_try_create(
-                     iree_hal_driver_registry_default(), driver_name,
-                     iree_allocator_system(), &driver),
-                 "Error creating driver");
-
-  // Cache.
-  py::object driver_obj = py::cast(HalDriver::StealFromRawPtr(driver));
+  // Create a new driver and put it in the cache.
+  py::object driver_obj = HalDriver::Create(parsed_uri);
   driver_cache[cache_key] = driver_obj;
   return driver_obj;
 }
@@ -1026,7 +1037,8 @@ HalDevice HalDriver::CreateDevice(iree_hal_device_id_t device_id,
   std::vector<iree_string_pair_t> params;
   iree_hal_device_t* device;
   CheckApiStatus(iree_hal_driver_create_device_by_id(
-                     raw_ptr(), device_id, params.size(), &params.front(),
+                     raw_ptr(), device_id, params.size(),
+                     (params.empty() ? nullptr : &params.front()),
                      iree_allocator_system(), &device),
                  "Error creating default device");
   CheckApiStatus(ConfigureDevice(device, allocators),
@@ -1288,6 +1300,14 @@ void SetupHalBindings(nanobind::module_ m) {
                                  const_cast<py::dict&>(driver_cache));
       },
       py::arg("device_uri"));
+
+  m.def(
+      "create_hal_driver",
+      [](std::string device_uri) { return HalDriver::Create(device_uri); },
+      py::arg("device_uri"));
+
+  m.def("clear_hal_driver_cache",
+        [driver_cache]() { const_cast<py::dict&>(driver_cache).clear(); });
 
   py::class_<HalAllocator>(m, "HalAllocator")
       .def("trim",
