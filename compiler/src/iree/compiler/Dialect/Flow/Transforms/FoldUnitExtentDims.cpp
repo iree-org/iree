3c40c6bdfd5343a1e665b2b95b8d2b71b8eaea7d
Ian Wood <ianwood2024@u.northwestern.edu>
2024-07-01 17:14:32 +0000

    manually revert 7884dc8 to test regressions
    Signed-off-by: Ian Wood <ianwood2024@u.northwestern.edu>

--- compiler/src/iree/compiler/Dialect/Flow/Transforms/FoldUnitExtentDims.cpp
+++ compiler/src/iree/compiler/Dialect/Flow/Transforms/FoldUnitExtentDims.cpp
@@ -13,6 +13,7 @@
 
 #include "iree/compiler/Dialect/Flow/Transforms/Passes.h"
 #include "iree/compiler/Dialect/Flow/Transforms/RegionOpUtils.h"
+#include "iree/compiler/Dialect/Util/Analysis/Explorer.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
@@ -26,6 +27,71 @@ namespace mlir::iree_compiler::IREE::Flow {
 #define GEN_PASS_DEF_FOLDUNITEXTENTDIMSPASS
 #include "iree/compiler/Dialect/Flow/Transforms/Passes.h.inc"
 
+//===----------------------------------------------------------------------===//
+// Pass helpers
+//===----------------------------------------------------------------------===//
+
+static LogicalResult
+foldUnitDimsOnGlobal(IRRewriter &rewriter, IREE::Util::GlobalOpInterface global,
+                     SmallVector<IREE::Util::GlobalLoadOpInterface> loadOps,
+                     SmallVector<IREE::Util::GlobalStoreOpInterface> storeOps,
+                     SymbolTable moduleSymbols) {
+  // Create a new transformed GlobalOp.
+  SmallVector<int64_t> newShape;
+  auto globalType = cast<RankedTensorType>(global.getGlobalType());
+  for (auto size : globalType.getShape()) {
+    if (size != 1) {
+      newShape.push_back(size);
+    }
+  }
+  auto newGlobalType = globalType.clone(newShape);
+  auto initialValue = global.getGlobalInitialValue();
+  // TODO: Handle non-uninitialized cases.
+  auto uninitializedAttr =
+      llvm::dyn_cast_if_present<IREE::Util::UninitializedAttr>(initialValue);
+  if (initialValue && !uninitializedAttr)
+    return success();
+  TypedAttr newInitialValue;
+  if (initialValue) {
+    newInitialValue = IREE::Util::UninitializedAttr::get(rewriter.getContext(),
+                                                         newGlobalType);
+  }
+  OpBuilder::InsertionGuard guard(rewriter);
+  rewriter.setInsertionPoint(global);
+  auto newGlobal =
+      clone(rewriter, global, global->getResultTypes(), global->getOperands());
+  newGlobal.setGlobalType(newGlobalType);
+  newGlobal.setGlobalInitialValue(newInitialValue);
+
+  // Rewrite loads and stores to use the new global.
+  auto expandShapeReInds =
+      getReassociationIndicesForReshape(globalType, newGlobalType);
+  if (!expandShapeReInds) {
+    return failure();
+  }
+
+  for (auto load : loadOps) {
+    rewriter.setInsertionPoint(load);
+    auto newLoad = clone(rewriter, load, {newGlobalType}, load->getOperands());
+    newLoad.setGlobalAttr(FlatSymbolRefAttr::get(newGlobal.getGlobalName()));
+    rewriter.replaceOpWithNewOp<tensor::ExpandShapeOp>(
+        load, globalType, newLoad->getResult(0), expandShapeReInds.value());
+  }
+  for (auto store : storeOps) {
+    rewriter.setInsertionPoint(store);
+    Value collapse = rewriter.create<tensor::CollapseShapeOp>(
+        store.getLoc(), newGlobalType, store->getOperand(0),
+        expandShapeReInds.value());
+    auto newStore =
+        clone(rewriter, store, store->getResultTypes(), store->getOperands());
+    newStore.setGlobalAttr(FlatSymbolRefAttr::get(newGlobal.getGlobalName()));
+    newStore.setStoredGlobalValue(collapse);
+    rewriter.eraseOp(store);
+  }
+  rewriter.eraseOp(global);
+  return success();
+}
+
 namespace {
 struct FoldUnitExtentDimsPass
     : public IREE::Flow::impl::FoldUnitExtentDimsPassBase<
@@ -35,8 +101,35 @@ struct FoldUnitExtentDimsPass
 } // namespace
 
 void FoldUnitExtentDimsPass::runOnOperation() {
-  Operation *funcOp = getOperation();
+  auto funcOp = getOperation();
   MLIRContext *context = &getContext();
+
+  Explorer explorer(funcOp, TraversalAction::RECURSE);
+  explorer.initialize();
+  IRRewriter rewriter(context);
+  SymbolTable moduleSymbols(funcOp);
+
+  // Fold unit dims of GlobalOpInterface ops.
+  explorer.forEachGlobal([&](const Explorer::GlobalInfo *globalInfo) {
+    IREE::Util::GlobalOpInterface global = globalInfo->op;
+    auto tensorType = dyn_cast<RankedTensorType>(global.getGlobalType());
+    if (!tensorType || !global.isGlobalPrivate() || !global.isGlobalMutable()) {
+      return;
+    }
+    if (llvm::none_of(tensorType.getShape(),
+                      [](int64_t size) { return size == 1; })) {
+      return;
+    }
+    SmallVector<IREE::Util::GlobalLoadOpInterface> loadOps =
+        llvm::to_vector(globalInfo->getLoads());
+    SmallVector<IREE::Util::GlobalStoreOpInterface> storeOps =
+        llvm::to_vector(globalInfo->getStores());
+    if (failed(foldUnitDimsOnGlobal(rewriter, global, loadOps, storeOps,
+                                    moduleSymbols))) {
+      return signalPassFailure();
+    }
+  });
+
   RewritePatternSet foldUnitDimsPatterns(context);
   linalg::ControlDropUnitDims options;
   auto defaultFn = options.controlFn;
