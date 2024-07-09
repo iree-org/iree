f0573fbba7a6f8f097afbc4f812874cfc48be5de
hanhanW <hanhan0912@gmail.com>
2024-07-09 09:56:11 -0700

    fixes for upstream data-layout propagation changes.
    Signed-off-by: hanhanW <hanhan0912@gmail.com>

--- compiler/src/iree/compiler/Codegen/Dialect/GPU/Transforms/PackToIntrinsics.cpp
+++ compiler/src/iree/compiler/Codegen/Dialect/GPU/Transforms/PackToIntrinsics.cpp
@@ -98,7 +98,9 @@ void PackToIntrinsicsPass::runOnOperation() {
   // Run layout propagation patterns to pull in adjacent un-configured ops.
   RewritePatternSet patterns(context);
   linalg::ControlPropagationFn control = [](OpOperand *opOperand) -> bool {
-    return !getLoweringConfig(opOperand->getOwner());
+    Operation *producer = opOperand->get().getDefiningOp();
+    Operation *consumer = opOperand->getOwner();
+    return !getLoweringConfig(producer) || !getLoweringConfig(consumer);
   };
 
   linalg::populateDataLayoutPropagationPatterns(patterns, control);
--- compiler/src/iree/compiler/GlobalOptimization/DataLayoutPropagation.cpp
+++ compiler/src/iree/compiler/GlobalOptimization/DataLayoutPropagation.cpp
@@ -25,12 +25,15 @@ struct DataLayoutPropagationPass
     RewritePatternSet patterns(context);
     linalg::populateDataLayoutPropagationPatterns(
         patterns, [](OpOperand *opOperand) {
+          Operation *producer = opOperand->get().getDefiningOp();
+          Operation *consumer = opOperand->getOwner();
+          (void)consumer;
           // Currently only bubble up/push down pack/unpack through
           // collapse/expand shape ops.
           // TODO(#17734): The propagation through expand_shape ops is broken.
           // Enable the propagation once we find it useful and the upstream
           // issue is fixed.
-          return isa<tensor::CollapseShapeOp>(opOperand->get().getDefiningOp());
+          return isa<tensor::CollapseShapeOp>(producer);
         });
     if (failed(applyPatternsAndFoldGreedily(funcOp, std::move(patterns)))) {
       funcOp.emitOpError("folding patterns failed");
