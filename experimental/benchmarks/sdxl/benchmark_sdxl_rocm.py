c37461c8fa081c09976d4f2d58a93f41450c725c
saienduri <saimanas.enduri@amd.com>
2024-07-12 05:45:36 -0700

    try expanding user in artifacts dir path for benchmarking
    Signed-off-by: saienduri <saimanas.enduri@amd.com>

--- experimental/benchmarks/sdxl/benchmark_sdxl_rocm.py
+++ experimental/benchmarks/sdxl/benchmark_sdxl_rocm.py
@@ -16,6 +16,7 @@ from pytest_check import check
 
 benchmark_dir = os.path.dirname(os.path.realpath(__file__))
 artifacts_dir = os.getenv("IREE_TEST_FILES", default=Path.cwd()) + "/artifacts"
+artifacts_dir = Path(os.path.expanduser(artifacts_dir)).resolve()
 prompt_encoder_dir = f"{artifacts_dir}/sdxl_clip"
 scheduled_unet_dir = f"{artifacts_dir}/sdxl_unet"
 vae_decode_dir = f"{artifacts_dir}/sdxl_vae"
