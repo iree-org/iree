12e252052e30d4cb2909edf45cbdf5fc1887e06d
saienduri <saimanas.enduri@amd.com>
2024-07-12 11:52:04 -0700

    license header for file
    Signed-off-by: saienduri <saimanas.enduri@amd.com>

--- experimental/regression_suite/shark-test-suite-models/conftest.py
+++ experimental/regression_suite/shark-test-suite-models/conftest.py
@@ -1,3 +1,9 @@
+# Copyright 2024 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 class VmfbManager:
     sdxl_clip_cpu_vmfb = None
     sdxl_vae_cpu_vmfb = None
