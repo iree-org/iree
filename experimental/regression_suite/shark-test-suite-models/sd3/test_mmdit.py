44808e143533e694644da13b75a8f8358ee289cf
saienduri <77521230+saienduri@users.noreply.github.com>
2024-07-12 15:51:47 -0700

    Add in-tree special_models test suite using reworked iree-tooling. (#17883)
    With this, we move away from using all the specialized json config files
    and complex workflows.
    Instead, we use python scripts which allow us to use custom flags,
    tolerances, and configurations based on the backend/model.
    Related PR in TestSuite:
    https://github.com/nod-ai/SHARK-TestSuite/pull/271
    
    This PR also removes all dependencies on SHARK-TestSuite tooling.
    Reworked the tools here so that downloading, caching, testing, and
    benchmarking occurs as intended with tools solely from this repo for
    iree_special_models. Whenever we are adding test files here, the goal is
    for an IREE user to be able to clone the repo and run the run tests
    knowing nothing about the SHARK-TestSuite .
    
    Also didn't realize, but ireers here already has a process of stamping
    here to check if a file is already produced. I think we have to remove
    this because it will skip even if there is a newer version of the file
    available and there's really no point when downloading to a cache
    because once it's there, it is never removed so not a valuable signal.
    
    (Third times the charm. Had to close the last two versions of this PR
    because couldn't get passed a pre-commit check that led me to rebase and
    add a bunch of commits that weren't mine 🤦 )
    
    ci-exactly: build_all, test_amd_mi300, build_packages, regression_test
    
    ---------
    
    Signed-off-by: saienduri <saimanas.enduri@amd.com>

---
+++ experimental/regression_suite/shark-test-suite-models/sd3/test_mmdit.py
@@ -0,0 +1,152 @@
+# Copyright 2024 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import pytest
+from ireers_tools import *
+import os
+from pathlib import Path
+from conftest import VmfbManager
+
+iree_test_path_extension = os.getenv("IREE_TEST_PATH_EXTENSION", default=Path.cwd())
+rocm_chip = os.getenv("ROCM_CHIP", default="gfx90a")
+
+###############################################################################
+# Fixtures
+###############################################################################
+
+sd3_mmdit_inference_input_0 = fetch_source_fixture(
+    "https://sharkpublic.blob.core.windows.net/sharkpublic/sai/sd3-mmdit/inference_input.0.bin",
+    group="sd3_mmdit",
+)
+
+sd3_mmdit_inference_input_1 = fetch_source_fixture(
+    "https://sharkpublic.blob.core.windows.net/sharkpublic/sai/sd3-mmdit/inference_input.1.bin",
+    group="sd3_mmdit",
+)
+
+sd3_mmdit_inference_input_2 = fetch_source_fixture(
+    "https://sharkpublic.blob.core.windows.net/sharkpublic/sai/sd3-mmdit/inference_input.2.bin",
+    group="sd3_mmdit",
+)
+
+sd3_mmdit_inference_input_3 = fetch_source_fixture(
+    "https://sharkpublic.blob.core.windows.net/sharkpublic/sai/sd3-mmdit/inference_input.3.bin",
+    group="sd3_mmdit",
+)
+
+sd3_mmdit_inference_output_0 = fetch_source_fixture(
+    "https://sharkpublic.blob.core.windows.net/sharkpublic/sai/sd3-mmdit/inference_output.0.bin",
+    group="sd3_mmdit",
+)
+
+sd3_mmdit_real_weights = fetch_source_fixture(
+    "https://sharkpublic.blob.core.windows.net/sharkpublic/sai/sd3-mmdit/real_weights.irpa",
+    group="sd3_mmdit",
+)
+
+sd3_mmdit_mlir = fetch_source_fixture(
+    "https://sharkpublic.blob.core.windows.net/sharkpublic/sai/sd3-mmdit/model.mlirbc",
+    group="sd3_mmdit",
+)
+
+CPU_COMPILE_FLAGS = [
+    "--iree-hal-target-backends=llvm-cpu",
+    "--iree-llvmcpu-target-cpu-features=host",
+    "--iree-llvmcpu-fail-on-out-of-bounds-stack-allocation=false",
+    "--iree-llvmcpu-distribution-size=32",
+    "--iree-opt-const-eval=false",
+    "--iree-llvmcpu-enable-ukernels=all",
+    "--iree-global-opt-enable-quantized-matmul-reassociation",
+]
+
+
+@pytest.fixture
+def SD3_MMDIT_COMMON_RUN_FLAGS(
+    sd3_mmdit_inference_input_0,
+    sd3_mmdit_inference_input_1,
+    sd3_mmdit_inference_input_2,
+    sd3_mmdit_inference_input_3,
+    sd3_mmdit_inference_output_0,
+):
+    return [
+        f"--input=2x16x128x128xf16=@{sd3_mmdit_inference_input_0.path}",
+        f"--input=2x154x4096xf16=@{sd3_mmdit_inference_input_1.path}",
+        f"--input=2x2048xf16=@{sd3_mmdit_inference_input_2.path}",
+        f"--input=2xf16=@{sd3_mmdit_inference_input_3.path}",
+        f"--expected_output=2x16x128x128xf32=@{sd3_mmdit_inference_output_0.path}",
+    ]
+
+
+ROCM_COMPILE_FLAGS = [
+    "--iree-hal-target-backends=rocm",
+    f"--iree-rocm-target-chip={rocm_chip}",
+    "--iree-opt-const-eval=false",
+    f"--iree-codegen-transform-dialect-library={iree_test_path_extension}/attention_and_matmul_spec.mlir",
+    "--iree-global-opt-propagate-transposes=true",
+    "--iree-global-opt-enable-fuse-horizontal-contractions=true",
+    "--iree-flow-enable-aggressive-fusion=true",
+    "--iree-opt-aggressively-propagate-transposes=true",
+    "--iree-opt-outer-dim-concat=true",
+    "--iree-vm-target-truncate-unsupported-floats",
+    "--iree-llvmgpu-enable-prefetch=true",
+    "--iree-opt-data-tiling=false",
+    "--iree-codegen-gpu-native-math-precision=true",
+    "--iree-codegen-llvmgpu-use-vector-distribution",
+    "--iree-rocm-waves-per-eu=2",
+    "--iree-execution-model=async-external",
+    "--iree-preprocessing-pass-pipeline=builtin.module(iree-preprocessing-transpose-convolution-pipeline, util.func(iree-preprocessing-pad-to-intrinsics))",
+]
+
+###############################################################################
+# CPU
+###############################################################################
+
+
+def test_compile_mmdit_cpu(sd3_mmdit_mlir):
+    VmfbManager.sd3_mmdit_cpu_vmfb = iree_compile(
+        sd3_mmdit_mlir, "cpu", CPU_COMPILE_FLAGS
+    )
+
+
+@pytest.mark.xfail(
+    strict=True,
+    reason="Expected run to fail",
+)
+@pytest.mark.depends(on=["test_compile_mmdit_cpu"])
+def test_run_mmdit_cpu(SD3_MMDIT_COMMON_RUN_FLAGS, sd3_mmdit_real_weights):
+    return iree_run_module(
+        VmfbManager.sd3_mmdit_cpu_vmfb,
+        device="local-task",
+        function="run_forward",
+        args=[f"--parameters=model={sd3_mmdit_real_weights.path}"]
+        + SD3_MMDIT_COMMON_RUN_FLAGS,
+    )
+
+
+###############################################################################
+# ROCM
+###############################################################################
+
+
+@pytest.mark.xfail(
+    strict=True,
+    reason="Expected compilation to fail",
+)
+def test_compile_mmdit_rocm(sd3_mmdit_mlir):
+    VmfbManager.sd3_mmdit_rocm_vmfb = iree_compile(
+        sd3_mmdit_mlir, f"rocm_{rocm_chip}", ROCM_COMPILE_FLAGS
+    )
+
+
+@pytest.mark.depends(on=["test_compile_mmdit_rocm"])
+def test_run_mmdit_rocm(SD3_MMDIT_COMMON_RUN_FLAGS, sd3_mmdit_real_weights):
+    return iree_run_module(
+        VmfbManager.sd3_mmdit_rocm_vmfb,
+        device="hip",
+        function="run_forward",
+        args=[f"--parameters=model={sd3_mmdit_real_weights.path}"]
+        + SD3_MMDIT_COMMON_RUN_FLAGS,
+    )
---
+++ experimental/regression_suite/shark-test-suite-models/sd3/test_vae.py
@@ -0,0 +1,119 @@
+# Copyright 2024 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import pytest
+from ireers_tools import *
+import os
+from conftest import VmfbManager
+
+rocm_chip = os.getenv("ROCM_CHIP", default="gfx90a")
+
+###############################################################################
+# Fixtures
+###############################################################################
+
+sd3_vae_inference_input_0 = fetch_source_fixture(
+    "https://sharkpublic.blob.core.windows.net/sharkpublic/sai/sd3-vae/inference_input.0.bin",
+    group="sd3_vae",
+)
+
+sd3_vae_inference_output_0 = fetch_source_fixture(
+    "https://sharkpublic.blob.core.windows.net/sharkpublic/sai/sd3-vae/inference_output.0.bin",
+    group="sd3_vae",
+)
+
+sd3_vae_real_weights = fetch_source_fixture(
+    "https://sharkpublic.blob.core.windows.net/sharkpublic/sai/sd3-vae/real_weights.irpa",
+    group="sd3_vae",
+)
+
+sd3_vae_mlir = fetch_source_fixture(
+    "https://sharkpublic.blob.core.windows.net/sharkpublic/sai/sd3-vae/model.mlirbc",
+    group="sd3_vae",
+)
+
+CPU_COMPILE_FLAGS = [
+    "--iree-hal-target-backends=llvm-cpu",
+    "--iree-llvmcpu-target-cpu-features=host",
+    "--iree-llvmcpu-fail-on-out-of-bounds-stack-allocation=false",
+    "--iree-llvmcpu-distribution-size=32",
+    "--iree-opt-const-eval=false",
+    "--iree-llvmcpu-enable-ukernels=all",
+    "--iree-global-opt-enable-quantized-matmul-reassociation",
+]
+
+
+@pytest.fixture
+def SD3_VAE_COMMON_RUN_FLAGS(
+    sd3_vae_inference_input_0,
+    sd3_vae_inference_output_0,
+):
+    return [
+        f"--input=1x16x128x128xf16=@{sd3_vae_inference_input_0.path}",
+        f"--expected_output=3x1024x1024xf32=@{sd3_vae_inference_output_0.path}",
+    ]
+
+
+ROCM_COMPILE_FLAGS = [
+    "--iree-hal-target-backends=rocm",
+    f"--iree-rocm-target-chip={rocm_chip}",
+    "--iree-opt-const-eval=false",
+    "--iree-global-opt-propagate-transposes=true",
+    "--iree-opt-outer-dim-concat=true",
+    "--iree-llvmgpu-enable-prefetch=true",
+    "--iree-rocm-waves-per-eu=2",
+    "--iree-flow-enable-aggressive-fusion=true",
+    "--iree-codegen-llvmgpu-use-vector-distribution=true",
+    "--iree-execution-model=async-external",
+    "--iree-preprocessing-pass-pipeline=builtin.module(iree-preprocessing-transpose-convolution-pipeline, util.func(iree-preprocessing-pad-to-intrinsics))",
+]
+
+###############################################################################
+# CPU
+###############################################################################
+
+
+def test_compile_vae_cpu(sd3_vae_mlir):
+    VmfbManager.sd3_vae_cpu_vmfb = iree_compile(sd3_vae_mlir, "cpu", CPU_COMPILE_FLAGS)
+
+
+@pytest.mark.depends(on=["test_compile_vae_cpu"])
+def test_run_vae_cpu(SD3_VAE_COMMON_RUN_FLAGS, sd3_vae_real_weights):
+    return iree_run_module(
+        VmfbManager.sd3_vae_cpu_vmfb,
+        device="local-task",
+        function="decode",
+        args=[
+            f"--parameters=model={sd3_vae_real_weights.path}",
+            "--expected_f32_threshold=0.01f",
+        ]
+        + SD3_VAE_COMMON_RUN_FLAGS,
+    )
+
+
+###############################################################################
+# ROCM
+###############################################################################
+
+
+def test_compile_vae_rocm(sd3_vae_mlir):
+    VmfbManager.sd3_vae_rocm_vmfb = iree_compile(
+        sd3_vae_mlir, f"rocm_{rocm_chip}", ROCM_COMPILE_FLAGS
+    )
+
+
+@pytest.mark.depends(on=["test_compile_vae_rocm"])
+def test_run_vae_rocm(SD3_VAE_COMMON_RUN_FLAGS, sd3_vae_real_weights):
+    return iree_run_module(
+        VmfbManager.sd3_vae_rocm_vmfb,
+        device="hip",
+        function="decode",
+        args=[
+            f"--parameters=model={sd3_vae_real_weights.path}",
+            "--expected_f32_threshold=0.7f",
+        ]
+        + SD3_VAE_COMMON_RUN_FLAGS,
+    )
