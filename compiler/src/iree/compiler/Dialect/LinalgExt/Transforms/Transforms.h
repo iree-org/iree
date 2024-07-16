74346d06ef8e610e7450b46e982a975d61f29ea4
Ian Wood <ianwood2024@u.northwestern.edu>
2024-07-16 16:08:11 +0000

    Bubble expand_shape through AttentionOp
    This depends on https://github.com/iree-org/iree/pull/18030, so ignore
    the first 2 commits. Only the most recent commit should be considered
    for this pr. This PR addresses
    https://github.com/iree-org/iree/issues/17673.
    
    - Create LinalgExt::populateFoldReshapeOpsByExpansionPattern that
      emulates linalg::populateFoldReshapeOpsByExpansionPattern but for
    `AttentionOp` instead of linalg ops.
    
    Signed-off-by: Ian Wood <ianwood2024@u.northwestern.edu>
    Signed-off-by: saienduri <saimanas.enduri@amd.com>

---
+++ compiler/src/iree/compiler/Dialect/LinalgExt/Transforms/Transforms.h
@@ -0,0 +1,16 @@
+// Copyright 2024 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "mlir/Dialect/Linalg/Transforms/Transforms.h"
+
+namespace mlir::iree_compiler::IREE::LinalgExt {
+
+// Fold expand_shape ops with their producers (only `AttentionOp` supported)
+void populateFoldReshapeOpsByExpansionPatterns(
+    RewritePatternSet &patterns,
+    const linalg::ControlFusionFn &controlFoldingReshapes);
+
+}; // namespace mlir::iree_compiler::IREE::LinalgExt
