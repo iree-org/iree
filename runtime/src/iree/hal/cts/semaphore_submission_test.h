eac72df83ee3fbf1711457597c54cd995765b876
Boian Petkantchin <boian.petkantchin@amd.com>
2024-07-19 12:20:42 -0500

    [runtime][hip] Propagate errors through semaphores
    Adds proper handling of errors that occur when executing operations on the
    device or when a semaphore fails in the wait list.
    These errors will propagate to downstream semaphores that are in the
    signal list of the operation.
    
    This change includes some refactor the pending queue actions:
    * Make the context hold a sticky status instead of just an status code.
    * Remove the worker threads' "error" state. This can be handled by the
      context's status.
    * Remove "exit committed" thread state in favor of standard thread joining.
    * Make the "exit requested" thread state a separate boolean variable and
      guard against submitting more work after an exit is requested.
      Also wait on all work to complete before exiting worker threads, not just
      on the currently ran actions.
    * Make pending work items increment immediately when an action is
      enqueued instead of when scheduled on the HIP stream. This is required
      to properly count outstanding work.
    * Remove and merge some of the redundant state for the worker and
      completion threads.
    * Remove reference counting from the pending queue actions context. It
      has a clear owner, which is the device.
    * Rework when the threads exit, which is pretty much only when exit is
      requested and there is no more queued or executing actions. Errors
      don't cause the threads to exit.
    
    Here is not included moving the destruction and cleanup of actions from
    the worker thread to the completion thread. This is an optimization and
    code simplification that is now possible since we are not using HIP
    stream callbacks, so we could do that right after an action completes.
    Technically, actions get destroyed on the completion thread as well when
    not on the happy path and actions fail.

--- runtime/src/iree/hal/cts/semaphore_submission_test.h
+++ runtime/src/iree/hal/cts/semaphore_submission_test.h
@@ -841,6 +841,78 @@ TEST_F(SemaphoreSubmissionTest, BatchWaitingOnSmallerValueBeforeSignaled) {
   iree_hal_command_buffer_release(command_buffer);
 }
 
+// Submit an batch and check that the wait semaphore fails when the signal
+// semaphore fails.
+TEST_F(SemaphoreSubmissionTest, PropagateFailSignal) {
+  // signal-wait relation:
+  //
+  //     semaphore1
+  //         ↓
+  //  command_buffer
+  //         ↓
+  //     semaphore2
+
+  iree_hal_command_buffer_t* command_buffer = CreateEmptyCommandBuffer();
+  iree_hal_semaphore_t* semaphore1 = CreateSemaphore();
+  iree_hal_semaphore_t* semaphore2 = CreateSemaphore();
+
+  // Submit the command buffer.
+  uint64_t semaphore1_wait_value = 1;
+  iree_hal_semaphore_list_t command_buffer_wait_list = {
+      /*count=*/1, &semaphore1, &semaphore1_wait_value};
+  uint64_t semaphore2_signal_value = 1;
+  iree_hal_semaphore_list_t command_buffer_signal_list = {
+      /*count=*/1, &semaphore2, &semaphore2_signal_value};
+  IREE_ASSERT_OK(iree_hal_device_queue_execute(
+      device_, IREE_HAL_QUEUE_AFFINITY_ANY,
+      /*wait_semaphore_list=*/command_buffer_wait_list,
+      /*signal_semaphore_list=*/command_buffer_signal_list, 1, &command_buffer,
+      /*binding_tables=*/NULL));
+
+  iree_status_t status =
+      iree_make_status(IREE_STATUS_CANCELLED, "PropagateFailSignal test.");
+  std::thread signal_thread([&]() {
+    iree_hal_semaphore_fail(semaphore1, iree_status_clone(status));
+  });
+
+  iree_status_t wait_status =
+      iree_hal_semaphore_wait(semaphore2, semaphore2_signal_value,
+                              iree_make_deadline(IREE_TIME_INFINITE_FUTURE));
+  EXPECT_EQ(iree_status_code(wait_status), IREE_STATUS_ABORTED);
+  uint64_t value = 1234;
+  iree_status_t query_status = iree_hal_semaphore_query(semaphore2, &value);
+  EXPECT_EQ(value, IREE_HAL_SEMAPHORE_FAILURE_VALUE);
+  CheckStatusContains(query_status, status);
+
+  signal_thread.join();
+  iree_hal_semaphore_release(semaphore1);
+  iree_hal_semaphore_release(semaphore2);
+  iree_hal_command_buffer_release(command_buffer);
+  iree_status_ignore(status);
+  iree_status_ignore(wait_status);
+  iree_status_ignore(query_status);
+}
+
+// Submit an invalid dispatch and check that the wait semaphore fails.
+TEST_F(SemaphoreSubmissionTest, PropagateDispatchFailure) {
+  // signal-wait relation:
+  //
+  //     semaphore1
+  //         ↓
+  //  command_buffer
+  //         ↓
+  //     semaphore2
+
+  // TODO (sogartar):
+  // I tried to add a kernel that stores into a null pointer or
+  // traps(aborts), but with HIP that causes the whole executable to abort,
+  // which is not what we want.
+  // We want a failure of the kernel launch or when waiting on the stream for
+  // the kernel to complete.
+  // This needs to be "soft" failure that result in a returned error from the
+  // underlying API call.
+}
+
 }  // namespace iree::hal::cts
 
 #endif  // IREE_HAL_CTS_SEMAPHORE_SUBMISSION_TEST_H_
