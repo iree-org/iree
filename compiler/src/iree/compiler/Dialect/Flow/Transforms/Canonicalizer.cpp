1c0c5a6ff64bedb1cb1275eaba16aab3fc26acdf
Nirvedh Meshram <96096277+nirvedhmeshram@users.noreply.github.com>
2024-08-21 16:57:53 -0500

    [Flow] Convert from tensor.cast to flow.tensor.reshape early (#18256)
    The reason to do this is that tensor.cast's can end up in dispatch
    regions and break the logic there. If they are converted to
    flow.tensor.reshape before dispatch formation then the correct thing
    happens and the resulting flow.tensor.reshape ops are left out of the
    dispatch.regions.
    Fixes : https://github.com/iree-org/iree/issues/18229

--- compiler/src/iree/compiler/Dialect/Flow/Transforms/Canonicalizer.cpp
+++ compiler/src/iree/compiler/Dialect/Flow/Transforms/Canonicalizer.cpp
@@ -4,6 +4,7 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include "iree/compiler/Dialect/Flow/Conversion/TensorToFlow/Patterns.h"
 #include "iree/compiler/Dialect/Flow/Transforms/Passes.h"
 
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
@@ -103,6 +104,11 @@ struct CanonicalizerPass
       CanonicalizerPass>::CanonicalizerPassBase;
   /// Initialize the canonicalizer by building the set of patterns used during
   /// execution.
+
+  void getDependentDialects(DialectRegistry &registry) const override {
+    registry.insert<IREE::Flow::FlowDialect>();
+  }
+
   LogicalResult initialize(MLIRContext *context) override {
     // Inherit the same config defaults from the upstream canonicalizer pass.
     config.useTopDownTraversal = true;
@@ -117,6 +123,7 @@ struct CanonicalizerPass
     // Pull in some borderline/downstream canonicalizations for the Flow
     // compilation phase.
     tensor::populateMergeConsecutiveInsertExtractSlicePatterns(owningPatterns);
+    IREE::Flow::populateTensorDialectCastOpPattern(context, owningPatterns);
     owningPatterns.add<FoldConsecutiveConstantPadding>(context);
 
     patterns =
