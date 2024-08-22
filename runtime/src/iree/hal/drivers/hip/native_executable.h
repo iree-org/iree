AaronStGeorge/hrx-system

Converting HIP target to support executable-create2. This produces a new flatbuffer that supports multiple hipModule_ts per HAL executable, reorganizes per-export information to be per-export, and removes HAL pipeline layouts and the existing stateful command recording.

runtime/src/iree/hal/drivers/hip/native_executable.h
@@ -19,20 +19,31 @@
 extern "C" {
 #endif  // __cplusplus
 
-typedef struct iree_hal_hip_kernel_info_t {
-  // TODO(#18154): remove when using simplified bindings.
-  iree_hal_pipeline_layout_t* layout;
+// The max number of per-dispatch bindings allowed in the HIP HAL
+// implementation.
+#define IREE_HAL_HIP_MAX_DISPATCH_BINDING_COUNT 16
+
+// The max number of per-dispatch constants supported by the HIP HAL
+// implementation.
+#define IREE_HAL_HIP_MAX_DISPATCH_CONSTANT_COUNT 64
+
+typedef struct iree_hal_hip_kernel_debug_info_t {
+  iree_string_view_t function_name;
+  iree_string_view_t source_filename;
+  uint32_t source_line;
+} iree_hal_hip_kernel_debug_info_t;
+
+typedef struct iree_hal_hip_kernel_params_t {
   hipFunction_t function;
+
   uint32_t constant_count;
   uint32_t binding_count;
-  // TODO(#18154): add bitfield indicating indirect bindings.
-  uint32_t block_size[3];
-  uint32_t shared_memory_size;
 
-  IREE_TRACE(iree_string_view_t function_name;)
-  IREE_TRACE(iree_string_view_t source_filename;)
-  IREE_TRACE(uint32_t source_line;)
-} iree_hal_hip_kernel_info_t;
+  uint32_t block_dims[3];
+  uint32_t block_shared_memory_size;
+
+  IREE_TRACE(iree_hal_hip_kernel_debug_info_t debug_info;)
+} iree_hal_hip_kernel_params_t;
 
 // Creates an IREE executable from a HSACO module. The module may contain
 // several kernels that can be extracted along with the associated block size.
@@ -43,9 +54,9 @@ iree_status_t iree_hal_hip_native_executable_create(
 
 // Returns the kernel launch parameters for the given |entry_point| in the
 // |executable|.
-iree_status_t iree_hal_hip_native_executable_entry_point_kernel_info(
+iree_status_t iree_hal_hip_native_executable_lookup_kernel_params(
     iree_hal_executable_t* executable, int32_t entry_point,
-    iree_hal_hip_kernel_info_t* out_info);
+    const iree_hal_hip_kernel_params_t** out_params);
 
 #ifdef __cplusplus
 }  // extern "C"

runtime/src/iree/hal/utils/executable_debug_info.c
@@ -54,6 +54,52 @@ iree_status_t iree_hal_debug_verify_export_def(
   return iree_ok_status();
 }
 
+iree_host_size_t iree_hal_debug_calculate_export_info_size(
+    iree_hal_debug_ExportDef_table_t export_def) {
+  if (!export_def) return 0;
+
+  iree_host_size_t total_size = sizeof(iree_hal_debug_export_info_t);
+  total_size +=
+      flatbuffers_string_len(iree_hal_debug_ExportDef_name_get(export_def));
+
+  iree_hal_debug_FileLineLocDef_table_t location_def =
+      iree_hal_debug_ExportDef_location_get(export_def);
+  if (location_def) {
+    total_size += flatbuffers_string_len(
+        iree_hal_debug_FileLineLocDef_filename_get(location_def));
+  }
+
+  return total_size;
+}
+
+void iree_hal_debug_copy_export_info(
+    iree_hal_debug_ExportDef_table_t export_def,
+    iree_hal_debug_export_info_t* out_info) {
+  memset(out_info, 0, sizeof(*out_info));
+  if (!export_def) return;
+
+  char* ptr = (char*)out_info + sizeof(*out_info);
+
+  flatbuffers_string_t name = iree_hal_debug_ExportDef_name_get(export_def);
+  if (name) {
+    size_t name_length = flatbuffers_string_len(name);
+    memcpy(ptr, name, name_length);
+    out_info->function_name = iree_make_string_view(ptr, name_length);
+    ptr += name_length;
+  }
+
+  iree_hal_debug_FileLineLocDef_table_t location_def =
+      iree_hal_debug_ExportDef_location_get(export_def);
+  if (location_def) {
+    flatbuffers_string_t filename =
+        iree_hal_debug_FileLineLocDef_filename_get(location_def);
+    size_t filename_length = flatbuffers_string_len(filename);
+    memcpy(ptr, filename, filename_length);
+    out_info->source_filename = iree_make_string_view(ptr, filename_length);
+    ptr += filename_length;
+  }
+}
+
 void iree_hal_debug_publish_source_files(
     iree_hal_debug_SourceFileDef_vec_t source_files_vec) {
   if (!source_files_vec) return;

runtime/src/iree/hal/utils/executable_debug_info.h
@@ -23,6 +23,27 @@ extern "C" {
 iree_status_t iree_hal_debug_verify_export_def(
     iree_hal_debug_ExportDef_table_t export_def);
 
+// Basic debug information referencing allocated host memory.
+typedef struct iree_hal_debug_export_info_t {
+  iree_string_view_t function_name;
+  iree_string_view_t source_filename;
+  uint32_t source_line;
+} iree_hal_debug_export_info_t;
+
+// Returns the size in bytes required to store a copy of the export debug info.
+// Callers should allocate this amount of memory to populate with
+// iree_hal_debug_copy_export_info.
+iree_host_size_t iree_hal_debug_calculate_export_info_size(
+    iree_hal_debug_ExportDef_table_t export_def);
+
+// Clones the given export flatbuffer data into a heap structure allocated with
+// at least the size as calculated by iree_hal_debug_calculate_export_info_size.
+// The storage is valid until freed by the caller and decoupled from the
+// Flatbuffer storage.
+void iree_hal_debug_copy_export_info(
+    iree_hal_debug_ExportDef_table_t export_def,
+    iree_hal_debug_export_info_t* out_info);
+
 // Publishes the given source files to any attached debug/trace providers.
 // This must be called prior to emitting any debug/trace events that reference
 // the files that are contained within.

iree-org/iree

runtime/src/iree/hal/drivers/hip/native_executable.h
@@ -19,20 +19,31 @@
 extern "C" {
 #endif  // __cplusplus
 
-typedef struct iree_hal_hip_kernel_info_t {
-  // TODO(#18154): remove when using simplified bindings.
-  iree_hal_pipeline_layout_t* layout;
+// The max number of per-dispatch bindings allowed in the HIP HAL
+// implementation.
+#define IREE_HAL_HIP_MAX_DISPATCH_BINDING_COUNT 16
+
+// The max number of per-dispatch constants supported by the HIP HAL
+// implementation.
+#define IREE_HAL_HIP_MAX_DISPATCH_CONSTANT_COUNT 64
+
+typedef struct iree_hal_hip_kernel_debug_info_t {
+  iree_string_view_t name;
+  iree_string_view_t source_filename;
+  uint32_t source_line;
+} iree_hal_hip_kernel_debug_info_t;
+
+typedef struct iree_hal_hip_kernel_params_t {
   hipFunction_t function;
+
   uint32_t constant_count;
   uint32_t binding_count;
-  // TODO(#18154): add bitfield indicating indirect bindings.
-  uint32_t block_size[3];
-  uint32_t shared_memory_size;
 
-  IREE_TRACE(iree_string_view_t function_name;)
-  IREE_TRACE(iree_string_view_t source_filename;)
-  IREE_TRACE(uint32_t source_line;)
-} iree_hal_hip_kernel_info_t;
+  uint32_t block_dims[3];
+  uint32_t block_shared_memory_size;
+
+  IREE_TRACE(iree_hal_hip_kernel_debug_info_t debug_info;)
+} iree_hal_hip_kernel_params_t;
 
 // Creates an IREE executable from a HSACO module. The module may contain
 // several kernels that can be extracted along with the associated block size.
@@ -43,9 +54,9 @@ iree_status_t iree_hal_hip_native_executable_create(
 
 // Returns the kernel launch parameters for the given |entry_point| in the
 // |executable|.
-iree_status_t iree_hal_hip_native_executable_entry_point_kernel_info(
+iree_status_t iree_hal_hip_native_executable_lookup_kernel_params(
     iree_hal_executable_t* executable, int32_t entry_point,
-    iree_hal_hip_kernel_info_t* out_info);
+    const iree_hal_hip_kernel_params_t** out_params);
 
 #ifdef __cplusplus
 }  // extern "C"

runtime/src/iree/hal/utils/executable_debug_info.c
@@ -54,6 +54,52 @@ iree_status_t iree_hal_debug_verify_export_def(
   return iree_ok_status();
 }
 
+iree_host_size_t iree_hal_debug_calculate_export_info_size(
+    iree_hal_debug_ExportDef_table_t export_def) {
+  if (!export_def) return 0;
+
+  iree_host_size_t total_size = sizeof(iree_hal_debug_export_info_t);
+  total_size +=
+      flatbuffers_string_len(iree_hal_debug_ExportDef_name_get(export_def));
+
+  iree_hal_debug_FileLineLocDef_table_t location_def =
+      iree_hal_debug_ExportDef_location_get(export_def);
+  if (location_def) {
+    total_size += flatbuffers_string_len(
+        iree_hal_debug_FileLineLocDef_filename_get(location_def));
+  }
+
+  return total_size;
+}
+
+void iree_hal_debug_copy_export_info(
+    iree_hal_debug_ExportDef_table_t export_def,
+    iree_hal_debug_export_info_t* out_info) {
+  memset(out_info, 0, sizeof(*out_info));
+  if (!export_def) return;
+
+  char* ptr = (char*)out_info + sizeof(*out_info);
+
+  flatbuffers_string_t name = iree_hal_debug_ExportDef_name_get(export_def);
+  if (name) {
+    size_t name_length = flatbuffers_string_len(name);
+    memcpy(ptr, name, name_length);
+    out_info->name = iree_make_string_view(ptr, name_length);
+    ptr += name_length;
+  }
+
+  iree_hal_debug_FileLineLocDef_table_t location_def =
+      iree_hal_debug_ExportDef_location_get(export_def);
+  if (location_def) {
+    flatbuffers_string_t filename =
+        iree_hal_debug_FileLineLocDef_filename_get(location_def);
+    size_t filename_length = flatbuffers_string_len(filename);
+    memcpy(ptr, filename, filename_length);
+    out_info->source_filename = iree_make_string_view(ptr, filename_length);
+    ptr += filename_length;
+  }
+}
+
 void iree_hal_debug_publish_source_files(
     iree_hal_debug_SourceFileDef_vec_t source_files_vec) {
   if (!source_files_vec) return;

runtime/src/iree/hal/utils/executable_debug_info.h
@@ -23,6 +23,27 @@ extern "C" {
 iree_status_t iree_hal_debug_verify_export_def(
     iree_hal_debug_ExportDef_table_t export_def);
 
+// Basic debug information referencing allocated host memory.
+typedef struct iree_hal_debug_export_info_t {
+  iree_string_view_t name;
+  iree_string_view_t source_filename;
+  uint32_t source_line;
+} iree_hal_debug_export_info_t;
+
+// Returns the size in bytes required to store a copy of the export debug info.
+// Callers should allocate this amount of memory to populate with
+// iree_hal_debug_copy_export_info.
+iree_host_size_t iree_hal_debug_calculate_export_info_size(
+    iree_hal_debug_ExportDef_table_t export_def);
+
+// Clones the given export flatbuffer data into a heap structure allocated with
+// at least the size as calculated by iree_hal_debug_calculate_export_info_size.
+// The storage is valid until freed by the caller and decoupled from the
+// Flatbuffer storage.
+void iree_hal_debug_copy_export_info(
+    iree_hal_debug_ExportDef_table_t export_def,
+    iree_hal_debug_export_info_t* out_info);
+
 // Publishes the given source files to any attached debug/trace providers.
 // This must be called prior to emitting any debug/trace events that reference
 // the files that are contained within.