bc7b4bf194c6680a4b459924111bf59b63b259b8
Tim Gymnich <tgymnich@icloud.com>
2024-09-01 17:56:22 +0200

    Add more options
    Signed-off-by: Tim Gymnich <tgymnich@icloud.com>

--- compiler/bindings/python/iree/compiler/tools/import_onnx/__main__.py
+++ compiler/bindings/python/iree/compiler/tools/import_onnx/__main__.py
@@ -25,12 +25,15 @@ def main(args: argparse.Namespace):
 
     compile_saved_model(args.input_file,
                         output_file=output_file,
+                        min_opset_version=args.min_opset_version,
+                        entry_point_name=args.entry_point_name,
+                        module_name=args.module_name,
                         import_only=True,
-                        use_bytecode=False,
+                        save_temp_iree_input=args.save_temp_iree_input,
                         verify_module=not args.no_verify,
+                        use_bytecode=args.use_bytecode,
                         data_prop=args.data_prop,
                         data_dir=args.data_dir)
-
 
 def parse_arguments(argv=None) -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="IREE ONNX import tool")
@@ -54,6 +57,44 @@ def parse_arguments(argv=None) -> argparse.Namespace:
         help="Path to the base directory of the data."
         " Defaults to the directory of the input file.",
         type=Path,
+    )
+    parser.add_argument(
+        "--min-opset-version",
+        help="Minimu ONNX opset version. Model with lower opset version will be converted to this version",
+        type=int,
+        required=False,
+    )
+    parser.add_argument(
+        "--entry-point-name",
+        help="Name of the entry point for the exported graph",
+        type=str,
+        required=False,       
+    )
+    parser.add_argument(
+        "--module-name",
+        help="Name for the exported MLIR module",
+        type=str,
+        required=False,
+    )
+    parser.add_argument(
+        "--import-only",
+        help="Only import the ONNX graph do not run the iree compiler",
+        type=bool,
+        default=True,
+        required=False,
+    )
+    parser.add_argument(
+        "--save-temp-iree-input",
+        help="Save intermediate files to the given directory",
+        type=Path,
+        required=False,
+    )
+    parser.add_argument(
+        "--use-bytecode",
+        help="Use MLIR bytecode as the output format",
+        type=bool,
+        default=False,
+        required=False,
     )
     args = parser.parse_args(argv)
     return args
