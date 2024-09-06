0b70c8cc06d7905ad9f9985394a1fd75ec80a3c7
rohan-tan-bhowmik <rohan.t.bhowmik@gmail.com>
2024-09-06 14:19:29 -0700

    Final work

--- zzz.py
+++
@@ -1,9 +0,0 @@
-import torch
-import numpy as np
-
-q = (torch.rand(2,3,4,5).to(torch.float32) - 0.5) * 1
-k = (torch.rand(2,3,4,5).to(torch.float32) - 0.5) * 1
-v = (torch.rand(2,3,4,5).to(torch.float32) - 0.5) * 1
-mask = (torch.rand(2,3,4,4).to(torch.float32) - 0.5) > 0
-
-print(torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=mask))
