a6ba935a0f8caaaf0fdbeb324a3208a20d801a70
Maximilian Bartel <bartel@roofline.ai>
2024-09-11 12:35:34 +0200

    fix: make function really use the default value

--- runtime/setup.py
+++ runtime/setup.py
@@ -228,10 +228,10 @@ def maybe_nuke_cmake_cache(cmake_build_dir, cmake_install_dir):
         f.write(expected_stamp_contents)
 
 
-def get_env_cmake_option(name: str, default_value: bool = False) -> str:
+def get_env_cmake_option(name: str, default_value: str = "OFF") -> str:
     svalue = os.getenv(name)
     if not svalue:
-        svalue = "ON" if default_value else "OFF"
+        svalue = default_value
     return f"-D{name}={svalue}"
 
 
