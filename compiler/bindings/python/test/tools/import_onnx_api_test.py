aa327944903de3372bff97e273e2ded49cd8987e
Tim Gymnich <tgymnich@icloud.com>
2024-09-11 12:17:42 +0200

    format test

--- compiler/bindings/python/test/tools/import_onnx_api_test.py
+++ compiler/bindings/python/test/tools/import_onnx_api_test.py
@@ -12,7 +12,8 @@ import unittest
 from iree.compiler.tools.onnx import compile_model
 from iree.compiler.tools import OutputFormat
 
-ONNX_FILE_PATH = os.path.join(os.path.dirname(__file__), "testdata", "LeakyReLU.onnx")
+ONNX_FILE_PATH = os.path.join(os.path.dirname(
+    __file__), "testdata", "LeakyReLU.onnx")
 
 
 class ImportOnnxTest(unittest.TestCase):
@@ -23,52 +24,61 @@ class ImportOnnxTest(unittest.TestCase):
         return
 
     def testImport(self):
-        module = compile_model(self.model, import_only=True, verify_module=True, use_bytecode=False)
+        module = compile_model(self.model, import_only=True,
+                               verify_module=True, use_bytecode=False)
         self.assertIsNotNone(module)
         self.assertIn("module", module)
-    
+
     def testCompile(self):
-        module = compile_model(self.model, import_only=False, verify_module=True, use_bytecode=False, target_backends=["llvm-cpu"], output_format=OutputFormat.MLIR_TEXT)
+        module = compile_model(self.model, import_only=False, verify_module=True, use_bytecode=False, target_backends=[
+                               "llvm-cpu"], output_format=OutputFormat.MLIR_TEXT)
         self.assertIsNotNone(module)
         self.assertIn(b"module", module)
-    
+
     def testDisableVerify(self):
-        module = compile_model(self.model, import_only=True, verify_module=False, use_bytecode=False)
+        module = compile_model(self.model, import_only=True,
+                               verify_module=False, use_bytecode=False)
         self.assertIsNotNone(module)
 
     def testFileOutput(self):
         with tempfile.TemporaryDirectory() as tmpdir:
-            compile_model(self.model, output_file=tmpdir + "/" + "test.mlir", import_only=True, verify_module=True, use_bytecode=False)
+            compile_model(self.model, output_file=tmpdir + "/" + "test.mlir",
+                          import_only=True, verify_module=True, use_bytecode=False)
             with open(tmpdir + "/" + "test.mlir", "rt", encoding="utf-8") as f:
                 contents = f.read()
             self.assertIn("torch.operator", contents)
-            
+
     def testIntermediateFiles(self):
         with tempfile.TemporaryDirectory() as tmpdir:
-            compile_model(self.model, output_file=tmpdir + "/" + "test.mlir", save_temp_iree_input=tmpdir + "/" + "temp.mlir", import_only=False, verify_module=True, use_bytecode=False, target_backends=["llvm-cpu"])
+            compile_model(self.model, output_file=tmpdir + "/" + "test.mlir", save_temp_iree_input=tmpdir + "/" +
+                          "temp.mlir", import_only=False, verify_module=True, use_bytecode=False, target_backends=["llvm-cpu"])
             self.assertTrue(os.path.exists(tmpdir + "/" + "test.mlir"))
             self.assertTrue(os.path.exists(tmpdir + "/" + "temp.mlir"))
-            
+
     def testBytecode(self):
-        module = compile_model(self.model, import_only=True, verify_module=True, use_bytecode=True)
+        module = compile_model(self.model, import_only=True,
+                               verify_module=True, use_bytecode=True)
         self.assertIsNotNone(module)
-    
+
     def testEntryPointName(self):
-        module = compile_model(self.model, import_only=True, verify_module=True, use_bytecode=False, entry_point_name="testEntryPoint")
+        module = compile_model(self.model, import_only=True, verify_module=True,
+                               use_bytecode=False, entry_point_name="testEntryPoint")
         self.assertIsNotNone(module)
         self.assertIn("func.func @testEntryPoint", module)
-        
+
     def testModuleName(self):
-        module = compile_model(self.model, import_only=True, verify_module=True, use_bytecode=False, module_name="TestModule")
+        module = compile_model(self.model, import_only=True, verify_module=True,
+                               use_bytecode=False, module_name="TestModule")
         self.assertIsNotNone(module)
         self.assertIn("@TestModule", module)
-        
+
 
 if __name__ == "__main__":
     try:
         import onnx
     except ModuleNotFoundError:
-        print(f"Skipping test {__file__} because Python dependency `onnx` is not found")
+        print(
+            f"Skipping test {__file__} because Python dependency `onnx` is not found")
         sys.exit(0)
 
     unittest.main()
