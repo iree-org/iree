544d76ae8d90efbab31a483a7c383ac4ffe0f0a9
rohan-tan-bhowmik <rohan.t.bhowmik@gmail.com>
2024-09-13 10:44:29 -0700

    Fixed mlir tests

--- compiler/src/iree/compiler/Dialect/LinalgExt/Transforms/TileAttention.cpp
+++ compiler/src/iree/compiler/Dialect/LinalgExt/Transforms/TileAttention.cpp
@@ -161,6 +161,8 @@ getTileAttentionIndexingMaps(RewriterBase &rewriter, int64_t tiledInputRank,
       AffineMap::get(/*dimCount=*/4, /*symbolCount=*/0, {k2, k1}, ctx);
   AffineMap vMap =
       AffineMap::get(/*dimCount=*/4, /*symbolCount=*/0, {k2, n}, ctx);
+  AffineMap sMap = 
+      AffineMap::get(/*dimCount=*/4, /*symbolCount=*/0, {m, k2}, ctx);
   AffineMap rMap =
       AffineMap::get(/*dimCount=*/4, /*symbolCount=*/0, {m, n}, ctx);
   AffineMap maxMap =
@@ -174,7 +176,7 @@ getTileAttentionIndexingMaps(RewriterBase &rewriter, int64_t tiledInputRank,
     vMap = AffineMap::get(vMap.getNumDims(), vMap.getNumSymbols(), vDims, ctx);
   }
 
-  SmallVector<AffineMap> attentionMaps = {qMap, kMap,   vMap,
+  SmallVector<AffineMap> attentionMaps = {qMap, kMap, vMap, sMap,
                                           rMap, maxMap, sumMap};
   // Add batches to standard attention indexing maps.
   int64_t numBatches = tiledInputRank - 2;
