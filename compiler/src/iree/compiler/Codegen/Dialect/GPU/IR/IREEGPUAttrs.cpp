31d5b05672e421e8de5c88cae7eba69f50da9939
Max Dawkins <max.dawkins@gmail.com>
2024-09-18 11:27:39 -0400

    [GPU] Add workgroup/subgroup scope specification to mma attr interface
    Signed-off-by: Max Dawkins <max.dawkins@gmail.com>

--- compiler/src/iree/compiler/Codegen/Dialect/GPU/IR/IREEGPUAttrs.cpp
+++ compiler/src/iree/compiler/Codegen/Dialect/GPU/IR/IREEGPUAttrs.cpp
@@ -550,6 +550,10 @@ int64_t MMAAttr::getSubgroupSize() const {
   return getIntrinsicSubgroupSize(getIntrinsic().getValue());
 }
 
+FailureOr<IREE::GPU::MMAScope> MMAAttr::getMmaScope() const {
+  return IREE::GPU::MMAScope::Subgroup;
+}
+
 MMASingleSubgroupLayout getSingleSubgroupLayout(MMAIntrinsic intrinsic,
                                                 MMAFragment fragment) {
   switch (intrinsic) {
@@ -906,6 +910,10 @@ DataTiledMMAAttr::getABCVectorTypes() const {
 
 int64_t DataTiledMMAAttr::getSubgroupSize() const {
   return getIntrinsicSubgroupSize(getIntrinsic().getValue());
+}
+
+FailureOr<IREE::GPU::MMAScope> DataTiledMMAAttr::getMmaScope() const {
+  return IREE::GPU::MMAScope::Workgroup;
 }
 
 //===----------------------------------------------------------------------===//
