6b1c43345e430e277882b2083120009aa0fdb1f0
Scott Todd <scott.todd0@gmail.com>
2024-09-20 10:32:18 -0700

    Also fix sample_static.

--- experimental/web/sample_static/device_sync.c
+++ experimental/web/sample_static/device_sync.c
@@ -23,7 +23,7 @@ iree_status_t create_device_with_static_loader(iree_allocator_t host_allocator,
       iree_hal_executable_import_provider_null(), host_allocator,
       &library_loader);
 
-  iree_string_view_t identifier = iree_make_cstring_view("sync");
+  iree_string_view_t identifier = iree_make_cstring_view("local-sync");
   iree_hal_allocator_t* device_allocator = NULL;
   if (iree_status_is_ok(status)) {
     status = iree_hal_allocator_create_heap(identifier, host_allocator,
