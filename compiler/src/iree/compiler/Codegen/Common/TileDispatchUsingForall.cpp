a5fe968718a909684923e6137969090f258618a4
Prashant Kumar <pk5561@gmail.com>
2024-09-26 16:21:38 +0530

    [CODEGEN][COMMON] Select the last compute op that has workgroup tiling level
    Remove the check "expected only one op with a workgroup tiling level".

--- compiler/src/iree/compiler/Codegen/Common/TileDispatchUsingForall.cpp
+++ compiler/src/iree/compiler/Codegen/Common/TileDispatchUsingForall.cpp
@@ -53,18 +53,17 @@ struct TilingInfo {
 static FailureOr<TilingInfo>
 getTiledAndDistributionInfo(RewriterBase &rewriter,
                             ArrayRef<Operation *> computeOps) {
-  // It is expected that at most one compute op has a workgroup tiling level.
+  // TODO: It is expected that at most one compute op has a workgroup tiling
+  // level. Currently, it selects the last compute op that has workgroup tiling
+  // level.
   Operation *tilableOp = nullptr;
   for (Operation *op : llvm::reverse(computeOps)) {
     if (getLoweringConfig(op)) {
       if (!getLoweringConfig(op).hasWorkgroupTilingLevel()) {
         continue;
       }
-      if (tilableOp) {
-        return op->emitOpError("expected only one op with a workgroup tiling"
-                               "level.");
-      }
       tilableOp = op;
+      break;
     }
   }
   if (!tilableOp) {
