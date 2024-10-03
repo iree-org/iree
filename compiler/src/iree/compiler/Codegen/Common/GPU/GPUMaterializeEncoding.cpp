9494ebd40e5fd3251a9f0c04af566dc77eab9f57
Benoit Jacob <jacob.benoit.1@gmail.com>
2024-10-03 17:12:38 -0400

    review comment
    Signed-off-by: Benoit Jacob <jacob.benoit.1@gmail.com>

--- compiler/src/iree/compiler/Codegen/Common/GPU/GPUMaterializeEncoding.cpp
+++ compiler/src/iree/compiler/Codegen/Common/GPU/GPUMaterializeEncoding.cpp
@@ -436,7 +436,7 @@ materializeFuncOpEncodings(FunctionOpInterface funcOp,
     // 1. As linalg.matmul materializes into iree_gpu.multi_mma, which inherits
     //    its semantics from the wrapped intrinsic, we can't rely on any kind of
     //    LHS<->RHS symmetry.
-    // 2. We do not currently use ukernels, which would be one of the main ares
+    // 2. We do not currently use ukernels, which would be one of the main areas
     //    to benefit from transposeNarrowN.
     // 3. Heuristics for cache-friendly dispatch tiling are internal to the GPU
     //    runtime, so we don't need a simplification at that level either.
