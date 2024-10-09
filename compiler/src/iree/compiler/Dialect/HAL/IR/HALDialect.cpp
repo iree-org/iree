a5c78e10a5855e90cec0ca9eb4c1bc23f28c3217
hanhanW <hanhan0912@gmail.com>
2024-10-08 17:58:53 -0700

    Implement Stream::AffinityAnalysisDialectInterface for HAL dialect.
    Signed-off-by: hanhanW <hanhan0912@gmail.com>

--- compiler/src/iree/compiler/Dialect/HAL/IR/HALDialect.cpp
+++ compiler/src/iree/compiler/Dialect/HAL/IR/HALDialect.cpp
@@ -6,10 +6,13 @@
 
 #include "iree/compiler/Dialect/HAL/IR/HALDialect.h"
 
+#include "iree/compiler/Dialect/HAL/Analysis/DeviceAnalysis.h"
 #include "iree/compiler/Dialect/HAL/Conversion/HALToVM/Patterns.h"
 #include "iree/compiler/Dialect/HAL/IR/HALOps.h"
 #include "iree/compiler/Dialect/HAL/IR/HALTypes.h"
 #include "iree/compiler/Dialect/HAL/hal.imports.h"
+#include "iree/compiler/Dialect/Stream/Analysis/Affinity.h"
+#include "iree/compiler/Dialect/Stream/Analysis/AffinityAnalysisDialectInterface.h"
 #include "iree/compiler/Dialect/Util/IR/UtilDialect.h"
 #include "iree/compiler/Dialect/VM/Conversion/ConversionDialectInterface.h"
 #include "llvm/ADT/TypeSwitch.h"
@@ -115,6 +118,32 @@ public:
   }
 };
 
+class HALAffinityAnalysisDialectInterface
+    : public IREE::Stream::AffinityAnalysisDialectInterface {
+public:
+  using AffinityAnalysisDialectInterface::AffinityAnalysisDialectInterface;
+
+  std::function<LogicalResult(IREE::Stream::AffinityAttr, Operation *,
+                              SetVector<Attribute> &)>
+  makeTargetResolver(ModuleOp moduleOp) const {
+    return [=](IREE::Stream::AffinityAttr aff, Operation *op,
+               SetVector<Attribute> &targetAttrs) {
+      // TODO: This needs to be in the lambda. Otherwise, it could crash because
+      // the root op (i.e., the original moduleOp) could be outdated.
+      IREE::HAL::DeviceAnalysis deviceAnalysis(moduleOp);
+      if (failed(deviceAnalysis.run())) {
+        op->emitError("failed to run DeviceAnalysis");
+        return failure();
+      }
+      SetVector<IREE::HAL::ExecutableTargetAttr> resultSet;
+      deviceAnalysis.gatherRequiredExecutableTargets(aff, op, resultSet);
+      // TODO(hanchung): Populate the EncodingSolverAttr when it is ready.
+      targetAttrs.insert(resultSet.begin(), resultSet.end());
+      return success();
+    };
+  };
+};
+
 } // namespace
 
 HALDialect::HALDialect(MLIRContext *context)
@@ -131,6 +160,7 @@ HALDialect::HALDialect(MLIRContext *context)
 #include "iree/compiler/Dialect/HAL/IR/HALOps.cpp.inc"
       >();
   addInterfaces<HALInlinerInterface, HALOpAsmInterface,
+                HALAffinityAnalysisDialectInterface,
                 HALToVMConversionInterface>();
 }
 
---
+++ compiler/src/iree/compiler/Dialect/Stream/Analysis/AffinityAnalysisDialectInterface.h
@@ -0,0 +1,30 @@
+// Copyright 2024 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef IREE_COMPILER_DIALECT_STREAM_ANALYSIS_AFFINITYANALYSISDIALECTINTERFACE_H_
+#define IREE_COMPILER_DIALECT_STREAM_ANALYSIS_AFFINITYANALYSISDIALECTINTERFACE_H_
+
+#include "iree/compiler/Dialect/Stream/Analysis/Affinity.h"
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/DialectInterface.h"
+#include "mlir/Transforms/DialectConversion.h"
+
+namespace mlir::iree_compiler::IREE::Stream {
+
+class AffinityAnalysisDialectInterface
+    : public DialectInterface::Base<AffinityAnalysisDialectInterface> {
+public:
+  AffinityAnalysisDialectInterface(Dialect *dialect) : Base(dialect) {}
+
+  virtual std::function<LogicalResult(AffinityAttr, Operation *,
+                                      SetVector<Attribute> &)>
+  makeTargetResolver(ModuleOp moduleOp) const;
+};
+
+} // namespace mlir::iree_compiler::IREE::Stream
+
+#endif // IREE_COMPILER_DIALECT_STREAM_ANALYSIS_AFFINITYANALYSISDIALECTINTERFACE_H_
