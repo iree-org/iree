c8a03e21b1d6e3908d546c3aaf6ac1db8ffa10da
Kunwar Grover <groverkss@gmail.com>
2024-10-18 12:23:00 +0100

    [LinalgExt] Generalize attribute setting for attention decomposition

--- compiler/src/iree/compiler/Dialect/LinalgExt/IR/AggregatedOpInterfaceImpl.cpp
+++ compiler/src/iree/compiler/Dialect/LinalgExt/IR/AggregatedOpInterfaceImpl.cpp
@@ -309,6 +309,9 @@ OnlineAttentionOp::decomposeOperation(OpBuilder &b) {
   Value oldSum = getSum();
   Type elementType = getElementTypeOrSelf(getOutput().getType());
 
+  auto qkAttrs = (*this)->getAttrOfType<DictionaryAttr>("qk_attrs");
+  auto pvAttrs = (*this)->getAttrOfType<DictionaryAttr>("pv_attrs");
+
   FailureOr<AttentionOpDetail> maybeOpInfo =
       AttentionOpDetail::get(getIndexingMapsArray());
   assert(succeeded(maybeOpInfo) && "Invalid attention indexing maps");
@@ -363,10 +366,9 @@ OnlineAttentionOp::decomposeOperation(OpBuilder &b) {
   Value s = b.create<linalg::FillOp>(loc, sZero, emptyS).getResult(0);
 
   s = computeMatmul(b, loc, getQueryMap(), getKeyMap(), sMap, query, key, s);
-
-  // TODO: We shouldn't be relying on such attributes. We need a better
-  // mechanism to identify attention matmuls.
-  s.getDefiningOp()->setAttr("attention_qk_matmul", b.getUnitAttr());
+  if (qkAttrs) {
+    s.getDefiningOp()->setDiscardableAttrs(qkAttrs);
+  }
 
   s = applyPostQKMatmulElementwise(b, loc, getRegion(), s);
 
@@ -444,7 +446,9 @@ OnlineAttentionOp::decomposeOperation(OpBuilder &b) {
   newAcc = computeMatmul(b, loc, pMap, getValueMap(), accMap, p, value, newAcc);
   // TODO: We shouldn't be relying on such attributes. We need a better
   // mechanism to identify attention matmuls.
-  newAcc.getDefiningOp()->setAttr("attention_pv_matmul", b.getUnitAttr());
+  if (pvAttrs) {
+    newAcc.getDefiningOp()->setDiscardableAttrs(pvAttrs);
+  }
 
   return SmallVector<Value>{newAcc, newMax, newSum};
 }
