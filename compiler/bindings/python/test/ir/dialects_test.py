542b96a051371982120166545b15275a42c94a64
Scott Todd <scott.todd0@gmail.com>
2024-10-18 09:16:48 -0700

    Revert "`iree_gpu` Python bindings (`GPUPipelineOptionsAttr`) (#18804)"
    This reverts commit fb18c42422e0e9dae51df1f69f3b766e6e50aafe.

--- compiler/bindings/python/test/ir/dialects_test.py
+++ compiler/bindings/python/test/ir/dialects_test.py
@@ -7,69 +7,10 @@
 from iree.compiler import ir
 
 # Make sure that our dialects import.
-from iree.compiler.dialects import flow, hal, stream, vm, util, iree_gpu
-
-
-@lambda _: _()
-def gpu_pipeline_options_attr():
-    with ir.Context() as ctx, ir.Location.unknown():
-        module = ir.Module.create()
-        with ir.InsertionPoint(module.body):
-            reorder_attr = iree_gpu.ReorderWorkgroupsStrategyAttr.get(
-                iree_gpu.ReorderWorkgroupsStrategy.Swizzle, ctx
-            )
-            gpu_attr = iree_gpu.GPUPipelineOptionsAttr.get(
-                True,
-                False,
-                reorder_attr,
-            )
-            assert type(gpu_attr) is iree_gpu.GPUPipelineOptionsAttr
-            assert gpu_attr.prefetch_shared_memory
-            assert not gpu_attr.no_reduce_shared_memory_bank_conflicts
-
-            gpu_attr = iree_gpu.GPUPipelineOptionsAttr.get(
-                False,
-                True,
-                iree_gpu.ReorderWorkgroupsStrategyAttr.get(
-                    iree_gpu.ReorderWorkgroupsStrategy.Transpose, ctx
-                ),
-            )
-            assert not gpu_attr.prefetch_shared_memory
-            assert gpu_attr.no_reduce_shared_memory_bank_conflicts
-
-            gpu_attr = iree_gpu.GPUPipelineOptionsAttr.get()
-            assert (
-                gpu_attr.prefetch_shared_memory is None
-                and gpu_attr.no_reduce_shared_memory_bank_conflicts is None
-                and gpu_attr.reorder_workgroups_strategy is None
-            )
-
-            gpu_attr = iree_gpu.GPUPipelineOptionsAttr.get(True)
-            assert gpu_attr.prefetch_shared_memory
-            assert (
-                gpu_attr.no_reduce_shared_memory_bank_conflicts is None
-                and gpu_attr.reorder_workgroups_strategy is None
-            )
-
-            gpu_attr = iree_gpu.GPUPipelineOptionsAttr.get(True, False)
-            assert gpu_attr.reorder_workgroups_strategy is None
-
-            gpu_attr = iree_gpu.GPUPipelineOptionsAttr.get(
-                no_reduce_shared_memory_bank_conflicts=False
-            )
-            assert (
-                gpu_attr.no_reduce_shared_memory_bank_conflicts is not None
-                and not gpu_attr.no_reduce_shared_memory_bank_conflicts
-            )
-            assert gpu_attr.prefetch_shared_memory is None
-            assert gpu_attr.reorder_workgroups_strategy is None
-
-            gpu_attr = iree_gpu.GPUPipelineOptionsAttr.get(
-                reorder_workgroups_strategy=reorder_attr
-            )
-            assert gpu_attr.reorder_workgroups_strategy is not None
-            assert (
-                gpu_attr.reorder_workgroups_strategy.value
-                # unfortunately not `is`
-                == iree_gpu.ReorderWorkgroupsStrategy.Swizzle
-            )
+from iree.compiler.dialects import (
+    flow,
+    hal,
+    stream,
+    vm,
+    util,
+)
