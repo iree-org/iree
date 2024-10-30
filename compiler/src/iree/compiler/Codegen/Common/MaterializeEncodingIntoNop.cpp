57076d6316ab037ceb6fdf67ac170f206c460570
hanhanW <hanhan0912@gmail.com>
2024-10-30 16:52:06 -0700

    Fix type check and add more comments
    Signed-off-by: hanhanW <hanhan0912@gmail.com>

--- compiler/src/iree/compiler/Codegen/Common/MaterializeEncodingIntoNop.cpp
+++ compiler/src/iree/compiler/Codegen/Common/MaterializeEncodingIntoNop.cpp
@@ -11,6 +11,7 @@
 #include "iree/compiler/Dialect/HAL/IR/HALTypes.h"
 #include "mlir/Dialect/MemRef/Transforms/Transforms.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/IR/BuiltinTypes.h"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Transforms/DialectConversion.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
@@ -25,6 +26,18 @@ using namespace IREE::Encoding;
 
 namespace {
 
+/// Folds the tensor.extract_slice ops away when
+///    1. The source is unset_encoding
+///    2. The converted source and the result of the tensor.extract_slice op
+///       have the same type.
+/// If they static shapes, it can be folded away iff they have the same type.
+/// If one of dimensions is dynamic we can unconditionally remove the
+/// tensor.extract_slice op because the op won't be folded away unless the
+/// folding extract_slice(extract_slice) patterns are kicked in. This pattern is
+/// fragile in the case that we set encoding at program level. Because we can't
+/// expect what patterns are run after SetEncoding pass. However, it should be
+/// fine when we switch to data-tiling fusion path which set the encodings at
+/// dispatch level.
 struct FoldAwayExtractSliceOnUnsetEncodingPattern
     : public OpConversionPattern<tensor::ExtractSliceOp> {
   using OpConversionPattern<tensor::ExtractSliceOp>::OpConversionPattern;
@@ -37,6 +50,9 @@ struct FoldAwayExtractSliceOnUnsetEncodingPattern
     if (!unsetEncodingOp) {
       return rewriter.notifyMatchFailure(op,
                                          "source is not an unset_encoding op");
+    }
+    if (adaptor.getSource().getType() != op.getResultType()) {
+      return failure();
     }
     rewriter.replaceOp(op, adaptor.getSource());
     return success();
