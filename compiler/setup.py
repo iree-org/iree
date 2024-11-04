2b187e330c924d1f38249b9eab9959e765895a47
Marius Brehler <marius.brehler@amd.com>
2024-11-04 15:19:26 +0100

    Update classifiers
    Update classifiers to reflect with Python versions are supported.

--- compiler/setup.py
+++ compiler/setup.py
@@ -429,6 +429,8 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     ext_modules=[
         CMakeExtension("iree.compiler._mlir_libs._mlir"),
