5b3e3a732efceca459b15c74644e5fba15b714a3
Marius Brehler <marius.brehler@amd.com>
2024-11-05 16:04:22 +0100

    Apply new versioning scheme
    This implements a new versioning scheme, similar to the one recently
    proposed with #18938 and applies it to `iree-compiler`, `iree-runtime`
    and the `iree-dist` tarball. The TF and TFLite compiler tools use the
    (legacy) versioning and need to be updated with a follow up commit. The
    common version number is determined based on the versions of the
    compiler and the runtime and the higher version is picked as the common
    on. In contrast to the proposed versioning scheme, version numbers of
    development releases are in the format `X.Y.Z.dev0+${gitcommithash}` as
    discussed in #19003 and not `X.Y.Z.dev` as originally proposed.
    
    Not yet addressed is the refactoring of the workflows thus that the
    `version-local.json` files get only written once (and not for every OS)
    and to provide then to subsequent steps as artifacts.

--- compiler/setup.py
+++ compiler/setup.py
@@ -106,11 +106,12 @@ else:
     )
 
 # Setup and get version information.
-VERSION_INFO_FILE = os.path.join(IREE_SOURCE_DIR, "version_info.json")
+VERSION_FILE = os.path.join(IREE_SOURCE_DIR, "compiler/version.json")
+VERSION_FILE_LOCAL = os.path.join(IREE_SOURCE_DIR, "compiler/version-local.json")
 
 
-def load_version_info():
-    with open(VERSION_INFO_FILE, "rt") as f:
+def load_version_info(version_file):
+    with open(version_file, "rt") as f:
         return json.load(f)
 
 
@@ -146,17 +147,19 @@ def find_git_submodule_revision(submodule_path):
         return ""
 
 
+is_dev_build = False
 try:
-    version_info = load_version_info()
+    version_info = load_version_info(VERSION_FILE_LOCAL)
 except FileNotFoundError:
-    print("version_info.json not found. Using defaults", file=sys.stderr)
-    version_info = {}
+    print("version-local.json not found. Using version.json defaults")
+    version_info = load_version_info(VERSION_FILE)
+    is_dev_build = True
 git_versions = find_git_versions()
 
 PACKAGE_SUFFIX = version_info.get("package-suffix") or ""
 PACKAGE_VERSION = version_info.get("package-version")
-if not PACKAGE_VERSION:
-    PACKAGE_VERSION = f"0.dev0+{git_versions.get('IREE') or '0'}"
+if is_dev_build:
+    PACKAGE_VERSION += f"+{git_versions.get('IREE') or '0'}"
 
 
 def get_cmake_version_info_args():
