c2a5478553d8c35e74f0e4918a65333786106903
Marius Brehler <marius.brehler@amd.com>
2024-11-05 14:26:56 +0100

    Update classifiers of Python packages (#19004)
    Update classifiers to reflect which Python versions are supported.

--- integrations/tensorflow/python_projects/iree_tflite/setup.py
+++ integrations/tensorflow/python_projects/iree_tflite/setup.py
@@ -57,6 +57,9 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     python_requires=">=3.9",
     packages=find_namespace_packages(
--- runtime/setup.py
+++ runtime/setup.py
@@ -533,6 +533,8 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     url="https://github.com/iree-org/iree",
     python_requires=">=3.9",
