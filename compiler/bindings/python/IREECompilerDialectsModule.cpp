b406088df6c8ef5a8e38a8c4f267217ac76f0abf
Jakub Kuderski <jakub@nod-labs.com>
2024-11-11 14:26:16 -0500

    Clean up includes
    Signed-off-by: Jakub Kuderski <jakub@nod-labs.com>

--- compiler/bindings/python/IREECompilerDialectsModule.cpp
+++ compiler/bindings/python/IREECompilerDialectsModule.cpp
@@ -4,7 +4,6 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#include <pybind11/pytypes.h>
 #include <cstdint>
 #include "iree/compiler/dialects/iree_gpu.h"
 #include "mlir-c/BuiltinAttributes.h"
