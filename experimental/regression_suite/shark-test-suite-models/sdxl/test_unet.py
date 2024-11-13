9aaf51791a79d79f3bfe72027a11f070a7c25151
saienduri <saimanas.enduri@amd.com>
2024-11-13 01:09:53 -0800

    update outs for fp16 and fp8
    Signed-off-by: saienduri <saimanas.enduri@amd.com>

--- experimental/regression_suite/shark-test-suite-models/sdxl/test_unet.py
+++ experimental/regression_suite/shark-test-suite-models/sdxl/test_unet.py
@@ -94,7 +94,7 @@ sdxl_punet_int8_inference_input_5 = fetch_source_fixture(
 )
 
 sdxl_punet_int8_fp16_inference_output_0 = fetch_source_fixture(
-    "https://sharkpublic.blob.core.windows.net/sharkpublic/sai/sdxl-punet/11-8-2024/punet_fp16_out.0.bin",
+    "https://sharkpublic.blob.core.windows.net/sharkpublic/sai/sdxl-punet/11-13-2024/punet_fp16_out.0.bin",
     group="sdxl_punet_int8_fp16",
 )
 
@@ -111,7 +111,7 @@ sdxl_punet_int8_fp16_mlir = fetch_source_fixture(
 # INT8 Punet + FP8 Attention
 
 sdxl_punet_int8_fp8_inference_output_0 = fetch_source_fixture(
-    "https://sharkpublic.blob.core.windows.net/sharkpublic/sai/sdxl-punet/11-8-2024/punet_fp8_out.0.bin",
+    "https://sharkpublic.blob.core.windows.net/sharkpublic/sai/sdxl-punet/11-13-2024/punet_fp8_out.0.bin",
     group="sdxl_punet_int8_fp8",
 )
 
