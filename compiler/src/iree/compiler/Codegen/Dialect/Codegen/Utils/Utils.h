3bf3d2755f3cd40308d5b69b352e5505548d3894
hanhanW <hanhan0912@gmail.com>
2024-11-20 15:23:29 -0800

    fix the `ifdef` label in headers
    Signed-off-by: hanhanW <hanhan0912@gmail.com>

--- compiler/src/iree/compiler/Codegen/Dialect/Codegen/Utils/Utils.h
+++ compiler/src/iree/compiler/Codegen/Dialect/Codegen/Utils/Utils.h
@@ -4,8 +4,8 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#ifndef IREE_COMPILER__CODEGEN_DIALECT_CODEGEN_UTILS_H_
-#define IREE_COMPILER__CODEGEN_DIALECT_CODEGEN_UTILS_H_
+#ifndef IREE_COMPILER_CODEGEN_DIALECT_CODEGEN_UTILS_H_
+#define IREE_COMPILER_CODEGEN_DIALECT_CODEGEN_UTILS_H_
 
 #include <cstdint>
 
@@ -110,4 +110,4 @@ getExpandedTileShape(const TileSwizzle::ExpandShapeType &expandShape);
 
 } // namespace mlir::iree_compiler::IREE::Codegen
 
-#endif // IREE_COMPILER__CODEGEN_DIALECT_CODEGEN_UTILS_H_
+#endif // IREE_COMPILER_CODEGEN_DIALECT_CODEGEN_UTILS_H_
