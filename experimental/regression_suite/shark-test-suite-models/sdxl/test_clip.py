733cf28315b329c9be41262584c8e4566ec891b3
saienduri <saimanas.enduri@amd.com>
2024-11-27 01:46:35 -0600

    default always to gfx942
    Signed-off-by: saienduri <saimanas.enduri@amd.com>

--- experimental/regression_suite/shark-test-suite-models/sdxl/test_clip.py
+++ experimental/regression_suite/shark-test-suite-models/sdxl/test_clip.py
@@ -10,7 +10,7 @@ import os
 from conftest import VmfbManager
 from pathlib import Path
 
-rocm_chip = os.getenv("ROCM_CHIP", default="gfx90a")
+rocm_chip = os.getenv("ROCM_CHIP", default="gfx942")
 vmfb_dir = os.getenv("TEST_OUTPUT_ARTIFACTS", default=Path.cwd())
 
 ###############################################################################
