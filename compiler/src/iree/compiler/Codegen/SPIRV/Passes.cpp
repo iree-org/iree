cb5be1dbd3560f692578c137eadbb413b41e44c7
Krzysztof Drewniak <Krzysztof.Drewniak@amd.com>
2024-12-03 15:58:02 -0600

    Revert "[Codegen][GPU] Add range information to GPU dispatch IDs" (#19361)
    Reverts iree-org/iree#17707
    
    Potential regression and I'm not in today to debug it, reverting

--- compiler/src/iree/compiler/Codegen/SPIRV/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/SPIRV/Passes.cpp
@@ -227,11 +227,9 @@ static void addMemRefLoweringPasses(OpPassManager &modulePassManager) {
 /// Adds passes to perform the final SPIR-V conversion.
 static void addSPIRVLoweringPasses(OpPassManager &modulePassManager) {
   FunctionLikeNest(modulePassManager)
-      .addPass(createGPUPropagateDispatchSizeBoundsPass)
       .addPass(createCanonicalizerPass)
       .addPass(createCSEPass)
       .addPass(createLowerAffinePass)
-      .addPass(IREE::Util::createOptimizeIntArithmeticPass)
 
       // Lower ApplyScale before the i64 Emulation Pass so that new 64-bit ops
       // are also emulated if not supported by the target.
