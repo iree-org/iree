d2a66d77deaf51cd95b6c1301f3cbf97cd3a2cfa
Andrew Woloszyn <andrew.woloszyn@gmail.com>
2024-12-20 12:14:06 -0800

    [hip] Fixed a busy wait in event_semaphore. (#19540)
    Because of how locks were scheduled in iree_hal_hip_semaphore_wait we
    would run scheduled callbacks after preparing our notification but
    before waiting on it. However in running the scheduled callbacks it
    would unconditionally post to the notification.
    
    This change only posts to the notification if there was any forward
    progress made.
    
    ---------
    
    Signed-off-by: Andrew Woloszyn <andrew.woloszyn@gmail.com>

--- runtime/src/iree/hal/drivers/hip/event_semaphore.c
+++ runtime/src/iree/hal/drivers/hip/event_semaphore.c
@@ -842,10 +842,6 @@ static iree_status_t iree_hal_hip_semaphore_wait(
           iree_notification_prepare_wait(&semaphore->state_notification);
       iree_slim_mutex_unlock(&semaphore->mutex);
 
-      // We are going to pick up the correct status from query_locked below.
-      iree_status_ignore(
-          iree_hal_hip_event_semaphore_run_scheduled_callbacks(base_semaphore));
-
       // We have to wait for the semaphore to catch up.
       bool committed =
           iree_notification_commit_wait(&semaphore->state_notification, wait,
