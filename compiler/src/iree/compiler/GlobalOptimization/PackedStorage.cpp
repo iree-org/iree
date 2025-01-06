9bde48c37a7ed0bcd56552a2e04db9c9f370ee7d
Alan Li <me@alanli.org>
2025-01-06 02:57:45 +0000

    implement more things

--- compiler/src/iree/compiler/GlobalOptimization/PackedStorage.cpp
+++ compiler/src/iree/compiler/GlobalOptimization/PackedStorage.cpp
@@ -7,11 +7,13 @@
 #include "iree/compiler/Codegen/Common/EncodingUtils.h"
 #include "iree/compiler/Dialect/VM/Transforms/Passes.h"
 #include "iree/compiler/GlobalOptimization/Passes.h"
+#include "llvm/Support/Casting.h"
 #include "llvm/Support/Debug.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/Tensor/Transforms/Transforms.h"
 #include "mlir/Pass/Pass.h"
+#include "mlir/Transforms/DialectConversion.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
-#include "mlir/Dialect/Func/IR/FuncOps.h"
 
 #define DEBUG_TYPE "iree-global-opt-pack-storage"
 
@@ -20,66 +22,87 @@ namespace mlir::iree_compiler::GlobalOptimization {
 #define GEN_PASS_DEF_PACKSTORAGEPASS
 #include "iree/compiler/GlobalOptimization/Passes.h.inc"
 
-
 static RankedTensorType appendAttributeToTensor(RankedTensorType type) {
-  IntegerAttr bitwidthAttr =
-      IntegerAttr::get(IntegerType::get(type.getContext(), 32),
-                       type.getElementType().getIntOrFloatBitWidth());
-  IREE::Encoding::PackedStorageAttr packedAttr =
-      IREE::Encoding::PackedStorageAttr::get(type.getContext(), bitwidthAttr);
-  auto newType = RankedTensorType::get(type.getShape(), type.getElementType(),
-                               packedAttr);
+  // IntegerAttr bitwidthAttr =
+  //     IntegerAttr::get(IntegerType::get(type.getContext(), 32),
+  //                      type.getElementType().getIntOrFloatBitWidth());
+  IREE::Encoding::PackedStorageAttr packedAttr;
+  //    IREE::Encoding::PackedStorageAttr::get(type.getContext(), bitwidthAttr);
+  auto newType =
+      RankedTensorType::get(type.getShape(), type.getElementType(), packedAttr);
+
   assert(mlir::iree_compiler::IREE::Encoding::hasPackedStorageAttr(newType));
-  LLVM_DEBUG(llvm::dbgs() << " appending packed tensor type: " << newType << "\n");
+  LLVM_DEBUG(llvm::dbgs() << " appending packed tensor type: " << newType
+                          << "\n");
   return newType;
 }
 
-struct PackAttributeSignaturePattern : public OpConversionPattern<func::FuncOp> {
+static bool isPackStorageCandidate(Type type) {
+  if (auto tensorType = dyn_cast<RankedTensorType>(type)) {
+    auto elementType = tensorType.getElementType();
+    return elementType.isIntOrFloat() &&
+           elementType.getIntOrFloatBitWidth() == 1;
+  }
+  return false;
+}
+
+class PackedStorageConverter : public TypeConverter {
+public:
+  explicit PackedStorageConverter() {
+    addConversion([](RankedTensorType ty) -> Type {
+      return appendAttributeToTensor(ty);
+    });
+  }
+};
+
+struct PackAttributeSignaturePattern
+    : public OpConversionPattern<func::FuncOp> {
   using OpConversionPattern<func::FuncOp>::OpConversionPattern;
 
   LogicalResult
   matchAndRewrite(func::FuncOp funcOp, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
 
-    TypeConverter::SignatureConversion convertedResult(
-        funcOp.getNumArguments());
-    if (failed(getTypeConverter()->convertSignatureArgs(
-            funcOp.getArgumentTypes(), convertedResult)))
-      return failure();
+    TypeConverter::SignatureConversion converter(funcOp.getNumArguments());
+
+    for (const auto [index, arg] : llvm::enumerate(funcOp.getArguments())) {
+      if (isPackStorageCandidate(arg.getType())) {
+        converter.addInputs(index, arg.getType());
+      }
+    }
+    rewriter.applySignatureConversion(&funcOp.getFunctionBody().front(),
+                                      converter);
+    // TODO: check this
     rewriter.modifyOpInPlace(funcOp, [&] {
-      rewriter.applySignatureConversion(&funcOp.getFunctionBody().front(),
-                                        convertedResult);
+      funcOp.setType(rewriter.getFunctionType(converter.getConvertedTypes(),
+                                              std::nullopt));
     });
     return success();
   }
 };
 
-
 struct PackStoragePass : impl::PackStoragePassBase<PackStoragePass> {
-
   void getDependentDialects(DialectRegistry &registry) const override {
     registry.insert<tensor::TensorDialect>();
   }
-  void runOnOperation() override;
+  void runOnOperation() override {
+    auto funcOp = getOperation();
+    LLVM_DEBUG(llvm::dbgs() << "== Running PackStoragePass on "
+                            << funcOp->getName() << "\n");
+    auto context = &getContext();
 
-  static bool isPackStorageCandidate(RankedTensorType type) {
-    auto elementType = type.getElementType();
-    return elementType.isIntOrFloat() &&
-           elementType.getIntOrFloatBitWidth() == 1;
+    ConversionTarget target(*context);
+    PackedStorageConverter typeConverter;
+
+    RewritePatternSet conversionPatterns(context);
+    conversionPatterns.add<PackAttributeSignaturePattern>(typeConverter,
+                                                          context);
+
+    if (failed(applyPartialConversion(funcOp, target,
+                                      std::move(conversionPatterns)))) {
+      signalPassFailure();
+    }
   }
 };
-
-void PackStoragePass::runOnOperation() {
-  auto funcOp = getOperation();
-  LLVM_DEBUG(llvm::dbgs() << "== Running PackStoragePass on "
-                          << funcOp.getName() << "\n");
-  RewritePatternSet conversionPatterns(&getContext());
-  conversionPatterns.add<PackAttributeSignaturePattern>(&getContext());
-
-  if (failed(applyPatternsAndFoldGreedily(funcOp,
-                                          std::move(conversionPatterns)))) {
-    signalPassFailure();
-  }
-}
 
 } // namespace mlir::iree_compiler::GlobalOptimization
---
+++ compiler/src/iree/compiler/GlobalOptimization/test/pack_storage.mlir
@@ -0,0 +1,10 @@
+// RUN: iree-opt --split-input-file --allow-unregistered-dialect \
+// RUN:          --pass-pipeline="builtin.module(util.func(iree-global-opt-pack-storage))" \
+// RUN:          %s | FileCheck %s
+
+
+func.func @hloElementwiseOps(%arg0 : tensor<4xi1>, %arg1 : tensor<4xi1>) -> tensor<4xi1> {
+  %0 = arith.xori %arg0, %arg1 : tensor<4xi1>
+  return %0 : tensor<4xi1>
+}
+
