7cd8a0806b0c774346dbc584db027ac1a794cffd
Marius Brehler <marius.brehler@amd.com>
2025-01-08 18:48:23 +0100

    Update minor Python versions used to build packages (#19632)

--- build_tools/python_deploy/install_windows_deps.ps1
+++ build_tools/python_deploy/install_windows_deps.ps1
@@ -24,8 +24,8 @@ $PYTHON_VERSIONS_NO_DOT = @(
 
 # These can be discovered at https://www.python.org/downloads/windows/
 $PYTHON_INSTALLER_URLS = @(
-  "https://www.python.org/ftp/python/3.13.0/python-3.13.0-amd64.exe" #,
-  "https://www.python.org/ftp/python/3.12.6/python-3.12.6-amd64.exe" #,
+  "https://www.python.org/ftp/python/3.13.1/python-3.13.1-amd64.exe" #,
+  "https://www.python.org/ftp/python/3.12.8/python-3.12.8-amd64.exe" #,
   "https://www.python.org/ftp/python/3.11.9/python-3.11.9-amd64.exe" #,
   # "https://www.python.org/ftp/python/3.10.5/python-3.10.5-amd64.exe",
   # "https://www.python.org/ftp/python/3.9.13/python-3.9.13-amd64.exe"
