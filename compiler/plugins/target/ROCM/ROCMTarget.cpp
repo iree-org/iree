e64cb12ce4c18764f963fb40d892267fa14e5fca
Tres <tpopp@users.noreply.github.com>
2025-01-10 15:48:53 +0100

    Increase strictness of global isel use for ROCM (#19247)
    Increase strictness while this is primarily for development instead of
    robust production cases (though it is still quite robust).
    
    This will prevent fallback from global-isel with unsupported inputs to
    prevent a situation of attributing a witnessed change to improved global
    isel results that are actually caused by not using global isel or to
    prevent a lack of change caused by not actually utilizing global isel.
    
    In this form, unsupported inputs will fail to compile entirely.
    
    ---------
    
    Signed-off-by: Tres Popp <tpopp@brium.ai>

--- compiler/plugins/target/ROCM/ROCMTarget.cpp
+++ compiler/plugins/target/ROCM/ROCMTarget.cpp
@@ -475,7 +475,20 @@ public:
         opt.UnsafeFPMath = false;
         opt.NoInfsFPMath = false;
         opt.NoNaNsFPMath = true;
+        // Be extra cautious while this is less tested, and prevent unknown
+        // fallbacks from global isel.
+        //
+        // When GlobalISelAbort is set, any failure of GlobalISel,
+        // whether due to being not yet implemented or incorrect IR will result
+        // in an immediate abortion of compilation. This disables the fallback
+        // path of AMDGPUPassConfig::addInstSelector and 2 legacy passes which
+        // might work around unimplemented cases or errors in GlobalISel
+        // resulting in a successful compilation but would make one assume
+        // results are with GlobalISel when they are not.
         opt.EnableGlobalISel = options.globalISel;
+        opt.GlobalISelAbort = options.globalISel
+                                  ? llvm::GlobalISelAbortMode::Enable
+                                  : llvm::GlobalISelAbortMode::Disable;
         SmallVector<std::string> features;
         if (targetArch.starts_with("gfx10") ||
             targetArch.starts_with("gfx11")) {
