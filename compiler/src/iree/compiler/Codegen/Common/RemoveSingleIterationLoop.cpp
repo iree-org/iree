00fb633d9d73dd86b3188fabee351315e08a33e0
Krzysztof Drewniak <Krzysztof.Drewniak@amd.com>
2025-01-10 23:58:07 +0000

    [Codegen] Refactor RemoveSingleIteratinLoop  to use ValueBoundsOpInterface
    Now, RemoveLingleIterationLoop uses the same ValueBoundsOpInterface
    logic as IREELoopInvariantCodeMotion, unifying the two passes and
    getting rid of a half-baked implementation of that constraint
    analysis.
    
    In order to preserve existing functionality,
    PropagateDispatchSizeBounds is updated to correctly propagate constant
    sizes to constants.
    
    Also, add a test to make sure that the eliminator now understands the
    constraints imposed by delinearize and linearize.
    
    Co-authored-by: Quinn Dawkins <quinn.dawkins@gmail.com>

--- compiler/src/iree/compiler/Codegen/Common/RemoveSingleIterationLoop.cpp
+++ compiler/src/iree/compiler/Codegen/Common/RemoveSingleIterationLoop.cpp
@@ -24,89 +24,9 @@ namespace mlir::iree_compiler {
 #define GEN_PASS_DEF_REMOVESINGLEITERATIONLOOPPASS
 #include "iree/compiler/Codegen/Common/Passes.h.inc"
 
-/// Converts a symbolic GPU processor dimension to its numeric one.
-static unsigned dimToIndex(gpu::Dimension dim) {
-  switch (dim) {
-  case gpu::Dimension::x:
-    return 0;
-  case gpu::Dimension::y:
-    return 1;
-  case gpu::Dimension::z:
-    return 2;
-  default:
-    assert(false && "invalid dimension");
-    return 0;
-  }
-}
-
-/// If the value is a threadID return the range [0, workgroupSize-1].
-/// If the number of workgroup is known also return the range of workgroupId ad
-/// workgroupCount.
-static std::optional<std::pair<AffineExpr, AffineExpr>>
-getWorkgroupRange(Value processorValue, SmallVectorImpl<Value> & /*dims*/,
-                  SmallVectorImpl<Value> & /*symbols*/,
-                  ArrayRef<int64_t> workgroupCount,
-                  ArrayRef<int64_t> workgroupSize) {
-  if (!workgroupSize.empty()) {
-    if (auto idOp = processorValue.getDefiningOp<gpu::ThreadIdOp>()) {
-      unsigned index = dimToIndex(idOp.getDimension());
-      OpBuilder b(processorValue.getContext());
-      AffineExpr zero = b.getAffineConstantExpr(0);
-      AffineExpr ubExpr = b.getAffineConstantExpr(workgroupSize[index]);
-      return std::make_pair(zero, ubExpr - 1);
-    }
-    if (auto dimOp = processorValue.getDefiningOp<gpu::BlockDimOp>()) {
-      OpBuilder builder(processorValue.getContext());
-      unsigned index = dimToIndex(dimOp.getDimension());
-      AffineExpr bound = builder.getAffineConstantExpr(workgroupSize[index]);
-      return std::make_pair(bound, bound);
-    }
-  }
-
-  if (workgroupCount.empty() ||
-      llvm::any_of(workgroupCount, ShapedType::isDynamic))
-    return std::nullopt;
-
-  if (auto idOp =
-          processorValue.getDefiningOp<IREE::HAL::InterfaceWorkgroupIDOp>()) {
-    OpBuilder builder(processorValue.getContext());
-
-    // Can't infer the range when workroupCount is unknown.
-    unsigned index = idOp.getDimension().getZExtValue();
-    if (!workgroupCount[index])
-      return std::nullopt;
-
-    AffineExpr zero = builder.getAffineConstantExpr(0);
-    AffineExpr ubExpr = builder.getAffineConstantExpr(workgroupCount[index]);
-    return std::make_pair(zero, ubExpr - 1);
-  }
-  if (auto dimOp = processorValue
-                       .getDefiningOp<IREE::HAL::InterfaceWorkgroupCountOp>()) {
-    OpBuilder builder(processorValue.getContext());
-
-    // Can't infer the range when workroupCount is unknown.
-    unsigned index = dimOp.getDimension().getZExtValue();
-    if (!workgroupCount[index])
-      return std::nullopt;
-
-    AffineExpr bound = builder.getAffineConstantExpr(workgroupCount[index]);
-    return std::make_pair(bound, bound);
-  }
-  return std::nullopt;
-}
-
-static LogicalResult removeOneTripTiledLoops(mlir::FunctionOpInterface funcOp,
-                                             ArrayRef<int64_t> workgroupSize,
-                                             ArrayRef<int64_t> numWorkgroups) {
-  auto getWorkgroupRangeFn = [numWorkgroups,
-                              workgroupSize](Value processorValue,
-                                             SmallVectorImpl<Value> &dims,
-                                             SmallVectorImpl<Value> &symbols) {
-    return getWorkgroupRange(processorValue, dims, symbols, numWorkgroups,
-                             workgroupSize);
-  };
+static LogicalResult removeOneTripTiledLoops(mlir::FunctionOpInterface funcOp) {
   RewritePatternSet patterns(funcOp.getContext());
-  populateRemoveSingleIterationLoopPattern(patterns, getWorkgroupRangeFn);
+  populateRemoveSingleIterationLoopPattern(patterns);
   return applyPatternsGreedily(funcOp, std::move(patterns));
 }
 
@@ -118,15 +38,7 @@ class RemoveSingleIterationLoopPass final
   void runOnOperation() override {
     auto funcOp = getOperation();
 
-    std::optional<SmallVector<int64_t>> workgroupSize =
-        getWorkgroupSize(funcOp);
-    if (!workgroupSize) {
-      return;
-    }
-    SmallVector<int64_t> numWorkgroups = getStaticNumWorkgroups(funcOp);
-
-    if (failed(removeOneTripTiledLoops(funcOp, workgroupSize.value(),
-                                       numWorkgroups))) {
+    if (failed(removeOneTripTiledLoops(funcOp))) {
       return signalPassFailure();
     }
   }
