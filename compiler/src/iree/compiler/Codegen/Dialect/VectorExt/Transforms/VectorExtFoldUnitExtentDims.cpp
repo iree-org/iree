697051a612c9fed96e3cca19ac91ac2960c2f634
Manupa Karunaratne <manupa.karunaratne@amd.com>
2025-01-22 08:19:42 -0800

    [VectorExt] make fold unit dims work with dynamic shape
    This commit adds a one liner to make the empty destination
    be compatible with dynamic shapes (if any).
    
    Signed-off-by: Manupa Karunaratne <manupa.karunaratne@amd.com>

--- compiler/src/iree/compiler/Codegen/Dialect/VectorExt/Transforms/VectorExtFoldUnitExtentDims.cpp
+++ compiler/src/iree/compiler/Codegen/Dialect/VectorExt/Transforms/VectorExtFoldUnitExtentDims.cpp
@@ -64,10 +64,9 @@ struct DropToLayoutUnitDims final
         toLayoutOp.getSharedMemoryConversion(), toLayoutOp.getMmaKindAttr());
 
     // Expand to preserve output shape using insert_slice.
-    // Here, since the shape comes from the result of a to_layout op, it will
-    // always be static.
-    Value dest =
-        rewriter.create<tensor::EmptyOp>(loc, shape, inputTy.getElementType());
+    Value dest = rewriter.create<tensor::EmptyOp>(
+        loc, tensor::getMixedSizes(rewriter, loc, toLayoutOp.getInput()),
+        inputTy.getElementType());
 
     int64_t rank = inputTy.getRank();
     SmallVector<OpFoldResult> offsets(rank, rewriter.getIndexAttr(0));
