dade56201bf47fa164e856b58ba33bc256999ec8
Scott Todd <scott.todd0@gmail.com>
2025-01-21 20:27:51 -0800

    Fix or work around gcc-14 warnings/errors. (#19758)
    Fixing or working around a few "warnings as errors" on gcc-14 (released
    May 2024). These are also causing the aarch64 nightly distribution
    builds to fail. Note that we also have some new warnings as errors
    coming in clang-20 (not yet released):
    https://clang.llvm.org/docs/ReleaseNotes.html#improvements-to-clang-s-diagnostics.
    
    ### dangling-reference
    
    Not sure how we want to handle these. We have a bunch of code in IREE
    and torch-mlir that uses temporaries in loop headers, which this warning
    does not like. For now just disabling the warning.
    
    ```
    /iree/compiler/src/iree/compiler/API/Internal/Diagnostics.cpp: In function 'std::optional<mlir::CallSiteLoc> mlir::iree_compiler::embed::{anonymous}::getCallSiteLoc(mlir::Location)':
    /iree/compiler/src/iree/compiler/API/Internal/Diagnostics.cpp:30:57: error: possibly dangling reference to a temporary [-Werror=dangling-reference]
       30 |     for (auto subLoc : cast<FusedLoc>(loc).getLocations()) {
          |                                                         ^
    /iree/compiler/src/iree/compiler/API/Internal/Diagnostics.cpp:30:56: note: the temporary was destroyed at the end of the full expression 'llvm::cast<mlir::FusedLoc, mlir::Location>(loc).mlir::FusedLoc::getLocations()'
       30 |     for (auto subLoc : cast<FusedLoc>(loc).getLocations()) {
          |                        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~
    ```
    
    ### template-id-cdtor
    
    These looked legit and were easy enough to fix.
    
    ```
    /iree/compiler/src/iree/compiler/Dialect/LinalgExt/Transforms/ConvertConv2DToWinograd.cpp:159:33: error: template-id not allowed for constructor in C++20 [-Werror=template-id-cdtor]
      159 |   ConvertConvToWinograd<ConvOp>(MLIRContext *context, bool replaceAllConvs,
          |                                 ^~~~~~~~~~~
    /iree/compiler/src/iree/compiler/Dialect/LinalgExt/Transforms/ConvertConv2DToWinograd.cpp:159:33: note: remove the '< >'
    ```

--- compiler/src/iree/compiler/Codegen/Common/GPU/GPUVectorDistribution.h
+++ compiler/src/iree/compiler/Codegen/Common/GPU/GPUVectorDistribution.h
@@ -53,8 +53,7 @@ protected:
 
 template <typename SourceOp>
 struct OpDistributionPattern : DistributionPattern {
-  OpDistributionPattern<SourceOp>(MLIRContext *context,
-                                  PatternBenefit benefit = 1)
+  OpDistributionPattern(MLIRContext *context, PatternBenefit benefit = 1)
       : DistributionPattern(SourceOp::getOperationName(), benefit, context) {}
 
   virtual LogicalResult matchAndRewrite(SourceOp op,
