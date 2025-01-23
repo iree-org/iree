7c7d4ff26deb256f901c8049b5182cf57f8bc19e
Scott Todd <scott.todd0@gmail.com>
2025-01-23 12:15:38 -0800

    Prune LegalizeToLinalgUtils.

--- compiler/plugins/input/StableHLO/Conversion/LegalizeToLinalgUtils.h
+++ compiler/plugins/input/StableHLO/Conversion/LegalizeToLinalgUtils.h
@@ -15,26 +15,16 @@
 #include <string>
 #include <utility>
 
-#include "compiler/plugins/input/StableHLO/Conversion/MapStableHLOToScalarOp.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/SmallVector.h"
-#include "llvm/ADT/StringSet.h"
-#include "mlir/Dialect/Linalg/IR/Linalg.h"
 #include "mlir/Dialect/Linalg/Utils/Utils.h"
-#include "mlir/Dialect/Tensor/IR/Tensor.h"
-#include "mlir/IR/AffineExpr.h"
 #include "mlir/IR/Attributes.h"
-#include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/BuiltinTypes.h"
-#include "mlir/IR/Location.h"
-#include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/Operation.h"
 #include "mlir/IR/OperationSupport.h"
 #include "mlir/IR/TypeUtilities.h"
 #include "mlir/Support/LLVM.h"
-#include "mlir/Support/LogicalResult.h"
-#include "mlir/Transforms/DialectConversion.h"
 #include "stablehlo/dialect/StablehloOps.h"
 
 namespace mlir::iree_compiler::stablehlo {
@@ -49,74 +39,8 @@ getParallelAndReductionIterators(unsigned nLoops, unsigned nReduction);
 SmallVector<utils::IteratorType, 3>
 getNParallelLoopsAttrs(unsigned nParallelLoops);
 
-/// Generates an init sparse tensor.
-Value getEmptySparseTensor(OpBuilder &b, Location loc, ShapedType type,
-                           ArrayRef<Value> dynSizes);
-
-/// Generates a tensor.empty op.
-Value getEmptyTensor(OpBuilder &b, Location loc, ShapedType type,
-                     ArrayRef<Value> dynSizes);
-
-/// Generates an empty tensor for the result of the operation, which could be a
-/// dense tensor or a sparse tensor.
-Value getEmptyTensorFor(OpBuilder &b, Location loc, ShapedType resultType,
-                        Operation *op, ValueRange operands);
-
-/// Ensures a tensor has the same shape (not including the element type) as
-/// another.
-Value coerceTensorShape(OpBuilder &builder, Location loc,
-                        TypedValue<ShapedType> value, ShapedType targetType);
-
-/// Verifies |op|'s semantics by checking if all operands and results have
-/// ranged tensor types.
-LogicalResult verifyHloOpBufferOrTensorSemantics(Operation *op);
-
-/// Fills |tensor| with a zero constant of the matching type. Returns the new
-/// value.
-Value fillTensorWithZeros(OpBuilder &builder, Location loc, Value tensor);
-
-/// Sparsifies a (block of) operation(s) that cannot be handled directly
-/// by the sparse compiler but has well-known semi-ring semantics.
-///
-/// This yields something of the following form:
-///
-///   %result = sparse_tensor.unary %values[0]
-///     present={
-///       ^bb1(%val):
-///         ... codegen proceeds here using %val ....
-///         sparse_tensor.yield
-///     }
-///     absent={}
-///   linalg.yield %result
-Value preSparsify(Operation *op, llvm::SmallVector<Value, 2> &values, Type rtp,
-                  OpBuilder *b);
-
-/// Finalizes sparse semi-ring construction.
-Value postSparsify(Operation *op, Value semiring, Value result, OpBuilder *b);
-
-/// Returns true if all operands are tensors with rank 0.
-bool allOperandsAreScalarTensors(Operation *op);
-
-/// Returns true if parent op is linalg.
-bool isInBodyOfLinalgOps(Operation *op);
-
 /// Extracts integer values from the attribute |elements|.
 SmallVector<int64_t> extract1DVector(DenseIntElementsAttr elements);
-
-/// Returns true if the given |values| is a splat of the given |queryValue|.
-inline bool isSplatValue(const ArrayRef<int64_t> &values, int64_t queryValue) {
-  for (auto value : values) {
-    if (value != queryValue) {
-      return false;
-    }
-  }
-  return true;
-}
-
-/// Returns true if the given |attr| is a splat of the given |value|.
-inline bool isSplatValue(DenseIntElementsAttr attr, uint64_t value) {
-  return attr.isSplat() && attr.getSplatValue<uint64_t>() == value;
-}
 
 } // namespace mlir::iree_compiler::stablehlo
 
