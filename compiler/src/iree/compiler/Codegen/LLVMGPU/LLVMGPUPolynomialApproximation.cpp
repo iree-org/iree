a6ee7ec465ebf4ba8f999ff9872b0d4e775ba7bc
Alan Li <me@alanli.org>
2025-01-23 15:47:54 +0000

    Remove FPowI expansion

--- compiler/src/iree/compiler/Codegen/LLVMGPU/LLVMGPUPolynomialApproximation.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/LLVMGPUPolynomialApproximation.cpp
@@ -26,7 +26,6 @@ struct LLVMGPUPolynomialApproximationPass final
     // TODO(lialan): Handle these functions efficiently in ROCDL/NVVM
     // conversion passes. This expansion is likely suboptimal.
     populateExpandPowFPattern(mathPatterns);
-    populateExpandFPowIPattern(mathPatterns);
 
     if (clNativeMathPrecision) {
       mathPatterns.add<math::ErfPolynomialApproximation>(&getContext());
