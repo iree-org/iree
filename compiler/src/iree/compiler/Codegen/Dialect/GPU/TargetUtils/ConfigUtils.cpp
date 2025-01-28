e1ebae34ddd9124936bb2d954c698e2c9ca7b13c
jerryyin <zhuoryin@amd.com>
2025-01-28 20:52:54 +0000

    Addressing review feedback
    Setting padding tile size to be coherent with reduction tile size
    
    Signed-off-by: jerryyin <zhuoryin@amd.com>

--- compiler/src/iree/compiler/Codegen/Dialect/GPU/TargetUtils/ConfigUtils.cpp
+++ compiler/src/iree/compiler/Codegen/Dialect/GPU/TargetUtils/ConfigUtils.cpp
@@ -336,14 +336,14 @@ getMatmulLoweringConfigAndWorkgroupSize(SmallVector<int64_t> bounds,
     GPU::setPromotedOperandList(context, attrs, {0, 1, 2});
     SmallVector<int64_t> paddingTileSizes = workgroupTileSizes;
 
-    // Initialize inner and outer padding sizes for K dimensions.
+    // Initialize inner and outer padding sizes from reductionTileSizes.
+    for (int64_t kDim : kDims) {
+      paddingTileSizes[kDim] = reductionTileSizes[kDim];
+    }
+
     int64_t innerKDim = contractionDims.k.back();
     int64_t kPackFactor = std::get<2>(mmaKind.getMNKShape());
-    paddingTileSizes[innerKDim] = reductionTileSizes[innerKDim] * kPackFactor;
-
-    for (int64_t k : llvm::drop_end(contractionDims.k)) {
-      paddingTileSizes[k] = 1;
-    }
+    paddingTileSizes[innerKDim] *= kPackFactor;
 
     attrs.emplace_back(StringAttr::get(context, "padding"),
                        b.getI64ArrayAttr(paddingTileSizes));
