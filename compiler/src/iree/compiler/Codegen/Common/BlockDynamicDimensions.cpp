d96d142fab6f059b44be38fb6ae1ea776097dd9a
Ian Wood <ianwood2024@u.northwestern.edu>
2025-02-26 12:38:03 -0800

    [Codegen] Block dyn dims of parallel linalg.generic ops (#20091)
    Fixes performance issues with dispatches with a single dynamic
    elementwise op.
    
    ---------
    
    Signed-off-by: Ian Wood <ianwood2024@u.northwestern.edu>

--- compiler/src/iree/compiler/Codegen/Common/BlockDynamicDimensions.cpp
+++ compiler/src/iree/compiler/Codegen/Common/BlockDynamicDimensions.cpp
@@ -265,6 +265,10 @@ static LogicalResult
 blockDynamicDimensions(RewriterBase &rewriter,
                        const TensorDynamicDimAnalysis &dynamicDimAnalysis,
                        linalg::LinalgOp linalgOp) {
+  if (isa<linalg::GenericOp>(linalgOp) && linalgOp.isAllParallelLoops()) {
+    return blockDynamicDimensionsOfAllTensorOperandsAndResults(
+        rewriter, dynamicDimAnalysis, linalgOp);
+  }
   if (linalg::isaContractionOpInterface(linalgOp)) {
     return blockDynamicDimensionsOfAllTensorOperandsAndResults(
         rewriter, dynamicDimAnalysis, linalgOp);
