b07e692e6ce3a1d689875c87cc54532430904be9
Benoit Jacob <jacob.benoit.1@gmail.com>
2025-03-05 21:32:42 -0500

    Add a tolerance parameter to `check.expect_almost_eq*` ops. (#20146)
    Signed-off-by: Benoit Jacob <jacob.benoit.1@gmail.com>

--- compiler/src/iree/compiler/Modules/Check/IR/CheckOps.cpp
+++ compiler/src/iree/compiler/Modules/Check/IR/CheckOps.cpp
@@ -14,28 +14,73 @@
 namespace mlir::iree_compiler::IREE::Check {
 
 namespace {
-template <typename SrcOp, typename DstOp>
-struct ExpandAttributeToConst : public OpRewritePattern<SrcOp> {
-  using OpRewritePattern<SrcOp>::OpRewritePattern;
-  LogicalResult matchAndRewrite(SrcOp op,
+// Rewrites expect_eq_const -> expect_eq
+struct ExpectEqConstOpToExpectEqOp : public OpRewritePattern<ExpectEqConstOp> {
+  using OpRewritePattern<ExpectEqConstOp>::OpRewritePattern;
+  LogicalResult matchAndRewrite(ExpectEqConstOp op,
                                 PatternRewriter &rewriter) const override {
     auto rhs = rewriter.create<arith::ConstantOp>(op.getLoc(), op.getValue());
-    rewriter.replaceOpWithNewOp<DstOp>(op, op.getDevice(), op.getLhs(), rhs);
+    rewriter.replaceOpWithNewOp<ExpectEqOp>(op, op.getDevice(), op.getLhs(),
+                                            rhs);
     return success();
   }
 };
+
+// Rewrites expect_almost_eq_const -> expect_almost_eq
+struct ExpectAlmostEqConstOpToExpectAlmostEqOp
+    : public OpRewritePattern<ExpectAlmostEqConstOp> {
+  using OpRewritePattern<ExpectAlmostEqConstOp>::OpRewritePattern;
+  LogicalResult matchAndRewrite(ExpectAlmostEqConstOp op,
+                                PatternRewriter &rewriter) const override {
+    auto rhs = rewriter.create<arith::ConstantOp>(op.getLoc(), op.getValue());
+    rewriter.replaceOpWithNewOp<ExpectAlmostEqOp>(
+        op, op.getDevice(), op.getLhs(), rhs, op.getToleranceAttr());
+    return success();
+  }
+};
+
+static constexpr char kToleranceKeyword[] = "tolerance";
+static constexpr float kToleranceDefaultValue = 1e-4f;
+
+static ParseResult parseOptionalFloatTolerance(OpAsmParser &parser,
+                                               FloatAttr &tolerance) {
+  float toleranceValue = kToleranceDefaultValue;
+  if (succeeded(parser.parseOptionalComma())) {
+    if (failed(parser.parseKeyword(kToleranceKeyword))) {
+      return parser.emitError(parser.getCurrentLocation(),
+                              llvm::Twine("Expected keyword: ") +
+                                  kToleranceKeyword);
+    }
+    llvm::APFloat parsedTolerance(APFloat::IEEEsingle());
+    if (failed(parser.parseFloat(parsedTolerance.getSemantics(),
+                                 parsedTolerance))) {
+      return parser.emitError(parser.getCurrentLocation(),
+                              "Failed to parse optional float tolerance.");
+    }
+    toleranceValue = parsedTolerance.convertToFloat();
+  }
+  tolerance = parser.getBuilder().getF32FloatAttr(toleranceValue);
+  return success();
+}
+
+static void printOptionalFloatTolerance(OpAsmPrinter &p, Operation *op,
+                                        FloatAttr tolerance) {
+  float toleranceValue = tolerance.getValue().convertToFloat();
+  if (toleranceValue != kToleranceDefaultValue) {
+    p << ", " << kToleranceKeyword << " " << toleranceValue;
+  }
+}
+
 } // namespace
 
 void ExpectEqConstOp::getCanonicalizationPatterns(RewritePatternSet &results,
                                                   MLIRContext *context) {
-  results.insert<ExpandAttributeToConst<ExpectEqConstOp, ExpectEqOp>>(context);
+  results.insert<ExpectEqConstOpToExpectEqOp>(context);
 }
 
 void ExpectAlmostEqConstOp::getCanonicalizationPatterns(
     RewritePatternSet &results, MLIRContext *context) {
-  results
-      .insert<ExpandAttributeToConst<ExpectAlmostEqConstOp, ExpectAlmostEqOp>>(
-          context);
+  results.insert<ExpectAlmostEqConstOpToExpectAlmostEqOp>(context);
 }
 
 } // namespace mlir::iree_compiler::IREE::Check
