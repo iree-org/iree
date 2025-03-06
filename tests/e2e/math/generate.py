844471fce901ab4ad148a3a15244ded7f2cede21
Benoit Jacob <jacob.benoit.1@gmail.com>
2025-03-06 09:19:07 -0600

    fix copyrights
    Signed-off-by: Benoit Jacob <jacob.benoit.1@gmail.com>

--- tests/e2e/math/generate.py
+++ tests/e2e/math/generate.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-# Copyright 2021 The IREE Authors
+
+# Copyright 2025 The IREE Authors
 #
 # Licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 """Generate e2e tests of math ops.
 
 Users looking to regenerate tests: do not run this directly, run generate.sh instead.
--- tests/e2e/math/generate.sh
+++ tests/e2e/math/generate.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Copyright 2025 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# Run this script, without arguments, to regenerate the .mlir test files.
+
 set -eu
 
 dir="$(dirname "$0")"
@@ -19,7 +27,7 @@ header="// Generated file, do not edit!
 
 for backend in ${backends[@]}; do
   for type in ${float_types[@]}; do
-    python ${dir}/generate.py \
+    python3 ${dir}/generate.py \
       --type="${type}" \
       --backend="${backend}" \
       --header="${header}" \
