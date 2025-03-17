e278f2c2d7e9ffec3391407f2cdbc2c327f9dc61
Kunwar Grover <groverkss@gmail.com>
2025-03-17 17:36:11 +0000

    [CPU] Improve Fusions and KernelConfig heuristics for Attention (#20245)
    - Use newer TileAndFuse passes for LinalgExt TileAndVectorize pipeline
    - Restrict QK and PV vector tile sizes based on register space available
    - Restrict vector tile sizes based on the max allowed vector tile size
    verification check. This requires us to move the flat for setting max
    vector size to LLVMCPU/Utils.
    
    This PR makes sure we never generate tile sizes that are too big to pass
    large vector verification check, i.e. attention will never fail to
    compile on CPU. The performance can be improved by selecting better
    heuristics, but that is a problem for another day.

--- compiler/src/iree/compiler/Codegen/LLVMCPU/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMCPU/Passes.cpp
@@ -620,14 +620,26 @@ void addCPULinalgExtTileAndVectorizePipeline(
     OpPassManager &funcPassManager, TilingConfig &tilingConfig,
     LLVMCPUPipelineOptions &pipelineOpt) {
   addTileAndDistributePasses(funcPassManager);
-  funcPassManager.addPass(
-      createLLVMCPUTilePass(tilingConfig.getVectorCommonParallelLevel()));
-  // TODO: Remove the pass once we have PartialReductionOpInterface implemented
-  // for AttentionOp.
+
+  {
+    LLVMCPUTileRootAndFuseProducerConsumerPassOptions options;
+    options.tilingLevel = tilingConfig.getVectorCommonParallelLevel();
+    options.onlyFuseProducerInputOperands = false;
+    funcPassManager.addPass(
+        createLLVMCPUTileRootAndFuseProducerConsumerPass(options));
+  }
+
   funcPassManager.addPass(
       IREE::LinalgExt::createConvertAttentionToOnlineAttentionPass());
-  funcPassManager.addPass(
-      createLLVMCPUTilePass(tilingConfig.getVectorReductionLevel()));
+
+  {
+    LLVMCPUTileRootAndFuseProducerConsumerPassOptions options;
+    options.tilingLevel = tilingConfig.getVectorReductionLevel();
+    options.onlyFuseProducerInputOperands = true;
+    funcPassManager.addPass(
+        createLLVMCPUTileRootAndFuseProducerConsumerPass(options));
+  }
+
   funcPassManager.addPass(
       IREE::LinalgExt::createDecomposeWinogradTransformPass());
   funcPassManager.addPass(IREE::LinalgExt::createDecomposeAttentionPass());
@@ -637,6 +649,8 @@ void addCPULinalgExtTileAndVectorizePipeline(
     options.useConfiguredVectorSizes = pipelineOpt.useConfiguredVectorSizes;
     options.enableVectorMasking = pipelineOpt.enableVectorMasking;
     funcPassManager.addPass(createGenericVectorizationPass(options));
+    funcPassManager.addPass(createCanonicalizerPass());
+    funcPassManager.addPass(createCSEPass());
     funcPassManager.addPass(createOptimizeTensorInsertExtractSlicesPass());
     funcPassManager.addPass(createCanonicalizerPass());
     funcPassManager.addPass(createCSEPass());
