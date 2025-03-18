93b369a55b9749cb0448e83725248f1e68aa8743
Han-Chung Wang <hanhan0912@gmail.com>
2025-03-18 10:04:37 -0700

    [Encoding] Implement isIdentityLayout interface method. (#20286)
    The revision introduces `isIdentityLayout` interface method to
    SerializableEncodingAttrInterface, and implements the method for
    `EncodingAttr` and `PadEncodingLayoutAttr` attributes.
    
    It also fixes a bug in `EncodingAttr` builders, where we should use NULL
    when maps are not present.
    
    ---------
    
    Signed-off-by: hanhanW <hanhan0912@gmail.com>

---
+++ compiler/src/iree/compiler/Dialect/Encoding/IR/unittests/EncodingAttrTest.cpp
@@ -0,0 +1,59 @@
+// Copyright 2025 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "iree/compiler/Dialect/Encoding/IR/EncodingDialect.h"
+#include "iree/compiler/Dialect/Encoding/IR/EncodingTypes.h"
+
+namespace mlir::iree_compiler::IREE::Encoding {
+namespace {
+
+class EncodingAttrsTest : public ::testing::Test {
+protected:
+  EncodingAttrsTest() {
+    reg.insert<IREEEncodingDialect>();
+    ctx.appendDialectRegistry(reg);
+    ctx.loadAllAvailableDialects();
+  }
+  ~EncodingAttrsTest() override {}
+
+  MLIRContext *getContext() { return &ctx; }
+
+private:
+  MLIRContext ctx;
+  DialectRegistry reg;
+};
+
+TEST_F(EncodingAttrsTest, EncodingAttr) {
+  MLIRContext *ctx = getContext();
+  Builder builder(ctx);
+  SmallVector<Type> elemTypes(3, builder.getF32Type());
+  auto attr = cast<SerializableEncodingAttrInterface>(EncodingAttr::get(
+      ctx, /*operandIndex=*/0, EncodingOpType::matmul, elemTypes));
+  EXPECT_FALSE(attr.isIdentityLayout());
+
+  attr = cast<SerializableEncodingAttrInterface>(attr.cloneWithLayouts(
+      PadEncodingLayoutAttr::getIdentityAttr(ctx, /*rank=*/2)));
+  EXPECT_TRUE(attr.isIdentityLayout());
+}
+
+TEST_F(EncodingAttrsTest, PadEncodingLayoutAttr) {
+  MLIRContext *ctx = getContext();
+  auto zeroPaddingAttr =
+      PadEncodingLayoutAttr::getIdentityAttr(ctx, /*rank=*/2);
+  EXPECT_TRUE(cast<SerializableEncodingAttrInterface>(zeroPaddingAttr)
+                  .isIdentityLayout());
+
+  SmallVector<int32_t> paddings = {4, 2};
+  auto nonZeroPaddingAttr = PadEncodingLayoutAttr::get(ctx, paddings);
+  EXPECT_FALSE(cast<SerializableEncodingAttrInterface>(nonZeroPaddingAttr)
+                   .isIdentityLayout());
+}
+
+} // namespace
+} // namespace mlir::iree_compiler::IREE::Encoding
