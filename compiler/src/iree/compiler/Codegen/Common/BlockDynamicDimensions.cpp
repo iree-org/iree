efdcb56ae01e358262c9608e7b9cd6996d1b72b2
Quinn Dawkins <quinn.dawkins@gmail.com>
2025-03-18 17:21:04 -0400

    Integrate llvm-project@681b2413 (#20274)
    Retains reverts for 418e07b7e and 1a3dc8e9c.
    
    ---------
    
    Signed-off-by: MaheshRavishankar <mahesh.ravishankar@gmail.com>
    Co-authored-by: MaheshRavishankar <mahesh.ravishankar@gmail.com>

--- compiler/src/iree/compiler/Codegen/Common/BlockDynamicDimensions.cpp
+++ compiler/src/iree/compiler/Codegen/Common/BlockDynamicDimensions.cpp
@@ -9,6 +9,7 @@
 #include "iree/compiler/Codegen/Transforms/Transforms.h"
 #include "iree/compiler/Dialect/LinalgExt/IR/LinalgExtOps.h"
 #include "iree/compiler/Dialect/LinalgExt/Transforms/Transforms.h"
+#include "iree/compiler/Dialect/Util/IR/UtilDialect.h"
 #include "iree/compiler/Dialect/Util/IR/UtilOps.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Arith/Utils/Utils.h"
@@ -233,9 +234,18 @@ static LogicalResult blockDynamicDimensions(
         blockDynamicDimensionsOfValue(rewriter, resultDivisibilityInfo, result);
     if (reshapes) {
       llvm::SmallPtrSet<Operation *, 1> ignoreUses;
-      ignoreUses.insert(reshapes->expandShapeOp);
-      rewriter.replaceAllUsesExcept(
-          result, reshapes->collapseShapeOp.getResult(), ignoreUses);
+      auto replaceIf = [&](OpOperand &use) {
+        Operation *user = use.getOwner();
+        if (user == reshapes->expandShapeOp) {
+          return false;
+        }
+        if (isa<tensor::DimOp>(user)) {
+          return false;
+        }
+        return true;
+      };
+      rewriter.replaceUsesWithIf(result, reshapes->collapseShapeOp.getResult(),
+                                 replaceIf);
     }
   }
   return success();
