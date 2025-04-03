65e954bf2ea86c9bbcc3c87b0e67546a16b396cc
Ean Garvey <87458719+monorimet@users.noreply.github.com>
2025-04-03 11:39:30 -0500

    Update int8 fp16 punet output.

--- experimental/regression_suite/shark-test-suite-models/sdxl/test_unet.py
+++ experimental/regression_suite/shark-test-suite-models/sdxl/test_unet.py
@@ -137,7 +137,7 @@ sdxl_punet_int8_inference_input_5 = fetch_source_fixture(
 )
 
 sdxl_punet_int8_fp16_inference_output_0 = fetch_source_fixture(
-    "https://sharkpublic.blob.core.windows.net/sharkpublic/sai/sdxl-punet/11-13-2024/punet_fp16_out.0.bin",
+    "https://sharkpublic.blob.core.windows.net/sharkpublic/sdxl/03142025/IO/punet_fp16_out.0.bin",
     group="sdxl_punet_int8_fp16",
 )
 
