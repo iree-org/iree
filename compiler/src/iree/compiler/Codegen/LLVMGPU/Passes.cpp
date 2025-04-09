640560833ada8f133a8e6a699cd54d387a7ee1f4
Quinn Dawkins <quinn@nod-labs.com>
2025-04-08 21:06:17 -0400

    [Codegen] Add op for swizzling memory accesses
    This patch introduces `iree_codegen.swizzle_hint` op which enables
    swizzling accesses to a 1-d memref for better access patterns that are
    difficult to represent at a higher level. The use case today is for
    rotating accesses to shared memory to avoid bank conflicts.
    
    This is designed as a generic op that takes an attribute interface to
    make it easier to implement new swizzling strategies in the future for
    new/different hardware and/or codegen strategies. Also by operating like
    a view, this op still works with other transformations like
    packing/reusing shared memory.

--- compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
@@ -1085,7 +1085,10 @@ static void addLowerToLLVMGPUPasses(OpPassManager &modulePassManager,
   FunctionLikeNest funcPassManager(modulePassManager);
   funcPassManager.addPass(createFoldTensorExtractOpPass)
       .addPass(createLLVMGPUVectorLoweringPass)
-      .addPass(createExpandGPUOpsPass);
+      .addPass(createExpandGPUOpsPass)
+      .addPass(createResolveSwizzleHintsPass)
+      .addPass(createCanonicalizerPass)
+      .addPass(createCSEPass);
 
   // This pass needs to run before SCF -> CF.
   addLowerAndOptimizeAddressComputationPasses(funcPassManager);
