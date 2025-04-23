69da81c391ae1d84f598d97e5cc801754748a34c
Kunwar Grover <groverkss@gmail.com>
2025-04-23 20:12:53 +0200

    [VectorExt] Add generic vectorization to iree_vector_ext.transfer_gather (#20476)
    This patch adds support to GenericVectorization to vectorize some
    special gather-like generic operations to transfer_gather. This support
    is added behind a flag and should not affect any existing pipelines.

---
+++ compiler/src/iree/compiler/Codegen/Dialect/VectorExt/Transforms/Transforms.h
@@ -0,0 +1,25 @@
+// Copyright 2025 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef IREE_COMPILER_CODEGEN_DIALECT_VECTOR_EXT_TRANSFORMS_TRANSFORMS_H_
+#define IREE_COMPILER_CODEGEN_DIALECT_VECTOR_EXT_TRANSFORMS_TRANSFORMS_H_
+
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+
+namespace mlir::iree_compiler::IREE::VectorExt {
+
+LogicalResult vectorizeGatherLikeGenericToTransferGather(
+    RewriterBase &rewriter, linalg::GenericOp linalgOp,
+    ArrayRef<int64_t> vectorSizes = {}, ArrayRef<bool> scalableVecDims = {},
+    bool vectorizeNDExtract = false);
+
+void populateVectorTransferGatherLoweringPatterns(RewritePatternSet &patterns);
+
+}; // namespace mlir::iree_compiler::IREE::VectorExt
+
+#endif // IREE_COMPILER_CODEGEN_DIALECT_VECTOR_EXT_TRANSFORMS_TRANSFORMS_H_
