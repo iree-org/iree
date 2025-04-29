0bba1ea4da1c49f9ccb62e7ea6d8bd97ded8f239
Vivek Agrawal <197589114+amd-vivekag@users.noreply.github.com>
2025-04-29 21:19:01 +0530

    Raise ValueError when output is not expected (#20575)
    Raising ValueError when benchmark output is not expected. This will help
    in debugging the issue faster because of better details in the error
    message.
    
    ---------
    
    Signed-off-by: Vivek Agrawal <197589114+amd-vivekag@users.noreply.github.com>

--- runtime/bindings/python/iree/runtime/benchmark.py
+++ runtime/bindings/python/iree/runtime/benchmark.py
@@ -126,6 +126,11 @@ def benchmark_module(module, entry_function=None, inputs=[], timeout=None, **kwa
         split = line.split()
         if len(split) == 0:
             continue
+        elif len(split) <= 5:
+            raise ValueError(
+                f"Benchmark standard output is not expected. Line: '{line}' should have at least 6 columns.\nDetails: {args=},\n{out=}"
+            )
+
         benchmark_name = split[0]
         time = " ".join(split[1:3])
         cpu_time = " ".join(split[3:5])
