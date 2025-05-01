906ad85981636075e1e1303122cfcc4f6f7a9347
Muzammiluddin Syed <muzasyed@amd.com>
2025-05-01 14:03:47 -0500

    Replacing option with function to check backend target
    Signed-off-by: Muzammiluddin Syed <muzasyed@amd.com>

--- compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
@@ -1137,7 +1137,7 @@ static void addLowerToLLVMGPUPasses(OpPassManager &modulePassManager,
   FunctionLikeNest funcPassManager(modulePassManager);
   funcPassManager.addPass(createFoldTensorExtractOpPass)
       .addPass(createLLVMGPUVectorLoweringPass)
-      .addPass([&]() { return createExpandGPUOpsPass(forROCDL); })
+      .addPass(createExpandGPUOpsPass)
       // Barrier elimination before we reach unstructured control flow.
       .addPass(createGpuEliminateBarriers);
 
