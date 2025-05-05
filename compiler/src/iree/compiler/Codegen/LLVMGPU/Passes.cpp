94944392bcaed8dd75438368eb526b855aab2429
Alan Li <me@alanli.org>
2025-05-05 15:12:21 +0000

    Move LowerToGlobalLoads to earlier to avoid linalg.copy being dismantled

--- compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
@@ -369,6 +369,10 @@ static void addGPUBufferizePasses(OpPassManager &funcPassManager) {
   };
   funcPassManager.addPass(
       createIREEComprehensiveBufferizePass(allocationFn, memcpyFn));
+
+  // Convert linalg.copy to direct loads. This has to be before any canonicalization.
+  funcPassManager.addPass(createGPULowerToGlobalLoadsPass());
+
   addIREEPostBufferizationPasses(funcPassManager);
 
   funcPassManager.addPass(createROCDLBufferInstructionsOptimizationPass());
@@ -530,8 +534,6 @@ void addGPUTileAndFusePassPipeline(OpPassManager &funcPassManager,
   funcPassManager.addPass(createGPUVerifyDistributionPass());
   funcPassManager.addPass(createGPUDistributeForallPass());
 
-  // Convert linalg.copy to direct loads
-  funcPassManager.addPass(createGPULowerToGlobalLoadsPass());
 
   // Vectorize copies that came out of bufferization.
   funcPassManager.addPass(createVectorizeMemrefCopyPass());
