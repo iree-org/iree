c721f30bc363352d2ca7b4705df27b47ae5349bf
Muzammiluddin Syed <muzasyed@amd.com>
2025-05-14 11:19:18 +0000

    integration changes to iree
    Signed-off-by: Muzammiluddin Syed <muzasyed@amd.com>

--- compiler/src/iree/compiler/Codegen/SPIRV/SPIRVInitialVectorLowering.cpp
+++ compiler/src/iree/compiler/Codegen/SPIRV/SPIRVInitialVectorLowering.cpp
@@ -84,9 +84,8 @@ SmallVector<int64_t> getNativeVectorShapeImpl(VectorTransferOpInterface op) {
        llvm::enumerate(op.getPermutationMap().getResults())) {
     if (auto dimExpr = dyn_cast<AffineDimExpr>(dim)) {
       if (dimExpr.getPosition() == op.getPermutationMap().getNumDims() - 1) {
-        nativeSize[index] =
-            getMemoryVectorSize(op.getSource(), vecType.getElementType(),
-                                vecType.getShape()[index]);
+        nativeSize[index] = getMemoryVectorSize(
+            op.getBase(), vecType.getElementType(), vecType.getShape()[index]);
       }
     }
   }
