d98172962fa767bfd04b02506f6370a54c6086aa
Muzammil <55665739+Muzammiluddin-Syed-ECE@users.noreply.github.com>
2025-05-16 17:46:33 -0400

    [Codegen][GPU] Add support for allocating private memory for unused DPS results (#20793)
    Fixes https://github.com/iree-org/iree/issues/20699
    
    Fixes bufferization failures caused by unused results of DPS ops. Adds a
    `tensor_alloc` copy in private space to prevent allocation in global
    space during bufferization.
    
    ---------
    
    Signed-off-by: Muzammiluddin Syed <muzasyed@amd.com>

--- compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
@@ -354,6 +354,7 @@ static void addGPUBufferizePasses(OpPassManager &funcPassManager) {
   funcPassManager.addPass(createGPUInferMemorySpacePass());
   funcPassManager.addPass(createROCDLConfigureBufferInstructionsPass());
   funcPassManager.addPass(createGPUBubbleResourceCastsPass());
+  funcPassManager.addPass(createGPUAllocPrivateMemoryForDPSOpsPass());
   BufferizationOptions::AllocationFn allocationFn =
       gpuRequireMemSpaceAllocationFn;
   BufferizationOptions::MemCpyFn memcpyFn = [](OpBuilder &builder, Location loc,
