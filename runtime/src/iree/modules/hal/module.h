bbfc44f9b21566c2e2ac050c18b9a6afc5ab9e0f
Ben Vanik <ben.vanik@gmail.com>
2025-05-20 14:17:13 -0700

    Adding `hal.allocator.select` runtime implementation. The `--device_lead_allocator=` flag can be used in tooling to specify a device that should be preferred as the allocator when a selection is requested. If the device isn't in the list the first will be returned. For simple cases of CPU + some accelerator this should work and more complex situations will need something more designed.
    Fixes #20857.

--- runtime/src/iree/modules/hal/module.h
+++ runtime/src/iree/modules/hal/module.h
@@ -19,6 +19,42 @@
 extern "C" {
 #endif  // __cplusplus
 
+//===----------------------------------------------------------------------===//
+// iree_hal_module_device_policy_t
+//===----------------------------------------------------------------------===//
+
+typedef uint64_t iree_hal_module_device_allocator_select_flags_t;
+typedef struct iree_hal_device_queue_affinity_pair_t {
+  iree_hal_device_t* device;
+  iree_hal_queue_affinity_t queue_affinity;
+} iree_hal_device_queue_affinity_pair_t;
+typedef iree_status_t(
+    IREE_API_PTR* iree_hal_module_device_allocator_select_callback_fn_t)(
+    void* user_data, iree_host_size_t device_count,
+    const iree_hal_device_queue_affinity_pair_t* devices,
+    iree_hal_memory_type_t memory_types, iree_hal_buffer_usage_t buffer_usage,
+    iree_hal_module_device_allocator_select_flags_t flags,
+    iree_host_size_t* out_selection);
+
+typedef struct iree_hal_module_device_allocator_select_callback_t {
+  iree_hal_module_device_allocator_select_callback_fn_t fn;
+  void* user_data;
+} iree_hal_module_device_allocator_select_callback_t;
+
+typedef struct iree_hal_module_device_policy_t {
+  iree_hal_module_device_allocator_select_callback_t allocator_select;
+} iree_hal_module_device_policy_t;
+
+// Returns a default device policy that chooses defaults deterministically.
+// For queries that must select amongst multiple options the first is always
+// chosen.
+IREE_API_EXPORT iree_hal_module_device_policy_t
+iree_hal_module_device_policy_default(void);
+
+//===----------------------------------------------------------------------===//
+// iree_hal_module_t
+//===----------------------------------------------------------------------===//
+
 enum iree_hal_module_flag_bits_t {
   IREE_HAL_MODULE_FLAG_NONE = 0u,
 
@@ -31,10 +67,10 @@ typedef uint32_t iree_hal_module_flags_t;
 // Each context using this module will share the devices and have compatible
 // allocations.
 IREE_API_EXPORT iree_status_t iree_hal_module_create(
-    iree_vm_instance_t* instance, iree_host_size_t device_count,
-    iree_hal_device_t** devices, iree_hal_module_flags_t flags,
-    iree_hal_module_debug_sink_t debug_sink, iree_allocator_t host_allocator,
-    iree_vm_module_t** out_module);
+    iree_vm_instance_t* instance, iree_hal_module_device_policy_t device_policy,
+    iree_host_size_t device_count, iree_hal_device_t** devices,
+    iree_hal_module_flags_t flags, iree_hal_module_debug_sink_t debug_sink,
+    iree_allocator_t host_allocator, iree_vm_module_t** out_module);
 
 // Returns the total number of available devices registered with the HAL module.
 IREE_API_EXPORT iree_host_size_t
