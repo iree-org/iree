1392bf8618d3ca18a35ccdcac43112e0a5df063c
Krzysztof Drewniak <Krzysztof.Drewniak@amd.com>
2025-05-20 18:41:55 +0000

    [AMDGPU] Rewrite some gpu.shuffle xor to ds_swizzle, per upstream
    This incerporates an @hc84 pattern that got added upstream into our
    lowering, mirroring changset to LowerGPUOpsToRocdl.cpp .

--- compiler/src/iree/compiler/Codegen/LLVMGPU/ConvertToROCDL.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/ConvertToROCDL.cpp
@@ -282,6 +282,7 @@ struct ConvertToROCDLPass final
     {
       RewritePatternSet patterns(&getContext());
       populateGpuRewritePatterns(patterns);
+      populateGpuPromoteShuffleToAMDGPUPatterns(patterns);
       if (failed(applyPatternsGreedily(m, std::move(patterns)))) {
         return signalPassFailure();
       }
