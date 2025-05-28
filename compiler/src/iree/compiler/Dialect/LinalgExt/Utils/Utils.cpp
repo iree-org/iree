df08be6682c409f635fa786fcd298c86bc7dfe14
Bangtian Liu <liubangtian@gmail.com>
2025-05-27 19:45:05 -0700

    [Codegen] split-k on argmax to ensure ukernel support
    Signed-off-by: Bangtian Liu <liubangtian@gmail.com>

--- compiler/src/iree/compiler/Dialect/LinalgExt/Utils/Utils.cpp
+++ compiler/src/iree/compiler/Dialect/LinalgExt/Utils/Utils.cpp
@@ -891,6 +891,15 @@ bool isArgmaxOp(linalg::GenericOp genericOp) {
     if (!matchPattern(producer, m_Op<arith::SelectOp>())) {
       return false;
     }
+    auto selectOp = dyn_cast<arith::SelectOp>(producerOutput.getDefiningOp());
+    Value trueVal = selectOp.getTrueValue();
+    if (auto castOp = trueVal.getDefiningOp<arith::IndexCastOp>())
+      trueVal = castOp.getIn();
+
+    // Ensure the true value is directly produced by linalg.index.
+    auto indexOp = trueVal.getDefiningOp<linalg::IndexOp>();
+    if (!indexOp)
+      return false;
   }
 
   // Producer of arith.select op is arith.cmpf
