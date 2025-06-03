c7a9eb3e4ac713068cfc0b36b6f96fbb72fa3b5d
default <ziereis@roofline.ai>
2025-06-03 13:55:41 +0000

    init
    Signed-off-by: default <ziereis@roofline.ai>

--- compiler/src/iree/compiler/Modules/HAL/Inline/Conversion/HALToHALInline/Patterns.cpp
+++ compiler/src/iree/compiler/Modules/HAL/Inline/Conversion/HALToHALInline/Patterns.cpp
@@ -50,6 +50,36 @@ struct EncodingTypeOpConversion
   }
 };
 
+struct MemoryTypeOpConversion
+    : public OpConversionPattern<IREE::HAL::MemoryTypeOp> {
+  using OpConversionPattern<IREE::HAL::MemoryTypeOp>::OpConversionPattern;
+  LogicalResult
+  matchAndRewrite(IREE::HAL::MemoryTypeOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto value = IREE::HAL::MemoryTypeOp::getTypeValue(op.getResult());
+    if (!value.has_value())
+      return rewriter.notifyMatchFailure(op.getLoc(),
+                                         "unsupported memory type");
+    rewriter.replaceOpWithNewOp<arith::ConstantIntOp>(op, value.value(), 32);
+    return success();
+  }
+};
+
+struct BufferUsageOpConversion
+    : public OpConversionPattern<IREE::HAL::BufferUsageOp> {
+  using OpConversionPattern<IREE::HAL::BufferUsageOp>::OpConversionPattern;
+  LogicalResult
+  matchAndRewrite(IREE::HAL::BufferUsageOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto value = IREE::HAL::BufferUsageOp::getUsageValue(op.getResult());
+    if (!value.has_value())
+      return rewriter.notifyMatchFailure(op.getLoc(),
+                                         "unsupported buffer usage");
+    rewriter.replaceOpWithNewOp<arith::ConstantIntOp>(op, value.value(), 32);
+    return success();
+  }
+};
+
 struct BufferSubspanOpPattern
     : public OpConversionPattern<IREE::HAL::BufferSubspanOp> {
   using OpConversionPattern::OpConversionPattern;
@@ -250,6 +280,8 @@ void populateHALToHALInlinePatterns(MLIRContext *context,
 
   patterns.insert<ElementTypeOpConversion>(context);
   patterns.insert<EncodingTypeOpConversion>(context);
+  patterns.insert<MemoryTypeOpConversion>(context);
+  patterns.insert<BufferUsageOpConversion>(context);
 
   patterns.insert<BufferViewCreateOpPattern>(typeConverter, context);
   patterns.insert<BufferViewAssertOpPattern>(typeConverter, context);
