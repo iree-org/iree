c9b0228ce2cf31c9de342e5bc90022d4c64713f1
adeel10x <adeel.ahmad@10xengineers.ai>
2025-06-06 03:24:26 +0500

    Enable compilation of riscv_64 ukernels using system toolchains, add riscv_64 matmul ukernel tile size querying function, and use suffix 'v' for V extension ukernels
    V, the standard vector extension for application processors is not included in the base ISA(https://five-embeddev.com/riscv-user-isa-manual/Priv-v1.12/intro.html#risc-v-isa-overview). So, suffix 'base' is replaced by 'v'
    
    Signed-off-by: adeel10x <adeel.ahmad@10xengineers.ai>

---
+++ runtime/src/iree/builtins/ukernel/arch/riscv_64/unpack_riscv_64_internal.h
@@ -0,0 +1,14 @@
+// Copyright 2025 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef IREE_BUILTINS_UKERNEL_ARCH_RISCV_64_UNPACK_RISCV_64_INTERNAL_H_
+#define IREE_BUILTINS_UKERNEL_ARCH_RISCV_64_UNPACK_RISCV_64_INTERNAL_H_
+
+#include "iree/builtins/ukernel/unpack_internal.h"
+
+// Placeholder for now
+
+#endif  // IREE_BUILTINS_UKERNEL_ARCH_RISCV_64_UNPACK_RISCV_64_INTERNAL_H_
