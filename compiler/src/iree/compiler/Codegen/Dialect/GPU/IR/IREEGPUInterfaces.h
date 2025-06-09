ba5f925e5d55ac3e422ef43dbc59819649c58b7c
Krzysztof Drewniak <Krzysztof.Drewniak@amd.com>
2025-06-09 08:17:31 -0700

    [Codegen] Generalize MultiMmaInterfaceAttr to InnerTileDescAttrInterface (#21000)
    This commit is phase 1 of refactoring iree_gpu.multi_mma to take
    arbitrarily many operands (and get renamed to
    `iree_codegen.inner_tiled`).
    
    In this commit, we introduce InnerTileDescAttrInterface, which becasem a
    parent of MultiMmaInterfaceAttr and has versions of most of its methods
    moved to it.
    
    The main changes in the migration are:
    
    1. Instead of returning M, N, and K values, the new interface returns
    the VectorType of the pre-distribution inner tile, which can be used to
    recover the M, N, and K values for a matrix multiply (though, in most
    cases, it's just the number of elements in those tiles that actually
    mattered) using getUndistributedTileTypes(), which populates a vector of
    results.
    2. getABCVectorTypes() becomes getDistributedTileTypes(), which
    populates a vector.
    3. getSubgroupSize() becomes optional just in case this all ends up on
    CPU.
    4. MMAScope gets moved to the Codegen dialect and renamed
    InnerTileScope.
    5. InnerTileDescAttrInterface gets a method to indicate the expected
    number of input arguments for a paritular inner tiled operation. It also
    allows for intrinsic-specific indexing map verefication (since
    non-matmuls may fail contraction map inference).
    6. populateOperandOffsetSizesAndStrides() now takes an integer index and
    not an MMA fragment.
    
    The verifier for multi_mma op has been rewritten to not mention "lhs",
    "rhs", and "acc" where possible.
    
    A future commit will actually make the input arguments (and
    permutations) variadic and do the big pile of code motion into the
    codegen dialect, but this seemed like an independently reviewable piece
    of code.
    
    ---------
    
    Co-authored-by: Quinn Dawkins <quinn.dawkins@gmail.com>

--- compiler/src/iree/compiler/Codegen/Dialect/GPU/IR/IREEGPUInterfaces.h
+++ compiler/src/iree/compiler/Codegen/Dialect/GPU/IR/IREEGPUInterfaces.h
@@ -7,6 +7,7 @@
 #ifndef IREE_COMPILER_CODEGEN_DIALECT_GPU_IREEGPUINTERFACES_H_
 #define IREE_COMPILER_CODEGEN_DIALECT_GPU_IREEGPUINTERFACES_H_
 
+#include "iree/compiler/Codegen/Dialect/Codegen/IR/IREECodegenInterfaces.h"
 #include "iree/compiler/Codegen/Dialect/GPU/IR/IREEGPUEnums.h"
 #include "iree/compiler/Codegen/Dialect/VectorExt/IR/VectorExtInterfaces.h"
 #include "mlir/Dialect/Vector/IR/VectorOps.h"
