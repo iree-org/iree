684110f673843605cd06e969415db6057c65d9d6
Max Dawkins <max.dawkins@gmail.com>
2025-06-09 01:37:36 +0000

    [LLVMGPU] Add early bufferization pass to configuration pipieline
    Signed-off-by: Max Dawkins <max.dawkins@gmail.com>

--- compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
@@ -1226,6 +1226,7 @@ static void buildLLVMGPUCodegenConfigurationPassPipelineImpl(
     addCommonTargetExecutablePreprocessingPasses(funcPassManager);
     // This materializes into 'nop' in the absence of pad encoding layout
     // attributes.
+    funcPassManager.addPass(createBufferizeDispatchTensorLoadStorePass);
     funcPassManager.addPass(createBlockDynamicDimensionsPass);
     funcPassManager.addPass(createConfigTrackingCanonicalizerPass);
     funcPassManager.addPass(createCSEPass);
