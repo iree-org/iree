2315ea04b4810fe5b41202d9bab983381e45cd99
Kunwar Grover <groverkss@gmail.com>
2025-06-18 17:53:56 +0100

    remove more code

--- compiler/src/iree/compiler/Codegen/Common/GPU/GPUNestedLayoutDistributionPatterns.cpp
+++ compiler/src/iree/compiler/Codegen/Common/GPU/GPUNestedLayoutDistributionPatterns.cpp
@@ -38,54 +38,6 @@ static bool isBroadcast(AffineExpr expr) {
   return false;
 }
 
-/// Given a set of base transfer |indices|, |offsets| for the batch/outer
-/// dimensions, and distributed warp and thread indices, computes the indices
-/// of the distributed transfer operation based on the |vectorLayout|.
-static SmallVector<Value> getTransferIndicesFromNestedLayout(
-    OpBuilder &b, ValueRange indices, ArrayRef<int64_t> offsets,
-    NestedLayoutAttr vectorLayout, AffineMap permutationMap,
-    ArrayRef<Value> warpIndices, ArrayRef<Value> threadIndices) {
-
-  int64_t rank = vectorLayout.getRank();
-  // Permute the batch and outer vector offsets to match the order of
-  // the vector dimensions using the inverse of the batch/offset order.
-  ArrayRef<int64_t> batchOffsets(offsets.begin(), rank);
-  ArrayRef<int64_t> outerVectorOffsets(offsets.begin() + rank, rank);
-
-  SmallVector<Value> slicedIndices(indices.begin(), indices.end());
-  for (const auto &[i, dim] : llvm::enumerate(permutationMap.getResults())) {
-    // Broadcasted dimension offsets can be used as-is; the read index is
-    // invariant of the thread in such cases (and illegal for writes).
-    if (isBroadcast(dim)) {
-      continue;
-    }
-    unsigned pos = cast<AffineDimExpr>(dim).getPosition();
-    Value offset = indices[pos];
-    int64_t elementCount = vectorLayout.getElementTile()[i];
-    Location loc = offset.getLoc();
-    SmallVector<Value> ids = {
-        warpIndices[i], b.create<arith::ConstantIndexOp>(loc, batchOffsets[i]),
-        b.create<arith::ConstantIndexOp>(loc, outerVectorOffsets[i]),
-        threadIndices[i], offset};
-    // The order in which a vector dimension is "tiled" is
-    // subgroups -> batches -> outer vectors -> threads -> elements
-    SmallVector<int64_t> sizes = {
-        vectorLayout.getSubgroupTile()[i], vectorLayout.getBatchTile()[i],
-        vectorLayout.getOuterTile()[i], vectorLayout.getThreadTile()[i],
-        elementCount};
-    // The offset is often not an offset within `elementCount`, so, in general,
-    // we can't mark this `disjoint`. However, if `offset` is known to be
-    // a constant less than `elementCount`, we can do this, unlocking
-    // potential optimizations.
-    bool disjoint = false;
-    if (std::optional<int64_t> offsetConst = getConstantIntValue(offset))
-      disjoint = *offsetConst < elementCount;
-    slicedIndices[pos] =
-        b.create<affine::AffineLinearizeIndexOp>(loc, ids, sizes, disjoint);
-  }
-  return slicedIndices;
-}
-
 static SmallVector<int64_t>
 getDistributedTransferOffsetsFromNestedLayout(ArrayRef<int64_t> offsets,
                                               NestedLayoutAttr vectorLayout) {
@@ -104,19 +56,6 @@ getDistributedTransferOffsetsFromNestedLayout(ArrayRef<int64_t> offsets,
                             outerOffset * elementSize);
   }
   return slicedOffsets;
-}
-
-static SmallVector<int64_t>
-getElementVectorTileShape(NestedLayoutAttr vectorLayout) {
-  int64_t rank = vectorLayout.getRank();
-  SmallVector<int64_t> tileShape = vectorLayout.getDistributedShape();
-  // We tile to a vector with BATCH, OUTER, and ELEMENT dimensions. So to access
-  // the subvector only containing elements, we need indices in all BATCH and
-  // OUTER (rank * 2) dimensions to have tile size 1.
-  for (int i = 0, e = rank * 2; i < e; ++i) {
-    tileShape[i] = 1;
-  }
-  return tileShape;
 }
 
 /// Given a distributed vector that has B1XB2xO1XO2xE1XE2,
@@ -437,7 +376,7 @@ private:
   int64_t subgroupSize;
 };
 
-struct DistributeTransferReadV3 final : DistributeXfer<vector::TransferReadOp> {
+struct DistributeTransferRead final : DistributeXfer<vector::TransferReadOp> {
   using DistributeXfer::DistributeXfer;
 
   void replaceXferOp(vector::TransferReadOp xferOp, Value expandedBase,
@@ -458,8 +397,7 @@ struct DistributeTransferReadV3 final : DistributeXfer<vector::TransferReadOp> {
   }
 };
 
-struct DistributeTransferWriteV3 final
-    : DistributeXfer<vector::TransferWriteOp> {
+struct DistributeTransferWrite final : DistributeXfer<vector::TransferWriteOp> {
   using DistributeXfer::DistributeXfer;
 
   void replaceXferOp(vector::TransferWriteOp xferOp, Value expandedBase,
@@ -1793,7 +1731,7 @@ void populateGPUDistributeNestedLayoutAttrPatterns(RewritePatternSet &patterns,
                                                    Value threadId,
                                                    int64_t subgroupSize,
                                                    int64_t maxBitsPerShuffle) {
-  patterns.add<DistributeTransferReadV3, DistributeTransferWriteV3>(
+  patterns.add<DistributeTransferRead, DistributeTransferWrite>(
       patterns.getContext(), threadId, subgroupSize);
   patterns.add<DistributeBroadcast, DistributeTranspose>(patterns.getContext());
   patterns.add<DistributeMultiReduction>(patterns.getContext(), subgroupSize,
