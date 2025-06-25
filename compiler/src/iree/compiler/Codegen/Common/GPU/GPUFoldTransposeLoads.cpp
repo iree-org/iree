c2afca899c979d2ede831cf6b337ee206c608e2f
Alan Li <me@alanli.org>
2025-06-25 01:57:13 +0000

    Update

--- compiler/src/iree/compiler/Codegen/Common/GPU/GPUFoldTransposeLoads.cpp
+++ compiler/src/iree/compiler/Codegen/Common/GPU/GPUFoldTransposeLoads.cpp
@@ -5,7 +5,6 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <cstdint>
-#include <numeric>
 #include <optional>
 #include "iree/compiler/Codegen/Common/GPU/Passes.h"
 #include "iree/compiler/Codegen/Dialect/Codegen/IR/IREECodegenAttrs.h"
@@ -14,16 +13,17 @@
 #include "iree/compiler/Codegen/Utils/GPUUtils.h"
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/Support/Debug.h"
+#include "llvm/Support/LogicalResult.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Arith/Utils/Utils.h"
-#include "mlir/Dialect/GPU/IR/GPUDialect.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Dialect/MemRef/Utils/MemRefUtils.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/Dialect/SCF/Utils/Utils.h"
 #include "mlir/Dialect/Vector/IR/VectorOps.h"
 #include "mlir/Dialect/Vector/Transforms/VectorTransforms.h"
+#include "mlir/Support/LLVM.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
 #define DEBUG_TYPE "iree-codegen-gpu-folds-to-transpose-loads"
@@ -34,32 +34,82 @@ namespace mlir::iree_compiler {
 #define GEN_PASS_DEF_GPUFOLDTRANSPOSELOADSPASS
 #include "iree/compiler/Codegen/Common/GPU/Passes.h.inc"
 
-template <typename T>
-struct SubgroupTransformPatternBase : public OpRewritePattern<T> {
-  SubgroupTransformPatternBase(MLIRContext *context,
-                               ArrayRef<int64_t> workgroupSize,
-                               int64_t subgroupSize)
-      : OpRewritePattern<T>(context), workgroupSize(workgroupSize),
-        subgroupSize(subgroupSize) {}
+struct FoldTransposeAndLoadPattern
+    : public OpRewritePattern<vector::TransposeOp> {
+
+  FoldTransposeAndLoadPattern(MLIRContext *context,
+                              ArrayRef<int64_t> workgroupSize,
+                              int64_t subgroupSize)
+      : OpRewritePattern<vector::TransposeOp>(context),
+        workgroupSize(workgroupSize), subgroupSize(subgroupSize) {}
+
+  LogicalResult matchAndRewrite(vector::TransposeOp transposeOp,
+                                PatternRewriter &rewriter) const override {
+    LDBG("==== folding transpose and load: ");
+
+    return llvm::failure();
+  }
+
+  LogicalResult
+  isTransferReadTransposeChain(vector::TransposeOp transposeOp) const {
+    Value sourceVector = transposeOp.getVector();
+    if (!isa<vector::TransferReadOp>(sourceVector.getDefiningOp())) {
+      return failure();
+    }
+    if (!sourceVector.hasOneUse()) {
+      return failure();
+    }
+    return success();
+  }
+
+  // TODO: mfma vectors have more than 2 dimensions.
+  LogicalResult
+  isColMajorToRowMajorTranspose(vector::TransposeOp transposeOp) const {
+    auto perm = transposeOp.getPermutation();
+    if (perm.size() != 2) {
+      return failure();
+    }
+    // Check if the transpose is from column-major to row-major.
+    if (!(perm[0] == 1 && perm[1] == 0)) {
+      return failure();
+    }
+    return success();
+  }
+
+  LogicalResult isInsideValidForallOp(vector::TransposeOp transposeOp) const {
+    auto parentOp = transposeOp->getParentOfType<scf::ForallOp>();
+    if (!parentOp) {
+      return failure();
+    }
+    // get the loop count from the parent forall op.
+    auto lb = parentOp.getStaticLowerBound();
+    auto ub = parentOp.getStaticUpperBound();
+    auto step = parentOp.getStaticStep();
+
+    // TODO: enable multiple dimension scf.forall ops.
+    if (lb.size() != 1) {
+      return failure();
+    }
+    if (step.front() != 1) {
+      return failure();
+    }
+    if (lb.front() != 0) {
+      return failure();
+    }
+
+    size_t loopCount = ub.front() - lb.front();
+
+    // For now, loop count should be a multiple of the subgroup size.
+    if (loopCount % subgroupSize != 0) {
+      return failure();
+    }
+
+    return success();
+  }
 
 protected:
   ArrayRef<int64_t> workgroupSize;
   int64_t subgroupSize;
-};
-
-struct CombineVectorTransposeWithTransferReadOpPattern
-    : SubgroupTransformPatternBase<vector::TransposeOp> {
-  CombineVectorTransposeWithTransferReadOpPattern(
-      MLIRContext *context, ArrayRef<int64_t> workgroupSize,
-      int64_t subgroupSize)
-      : SubgroupTransformPatternBase<vector::TransposeOp>(
-            context, workgroupSize, subgroupSize) {}
-  LogicalResult matchAndRewrite(vector::TransposeOp op,
-                                PatternRewriter &rewriter) const override {
-    return rewriter.notifyMatchFailure(
-        op,
-        "Transpose ops are not supported on MMA types, use mma.load instead.");
-  }
 };
 
 namespace {
@@ -85,8 +135,8 @@ struct GPUFoldTransposeLoadsPass final
     }
 
     RewritePatternSet patterns(context);
-    patterns.add<CombineVectorTransposeWithTransferReadOpPattern>(
-        context, *workgroupSize, *subgroupSize);
+    patterns.add<FoldTransposeAndLoadPattern>(context, *workgroupSize,
+                                              *subgroupSize);
     (void)applyPatternsGreedily(funcOp, std::move(patterns));
   }
 };
