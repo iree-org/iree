29c3d002a85ca287ddb6252d1b938e1ec06bf1b0
jerryyin <zhuoryin@amd.com>
2025-06-27 13:30:34 +0000

    Address review feedbacks
    Signed-off-by: jerryyin <zhuoryin@amd.com>

--- compiler/src/iree/compiler/Codegen/Common/FissionTransferOpsInControlFlow.cpp
+++ compiler/src/iree/compiler/Codegen/Common/FissionTransferOpsInControlFlow.cpp
@@ -233,7 +233,7 @@ struct FissionTarget {
 static FailureOr<FissionTarget> populateFissionTarget(scf::ForOp forOp) {
   // Fission Loop always has a transfer_write as the last operation.
   auto lastOp = forOp.getBody()->getTerminator()->getPrevNode();
-  if (lastOp == nullptr || !isa<vector::TransferWriteOp>(lastOp)) {
+  if (!isa_and_present<vector::TransferWriteOp>(lastOp)) {
     return failure();
   }
 
