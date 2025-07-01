2ec95a30ece43bdffff3ccea8d2de1866a7db348
MaheshRavishankar <mahesh.ravishankar@gmail.com>
2025-07-01 08:41:06 -0700

    [Codegen] Resolve `scf.forall` operations created during split-reduction.
    The `FormSplitReductionDispatches` forms dispatches containing
    `scf.forall` that represent the parallel partial reductions. These
    `scf.forall` have the
    `#iree_linalg_ext.split_reduction_mapping`. These loops exist in
    conjunction with the loops that distribute the partial reductions by
    themselves within the workgroup.
    
    The resolution for this involves taking the workgroup mapping that was
    decided by the distribution of the partial reduction op by itself and
    multiplying the highest dimension along which the distribution is done
    by the number of workgroups needed for executing the partial reduction
    in parallel.
    
    With this commit the following example can compile end-to-end
    
    ```
    util.func @sample_test(%arg0 : tensor<?x131072xf32>) -> tensor<?xf32> {
      %c0 = arith.constant 0 : index
      %c1 = arith.constant 1 : index
      %cst = arith.constant 0.0 : f32
      %d0 = tensor.dim %arg0, %c0 : tensor<?x131072xf32>
      %empty = tensor.empty(%d0) : tensor<?xf32>
      %fill = linalg.fill ins(%cst : f32)
          outs(%empty : tensor<?xf32>) -> tensor<?xf32>
      %0 = linalg.generic {
          indexing_maps = [affine_map<(d0, d1) -> (d0, d1)>,
                           affine_map<(d0, d1) -> (d0)>],
          iterator_types = ["parallel", "reduction"]}
          ins(%arg0 : tensor<?x131072xf32>) outs(%fill : tensor<?xf32>) {
        ^bb0(%b0 : f32, %b1 : f32) :
          %1 = arith.addf %b0, %b1 : f32
          linalg.yield %1 : f32
      } -> tensor<?xf32>
      util.return %0 : tensor<?xf32>
    }
    ```
    
    using the following command
    
    ```
    iree-compile --iree-hal-target-backends=rocm --iree-hip-target=gfx942 \
        --iree-dispatch-creation-splitk-by-tiling-size=128
        -o test.vmfb test.mlir
    ```
    
    Signed-off-by: MaheshRavishankar <mahesh.ravishankar@gmail.com>

--- compiler/src/iree/compiler/Codegen/Transforms/Transforms.h
+++ compiler/src/iree/compiler/Codegen/Transforms/Transforms.h
@@ -119,6 +119,10 @@ void analyseAllocsForPacking(mlir::FunctionOpInterface funcOp,
 void packAllocs(OpBuilder &builder, mlir::FunctionOpInterface funcOp,
                 ArrayRef<AliasGroup> aliasGroups);
 
+FailureOr<SmallVector<OpFoldResult>> materializeWorkgroupCountComputation(
+    RewriterBase &rewriter, mlir::FunctionOpInterface entryPointFn,
+    ArrayRef<OpFoldResult> workgroupCount, ValueRange workloadVals);
+
 /// Lower the workgroup count region for the default code-generation path in
 /// IREE. Given the list `workgroupCount` (fastest varying dimension innermost)
 /// as computed within the `entryPointFn`, clones a backward slice of the
@@ -128,7 +132,7 @@ void packAllocs(OpBuilder &builder, mlir::FunctionOpInterface funcOp,
 /// the `flow.dispatch.constant_ordinal` operations from within the
 /// `entryPointFn`. Expects the workgroup count region of the corresponding
 /// `hal.executable.export` to contain the
-/// `flow.dispatch.workgroup_count_default` operation as a placeholder for the
+/// `flow.dispatch.workgroup_count_slice` operation as a placeholder for the
 /// computation to compute the number of workgroups. In absence of this
 /// operation, this method does nothing assuming that the workgroup count
 /// computation has already been resolved.
