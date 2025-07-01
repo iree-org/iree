3a6202c692cd0ae234df3c313242ba2825ee4c15
Kunwar Grover <groverkss@gmail.com>
2025-07-01 13:12:26 +0100

    [VectorExt] Implement masked vectorization for iree_linalg_ext.gather (#21189)
    Implements masked vectorization for iree_linalg_ext.gather to
    iree_vector_ext.transfer_gather. The masked vector shape can be inferred
    from the dps result.
    
    Signed-off-by: keshavvinayak01 <keshavvinayakjha@gmail.com>

--- compiler/src/iree/compiler/Codegen/Utils/Utils.cpp
+++ compiler/src/iree/compiler/Codegen/Utils/Utils.cpp
@@ -1863,37 +1863,44 @@ std::optional<VectorizationTileSizes> inferSizesFromIR(linalg::UnPackOp op) {
   return result;
 }
 
+std::optional<VectorizationTileSizes> static inferSizesFromMixedSizes(
+    SmallVector<OpFoldResult> shape) {
+  VectorizationTileSizes result;
+  for (OpFoldResult dim : shape) {
+    LLVM_DEBUG(llvm::dbgs() << "Dim #" << dim << ": ");
+    FailureOr<int64_t> maybeDimBound =
+        ValueBoundsConstraintSet::computeConstantBound(
+            presburger::BoundType::UB, dim,
+            /*stopCondition=*/nullptr, /*closedUB=*/true);
+    if (failed(maybeDimBound)) {
+      LLVM_DEBUG(llvm::dbgs() << "failed\n");
+      return std::nullopt;
+    }
+
+    LLVM_DEBUG(llvm::dbgs() << maybeDimBound.value() << "\n");
+    result.vectorSizes.push_back(maybeDimBound.value());
+    result.destShape.push_back(maybeDimBound.value());
+  }
+  return result;
+}
+
 std::optional<VectorizationTileSizes> inferSizesFromIR(Value val) {
   if (!val.getDefiningOp())
     return std::nullopt;
 
   std::optional<VectorizationTileSizes> result;
+  LLVM_DEBUG(llvm::dbgs() << "Inferring sizes for:\n" << val << "\n");
   TypeSwitch<Operation *, void>(val.getDefiningOp())
       .Case<linalg::LinalgOp>(
           [&](auto op) { result = inferSizesFromIR(op, cast<OpResult>(val)); })
       .Case<linalg::PackOp>([&](auto op) { result = inferSizesFromIR(op); })
-      .Case<tensor::ExtractSliceOp>([&](tensor::ExtractSliceOp op) {
+      .Case<tensor::ExtractSliceOp, tensor::EmptyOp>([&](auto op) {
         // tensor::ExtractSliceOp is not vectorizable, so only `destShape` has
         // the values.
-        result = VectorizationTileSizes();
-        LLVM_DEBUG(llvm::dbgs() << "Inferring sizes for:\n" << op << "\n");
-        int64_t destRank = op.getResult().getType().getRank();
-        for (int dim = 0; dim < destRank; ++dim) {
-          LLVM_DEBUG(llvm::dbgs() << "Dim #" << dim << ": ");
-          FailureOr<int64_t> maybeDimBound =
-              ValueBoundsConstraintSet::computeConstantBound(
-                  presburger::BoundType::UB, {op, dim},
-                  /*stopCondition=*/nullptr, /*closedUB=*/true);
-          if (failed(maybeDimBound)) {
-            LLVM_DEBUG(llvm::dbgs() << "failed\n");
-            result = std::nullopt;
-            return;
-          }
-          LLVM_DEBUG(llvm::dbgs() << maybeDimBound.value() << "\n");
-          result->destShape.push_back(maybeDimBound.value());
-        }
+        result = inferSizesFromMixedSizes(op.getMixedSizes());
       })
       .Default([&](Operation *) {});
+
   return result;
 }
 
