bb878229cfdbef0ba594f4a126fb8af2928b73eb
jerryyin <zhuoryin@amd.com>
2025-07-02 18:54:28 +0000

    prototype promote c promotion

--- compiler/src/iree/compiler/Codegen/Dialect/GPU/TargetUtils/ConfigUtils.cpp
+++ compiler/src/iree/compiler/Codegen/Dialect/GPU/TargetUtils/ConfigUtils.cpp
@@ -336,7 +336,7 @@ getMatmulLoweringConfigAndWorkgroupSize(SmallVector<int64_t> bounds,
   if (!schedule && canSupportUnaligned) {
     LDBG("Attempting to deduce unaligned TileAndFuse MMA schedulee");
     mustBeAligned = false;
-    doCPromotion = true;
+    // doCPromotion = true;
     schedule = getMmaScheduleFromProblemAndTarget(target, problem,
                                                   transposedLhs, transposedRhs,
                                                   mustBeAligned, doCPromotion);
@@ -420,7 +420,8 @@ getMatmulLoweringConfigAndWorkgroupSize(SmallVector<int64_t> bounds,
   } else {
     // TODO (nirvedhmeshram, Max191, jerryyin) : Add support so that unaligned
     // shapes do not require c promotion.
-    GPU::appendPromotedOperandsList(context, attrs, {0, 1, 2});
+    // GPU::appendPromotedOperandsList(context, attrs, {0, 1, 2});
+    GPU::appendPromotedOperandsList(context, attrs, {0, 1});
     SmallVector<int64_t> paddingTileSizes = workgroupTileSizes;
 
     // Initialize inner and outer padding sizes from reductionTileSizes.
