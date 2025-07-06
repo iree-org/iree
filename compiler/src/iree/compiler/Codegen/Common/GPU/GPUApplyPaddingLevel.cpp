665fb3a728c830277ebe47b4bfcf0e8454f4b221
Nicolas Vasilache <Nico.Vasilache@amd.com>
2025-07-06 19:36:54 +0200

    Pad OnlineAttention (#21152)
    This PR makes OnlineAttention derive from IndexingMapOpInterface and
    make it pad with transform.structured.pad_tiling_interface.
    
    Additionally, ensures the dynamic case pads to a constant before tiling
    and properly canonicalizes to constant shapes
    once AffineMin simplification kicks in.
    
    This requires integrating LLVM past
    d31ba5256327d30f264c2f671bf197877b242cde.
    
    The integrate PR is separated in #21175
    
    ---------
    
    Signed-off-by: Nicolas Vasilache <nico.vasilache@amd.com>
    Signed-off-by: Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>
    Co-authored-by: Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>
    Co-authored-by: Kunwar Grover <groverkss@gmail.com>
    Signed-off-by: keshavvinayak01 <keshavvinayakjha@gmail.com>

--- compiler/src/iree/compiler/Codegen/Common/GPU/GPUApplyPaddingLevel.cpp
+++ compiler/src/iree/compiler/Codegen/Common/GPU/GPUApplyPaddingLevel.cpp
@@ -9,15 +9,27 @@
 #include "iree/compiler/Codegen/Dialect/Codegen/IR/IREECodegenInterfaces.h"
 #include "iree/compiler/Codegen/Dialect/GPU/IR/IREEGPUAttrs.h"
 #include "iree/compiler/Codegen/Dialect/GPU/IR/IREEGPUEnums.h"
+#include "iree/compiler/Dialect/LinalgExt/IR/LinalgExtInterfaces.h"
+#include "iree/compiler/Dialect/LinalgExt/IR/LinalgExtOps.h"
+#include "llvm/ADT/DenseSet.h"
+#include "llvm/Support/InterleavedRange.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Linalg/IR/LinalgInterfaces.h"
 #include "mlir/Dialect/Linalg/Transforms/Transforms.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Dialect/Tensor/Transforms/Transforms.h"
+#include "mlir/Dialect/Utils/StaticValueUtils.h"
 #include "mlir/IR/Dominance.h"
 #include "mlir/IR/PatternMatch.h"
+#include "mlir/IR/TypeUtilities.h"
+#include "mlir/IR/Value.h"
 #include "mlir/Interfaces/FunctionInterfaces.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
 #define DEBUG_TYPE "iree-codegen-gpu-apply-padding-level"
+
+#define DBGS() (llvm::dbgs() << "[" DEBUG_TYPE << "]: ")
+#define DBGSNL() (llvm::dbgs() << "\n")
 
 namespace mlir::iree_compiler {
 
@@ -52,45 +64,100 @@ getTiledOps(Operation *funcOp, IREE::GPU::TilingLevel tilingLevel) {
 static LogicalResult applyPaddingLevel(RewriterBase &rewriter,
                                        TilingInterface tilingInterfaceOp,
                                        IREE::GPU::TilingLevel tilingLevel) {
+
+  // 1.a. Get padding values.
+  SmallVector<Attribute> paddingValues;
+  for (Value operand : tilingInterfaceOp.getOperation()->getOperands()) {
+    paddingValues.push_back(
+        rewriter.getZeroAttr(getElementTypeOrSelf(operand.getType())));
+  }
+
+  // 1.b. Special adjustment for OnlineAttention mask padding that needs to be
+  // mindful of softmax and pad to -inf.
+  // TODO: Extract into an upstream PaddingOpInterface.
+  if (auto onlineAttentionOp = dyn_cast<IREE::LinalgExt::OnlineAttentionOp>(
+          tilingInterfaceOp.getOperation())) {
+    TypedValue<ShapedType> mask = onlineAttentionOp.getMask();
+    if (!mask) {
+      tilingInterfaceOp.emitRemark(
+          "failed to pad op: requires a mask operand to pad to the "
+          "proper value. Consider materializing the mask operand "
+          "explicitly.");
+      return failure();
+    }
+    Type maskEltType = getElementTypeOrSelf(mask.getType());
+    if (!llvm::isa<FloatType>(maskEltType)) {
+      tilingInterfaceOp.emitRemark(
+          "failed to pad op: -inf requires a float type");
+      return failure();
+    }
+    int64_t idx = onlineAttentionOp.getMaskMutable()
+                      .getAsOperandRange()
+                      .getBeginOperandIndex();
+    const auto &fltSemantics = cast<FloatType>(maskEltType).getFloatSemantics();
+    paddingValues[idx] = rewriter.getFloatAttr(
+        maskEltType, APFloat::getInf(fltSemantics, /*Negative=*/true));
+  }
+
+  // 2. Get padding sizes from tileSizes.
   SmallVector<int64_t> tileSizes =
       getLoweringConfig(tilingInterfaceOp)
           .getStaticTilingLevelSizes(llvm::to_underlying(tilingLevel),
                                      tilingInterfaceOp);
+  SmallVector<OpFoldResult> padSizes =
+      getAsIndexOpFoldResult(rewriter.getContext(), tileSizes);
 
-  // Pad the tile sizes with zero.
-  int64_t numLoops = tilingInterfaceOp.getLoopIteratorTypes().size();
-  if (tileSizes.size() > numLoops) {
+  // 3. Set options.
+  auto options = linalg::PadTilingInterfaceOptions()
+                     .setPaddingSizes(padSizes)
+                     .setPaddingValues(paddingValues)
+                     .setPadToMultipleOf(true);
+
+  LLVM_DEBUG(DBGS() << "Start padding " << *tilingInterfaceOp << "\n";
+             DBGS() << "--with tile sizes: "
+                    << llvm::interleaved_array(options.paddingSizes) << "\n";
+             DBGS() << "--with padding values: "
+                    << llvm::interleaved_array(options.paddingValues) << "\n";
+             DBGS() << "--with padToMultipleOf: " << options.padToMultipleOf
+                    << "\n");
+
+  // 4. Pad.
+  SmallVector<tensor::PadOp> padOps;
+  FailureOr<TilingInterface> maybePaddedOp =
+      linalg::rewriteAsPaddedOp(rewriter, tilingInterfaceOp, options, padOps);
+  if (failed(maybePaddedOp)) {
+    tilingInterfaceOp.emitWarning("failed to pad op");
     return failure();
   }
-  while (tileSizes.size() < numLoops) {
-    tileSizes.push_back(0);
-  }
 
-  SmallVector<int64_t> padSizes = llvm::map_to_vector(
-      tileSizes, [](int64_t tileSize) { return tileSize == 0 ? 1 : tileSize; });
+  // 5. For each PadOp, create a linalg::CopyOp to allow dim propagations.
+  TilingInterface paddedOp = *maybePaddedOp;
+  for (auto padOp : padOps) {
+    OpBuilder::InsertionGuard g(rewriter);
+    rewriter.setInsertionPointAfter(padOp);
 
-  SmallVector<int64_t> paddingDims =
-      llvm::to_vector(llvm::seq<int64_t>(0, numLoops));
+    // Record users for RAUW before creating new users.
+    llvm::SmallDenseSet<Operation *> users(padOp.getResult().getUsers().begin(),
+                                           padOp.getResult().getUsers().end());
 
-  auto options =
-      linalg::LinalgPaddingOptions()
-          .setPaddingDimensions(paddingDims)
-          .setCopyBackOp(linalg::LinalgPaddingOptions::CopyBackOp::None)
-          .setPadToMultipleOf(padSizes);
-
-  if (auto linalgOp =
-          dyn_cast<linalg::LinalgOp>(tilingInterfaceOp.getOperation())) {
-    linalg::LinalgOp paddedOp;
-    SmallVector<Value> newResults;
-    SmallVector<tensor::PadOp> padOps;
-    if (failed(linalg::rewriteAsPaddedOp(rewriter, linalgOp, options, paddedOp,
-                                         newResults, padOps))) {
-      linalgOp.emitWarning("failed to pad op");
-      return failure();
+    RankedTensorType tensorTy = padOp.getResultType();
+    int64_t rank = tensorTy.getRank();
+    SmallVector<OpFoldResult> sizes(rank, OpFoldResult());
+    for (int64_t i = 0; i < rank; ++i) {
+      sizes[i] = rewriter.createOrFold<tensor::DimOp>(paddedOp->getLoc(),
+                                                      padOp.getResult(), i);
+      if (auto v = dyn_cast<Value>(sizes[i]))
+        sizes[i] = getAsOpFoldResult(v);
     }
-    rewriter.replaceOp(linalgOp, paddedOp);
-  } else {
-    return failure();
+
+    Value out = rewriter.create<tensor::EmptyOp>(
+        paddedOp.getLoc(), sizes, getElementTypeOrSelf(tensorTy));
+    auto copied = rewriter.create<linalg::CopyOp>(paddedOp.getLoc(),
+                                                  padOp.getResult(), out);
+    rewriter.replaceUsesWithIf(padOp.getResult(), copied.getResult(0),
+                               [&](OpOperand &opOperand) {
+                                 return users.contains(opOperand.getOwner());
+                               });
   }
 
   return success();
