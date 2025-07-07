ce656af8a4eb4c001d0878719caf180f240cdea7
Max191 <44243577+Max191@users.noreply.github.com>
2025-07-07 13:45:43 -0400

    [Codegen] Support inner_tiled and load_from_buffer in ConvertAccGEMMToGEMMPass (#21245)
    Adds support for iree_codegen.inner_tiled ops in ConvertAccGEMMToGEMM,
    and allows iree_codegen.load_from_buffer as the source of the
    accumulator (instead of only iree_tensor_ext.dispatch.tensor.load). The
    pass is also refactored to a funcOp walk instead of a pattern rewrite to
    reuse more code without needing lots of unnecessary boilerplate.
    
    This is needed for the data tiling path, since GEMMs are rewritten into
    inner_tiled ops as part of encoding materialization.
    
    ---------
    
    Signed-off-by: Max Dawkins <max.dawkins@gmail.com>

--- compiler/src/iree/compiler/Codegen/Common/ConvertAccGEMMToGEMMPass.cpp
+++ compiler/src/iree/compiler/Codegen/Common/ConvertAccGEMMToGEMMPass.cpp
@@ -11,6 +11,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "iree/compiler/Codegen/Common/Passes.h"
+#include "iree/compiler/Codegen/Dialect/Codegen/IR/IREECodegenOps.h"
+#include "iree/compiler/Codegen/Dialect/GPU/IR/IREEGPUInterfaces.h"
 #include "iree/compiler/Dialect/TensorExt/IR/TensorExtOps.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
@@ -27,96 +29,94 @@ namespace mlir::iree_compiler {
 #define GEN_PASS_DEF_CONVERTACCGEMMTOGEMMPASS
 #include "iree/compiler/Codegen/Common/Passes.h.inc"
 
-namespace {
-
-struct ConvertAccGEMMtoGEMM final
-    : OpInterfaceRewritePattern<linalg::LinalgOp> {
-  using OpInterfaceRewritePattern::OpInterfaceRewritePattern;
-
-  LogicalResult matchAndRewrite(linalg::LinalgOp linalgOp,
-                                PatternRewriter &rewriter) const override {
-    if (!linalg::isaContractionOpInterface(linalgOp) &&
-        !isa<linalg::ConvolutionOpInterface>(*linalgOp)) {
-      return failure();
-    }
-    if (!linalgOp.hasPureTensorSemantics())
-      return failure();
-
-    // Nothing to do if the output tensor operand is already a fill op.
-    SmallVector<OpOperand *> outputOperands;
-    if (!linalgOp.hasPureBufferSemantics()) {
-      outputOperands = llvm::to_vector(
-          llvm::make_pointer_range(linalgOp.getDpsInitsMutable()));
-    }
-
-    Value outputOperand = outputOperands.front()->get();
-
-    auto outsDefiningOp =
-        outputOperand.getDefiningOp<IREE::TensorExt::DispatchTensorLoadOp>();
-    if (!outsDefiningOp) {
-      // If not DispatchTensorLoadOp then do nothing.
-      return failure();
-    }
-    auto outputType = cast<RankedTensorType>(outputOperand.getType());
-    if (!outputType.getElementType().isIntOrFloat())
-      return failure();
-    auto elementType = outputType.getElementType();
-
-    Location loc = linalgOp.getLoc();
-
-    // Check if the output tensor access is a projected permutation
-    if (!linalgOp.getMatchingIndexingMap(outputOperands.front())
-             .isProjectedPermutation()) {
-      return rewriter.notifyMatchFailure(
-          linalgOp, "Output indexing map must be a projected permutation.");
-    }
-
-    int64_t outputRank = outputType.getRank();
-    SmallVector<utils::IteratorType> iterators(outputRank,
-                                               utils::IteratorType::parallel);
-    SmallVector<AffineMap> maps(3, rewriter.getMultiDimIdentityMap(outputRank));
-
-    // Create a zero tensor as the new output tensor operand to the Linalg
-    // contraction op.
-    SmallVector<OpFoldResult> mixedSizes =
-        tensor::getMixedSizes(rewriter, loc, outputOperand);
-    auto initOp =
-        rewriter.create<tensor::EmptyOp>(loc, mixedSizes, elementType);
-    Value zero = rewriter.create<arith::ConstantOp>(
-        loc, rewriter.getZeroAttr(elementType));
-    Value fill =
-        rewriter.create<linalg::FillOp>(loc, zero, initOp.getResult()).result();
-
-    // Update the contraction op to use the new zero tensor as output operand.
-    rewriter.modifyOpInPlace(linalgOp,
-                             [&]() { linalgOp.setDpsInitOperand(0, fill); });
-
-    // Create a generic op to add back the original output tensor operand.
-    rewriter.setInsertionPointAfter(linalgOp);
-    auto genericOp = rewriter.create<linalg::GenericOp>(
-        loc, outputType, ValueRange{linalgOp->getResult(0), outputOperand},
-        fill, maps, iterators,
-        [&](OpBuilder &b, Location nestedLoc, ValueRange args) {
-          Value result;
-          if (llvm::isa<FloatType>(elementType)) {
-            result = b.create<arith::AddFOp>(nestedLoc, args[0], args[1]);
-          } else {
-            result = b.create<arith::AddIOp>(nestedLoc, args[0], args[1]);
-          }
-          b.create<linalg::YieldOp>(nestedLoc, result);
-        });
-    linalgOp->getResult(0).replaceAllUsesExcept(genericOp->getResult(0),
-                                                genericOp);
-    return success();
+static bool accGemmToGemmPrecondition(Operation *op) {
+  if (auto innerTiledOp = dyn_cast<IREE::Codegen::InnerTiledOp>(op)) {
+    return isa<IREE::GPU::MmaInterfaceAttr>(innerTiledOp.getKind());
   }
-};
+  auto linalgOp = dyn_cast<linalg::LinalgOp>(op);
+  if (!linalgOp) {
+    return false;
+  }
+  if (!linalg::isaContractionOpInterface(linalgOp) &&
+      !isa<linalg::ConvolutionOpInterface>(*linalgOp)) {
+    return false;
+  }
+  if (!linalgOp.hasPureTensorSemantics()) {
+    return false;
+  }
+  return linalgOp.getMatchingIndexingMap(linalgOp.getDpsInitOperand(0))
+      .isProjectedPermutation();
+}
+
+static void convertAccGemmToGemm(RewriterBase &rewriter,
+                                 DestinationStyleOpInterface dpsOp) {
+  SmallVector<OpOperand *> outputOperands =
+      llvm::to_vector(llvm::make_pointer_range(dpsOp.getDpsInitsMutable()));
+  Value outputOperand = outputOperands.front()->get();
+  auto outsDefiningOp = outputOperand.getDefiningOp();
+  // If not DispatchTensorLoadOp or LoadFromBufferOp then do nothing.
+  if (!isa_and_nonnull<IREE::TensorExt::DispatchTensorLoadOp,
+                       IREE::Codegen::LoadFromBufferOp>(outsDefiningOp)) {
+    return;
+  }
+  auto outputType = cast<RankedTensorType>(outputOperand.getType());
+  if (!outputType.getElementType().isIntOrFloat()) {
+    return;
+  }
+  auto elementType = outputType.getElementType();
+
+  Location loc = dpsOp.getLoc();
+  OpBuilder::InsertionGuard g(rewriter);
+  rewriter.setInsertionPoint(dpsOp);
+
+  int64_t outputRank = outputType.getRank();
+  SmallVector<utils::IteratorType> iterators(outputRank,
+                                             utils::IteratorType::parallel);
+  SmallVector<AffineMap> maps(3, rewriter.getMultiDimIdentityMap(outputRank));
+
+  // Create a zero tensor as the new output tensor operand to the Linalg
+  // contraction op.
+  SmallVector<OpFoldResult> mixedSizes =
+      tensor::getMixedSizes(rewriter, loc, outputOperand);
+  auto initOp = rewriter.create<tensor::EmptyOp>(loc, mixedSizes, elementType);
+  Value zero = rewriter.create<arith::ConstantOp>(
+      loc, rewriter.getZeroAttr(elementType));
+  Value fill =
+      rewriter.create<linalg::FillOp>(loc, zero, initOp.getResult()).result();
+
+  // Update the contraction op to use the new zero tensor as output operand.
+  rewriter.modifyOpInPlace(dpsOp, [&]() { dpsOp.setDpsInitOperand(0, fill); });
+
+  // Create a generic op to add back the original output tensor operand.
+  rewriter.setInsertionPointAfter(dpsOp);
+  auto genericOp = rewriter.create<linalg::GenericOp>(
+      loc, outputType, ValueRange{dpsOp->getResult(0), outputOperand}, fill,
+      maps, iterators, [&](OpBuilder &b, Location nestedLoc, ValueRange args) {
+        Value result;
+        if (llvm::isa<FloatType>(elementType)) {
+          result = b.create<arith::AddFOp>(nestedLoc, args[0], args[1]);
+        } else {
+          result = b.create<arith::AddIOp>(nestedLoc, args[0], args[1]);
+        }
+        b.create<linalg::YieldOp>(nestedLoc, result);
+      });
+  dpsOp->getResult(0).replaceAllUsesExcept(genericOp->getResult(0), genericOp);
+}
+
+namespace {
 
 struct ConvertAccGEMMToGEMMPass final
     : impl::ConvertAccGEMMToGEMMPassBase<ConvertAccGEMMToGEMMPass> {
   void runOnOperation() override {
-    RewritePatternSet patterns(&getContext());
-    patterns.add<ConvertAccGEMMtoGEMM>(&getContext());
-    walkAndApplyPatterns(getOperation(), std::move(patterns));
+    FunctionOpInterface funcOp = getOperation();
+    SmallVector<Operation *> candidates = llvm::filter_to_vector(
+        llvm::make_pointer_range(funcOp.getFunctionBody().getOps()),
+        accGemmToGemmPrecondition);
+    IRRewriter rewriter(&getContext());
+    for (Operation *candidate : candidates) {
+      convertAccGemmToGemm(rewriter,
+                           cast<DestinationStyleOpInterface>(candidate));
+    }
   }
 };
 
