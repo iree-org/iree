c5ff81a8174a07859d0c3957f0bac8661976052d
hanhanW <hanhan0912@gmail.com>
2025-07-10 08:17:06 -0700

    address the last comment
    Signed-off-by: hanhanW <hanhan0912@gmail.com>

--- compiler/src/iree/compiler/Codegen/Common/TileSizeSelection.cpp
+++ compiler/src/iree/compiler/Codegen/Common/TileSizeSelection.cpp
@@ -132,7 +132,7 @@ TilingConfig::getLoweringConfigWithNewVectorSizes(
 
   MLIRContext *context = loweringConfig.getContext();
   SmallVector<IREE::Codegen::LoweringConfigTilingLevelAttr> tilingLevels;
-  for (int i = 0, e = getNumTilingLevels(); i < e; ++i) {
+  for (unsinged i = 0, e = getNumTilingLevels(); i < e; ++i) {
     tilingLevels.push_back(cast<IREE::Codegen::LoweringConfigTilingLevelAttr>(
         loweringConfig.getTilingLevelAttr(i)));
   }
