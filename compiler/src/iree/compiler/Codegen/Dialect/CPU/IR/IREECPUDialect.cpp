8fdf0c504379d9f8d8dfcc13e77f4798932a4990
Han-Chung Wang <hanhan0912@gmail.com>
2025-07-10 08:54:43 -0700

    [CPU] Implement OpAsmDialectInterface for IREE::CPU::LoweringConfigAttr. (#21325)
    Signed-off-by: hanhanW <hanhan0912@gmail.com>

--- compiler/src/iree/compiler/Codegen/Dialect/CPU/IR/IREECPUDialect.cpp
+++ compiler/src/iree/compiler/Codegen/Dialect/CPU/IR/IREECPUDialect.cpp
@@ -7,13 +7,26 @@
 #include "iree/compiler/Codegen/Dialect/CPU/IR/IREECPUDialect.h"
 
 #include "iree/compiler/Codegen/Dialect/CPU/IR/IREECPUDialect.cpp.inc"
+#include "iree/compiler/Codegen/Dialect/CPU/IR/IREECPUTypes.h"
 #include "iree/compiler/Codegen/Dialect/Codegen/IR/IREECodegenDialect.h"
 
 namespace mlir::iree_compiler::IREE::CPU {
 
+struct IREECPUDialectOpAsmInterface : public OpAsmDialectInterface {
+  using OpAsmDialectInterface::OpAsmDialectInterface;
+  AliasResult getAlias(Attribute attr, raw_ostream &os) const override {
+    if (isa<IREE::CPU::LoweringConfigAttr>(attr)) {
+      os << "config";
+      return AliasResult::OverridableAlias;
+    }
+    return AliasResult::NoAlias;
+  }
+};
+
 void IREECPUDialect::initialize() {
   registerAttributes();
   getContext()->getOrLoadDialect<IREE::Codegen::IREECodegenDialect>();
+  addInterfaces<IREECPUDialectOpAsmInterface>();
 }
 
 } // namespace mlir::iree_compiler::IREE::CPU
