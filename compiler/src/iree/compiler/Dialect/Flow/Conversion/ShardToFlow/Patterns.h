9e5948c24f62317efe6dbc0d0a11406098755d52
Benoit Jacob <jacob.benoit.1@gmail.com>
2025-07-25 17:44:27 +0000

    Advance LLVM to 258daf539583b80e0217d1d87941412d65cf16aa: [mlir][NFC] update `mlir` create APIs (34/n) (#150660) (Maksim Levental on 2025-07-25 12:36:54 -0500) (7 of 8)
    Signed-off-by: Benoit Jacob <jacob.benoit.1@gmail.com>

--- compiler/src/iree/compiler/Dialect/Flow/Conversion/ShardToFlow/Patterns.h
+++ compiler/src/iree/compiler/Dialect/Flow/Conversion/ShardToFlow/Patterns.h
@@ -4,25 +4,26 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include "mlir/Dialect/Shard/IR/ShardOps.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/SymbolTable.h"
 #include "mlir/IR/Value.h"
 
-namespace mlir::mesh {
-class MeshOp;
-using MeshAxis = int16_t;
-} // namespace mlir::mesh
+namespace mlir::shard {
+class ShardOp;
+using ShardAxis = int16_t;
+} // namespace mlir::shard
 
 namespace mlir::iree_compiler::IREE::Flow {
 
 // Returns a `!flow.channel` value from |builder| that represents the given
-// mesh axes. If no mesh axes are provided then all will be used.
-using LookupMeshChannelFn = std::function<Value(
-    Location loc, mesh::MeshOp meshOp,
-    std::optional<ArrayRef<mesh::MeshAxis>> meshAxes, OpBuilder &builder)>;
+// grid axes. If no grid axes are provided then all will be used.
+using LookupShardChannelFn = std::function<Value(
+    Location loc, shard::GridOp gridOp,
+    std::optional<ArrayRef<shard::GridAxis>> gridAxes, OpBuilder &builder)>;
 
-void populateMeshToFlowCollectivesPatterns(
+void populateShardToFlowCollectivesPatterns(
     RewritePatternSet &patterns, SymbolTableCollection &symbolTableCollection,
-    LookupMeshChannelFn lookupChannel);
+    LookupShardChannelFn lookupChannel);
 
 } // namespace mlir::iree_compiler::IREE::Flow
