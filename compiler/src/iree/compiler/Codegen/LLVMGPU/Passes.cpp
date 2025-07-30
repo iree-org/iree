f310a677f20114fe27b6283d811fe46e9d8fcec8
Max Dawkins <max.dawkins@gmail.com>
2025-07-30 15:10:08 +0000

    [LLVMGPU] Move LLVMGPUVectorLowering after OptimizeIntArithmetic
    Signed-off-by: Max Dawkins <max.dawkins@gmail.com>

--- compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
@@ -1190,7 +1190,6 @@ static void addLowerToLLVMGPUPasses(OpPassManager &modulePassManager,
 
   FunctionLikeNest funcPassManager(modulePassManager);
   funcPassManager.addPass(createFoldTensorExtractOpPass)
-      .addPass(createLLVMGPUVectorLoweringPass)
       .addPass(createExpandGPUOpsPass)
       // Barrier elimination before we reach unstructured control flow.
       .addPass(createGpuEliminateBarriers)
@@ -1203,6 +1202,7 @@ static void addLowerToLLVMGPUPasses(OpPassManager &modulePassManager,
 
   // This pass needs to run before SCF -> CF.
   addLowerAndOptimizeAddressComputationPasses(funcPassManager);
+  funcPassManager.addPass(createLLVMGPUVectorLoweringPass);
 
   funcPassManager
       // Run checks on shared memory usage.
