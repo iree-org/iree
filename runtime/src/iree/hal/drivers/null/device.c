ba4ad9a2cf3e8eef8ada0f9b8fb12d0e4792cc3e
Ben Vanik <ben.vanik@gmail.com>
2025-08-08 09:47:48 -0700

    Moving HAL queue emulation utilities out of the main device.c. This lets us avoid introducing dependencies into `iree::hal` for the emulation and makes it clearer what's part of the API vs what's an implementation detail.

--- runtime/src/iree/hal/drivers/null/device.c
+++ runtime/src/iree/hal/drivers/null/device.c
@@ -16,6 +16,7 @@
 #include "iree/hal/drivers/null/semaphore.h"
 #include "iree/hal/utils/file_registry.h"
 #include "iree/hal/utils/file_transfer.h"
+#include "iree/hal/utils/queue_emulation.h"
 
 //===----------------------------------------------------------------------===//
 // iree_hal_null_device_options_t
