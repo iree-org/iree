ae2abe9a242e4040155956a6e3a1442ce818babd
Fabian Mora <fabian.mora-cordero@amd.com>
2025-08-20 14:07:30 +0000

    Add thread uniform analysis and use int range analysis
    Signed-off-by: Fabian Mora <fabian.mora-cordero@amd.com>

---
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/Analysis/ROCDLThreadUniformAnalysis.h
@@ -0,0 +1,85 @@
+// Copyright 2024 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "mlir/Analysis/DataFlow/SparseAnalysis.h"
+
+namespace mlir::iree_compiler::ROCDL {
+//===----------------------------------------------------------------------===//
+// ThreadUniform
+//===----------------------------------------------------------------------===//
+/// Class holding the state of the sparse lattice. There are three possible
+/// states for the lattice:
+/// `uninitialized`: the state is unknown.
+/// `uniform`: the value was determined to be uniform across threads.
+/// `dependent`: the value is thread dependent.
+struct ThreadUniform {
+  ThreadUniform() = default;
+  /// Creates a dependent state.
+  static ThreadUniform getDependent() { return ThreadUniform(dependent); }
+  /// Creates a uniform state.
+  static ThreadUniform getUniform() { return ThreadUniform(uniform); }
+  /// Returns whether this state is uniform.
+  bool isUniform() const { return state == uniform; }
+  /// Compares two states.
+  bool operator==(const ThreadUniform &other) const {
+    return state == other.state;
+  }
+  /// Prints the state to a stream.
+  void print(llvm::raw_ostream &s) const;
+  /// Joins two states, where `dependent` is the top state of dataflow.
+  static ThreadUniform join(const ThreadUniform &lhs, const ThreadUniform &rhs);
+
+private:
+  typedef enum { uninitialized, uniform, dependent } State;
+  ThreadUniform(State state) : state(state) {}
+  State state = uninitialized;
+};
+
+//===----------------------------------------------------------------------===//
+// ThreadUniformLattice
+//===----------------------------------------------------------------------===//
+/// Class holding a lattice for the sparse analysis.
+class ThreadUniformLattice : public dataflow::Lattice<ThreadUniform> {
+public:
+  using Lattice::Lattice;
+};
+
+//===----------------------------------------------------------------------===//
+// ThreadUniformAnalysis
+//===----------------------------------------------------------------------===//
+/// The dataflow analysis computing whether a value is thread uniform or not.
+class ThreadUniformAnalysis
+    : public dataflow::SparseForwardDataFlowAnalysis<ThreadUniformLattice> {
+public:
+  using SparseForwardDataFlowAnalysis::SparseForwardDataFlowAnalysis;
+
+  /// Sets the lattice to a pessimistic state.
+  void setToEntryState(ThreadUniformLattice *lattice) override {
+    propagateIfChanged(lattice, lattice->join(ThreadUniform::getDependent()));
+  }
+
+  /// Visits an operation and determines whether it's uniform or not.
+  LogicalResult
+  visitOperation(Operation *op, ArrayRef<const ThreadUniformLattice *> operands,
+                 ArrayRef<ThreadUniformLattice *> results) override;
+
+  /// Handles the uniformity of control-flow arguments and results.
+  void
+  visitNonControlFlowArguments(Operation *op, const RegionSuccessor &successor,
+                               ArrayRef<ThreadUniformLattice *> argLattices,
+                               unsigned firstIndex) override;
+
+  /// Override the default handling, this is necessary as control-flow with
+  /// `scf.forall` / `scf.in_parallel` is broken, and the fix is large. TODO:
+  /// Remove this once the ops have been fixed.
+  void visitRegionSuccessors(
+      ProgramPoint *point, RegionBranchOpInterface branch,
+      RegionBranchPoint successor,
+      ArrayRef<dataflow::AbstractSparseLattice *> lattices) override;
+};
+} // namespace mlir::iree_compiler::ROCDL
+
+MLIR_DECLARE_EXPLICIT_TYPE_ID(mlir::iree_compiler::ROCDL::ThreadUniformLattice)
--- compiler/src/iree/compiler/Codegen/LLVMGPU/ROCDLConfigureBufferInstructions.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/ROCDLConfigureBufferInstructions.cpp
@@ -5,154 +5,214 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include "iree/compiler/Codegen/Dialect/GPU/IR/IREEGPUDialect.h"
+#include "iree/compiler/Codegen/LLVMGPU/Analysis/ROCDLThreadUniformAnalysis.h"
 #include "iree/compiler/Codegen/LLVMGPU/Passes.h"
 #include "iree/compiler/Codegen/Utils/GPUUtils.h"
 #include "iree/compiler/Dialect/HAL/IR/HALOps.h"
 #include "iree/compiler/Dialect/TensorExt/IR/TensorExtTypes.h"
-#include "iree/compiler/Dialect/Util/IR/UtilOps.h"
-#include "iree/compiler/Dialect/Util/IR/UtilTypes.h"
-#include "llvm/Support/DebugLog.h"
+#include "llvm/Support/Debug.h"
+#include "mlir/Analysis/DataFlow/ConstantPropagationAnalysis.h"
+#include "mlir/Analysis/DataFlow/DeadCodeAnalysis.h"
+#include "mlir/Analysis/DataFlow/IntegerRangeAnalysis.h"
 #include "mlir/Analysis/SliceAnalysis.h"
 #include "mlir/Interfaces/FunctionInterfaces.h"
 
 #define DEBUG_TYPE "iree-codegen-rocdl-configure-buffer-instructions"
-#define DBGS() (llvm::dbgs() << "[" DEBUG_TYPE "]: ")
+#define LDBGS(X) LLVM_DEBUG((llvm::dbgs() << "[" DEBUG_TYPE "]:") << X << "\n")
 
 namespace mlir::iree_compiler {
-
-#define GEN_PASS_DEF_ROCDLCONFIGUREBUFFERINSTRUCTIONSPASS
-#include "iree/compiler/Codegen/LLVMGPU/ROCDLPasses.h.inc"
-
 static llvm::cl::opt<bool> clROCDLlEnableBufferInstructions(
     "iree-rocdl-enable-buffer-instructions",
     llvm::cl::desc("Use buffer instructions (by using buffer fat pointers) "
                    "where possible on AMD targets"),
     llvm::cl::init(true));
 
-static Value stripIntegerCasts(Value val) {
-  while (isa_and_nonnull<arith::IndexCastOp, arith::IndexCastUIOp,
-                         arith::ExtSIOp, arith::ExtUIOp>(val.getDefiningOp())) {
-    val = val.getDefiningOp()->getOperand(0);
-  }
-  return val;
-}
-
-/// Determine if `arg` is an arithmetic function of constants or HAL constant
-/// loads, which is a conservative approximatino for workgroup-uniformity that
-/// can be made more extensive if needed.
-static bool isDefinitelyWorkgroupUniform(Value arg) {
-  if (!arg)
-    return true;
-  SetVector<Operation *> dependencies;
-  BackwardSliceOptions opts;
-  arg = stripIntegerCasts(arg);
-  if (auto assume = arg.getDefiningOp<IREE::Util::AssumeIntOp>()) {
-    arg = assume.getOperand(cast<OpResult>(arg).getResultNumber());
-  }
-  // Note: this is a bit conservative, in that it will traverse all the
-  // arguments to a util.assume.int that isn't the immediate parent of val.
-  [[maybe_unused]] LogicalResult result =
-      getBackwardSlice(arg, &dependencies, opts);
-  assert(result.succeeded());
-  return llvm::all_of(dependencies, [&](Operation *op) {
-    if (matchPattern(op, m_Constant()))
-      return true;
-    if (isa<IREE::HAL::InterfaceConstantLoadOp, IREE::Util::AssumeIntOp>(op)) {
-      return true;
-    }
-    if (isa_and_nonnull<arith::ArithDialect>(op->getDialect())) {
-      return true;
-    }
-    return false;
-  });
-}
-
-/// Return the maximum value that has been `util.assume.int`'d about this value
-/// if there is one.
-/// TODO: it'd be nice to be able to run the IntRangeAnalysis just up to the
-/// value in question, but we don't have that, so we approximate it.
-static std::optional<int64_t> getDynamicSizeMax(Value size) {
-  size = stripIntegerCasts(size);
-  // Special case for constants that're still dynamic.
-  APInt constVal;
-  if (matchPattern(size, m_ConstantInt(&constVal))) {
-    return constVal.getZExtValue();
-  }
-  auto assumeOp = size.getDefiningOp<IREE::Util::AssumeIntOp>();
-  if (!assumeOp)
-    return std::nullopt;
-  std::optional<int64_t> maybeMax =
-      assumeOp.getUnionedUnsignedRange(cast<OpResult>(size).getResultNumber())
-          .second;
-  return maybeMax;
-}
-
-static std::optional<int64_t>
-getSpannedBytes(IREE::HAL::InterfaceBindingSubspanOp binding) {
-  int64_t maxNumElems = 1;
-  ShapedType resultTy = dyn_cast<ShapedType>(binding.getType());
-  if (auto tensorType =
-          dyn_cast<IREE::TensorExt::DispatchTensorType>(binding.getType())) {
-    resultTy = tensorType.asRankedTensorType();
-  }
-  if (!resultTy || !resultTy.hasRank())
-    return std::nullopt;
-  for (Value dynArg : binding.getResultDynamicDims(0)) {
-    std::optional<int64_t> dimMax = getDynamicSizeMax(dynArg);
-    if (!dimMax)
-      return std::nullopt;
-    maxNumElems *= (*dimMax);
-  }
-  for (int64_t dim : resultTy.getShape()) {
-    if (ShapedType::isDynamic(dim))
-      continue;
-    maxNumElems *= dim;
-  }
-  return maxNumElems * IREE::Util::getTypeBitWidth(resultTy.getElementType()) /
-         8;
-}
+#define GEN_PASS_DEF_ROCDLCONFIGUREBUFFERINSTRUCTIONSPASS
+#include "iree/compiler/Codegen/LLVMGPU/ROCDLPasses.h.inc"
 
 namespace {
-
+//===----------------------------------------------------------------------===//
+// ROCDLConfigureBufferInstructionsPass
+//===----------------------------------------------------------------------===//
 struct ROCDLConfigureBufferInstructionsPass final
-    : impl::ROCDLConfigureBufferInstructionsPassBase<
+    : mlir::iree_compiler::impl::ROCDLConfigureBufferInstructionsPassBase<
           ROCDLConfigureBufferInstructionsPass> {
-  void runOnOperation() override {
-    if (!clROCDLlEnableBufferInstructions)
-      return;
-    FunctionOpInterface func = getOperation();
-    // Is this really he best way to skip this pass on non-rocdl targets?
-    IREE::GPU::TargetAttr target = getGPUTargetAttr(func);
-    if (!target || !target.isAMD())
-      return;
-    auto *gpuDialect =
-        getContext().getLoadedDialect<IREE::GPU::IREEGPUDialect>();
-    auto annotationHelper =
-        gpuDialect->getUseRocdlBufferInstructionsAttrHelper();
-    auto unitAttr = UnitAttr::get(&getContext());
-    func.walk([&](IREE::HAL::InterfaceBindingSubspanOp binding) {
-      Value offset = binding.getByteOffset();
-      if (offset && !isDefinitelyWorkgroupUniform(offset)) {
-        LDBG() << "Binding offset " << offset
-               << " not known workgroup-uniform\n";
-        return;
-      }
-      std::optional<int64_t> maxBytes = getSpannedBytes(binding);
-      if (!maxBytes) {
-        LDBG() << "Couldn't bound binding size for " << binding;
-        return;
-      }
-      if (*maxBytes >=
-          static_cast<int64_t>(std::numeric_limits<int32_t>::max())) {
-        LDBG() << "Size of " << binding << " too large (" << *maxBytes
-               << " bytes)";
-        return;
-      }
-      annotationHelper.setAttr(binding, unitAttr);
-    });
-  }
+  void runOnOperation() override;
 };
-
 } // namespace
 
+//===----------------------------------------------------------------------===//
+// Internal functions
+//===----------------------------------------------------------------------===//
+
+/// Multiply two numbers, return std::nullopt if an overflow happened.
+static std::optional<int64_t> safeMul(int64_t lhs, int64_t rhs) {
+  if (lhs > std::numeric_limits<int64_t>::max() / rhs) {
+    return std::nullopt;
+  }
+  return lhs * rhs;
+}
+
+/// Return the type bit width without using the data layout. TODO: use the data
+/// layout.
+static unsigned getTypeBitWidth(Type type) {
+  if (auto complexType = dyn_cast<ComplexType>(type)) {
+    return 2 * complexType.getElementType().getIntOrFloatBitWidth();
+  }
+  if (auto vectorType = dyn_cast<VectorType>(type)) {
+    return vectorType.getNumElements() *
+           getTypeBitWidth(vectorType.getElementType());
+  }
+  // Over approximate the size of index to not require using the data layout.
+  if (type.isIndex()) {
+    return 8u;
+  }
+  // If it's not an int or float return 0.
+  return type.isIntOrFloat() ? type.getIntOrFloatBitWidth() : 0;
+}
+
+//===----------------------------------------------------------------------===//
+// ROCDLConfigureBufferInstructionsPass
+//===----------------------------------------------------------------------===//
+
+/// Returns whether the binding op satisfies the requirement of being less than
+/// 2GB in size.
+static bool
+isValidTensorSpan(IREE::HAL::InterfaceBindingSubspanOp op,
+                  llvm::function_ref<int64_t(OpFoldResult)> getMax) {
+  RankedTensorType tensorTy =
+      cast<IREE::TensorExt::DispatchTensorType>(op.getType())
+          .asRankedTensorType();
+  ArrayRef<int64_t> shape = tensorTy.getShape();
+
+  // Return immediately if the tensor is trivial.
+  if (shape.empty()) {
+    return true;
+  }
+
+  // Compute the max allowed size.
+  unsigned byteSize = getTypeBitWidth(tensorTy.getElementType());
+  if (byteSize == 0) {
+    LDBGS(
+        "- Couldn't determine the type size of: " << tensorTy.getElementType());
+    return false;
+  }
+  // Convert from bit-width to bytes.
+  byteSize = std::max(byteSize, 8u) / 8u;
+
+  // Get the total size.
+  ValueRange dymDims = op.getResultDynamicDims(0);
+  int64_t size = 1;
+  for (auto [i, rawDim] : llvm::enumerate(shape)) {
+    int64_t dim = rawDim;
+    if (dim == ShapedType::kDynamic) {
+      dim = getMax(dymDims.front());
+      // The size is less than 0 if `IntRange` couldn't determine the size.
+      if (dim <= 0) {
+        LDBGS("- Failed to get the size of the dynamic dimension: " << i);
+        return false;
+      }
+      dymDims = dymDims.drop_front();
+    }
+    std::optional<int64_t> tmp = safeMul(dim, size);
+    if (!tmp) {
+      LDBGS("- Overflow detected between the partial total size: "
+            << size << ", and shape[" << i << "] = " << dim);
+      return false;
+    }
+    LDBGS("- shape[" << i << "] = " << dim);
+    size = *tmp;
+  }
+
+  // Get the span in bytes.
+  std::optional<int64_t> tmp = safeMul(byteSize, size);
+  if (!tmp) {
+    LDBGS("- Overflow detected when computing the size of the tensor: "
+          << size << ", and " << byteSize);
+    return false;
+  }
+  size = *tmp;
+  LDBGS("- The tensor size in bytes is: " << size);
+  return static_cast<int64_t>(std::numeric_limits<int32_t>::max()) > size;
+}
+
+void ROCDLConfigureBufferInstructionsPass::runOnOperation() {
+  if (!clROCDLlEnableBufferInstructions)
+    return;
+  FunctionOpInterface func = getOperation();
+
+  // Don't perform any actions if the target is not an AMD GPU.
+  IREE::GPU::TargetAttr target = getGPUTargetAttr(func);
+  if (!target || !target.isAMD())
+    return;
+
+  // Configure and run the dataflow analysis.
+  DataFlowSolver solver;
+  solver.load<dataflow::SparseConstantPropagation>();
+  solver.load<dataflow::DeadCodeAnalysis>();
+  solver.load<dataflow::IntegerRangeAnalysis>();
+  solver.load<ROCDL::ThreadUniformAnalysis>();
+
+  if (failed(solver.initializeAndRun(func))) {
+    LDBGS(" Dataflow failed, aborting");
+    return signalPassFailure();
+  }
+
+  // Detect whether a source binding is already annotated and should be skipped.
+  auto *gpuDialect = getContext().getLoadedDialect<IREE::GPU::IREEGPUDialect>();
+  IREE::GPU::IREEGPUDialect::UseRocdlBufferInstructionsAttrHelper
+      annotationHelper = gpuDialect->getUseRocdlBufferInstructionsAttrHelper();
+
+  // Retrieve the max int of a value or -1 if it cannot be determined.
+  auto getMax = [&](OpFoldResult ofr) -> int64_t {
+    if (auto attr = dyn_cast<Attribute>(ofr)) {
+      return cast<IntegerAttr>(attr).getValue().getSExtValue();
+    }
+    auto *state = solver.lookupState<dataflow::IntegerValueRangeLattice>(
+        cast<Value>(ofr));
+    if (!state || state->getValue().isUninitialized()) {
+      // Return -1 if the state is unknown as we only care for positive values.
+      return -1;
+    }
+    const ConstantIntRanges &range = state->getValue().getValue();
+    if (range.umax().getBitWidth() != 0) {
+      std::optional<int64_t> m = range.umax().trySExtValue();
+      return m ? *m : int64_t{-1};
+    }
+    if (range.smax().getBitWidth() != 0) {
+      std::optional<int64_t> m = range.smax().trySExtValue();
+      return m ? *m : int64_t{-1};
+    }
+    return -1;
+  };
+
+  UnitAttr unitAttr = UnitAttr::get(&getContext());
+  func.walk([&](IREE::HAL::InterfaceBindingSubspanOp bOp) {
+    // Skip if we are not on tensors.
+    if (!isa<IREE::TensorExt::DispatchTensorType>(bOp.getType())) {
+      return;
+    }
+
+    LDBGS(" found binding op: " << bOp);
+
+    // Skip if the op offsets are not thread uniform.
+    if (Value offset = bOp.getByteOffset()) {
+      auto opAnalysis = solver.lookupState<ROCDL::ThreadUniformLattice>(offset);
+      if (!(opAnalysis && opAnalysis->getValue().isUniform())) {
+        LDBGS("- Failure, the op offset is not thread uniform");
+        return;
+      }
+    }
+
+    // Check if we can annotate the binding.
+    if (!isValidTensorSpan(bOp, getMax)) {
+      LDBGS("- Failure, the op byte span is too big");
+      return;
+    }
+
+    // Annotate the binding.
+    LDBGS("- Success, annotating the op");
+    annotationHelper.setAttr(bOp, unitAttr);
+  });
+}
 } // namespace mlir::iree_compiler
