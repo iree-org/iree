6cf470248c43d81c3daf409de5f8b8345ae46b85
MaheshRavishankar <mravisha@amd.com>
2025-08-20 17:30:34 +0000

    Drop TensorCore/MMA pipelines.
    These pipelines were meant to support tensor core operations, but are
    legacy. They havent been updated to target latest GPUs (I think they
    mostly support Ampere, at best Volta) architecture. They are
    unmaintained, and use some legacy code generation strategies. Dropping
    these pipelines for now.
    
    The newer LLVMGPUTileAndFuse / LLVMGPUVectorDistribute passes should
    be able to target tensor cores too. So if this becomes of interest,
    the path forward would be to adapt those pipelines to target tensor
    core operations.
    
    Signed-off-by: MaheshRavishankar <mravisha@amd.com>

--- compiler/src/iree/compiler/Codegen/LLVMGPU/LLVMGPULowerExecutableTarget.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/LLVMGPULowerExecutableTarget.cpp
@@ -114,30 +114,6 @@ void LLVMGPULowerExecutableTargetPass::runOnOperation() {
   case IREE::Codegen::DispatchLoweringPassPipeline::LLVMGPUWinogradVectorize:
     addGPUWinogradVectorizePassPipeline(pipeline);
     break;
-  case IREE::Codegen::DispatchLoweringPassPipeline::LLVMGPUMatmulTensorCore: {
-    FailureOr<int64_t> maybeDepth =
-        getSoftwarePipelineDepth(translationInfo.getConfiguration());
-    if (failed(maybeDepth)) {
-      funcOp.emitOpError(
-          "invalid matmul configuration without software pipelining config");
-      return signalPassFailure();
-    }
-    addGPUMatmulTensorCorePassPipeline(pipeline, pipelineOptions, *maybeDepth);
-    break;
-  }
-  case IREE::Codegen::DispatchLoweringPassPipeline::
-      LLVMGPUMatmulTensorCoreMmaSync: {
-    FailureOr<int64_t> maybeDepth =
-        getSoftwarePipelineDepth(translationInfo.getConfiguration());
-    if (failed(maybeDepth)) {
-      funcOp.emitOpError(
-          "invalid matmul configuration without software pipelining config");
-      return signalPassFailure();
-    }
-    addGPUMatmulTensorCoreMmaSyncPassPipeline(pipeline, pipelineOptions,
-                                              *maybeDepth);
-    break;
-  }
   case IREE::Codegen::DispatchLoweringPassPipeline::LLVMGPUTransposeSharedMem:
     addGPUTransposePassPipeline(pipeline, pipelineOptions);
     break;
