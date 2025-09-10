4a3a6dcf7c8c004a9e1b580a67f704753e042a26
Jorn Tuyls <jorn.tuyls@gmail.com>
2025-09-10 21:32:09 +0000

    [ROCM][DT] Add encoding specialization infra for data-tiled ukernels
    Signed-off-by: Jorn Tuyls <jorn.tuyls@gmail.com>

--- compiler/plugins/target/ROCM/Dialect/ROCM/IR/ROCMAttrs.cpp
+++ compiler/plugins/target/ROCM/Dialect/ROCM/IR/ROCMAttrs.cpp
@@ -11,6 +11,7 @@
 #include "iree/compiler/Codegen/Dialect/GPU/IR/GPUTileSwizzleUtils.h"
 #include "iree/compiler/Codegen/Dialect/GPU/IR/IREEGPUOps.h"
 #include "iree/compiler/Codegen/Utils/GPUUtils.h"
+#include "iree/compiler/Dialect/Encoding/IR/EncodingTypes.h"
 #include "iree/compiler/Dialect/HAL/IR/HALTypes.h"
 #include "llvm/ADT/TypeSwitch.h"
 #include "llvm/ExecutionEngine/ExecutionEngine.h"
@@ -20,6 +21,7 @@
 #include "mlir/Dialect/Bufferization/IR/Bufferization.h"
 #include "mlir/Dialect/GPU/IR/GPUDialect.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/Linalg/IR/LinalgInterfaces.h"
 #include "mlir/Dialect/Utils/IndexingUtils.h"
 #include "mlir/IR/AsmState.h"
 #include "mlir/IR/Attributes.h"
@@ -461,6 +463,48 @@ std::optional<LogicalResult> UKernelProviderAttr::createAndReplaceWithUkernelOp(
                                       otherOperands);
   }
   return std::nullopt;
+}
+
+//===---------------------------------------------------------------------===//
+// rocm.tensor_ukernel_provider
+//===---------------------------------------------------------------------===//
+
+FailureOr<Operation *>
+TensorUKernelProviderAttr::getMLIRUKernel(StringRef name, DictionaryAttr,
+                                          Operation *annotationSite) const {
+  auto *symbolTableOp = SymbolTable::getNearestSymbolTable(annotationSite);
+  SymbolTable symbolTable(symbolTableOp);
+  return symbolTable.lookup(name);
+}
+
+Attribute TensorUKernelProviderAttr::getDataLayoutForUKernel(
+    Attribute encoding, DictionaryAttr targetConfiguration) const {
+  auto encodingAttr =
+      dyn_cast_if_present<IREE::Encoding::EncodingAttr>(encoding);
+  if (!encodingAttr) {
+    return {};
+  }
+  IREE::GPU::TargetAttr targetAttr = getGPUTargetAttr(targetConfiguration);
+  if (!targetAttr || targetAttr.getArch() != "gfx942") {
+    return {};
+  }
+  ArrayAttr indexingMapsAttr = encodingAttr.getUserIndexingMaps();
+  if (!indexingMapsAttr) {
+    return {};
+  }
+  if (failed(linalg::inferContractionDims(encodingAttr.getRootMaps()))) {
+    return {};
+  }
+  SmallVector<Type> types = encodingAttr.getElementTypesArray();
+  Type f16 = Float16Type::get(encoding.getContext());
+  Type f32 = Float32Type::get(encoding.getContext());
+  if (types.size() != 3 || types[0] != f16 || types[1] != f16 ||
+      types[2] != f32) {
+    return {};
+  }
+  return IREE::GPU::DataTiledMMAAttr::get(
+      encoding.getContext(), IREE::GPU::MMAIntrinsic::MFMA_F32_16x16x16_F16, 8,
+      2, 4, 4, 1);
 }
 
 //===----------------------------------------------------------------------===//
