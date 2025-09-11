7b90aeac207db55dce7a6af9ed0dab05ec029c03
Ivan Ho <ivan.ho@cl.cam.ac.uk>
2025-09-11 11:33:54 +0100

    removed tiling interface
    Signed-off-by: Ivan Ho <ivan.ho@cl.cam.ac.uk>

--- compiler/src/iree/compiler/Dialect/LinalgExt/IR/LinalgExtOps.cpp
+++ compiler/src/iree/compiler/Dialect/LinalgExt/IR/LinalgExtOps.cpp
@@ -2250,19 +2250,6 @@ LogicalResult ExpReductionOp::verify() {
   return success();
 }
 
-std::string ExpReductionOp::getLibraryCallName() { return ""; }
-
-bool ExpReductionOp::hasIndexSemantics() {
-  // TODO: Index semantics are valid really.
-  return false;
-}
-
-SmallVector<utils::IteratorType> ExpReductionOp::getIteratorTypesArray() {
-  return llvm::map_to_vector(getIteratorTypes(), [](Attribute attr) {
-    return cast<IREE::LinalgExt::IteratorTypeAttr>(attr).getValue();
-  });
-}
-
 //===----------------------------------------------------------------------===//
 // Im2colOp
 //===----------------------------------------------------------------------===//
