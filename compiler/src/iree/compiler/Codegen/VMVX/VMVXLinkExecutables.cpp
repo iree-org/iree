28c63e32a9458918865fb20a0dab317616cef754
Ben Vanik <ben.vanik@gmail.com>
2025-09-15 08:23:30 -0700

    Adding hal.executable `lazy` flag. (#21966)
    This allows for executables to be hinted as lazy-loadable. Frontends
    lowering into IREE IR can set this flag if it knows certain executables
    will only be used on certain cold code paths. Currently the
    initialization behavior is unchanged but in the future a new pass or
    modification to MaterializeResourceCachesPass will take the flag into
    account to (possibly) wrap the executable initialization in a
    `hal.device.memoize` op. More plumbing/expressiveness is required to do
    this so this just adds the flag for frontends to target and we'll light
    it up in the future when required.
    
    (this will be used by parameter encoding modules that multi-target and
    don't need to load all encoding executables for all targets just to
    encode for one)

--- compiler/src/iree/compiler/Codegen/VMVX/VMVXLinkExecutables.cpp
+++ compiler/src/iree/compiler/Codegen/VMVX/VMVXLinkExecutables.cpp
@@ -18,62 +18,80 @@ namespace mlir::iree_compiler {
 
 namespace {
 
+static LogicalResult linkAllTargetExecutables(StringRef target, bool lazy,
+                                              ModuleOp moduleOp,
+                                              OpBuilder &moduleBuilder) {
+  auto sourceExecutableOps = gatherExecutablesForTarget(moduleOp, target);
+  if (sourceExecutableOps.size() <= 1) {
+    return success();
+  }
+
+  // Guess a module name, if needed, to make the output files readable.
+  auto moduleName = guessModuleName(moduleOp, "module");
+
+  // Create our new "linked" hal.executable.
+  std::string linkedExecutableName =
+      llvm::formatv("{}_linked_{}", moduleName, target);
+  auto linkedExecutableOp = IREE::HAL::ExecutableOp::create(
+      moduleBuilder, moduleOp.getLoc(), linkedExecutableName);
+  linkedExecutableOp.setVisibility(sourceExecutableOps.front().getVisibility());
+  auto executableBuilder =
+      OpBuilder::atBlockBegin(&linkedExecutableOp.getBlock());
+
+  // Gather all unique executable targets - we may have multiple.
+  auto executableTargetAttrs = gatherExecutableTargets(sourceExecutableOps);
+  for (auto [index, targetAttr] : llvm::enumerate(executableTargetAttrs)) {
+    // Add our VMVX hal.executable.variant with an empty module.
+    std::string linkedVariantName =
+        executableTargetAttrs.size() == 1
+            ? targetAttr.getSymbolNameFragment()
+            : llvm::formatv("{}_{}", targetAttr.getSymbolNameFragment(), index);
+    auto linkedTargetOp = IREE::HAL::ExecutableVariantOp::create(
+        executableBuilder, moduleOp.getLoc(), linkedVariantName, targetAttr);
+    auto targetBuilder = OpBuilder::atBlockBegin(&linkedTargetOp.getBlock());
+    auto linkedModuleOp = ModuleOp::create(targetBuilder, moduleOp.getLoc());
+
+    // Add an empty vm.module to that module as our vm.funcs must live in it.
+    auto nestedBuilder = OpBuilder::atBlockBegin(linkedModuleOp.getBody());
+    IREE::VM::ModuleOp::create(nestedBuilder, moduleOp.getLoc(),
+                               "linked_module");
+
+    auto mergeModuleFn = [](mlir::ModuleOp sourceInnerModule,
+                            mlir::ModuleOp linkedInnerModule,
+                            DenseMap<StringRef, Operation *> &symbolMap) {
+      auto srcModule = sourceInnerModule.getOps<IREE::VM::ModuleOp>().begin();
+      auto dstModule = linkedInnerModule.getOps<IREE::VM::ModuleOp>().begin();
+      return mergeModuleInto(*srcModule, *dstModule, symbolMap);
+    };
+
+    // Try linking together all executable variants for this target.
+    if (failed(linkExecutablesInto(moduleOp, sourceExecutableOps,
+                                   linkedExecutableOp, linkedTargetOp,
+                                   mergeModuleFn))) {
+      return failure();
+    }
+  }
+
+  // Remove if we didn't add anything.
+  if (linkedExecutableOp.getBody().empty()) {
+    linkedExecutableOp.erase();
+  }
+  return success();
+}
+
 struct VMVXLinkExecutablesPass
     : public impl::VMVXLinkExecutablesPassBase<VMVXLinkExecutablesPass> {
   void runOnOperation() override {
     mlir::ModuleOp moduleOp = getOperation();
     auto moduleBuilder = OpBuilder::atBlockBegin(moduleOp.getBody());
-
-    auto sourceExecutableOps = gatherExecutablesForTarget(moduleOp, "vmvx");
-    if (sourceExecutableOps.size() <= 1)
-      return;
-
-    // Guess a module name, if needed, to make the output files readable.
-    auto moduleName = guessModuleName(moduleOp, "vmvx_module");
-
-    // Create our new "linked" hal.executable.
-    std::string linkedExecutableName =
-        llvm::formatv("{}_linked_{}", moduleName, "vmvx");
-    auto linkedExecutableOp = IREE::HAL::ExecutableOp::create(
-        moduleBuilder, moduleOp.getLoc(), linkedExecutableName);
-    linkedExecutableOp.setVisibility(
-        sourceExecutableOps.front().getVisibility());
-    auto executableBuilder =
-        OpBuilder::atBlockBegin(&linkedExecutableOp.getBlock());
-
-    // Gather all unique executable targets - we may have multiple.
-    auto executableTargetAttrs = gatherExecutableTargets(sourceExecutableOps);
-    for (auto [index, targetAttr] : llvm::enumerate(executableTargetAttrs)) {
-      // Add our VMVX hal.executable.variant with an empty module.
-      std::string linkedVariantName =
-          executableTargetAttrs.size() == 1
-              ? targetAttr.getSymbolNameFragment()
-              : llvm::formatv("{}_{}", targetAttr.getSymbolNameFragment(),
-                              index);
-      auto linkedTargetOp = IREE::HAL::ExecutableVariantOp::create(
-          executableBuilder, moduleOp.getLoc(), linkedVariantName, targetAttr);
-      auto targetBuilder = OpBuilder::atBlockBegin(&linkedTargetOp.getBlock());
-      auto linkedModuleOp = ModuleOp::create(targetBuilder, moduleOp.getLoc());
-
-      // Add an empty vm.module to that module as our vm.funcs must live in it.
-      auto nestedBuilder = OpBuilder::atBlockBegin(linkedModuleOp.getBody());
-      IREE::VM::ModuleOp::create(nestedBuilder, moduleOp.getLoc(),
-                                 "linked_module");
-
-      auto mergeModuleFn = [](mlir::ModuleOp sourceInnerModule,
-                              mlir::ModuleOp linkedInnerModule,
-                              DenseMap<StringRef, Operation *> &symbolMap) {
-        auto srcModule = sourceInnerModule.getOps<IREE::VM::ModuleOp>().begin();
-        auto dstModule = linkedInnerModule.getOps<IREE::VM::ModuleOp>().begin();
-        return mergeModuleInto(*srcModule, *dstModule, symbolMap);
-      };
-
-      // Try linking together all executable variants for this target.
-      if (failed(linkExecutablesInto(moduleOp, sourceExecutableOps,
-                                     linkedExecutableOp, linkedTargetOp,
-                                     mergeModuleFn))) {
-        return signalPassFailure();
-      }
+    StringRef target = "vmvx";
+    if (failed(linkAllTargetExecutables(target, /*lazy=*/false, moduleOp,
+                                        moduleBuilder))) {
+      return signalPassFailure();
+    }
+    if (failed(linkAllTargetExecutables(target, /*lazy=*/true, moduleOp,
+                                        moduleBuilder))) {
+      return signalPassFailure();
     }
   }
 };
