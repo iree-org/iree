e79baab9970ba5d9254966ab39f2ea58f227491c
Eric Feng <Eric.Feng@amd.com>
2025-10-13 17:00:59 -0700

    generalize to all linalg ops to fix test + refactor common methods
    Signed-off-by: Eric Feng <Eric.Feng@amd.com>

--- compiler/src/iree/compiler/Codegen/Common/GPU/GPUExpandDimensions.cpp
+++ compiler/src/iree/compiler/Codegen/Common/GPU/GPUExpandDimensions.cpp
@@ -9,15 +9,14 @@
 #include "iree/compiler/Codegen/Dialect/GPU/IR/GPULoweringConfigUtils.h"
 #include "iree/compiler/Codegen/Dialect/GPU/IR/IREEGPUAttrs.h"
 #include "iree/compiler/Codegen/Dialect/GPU/IR/IREEGPUEnums.h"
+#include "iree/compiler/Dialect/LinalgExt/IR/LinalgExtOps.h"
 #include "iree/compiler/Dialect/LinalgExt/Transforms/Transforms.h"
-#include "iree/compiler/Dialect/Util/IR/UtilOps.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/STLForwardCompat.h"
-#include "mlir/Dialect/Affine/IR/AffineOps.h"
-#include "mlir/Dialect/Arith/Utils/Utils.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Dialect/MemRef/Transforms/Transforms.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
@@ -29,17 +28,6 @@ namespace mlir::iree_compiler {
 #include "iree/compiler/Codegen/Common/GPU/Passes.h.inc"
 
 namespace {
-
-struct RemoveOptimizationBarrier final
-    : public OpRewritePattern<IREE::Util::OptimizationBarrierOp> {
-  using Base::Base;
-
-  LogicalResult matchAndRewrite(IREE::Util::OptimizationBarrierOp barrierOp,
-                                PatternRewriter &rewriter) const override {
-    rewriter.replaceOp(barrierOp, barrierOp.getOperands());
-    return success();
-  }
-};
 
 struct GPUExpandDimensionsPass final
     : impl::GPUExpandDimensionsPassBase<GPUExpandDimensionsPass> {
@@ -87,84 +75,8 @@ getExpansionInfo(IREE::GPU::LoweringConfigAttr config) {
   return expansionInfo;
 }
 
-/// Helper struct to hold the expand/collapse shape ops created for dimension
-/// expansion.
-struct ReshapeOps {
-  tensor::ExpandShapeOp expandShapeOp;
-  tensor::CollapseShapeOp collapseShapeOp;
-};
-
-/// For a value `v`, expand dimensions according to `expansionInfo` by
-/// inserting:
-///
-/// ```mlir
-/// %v_expand = tensor.expand_shape %v
-/// %barrier = util.optimization.barrier %v_expand
-/// %v_collapse = tensor.collapse_shape %barrier
-/// ```
-///
-/// where the generated `tensor.expand_shape` and `tensor.collapse_shape` are
-/// inverses of each other. The `util.optimization.barrier` prevents these from
-/// getting folded away during reshape propagation.
-static std::optional<ReshapeOps>
-expandIterationSpaceOfValue(RewriterBase &rewriter,
-                            const DimensionExpansionInfo &expansionInfo,
-                            Value v) {
-  auto tensorType = dyn_cast<RankedTensorType>(v.getType());
-  if (!tensorType) {
-    return std::nullopt;
-  }
-
-  SmallVector<OpFoldResult> outputShape;
-  SmallVector<ReassociationIndices> reassociation;
-  Location loc = v.getLoc();
-  SmallVector<OpFoldResult> origShape = tensor::getMixedSizes(rewriter, loc, v);
-
-  for (auto [index, dim] : llvm::enumerate(origShape)) {
-    reassociation.emplace_back(ReassociationIndices{});
-
-    if (!expansionInfo.contains(index)) {
-      reassociation.back().push_back(outputShape.size());
-      outputShape.push_back(dim);
-      continue;
-    }
-
-    int64_t factor = expansionInfo.lookup(index);
-    AffineExpr s0 = rewriter.getAffineSymbolExpr(0);
-    AffineExpr divExpr = s0.floorDiv(factor);
-    OpFoldResult newOuterDim = affine::makeComposedFoldedAffineApply(
-        rewriter, loc, divExpr, ArrayRef<OpFoldResult>{dim});
-    OpFoldResult newInnerDim = rewriter.getIndexAttr(factor);
-
-    reassociation.back().push_back(outputShape.size());
-    reassociation.back().push_back(outputShape.size() + 1);
-
-    outputShape.push_back(newOuterDim);
-    outputShape.push_back(newInnerDim);
-  }
-
-  auto staticOutputShape =
-      llvm::map_to_vector(outputShape, [](OpFoldResult ofr) {
-        if (auto staticShapeAttr = dyn_cast<Attribute>(ofr)) {
-          return cast<IntegerAttr>(staticShapeAttr).getInt();
-        }
-        return ShapedType::kDynamic;
-      });
-  auto outputType = RankedTensorType::get(
-      staticOutputShape, tensorType.getElementType(), tensorType.getEncoding());
-
-  auto expandShapeOp = tensor::ExpandShapeOp::create(
-      rewriter, loc, outputType, v, reassociation, outputShape);
-  Value barrier = IREE::Util::OptimizationBarrierOp::create(
-                      rewriter, loc, expandShapeOp.getResult())
-                      .getResult(0);
-  auto collapseShapeOp = tensor::CollapseShapeOp::create(
-      rewriter, loc, tensorType, barrier, reassociation);
-  return ReshapeOps{expandShapeOp, collapseShapeOp};
-}
-
 static LogicalResult expandIterationSpace(RewriterBase &rewriter,
-                                          linalg::GenericOp genericOp) {
+                                          linalg::LinalgOp genericOp) {
   auto loweringConfig = getLoweringConfig(genericOp);
   if (!loweringConfig)
     return success();
@@ -212,7 +124,7 @@ static LogicalResult expandIterationSpace(RewriterBase &rewriter,
     if (tensorExpansionInfo.empty())
       continue;
 
-    std::optional<ReshapeOps> reshapes = expandIterationSpaceOfValue(
+    std::optional<ReshapeOps> reshapes = createDimensionExpansionOps(
         rewriter, tensorExpansionInfo, operand.get());
     if (reshapes) {
       rewriter.modifyOpInPlace(
@@ -245,7 +157,7 @@ static LogicalResult expandIterationSpace(RewriterBase &rewriter,
       continue;
 
     std::optional<ReshapeOps> reshapes =
-        expandIterationSpaceOfValue(rewriter, tensorExpansionInfo, result);
+        createDimensionExpansionOps(rewriter, tensorExpansionInfo, result);
     if (reshapes) {
       // Replace uses of the result with the collapse_shape, but exclude:
       // - The expand_shape operation itself (it uses the result as input).
@@ -270,7 +182,7 @@ static LogicalResult expandIterationSpace(RewriterBase &rewriter,
 
 static LogicalResult expandIterationSpace(RewriterBase &rewriter,
                                           Operation *operation) {
-  if (auto genericOp = dyn_cast<linalg::GenericOp>(operation))
+  if (auto genericOp = dyn_cast<linalg::LinalgOp>(operation))
     return expandIterationSpace(rewriter, genericOp);
   return success();
 }
@@ -305,19 +217,7 @@ void GPUExpandDimensionsPass::runOnOperation() {
       return !isa_and_nonnull<linalg::FillOp, tensor::EmptyOp>(
           opOperand->get().getDefiningOp());
     };
-    linalg::populateFoldReshapeOpsByExpansionPatterns(bubbleExpandShapePatterns,
-                                                      controlFn);
-    IREE::LinalgExt::populateFoldReshapeOpsByExpansionPatterns(
-        bubbleExpandShapePatterns, controlFn);
-    populateReshapeToInterfaceTensorPatterns(bubbleExpandShapePatterns);
-    populateCombineRelayoutOpPatterns(bubbleExpandShapePatterns);
-    populateFoldTensorReshapeIntoBufferPatterns(bubbleExpandShapePatterns);
-    tensor::populateFoldTensorEmptyPatterns(bubbleExpandShapePatterns);
-    tensor::populateBubbleUpExpandShapePatterns(bubbleExpandShapePatterns);
-    linalg::FillOp::getCanonicalizationPatterns(bubbleExpandShapePatterns,
-                                                context);
-    memref::populateResolveRankedShapedTypeResultDimsPatterns(
-        bubbleExpandShapePatterns);
+    populateReshapePropagationPatterns(bubbleExpandShapePatterns, controlFn);
     if (failed(applyPatternsGreedily(
             operation, std::move(bubbleExpandShapePatterns), config))) {
       operation->emitOpError(
@@ -334,7 +234,7 @@ void GPUExpandDimensionsPass::runOnOperation() {
 
   {
     RewritePatternSet removeBarrierOpsPatterns(context);
-    removeBarrierOpsPatterns.insert<RemoveOptimizationBarrier>(context);
+    populateRemoveOptimizationBarrierPatterns(removeBarrierOpsPatterns);
     tensor::ExpandShapeOp::getCanonicalizationPatterns(removeBarrierOpsPatterns,
                                                        context);
     tensor::CollapseShapeOp::getCanonicalizationPatterns(
