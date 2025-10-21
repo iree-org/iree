3815582bbd42332b39e48e23f086a762e9be98b4
Han-Chung Wang <hanhan0912@gmail.com>
2025-10-21 10:12:23 -0700

    [DT] Implement LayoutMaterializerAttr for identity resolver.  (#22337)
    The identity resolver is one of the basic encoding resolvers that drops
    encodings. The revision implements the interface and retires the
    IREE::Codegen::EncodingNopLayoutAttr.
    
    Fixes https://github.com/iree-org/iree/issues/20846
    
    ---------
    
    Signed-off-by: hanhanW <hanhan0912@gmail.com>

--- compiler/src/iree/compiler/Dialect/Encoding/IR/EncodingAttrs.cpp
+++ compiler/src/iree/compiler/Dialect/Encoding/IR/EncodingAttrs.cpp
@@ -524,6 +524,53 @@ Attribute IdentityResolverAttr::getLayout(RankedTensorType type) const {
   return Encoding::IdentityAttr::get(getContext());
 }
 
+Type IdentityResolverAttr::convertType(Type type) const {
+  using IREE::TensorExt::DispatchTensorType;
+  return TypeSwitch<Type, Type>(type)
+      .Case<RankedTensorType>([&](auto rankedTensorType) {
+        return rankedTensorType.dropEncoding();
+      })
+      .Case<DispatchTensorType>([&](auto dispatchTensorType) {
+        auto boundType =
+            dyn_cast<RankedTensorType>(dispatchTensorType.getBoundType());
+        if (!boundType || !boundType.getEncoding()) {
+          return dispatchTensorType;
+        }
+        Type convertedBoundType = this->convertType(boundType);
+        return DispatchTensorType::get(dispatchTensorType.getAccess(),
+                                       convertedBoundType);
+      })
+      .Default([&](auto concreteType) { return concreteType; });
+}
+
+LogicalResult IdentityResolverAttr::getOffsetsSizesStrides(
+    OpBuilder &builder, Location loc, IREE::TensorExt::DispatchTensorType type,
+    ValueRange dynamicDims, ArrayRef<OpFoldResult> offsets,
+    ArrayRef<OpFoldResult> sizes, ArrayRef<OpFoldResult> strides,
+    SmallVectorImpl<OpFoldResult> &newOffsets,
+    SmallVectorImpl<OpFoldResult> &newSizes,
+    SmallVectorImpl<OpFoldResult> &newStrides) const {
+  // Only handle cases where the slice spans the whole
+  // `!iree_tensor_ext.dispatch.tensor` type.
+  // TODO(hanchung): Enable partial slices. It was copied from pattern's
+  // implementaion, i.e., the users, and it can be dropped after we move the
+  // checks to the interface implementations.
+  if (!type.doesSliceSpanWholeTensor(dynamicDims, offsets, sizes, strides)) {
+    return failure();
+  }
+  auto boundTensorType = cast<RankedTensorType>(type.getBoundType());
+  newSizes = getMixedValues(boundTensorType.getShape(), dynamicDims, builder);
+  newOffsets.resize(newSizes.size(), builder.getIndexAttr(0));
+  newStrides.resize(newSizes.size(), builder.getIndexAttr(1));
+  return success();
+}
+
+Operation *IdentityResolverAttr::lowerOp(OpBuilder &b, Operation *op,
+                                         TypeRange convertedResTypes,
+                                         ValueRange convertedOperands) const {
+  return clone(b, op, convertedResTypes, convertedOperands);
+}
+
 //===---------------------------------------------------------------------===//
 // iree_encoding.unsupported_resolver
 //===---------------------------------------------------------------------===//
