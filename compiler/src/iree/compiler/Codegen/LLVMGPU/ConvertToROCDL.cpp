ed0df578abe27ba4c8eecb3741fadfa326f72a71
James Newling <james.newling@gmail.com>
2025-10-23 09:42:32 -0700

    Revert "Reapply "Re-enable lds_barrier on RDNA4" (#22278) (#22326)"
    This reverts commit f732710ec273e4bfaea882e90dfc4eea3abf77be.
    
    Signed-off-by: James Newling <james.newling@gmail.com>

--- compiler/src/iree/compiler/Codegen/LLVMGPU/ConvertToROCDL.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/ConvertToROCDL.cpp
@@ -69,8 +69,13 @@ struct ReplaceGPUBarrierWithLDSBarrier
   }
 };
 
-static void populateConvertGPUToAMDGPUPatterns(RewritePatternSet &patterns) {
-  patterns.add<ReplaceGPUBarrierWithLDSBarrier>(patterns.getContext());
+static void populateConvertGPUToAMDGPUPatterns(RewritePatternSet &patterns,
+                                               const amdgpu::Chipset &chipset) {
+  // TODO(kdrewnia): This if statement is an emergency fix for an incorrect
+  // lowering of amdgpu.lds_barrier.
+  if (chipset.majorVersion != 12) {
+    patterns.add<ReplaceGPUBarrierWithLDSBarrier>(patterns.getContext());
+  }
 }
 
 /// Hacky pattern to swap `s_setprio` operations with `amdgpu.mfma` ops.
@@ -250,7 +255,7 @@ struct ConvertToROCDLPass final
           /*chipset=*/*maybeChipset);
       arith::populateCeilFloorDivExpandOpsPatterns(patterns);
       populateSwapSetPrioWithMFMAPatterns(patterns);
-      populateConvertGPUToAMDGPUPatterns(patterns);
+      populateConvertGPUToAMDGPUPatterns(patterns, *maybeChipset);
       populateConvertSharedMemoryAllocOps(patterns);
       populateDropSharedMemoryDeallocOpPatterns(patterns);
       vector::populateVectorToVectorCanonicalizationPatterns(patterns);
