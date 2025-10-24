00fa43a3f0f09fbd10d7bfc26d13a60e1b929d4a
Quinn Dawkins <quinn@nod-labs.com>
2025-10-24 08:32:27 -0400

    [WIP] Add pass for converting workgroup foralls to pcf

---
+++ compiler/src/iree/compiler/Codegen/Common/ConvertWorkgroupForallToPCF.cpp
@@ -0,0 +1,51 @@
+// Copyright 2025 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "iree/compiler/Codegen/Common/Passes.h"
+#include "iree/compiler/Codegen/Dialect/Codegen/IR/IREECodegenAttrs.h"
+#include "iree/compiler/Codegen/Dialect/PCF/IR/PCF.h"
+#include "iree/compiler/Codegen/Dialect/PCF/Transforms/Transforms.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Transforms/WalkPatternRewriteDriver.h"
+
+namespace mlir::iree_compiler {
+
+#define GEN_PASS_DEF_CONVERTWORKGROUPFORALLTOPCFPASS
+#include "iree/compiler/Codegen/Common/Passes.h.inc"
+
+namespace {
+struct ConvertWorkgroupForall : public OpRewritePattern<scf::ForallOp> {
+  using OpRewritePattern<scf::ForallOp>::OpRewritePattern;
+  LogicalResult matchAndRewrite(scf::ForallOp op,
+                                PatternRewriter &rewriter) const override;
+};
+
+struct ConvertWorkgroupForallToPCFPass final
+    : public impl::ConvertWorkgroupForallToPCFPassBase<
+          ConvertWorkgroupForallToPCFPass> {
+  void runOnOperation() override;
+  using Base::Base;
+};
+} // namespace
+
+// DO NOT SUBMIT: Write tests.
+LogicalResult
+ConvertWorkgroupForall::matchAndRewrite(scf::ForallOp op,
+                                        PatternRewriter &rewriter) const {
+  // TODO: Sort and linearize/delinearize ids.
+  // Interface is implemented via external models hence the cast.
+  auto scope = cast<IREE::PCF::ScopeAttr>(
+      IREE::Codegen::WorkgroupAttr::get(rewriter.getContext()));
+  return convertForallToPCF(rewriter, op, scope);
+}
+
+void ConvertWorkgroupForallToPCFPass::runOnOperation() {
+  RewritePatternSet patterns(&getContext());
+  patterns.add<ConvertWorkgroupForall>(&getContext());
+  walkAndApplyPatterns(getOperation(), std::move(patterns));
+}
+
+} // namespace mlir::iree_compiler
