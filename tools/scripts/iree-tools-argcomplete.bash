1c72bc0ffb8406c7e555c8386d01ad545ff27fec
Kunwar Grover <groverkss@gmail.com>
2025-10-27 16:50:05 +0000

    [tools] Add bash autocomplete script for iree-opt/iree-compile

---
+++ tools/scripts/iree-tools-argcomplete.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Include this script in your shell to enable argument completion for IREE tools.
+
+_iree_tools_autocomplete() {
+    local tool="$1"
+    local commands
+    local cur
+
+    # Get the list of commands from the appropriate tool's `--help-list`
+    commands=$($tool --help-list | awk '{print $1}' | grep -v '^ *=' | sed 's/[=<].*//')
+
+    cur=${COMP_WORDS[COMP_CWORD]}
+
+    case "$cur" in
+      -*)
+        COMPREPLY=($(compgen -W "$commands" -- "${cur}"))
+        ;;
+      *)
+        COMPREPLY=($(compgen -f -- "${cur}"))
+        ;;
+    esac
+
+    return 0
+}
+
+# Register the completion function for iree-opt
+complete -F _iree_tools_autocomplete iree-opt
+# Register the completion function for iree-compile
+complete -F _iree_tools_autocomplete iree-compile
