710be474657009621afe15d288a9c02c069f38ce
Eric Feng <Eric.Feng@amd.com>
2025-11-03 16:27:18 -0800

    bring in changes for dim exp
    Signed-off-by: Eric Feng <Eric.Feng@amd.com>

--- compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
@@ -770,6 +770,8 @@ void addGPUVectorDistributePassPipeline(OpPassManager &funcPassManager,
   funcPassManager.addPass(createCSEPass());
   funcPassManager.addPass(createGPUPromoteMatmulOperandsPass());
 
+  funcPassManager.addPass(createGPUExpandDimensionsPass());
+
   // Tile to reduction loops.
   {
     GPUApplyTilingLevelPassOptions options;
@@ -1115,6 +1117,9 @@ static void buildLLVMGPUCodegenCommonConfigurationPassPipelineImpl(
   {
     FunctionLikeNest funcPassManager(modulePassManager);
     funcPassManager.addPass(createMaterializeDeviceEncodingPass);
+    // TODO(#20160): Combine the EncodingToPaddingPasses with the
+    // MaterializeDeviceEncodingPass.
+    addEncodingToPaddingPasses(funcPassManager);
     funcPassManager.addPass(createGPUGeneralizeNamedOpsPass);
     funcPassManager.addPass(createROCDLConfigureBufferInstructionsPass);
     addCommonTargetExecutablePreprocessingPasses(funcPassManager);
