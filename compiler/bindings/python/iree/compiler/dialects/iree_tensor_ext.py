aa3734d5cf49a55e8c4ecef546e378e863638d94
Rahul Kayaith <rkayaith@gmail.com>
2025-12-09 17:42:00 -0500

    [bindings] Add `iree_tensor_ext` to python bindings (#22872)
    This is for use downstream in iree-turbine.
    
    Signed-off-by: Keshav Vinayak Jha <keshavvinayakjha@gmail.com>

---
+++ compiler/bindings/python/iree/compiler/dialects/iree_tensor_ext.py
@@ -0,0 +1,8 @@
+# Copyright 2025 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+from ._iree_tensor_ext_ops_gen import *
+from ._iree_tensor_ext_enum_gen import *
