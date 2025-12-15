43031b74fa29dab98abbad13efa951b6a71f6262
Jorn Tuyls <jorn.tuyls@gmail.com>
2025-12-15 05:03:53 -0600

    [Encoding] Add dynamic encoding dims to (Un)SetEncodingOp
    Signed-off-by: Jorn Tuyls <jorn.tuyls@gmail.com>

--- compiler/src/iree/compiler/DispatchCreation/HoistEncodingOps.cpp
+++ compiler/src/iree/compiler/DispatchCreation/HoistEncodingOps.cpp
@@ -110,7 +110,7 @@ bubbleUpSetEncodingThroughGenericOp(RewriterBase &rewriter,
     auto resType = RankedTensorType::get(
         operandType.getShape(), operandType.getElementType(), newEncoding);
     Value encodedInput = IREE::Encoding::SetEncodingOp::create(
-        rewriter, loc, resType, operand->get());
+        rewriter, loc, resType, operand->get(), /*encodingDims=*/ValueRange{});
     encodedOperands.push_back(encodedInput);
   }
 
