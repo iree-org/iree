aa1052af8e830ebfa09843632e98be646c8cbbcc
Bangtian Liu <liubangtian@gmail.com>
2025-12-25 23:44:33 -0500

    use empty commit to check ci

--- compiler/src/iree/compiler/Codegen/Common/Transforms.cpp
+++ compiler/src/iree/compiler/Codegen/Common/Transforms.cpp
@@ -712,6 +712,7 @@ struct SwapCollapseShapeWithSlicePattern
 } // namespace
 
 void populateSwapExtractWithCollapsePattern(RewritePatternSet &patterns) {
+  //
   patterns.add<SwapCollapseShapeWithSlicePattern>(patterns.getContext());
 }
 
