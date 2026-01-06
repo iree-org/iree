27a207c8544478349a07f9c0d7dabdd1febbf8e8
yzhang93 <zhyuhang88@gmail.com>
2026-01-06 12:47:15 -0800

    Add a new flag
    Signed-off-by: yzhang93 <zhyuhang88@gmail.com>

--- compiler/src/iree/compiler/GlobalOptimization/Passes.cpp
+++ compiler/src/iree/compiler/GlobalOptimization/Passes.cpp
@@ -29,6 +29,11 @@ static llvm::cl::opt<bool> clEnableTransposePropagation(
     llvm::cl::desc(
         "Enables propagation of transpose ops to improve fusion chances."),
     llvm::cl::init(true));
+static llvm::cl::opt<bool> clEnableConvolutionPropagation(
+    "iree-global-opt-propagate-transposes-through-conv",
+    llvm::cl::desc(
+        "Enables propagation of transpose ops through convolutions."),
+    llvm::cl::init(false));
 static llvm::cl::opt<bool> clEnableAttentionVTranspose(
     "iree-global-opt-enable-attention-v-transpose",
     llvm::cl::desc("Enables transposition of v operand of attention ops,"),
@@ -179,7 +184,7 @@ void buildGlobalOptimizationPassPipeline(
                            options.enableAggressivePropagation =
                                transformOptions.aggressiveTransposePropagation;
                            options.enableConvolutionPropagation =
-                               transformOptions.aggressiveTransposePropagation;
+                               clEnableConvolutionPropagation;
                            options.enableAttentionVTranspose =
                                clEnableAttentionVTranspose;
                            options.enableEdgeReshapePropagation =
