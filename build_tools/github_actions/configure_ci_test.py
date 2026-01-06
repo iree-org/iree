456b5ace7d350483f1d6cb47d4ed061793785b4c
Erick Ochoa <erick.ochoalopez@amd.com>
2026-01-06 16:39:19 -0500

    Add test for llvm integrate with ci-extra

--- build_tools/github_actions/configure_ci_test.py
+++ build_tools/github_actions/configure_ci_test.py
@@ -130,6 +130,23 @@ class ConfigureCITest(unittest.TestCase):
         )
         self.assertCountEqual(jobs, all_jobs)
 
+    def test_get_enabled_jobs_llvm_integrate_with_ci_extra(self):
+        trailers = {"ci-extra": "linux_x64_clang_tsan"}
+        default_jobs = {"job1", "job2", "job3"}
+        postsubmit_job = next(iter(configure_ci.DEFAULT_POSTSUBMIT_ONLY_JOBS))
+        all_jobs = default_jobs | {postsubmit_job} | {"linux_x64_clang_tsan"}
+        is_pr = True
+        is_llvm_integrate_pr = True
+        modified_paths = ["runtime/file"]
+        jobs = configure_ci.get_enabled_jobs(
+            trailers,
+            all_jobs,
+            modified_paths=modified_paths,
+            is_pr=is_pr,
+            is_llvm_integrate_pr=is_llvm_integrate_pr,
+        )
+        self.assertEqual(jobs, all_jobs)
+
     def test_get_enabled_jobs_no_modifies(self):
         trailers = {}
         default_jobs = {"job1", "job2", "job3"}
