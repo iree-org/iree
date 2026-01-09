c4c3fbd5e961ce3ab9b9f913f2a6392fb22854fe
Abhishek Varma <abhvarma@amd.com>
2026-01-09 09:12:17 +0000

    [LinalgExt] Implement generateScalarImplementation for map_gather
    -- This commit implements `generateScalarImplementation` for `map_gather` op.
    
    Signed-off-by: Abhishek Varma <abhvarma@amd.com>

--- compiler/src/iree/compiler/Dialect/LinalgExt/IR/LinalgExtOps.cpp
+++ compiler/src/iree/compiler/Dialect/LinalgExt/IR/LinalgExtOps.cpp
@@ -656,6 +656,36 @@ void MapGatherOp::insertTransformationAtStart(
   transformBody.eraseArguments(0, oldOutputIndices.size());
 }
 
+void MapGatherOp::inlineMapGatherBody(
+    OpBuilder &b, Location loc, ValueRange transformBodyIndices,
+    function_ref<void(OpBuilder &, Location, ArrayRef<Value>)> bodyBuilder) {
+  Block &transformBlock = getTransformationRegion().front();
+  IRMapping mapping;
+  // Map the induction variables of the loop nest to the block arguments of the
+  // transformation body. The induction variables are the indices looping over
+  // the elements of output operand.
+  for (auto [idx, arg] : llvm::enumerate(transformBlock.getArguments())) {
+    mapping.map(arg, transformBodyIndices[idx]);
+  }
+  // Clone the operations within the transformation body to the current
+  // insertion point, and map their results to the new cloned operations'
+  // results.
+  for (Operation &op : transformBlock.without_terminator()) {
+    Operation *clonedOp = b.clone(op, mapping);
+    for (auto [result, clonedResult] :
+         llvm::zip_equal(op.getResults(), clonedOp->getResults())) {
+      mapping.map(result, clonedResult);
+    }
+  }
+
+  // Get the cloned values that were yielded by the transformation body to pass
+  // to the bodyBuilder.
+  SmallVector<Value> mappedYieldedValues = llvm::map_to_vector(
+      transformBlock.getTerminator()->getOperands(),
+      [&](Value operand) -> Value { return mapping.lookupOrDefault(operand); });
+  bodyBuilder(b, loc, mappedYieldedValues);
+}
+
 //===----------------------------------------------------------------------===//
 // MapScatterOp
 //===----------------------------------------------------------------------===//
