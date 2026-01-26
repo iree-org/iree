1ce2fa259f13e54e4babd2ed236f4ce3298a018a
Han-Chung Wang <hanhan0912@gmail.com>
2026-01-26 11:32:34 -0800

    [LLVMCPU] Fix crash in limitVectorTileSizes with dynamic operand shapes. (#23281)
    It did not take dynamic shape into account, which leads to a crash. The
    revision does not adjust the tile size if the bound is dynamic.
    
    Fixes https://github.com/iree-org/iree/issues/23277
    
    ---------
    
    Signed-off-by: hanhanW <hanhan0912@gmail.com>

--- compiler/src/iree/compiler/Codegen/LLVMCPU/KernelDispatch.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMCPU/KernelDispatch.cpp
@@ -787,10 +787,10 @@ static void limitVectorTileSizes(SmallVectorImpl<int64_t> &vecTileSizes,
       }
 
       if (oldVal == 0) {
-        // Skip updating tile sizes of 0, we want to preserve these values.
-        if (!bounds.empty()) {
-          // If we have enough information about the upper bound of this tile
-          // size, use it.
+        // Skip updating tile sizes of 0, we want to preserve these values. If
+        // we do not have enough information about the upper bound of this tile
+        // size, don't use it.
+        if (!bounds.empty() && !ShapedType::isDynamic(bounds[loopNum])) {
           tileBits[i] *= bounds[loopNum];
         }
       } else {
