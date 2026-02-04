10303c0d5bcc2e78cddbf0a747a933127607da0a
Hyun Gyu Kim <kimm240@telepix.net>
2026-02-04 11:20:06 +0900

    Change code convention to llvm::map_to_vector
    Signed-off-by: Hyun Gyu Kim <kimm240@telepix.net>

--- compiler/src/iree/compiler/Codegen/LLVMGPU/ConvertToLLVM.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/ConvertToLLVM.cpp
@@ -363,10 +363,9 @@ public:
       // Stream → HAL conversion. If not, we'll need to preserve it explicitly.
       if (auto correlationAttr = funcOp.getArgAttrOfType<ArrayAttr>(
               binding, "stream.binding_correlation")) {
-        SmallVector<int64_t> correlatedBindings;
-        for (IntegerAttr intAttr : correlationAttr.getAsRange<IntegerAttr>()) {
-          correlatedBindings.push_back(intAttr.getInt());
-        }
+        auto correlatedBindings =
+            llvm::map_to_vector(correlationAttr.getAsRange<IntegerAttr>(),
+                                [](IntegerAttr attr) { return attr.getInt(); });
         if (!correlatedBindings.empty()) {
           bindingCorrelationMap[binding] = std::move(correlatedBindings);
         }
@@ -374,10 +373,9 @@ public:
       // Read noalias information (bindings pointing to different resources).
       if (auto noaliasAttr = funcOp.getArgAttrOfType<ArrayAttr>(
               binding, "stream.binding_noalias")) {
-        SmallVector<int64_t> noaliasBindings;
-        for (IntegerAttr intAttr : noaliasAttr.getAsRange<IntegerAttr>()) {
-          noaliasBindings.push_back(intAttr.getInt());
-        }
+        auto noaliasBindings =
+            llvm::map_to_vector(noaliasAttr.getAsRange<IntegerAttr>(),
+                                [](IntegerAttr attr) { return attr.getInt(); });
         if (!noaliasBindings.empty()) {
           bindingNoaliasMap[binding] = std::move(noaliasBindings);
         }
