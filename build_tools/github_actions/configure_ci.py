0451f1a77bfb5885bc9415c5cd90d3dfd0f0e58c
Erick Ochoa <erick.ochoalopez@amd.com>
2026-02-09 08:39:11 -0500

    Move linux_x64_clang_debug to postsubmit

--- build_tools/github_actions/configure_ci.py
+++ build_tools/github_actions/configure_ci.py
@@ -120,6 +120,7 @@ DEFAULT_POSTSUBMIT_ONLY_JOBS = frozenset(
     [
         "windows_x64_msvc",
         "test_torch",
+        "linux_x64_clang_debug",
     ]
 )
 
@@ -131,7 +132,6 @@ DEFAULT_SCHEDULE_ONLY_JOBS = frozenset(
         "macos_x64_clang",
         "linux_arm64_clang",
         "linux_x64_clang_byollvm",
-        "linux_x64_clang_debug",
         "linux_x64_clang_tsan",
         "linux_x64_gcc",
     ]
