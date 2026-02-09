e13bc59667e21fa43c2471b7cfc80fc06d133ecf
Max191 <44243577+Max191@users.noreply.github.com>
2026-02-09 16:15:59 -0500

    [LLVMGPU] Fix barrier handling when pipelining with non-transfer_read ops (#23435)
    Software pipelining removes barriers from the pipelined loop, and then
    adds them back based on the loads/stores in the schedule, but it
    performs specific matching for vector.transfer_read ops. When we use
    other operations, like amdgpu.transpose_load, instead of
    vector.transfer_read, this causes barriers to be removed from the IR,
    causing data race possibilities, and in some cases performance issues.
    
    This PR fixes the issue by using the MemoryEffectsOpInterface instead of
    explicit transfer_read matching.
    
    Fixes https://github.com/iree-org/iree/issues/23421
    
    ---------
    
    Signed-off-by: Max Dawkins <max.dawkins@gmail.com>

--- compiler/src/iree/compiler/Codegen/LLVMGPU/Utils/ROCDLPrefetchSharedMemoryCopy.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/Utils/ROCDLPrefetchSharedMemoryCopy.cpp
@@ -19,6 +19,7 @@
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/GPU/IR/GPUDialect.h"
 #include "mlir/Dialect/MemRef/Transforms/Transforms.h"
+#include "mlir/Dialect/MemRef/Utils/MemRefUtils.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/Dialect/SCF/Transforms/Transforms.h"
 #include "mlir/Dialect/Vector/IR/VectorOps.h"
@@ -756,8 +757,21 @@ static bool hasSharedMemory(Value val) {
 // Helper to check if operation or its nested ops have shared memory reads.
 static bool hasNestedSharedRead(Operation *op) {
   bool found = false;
-  op->walk([&](vector::TransferReadOp readOp) {
-    if (hasSharedMemory(readOp.getBase())) {
+  op->walk([&](MemoryEffectOpInterface memoryEffectOp) {
+    SmallVector<MemoryEffects::EffectInstance> effects;
+    memoryEffectOp.getEffects(effects);
+    for (MemoryEffects::EffectInstance effect : effects) {
+      // Ignore non-read effects. We are just looking for read operations.
+      if (!isa<MemoryEffects::Read>(effect.getEffect())) {
+        continue;
+      }
+      // We also only care about shared memory reads.
+      Value readBuffer = effect.getValue();
+      // effect.getValue() could return nullptr, so we also need to check that
+      // readBuffer exists.
+      if (!readBuffer || !hasSharedMemory(readBuffer)) {
+        continue;
+      }
       found = true;
       return WalkResult::interrupt();
     }
