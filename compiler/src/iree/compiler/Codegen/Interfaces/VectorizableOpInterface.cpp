a4dc64baa870c44c42e3a20b8b0ee56498076cef
hanhanW <hanhan0912@gmail.com>
2026-03-02 13:59:29 -0800

    [Codegen] Migrate ArgCompareOp to VectorizableOpInterface
    - Add external model for IREE::LinalgExt::ArgCompareOp, inlining the
      vectorization logic from vectorizeLinalgExtArgCompare.
    - Remove ArgCompareOp from GenericVectorization TypeSwitch and
      candidate isa<> check (now dispatched through interface).
    
    Signed-off-by: hanhanW <hanhan0912@gmail.com>

--- compiler/src/iree/compiler/Codegen/Interfaces/VectorizableOpInterface.cpp
+++ compiler/src/iree/compiler/Codegen/Interfaces/VectorizableOpInterface.cpp
@@ -14,6 +14,7 @@
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Dialect/UB/IR/UBOps.h"
 #include "mlir/Dialect/Vector/IR/VectorOps.h"
+#include "mlir/IR/IRMapping.h"
 
 // clang-format off
 #include "iree/compiler/Codegen/Interfaces/VectorizableOpInterface.cpp.inc"
@@ -155,6 +156,160 @@ struct GatherOpVectorizationModel
   }
 };
 
+struct ArgCompareOpVectorizationModel
+    : public VectorizableOpInterface::ExternalModel<
+          ArgCompareOpVectorizationModel, IREE::LinalgExt::ArgCompareOp> {
+
+  bool isVectorizable(Operation *op, ArrayRef<int64_t> vectorSizes,
+                      ArrayRef<bool> scalableDims,
+                      DictionaryAttr options) const {
+    auto argCompareOp = cast<IREE::LinalgExt::ArgCompareOp>(op);
+    // Only static shapes are supported. Dynamic shapes would require masking.
+    // Check input shape (includes reduction dimension) to catch dynamic
+    // reduction dimensions that wouldn't appear in the static output shape.
+    auto inputValTy = cast<ShapedType>(argCompareOp.getInputValue().getType());
+    if (!inputValTy.hasStaticShape()) {
+      return false;
+    }
+    ShapedType outValTy = argCompareOp.getOutputValueType();
+    SmallVector<int64_t> sizes(vectorSizes);
+    if (sizes.empty()) {
+      sizes = llvm::to_vector(outValTy.getShape());
+    }
+    if (sizes != llvm::to_vector(outValTy.getShape())) {
+      return false;
+    }
+    return true;
+  }
+
+  FailureOr<SmallVector<Value>> vectorize(Operation *op, RewriterBase &rewriter,
+                                          ArrayRef<int64_t> vectorSizes,
+                                          ArrayRef<bool> scalableDims,
+                                          DictionaryAttr options) const {
+    auto argCompareOp = cast<IREE::LinalgExt::ArgCompareOp>(op);
+    Location loc = argCompareOp.getLoc();
+    RewriterBase::InsertionGuard g(rewriter);
+    rewriter.setInsertionPoint(argCompareOp);
+
+    auto inputValTy = cast<ShapedType>(argCompareOp.getInputValue().getType());
+    // Only static shapes are supported. Dynamic shapes would require masking.
+    if (!inputValTy.hasStaticShape()) {
+      return failure();
+    }
+
+    ShapedType outValTy = argCompareOp.getOutputValueType();
+    ShapedType outIdxTy = argCompareOp.getOutputIndexType();
+
+    if (vectorSizes.empty()) {
+      vectorSizes = outValTy.getShape();
+    }
+
+    // Ensure full tiles - partial tiles would require masking support.
+    if (vectorSizes != outValTy.getShape()) {
+      return failure();
+    }
+
+    Value zero = arith::ConstantIndexOp::create(rewriter, loc, 0);
+
+    // Vectorize the DPS input operands (input_value and optional input_index).
+    // We explicitly access these by name to avoid accidentally including
+    // index_base.
+    SmallVector<Value> inputVecs;
+    auto vectorizeInput = [&](Value input) {
+      auto inputTy = cast<ShapedType>(input.getType());
+      SmallVector<Value> readIndices(inputTy.getRank(), zero);
+      auto inputVecTy =
+          VectorType::get(inputTy.getShape(), inputTy.getElementType());
+      // TODO(Bangtian): Add masking/padding support for partial tiles.
+      // Currently passes std::nullopt, assuming vector size matches tensor
+      // shape.
+      auto readOp = vector::TransferReadOp::create(
+          rewriter, loc, inputVecTy, input, readIndices, std::nullopt);
+      inputVecs.push_back(readOp);
+    };
+
+    vectorizeInput(argCompareOp.getInputValue());
+    if (Value inputIndex = argCompareOp.getInputIndex()) {
+      vectorizeInput(inputIndex);
+    }
+
+    SmallVector<Value> initVecs;
+    for (Value init : argCompareOp.getDpsInits()) {
+      auto initTy = cast<ShapedType>(init.getType());
+      SmallVector<Value> readIndices(vectorSizes.size(), zero);
+      auto initVecTy = VectorType::get(vectorSizes, initTy.getElementType());
+      auto readOp = vector::TransferReadOp::create(
+          rewriter, loc, initVecTy, init, readIndices, std::nullopt);
+      initVecs.push_back(readOp);
+    }
+
+    auto outValVecTy = VectorType::get(vectorSizes, outValTy.getElementType());
+    auto outIdxVecTy = VectorType::get(vectorSizes, outIdxTy.getElementType());
+
+    Region &srcRegion = argCompareOp.getRegion();
+
+    // Create the vector arg_compare operation using the builder that takes
+    // individual operands (this properly handles AttrSizedOperandSegments).
+    Value inputIndex = (inputVecs.size() > 1) ? inputVecs[1] : Value();
+    Value indexBase = argCompareOp.getIndexBase();
+
+    auto vectorArgCompareOp = IREE::VectorExt::ArgCompareOp::create(
+        rewriter, loc, TypeRange{outValVecTy, outIdxVecTy},
+        /*input_value=*/inputVecs[0],
+        /*input_index=*/inputIndex,
+        /*init_value=*/initVecs[0],
+        /*init_index=*/initVecs[1],
+        /*index_base=*/indexBase,
+        /*dimension=*/argCompareOp.getDimension());
+
+    Block *srcBlock = &srcRegion.front();
+    Region &dstRegion = vectorArgCompareOp.getRegion();
+    rewriter.modifyOpInPlace(vectorArgCompareOp,
+                             [&]() { dstRegion.getBlocks().clear(); });
+    SmallVector<Location> argLocs(srcBlock->getNumArguments(), loc);
+    Block *dstBlock = rewriter.createBlock(
+        &dstRegion, dstRegion.end(), srcBlock->getArgumentTypes(), argLocs);
+
+    // Clone operations from source block to destination block using rewriter.
+    IRMapping mapper;
+    for (auto [srcArg, dstArg] :
+         llvm::zip_equal(srcBlock->getArguments(), dstBlock->getArguments())) {
+      mapper.map(srcArg, dstArg);
+    }
+
+    rewriter.setInsertionPointToStart(dstBlock);
+    for (Operation &bodyOp : srcBlock->getOperations()) {
+      auto yieldOp = dyn_cast<IREE::LinalgExt::YieldOp>(bodyOp);
+      if (!yieldOp) {
+        rewriter.clone(bodyOp, mapper);
+        continue;
+      }
+      // Replace LinalgExt::YieldOp with VectorExt::YieldOp.
+      SmallVector<Value> mappedOperands;
+      for (Value operand : yieldOp.getOperands()) {
+        mappedOperands.push_back(mapper.lookup(operand));
+      }
+      IREE::VectorExt::YieldOp::create(rewriter, yieldOp.getLoc(),
+                                       mappedOperands);
+    }
+
+    // Set insertion point to after the vectorArgCompareOp for subsequent
+    // operations.
+    rewriter.setInsertionPointAfter(vectorArgCompareOp);
+
+    SmallVector<Value> results;
+    for (auto [result, output] : llvm::zip_equal(
+             vectorArgCompareOp.getResults(), argCompareOp.getDpsInits())) {
+      SmallVector<Value> writeIndices(vectorSizes.size(), zero);
+      auto writeOp = vector::TransferWriteOp::create(rewriter, loc, result,
+                                                     output, writeIndices);
+      results.push_back(writeOp.getResult());
+    }
+
+    return results;
+  }
+};
+
 } // namespace
 
 void registerVectorizableOpInterfaceExternalModels(DialectRegistry &registry) {
@@ -162,6 +317,8 @@ void registerVectorizableOpInterfaceExternalModels(DialectRegistry &registry) {
       +[](MLIRContext *ctx, IREE::LinalgExt::IREELinalgExtDialect *dialect) {
         IREE::LinalgExt::GatherOp::attachInterface<GatherOpVectorizationModel>(
             *ctx);
+        IREE::LinalgExt::ArgCompareOp::attachInterface<
+            ArgCompareOpVectorizationModel>(*ctx);
       });
 }
 
