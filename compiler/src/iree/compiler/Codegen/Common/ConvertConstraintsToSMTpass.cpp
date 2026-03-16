37091ce5c21e1698222408f96a620a6977b576f3
Amily Wu <amilywu2@amd.com>
2026-03-16 22:13:32 +0000

    Run clang format

--- compiler/src/iree/compiler/Codegen/Common/ConvertConstraintsToSMTpass.cpp
+++ compiler/src/iree/compiler/Codegen/Common/ConvertConstraintsToSMTpass.cpp
@@ -4,10 +4,10 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#include "mlir/Dialect/SMT/IR/SMTOps.h"
 #include "iree/compiler/Codegen/Dialect/Codegen/IR/IREECodegenOps.h"
-#include "mlir/IR/IRMapping.h"
 #include "llvm/ADT/TypeSwitch.h"
+#include "mlir/Dialect/SMT/IR/SMTOps.h"
+#include "mlir/IR/IRMapping.h"
 #include "mlir/Pass/Pass.h"
 
 namespace mlir::iree_compiler {
@@ -15,84 +15,88 @@ namespace mlir::iree_compiler {
 #define GEN_PASS_DEF_CONVERTCONSTRAINTSTOSMTPASS
 #include "iree/compiler/Codegen/Common/Passes.h.inc"
 
+smt::SolverOp convertConstraintsToSMTSolver(IREE::Codegen::ConstraintsOp op,
+                                            OpBuilder &builder) {
+  Location loc = op.getLoc();
 
-smt::SolverOp convertConstraintsToSMTSolver(IREE::Codegen::ConstraintsOp op, OpBuilder &builder) {
-    Location loc = op.getLoc();
+  auto solverOp =
+      smt::SolverOp::create(builder, loc, TypeRange{}, ValueRange{});
 
-    auto solverOp = smt::SolverOp::create(builder, loc, TypeRange{}, ValueRange{});
+  Block *solverBody = &solverOp.getBodyRegion().emplaceBlock();
+  OpBuilder::InsertionGuard guard(builder);
+  builder.setInsertionPointToStart(solverBody);
 
-    Block *solverBody = &solverOp.getBodyRegion().emplaceBlock();
-    OpBuilder::InsertionGuard guard(builder);
-    builder.setInsertionPointToStart(solverBody);
-
-    IRMapping mapping;
-    Block &constraintsBody = op.getBody().front();
-    for (auto [idx, blockArg] : llvm::enumerate(constraintsBody.getArguments())) {
+  IRMapping mapping;
+  Block &constraintsBody = op.getBody().front();
+  for (auto [idx, blockArg] : llvm::enumerate(constraintsBody.getArguments())) {
     std::string name = "problem_dim_" + std::to_string(idx);
     auto declareFun = smt::DeclareFunOp::create(
         builder, loc, blockArg.getType(), builder.getStringAttr(name));
     mapping.map(blockArg, declareFun.getResult());
-    }
+  }
 
-    for (Operation &bodyOp : constraintsBody) {
-        TypeSwitch<Operation *>(&bodyOp)
-            .Case<IREE::Codegen::AssertOp>([&](auto assertOp) {
-                Value mappedCond = mapping.lookupOrDefault(assertOp.getCondition());
-                smt::AssertOp::create(builder, loc, mappedCond);
-            })
-            .Case<IREE::Codegen::KnobOp>([&](auto knobOp) {
-                auto declareFun = smt::DeclareFunOp::create(
-                    builder, loc, knobOp.getResult().getType(), knobOp.getNameAttr());
-                mapping.map(knobOp.getResult(), declareFun.getResult());
-            })
-            .Case<IREE::Codegen::LookupOp>([&](auto lookupOp) {
-                Value mappedIdx = mapping.lookupOrDefault(lookupOp.getIndex());
-                auto keys = lookupOp.getKeys();
-                auto vals = lookupOp.getValues();
+  for (Operation &bodyOp : constraintsBody) {
+    TypeSwitch<Operation *>(&bodyOp)
+        .Case<IREE::Codegen::AssertOp>([&](auto assertOp) {
+          Value mappedCond = mapping.lookupOrDefault(assertOp.getCondition());
+          smt::AssertOp::create(builder, loc, mappedCond);
+        })
+        .Case<IREE::Codegen::KnobOp>([&](auto knobOp) {
+          auto declareFun = smt::DeclareFunOp::create(
+              builder, loc, knobOp.getResult().getType(), knobOp.getNameAttr());
+          mapping.map(knobOp.getResult(), declareFun.getResult());
+        })
+        .Case<IREE::Codegen::LookupOp>([&](auto lookupOp) {
+          Value mappedIdx = mapping.lookupOrDefault(lookupOp.getIndex());
+          auto keys = lookupOp.getKeys();
+          auto vals = lookupOp.getValues();
 
-                Value result = smt::IntConstantOp::create(
-                    builder, loc, builder.getI64IntegerAttr(vals.back()));
+          Value result = smt::IntConstantOp::create(
+              builder, loc, builder.getI64IntegerAttr(vals.back()));
 
-                for (auto [key, val] : llvm::reverse(llvm::zip(keys.drop_back(), vals.drop_back()))) {
-                    Value keyVal = smt::IntConstantOp::create(
-                        builder, loc, builder.getI64IntegerAttr(key));
-                    Value thenVal = smt::IntConstantOp::create(
-                        builder, loc, builder.getI64IntegerAttr(val));
-                    Value cond = smt::EqOp::create(builder, loc, mappedIdx, keyVal);
-                    result = smt::IteOp::create(builder, loc, cond, thenVal, result);
-                }
-                mapping.map(lookupOp.getResult(), result);
-            })
-            .Default([&](Operation *bodyOp) { builder.clone(*bodyOp, mapping); });
-        
-    }
+          for (auto [key, val] :
+               llvm::reverse(llvm::zip(keys.drop_back(), vals.drop_back()))) {
+            Value keyVal = smt::IntConstantOp::create(
+                builder, loc, builder.getI64IntegerAttr(key));
+            Value thenVal = smt::IntConstantOp::create(
+                builder, loc, builder.getI64IntegerAttr(val));
+            Value cond = smt::EqOp::create(builder, loc, mappedIdx, keyVal);
+            result = smt::IteOp::create(builder, loc, cond, thenVal, result);
+          }
+          mapping.map(lookupOp.getResult(), result);
+        })
+        .Default([&](Operation *bodyOp) { builder.clone(*bodyOp, mapping); });
+  }
 
-    // Add smt.yield terminator.
-    smt::YieldOp::create(builder, loc);
-    return solverOp;
+  // Add smt.yield terminator.
+  smt::YieldOp::create(builder, loc);
+  return solverOp;
 }
 
-OwningOpRef<ModuleOp> convertConstraintsToSMTModule(IREE::Codegen::ConstraintsOp op) {
-    OwningOpRef<ModuleOp> tempModule = ModuleOp::create(op->getLoc());
-    OpBuilder builder(tempModule->getBodyRegion());
-    convertConstraintsToSMTSolver(op, builder);
-    return tempModule;
+OwningOpRef<ModuleOp>
+convertConstraintsToSMTModule(IREE::Codegen::ConstraintsOp op) {
+  OwningOpRef<ModuleOp> tempModule = ModuleOp::create(op->getLoc());
+  OpBuilder builder(tempModule->getBodyRegion());
+  convertConstraintsToSMTSolver(op, builder);
+  return tempModule;
 }
 
 namespace {
 
-struct ConvertConstraintsToSMTPass final : impl::ConvertConstraintsToSMTPassBase<ConvertConstraintsToSMTPass> {
-    void runOnOperation() override {
-        auto constraintsOp = getOperation();
-        OpBuilder builder(constraintsOp);
-        auto solverOp = convertConstraintsToSMTSolver(constraintsOp, builder);
-        // Swap the body ops for converted SMT equivalents.
-        // Block args are preserved to match the verifier's arg count check.
-        Block &bodyBlock = constraintsOp.getBody().front();
-        bodyBlock.clear();
-        bodyBlock.getOperations().splice(bodyBlock.end(), solverOp.getBodyRegion().front().getOperations());
-        solverOp.erase();
-    }
+struct ConvertConstraintsToSMTPass final
+    : impl::ConvertConstraintsToSMTPassBase<ConvertConstraintsToSMTPass> {
+  void runOnOperation() override {
+    auto constraintsOp = getOperation();
+    OpBuilder builder(constraintsOp);
+    auto solverOp = convertConstraintsToSMTSolver(constraintsOp, builder);
+    // Swap the body ops for converted SMT equivalents.
+    // Block args are preserved to match the verifier's arg count check.
+    Block &bodyBlock = constraintsOp.getBody().front();
+    bodyBlock.clear();
+    bodyBlock.getOperations().splice(
+        bodyBlock.end(), solverOp.getBodyRegion().front().getOperations());
+    solverOp.erase();
+  }
 };
 
 } // namespace
