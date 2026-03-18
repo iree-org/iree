9d2d0ad0d94275521f5f1f65cd5848328c04328b
Eric Feng <Eric.Feng@amd.com>
2026-03-18 15:27:11 -0700

    reapply windows
    Signed-off-by: Eric Feng <Eric.Feng@amd.com>

--- build_tools/github_actions/configure_ci.py
+++ build_tools/github_actions/configure_ci.py
@@ -119,7 +119,6 @@ CONTROL_JOB_REGEXES = frozenset(
 DEFAULT_POSTSUBMIT_ONLY_JOBS = frozenset(
     [
         "linux_x64_clang_debug",
-        "windows_x64_msvc",
         "test_torch",
     ]
 )
@@ -188,7 +187,7 @@ PRESUBMIT_TOUCH_ONLY_JOBS = [
             "*win32*",
             "*windows*",
             "*msvc*",
-            ".github/worklflows/ci_windows_x64_msvc.yml",
+            ".github/workflows/ci_windows_x64_msvc.yml",
         ],
     ),
     (
