f02ed7d441196af4920a2ca13c49648575724e59
Abhishek Varma <abhvarma@amd.com>
2026-04-08 01:52:48 +0530

    [CPU] Enable generalization of linalg.conv (#23377)
    -- This commit adds InsertBatchDimForBatchlessConv pass after
       RematerializeParallelOps pass to convert a batchless Conv into
       a Conv with unit batch dim.
    -- This is done so that LLVMCPUSelectLoweringStrategy pass can
       correctly assign CPUConvTileAndDecomposeExpert to this unit batch
       dim which was previously batchless.
    -- With this we can get rid of the experimental
    `iree-global-opt-experimental-disable-conv-generalization`
       flag and re-enable linalg.conv generalization.
    -- Aims to resolve https://github.com/iree-org/iree/issues/21955 via the
    short-term approach. Refer [this
    thread](https://github.com/iree-org/iree/issues/21955#issuecomment-3807719144)
    for more context.
    
    Performance number with this PR :
    https://github.com/iree-org/iree/actions/runs/21673429818/job/62488213210?pr=23377:
    
    ```
    ============================== slowest durations ===============================
    109.27s call     quality_tests/model_quality_run.py::sdxl :: unet_fp16_cpu
    54.44s call     quality_tests/model_quality_run.py::sdxl :: unet_fp16_960_1024_cpu
    34.97s call     quality_tests/model_quality_run.py::sd3 :: clip_cpu
    18.31s call     quality_tests/model_quality_run.py::sdxl :: vae_cpu
    18.22s call     quality_tests/model_quality_run.py::sd3 :: vae_cpu
    15.59s call     quality_tests/model_quality_run.py::sdxl :: clip_cpu
    12.54s call     quality_tests/model_quality_run.py::sd3 :: mmdit_cpu
    1.06s call     quality_tests/model_quality_run.py::sdxl :: scheduler_cpu
    ```
    
    Signed-off-by: Abhishek Varma <abhvarma@amd.com>

--- compiler/src/iree/compiler/Codegen/LLVMCPU/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMCPU/Passes.cpp
@@ -630,6 +630,7 @@ void buildLLVMCPUCodegenConfigurationPassPipelineImpl(
       .addPass(createMaterializeDeviceEncodingPass)
       .addPass(createCPUPropagateDataLayoutPass)
       .addPass(createRematerializeParallelOpsPass)
+      .addPass(createInsertBatchDimForBatchlessConvPass)
       // TODO(#13888): This(createExpandF16OpToF32Pass()) pass is being added
       // way to late and should instead be be done during lowering to LLVM.
       .addPass(createExpandF16OpToF32Pass)
