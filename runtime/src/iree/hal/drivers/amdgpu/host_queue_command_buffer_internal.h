5ef3b6a518c730fd9bae473823efcd48808da00a
Ben Vanik <ben.vanik@gmail.com>
2026-04-18 18:21:34 -0700

    Split AMDGPU command buffer replay continuation

---
+++ runtime/src/iree/hal/drivers/amdgpu/host_queue_command_buffer_internal.h
@@ -0,0 +1,54 @@
+// Copyright 2026 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef IREE_HAL_DRIVERS_AMDGPU_HOST_QUEUE_COMMAND_BUFFER_INTERNAL_H_
+#define IREE_HAL_DRIVERS_AMDGPU_HOST_QUEUE_COMMAND_BUFFER_INTERNAL_H_
+
+#include "iree/hal/drivers/amdgpu/host_queue_command_buffer.h"
+#include "iree/hal/drivers/amdgpu/util/aql_emitter.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+// Computes AQL packet control for one replayed command-buffer packet.
+iree_hal_amdgpu_aql_packet_control_t
+iree_hal_amdgpu_host_queue_command_buffer_packet_control(
+    iree_hal_amdgpu_host_queue_t* queue,
+    const iree_hal_amdgpu_wait_resolution_t* resolution,
+    const iree_hal_semaphore_list_t signal_semaphore_list,
+    uint32_t packet_index, bool has_execution_barrier, bool is_final_packet);
+
+// Submits one finalized command-buffer block to the queue.
+iree_status_t iree_hal_amdgpu_host_queue_submit_command_buffer_block(
+    iree_hal_amdgpu_host_queue_t* queue,
+    const iree_hal_amdgpu_wait_resolution_t* resolution,
+    const iree_hal_semaphore_list_t signal_semaphore_list,
+    iree_hal_command_buffer_t* command_buffer,
+    iree_hal_buffer_binding_table_t binding_table,
+    const iree_hal_amdgpu_command_buffer_block_header_t* block,
+    iree_hal_resource_set_t** inout_binding_resource_set,
+    iree_hal_amdgpu_reclaim_action_t pre_signal_action,
+    iree_hal_resource_t* const* operation_resources,
+    iree_host_size_t operation_resource_count,
+    iree_hal_amdgpu_host_queue_submission_flags_t submission_flags,
+    bool* out_ready);
+
+// Starts multi-block command-buffer replay.
+iree_status_t iree_hal_amdgpu_command_buffer_replay_start_under_lock(
+    iree_hal_amdgpu_host_queue_t* queue,
+    const iree_hal_amdgpu_wait_resolution_t* resolution,
+    const iree_hal_semaphore_list_t signal_semaphore_list,
+    iree_hal_command_buffer_t* command_buffer,
+    iree_hal_buffer_binding_table_t binding_table,
+    iree_hal_execute_flags_t execute_flags,
+    iree_hal_resource_set_t** inout_binding_resource_set);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // IREE_HAL_DRIVERS_AMDGPU_HOST_QUEUE_COMMAND_BUFFER_INTERNAL_H_
