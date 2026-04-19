7b2ef3252cf58e4a5ea3f931f7dc3cfe23c148d0
Ben Vanik <ben.vanik@gmail.com>
2026-04-18 21:19:59 -0700

    Test HAL profiling begin validation

--- runtime/src/iree/hal/device_test.cc
+++ runtime/src/iree/hal/device_test.cc
@@ -14,37 +14,218 @@
 namespace iree::hal {
 namespace {
 
-TEST(DeviceProfilingTest, BeginNoneIsCommonNoop) {
-  iree_hal_mock_device_options_t mock_options;
-  iree_hal_mock_device_options_initialize(&mock_options);
+struct CountingProfileSink {
+  // HAL resource header for the sink.
+  iree_hal_resource_t resource;
 
-  iree_hal_device_t* device = NULL;
-  IREE_ASSERT_OK(iree_hal_mock_device_create(&mock_options,
-                                             iree_allocator_system(), &device));
+  // Number of begin-session callbacks observed.
+  int begin_count = 0;
+  // Number of write callbacks observed.
+  int write_count = 0;
+  // Number of end-session callbacks observed.
+  int end_count = 0;
+};
 
-  iree_hal_device_profiling_options_t profiling_options = {0};
-  profiling_options.data_families = IREE_HAL_DEVICE_PROFILING_DATA_NONE;
-  IREE_EXPECT_OK(iree_hal_device_profiling_begin(device, &profiling_options));
-
-  iree_hal_device_release(device);
+static CountingProfileSink* CountingProfileSinkCast(
+    iree_hal_profile_sink_t* sink) {
+  return reinterpret_cast<CountingProfileSink*>(sink);
 }
 
-TEST(DeviceProfilingTest, BeginDataRequiresSink) {
-  iree_hal_mock_device_options_t mock_options;
-  iree_hal_mock_device_options_initialize(&mock_options);
+static void CountingProfileSinkDestroy(iree_hal_profile_sink_t* sink) {
+  (void)sink;
+}
 
-  iree_hal_device_t* device = NULL;
-  IREE_ASSERT_OK(iree_hal_mock_device_create(&mock_options,
-                                             iree_allocator_system(), &device));
+static iree_status_t CountingProfileSinkBeginSession(
+    iree_hal_profile_sink_t* sink,
+    const iree_hal_profile_chunk_metadata_t* metadata) {
+  (void)metadata;
+  ++CountingProfileSinkCast(sink)->begin_count;
+  return iree_ok_status();
+}
+
+static iree_status_t CountingProfileSinkWrite(
+    iree_hal_profile_sink_t* sink,
+    const iree_hal_profile_chunk_metadata_t* metadata,
+    iree_host_size_t iovec_count, const iree_const_byte_span_t* iovecs) {
+  (void)metadata;
+  (void)iovec_count;
+  (void)iovecs;
+  ++CountingProfileSinkCast(sink)->write_count;
+  return iree_ok_status();
+}
+
+static iree_status_t CountingProfileSinkEndSession(
+    iree_hal_profile_sink_t* sink,
+    const iree_hal_profile_chunk_metadata_t* metadata,
+    iree_status_code_t session_status_code) {
+  (void)metadata;
+  (void)session_status_code;
+  ++CountingProfileSinkCast(sink)->end_count;
+  return iree_ok_status();
+}
+
+static const iree_hal_profile_sink_vtable_t kCountingProfileSinkVTable = {
+    .destroy = CountingProfileSinkDestroy,
+    .begin_session = CountingProfileSinkBeginSession,
+    .write = CountingProfileSinkWrite,
+    .end_session = CountingProfileSinkEndSession,
+};
+
+static void CountingProfileSinkInitialize(CountingProfileSink* sink) {
+  iree_hal_resource_initialize(&kCountingProfileSinkVTable, &sink->resource);
+}
+
+static iree_hal_profile_sink_t* CountingProfileSinkAsBase(
+    CountingProfileSink* sink) {
+  return reinterpret_cast<iree_hal_profile_sink_t*>(sink);
+}
+
+class DeviceProfilingTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    iree_hal_mock_device_options_t mock_options;
+    iree_hal_mock_device_options_initialize(&mock_options);
+    IREE_ASSERT_OK(iree_hal_mock_device_create(
+        &mock_options, iree_allocator_system(), &device_));
+  }
+
+  void TearDown() override { iree_hal_device_release(device_); }
+
+  iree_status_t Begin(const iree_hal_device_profiling_options_t* options) {
+    return iree_hal_device_profiling_begin(device_, options);
+  }
+
+  iree_hal_device_t* device_ = NULL;
+};
+
+TEST_F(DeviceProfilingTest, BeginNoneIsCommonNoop) {
+  iree_hal_device_profiling_options_t profiling_options = {0};
+  profiling_options.data_families = IREE_HAL_DEVICE_PROFILING_DATA_NONE;
+  IREE_EXPECT_OK(Begin(&profiling_options));
+}
+
+TEST_F(DeviceProfilingTest, BeginDataRequiresSink) {
+  iree_hal_device_profiling_options_t profiling_options = {0};
+  profiling_options.data_families =
+      IREE_HAL_DEVICE_PROFILING_DATA_DISPATCH_EVENTS;
+  IREE_EXPECT_STATUS_IS(IREE_STATUS_INVALID_ARGUMENT,
+                        Begin(&profiling_options));
+}
+
+TEST_F(DeviceProfilingTest, BeginRejectsUnsupportedCaptureFilterFlags) {
+  iree_hal_device_profiling_options_t profiling_options = {0};
+  profiling_options.capture_filter.flags = 1u << 31;
+  IREE_EXPECT_STATUS_IS(IREE_STATUS_INVALID_ARGUMENT,
+                        Begin(&profiling_options));
+}
+
+TEST_F(DeviceProfilingTest, BeginRejectsReservedCaptureFilterFields) {
+  iree_hal_device_profiling_options_t profiling_options = {0};
+  profiling_options.capture_filter.reserved0 = 1;
+  IREE_EXPECT_STATUS_IS(IREE_STATUS_INVALID_ARGUMENT,
+                        Begin(&profiling_options));
+}
+
+TEST_F(DeviceProfilingTest, BeginRejectsEmptyExecutableExportFilter) {
+  iree_hal_device_profiling_options_t profiling_options = {0};
+  profiling_options.capture_filter.flags =
+      IREE_HAL_PROFILE_CAPTURE_FILTER_FLAG_EXECUTABLE_EXPORT_PATTERN;
+  IREE_EXPECT_STATUS_IS(IREE_STATUS_INVALID_ARGUMENT,
+                        Begin(&profiling_options));
+}
+
+TEST_F(DeviceProfilingTest, BeginRejectsZeroCommandBufferFilter) {
+  iree_hal_device_profiling_options_t profiling_options = {0};
+  profiling_options.capture_filter.flags =
+      IREE_HAL_PROFILE_CAPTURE_FILTER_FLAG_COMMAND_BUFFER_ID;
+  IREE_EXPECT_STATUS_IS(IREE_STATUS_INVALID_ARGUMENT,
+                        Begin(&profiling_options));
+}
+
+TEST_F(DeviceProfilingTest, BeginRejectsCounterSetsWithoutArray) {
+  iree_hal_device_profiling_options_t profiling_options = {0};
+  profiling_options.data_families =
+      IREE_HAL_DEVICE_PROFILING_DATA_COUNTER_SAMPLES;
+  profiling_options.counter_set_count = 1;
+  IREE_EXPECT_STATUS_IS(IREE_STATUS_INVALID_ARGUMENT,
+                        Begin(&profiling_options));
+}
+
+TEST_F(DeviceProfilingTest, BeginRejectsCounterSetsWithoutCounterSamples) {
+  iree_string_view_t counter_name = IREE_SV("SQ_WAVES");
+  iree_hal_profile_counter_set_selection_t counter_set = {0};
+  counter_set.counter_name_count = 1;
+  counter_set.counter_names = &counter_name;
 
   iree_hal_device_profiling_options_t profiling_options = {0};
   profiling_options.data_families =
       IREE_HAL_DEVICE_PROFILING_DATA_DISPATCH_EVENTS;
-  IREE_EXPECT_STATUS_IS(
-      IREE_STATUS_INVALID_ARGUMENT,
-      iree_hal_device_profiling_begin(device, &profiling_options));
+  profiling_options.counter_set_count = 1;
+  profiling_options.counter_sets = &counter_set;
+  IREE_EXPECT_STATUS_IS(IREE_STATUS_INVALID_ARGUMENT,
+                        Begin(&profiling_options));
+}
 
-  iree_hal_device_release(device);
+TEST_F(DeviceProfilingTest, BeginRejectsCounterSamplesWithoutCounterSets) {
+  iree_hal_device_profiling_options_t profiling_options = {0};
+  profiling_options.data_families =
+      IREE_HAL_DEVICE_PROFILING_DATA_COUNTER_SAMPLES;
+  IREE_EXPECT_STATUS_IS(IREE_STATUS_INVALID_ARGUMENT,
+                        Begin(&profiling_options));
+}
+
+TEST_F(DeviceProfilingTest, BeginRejectsCounterSetWithoutCounterNames) {
+  iree_hal_profile_counter_set_selection_t counter_set = {0};
+
+  iree_hal_device_profiling_options_t profiling_options = {0};
+  profiling_options.data_families =
+      IREE_HAL_DEVICE_PROFILING_DATA_COUNTER_SAMPLES;
+  profiling_options.counter_set_count = 1;
+  profiling_options.counter_sets = &counter_set;
+  IREE_EXPECT_STATUS_IS(IREE_STATUS_INVALID_ARGUMENT,
+                        Begin(&profiling_options));
+}
+
+TEST_F(DeviceProfilingTest, BeginRejectsCounterSetWithoutCounterNameArray) {
+  iree_hal_profile_counter_set_selection_t counter_set = {0};
+  counter_set.counter_name_count = 1;
+
+  iree_hal_device_profiling_options_t profiling_options = {0};
+  profiling_options.data_families =
+      IREE_HAL_DEVICE_PROFILING_DATA_COUNTER_SAMPLES;
+  profiling_options.counter_set_count = 1;
+  profiling_options.counter_sets = &counter_set;
+  IREE_EXPECT_STATUS_IS(IREE_STATUS_INVALID_ARGUMENT,
+                        Begin(&profiling_options));
+}
+
+TEST_F(DeviceProfilingTest, BeginRejectsEmptyCounterName) {
+  iree_string_view_t counter_name = iree_string_view_empty();
+  iree_hal_profile_counter_set_selection_t counter_set = {0};
+  counter_set.counter_name_count = 1;
+  counter_set.counter_names = &counter_name;
+
+  iree_hal_device_profiling_options_t profiling_options = {0};
+  profiling_options.data_families =
+      IREE_HAL_DEVICE_PROFILING_DATA_COUNTER_SAMPLES;
+  profiling_options.counter_set_count = 1;
+  profiling_options.counter_sets = &counter_set;
+  IREE_EXPECT_STATUS_IS(IREE_STATUS_INVALID_ARGUMENT,
+                        Begin(&profiling_options));
+}
+
+TEST_F(DeviceProfilingTest, BeginUnsupportedBackendDoesNotTouchSink) {
+  CountingProfileSink sink = {};
+  CountingProfileSinkInitialize(&sink);
+
+  iree_hal_device_profiling_options_t profiling_options = {0};
+  profiling_options.data_families =
+      IREE_HAL_DEVICE_PROFILING_DATA_DISPATCH_EVENTS;
+  profiling_options.sink = CountingProfileSinkAsBase(&sink);
+  IREE_EXPECT_STATUS_IS(IREE_STATUS_UNIMPLEMENTED, Begin(&profiling_options));
+  EXPECT_EQ(0, sink.begin_count);
+  EXPECT_EQ(0, sink.write_count);
+  EXPECT_EQ(0, sink.end_count);
 }
 
 TEST(DeviceProfilingOptionsTest, CloneOwnsBorrowedStringsAndArrays) {
