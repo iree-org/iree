7e8eb3e027ecd29096db4b8705a5690d5805db3d
Max Dawkins <max.dawkins@gmail.com>
2026-04-30 16:08:44 +0000

    Address load grouping pass comments
    Signed-off-by: Max Dawkins <max.dawkins@gmail.com>

--- compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
@@ -1051,10 +1051,10 @@ static void addLowerToLLVMGPUPasses(OpPassManager &modulePassManager,
 
   // Group global loads together to improve AMDGPU instruction scheduling.
   // The transformation is target-agnostic, but currently only enabled for
-  // ROCDL targets.
-  // This must run after SCF->CF lowering and memref optimizations, but before
-  // the func-level adaptor is committed.
-  funcPassManager.addPredicatedPass(forROCDL, createLLVMGPUGroupGlobalLoadsPass);
+  // ROCDL targets until there is data to support that it benefits other
+  // targets.
+  funcPassManager.addPredicatedPass(forROCDL,
+                                    createLLVMGPUGroupGlobalLoadsPass);
 
   // Commit the func-level adaptor before adding module-level passes.
   funcPassManager.commitPass();
