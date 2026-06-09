73acc2bceaf7da0a9d54c372098e16490e40cd21
Rechie Kho <50512341+RechieKho@users.noreply.github.com>
2026-06-09 22:20:20 +0800

    Use aligned variant for memory allocation for proactor.
    Signed-off-by: Rechie Kho <50512341+RechieKho@users.noreply.github.com>

--- runtime/src/iree/async/platform/io_uring/proactor.c
+++ runtime/src/iree/async/platform/io_uring/proactor.c
@@ -116,7 +116,10 @@ iree_status_t iree_async_proactor_create_io_uring(
   // Allocate the proactor structure with trailing data.
   iree_async_proactor_io_uring_t* proactor = NULL;
   IREE_RETURN_AND_END_ZONE_IF_ERROR(
-      z0, iree_allocator_malloc(allocator, total_size, (void**)&proactor));
+      z0,
+      iree_allocator_malloc_aligned(allocator, total_size,
+                                    iree_hardware_destructive_interference_size,
+                                    0, (void**)&proactor));
   memset(proactor, 0, total_size);
 
   // Initialize base fields.
