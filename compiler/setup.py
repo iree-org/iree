e11559a5400afa9bd14e1ef7e0b81705333bcfce
Alex-Wengg <hanweng9@gmail.com>
2026-06-19 14:32:45 -0400

    Drop the _ireeDialects extension from the compiler wheel build
    compiler/setup.py still declared the iree.compiler._mlir_libs._ireeDialects
    CMakeExtension after this PR removed the sources that built it. With
    IREE_BUILD_PYTHON_BINDINGS=ON the wheel build iterates ext_modules and tries
    to link _ireeDialects with no inputs, so gcc fails ("no input files") and the
    iree-base-compiler wheel build errors out. Remove the dead extension entry.
    
    Signed-off-by: Alex-Wengg <hanweng9@gmail.com>

--- compiler/setup.py
+++ compiler/setup.py
@@ -503,9 +503,6 @@ setup(
     },
     ext_modules=[
         CMakeExtension("iree.compiler._mlir_libs._mlir", py_limited_api=_is_abi3_build),
-        CMakeExtension(
-            "iree.compiler._mlir_libs._ireeDialects", py_limited_api=_is_abi3_build
-        ),
         # TODO: MHLO has been broken for a while so disabling. If re-enabling,
         # it also needs to be enabled on the build side.
         # CMakeExtension("iree.compiler._mlir_libs._mlirHlo"),
